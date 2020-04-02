5a9e57c5be97a517e2edd6a4443d68bd2a0a085c
Shaw <shawmakesmusic@gmail.com>
2020-04-02 07:07:11 -0700

    Moved frontend client to react / next

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
+      <Link href="/login">
+        <a>Login</a>
       </Link>
     </p>
   </Layout>
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const AboutPage: React.FunctionComponent = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
+const LoginPage: React.FunctionComponent = () => (
+  <Layout title="LoginPage | Next.js + TypeScript Example">
+    <h1>Login</h1>
+    <p>This is the login page</p>
     <p>
       <Link href="/">
         <a>Go home</a>
@@ -14,4 +14,4 @@ const AboutPage: React.FunctionComponent = () => (
   </Layout>
 )
 
-export default AboutPage
+export default LoginPage
