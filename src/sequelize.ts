b2523c25b2fb53c8bdd3bb8d132a8619ee82fd7f
Shaw <shawmakesmusic@gmail.com>
2020-04-02 04:06:45 -0700

    Init commit

---
+++ src/sequelize.ts
@@ -0,0 +1,33 @@
+import { Sequelize } from 'sequelize';
+import { Application } from './declarations';
+
+export default function (app: Application) {
+  const connectionString = app.get('mysql');
+  const sequelize = new Sequelize(connectionString, {
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      freezeTableName: true
+    }
+  });
+  const oldSetup = app.setup;
+
+  app.set('sequelizeClient', sequelize);
+
+  app.setup = function (...args) {
+    const result = oldSetup.apply(this, args);
+
+    // Set up data relationships
+    const models = sequelize.models;
+    Object.keys(models).forEach(name => {
+      if ('associate' in models[name]) {
+        (models[name] as any).associate(models);
+      }
+    });
+
+    // Sync to the database
+    app.set('sequelizeSync', sequelize.sync());
+
+    return result;
+  };
+}
