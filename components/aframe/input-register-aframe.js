3e0880eefa072175463ea18abdf0593873c96321
Shaw <shawmakesmusic@gmail.com>
2020-04-02 04:02:49 -0700

    Replace Vue with React and Next

--- components/aframe/input-register-aframe.js
+++
@@ -1,5 +0,0 @@
-import { mappings, inputActions } from './input-mappings.js'
-export default function () {
-    AFRAME.registerInputActions(inputActions, 'default')
-    AFRAME.registerInputMappings(mappings)
-}
