cc81dd3496ff5e1d46b8def8366716de7e42fe5f
Shaw <shawmakesmusic@gmail.com>
2020-04-02 17:12:27 -0700

    Stubbed out services -- need to wire up redis to groups and locations

---
+++ test/services/groups.test.ts
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import app from '../../src/app';
+
+describe('\'groups\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('groups');
+
+    assert.ok(service, 'Registered the service');
+  });
+});
