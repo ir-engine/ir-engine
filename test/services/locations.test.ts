638cffe333878cbc6597cad04b6d115946a83ace
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-07 12:17:46 -0400

    Re-add Locations as a Sequelize service.

--- test/services/locations.test.ts
+++ test/services/locations.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import app from '../../src/app'
 
-describe('\'locations\' service', () => {
+describe('\'Locations\' service', () => {
   it('registered the service', () => {
     const service = app.service('locations')
 
