dcc96e007094d4885c0b0bf4765e4248028dabca
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-07 12:17:46 -0400

    Re-add Locations as a Sequelize service.

---
+++ src/models/locations.model.ts
@@ -0,0 +1,28 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+import { Sequelize, DataTypes } from 'sequelize'
+import { Application } from '../declarations'
+
+export default function (app: Application): any {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient')
+  const locations = sequelizeClient.define('locations', {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    hooks: {
+      beforeCount (options: any) {
+        options.raw = true
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  (locations as any).associate = function (models: any) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  }
+
+  return locations
+}
