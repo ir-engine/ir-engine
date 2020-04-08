c0fc5a06ac4edc11a9f51b96e4cc2c3fd67274aa
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-07 23:16:45 -0400

    Add XrLocationInstances on /instances.

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,10 +3,11 @@ import users from './users/users.service'
 import scenes from './scenes/scenes.service'
 import groups from './groups/groups.service'
 import contacts from './contacts/contacts.service'
-import instances from './instances/instances.service'
 import xrAvatars from './xr-avatars/xr-avatars.service'
 import xrLocations from './xr-locations/xr-locations.service'
 import xrObjects from './xr-objects/xr-objects.service'
+
+import xrLocationInstances from './xr-location-instances/xr-location-instances.service'
 
 // Don't remove this comment. It's needed to format import lines nicely.
 
@@ -15,8 +16,8 @@ export default function (app: Application): void {
   app.configure(scenes)
   app.configure(groups)
   app.configure(contacts)
-  app.configure(instances)
   app.configure(xrAvatars)
   app.configure(xrLocations)
   app.configure(xrObjects)
+  app.configure(xrLocationInstances)
 }
--- test/services/xr-location-instances.test.ts
+++ test/services/xr-location-instances.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import app from '../../src/app'
 
-describe('\'instances\' service', () => {
+describe('\'XrLocationInstances\' service', () => {
   it('registered the service', () => {
     const service = app.service('instances')
 
