7d4574b80260f7080261a1cc9f1cef92575bf4bc
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-07 23:23:59 -0400

    Fixup service return type.

--- src/services/xr-location-instances/xr-location-instances.service.ts
+++ src/services/xr-location-instances/xr-location-instances.service.ts
@@ -12,7 +12,7 @@ declare module '../../declarations' {
   }
 }
 
-export default function (app: Application): any {
+export default function (app: Application): void {
   const options = {
     Model: createModel(app),
     paginate: app.get('paginate')
