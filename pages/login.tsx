946420268d69fa7fe05cb7868a2fd3152fefaea0
fashioncstar <apple@mac-dragon.local>
2020-04-10 01:43:44 +0800

    login pge

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignIn from '../components/ui/Auth/Login'
+
+export default class LoginPage extends React.Component {
+  render () {
+    return (
+      <SignIn />
+    )
+  }
+}
