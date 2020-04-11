e40f10ad769eb445f8e736aa88f61fdd56283410
Kyle Baran <kbaran@bitscoop.com>
2020-04-11 15:43:41 -0700

    Added landing page and 360 video gallery.
    Resolves kxr-dev #12
    
    Added a barebones landing page.
    Page linkes to /space (XR demo space) and /videos
    (360 video gallery).
    Video gallery pulls from /public-video on xrchat-server,
    then populates page with barebones links to the 'link'
    field on each video.

---
+++ redux/video/actions.ts
@@ -0,0 +1,40 @@
+import {
+    VIDEOS_FETCHED_SUCCESS,
+    VIDEOS_FETCHED_ERROR
+} from '../actions';
+
+export interface PublicVideoState {
+    videos: PublicVideo[];
+    error: string;
+}
+
+export interface PublicVideo {
+    id: number;
+    title: string;
+    original_title: string;
+    description: string;
+    link: string;
+}
+
+
+export interface VideosFetchedAction {
+    type: string;
+    videos?: PublicVideo[];
+    message?: string;
+}
+
+
+export function videosFetchedSuccess(videos: PublicVideo[]): VideosFetchedAction {
+    return {
+        type: VIDEOS_FETCHED_SUCCESS,
+        videos: videos
+    }
+}
+
+
+export function videosFetchedError(err: string): VideosFetchedAction {
+    return {
+        type: VIDEOS_FETCHED_ERROR,
+        message: err
+    }
+}
