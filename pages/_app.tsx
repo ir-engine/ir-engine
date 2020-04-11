b29daa593ec772026b02afc018d109b2abdb77d1
fashioncstar <apple@mac-dragon.local>
2020-04-11 11:42:38 +0800

    fix next app architecture

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,46 @@
 import App from 'next/app';
-import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import React, { Fragment } from 'react';
 import withRedux from 'next-redux-wrapper';
 import { Provider } from 'react-redux';
 import { fromJS } from 'immutable';
-
 import { configureStore } from '../redux/store'
 import { Store } from 'redux';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../components/assets/theme';
 
-interface Props {
+interface Props extends AppProps{
     store: Store;
 }
+
 class MyApp extends App<Props> {
-  static async getInitialProps(props: any) {
-    const { Component, ctx } = props;
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps({ ctx });
-    }
-    return { pageProps };
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles)
   }
 
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <Fragment>
+          <Head>
+            <title>XRChat</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+          </Head>
+          <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Provider store={store}>
+              <Component {...pageProps} />
+            </Provider>
+          </ThemeProvider>
+        </Fragment>
     );
   }
 }
+
 
 export default withRedux(configureStore, {
   serializeState: (state) => state.toJS(),
