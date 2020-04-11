ddca5416ed426e0dfb350235f552f2fc6d6a59a9
fashioncstar <apple@mac-dragon.local>
2020-04-11 11:43:51 +0800

    fix register component

--- components/ui/Auth/Register.tsx
+++ components/ui/Auth/Register.tsx
@@ -1,117 +1,163 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import GoogleIcon from '../../assets/GoogleIcon';
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
+import { 
+  loginUserByGithub,
+  registerUserByEmail,
+  loginUserByGoogle,
+  loginUserByFacebook,
+} from '../../../redux/auth/service'
+import { selectAuthState } from '../../../redux/auth/selector'
+import './auth.scss'
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+interface Props {
+  auth: any
+  registerUserByEmail: typeof registerUserByEmail
+  loginUserByGithub: typeof loginUserByGithub
+  loginUserByGoogle: typeof loginUserByGoogle
+  loginUserByFacebook: typeof loginUserByFacebook
+};
 
-export default function SignUp() {
-  const classes = useStyles();
+const mapStateToProps = (state: any) => {
+  return {
+    auth: selectAuthState(state),
+  }
+};
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  registerUserByEmail: bindActionCreators(registerUserByEmail, dispatch),
+  loginUserByGithub: bindActionCreators(loginUserByGithub, dispatch),
+  loginUserByGoogle: bindActionCreators(loginUserByGoogle, dispatch),
+  loginUserByFacebook: bindActionCreators(loginUserByFacebook, dispatch),
+});
+
+class SignUp extends React.Component<Props> {
+  state = {
+    email: '',
+    password: '',
+    phone: ''
+  }
+
+  handleRegister = (e: any) => {
+    e.preventDefault()
+    this.props.registerUserByEmail({
+      email: this.state.email,
+      password: this.state.password
+    });
+  }
+
+  handleGithubLogin = (e: any) => {
+    e.preventDefault()
+    this.props.loginUserByGithub();
+  }
+
+  handleGoogleLogin = (e: any) => {
+    e.preventDefault()
+    this.props.loginUserByGithub();
+  }
+
+  handleFacebookLogin = (e: any) => {
+    e.preventDefault()
+    this.props.loginUserByGithub();
+  }
+
+  render () {
+    return (
+      <Container component="main" maxWidth="xs">
+        <div className={'paper'}>
+          <Avatar className={'avatar'}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={'form'} noValidate onSubmit={(e) => this.handleRegister(e) }>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={'submit'}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/auth/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
             </Grid>
+          </form>
+        </div>
+
+        <div style={{marginTop: '20px'}}>
+          &nbsp;
+        </div>
+        
+        <Grid container justify="center" spacing={2}>
+          <Grid item>
+            <Button onClick={(e) => this.handleGithubLogin(e)}>
+              <GitHubIcon fontSize="large"/>
+            </Button>
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
+          <Grid item>
+            <Button onClick={(e) => this.handleFacebookLogin(e)}>
+              <FacebookIcon fontSize="large"/>
+            </Button>
           </Grid>
-        </form>
-      </div>
-
-      <div style={{marginTop: '20px'}}>
-        &nbsp;
-      </div>
-      
-      <Grid container justify="center" spacing={2}>
-        <Grid item>
-          <Link href="#" variant="body2">
-            <GitHubIcon fontSize="large"/>
-          </Link>
+          <Grid item>
+            <Button onClick={(e) => this.handleGoogleLogin(e)}>
+              <GoogleIcon/>
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Link href="#" variant="body2">
-            <FacebookIcon fontSize="large"/>
-          </Link>
-        </Grid>
-        <Grid item>
-          <Link href="#" variant="body2">
-            <GoogleIcon/>
-          </Link>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
+
+function SignUpWrapper(props: any) {
+  return <SignUp {...props}/>
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUpWrapper);
