b544ed804fd2ae00d194e171ee0c2f8afb31fb88
Kyle Baran <kbaran@bitscoop.com>
2020-04-11 15:43:41 -0700

    Added landing page and 360 video gallery.
    Resolves kxr-dev #12
    
    Added a barebones landing page.
    Page linkes to /space (XR demo space) and /videos
    (360 video gallery).
    Video gallery pulls from /public-video on xrchat-server,
    then populates page with barebones links to the 'link'
    field on each video.

--- redux/reducers.ts
+++ redux/reducers.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux-immutable';
 import authReducer from './auth/reducers';
+import videoReducer from "./video/reducers";
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    videos: videoReducer
 });
