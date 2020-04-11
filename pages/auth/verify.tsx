556dae424b6726efb07d601d5a3ba057c4e0cf93
fashioncstar <apple@mac-dragon.local>
2020-04-11 11:44:26 +0800

    add verify email page

---
+++ pages/auth/verify.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VerifyEmail from '../../components/ui/Auth/VerifyEmail'
+
+export default class VerifyEmailPage extends React.Component {
+  render () {
+    return (
+      <VerifyEmail />
+    )
+  }
+}
