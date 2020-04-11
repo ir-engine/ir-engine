3dd1f6c8363558202109d12b92e2d92524383203
fashioncstar <apple@mac-dragon.local>
2020-04-11 11:43:22 +0800

    fix register page

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignUp from '../components/ui/Auth/Register'
+import SignUp from '../../components/ui/Auth/Register'
 
 export default class RegisterPage extends React.Component {
   render () {
