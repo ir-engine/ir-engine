eaa3dbadc2fa11026f53ab77ea97abc47bf07d8f
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-13 19:51:07 -0400

    Fix lint errors.

--- src/services/magiclink/magiclink.service.ts
+++ src/services/magiclink/magiclink.service.ts
@@ -1,26 +1,26 @@
 // Initializes the `magiclink` service on path `/magiclink`
-import { ServiceAddons } from '@feathersjs/feathers';
-import { Application } from '../../declarations';
-import { Magiclink } from './magiclink.class';
-import hooks from './magiclink.hooks';
+import { ServiceAddons } from '@feathersjs/feathers'
+import { Application } from '../../declarations'
+import { Magiclink } from './magiclink.class'
+import hooks from './magiclink.hooks'
 
 // Add this service to the service type index
 declare module '../../declarations' {
   interface ServiceTypes {
-    'magiclink': Magiclink & ServiceAddons<any>;
+    'magiclink': Magiclink & ServiceAddons<any>
   }
 }
 
-export default function (app: Application) {
+export default function (app: Application): void {
   const options = {
     paginate: app.get('paginate')
-  };
+  }
 
   // Initialize our service with any options it requires
-  app.use('/magiclink', new Magiclink(options, app));
+  app.use('/magiclink', new Magiclink(options, app))
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('magiclink');
+  const service = app.service('magiclink')
 
-  service.hooks(hooks);
+  service.hooks(hooks)
 }
