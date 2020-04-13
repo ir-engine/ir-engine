104f4776ae68db6244c0f631c0a0d43cd0376c31
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-13 19:51:07 -0400

    Fix lint errors.

--- test/services/magiclink.test.ts
+++ test/services/magiclink.test.ts
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import app from '../../src/app';
+import assert from 'assert'
+import app from '../../src/app'
 
 describe('\'magiclink\' service', () => {
   it('registered the service', () => {
-    const service = app.service('magiclink');
+    const service = app.service('magiclink')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
