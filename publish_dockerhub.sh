e73b1928144dc187ff44828d6447a4d66a9e33db
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-04-14 17:40:01 +0200

    Revert "push the tag and latest"
    This reverts commit 19fb70cadcd4bbdee820aebc0a17e2df5809ea12.

--- publish_dockerhub.sh
+++ publish_dockerhub.sh
@@ -6,12 +6,15 @@ set -x
 
 TAG=$1
 
+echo "$TAG"
+
 
 docker build --tag xrchat/server .
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker tag xrchat/server xrchat/server:latest
-docker push xrchat/server:latest
+# Comment until everything works ok.
+# docker tag xrchat/server xrchat/server:latest
+# docker push xrchat/server:latest
 
 docker tag xrchat/server xrchat/server:${TAG}
 docker push xrchat/server:${TAG}
