72ed5476408ac4b278793a98caa2a4da302583b6
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-04-14 17:35:54 +0200

    push the tag and latest

--- publish_dockerhub.sh
+++ publish_dockerhub.sh
@@ -6,15 +6,12 @@ set -x
 
 TAG=$1
 
-echo "$TAG"
-
 
 docker build --tag xrchat/server .
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Comment until everything works ok.
-# docker tag xrchat/server xrchat/server:latest
-# docker push xrchat/server:latest
+docker tag xrchat/server xrchat/server:latest
+docker push xrchat/server:latest
 
 docker tag xrchat/server xrchat/server:${TAG}
 docker push xrchat/server:${TAG}
