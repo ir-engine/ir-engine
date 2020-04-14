0790c0b42054b73f183c401512f51cf3bd962180
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-04-14 16:59:23 +0200

    add script to push to dockerhub

---
+++ publish_dockerhub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+
+# I added DOCKER_HUB_PASSWORD as a secret on Github repo
+
+TAG="$(git describe --abbrev=0 --tags)"
+
+
+docker build --tag xrchat/server .
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Comment until everything works ok.
+# docker tag xrchat/server xrchat/server:latest
+# docker push xrchat/server:latest
+
+docker tag xrchat/server xrchat/server:${TAG}
+docker push xrchat/server:${TAG}
