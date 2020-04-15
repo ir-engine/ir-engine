1513ef1e4d8d0fefd37c3c8c1b55c8929ca31bc9
shawticus <shawmakesmagic@gmail.com>
2020-04-15 03:40:04 -0700

    Add dark ground plane

--- components/ui/NavMenu/index.tsx
+++ components/ui/NavMenu/index.tsx
@@ -1,7 +1,6 @@
 import NavUserWidget from '../NavUserWidget'
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
 import Logo from '../../../assets/logo.png'
 import './style.scss'
 
@@ -9,20 +8,10 @@ export default class NavMenu extends Component {
   render() {
     return (
       <AppBar className="appbar" position="sticky">
-        <Toolbar>
-          <div className="logo">
-            <img
-              src={Logo}
-              alt="logo"
-              crossOrigin="anonymous"
-              className="logo"
-            />
-          </div>
-
-          <div className="gap">&ngsp;</div>
-          
-          <NavUserWidget />
-        </Toolbar>
+        <div className="logo">
+          <img src={Logo} alt="logo" crossOrigin="anonymous" className="logo" />
+        </div>
+        <NavUserWidget />
       </AppBar>
     )
   }
