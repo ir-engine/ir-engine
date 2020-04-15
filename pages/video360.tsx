3ee6824202d1ca01fa5a233980c7e6f2d5d1013c
mohrtw <mohrtw@gmail.com>
2020-04-15 04:26:05 -0700

    hooked grid up to videos

--- pages/video360.tsx
+++ pages/video360.tsx
@@ -1,5 +1,6 @@
-import Scene from '../components/xr/scene/scene-video'
 import React from 'react'
+import dynamic from 'next/dynamic'
+const Scene = dynamic(() => import('../components/xr/scene/scene-video'), { ssr: false })
 
 export default class IndexPage extends React.Component {
   render () {
