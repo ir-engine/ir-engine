f4f7d54ac9b636c35cf83575aa02912d7714a50a
mohrtw <mohrtw@gmail.com>
2020-04-15 03:03:36 -0700

    working on route when clicking on grid image

--- components/xr/scene/scene-video.tsx
+++ components/xr/scene/scene-video.tsx
@@ -5,9 +5,6 @@ import Assets from './assets'
 import Video360 from '../video360/Video360Room'
 import './index.scss'
 
-import getConfig from 'next/config'
-const config = getConfig().publicRuntimeConfig.xr['networked-scene']
-
 type State = {
   appRendered?: boolean
   color?: string
@@ -32,7 +29,6 @@ export default class VideoScene extends React.Component<State> {
       <div style={{ height: '100%', width: '100%' }}>
         {this.state.appRendered && (
           <Scene
-            networked-scene={config}
             class="scene"
             renderer="antialias: true"
             background="color: #FAFAFA"
