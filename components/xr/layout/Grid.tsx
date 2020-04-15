5bce05833142ac18e59ab66c2b858a9b79b576f8
mohrtw <mohrtw@gmail.com>
2020-04-15 03:03:36 -0700

    working on route when clicking on grid image

--- components/xr/layout/Grid.tsx
+++ components/xr/layout/Grid.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 // @ts-ignore
 import { Entity } from 'aframe-react'
 import { CylindricalGrid } from '../../../classes/aframe/layout/GridUtils'
@@ -10,85 +11,73 @@ const playerHeight = getConfig().publicRuntimeConfig.xr.playerHeight
 const range = (n: number) => Array.from(Array(n).keys())
 const numbers = range(config.rows * config.columns)
 
-export default class GridLayout extends React.Component {
-  cylindricalGrid: CylindricalGrid
+function GridLayout () {
+  const router = useRouter()
+  const cylindricalGrid: CylindricalGrid = new CylindricalGrid(config.gridCellsPerRow, config.cellHeight,
+    config.radius, config.rows, config.columns)
 
-  constructor(props: any) {
-    super(props)
-    this.cylindricalGrid = new CylindricalGrid(config.gridCellsPerRow, config.cellHeight,
-      config.radius, config.rows, config.columns)
-
-    this.gridRotation = this.gridRotation.bind(this)
-    this.gridOffsetY = this.gridOffsetY.bind(this)
-    this.gridCellRotation = this.gridCellRotation.bind(this)
-    this.gridCellPosition = this.gridCellPosition.bind(this)
-    this.gridRotationString = this.gridRotationString.bind(this)
-    this.gridOffsetString = this.gridOffsetString.bind(this)
-    this.Grid = this.Grid.bind(this)
-    this.followLink = this.followLink.bind(this)
-  }
-
-  gridRotation() {
+  function gridRotation() {
     return (180 - (360 / config.gridCellsPerRow) * 2)
   }
 
-  gridOffsetY() {
+  function gridOffsetY() {
     console.log((1 - config.rows / 2) * config.cellHeight)
     return (1 - config.rows / 2) * config.cellHeight
   }
 
-  gridCellRotation (itemNum: number) {
-    const rot = this.cylindricalGrid.cellRotation(itemNum)
+  function gridCellRotation (itemNum: number) {
+    const rot = cylindricalGrid.cellRotation(itemNum)
     return `${rot.x} ${rot.y + 180} ${rot.z}`
   }
 
-  gridCellPosition (itemNum: number) {
-    const pos = this.cylindricalGrid.cellPosition(itemNum)
+  function gridCellPosition (itemNum: number) {
+    const pos = cylindricalGrid.cellPosition(itemNum)
     return `${pos.x} ${pos.y} ${pos.z}`
   }
 
-  gridRotationString (): string {
-    return '0 ' + this.gridRotation() + ' 0'
+  function gridRotationString (): string {
+    return '0 ' + gridRotation() + ' 0'
   }
 
-  gridOffsetString (): string {
-    return '0 ' + (this.gridOffsetY() + playerHeight) + ' 0'
+  function gridOffsetString (): string {
+    return '0 ' + (gridOffsetY() + playerHeight) + ' 0'
   }
 
-  followLink() {
+  function followLink() {
     console.log('image clicked!')
+    router.push('/video360?manifest=%2Fvideo360%2Fstone%2Fstream.mpd')
   }
 
-  Grid(props: any) {
+  function Grid(props: any) {
     const numbers = props.numbers
 
     const gridItems = numbers.map((number: number) =>
       <Entity key={number.toString()}
-        position={ this.gridCellPosition(number) }
-        rotation={ this.gridCellRotation(number) }>
+        position={ gridCellPosition(number) }
+        rotation={ gridCellRotation(number) }>
         <Entity
           primitive="a-image"
           class='clickable'
           src="#placeholder"
           width={ config.cellWidth }
           height={ config.cellContentHeight }
-          events={{ click: this.followLink } }>
+          events={{ click: followLink } }>
         </Entity>
       </Entity>
     )
     return gridItems
   }
 
-  render() {
-    return (
-      <Entity
-        class="grid-cylinder"
-        rotation={this.gridRotationString()}
-        position={this.gridOffsetString()}>
+  return (
+    <Entity
+      class="grid-cylinder"
+      rotation={gridRotationString()}
+      position={gridOffsetString()}>
 
-        { this.Grid({ numbers: numbers }) }
+      { Grid({ numbers: numbers }) }
 
-      </Entity>
-    )
-  }
+    </Entity>
+  )
 }
+
+export default GridLayout
