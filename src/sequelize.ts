9decaf33c8d0ad7fe568429606b848fc8e048c21
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-04-15 23:31:55 +0500

    #27 Calling associating method

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -26,9 +26,12 @@ export default (app: Application): void => {
   app.setup = function (...args: any) {
     // Set up data relationships
     const models = sequelize.models
-    Object.keys(models).forEach(name =>
-      'associate' in models[name] ?? (models[name] as any).associate(models)
-    )
+
+    Object.keys(models).forEach((name) => {
+      if ('associate' in models[name]) {
+        (models[name] as any).associate(models);
+      }
+    })
 
     app.set('sequelizeSync', sequelize.sync()) // Sync to the database
 
