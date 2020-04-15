0ba4ea65cff42d45440a22f4e6e0e0350a90d3fd
shawticus <shawmakesmagic@gmail.com>
2020-04-15 03:53:19 -0700

    Work on video grid

--- components/xr/scene/scene-video-grid.tsx
+++ components/xr/scene/scene-video-grid.tsx
@@ -4,7 +4,8 @@ import { Scene, Entity } from 'aframe-react'
 import Assets from './assets'
 import Grid from '../layout/Grid'
 import Skybox from './skybox-grid'
-import './index.scss'
+import './style.scss'
+import SvgVr from '../../icons/svg/Vr'
 
 import getConfig from 'next/config'
 const config = getConfig().publicRuntimeConfig.xr['networked-scene']
@@ -39,12 +40,19 @@ export default class VideoScene extends React.Component<State> {
             background="color: #FAFAFA"
             embedded
           >
-            <Assets/>
-            <Grid/>
-            <Skybox/>
-            <a-plane color="#000" height="20000" width="200000" rotation="-90 0 0"></a-plane>
-            <Entity
-              player="fuseCursor: true"/>
+            <Assets />
+            <Grid />
+            <Skybox />
+            <a-plane
+              color="#000"
+              height="20000"
+              width="200000"
+              rotation="-90 0 0"
+            ></a-plane>
+            <Entity player="fuseCursor: true" />
+            <a className="enterVR" id="enterVRButton" href="#">
+              <SvgVr className="enterVR" />
+            </a>
           </Scene>
         )}
       </div>
