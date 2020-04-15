4997ff16523dbcf2ae978a6af471ed1a066ae3bc
mohrtw <mohrtw@gmail.com>
2020-04-15 01:03:21 -0700

    Grid view

--- components/xr/scene/assets-default.tsx
+++ components/xr/scene/assets-default.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import getConfig from 'next/config'
 const config = getConfig().publicRuntimeConfig.xr.environment
+const grid = getConfig().publicRuntimeConfig.xr.grid
 
 export default class DefaultAssets extends React.Component {
   componentDidMount() {
@@ -20,12 +21,23 @@ export default class DefaultAssets extends React.Component {
           src={config.skybox.src}
           crossOrigin="anonymous"
         />
+        <img
+          id="gridSky"
+          src={grid.skybox.src}
+          crossOrigin="anonymous"
+        />
+        <img
+          id="placeholder"
+          src={grid.placeholderImageSrc}
+          crossOrigin="anonymous"
+        />
         <a-gltf-model id={config['scene-gltf'].name}
           src={config['scene-gltf'].src}
           crossOrigin="anonymous"/>
 
         <video id="video360Shaka"
-        ></video>
+          crossOrigin="anonymous">
+        </video>
       </div>
     )
   }
