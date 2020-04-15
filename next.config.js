4e4f93378e90965b312ac4e4683fb60b061a90a7
shawticus <shawmakesmagic@gmail.com>
2020-04-14 21:30:58 -0700

    Pushing login component over, adding logo

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ module.exports = withImages(
     publicRuntimeConfig: config.publicRuntimeConfig,
     auth: config.auth,
     webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000
-          }
-        }
-      })
       return config
     }
   })
