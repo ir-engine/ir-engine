2693feeac7924eeb9dc54158b4ea400f3a74ba8b
Kyle Baran <kbaran@bitscoop.com>
2020-04-14 21:42:10 -0700

    Updated video conversion hook. fluent-ffmpeg and fluent-bento4 were both not working correctly and were removed as a result. *Bento4 was scrapped for generating DASH output in favor of ffmpeg. *fluent-ffmpeg was mangling DASH output even though the command it was  supposed to be executing ran flawlessly when run manually in a shell. Hook now downloads best quality video from source as mp4 and writes to disk. ffmpeg is called via exec() to convert it to DASH output, also on disk. Finally, s3BlobBucket is used to upload all of the DASH files to /<file-id> on S3, and all local files are deleted when that's done. Hook was renamed to voncert-video since it now can do Vimeo as well.

--- src/hooks/ffmpeg/mse.ts
+++
@@ -1,6 +0,0 @@
-exports.load = function (command: any) {
-  command.videoCodec('libx264')
-    .format('mp4')
-    .outputOptions('-movflags frag_keyframe+empty_moov+faststart')
-    .outputOptions('-crf 18')
-}
