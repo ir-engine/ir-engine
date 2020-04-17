312a0eaeeadd9e1f2aac8a05c25102172b15ebd9
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-04-17 17:34:25 +0500

    #26 Implemented group API

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -4,17 +4,45 @@ import { Application } from '../declarations'
 export default (app: Application): any => {
   const sequelizeClient: Sequelize = app.get('sequelizeClient')
   const Group = sequelizeClient.define('group', {
-    text: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
+      primaryKey: true
+    },
+    ownerId: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    isPublic: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
   }, {
     hooks: {
       beforeCount (options: any) {
         options.raw = true
       }
-    }
-  })
+    },
+    indexes: [
+      {
+        unique: true,
+        fields: ['id', 'ownerId']
+      },
+      {
+        fields: ['isPublic']
+      }
+    ]
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  (Group as any).associate = (models: any) => {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+
+    (Group as any).belongsTo(models.user, { foreignKey: 'ownerId' });
+    (Group as any).belongsToMany(models.user, { through: models.group_member, otherKey: 'userId' })
+  }
 
   return Group
 }
