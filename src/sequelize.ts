62020f7cec97d59f5431d62c4e8dbf3cf2fc9ed3
fashioncstar <apple@mac-dragon.local>
2020-04-18 20:26:43 +0800

    sms integrated

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -29,7 +29,7 @@ export default (app: Application): void => {
 
     Object.keys(models).forEach((name) => {
       if ('associate' in models[name]) {
-        (models[name] as any).associate(models);
+        (models[name] as any).associate(models)
       }
     })
 
