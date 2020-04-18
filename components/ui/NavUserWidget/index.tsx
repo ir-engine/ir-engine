1d9eb7a243019a22f3c2178a81ef03a8804b10a0
fashioncstar <apple@mac-dragon.local>
2020-04-18 10:45:41 +0800

    login as dialog

--- components/ui/NavUserWidget/index.tsx
+++ components/ui/NavUserWidget/index.tsx
@@ -1,16 +1,14 @@
-import Link from '@material-ui/core/Typography'
-import NextLink from 'next/link'
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
-
-import './style.scss'
-
+import SignIn from '../Auth/Login'
 import { logoutUser } from '../../../redux/auth/service'
 import { selectAuthState } from '../../../redux/auth/selector'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 import Avatar from '@material-ui/core/Avatar'
+import { showDialog } from '../../../redux/dialog/service'
 
+import './style.scss'
 // Get auth state from redux
 // Get user email address
 // If not logged in, show login
@@ -19,6 +17,7 @@ import Avatar from '@material-ui/core/Avatar'
 type Props = {
   auth: any
   logoutUser: typeof logoutUser
+  showDialog: typeof showDialog
 }
 
 const mapStateToProps = (state: any) => {
@@ -28,7 +27,8 @@ const mapStateToProps = (state: any) => {
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  logoutUser: bindActionCreators(logoutUser, dispatch)
+  logoutUser: bindActionCreators(logoutUser, dispatch),
+  showDialog: bindActionCreators(showDialog, dispatch)
 })
 
 class NavUserBadge extends Component<Props> {
@@ -63,12 +63,15 @@ class NavUserBadge extends Component<Props> {
           </div>
         )}
         {!isLoggedIn && (
-          <Button>
-            <NextLink href="/auth/login">
-              <Link className="loginText" title="Login">
-                Login
-              </Link>
-            </NextLink>
+          <Button 
+            className="loginButton"
+            onClick={() => this.props.showDialog({
+              children: (
+                <SignIn />
+              )
+            })}
+            >
+            Log In
           </Button>
         )
         }
---
+++ redux/dialog/service.ts
@@ -0,0 +1,16 @@
+import { Dispatch } from "redux"
+import {
+  dialogShow,
+  dialogClose
+} from "./actions"
+
+export function showDialog(content: any) {
+  return (dispatch: Dispatch) => {
+    dispatch(dialogShow(content))
+  }
+}
+export function closeDialog() {
+  return (dispatch: Dispatch) => {
+    dispatch(dialogClose())
+  }
+}
