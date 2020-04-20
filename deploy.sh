44498078b3c47213742c579b1fd722d9e88a0c34
shawticus <shawmakesmagic@gmail.com>
2020-04-20 07:59:13 -0700

    Maybe fixed submodule problem

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t docsy # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
