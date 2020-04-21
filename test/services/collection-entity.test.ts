9cb4da26c1430012cc376b79bfb8dbea97964121
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-04-21 14:21:54 -0400

    Fix collection-entity test.

--- test/services/collection-entity.test.ts
+++ test/services/collection-entity.test.ts
@@ -3,7 +3,8 @@ import app from '../../src/app'
 
 describe('\'collection-entity\' service', () => {
   it('registered the service', () => {
-    const service = app.service('collection/:collectionId/entity/:entityId')
+    // const service = app.service('collection/:collectionId/entity/:entityId')
+    const service = app.service('collection-entity')
 
     assert.ok(service, 'Registered the service')
   })
