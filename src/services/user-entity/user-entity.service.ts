066f48ad163a7907ac67c256f303662c4429f34c
shawticus <shawmakesmagic@gmail.com>
2020-04-20 19:36:40 -0700

    Linted to pass tests

--- src/services/user-entity/user-entity.service.ts
+++ src/services/user-entity/user-entity.service.ts
@@ -1,28 +1,28 @@
 // Initializes the `user-entity` service on path `/user-entity`
-import { ServiceAddons } from '@feathersjs/feathers';
-import { Application } from '../../declarations';
-import { UserEntity } from './user-entity.class';
-import createModel from '../../models/user-entity.model';
-import hooks from './user-entity.hooks';
+import { ServiceAddons } from '@feathersjs/feathers'
+import { Application } from '../../declarations'
+import { UserEntity } from './user-entity.class'
+import createModel from '../../models/user-entity.model'
+import hooks from './user-entity.hooks'
 
 // Add this service to the service type index
 declare module '../../declarations' {
   interface ServiceTypes {
-    'user-entity': UserEntity & ServiceAddons<any>;
+    'user-entity': UserEntity & ServiceAddons<any>
   }
 }
 
-export default function (app: Application) {
+export default (app: Application): any => {
   const options = {
     Model: createModel(app),
     paginate: app.get('paginate')
-  };
+  }
 
   // Initialize our service with any options it requires
-  app.use('/user-entity', new UserEntity(options, app));
+  app.use('/user-entity', new UserEntity(options, app))
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('user-entity');
+  const service = app.service('user-entity')
 
-  service.hooks(hooks);
+  service.hooks(hooks)
 }
--- test/services/user-resource.test.ts
+++ test/services/user-resource.test.ts
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import app from '../../src/app';
+import assert from 'assert'
+import app from '../../src/app'
 
 describe('\'user-resource\' service', () => {
   it('registered the service', () => {
-    const service = app.service('user-resource');
+    const service = app.service('user-resource')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
