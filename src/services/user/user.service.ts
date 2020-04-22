efb0bc2f8f2e458c0551b9413ca6fcde3af3cbba
shawticus <shawmakesmagic@gmail.com>
2020-04-21 20:46:49 -0700

    Added seeder and tested on user

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -14,7 +14,8 @@ declare module '../../declarations' {
 export default (app: Application): void => {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get('paginate'),
+    multi: true
   }
 
   app.use('/user', new User(options, app))
