eb8a5fa2cf6a4eeef0be1294b51ba7c6d4cd7e08
fashioncstar <apple@mac-dragon.local>
2020-04-27 06:00:26 +0800

    fix linting

--- test/services/identity-provider.test.ts
+++ test/services/identity-provider.test.ts
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import app from '../../src/app';
+import assert from 'assert'
+import app from '../../src/app'
 
 describe('\'identity-provider\' service', () => {
   it('registered the service', () => {
-    const service = app.service('identity-provider');
+    const service = app.service('identity-provider')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
