b2fec798ce87e5c181954d4f9a1915b530e8ab59
mohrtw <mohrtw@gmail.com>
2020-04-27 11:21:12 -0700

    metadata json parse

--- redux/video/service.ts
+++ redux/video/service.ts
@@ -13,6 +13,9 @@ export function fetchPublicVideos() {
   return (dispatch: Dispatch) => {
     client.service('static-resource').find({ query: { mime_type: 'application/dash+xml' }})
       .then((res: any) => {
+        for (const video of res.data) {
+          video.metadata = JSON.parse(video.metadata)
+        }
         const videos = res.data as PublicVideo[]
 
         return dispatch(videosFetchedSuccess(videos))
