4f8ad0fddffb3eac9e55dee3cd623722fa430bff
Ayushi <25ayushi1995@gmail.com>
2020-04-30 10:44:41 -0700

    uploading in progress

--- components/ui/NavUserWidget/index.tsx
+++ components/ui/NavUserWidget/index.tsx
@@ -56,7 +56,7 @@ class NavUserBadge extends Component<Props> {
 
     return (
       <div className="userWidget">
-        {isLoggedIn && (
+        {!isLoggedIn && (
           <div className="flex">
             {/* <Button onClick={() => this.handleLogout()} 
               style={this.styles.logoutButton}
@@ -74,7 +74,7 @@ class NavUserBadge extends Component<Props> {
             </Dropdown>
           </div>
         )}
-        {!isLoggedIn && (
+        {isLoggedIn && (
           <Button 
             style={this.styles.loginButton}
             onClick={() => this.props.showDialog({
--- components/ui/Profile/UserIcon.tsx
+++ components/ui/Profile/UserIcon.tsx
@@ -2,23 +2,37 @@ import React from 'react'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
 import './style.scss'
+import { uploadFile } from '../../../redux/video/service'
+import { bindActionCreators, Dispatch } from 'redux'
+import { connect } from 'react-redux'
 
+interface profileProps {
+  uploadFile: typeof uploadFile
+}
 
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  uploadFile: bindActionCreators(uploadFile, dispatch)
+})
 
-
-
-const UserProfile: React.FC = () => {
+const UserProfile: React.FC<profileProps> = (props) => {
 
     const [file,setFile] = React.useState('')
-    const handleChange = (file:any) =>{
+    const handleChange = (e:any) =>{
+      let file = e.target.files
        setFile(file)
       }
+
+    const handleSubmit = async () => {
+      let data = new FormData();
+      data.append('file',file)
+      await props.uploadFile(data)
+    }
     return (
         <div className="uploadform">
         <label htmlFor="fileInput">
            
         {file ? <img
-          src={file}
+          src={URL.createObjectURL(file[0])}
           className="rounded mx-auto d-block"
           width="200px"
           height="150px"
@@ -33,11 +47,11 @@ const UserProfile: React.FC = () => {
         className="signup__fileField"
         onChange={handleChange}
       />
-      <Button variant="contained" color="primary">
-      Upload Avatar
+      <Button variant="contained" color="primary" onClick={handleSubmit}>
+        Upload Avatar
       </Button>
       </div>
     )
 }
 
-export default UserProfile;
+export default connect(null, mapDispatchToProps)(UserProfile);
