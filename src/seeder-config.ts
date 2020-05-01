9f6aaf5d02d9c360a4b9717d3527119ca1598f88
shawticus <shawmakesmagic@gmail.com>
2020-04-30 20:18:14 -0700

    Fixed identity providers

--- src/seeder-config.ts
+++ src/seeder-config.ts
@@ -3,23 +3,25 @@ import AccessControlScopeSeed from './services/access-control-scope/access-contr
 import CollectionType from './services/collection-type/collection-type.seed'
 import ComponentTypeSeed from './services/component-type/component-type.seed'
 import EntityTypeSeed from './services/entity-type/entity-type.seed'
-import UserRelationshipTypeSeed from './services/user-relationship-type/user-relationship-type.seed'
-import UserRoleSeed from './services/user-role/user-role.seed'
 import GroupUserRankSeed from './services/group-user-rank/group-user-rank.seed'
+import IdentityProviderTypeSeed from './services/identity-provider-type/identity-provider-type.seed'
 import ResourceTypeSeed from './services/resource-type/resource-type.seed'
 import StaticResourceTypeSeed from './services/static-resource-type/static-resource-type.seed'
+import UserRelationshipTypeSeed from './services/user-relationship-type/user-relationship-type.seed'
+import UserRoleSeed from './services/user-role/user-role.seed'
 
 module.exports = {
   services: [
+    AccessControlScopeSeed,
+    AccessControlSeed,
     CollectionType,
     ComponentTypeSeed,
     EntityTypeSeed,
-    UserRelationshipTypeSeed,
-    UserRoleSeed,
     GroupUserRankSeed,
+    IdentityProviderTypeSeed,
     ResourceTypeSeed,
-    AccessControlScopeSeed,
-    AccessControlSeed,
-    StaticResourceTypeSeed
+    StaticResourceTypeSeed,
+    UserRelationshipTypeSeed,
+    UserRoleSeed
   ]
 }
