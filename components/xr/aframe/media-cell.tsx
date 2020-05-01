4b15960fd5dd56b31c09057899ddeec03053c9d7
mohrtw <mohrtw@gmail.com>
2020-04-30 17:43:41 -0700

    video details added to explore

--- components/xr/aframe/media-cell.tsx
+++ components/xr/aframe/media-cell.tsx
@@ -46,7 +46,8 @@ export interface MediaCellData {
   tags: string[],
   mediatype: string,
   linktype: string,
-  videoformat: string
+  videoformat: string,
+  linkEnabled: boolean
 }
 
 export const MediaCellComponentSchema: AFRAME.MultiPropertySchema<MediaCellData> = {
@@ -65,13 +66,14 @@ export const MediaCellComponentSchema: AFRAME.MultiPropertySchema<MediaCellData>
   tags: { default: [] },
   mediatype: { default: 'video360' },
   linktype: { default: 'internal' },
-  videoformat: { default: 'eac' }
-
+  videoformat: { default: 'eac' },
+  linkEnabled: { default: true }
 }
 
 export interface MediaCellProps {
   initCell: () => void,
   createCell: () => AFRAME.Entity,
+  enableLink: (el: any) => void
 }
 
 export const MediaCellComponent: AFRAME.ComponentDefinition<MediaCellProps> = {
@@ -96,12 +98,19 @@ export const MediaCellComponent: AFRAME.ComponentDefinition<MediaCellProps> = {
 
   createCell() {
     const imageEl = document.createElement('a-image')
-    imageEl.classList.add('clickable')
     const source = (this.system as AFRAME.SystemDefinition<MediaCellSystemProps>).getSource(this.data)
     imageEl.setAttribute('src', source)
     imageEl.setAttribute('width', this.data.cellWidth)
     imageEl.setAttribute('height', this.data.cellContentHeight)
+    imageEl.classList.add('clickable')
 
+    if (this.data.linkEnabled) this.enableLink(imageEl)
+
+    return imageEl
+  },
+
+  enableLink(el: any) {
+    el.classList.add('clickable')
     let url: string
     switch (this.data.linktype) {
       case 'external':
@@ -133,11 +142,9 @@ export const MediaCellComponent: AFRAME.ComponentDefinition<MediaCellProps> = {
         url += ''
         break
     }
-    imageEl.addEventListener('click', () => {
+    el.addEventListener('click', () => {
       window.location.href = url
     })
-
-    return imageEl
   }
 }
 
@@ -164,7 +171,8 @@ export const MediaCellPrimitive: AFRAME.PrimitiveDefinition = {
     // tags: ComponentName + '.tags',
     mediatype: ComponentName + '.mediatype',
     linktype: ComponentName + '.linktype',
-    videoformat: ComponentName + '.videoformat'
+    videoformat: ComponentName + '.videoformat',
+    'link-enabled': ComponentName + '.linkEnabled'
   }
 }
 
