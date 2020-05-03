665c0d4c758d32302f63a19fb90bfdadd51dbec4
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-05-03 14:42:23 +0200

    take from values folder

--- upgrade.sh
+++ upgrade.sh
@@ -4,4 +4,4 @@ RELEASE=$1
 
 helm repo update
 
-helm upgrade --install $RELEASE xrchat/xrchat --values configs/$RELEASE.values.yaml
+helm upgrade --install $RELEASE xrchat/xrchat --values values/$RELEASE.values.yaml
