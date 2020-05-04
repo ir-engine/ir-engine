9a1e540f3b738b6ef4291ac7e1d55ec97c831477
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-04 17:31:27 +0500

    Added project hook for mapping project data and logged in user id

--- src/services/project/project.hooks.ts
+++ src/services/project/project.hooks.ts
@@ -1,13 +1,26 @@
+import { HookContext } from '@feathersjs/feathers';
+import * as authentication from '@feathersjs/authentication'
+import attachOwnerIdInSavingContact from '../../hooks/set-loggedin-user-in-body'
+import setResponseStatusCode from '../../hooks/set-response-status-code'
+
 // Don't remove this comment. It's needed to format import lines nicely.
 
-// const { authenticate } = authentication.hooks;
+const { authenticate } = authentication.hooks;
 
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      authenticate('jwt'),
+      (context: HookContext) => {
+        context.data.project_owned_file_id = context.data.project.project_file_id
+        context.data.name = context.data.project.name
+        context.data.thumbnail_owned_file_id = context.data.project.thumbnail_file_id
+      },
+      attachOwnerIdInSavingContact('created_by_account_id')
+    ],
     update: [],
     patch: [],
     remove: []
@@ -17,7 +30,10 @@ export default {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      // Spoke is expecting 200, while feather is sending 201 for creation
+      setResponseStatusCode(200)
+    ],
     update: [],
     patch: [],
     remove: []
