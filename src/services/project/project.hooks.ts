4960723af7064486f164f8feb85389a0a0cc7094
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-05 17:10:18 +0500

    Implemented saving of project api

--- src/services/project/project.hooks.ts
+++ src/services/project/project.hooks.ts
@@ -1,26 +1,26 @@
-import { HookContext } from '@feathersjs/feathers';
-import * as authentication from '@feathersjs/authentication'
-import attachOwnerIdInSavingContact from '../../hooks/set-loggedin-user-in-body'
-import setResponseStatusCode from '../../hooks/set-response-status-code'
+import * as authentication from '@feathersjs/authentication';
 
+import { HookContext } from '@feathersjs/feathers';
+import setResponseStatusCode from '../../hooks/set-response-status-code'
 // Don't remove this comment. It's needed to format import lines nicely.
 
-const { authenticate } = authentication.hooks;
+const { authenticate } = authentication.hooks
+
+const mapProjectSaveData = () => {
+  return (context: HookContext) => {
+    context.data.project_owned_file_id = context.data.project.project_file_id
+    context.data.name = context.data.project.name
+    context.data.thumbnail_owned_file_id = context.data.project.thumbnail_file_id
+    return context
+  }
+}
 
 export default {
   before: {
-    all: [],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
-    create: [
-      authenticate('jwt'),
-      (context: HookContext) => {
-        context.data.project_owned_file_id = context.data.project.project_file_id
-        context.data.name = context.data.project.name
-        context.data.thumbnail_owned_file_id = context.data.project.thumbnail_file_id
-      },
-      attachOwnerIdInSavingContact('created_by_account_id')
-    ],
+    create: [mapProjectSaveData()],
     update: [],
     patch: [],
     remove: []
