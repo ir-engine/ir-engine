2336dbbb01b892a4608d090cc7f08d47d373ba7d
anuragQuepp <anurag.k@queppelintech.com>
2020-05-05 22:49:10 +0530

    added access control hook to collect analytics

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,18 +1,18 @@
-import { RequestHandler } from 'express'
-import ua from 'universal-analytics'
-import config from 'config'
+// import { Response, Request, NextFunction } from 'express'
+// import ua from 'universal-analytics'
+// import logger from './../logger'
 
-// import { Application } from '../declarations'
+import { Application } from '../declarations'
 // Don't remove this comment. It's needed to format import lines nicely.
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-// export default (app: Application): void => {}
-
-export const googleAnalytics: RequestHandler = (req, res, next) => {
-  console.log('TRACKED=====')
-  const visitor = ua(config.get('gaTrackingId'), { https: false })
-  visitor.pageview('/').send()
-  visitor.screenview('Home Screen', 'XRchat').send()
-  visitor.event('Page Visit', 'Visted').send()
-  next()
+export default (app: Application): void => {
+  // app.use(async (req: Request, res: Response, next: NextFunction) => {
+  //   const visitor = ua(app.get('gaTrackingId'), { https: false })
+  //   visitor.pageview(req.path).send()
+  //   visitor.screenview('Home Screen', 'XRchat').send()
+  //   visitor.event(req.method, 'Requeset').send()
+  //   logger.info('Tracked..............')
+  //   next()
+  // })
 }
