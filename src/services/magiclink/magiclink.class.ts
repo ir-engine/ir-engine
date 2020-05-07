361ef9d37e6aaedc9b933e9fe5d70cb4965c815d
anuragQuepp <anurag.k@queppelintech.com>
2020-05-07 18:35:11 +0530

    added collectAnalytics hook to all resources endpoints

--- src/services/magiclink/magiclink.class.ts
+++ src/services/magiclink/magiclink.class.ts
@@ -1,6 +1,16 @@
-import { Id, NullableId, Paginated, Params, ServiceMethods } from '@feathersjs/feathers'
+import {
+  Id,
+  NullableId,
+  Paginated,
+  Params,
+  ServiceMethods
+} from '@feathersjs/feathers'
 import { Application } from '../../declarations'
-import { getLink, sendEmail, sendSms } from '../auth-management/auth-management.utils'
+import {
+  getLink,
+  sendEmail,
+  sendSms
+} from '../auth-management/auth-management.utils'
 import * as path from 'path'
 import * as pug from 'pug'
 import { Service } from 'feathers-sequelize'
@@ -10,37 +20,46 @@ interface Data {}
 interface ServiceOptions {}
 
 export class Magiclink implements ServiceMethods<Data> {
-  app: Application;
-  options: ServiceOptions;
+  app: Application
+  options: ServiceOptions
 
-  constructor (options: ServiceOptions = {}, app: Application) {
+  constructor(options: ServiceOptions = {}, app: Application) {
     this.options = options
     this.app = app
   }
 
-  async find (params?: Params): Promise<Data[] | Paginated<Data>> {
+  async find(params?: Params): Promise<Data[] | Paginated<Data>> {
     return []
   }
 
-  async get (id: Id, params?: Params): Promise<Data> {
+  async get(id: Id, params?: Params): Promise<Data> {
     return {
-      id, text: `A new message with ID: ${id}!`
+      id,
+      text: `A new message with ID: ${id}!`
     }
   }
 
-  async sendEmail (toEmail: string, token: string): Promise<void> {
+  async sendEmail(toEmail: string, token: string): Promise<void> {
     const hashLink = getLink('login', token)
     const appPath = path.dirname(require.main ? require.main.filename : '')
-    const emailAccountTemplatesPath =
-      path.join(appPath, '..', 'src', 'email-templates', 'account')
+    const emailAccountTemplatesPath = path.join(
+      appPath,
+      '..',
+      'src',
+      'email-templates',
+      'account'
+    )
 
-    const templatePath = path.join(emailAccountTemplatesPath, 'magiclink-email.pug')
+    const templatePath = path.join(
+      emailAccountTemplatesPath,
+      'magiclink-email.pug'
+    )
     const compiledHTML = pug.compileFile(templatePath)({
       logo: '',
       hashLink
     })
     const mailFrom = process.env.SMTP_FROM_EMAIL ?? 'noreply@myxr.email'
-    const mailSender = `${(process.env.SMTP_FROM_NAME ?? '')}<${mailFrom}>`
+    const mailSender = `${process.env.SMTP_FROM_NAME ?? ''}<${mailFrom}>`
     const email = {
       from: mailSender,
       to: toEmail,
@@ -51,12 +70,20 @@ export class Magiclink implements ServiceMethods<Data> {
     return await sendEmail(this.app, email)
   }
 
-  async sendSms (mobile: string, token: string): Promise<void> {
+  async sendSms(mobile: string, token: string): Promise<void> {
     const hashLink = getLink('login', token)
     const appPath = path.dirname(require.main ? require.main.filename : '')
-    const emailAccountTemplatesPath =
-      path.join(appPath, '..', 'src', 'email-templates', 'account')
-    const templatePath = path.join(emailAccountTemplatesPath, 'magiclink-sms.pug')
+    const emailAccountTemplatesPath = path.join(
+      appPath,
+      '..',
+      'src',
+      'email-templates',
+      'account'
+    )
+    const templatePath = path.join(
+      emailAccountTemplatesPath,
+      'magiclink-sms.pug'
+    )
     const compiledHTML = pug.compileFile(templatePath)({
       hashLink
     })
@@ -68,52 +95,66 @@ export class Magiclink implements ServiceMethods<Data> {
     return await sendSms(this.app, sms)
   }
 
-  async create (data: any, params?: Params): Promise<Data> {
+  async create(data: any, params?: Params): Promise<Data> {
     const authService = this.app.service('authentication')
-    const identityProviderService: Service = this.app.service('identity-provider')
+    const identityProviderService: Service = this.app.service(
+      'identity-provider'
+    )
 
     let identityProvider
     if (data.type === 'email') {
-      const identityProviders = (await identityProviderService.find({
+      const identityProviders = ((await identityProviderService.find({
         query: {
           token: data.email,
           identityProviderType: 'email'
         }
-      }) as any).data
+      })) as any).data
 
       if (identityProviders.length === 0) {
-        identityProvider = await identityProviderService.create({
-          token: data.email,
-          identityProviderType: 'email'
-        }, params)
+        identityProvider = await identityProviderService.create(
+          {
+            token: data.email,
+            identityProviderType: 'email'
+          },
+          params
+        )
       } else {
         identityProvider = identityProviders[0]
       }
 
       if (identityProvider) {
-        const accessToken = await authService.createAccessToken({}, { subject: identityProvider.id.toString() })
+        const accessToken = await authService.createAccessToken(
+          {},
+          { subject: identityProvider.id.toString() }
+        )
 
         await this.sendEmail(data.email, accessToken)
       }
     } else if (data.type === 'sms') {
-      const identityProviders = (await identityProviderService.find({
+      const identityProviders = ((await identityProviderService.find({
         query: {
           token: data.mobile,
           identityProviderType: 'sms'
         }
-      }) as any).data
+      })) as any).data
 
       if (identityProviders.length === 0) {
-        identityProvider = await identityProviderService.create({
-          token: data.mobile,
-          identityProviderType: 'sms'
-        }, params)
+        identityProvider = await identityProviderService.create(
+          {
+            token: data.mobile,
+            identityProviderType: 'sms'
+          },
+          params
+        )
       } else {
         identityProvider = identityProviders[0]
       }
 
       if (identityProvider) {
-        const accessToken = await authService.createAccessToken({}, { subject: identityProvider.id.toString() })
+        const accessToken = await authService.createAccessToken(
+          {},
+          { subject: identityProvider.id.toString() }
+        )
 
         await this.sendSms(data.mobile, accessToken)
       }
@@ -121,15 +162,15 @@ export class Magiclink implements ServiceMethods<Data> {
     return data
   }
 
-  async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
+  async update(id: NullableId, data: Data, params?: Params): Promise<Data> {
     return data
   }
 
-  async patch (id: NullableId, data: Data, params?: Params): Promise<Data> {
+  async patch(id: NullableId, data: Data, params?: Params): Promise<Data> {
     return data
   }
 
-  async remove (id: NullableId, params?: Params): Promise<Data> {
+  async remove(id: NullableId, params?: Params): Promise<Data> {
     return { id }
   }
 }
