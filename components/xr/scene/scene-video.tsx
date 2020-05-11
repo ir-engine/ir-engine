d01aebcab89f746f4bfbabc4d1f2aae805704167
mohrtw <mohrtw@gmail.com>
2020-05-11 13:01:46 -0700

    fixed 360 videos and dream assets

--- components/xr/scene/scene-video.tsx
+++ components/xr/scene/scene-video.tsx
@@ -3,13 +3,8 @@ import React from 'react'
 import { Entity, Scene } from 'aframe-react'
 import AFRAME from 'aframe'
 import Assets from './assets'
+import Video360 from '../video360/Video360Room'
 import './style.scss'
-// @ts-ignore
-import { useRouter } from 'next/router'
-import dynamic from 'next/dynamic'
-import VideoControls from '../video360/VideoControls'
-import AframeComponentRegisterer from '../aframe'
-const ShakaPlayerComp = dynamic(() => import('../video360/ShakaPlayerComp'), { ssr: false })
 
 type State = {
   appRendered?: boolean
@@ -21,12 +16,6 @@ export default class VideoScene extends React.Component<State> {
     appRendered: false,
     color: 'red'
   }
-
-  router = useRouter()
-  title = this.router.query.title as string
-  format = this.router.query.videoformat as string
-  videospherePrimitive = this.format === 'eac' ? 'a-eaccube' : 'a-videosphere'
-  text = `${this.title || ''}\n\n(click to play)`
 
   componentDidMount() {
     if (typeof window !== 'undefined') {
@@ -40,7 +29,7 @@ export default class VideoScene extends React.Component<State> {
   isDesktop() {
     const mobile = AFRAME.utils.device.isMobile()
     const headset = AFRAME.utils.device.checkHeadsetConnected()
-    return !mobile && !headset
+    return !(mobile) && !(headset)
   }
 
   render() {
@@ -52,58 +41,14 @@ export default class VideoScene extends React.Component<State> {
             renderer="antialias: true"
             background="color: #FAFAFA"
           >
-            <Assets />
-            <Entity>
-              <AframeComponentRegisterer />
-              <ShakaPlayerComp />
-              <Entity id="videoPlayerContainer"></Entity>
-              <Entity
-                primitive={this.videospherePrimitive}
-                class="videosphere"
-                src="#video360Shaka"
-                loop="false"
-              />
-              <Entity
-                id="video-player-vr-ui"
-                video-player-vr-ui={{}}
-                position={{ x: 0, y: 0.98, z: -0.9 }}
-              />
-              <VideoControls
-                videosrc="#video360Shaka"
-                videotext="#videotext"
-                videovrui="#video-player-vr-ui"
-              />
-              <Entity
-                id="videotext"
-                text={{
-                  font: 'mozillavr',
-                  width: 2,
-                  align: 'center',
-                  baseline: 'center',
-                  color: 'black',
-                  transparent: false,
-                  value: this.text
-                }}
-                position={{ x: 0, y: 1.6, z: -0.8 }}
-              />
-            </Entity>
-            <Entity
-              camera={{}}
-              look-controls={{}}
-              position={{ x: 0, y: 1.6, z: 0 }}
-            >
-              <Entity
-                visible={!this.isDesktop()}
-                cursor={{ rayOrigin: this.isDesktop() ? 'mouse' : 'entity' }}
+            <Assets/>
+            <Video360/>
+            <Entity camera={{}} look-controls={{}} position={{ x: 0, y: 1.6, z: 0 }}>
+              <Entity visible={ !this.isDesktop() } cursor={{ rayOrigin: this.isDesktop() ? 'mouse' : 'entity' }}
                 raycaster={{ objects: '#video-player-vr-ui,#videotext' }}
-                position={{ x: 0, y: 0, z: -0.8 }}
-                geometry={{
-                  primitive: 'ring',
-                  radiusInner: 0.01,
-                  radiusOuter: 0.02
-                }}
-                material={{ shader: 'flat', color: 'red' }}
-              ></Entity>
+                position={{ x: 0, y: 0, z: -0.8 }} geometry={{ primitive: 'ring', radiusInner: 0.01, radiusOuter: 0.02 }}
+                material={{ shader: 'flat', color: 'red' }}>
+              </Entity>
             </Entity>
           </Scene>
         )}
