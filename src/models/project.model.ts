78f164bb8cdc7a8042f3405729e6ba2b1660b013
shawticus <shawmakesmagic@gmail.com>
2020-05-10 20:57:22 -0700

    Add Husky

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -1,5 +1,3 @@
-// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
-// for more of what you can do here.
 import { Sequelize, DataTypes } from 'sequelize'
 import { Application } from '../declarations'
 
