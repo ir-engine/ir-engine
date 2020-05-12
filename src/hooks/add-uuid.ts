fc587c35b8cbe4255dcd6efbe3a6aaca4ca3620f
shawticus <shawmakesmagic@gmail.com>
2020-05-12 02:49:19 -0700

    Using function for module exports

--- src/hooks/add-uuid.ts
+++ src/hooks/add-uuid.ts
@@ -1,7 +1,7 @@
 import { Hook, HookContext } from '@feathersjs/feathers'
 import { v1 } from 'uuid'
 
-export default (options = {}): Hook => {
+export default function (options = {}): Hook {
   return async (context: HookContext) => {
     context.params.uuid = context.params.uuid ? context.params.uuid : v1()
     return context
