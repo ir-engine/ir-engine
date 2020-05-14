c2b2ee7b0f391ba77bff310a7a2b9cdd40442ee2
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-14 17:12:44 +0500

    Updated association method for adding support of nested include

--- src/hooks/add-associations.ts
+++ src/hooks/add-associations.ts
@@ -1,9 +1,11 @@
+import { HookContext } from '@feathersjs/feathers'
+
 export default function (options = {}) {
-  return async (context: any): Promise<void> => {
+  return (context: any): any => {
     const sequelize = context.params.sequelize || {}
     const include = sequelize.include || []
     sequelize.include = include.concat((options as any).models.map((model: any) => {
-      const newModel = { ...model }
+      const newModel = { ...model, include: processInclude(model.include, context) }
       newModel.model = context.app.services[model.model].Model
       return newModel
     }))
@@ -12,4 +14,17 @@ export default function (options = {}) {
     context.params.sequelize = sequelize
     return context
   }
+}
+
+function processInclude (includeCollection: any, context: HookContext): any {
+  if (!includeCollection) {
+    return []
+  }
+  includeCollection = includeCollection.map((model: any) => {
+    const newModel = { ...model }
+    newModel.include = processInclude(newModel.include, context)
+    newModel.model = context.app.services[model.model].Model
+    return newModel
+  })
+  return includeCollection
 }
