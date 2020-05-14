a27e2b14753807fbdd3ac619295965d07aec642d
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-14 17:34:23 +0500

    Added authentication in collection API

--- src/services/collection/collection.hooks.ts
+++ src/services/collection/collection.hooks.ts
@@ -1,10 +1,15 @@
+import * as authentication from '@feathersjs/authentication'
+import attachOwnerIdInQuery from '../../hooks/set-loggedin-user-in-query'
 import addAssociations from '../../hooks/add-associations'
 import collectAnalytics from '../../hooks/collect-analytics'
 
+const { authenticate } = authentication.hooks
+
 export default {
   before: {
-    all: [collectAnalytics()],
+    all: [collectAnalytics(), authenticate('jwt')],
     find: [
+      attachOwnerIdInQuery('userId'),
       addAssociations({
         models: [
           {
