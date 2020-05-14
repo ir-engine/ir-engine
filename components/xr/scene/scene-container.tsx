3d7247d1234946258d72e997b2b05093c813522d
George Campbell <campbell.goe@gmail.com>
2020-05-15 00:33:05 +0100

    Much more responsive (speed-wise) loading screen. - Internal links also now use next/router

--- components/xr/scene/scene-container.tsx
+++ components/xr/scene/scene-container.tsx
@@ -4,14 +4,39 @@ import { setAppLoaded } from '../../../redux/app/actions'
 import { Scene } from 'aframe-react'
 import SvgVr from '../../icons/svg/Vr'
 import LoadingScreen from '../../ui/Loader'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import isExternalUrl from '../../../utils/isExternalUrl'
 
 type Props = {
   children: any
 }
 export default function SceneContainer({ children }: Props): any {
   const dispatch = useDispatch()
-  const { loaded } = useSelector(selectAppState)
+  const loaded = useSelector(state => selectAppState(state)).get('loaded')
   const setLoaded = loaded => dispatch(setAppLoaded(loaded))
+  const router = useRouter()
+  const navigateToUrl = e => {
+    let url = e.detail.url
+    setLoaded(false)
+    if (isExternalUrl(url)) {
+      window.location.href = url
+    } else {
+      // sometimes the url given is doesn't start with '/' meaning it is invalid and errors
+      // this forces it to start with '/'
+      if (!url.startsWith('/')) {
+        url = '/' + url
+      }
+      // navigate to internal url using next/router
+      router.push(url)
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('navigate', navigateToUrl)
+    return () => {
+      document.removeEventListener('navigate', navigateToUrl)
+    }
+  }, [navigateToUrl])
   return (
     <>
       {!loaded && <LoadingScreen />}
