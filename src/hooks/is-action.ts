48ad20de24c2a7e9b0f65ca57236b188ee8f7bc4
green9016 <truegreenbelt@gmail.com>
2020-05-14 09:14:18 +0800

    friends view

--- src/hooks/is-action.ts
+++ src/hooks/is-action.ts
@@ -1,6 +1,6 @@
-export default function (...params: any) {
+export default function (...params: any): any {
   const args = Array.from(params)
-  return (hook: any) => {
+  return (hook: any): boolean => {
     return hook.data && args.includes(hook.data.action)
   }
 }
