f77020a97796aa5b51fe3ece0e7b894ed3def2f8
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-19 13:54:43 +0500

    Fixed lint error

--- test/services/meta.test.ts
+++ test/services/meta.test.ts
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import app from '../../src/app';
+import assert from 'assert'
+import app from '../../src/app'
 
 describe('\'Meta\' service', () => {
   it('registered the service', () => {
-    const service = app.service('api/v1/meta');
+    const service = app.service('api/v1/meta')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
