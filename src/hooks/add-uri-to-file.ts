b1712081910735c7651b41859b4583517fae3fc8
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-21 18:12:35 +0500

    Re-Added file extension work

--- src/hooks/add-uri-to-file.ts
+++ src/hooks/add-uri-to-file.ts
@@ -2,6 +2,7 @@ import dauria from 'dauria'
 import { Hook, HookContext } from '@feathersjs/feathers'
 import * as path from 'path'
 import _ from 'lodash'
+import { extension } from 'mime-types'
 
 export default function (options = {}): Hook {
   return async (context: HookContext) => {
@@ -13,7 +14,12 @@ export default function (options = {}): Hook {
 
     if (!context.data.id && _.has(context, 'params.uploadPath')) {
       const uploadPath = _.get(context, 'params.uploadPath')
-      context.data.id = uploadPath == null ? context.params.file.originalname : path.join(uploadPath, context.params.file.originalname)
+      if (context.params.file.originalname === 'blob') {
+        const fileExtenstion: string = String(extension(context.params.file.mimetype))
+        context.data.id = uploadPath == null ? `${(context.params.file.originalname as string)}.${fileExtenstion}` : `${uploadPath}.${fileExtenstion}`
+      } else {
+        context.data.id = uploadPath == null ? context.params.file.originalname : path.join(uploadPath, context.params.file.originalname)
+      }
     }
 
     return context
