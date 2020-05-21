a289d584fb7abc0caa2c461a14b0b44ffe97d55d
mohrtw <mohrtw@gmail.com>
2020-05-21 16:32:36 -0700

    A-Frame primitives are using PropertyMapper

--- components/xr/aframe/template.tsx
+++ components/xr/aframe/template.tsx
@@ -1,4 +1,5 @@
 import AFRAME from 'aframe'
+import PropertyMapper from './ComponentUtils'
 
 export const ComponentName = 'mycomp'
 
@@ -97,14 +98,18 @@ export const Component: AFRAME.ComponentDefinition<Props> = {
 
 }
 
+const primitiveProps = ['id', 'someData']
+
 export const Primitive: AFRAME.PrimitiveDefinition = {
   defaultComponents: {
     ComponentName: {}
   },
   deprecated: false,
   mappings: {
-    id: ComponentName + '.id',
-    'some-date': ComponentName + '.someData'
+    ...PropertyMapper(primitiveProps, ComponentName),
+    'another-prop': '.someOtherComponent.anotherProp',
+    'a-nested-prop': ComponentName + '.objectProp.aNestedProp',
+    'prop-with-different-name-in-html': ComponentName + '.propNameOnComponent'
   }
 }
 
