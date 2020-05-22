a3085e5264263bd066b2e71127af3fb279e85d62
anuragQuepp <anurag.k@queppelintech.com>
2020-05-22 12:22:30 +0530

    linting fixes

--- test/services/subscription.test.ts
+++ test/services/subscription.test.ts
@@ -1,10 +1,10 @@
-import assert from 'assert';
-import app from '../../src/app';
+import assert from 'assert'
+import app from '../../src/app'
 
 describe('\'subscription\' service', () => {
   it('registered the service', () => {
-    const service = app.service('subscription');
+    const service = app.service('subscription')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
