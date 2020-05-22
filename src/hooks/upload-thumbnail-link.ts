7eeb4b82e5cc6e22e9fed894060fc2bfc86ee865
Kyle Baran <kbaran@bitscoop.com>
2020-05-21 17:43:26 -0700

    Added support for patching staticResources that are videos. If metadata.thumbnail_url is different than what's current present, it will remove the current thumbnail from DB and storage, then download the new thumbnail and put it into storage.
    Added JWT authentication to staticResource create, update,
    patch, and remove.
    
    Updated some hooks to account for multiple places that information
    may be stored, i.e. they look in context.result, .data, or .params.

--- src/hooks/upload-thumbnail-link.ts
+++ src/hooks/upload-thumbnail-link.ts
@@ -6,13 +6,15 @@ const getBuffer = bent('buffer')
 
 export default (options = {}): Hook => {
   return async (context: HookContext) => {
-    const { app, result } = context
-    const extensionMatch = result.metadata.thumbnail_url.match(fileRegex)
-    if (result.id != null && result.metadata.thumbnail_url && extensionMatch != null) {
+    const { app, result, data, params } = context
+    const metadata = result?.metadata || data?.metadata
+    const id = result?.id || data?.id
+    const extensionMatch = metadata.thumbnail_url.match(fileRegex)
+    if (id != null && metadata.thumbnail_url && extensionMatch != null) {
       const extension = extensionMatch[1] as string
-      const url = result.metadata.thumbnail_url
+      const url = metadata.thumbnail_url
       const fileBuffer = await getBuffer(url)
-      context.params.file = {
+      params.file = {
         fieldname: 'file',
         originalname: 'thumbnail.' + extension,
         encoding: '7bit',
@@ -20,21 +22,21 @@ export default (options = {}): Hook => {
         size: fileBuffer.byteLength
       }
 
-      context.params.mimeType = context.params.file.mimetype = 'image/' + extension
-      context.params.subscriptionLevel = context.data.subscriptionLevel
-      context.data.name = context.params.file.originalname
+      params.mimeType = params.file.mimetype = 'image/' + extension
+      params.subscriptionLevel = context.data.subscriptionLevel
+      context.data.name = params.file.originalname
       context.data.metadata = context.data.metadata ? context.data.metadata : {}
-      context.data.parentResourceId = context.params.parentResourceId
-      context.params.uploadPath = context.params.uploadPath.replace('video', 'image')
-      const uploadResult = await app.services.upload.create(context.data, context.params)
-      const parent = await app.services['static-resource'].get(result.id)
-      const metadata = JSON.parse(parent.metadata)
-      metadata.thumbnail_url = uploadResult.url
-      await app.services['static-resource'].patch(result.id, {
-        metadata: metadata
+      context.data.parentResourceId = params.parentResourceId
+      params.uploadPath = params.uploadPath.replace('video', 'image')
+      const uploadResult = await app.services.upload.create(context.data, params)
+      const parent = await app.services['static-resource'].get(id)
+      const parsedMetadata = JSON.parse(parent.metadata)
+      parsedMetadata.thumbnail_url = uploadResult.url
+      await app.services['static-resource'].patch(id, {
+        metadata: parsedMetadata
       })
 
-      context.params.thumbnailUrl = metadata.thumbnail_url
+      params.thumbnailUrl = parsedMetadata.thumbnail_url
 
       return context
     } else {
