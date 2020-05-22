dc2fb5bfcabc86efc06f31d1faf714546780f527
anuragQuepp <anurag.k@queppelintech.com>
2020-05-22 12:22:30 +0530

    linting fixes

--- src/models/subscription.model.ts
+++ src/models/subscription.model.ts
@@ -5,40 +5,40 @@ export default (app: Application): any => {
   // prettier-ignore
   const sequelizeClient: Sequelize = app.get('sequelizeClient')
   const subscription = sequelizeClient.define(
-      'subscription',
-      {
-        plan: {
-          type: DataTypes.STRING,
-          allowNull: true
-        },
-        amount: {
-          type: DataTypes.FLOAT,
-          allowNull: false
-        },
-        currency: {
-          type: DataTypes.STRING,
-          defaultValue: 'usd',
-          allowNull: false
-        },
-        quantity: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        status: {
-          type: DataTypes.BOOLEAN,
-          defaultValue: false
-        }
+    'subscription',
+    {
+      plan: {
+        type: DataTypes.STRING,
+        allowNull: true
       },
-      {
-        hooks: {
-          beforeCount(options: any) {
-            options.raw = true
-          }
+      amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+      },
+      currency: {
+        type: DataTypes.STRING,
+        defaultValue: 'usd',
+        allowNull: false
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    },
+    {
+      hooks: {
+        beforeCount (options: any) {
+          options.raw = true
         }
       }
-    )
+    }
+  )
 
-    // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   ;(subscription as any).associate = (models: any) => {
     // Define associations here
     ;(subscription as any).belongsTo(models.user)
--- src/services/subscription/subscription.service.ts
+++ src/services/subscription/subscription.service.ts
@@ -1,28 +1,28 @@
 // Initializes the `subscription` service on path `/subscription`
-import { ServiceAddons } from '@feathersjs/feathers';
-import { Application } from '../../declarations';
-import { Subscription } from './subscription.class';
-import createModel from '../../models/subscription.model';
-import hooks from './subscription.hooks';
+import { ServiceAddons } from '@feathersjs/feathers'
+import { Application } from '../../declarations'
+import { Subscription } from './subscription.class'
+import createModel from '../../models/subscription.model'
+import hooks from './subscription.hooks'
 
 // Add this service to the service type index
 declare module '../../declarations' {
   interface ServiceTypes {
-    'subscription': Subscription & ServiceAddons<any>;
+    subscription: Subscription & ServiceAddons<any>
   }
 }
 
-export default function (app: Application) {
+export default (app: Application): any => {
   const options = {
     Model: createModel(app),
     paginate: app.get('paginate')
-  };
+  }
 
   // Initialize our service with any options it requires
-  app.use('/subscription', new Subscription(options, app));
+  app.use('/subscription', new Subscription(options, app))
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('subscription');
+  const service = app.service('subscription')
 
-  service.hooks(hooks);
+  service.hooks(hooks)
 }
