12691ba626c69163569dc9de667a502b2b3fb9c8
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-05-23 18:16:12 +0500

    Removing owned file routes

--- src/services/owned-file/owned-file.hooks.ts
+++ src/services/owned-file/owned-file.hooks.ts
@@ -1,11 +1,9 @@
-import * as authentication from '@feathersjs/authentication'
+import { disallow } from 'feathers-hooks-common'
 // Don't remove this comment. It's needed to format import lines nicely.
-
-const { authenticate } = authentication.hooks
 
 export default {
   before: {
-    all: [authenticate('jwt')],
+    all: [disallow()],
     find: [],
     get: [],
     create: [],
