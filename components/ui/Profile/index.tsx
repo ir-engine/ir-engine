134e1edd28c8223191da8375e1ce5fce1ae6fc9d
Kyle Baran <kbaran@bitscoop.com>
2020-05-22 21:51:48 -0700

    Moved subscription tab to a separate component.
    Added seat information to subscription tab. Added button to go to
    seat management page.

--- components/ui/Profile/index.tsx
+++ components/ui/Profile/index.tsx
@@ -10,6 +10,7 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox'
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
 import UserProfile from './UserIcon'
 import UserSettings from './userSettings'
+import Subscription from './Subscription'
 import './style.scss'
 
 interface Props {
@@ -31,18 +32,6 @@ const useStyles = makeStyles((theme: Theme) =>
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3)
-    },
-    subscriptionTitle: {
-      display: 'flex',
-      'justify-content': 'center',
-      'font-size': '2em',
-      'font-weight': 'bold'
-    },
-    subscriptionBody: {
-      display: 'flex',
-      'flex-direction': 'column',
-      'align-items': 'center',
-      'justify-content': 'center'
     }
   })
 )
@@ -75,14 +64,7 @@ const ProfileModal = (props: Props) => {
   )
   const subscription = (
     <TabPanel value={tabIndex} className="subscription-profile" index={3}>
-      <div className={classes.subscriptionTitle}>Your Subscription</div>
-      {authUser.subscription == null && <div className={classes.subscriptionBody}>Free Tier</div>}
-      {authUser.subscription != null &&
-        <div className={classes.subscriptionBody}>
-          <div>Plan: {authUser.subscription.subscriptionType.name}</div>
-          <div>Seats: {authUser.subscription.subscriptionType.seats}</div>
-        </div>
-      }
+      <Subscription auth={props.auth}/>
     </TabPanel>
   )
   return (
