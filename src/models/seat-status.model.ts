847ca036c9f21e214071673c969dc0ca09686a58
Kyle Baran <kbaran@bitscoop.com>
2020-05-22 21:58:54 -0700

    Added seats.

---
+++ src/models/seat-status.model.ts
@@ -0,0 +1,32 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+import { Sequelize, DataTypes } from 'sequelize';
+import { Application } from '../declarations';
+
+export default function (app: Application) {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const seatStatus = sequelizeClient.define('seat_status', {
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true
+    }
+  }, {
+    hooks: {
+      beforeCount(options: any) {
+        options.raw = true;
+      }
+    },
+    timestamps: false
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  (seatStatus as any).associate = function (models: any) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    (seatStatus as any).hasMany(models.seat, { foreignKey: 'seatStatus' })
+  };
+
+  return seatStatus;
+}
