b3a32632b30ffa27ae5172bce6889f04b72fc7c6
George Campbell <campbell.goe@gmail.com>
2020-05-24 15:47:08 +0100

    video pagination - dynamic fetch on change page

--- redux/video/service.ts
+++ redux/video/service.ts
@@ -2,24 +2,28 @@ import { Dispatch } from 'redux'
 import {
   videosFetchedSuccess,
   videosFetchedError,
-  // fileUploadFailure,
-  fileUploadSuccess,
   // eslint-disable-next-line no-unused-vars
-  PublicVideo
+  PublicVideo,
+  fileUploadSuccess
 } from './actions'
+import { client } from '../feathers'
 import axios from 'axios'
 import { apiUrl } from '../service.common'
-import { client } from '../feathers'
 
-export function fetchPublicVideos () {
+export function fetchPublicVideos (pageOffset = 0) {
   return (dispatch: Dispatch) => {
-    client.service('static-resource').find({ query: { $limit: 100, mimeType: 'application/dash+xml' } })
+    // loads next pages videos +1
+    // doesn't work with a lower number
+    // must load next page and at least 1 video of page after that
+    // for grid arrows to show, and for videos to show on click arrow.
+    const nVideosToLoad = 31
+    client.service('static-resource').find({ query: { $limit: nVideosToLoad, $skip: nVideosToLoad * pageOffset, mimeType: 'application/dash+xml' } })
       .then((res: any) => {
         for (const video of res.data) {
           video.metadata = JSON.parse(video.metadata)
         }
         const videos = res.data as PublicVideo[]
-
+        console.log('fetched', videos.length, 'videos.')
         return dispatch(videosFetchedSuccess(videos))
       })
       .catch(() => dispatch(videosFetchedError('Failed to fetch videos')))
