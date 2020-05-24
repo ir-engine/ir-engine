65354d22ee8ef50b7b7af3dffb28e1bbdba5c81a
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-05-23 20:09:12 -0400

    Fix indentation on make-user-admin.

--- scripts/make-user-admin.js
+++ scripts/make-user-admin.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv')
-
 const cli = require('cli')
 const Sequelize = require('sequelize')
 
@@ -9,52 +8,52 @@ import { db } from '../src/db-config'
 cli.enable('status')
 
 cli.parse({
-	userId: [false, 'UserID to make admin', 'string']
+  userId: [false, 'UserID to make admin', 'string']
 });
 
 cli.main(async function(args, options) {
-	try {
-		const sequelizeClient = new Sequelize({
-			...db,
-			logging: true,
-			define: {
-				freezeTableName: true
-			}
-		})
+  try {
+    const sequelizeClient = new Sequelize({
+      ...db,
+      logging: true,
+      define: {
+        freezeTableName: true
+      }
+    })
 
-		await sequelizeClient.sync()
+    await sequelizeClient.sync()
 
-		const User = sequelizeClient.define('user', {
-			id: {
-				type: Sequelize.DataTypes.UUID,
-				defaultValue: Sequelize.DataTypes.UUIDV1,
-				allowNull: false,
-				primaryKey: true
-			},
-			name: {
-				type: Sequelize.DataTypes.STRING,
-				allowNull: false
-			},
-			userRole: {
-				type: Sequelize.DataTypes.STRING,
-				allowNull: true
-			}
-		});
+    const User = sequelizeClient.define('user', {
+      id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.DataTypes.UUIDV1,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+      },
+      userRole: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true
+      }
+    });
 
-		console.log(options.userId)
-		const userMatch = await User.findOne({
-			where: {
-				id: options.userId
-			}
-		})
+    console.log(options.userId)
+    const userMatch = await User.findOne({
+      where: {
+        id: options.userId
+      }
+    })
 
-		userMatch.userRole = 'admin'
-		await userMatch.save()
+    userMatch.userRole = 'admin'
+    await userMatch.save()
 
-		this.ok('User with ID ' + options.userId + ' made an admin')
-		process.exit(0);
-	}
-	catch(err) {
-		this.fatal(err)
-	}
+    this.ok('User with ID ' + options.userId + ' made an admin')
+    process.exit(0);
+  }
+  catch(err) {
+    this.fatal(err)
+  }
 })
