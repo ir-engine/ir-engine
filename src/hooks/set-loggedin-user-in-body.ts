c40bbf10c0c406d4a18aae781007dce3f7a3b816
William Broza <lbroza@bitscoop.com>
2020-05-24 17:50:09 -0700

    Please enter the commit message for your changes. Lines starting

--- src/hooks/set-loggedin-user-in-body.ts
+++ src/hooks/set-loggedin-user-in-body.ts
@@ -6,22 +6,13 @@ export default (propertyName: string) => {
   return (context: HookContext): HookContext => {
     // Getting logged in user and attaching owner of user
     const loggedInUser = extractLoggedInUserFromParams(context.params)
-    if (Array.isArray(context.data)) {
-      context.data = context.data.map((item: any) => {
-        return {
-          ...item,
-          [propertyName]: loggedInUser.userId
-        }
-      })
-    } else {
-      context.data = {
-        ...context.data,
-        [propertyName]: loggedInUser.userId
-      }
-      context.params.body = {
-        ...context.params.body,
-        [propertyName]: loggedInUser.userId
-      }
+    context.data = {
+      ...context.data,
+      [propertyName]: loggedInUser.userId
+    }
+    context.data = {
+      ...context.data,
+      [propertyName]: loggedInUser.userId
     }
 
     return context
