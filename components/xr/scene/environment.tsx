9611b9d00cbc210b232110236b9590147761d443
mohrtw <mohrtw@gmail.com>
2020-05-26 15:04:15 -0700

    replaced landing gltf model with floor

--- components/xr/scene/environment.tsx
+++ components/xr/scene/environment.tsx
@@ -1,14 +1,12 @@
 import { Entity } from 'aframe-react'
 import Lights from './lights'
+import Floor from './floor'
 import Skybox from './skybox'
 
-import getConfig from 'next/config'
 export const Environment = () => {
-  const config = getConfig().publicRuntimeConfig.xr.environment['scene-gltf']
-  const gltfsrc = '#' + config.name
   return (
     <Entity>
-      <a-gltf-model src={gltfsrc} position="0 0 -50" />
+      <Floor />
       <Lights />
       <Skybox />
     </Entity>
