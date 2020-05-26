d7920480fa3b63b5ac5db9d10cbb755a8cbf8ea1
George Campbell <campbell.goe@gmail.com>
2020-05-26 03:30:07 +0100

    dynamic routing

--- components/xr/scene/index.tsx
+++ components/xr/scene/index.tsx
@@ -1,3 +1,6 @@
+// TODO: reset player location to center/origin on navigate
+// TODO: fix dreamscene models not loading textures/lighting on navigation
+
 import React, { useState, useEffect } from 'react'
 import SceneContainer from './scene-container'
 import Assets from './assets'
@@ -17,64 +20,69 @@ import VideoScene, { VideoProps } from './video'
 import isExternalUrl from '../../../utils/isExternalUrl'
 
 interface Props {
-  startingScene?: string,
+  sceneName?: string,
   manifest?: string,
   title?: string,
   format?: string,
-  dreamUrl?: string
+  url?: string
 }
 
-export default function RootScene (props: Props): any {
-  const [sceneName, setSceneName] = useState(props.startingScene || 'landing')
+export default function RootScene(props: Props): any {
+  const [sceneName, setSceneName] = useState(props.sceneName || 'landing')
   const [videoProps, setVideoProps] = useState({
     manifest: props.manifest || '',
     title: props.title || '',
     format: props.format || ''
   } as VideoProps)
-  const [dreamUrl, setDreamUrl] = useState(props.dreamUrl || '')
+  const [dreamUrl, setDreamUrl] = useState(props.url || '')
 
   const navigationHandler = e => {
-    const url = e.detail.url
+    let url = e.detail.url
 
     if (isExternalUrl(url)) {
       window.location.href = url
     } else {
-      if (/^\/?explore/g.test(url)) setSceneName('explore')
-      else if (/^\/?dreamscene/g.test(url)) {
-        const urlRegex = /url=(.*)&?/g
-        const urlMatch = urlRegex.exec(url)
-        // const dreamurl = urlRegex.test(url) ? urlMatch[1] : ''
-
-        setDreamUrl(urlMatch[1])
-        setSceneName('dreamscene')
-      } else if (/^\/?dream/g.test(url)) setSceneName('dream')
-      else if (/^\/?video/g.test(url)) {
-        const manifestRegex = /manifest=(.*\.mpd)&?/
-        const manifestMatch = manifestRegex.exec(url)
-        const manifest = manifestMatch != null ? manifestMatch[1] : ''
-
-        const titleRegex = /title=([\w\s]+)&?/
-        const titleMatch = titleRegex.exec(url)
-        const title = titleMatch != null ? titleMatch[1] : ''
-
-        const formatRegex = /format=([\w]+)&?/
-        const formatMatch = formatRegex.exec(url)
-        const format = formatMatch != null ? formatMatch[1] : ''
-
-        const videoProps: VideoProps = {
-          manifest: manifest,
-          title: title,
-          format: format
+      if (!url.startsWith('/')) {
+        url = '/' + url
+      }
+      // don't push state when popstate event (e.g. back button clicked)
+      // push state (change page url) to new location without reloading page
+      if (!e.detail.isPopState) {
+        window.history.pushState({
+          url: '/',
+          as: url
+        }, '', url)
+      }
+      const pathname = window.location.pathname
+      const newSceneName = pathname.slice(1) || 'landing'
+      if (newSceneName === 'dreamscene' || newSceneName === 'video') {
+        const params = new URLSearchParams(document.location.search.substring(1))
+        if (newSceneName === 'dreamscene') {
+          setDreamUrl(params.get('url'))
+        } else {
+          setVideoProps({
+            manifest: params.get('manifest'),
+            title: params.get('title'),
+            format: params.get('format')
+          })
         }
-        setVideoProps(videoProps)
-        setSceneName('video')
-      } else setSceneName('landing')
+      }
+      setSceneName(newSceneName)
     }
+  }
+  // handle change page e.g. browser back button clicked
+  function popStateHandler(e) {
+    e.preventDefault()
+    // use 'as' as the url, from history state.
+    navigationHandler({ detail: { url: (e as PopStateEvent).state.as, isPopState: true } })
+    return false
   }
   useEffect(() => {
     document.addEventListener('navigate', navigationHandler, { once: true })
+    window.addEventListener('popstate', popStateHandler)
     return () => {
       document.removeEventListener('navigate', navigationHandler)
+      window.removeEventListener('popstate', popStateHandler)
     }
   }, [navigationHandler])
 
@@ -83,18 +91,18 @@ export default function RootScene (props: Props): any {
       <SceneContainer>
         <AframeComponentRegisterer />
         <Assets />
-        { sceneName !== 'video' && <Player /> }
-        { sceneName !== 'video' && sceneName !== 'dreamscene' && <Environment /> }
+        {sceneName !== 'video' && <Player />}
+        {sceneName !== 'video' && sceneName !== 'dreamscene' && <Environment />}
 
-        { sceneName === 'landing' && <LandingScene /> }
-        { sceneName === 'explore' && <ExploreScene /> }
-        { sceneName === 'dream' && <DreamScene /> }
-        { sceneName === 'dreamscene' && <DreamSceneScene url={dreamUrl}/> }
-        { sceneName === 'video' &&
+        {sceneName === 'landing' && <LandingScene />}
+        {sceneName === 'explore' && <ExploreScene />}
+        {sceneName === 'dream' && <DreamScene />}
+        {sceneName === 'dreamscene' && <DreamSceneScene url={dreamUrl} />}
+        {sceneName === 'video' &&
           <VideoScene
             manifest={videoProps.manifest}
             title={videoProps.title}
-            format={videoProps.format}/> }
+            format={videoProps.format} />}
         <a className="enterVR" id="enterVRButton" href="#">
           <SvgVr className="enterVR" />
         </a>
