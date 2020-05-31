469d3f69096f1394478e92614231ba94c0b70550
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-05-31 12:10:23 +0200

    fix script to use right use in case of usage

--- scripts/publish-docker-hub.sh
+++ scripts/publish-docker-hub.sh
@@ -8,18 +8,18 @@ cd ../xrchat-client
 git fetch --tags
 TAG="$(git describe --abbrev=0 --tags)"
 
-cd ../xrchat-ops
+cd ../xrsocial-ops
 
 export NEW_TAG=rc0.0.5
 docker-compose -f docker-compose-local.yml build
-docker login --username xrchat --password ${DOCKER_HUB_TOKEN}
-docker tag xrchat/client xrchat/client:${NEW_TAG}
-docker push xrchat/client:${NEW_TAG}
+docker login --username xr3ngine --password ${DOCKER_HUB_PASSWORD}
+# docker tag xrchat/client xrchat/client:${NEW_TAG}
+# docker push xrchat/client:${NEW_TAG}
 
 for repo in {client,server,realtime-server}; do
     for tag in {$TAG,latest}; do
-        docker tag xrchat/${repo} xrchat/${repo}:${tag}
-        docker push xrchat/${repo}:${tag}
+        docker tag xr3ngine/${repo} xr3ngine/${repo}:${tag}
+        docker push xr3ngine/${repo}:${tag}
     done
 done 
 
