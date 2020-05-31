0287e06d6eb2fee281452eab830dee8d2762413b
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-05-30 21:30:04 -0400

    Convert AWS_ env vars.

--- src/storage/storageprovider.ts
+++ src/storage/storageprovider.ts
@@ -1,11 +1,14 @@
 import S3Storage from './s3.storage'
 import LocalStorage from './local.storage'
 import IStorageProvider from './storageprovider.interface'
+import config from '../../src/config'
 
 export default class StorageProvider implements IStorageProvider {
   provider: IStorageProvider
   constructor () {
-    this.provider = process.env.STORAGE_PROVIDER === 'aws' ? new S3Storage() : new LocalStorage()
+    this.provider = config.server.storageProvider === 'aws'
+      ? new S3Storage()
+      : new LocalStorage()
   }
 
   getProvider = (): IStorageProvider => this.provider
