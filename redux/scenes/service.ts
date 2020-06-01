1eeb9d8ad9a8dded620fbab28579697b85ad29d7
mohrtw <mohrtw@gmail.com>
2020-06-01 11:52:12 -0700

    get scenes from config instead of hard coded

--- redux/scenes/service.ts
+++ redux/scenes/service.ts
@@ -5,50 +5,10 @@ import {
   PublicScene
 } from './actions'
 
-const media: PublicScene[] = [{
-  name: 'Launchpad',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Launchpad/Launchpad.gltf'
-},
-{
-  name: 'ArtGallery',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Art_Gallery/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Art_Gallery/thumbnail.png'
-},
-{
-  name: 'CollegeCampus',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/College_Campus/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/College_Campus/thumbnail.png'
-},
-{
-  name: 'MicroWorld',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Micro_World/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Micro_World/thumbnail.png'
-},
-{
-  name: 'ScienceLab',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Science_Lab/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Science_Lab/thumbnail.png'
-},
-{
-  name: 'Underwater',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Underwater/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Underwater/thumbnail.png'
-},
-{
-  name: 'CubeRoom',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Cube_Room/CubeRoom.gltf'
-},
-{
-  name: 'Greenhouse',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Greenhouse/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Greenhouse/thumbnail.png'
-},
-{
-  name: 'MusicRoom',
-  url: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Music_Room/scene.glb',
-  thumbnailUrl: 'https://kaixr-scenes.s3-us-west-2.amazonaws.com/Music_Room/thumbnail.png'
-}
-]
+import getConfig from 'next/config'
+const config = getConfig().publicRuntimeConfig.xr.vrRoomGrid
+
+const media: PublicScene[] = config.scenes
 
 export function fetchPublicScenes () {
   return (dispatch: Dispatch) => {
