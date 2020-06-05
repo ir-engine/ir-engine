313c5f03044d94934a3bf489ca70bba55bdbcf97
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-06-05 14:32:04 +0500

    Fixed collection relations

--- server/models/collection.model.ts
+++ server/models/collection.model.ts
@@ -69,8 +69,8 @@ export default (app: Application): any => {
   });
 
   (collection as any).associate = (models: any) => {
-    (collection as any).belongsTo(models.collection_type, { foreignKey: 'type', required: true });
-    (collection as any).hasOne(models.attribution);
+    (collection as any).belongsTo(models.collection_type, { foreignKey: 'collectionType', required: true });
+    (collection as any).belongsTo(models.attribution);
     (collection as any).belongsTo(models.static_resource, { as: 'thumbnail_owned_file' });
     (collection as any).hasMany(models.entity, { onDelete: 'cascade' });
     (collection as any).hasMany(models.tag);
