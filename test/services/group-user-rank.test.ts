438c35b02552fc0a3112418ef1614a653a6c4247
shawticus <shawmakesmagic@gmail.com>
2020-06-08 07:34:27 -0700

    Remove mocha, make way for jest, tests passing

--- test/services/group-user-rank.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import app from '../../src/app'
-
-describe('\'GroupUserRank\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('group-user-rank')
-    assert.ok(service, 'Registered the service')
-  })
-})
--- test/services/upload.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import app from '../../src/app'
-
-describe('\'Upload\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('upload')
-    assert.ok(service, 'Registered the service')
-  })
-})
