fa265cb4c8ef0aa0540948374f341c31c23c6e9d
Kyle Baran <kbaran@bitscoop.com>
2020-06-19 16:29:34 -0700

    Updated setup_helm script.

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -5,7 +5,7 @@ sudo snap install kubectl --classic
 
 sudo snap install helm --classic
 
-helm repo add xr3ngine https://xr3ngine.github.io/xr3ngine-ops/
+helm repo add xr3ngine https://xr3ngine.github.io/xrsocial-ops/
 
 helm repo update
 
