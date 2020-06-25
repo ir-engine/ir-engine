5717ca7c64581a0a4b540d0011f9fd7e8c07c68d
Kyle Baran <kbaran@bitscoop.com>
2020-06-24 17:25:05 -0700

    Got Chargebee subscriptions working again.
    Added 'description' field to subscription-type.
    
    subscription-confirm now uses Chargebee API to confirm id and userId
    go together.
    
    Updated subscription-type.seed
    
    Added 'deployStage' config field so that appropriate Chargebee
    seeds are applied; new test Chargebee account has a copy of each plan
    for local/dev/staging/prod, and we don't want to load all of them
    into the database.

--- server/services/subscription-type/subscription-type.seed.ts
+++ server/services/subscription-type/subscription-type.seed.ts
@@ -1,69 +1,116 @@
 import config from '../../config'
 
+const localTemplates = [
+  {
+    plan: 'xr3ngine-basic-local',
+    name: 'XR3ngine Basic Local',
+    type: 'monthly',
+    amount: 5,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-premium-local',
+    name: 'XR3ngine Premium Local',
+    type: 'monthly',
+    amount: 20,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-deluxe-local',
+    name: 'XR3ngine Deluxe Local',
+    type: 'monthly',
+    amount: 100,
+    seats: 1
+  }
+]
+
+const devTemplates = [
+  {
+    plan: 'xr3ngine-basic-dev',
+    name: 'XR3ngine Basic Dev',
+    type: 'monthly',
+    amount: 5,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-premium-dev',
+    name: 'XR3ngine Premium Dev',
+    type: 'monthly',
+    amount: 20,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-deluxe-dev',
+    name: 'XR3ngine Deluxe Dev',
+    type: 'monthly',
+    amount: 100,
+    seats: 1
+  }
+]
+
+const stagingTemplates = [
+  {
+    plan: 'xr3ngine-basic-staging',
+    name: 'XR3ngine Basic Staging',
+    type: 'monthly',
+    amount: 5,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-premium-staging',
+    name: 'XR3ngine Premium Staging',
+    type: 'monthly',
+    amount: 20,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-deluxe-staging',
+    name: 'XR3ngine Deluxe Staging',
+    type: 'monthly',
+    amount: 100,
+    seats: 1
+  }
+]
+
+const prodTemplates = [
+  {
+    plan: 'xr3ngine-basic-prod',
+    name: 'XR3ngine Basic',
+    type: 'monthly',
+    amount: 5,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-premium-prod',
+    name: 'XR3ngine Premium',
+    type: 'monthly',
+    amount: 20,
+    seats: 1
+  },
+  {
+    plan: 'xr3ngine-deluxe-prod',
+    name: 'XR3ngine Deluxe',
+    type: 'monthly',
+    amount: 100,
+    seats: 1
+  }
+]
+
+const templatesMap = {
+  local: localTemplates,
+  dev: devTemplates,
+  staging: stagingTemplates,
+  prod: prodTemplates
+}
+
+const templateMap = config.deployStage ? templatesMap[config.deployStage] : templatesMap.local
+
 export const seed = {
   disabled: !config.db.forceRefresh,
   delete: config.db.forceRefresh,
   path: 'subscription-type',
   randomize: false,
-  templates:
-    [
-      {
-        plan: 'journey',
-        name: 'Monthly Subscription | Journey',
-        type: 'monthly',
-        amount: 9.99,
-        seats: 5
-      },
-      {
-        plan: 'trek',
-        name: 'Monthly Subscription | Trek',
-        type: 'monthly',
-        amount: 37.99,
-        seats: 20
-      },
-      {
-        plan: 'voyage',
-        name: 'Monthly Subscription | Voyage',
-        type: 'monthly',
-        amount: 87.99,
-        seats: 49
-      },
-      {
-        plan: 'adventure',
-        name: 'Monthly Subscription | Adventure',
-        type: 'monthly',
-        amount: 174.99,
-        seats: 100
-      },
-      {
-        plan: 'annual-subscription-|-journey',
-        name: 'Annual Subscription | Journey',
-        type: 'annual',
-        amount: 94.99,
-        seats: 5
-      },
-      {
-        plan: 'annual-subscription-|-trek',
-        name: 'Annual Subscription | Trek',
-        type: 'annual',
-        amount: 364.99,
-        seats: 20
-      },
-      {
-        plan: 'annual-subscription-|-voyage',
-        name: 'Annual Subscription | Voyage',
-        type: 'annual',
-        amount: 844.99,
-        seats: 49
-      },
-      {
-        plan: 'annual-subscription-|-adventure',
-        name: 'Annual Subscription | Adventure',
-        type: 'annual',
-        amount: 1679.99,
-        seats: 100
-      }
-    ]
+  templates: templateMap
 }
 
 export default seed
