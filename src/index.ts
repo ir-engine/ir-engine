ff86453f79d1ec2b5e6aaa7d3f20ab907730a463
= <piyush@oxenindia.in>
2020-07-01 19:01:54 +0530

    Added test encoder

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import GLTFPlayer from "./GLTFPlayer";
 // export { DepthVideoPlayer } from './DepthVideoPlayer'
-export { DracosisPlayer, DracosisEncoder } from './Dracosis'
-export { GLTFPlayer } from './GLTFPlayer'
+export { DracosisPlayer, DracosisEncoder } from "./Dracosis";
+export { GLTFPlayer };
