6d6ad60527012ef1cd952b6a6850ca710ac16a65
shawticus <shawmakesmagic@gmail.com>
2020-07-02 18:32:26 -0700

    Rollup skips typescript phase

--- src/defaults/DefaultActionMap.ts
+++ src/defaults/DefaultActionMap.ts
@@ -6,7 +6,12 @@ export const ActionMap: IActionMap = {
   [Actions.FORWARD]: { opposes: [Actions.BACKWARD] } as ActionMapping,
   [Actions.BACKWARD]: { opposes: [Actions.FORWARD] } as ActionMapping,
   [Actions.LEFT]: { opposes: [Actions.RIGHT] } as ActionMapping,
-  [Actions.RIGHT]: { opposes: [Actions.LEFT] } as ActionMapping
+  [Actions.RIGHT]: { opposes: [Actions.LEFT] } as ActionMapping,
+  [Actions.CROUCH]: { blockedBy: [Actions.JUMP, Actions.SPRINT] } as ActionMapping,
+  [Actions.JUMP]: { overrides: [Actions.CROUCH] } as ActionMapping,
+  [Actions.SPRINT]: { blockedBy: [Actions.JUMP], overrides: [Actions.CROUCH] } as ActionMapping,
+  [Actions.WALK]: { blockedBy: [Actions.JUMP, Actions.SPRINT], overrides: [Actions.CROUCH] } as ActionMapping,
+  [Actions.INTERACT]: { blockedBy: [Actions.JUMP] } as ActionMapping
 }
 
 export default ActionMap
