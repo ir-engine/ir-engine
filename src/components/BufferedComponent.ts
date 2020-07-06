1620e06bc492fbb1d31970047ee3924cc17dd1a5
shawticus <shawmakesmagic@gmail.com>
2020-07-06 00:57:26 -0700

    Fixed BufferedComponent to not require static schema

--- src/components/BufferedComponent.ts
+++ src/components/BufferedComponent.ts
@@ -1,22 +1,23 @@
 import { Component } from "ecsy"
-import { createType, copyCopyable, cloneClonable } from "ecsy"
 import RingBuffer from "../classes/RingBuffer"
 
-export default class BufferedComponent<Props, BufferedType> extends Component<Props> {
+export default class BufferedComponent<Props, T> extends Component<Props> {
   bufferSize = 10
-  values: RingBuffer<BufferedType> = new RingBuffer<BufferedType>(this.bufferSize)
+  values: RingBuffer<T> = new RingBuffer<T>(this.bufferSize)
+  constructor(props) {
+    super(false)
+    this.values = new RingBuffer<T>(this.bufferSize)
+  }
   setBufferSize(newSize: number, resetBuffer = true): void {
     this.bufferSize = newSize
-    if (resetBuffer) this.values = new RingBuffer<BufferedType>(this.bufferSize)
+    if (resetBuffer) this.values = new RingBuffer<T>(this.bufferSize)
   }
-  schema = {
-    values: {
-      type: createType<RingBuffer<BufferedType>>({
-        name: "ActionBuffer",
-        default: new RingBuffer<BufferedType>(this.bufferSize),
-        copy: copyCopyable,
-        clone: cloneClonable
-      })
-    }
+  copy(src) {
+    this.bufferSize = src.bufferSize
+    this.values = new RingBuffer<T>(src.bufferSize)
+    return this
+  }
+  reset(): void {
+    this.values.clear()
   }
 }
