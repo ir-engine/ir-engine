1d61893cbcb7845e2ebcc37294880df80c38fea8
shawticus <shawmakesmagic@gmail.com>
2020-07-07 02:55:09 -0700

    Rollup works now!

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
-import ActionMapping from "./ActionMapping"
+import ActionRules from "./ActionRules"
 import ActionValue from "./ActionValue"
 import Axis1D from "./Axis1D"
 import Axis2D from "./Axis2D"
@@ -7,4 +7,4 @@ import AxisValue from "./AxisValue"
 import GamepadInputState from "./GamepadInputState"
 import InputActionTable from "./InputActionTable"
 
-export { ActionMapping, ActionValue, Axis1D, Axis2D, Axis3D, AxisValue, GamepadInputState, InputActionTable }
+export { ActionRules, ActionValue, Axis1D, Axis2D, Axis3D, AxisValue, GamepadInputState, InputActionTable }
