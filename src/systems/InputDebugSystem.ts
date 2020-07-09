f5b1ab4c4ed4d4ea05dc8c340e4fc89b403f4e68
xiani <xianionline@gmail.com>
2020-07-09 20:50:39 +0300

    debug system change to show all values

--- src/systems/InputDebugSystem.ts
+++ src/systems/InputDebugSystem.ts
@@ -8,33 +8,33 @@ export default class InputDebugSystem extends System {
   _axisDataUIElement: any
   public execute(): void {
     this.queries.actionReceivers.changed.forEach(entity => {
-      if (entity.getComponent(InputActionHandler).values.getBufferLength() > 0) {
-        entity
-          .getComponent(InputActionHandler)
-          .values.toArray()
-          .forEach(element => {
-            console.log(element)
-            this._actionDataUIElement = document.getElementById("actionData")
-            if (this._actionDataUIElement) {
-              this._actionDataUIElement.innerHTML =
-                entity.getComponent(InputActionHandler).values.toArray()[0].action +
-                " | " +
-                entity.getComponent(InputActionHandler).values.toArray()[0].value
-            }
-          })
+      const values = entity.getComponent(InputActionHandler).values
+      if (values.getBufferLength() > 0) {
+        this._actionDataUIElement = document.getElementById("actionData")
+        if (this._actionDataUIElement) {
+          this._actionDataUIElement.innerHTML = values
+            .toArray()
+            .map((element, index) => {
+              return index + ": " + element.action + " | " + element.value
+            })
+            .join("<br/>")
+        }
       }
     })
     this.queries.axisReceivers.changed.forEach(entity => {
-      if (entity.getComponent(InputAxisHandler2D).values.getBufferLength() > 0)
-        console.log("Axes: " + entity.getComponent(InputAxisHandler2D).values.getBufferLength())
+      const inputHandler = entity.getComponent(InputAxisHandler2D)
+      if (inputHandler.values.getBufferLength() > 0) {
+        console.log("Axes: " + inputHandler.values.getBufferLength())
+      }
+
       this._axisDataUIElement = document.getElementById("axisData")
       if (this._axisDataUIElement) {
-        this._axisDataUIElement.innerHTML =
-          entity.getComponent(InputAxisHandler2D).values.toArray()[0].axis +
-          " | x: " +
-          entity.getComponent(InputAxisHandler2D).values.toArray()[0].value.x +
-          " | y: " +
-          entity.getComponent(InputAxisHandler2D).values.toArray()[0].value.y
+        this._axisDataUIElement.innerHTML = inputHandler.values
+          .toArray()
+          .map((element, index) => {
+            return `${index}:  ${element.axis} | x: ${element.value.x} | y: ${element.value.y}`
+          })
+          .join("<br />")
       }
     })
   }
