a8930d3f93c95344f457e04d1c98cd8b0d87d250
Kyle Baran <kbaran@bitscoop.com>
2020-07-10 16:01:02 -0700

    Added circular-dependency-plugin to detect webpack circular dependencies.
    Removed all circular dependencies found with that and the 'madge'
    library (not added to package.json since it's better to install globally
    and run from the cli). Password signin's circular dependency needs to be
    resolved better at some point than just commenting it out.

--- server/services/seat/seat.class.ts
+++ server/services/seat/seat.class.ts
@@ -2,11 +2,12 @@ import { Service, SequelizeServiceOptions } from 'feathers-sequelize'
 import { Params } from '@feathersjs/feathers'
 import { Application } from '../../declarations'
 import { BadRequest, NotFound } from '@feathersjs/errors'
-import app from './../../app'
 
 export class Seat extends Service {
+  app: Application
   constructor (options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options)
+    this.app = app
   }
 
   async create (data: any, params?: Params): Promise<any> {
@@ -14,7 +15,7 @@ export class Seat extends Service {
     if (userId == null) {
       throw new Error('Invalid user')
     }
-    const subscription = await app.service('subscription').find({
+    const subscription = await this.app.service('subscription').find({
       query: {
         status: true,
         userId: userId
@@ -46,18 +47,18 @@ export class Seat extends Service {
         seatStatus: 'filled'
       })
 
-      await app.service('subscription').patch(data.subscriptionId, {
+      await this.app.service('subscription').patch(data.subscriptionId, {
         unusedSeats: (unusedSeats as number) - 1,
         filledSeats: (filledSeats as number) + 1
       })
     } else {
-      const identityProvider = await app.service('identity-provider').find({
+      const identityProvider = await this.app.service('identity-provider').find({
         query: {
           type: 'email',
           token: data.email
         }
       })
-      const existingUserInSeat = (identityProvider as any).total > 0 ? await app.service('seat').find({
+      const existingUserInSeat = (identityProvider as any).total > 0 ? await this.app.service('seat').find({
         query: {
           userId: (identityProvider as any).data[0].userId
         }
@@ -66,12 +67,12 @@ export class Seat extends Service {
         throw new BadRequest('User already has a seat')
       }
 
-      const link = await app.service('magic-link').create({
+      const link = await this.app.service('magic-link').create({
         type: 'email',
         email: data.email,
         subscriptionId: data.subscriptionId
       })
-      const newIdentityProvider = await app.service('identity-provider').find({
+      const newIdentityProvider = await this.app.service('identity-provider').find({
         query: {
           type: (link as any).type,
           token: (link as any).email
@@ -86,7 +87,7 @@ export class Seat extends Service {
         seatStatus: 'pending'
       })
 
-      await app.service('subscription').patch(data.subscriptionId, {
+      await this.app.service('subscription').patch(data.subscriptionId, {
         unusedSeats: (unusedSeats as number) - 1,
         pendingSeats: (pendingSeats as number) + 1
       })
@@ -96,7 +97,7 @@ export class Seat extends Service {
 
   async patch (id: string, data: any, params?: Params): Promise<any> {
     const subscriptionId = data.subscriptionId as string
-    const subscription = await app.service('subscription').get(subscriptionId)
+    const subscription = await this.app.service('subscription').get(subscriptionId)
     if (subscription == null) {
       console.log('Attempt to patch subscription ' + subscriptionId + ' failed because that is not a valid subscription')
       throw new NotFound()
@@ -125,7 +126,7 @@ export class Seat extends Service {
         seatStatus: 'filled'
       })
 
-      await app.service('subscription').patch(subscription.id, {
+      await this.app.service('subscription').patch(subscription.id, {
         pendingSeats: (subscription.pendingSeats as number) - 1,
         filledSeats: (subscription.pendingSeats as number) + 1
       })
