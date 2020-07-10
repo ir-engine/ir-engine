80d4933ed0bef6ecd03ed63e2f07c7cd063e7073
Kyle Baran <kbaran@bitscoop.com>
2020-07-09 18:26:19 -0700

    Friend invites work for email and user ID. Unfriending works.

--- server/services/invite/invite.hooks.ts
+++ server/services/invite/invite.hooks.ts
@@ -3,6 +3,7 @@ import * as authentication from '@feathersjs/authentication'
 import { disallow } from 'feathers-hooks-common'
 import generateInvitePasscode from '../../hooks/generate-invite-passcode'
 import sendInvite from '../../hooks/send-invite'
+import attachOwnerIdInBody from '../../hooks/set-loggedin-user-in-body'
 
 const { authenticate } = authentication.hooks
 
@@ -13,6 +14,7 @@ export default {
     get: [],
     create: [
         authenticate('jwt'),
+        attachOwnerIdInBody('userId'),
         generateInvitePasscode()
     ],
     update: [disallow()],
