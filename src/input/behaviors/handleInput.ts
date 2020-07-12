c5b088c92b7807ebc804495be8212d0ea7bdb365
shawticus <shawmakesmagic@gmail.com>
2020-07-12 10:36:15 -0700

    InputSystem is working, debug system needs work but input is done, commiting before networkin

--- src/input/behaviors/handleInput.ts
+++ src/input/behaviors/handleInput.ts
@@ -8,18 +8,18 @@ import { InputType } from "../enums/InputType"
 
 let input: Input
 let outputArgs: { entityIn: Entity; args: any }
-export const handleInput: Behavior = (entityIn: Entity, args: any, delta: number): void => {
+export const handleInput: Behavior = (entityIn: Entity, delta: number): void => {
   input = entityIn.getComponent(Input)
 
   input.data.forEach((value: InputValue<NumericalType>, key: InputAlias) => {
-    if (args.type === InputType.BUTTON) {
+    if (value.type === InputType.BUTTON) {
       if (input.map.inputButtonBehaviors[key] && input.map.inputButtonBehaviors[key][value.value as number]) {
-        outputArgs = { entityIn: entityIn, args }
+        outputArgs = { entityIn: entityIn, args: input.map.inputButtonBehaviors[key][value.value as number].args }
         Function.call(input.map.inputButtonBehaviors[key][value.value as number].behavior, outputArgs, delta)
       }
-    } else if (args.type === InputType.ONED || args.type === InputType.TWOD || args.type === InputType.THREED) {
+    } else if (value.type === InputType.ONED || value.type === InputType.TWOD || value.type === InputType.THREED) {
       if (input.map.inputAxisBehaviors[key]) {
-        outputArgs = { entityIn: entityIn, args }
+        outputArgs = { entityIn: entityIn, args: input.map.inputAxisBehaviors[key].args }
         Function.call(input.map.inputAxisBehaviors[key].behavior, outputArgs, delta)
       }
     } else {
--- src/input/systems/InputSystem.ts
+++ src/input/systems/InputSystem.ts
@@ -1,14 +1,14 @@
 import { System } from "ecsy"
 import Input from "../components/Input"
-
+import { handleInput } from "../behaviors/handleInput"
 export default class InputSystem extends System {
   private _inputEntity: Input
-  public execute(): void {
+  public execute(delta: number): void {
     this.queries.inputs.added.forEach(entity => {
       this._inputEntity = entity.getComponent(Input)
 
       // Call all behaviors in "onAdded" of input map
-      this._inputEntity.inputMap.onAdded.forEach(behavior => {
+      this._inputEntity.map.onAdded.forEach(behavior => {
         behavior.behavior.call({
           entityIn: entity.getComponent(Input),
           args: behavior.args
@@ -16,13 +16,13 @@ export default class InputSystem extends System {
       })
 
       // Bind events to DOM
-      if (!this._inputEntity.inputMap.eventBindings) return
-      Object.keys(this._inputEntity.inputMap.eventBindings).forEach((key: string) => {
+      if (!this._inputEntity.map.eventBindings) return
+      Object.keys(this._inputEntity.map.eventBindings).forEach((key: string) => {
         document.addEventListener(key, e => {
-          this._inputEntity.inputMap.eventBindings[key].behavior.call({
+          this._inputEntity.map.eventBindings[key].behavior.call({
             event: e,
             entityIn: this._inputEntity,
-            args: this._inputEntity.inputMap.eventBindings[key].args
+            args: this._inputEntity.map.eventBindings[key].args
           })
         })
       })
@@ -30,23 +30,27 @@ export default class InputSystem extends System {
 
     // Call all behaviors in "onRemoved" of input map
     this.queries.inputs.removed.forEach(entity => {
-      this._inputEntity.inputMap.onRemoved.forEach(behavior => {
+      this._inputEntity.map.onRemoved.forEach(behavior => {
         behavior.behavior.call({
           entityIn: entity.getComponent(Input),
           args: behavior.args
         })
       })
       // Unbind events from DOM
-      if (!this._inputEntity.inputMap.eventBindings) return
-      Object.keys(this._inputEntity.inputMap.eventBindings).forEach((key: string) => {
+      if (!this._inputEntity.map.eventBindings) return
+      Object.keys(this._inputEntity.map.eventBindings).forEach((key: string) => {
         document.addEventListener(key, e => {
-          this._inputEntity.inputMap.eventBindings[key].behavior.call({
+          this._inputEntity.map.eventBindings[key].behavior.call({
             event: e,
             entityIn: this._inputEntity,
-            args: this._inputEntity.inputMap.eventBindings[key].args
+            args: this._inputEntity.map.eventBindings[key].args
           })
         })
       })
+    })
+
+    this.queries.inputs.results.forEach(entity => {
+      handleInput(entity, delta)
     })
   }
 }
