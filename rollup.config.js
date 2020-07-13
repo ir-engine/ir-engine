16e1b5d4142d95a4957b9b689b93e63eb3c08a5e
shawticus <shawmakesmagic@gmail.com>
2020-07-13 16:58:49 -0700

    replace webpack with rollup, build is building

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import json from "@rollup/plugin-json"
+import resolve from "@rollup/plugin-node-resolve"
+import html from "@open-wc/rollup-plugin-html"
+import babel from "@rollup/plugin-babel"
+import typescript from "rollup-plugin-typescript2"
+import pkg from "./package.json"
+
+export default [
+  {
+    input: "src/index.ts",
+    plugins: [
+      typescript(),
+      resolve(),
+      json({ exclude: ["node_modules/**", "examples/**"] }),
+      // terser(),
+      babel({ babelHelpers: "bundled" })
+    ],
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    output: [
+      {
+        file: pkg.module,
+        format: "es",
+        sourcemap: true,
+        namedExports: true
+      }
+    ]
+  },
+  // HTML pages
+  {
+    input: "examples/sequence_player.html",
+    output: { dir: "dist/examples" },
+    plugins: [html(), resolve()]
+  }
+]
