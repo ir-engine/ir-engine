42d245fb3c20f3cd61bf84337c03d52e32022ddf
= <piyush@oxenindia.in>
2020-07-15 03:44:39 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three';
+import React, { useEffect, useRef, useState } from 'react';
 
-function App() {
+// import { unstable_createResource as createResource } from "../../resources/cache";
+// import { Encoder } from "three/examples/jsm/loaders/GLTFLoader";
+import ReactDOM from 'react-dom';
+import {
+  Canvas,
+  useFrame,
+  useThree,
+  extend,
+  ReactThreeFiber,
+} from 'react-three-fiber';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// import { DracosisPlayer } from 'three-volumetric/dist/index';
+
+// eslint-disable-next-line
+import TestWorker from './TestWorker';
+
+// import Worker from 'worker-loader!./TestWorker.js';
+
+extend({ OrbitControls });
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        typeof OrbitControls
+      >;
+    }
+  }
+  var loaded: boolean;
+  var model: any;
+}
+
+function Box(props: any) {
+  // This reference will give us direct access to the mesh
+  const mesh: any = useRef();
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  let myWorker = new TestWorker();
+  myWorker.postMessage('Hello World');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
   );
+}
+
+export function Controls() {
+  const ref: any = useRef();
+  const { camera, gl } = useThree();
+  useFrame(() => ref.current.update());
+  return (
+    <orbitControls
+      ref={ref}
+      args={[camera, gl.domElement]}
+      enableDamping
+      dampingFactor={0.1}
+      rotateSpeed={0.8}
+    />
+  );
+}
+
+function App(props: any) {
+  useEffect(() => {
+    ReactDOM.render(
+      <Canvas camera={{ position: [0, 0, 5], up: [0, 1, 0] }}>
+        <ambientLight intensity={0.2} />
+        <pointLight position={[1, 1, 1]} />
+        <Box position={[0, 0, 0]} />
+        <Controls />
+      </Canvas>,
+      document.getElementById('root')
+    );
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
