8e810320585426ec7a8f5d2251a1df07d09e98e0
shawticus <shawmakesmagic@gmail.com>
2020-07-14 19:19:06 -0700

    Fixed teh bugz

--- src/input/behaviors/DesktopInputBehaviors.ts
+++ src/input/behaviors/DesktopInputBehaviors.ts
@@ -5,30 +5,29 @@ import Behavior from "../../common/interfaces/Behavior"
 import { InputType } from "../../input/enums/InputType"
 
 let input: Input
-export const handleMouseMovement: Behavior = (entity: Entity, args: { e: MouseEvent }): void => {
-  console.log(" mouse movement: " + entity.id)
+export const handleMouseMovement: Behavior = (entity: Entity, args: { event: MouseEvent }): void => {
   input = entity.getComponent(Input)
   if (!input || input.map.mouseInputMap === undefined) return
-  entity.getMutableComponent(Input).data.set(input.map.mouseInputMap.input.mousePosition, {
+  entity.getMutableComponent(Input).data.set(input.map.mouseInputMap["mousePosition"], {
     type: InputType.TWOD,
-    value: [(args.e.clientX / window.innerWidth) * 2 - 1, (args.e.clientY / window.innerHeight) * -2 + 1]
+    value: [(args.event.clientX / window.innerWidth) * 2 - 1, (args.event.clientY / window.innerHeight) * -2 + 1]
   })
 }
 
-export const handleMouseButton: Behavior = (entity: Entity, args: { e: MouseEvent; value: BinaryValue }, delta: number): void => {
+export const handleMouseButton: Behavior = (entity: Entity, args: { event: MouseEvent; value: BinaryValue }, delta: number): void => {
   input = entity.getComponent(Input)
-  if (!input || input.map.mouseInputMap.buttons[args.e.button] === undefined) return
-  entity.getMutableComponent(Input).data.set(input.map.mouseInputMap.buttons[args.e.button], {
+  if (!input || input.map.mouseInputMap.buttons[args.event.button] === undefined) return
+  entity.getMutableComponent(Input).data.set(input.map.mouseInputMap.buttons[args.event.button], {
     type: InputType.BUTTON,
     value: args.value
   })
 }
 
-export function handleKey(entity: Entity, key: string, value: BinaryValue): any {
+export function handleKey(entity: Entity, args: { event: KeyboardEvent; value: BinaryValue }): any {
   input = entity.getComponent(Input)
-  if (input.map.keyboardInputMap.input[key] === undefined) return
-  entity.getMutableComponent(Input).data.set(input.map.keyboardInputMap.input[key], {
+  if (input.map.keyboardInputMap[args.event.key] === undefined) return
+  entity.getMutableComponent(Input).data.set(input.map.keyboardInputMap[args.event.key], {
     type: InputType.BUTTON,
-    value: value
+    value: args.value
   })
 }
