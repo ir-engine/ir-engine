c852cb6f8cb42e0f65147800dd7bb40bc6a0ed5a
= <piyush@oxenindia.in>
2020-07-16 01:36:38 +0530

    Modified package.json

---
+++ src/Shared/Utilities.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+export declare function longToByteArray(/*long*/ long: number): number[];
+export declare function byteArrayToLong(/*byte[]*/ byteArray: Buffer): number;
+export declare function padFrameNumberWithZeros(n: any, width: number): any;
+export declare function lerp(v0: number, v1: number, t: number): number;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import GLTFPlayer from "./GLTFPlayer";
+// export { DepthVideoPlayer } from './DepthVideoPlayer'
+export { DracosisPlayer, DracosisEncoder } from "./Dracosis";
+export { GLTFPlayer };
