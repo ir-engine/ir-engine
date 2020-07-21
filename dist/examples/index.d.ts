ab88dba8670106c9f98b0a21aba2d8a4c6600373
mohrtw <mohrtw@gmail.com>
2020-07-21 10:27:48 -0700

    added inputButtonBehaviors for move
    added subscription map to initializeActor
    registered State and Subscription systems
    added changed query for state system
    fixed move behavior from assigning NaNs

--- dist/examples/index.d.ts
+++ dist/examples/index.d.ts
@@ -3,6 +3,7 @@ export * from "./input";
 export * from "./state";
 import { Entity, World } from "ecsy";
 import InputMap from "./input/interfaces/InputMap";
+import SubscriptionMap from "./subscription/interfaces/SubscriptionMap";
 import StateMap from "./state/interfaces/StateMap";
 export declare function initializeInputSystems(world: World, options?: {
     debug: boolean;
@@ -10,4 +11,5 @@ export declare function initializeInputSystems(world: World, options?: {
 export declare function initializeActor(entity: Entity, options: {
     inputMap?: InputMap;
     stateMap?: StateMap;
+    subscriptionMap?: SubscriptionMap;
 }): Entity;
