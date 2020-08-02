fa122be7f139eafd72fbec4c6317f9e84d33afbe
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-08-02 18:44:01 +0500

    Implement socket events for user parties connection, message, disconnection and creating rooms for parties.

--- client/redux/party/service.ts
+++ client/redux/party/service.ts
@@ -9,20 +9,61 @@ import {
   createdPartyUser,
   patchedPartyUser
 } from './actions'
-import {dispatchAlertError} from "../alert/service";
-import store from "../store";
+import { dispatchAlertError } from '../alert/service'
+import store from './../store'
+// import { Party } from '../../../shared/interfaces/Party'
 
-export function getParty() {
+export function getParty () {
   return async (dispatch: Dispatch, getState: any): Promise<any> => {
     try {
       const partyResult = await client.service('party').get(null)
       console.log('GOT PARTY')
       console.log(partyResult)
       dispatch(loadedParty(partyResult))
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
       dispatchAlertError(dispatch, err.message)
     }
+  }
+}
+
+// Temporary Method for arbitrary testing
+
+let socketId: any
+export const getParties = async (): Promise<void> => {
+  const parties = await client.service('party').find()
+  console.log('PARTIES', parties)
+  const userId = (store.getState() as any).get('auth').get('user').id
+  console.log('USERID: ', userId)
+  if (client.io && socketId === undefined) {
+    client.io.emit('request-socket-id', ({ id }: { id: number }) => {
+      console.log('Socket-ID received: ', id)
+      socketId = id
+    })
+    client.io.on('message-party', (data: any) => {
+      console.warn('Message received, data: ', data)
+    })
+    ;(window as any).joinParty = (userId: number, partyId: number) => {
+      client.io.emit('join-party', {
+        userId,
+        partyId
+      }, (res) => {
+        console.log('Join response: ', res)
+      })
+    }
+    ;(window as any).messageParty = (userId: number, partyId: number, message: string) => {
+      client.io.emit('message-party-request', {
+        userId,
+        partyId,
+        message
+      })
+    }
+    ;(window as any).partyInit = (userId: number) => {
+      client.io.emit('party-init', { userId }, (response: any) => {
+        response ? console.log('Init success', response) : console.log('Init failed')
+      })
+    }
+  } else {
+    console.log('Your socket id is: ', socketId)
   }
 }
 
@@ -46,12 +87,12 @@ export function removeParty(partyId: string) {
         query: {
           partyId: partyId
         }
-      }) as any
+      })
       if (channelResult.total > 0) {
         await client.service('channel').remove(channelResult.data[0].id)
       }
       await client.service('party').remove(partyId)
-    } catch(err) {
+    } catch (err) {
       console.log(err)
       dispatchAlertError(dispatch, err.message)
     }
