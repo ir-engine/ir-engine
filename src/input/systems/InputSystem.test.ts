9bee33d1bc023a36c785fd758332a54683510ceb
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-04 02:28:42 +0300

    watch tests

--- src/input/systems/InputSystem.test.ts
+++ src/input/systems/InputSystem.test.ts
@@ -1,10 +1,11 @@
+import {describe, expect, it, test} from '@jest/globals'
 import InputSystem from  "./InputSystem"
-import {test, expect} from "jest-runner-tsc"
+
 test('adds 1 + 2 to equal 3', () => {
     expect( {
         a:1,
         b:2,
-    }).equal({
+    }).toBe({
         a:1
     })
 })
