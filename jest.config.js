738a55b0fff8f8d46ef8ce8fbcda9a702da0d17b
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-04 02:28:42 +0300

    watch tests
    Former-commit-id: 9bee33d1bc023a36c785fd758332a54683510ceb

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     runner: 'jest-runner-tsc',
     displayName: 'tsc',
     moduleFileExtensions: ['js','ts', 'tsx'],
-    testMatch: ['<rootDir>/src/**/*.ts']
+    testMatch: ['<rootDir>/src/**/*.ts'],
+    snapshotSerializers: ["three-snapshot-serializer"],
   };
