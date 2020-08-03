106ad4a2db6c4e1d31bf799e69e8c291bd56cd7d
shawticus <shawmakesmagic@gmail.com>
2020-08-03 02:52:30 -0700

    Add all and prepare to push to master, physics examples disabled in rollup config
    Former-commit-id: 16422a188c48cd391b811481d9ac53b9554b0378

--- src/physics/components/VehicleBody.ts
+++ src/physics/components/VehicleBody.ts
@@ -1,6 +1,6 @@
 import { Component, Types } from "ecsy"
 
-export class VehicleBody extends Component<any> {}
+export default class VehicleBody extends Component<any> {}
 VehicleBody.schema = {
   mass: { type: Types.Number, default: 1 },
   scale: { type: Types.Number, default: { x: 0.2, y: 0.1, z: 0.1 } },
