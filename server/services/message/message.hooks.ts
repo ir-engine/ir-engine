18105c16091deff70af949f90e89c2c5e2fcadc9
Kyle Baran <kbaran@bitscoop.com>
2020-08-04 10:37:20 -0700

    Fixed some CSS issues.
    Added hook to trigger attachment of message
    status, but commented it out since we're shelving message status
    updating for now.

--- server/services/message/message.hooks.ts
+++ server/services/message/message.hooks.ts
@@ -2,6 +2,7 @@ import * as authentication from '@feathersjs/authentication'
 import messagePermissionAuthenticate from '../../hooks/message-permission-authenticate'
 import removeMessageStatuses from '../../hooks/remove-message-statuses'
 import channelPermissionAuthenticate from '../../hooks/channel-permission-authenticate'
+// import { extractLoggedInUserFromParams } from '../auth-management/auth-management.utils'
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks
@@ -9,7 +10,25 @@ const { authenticate } = authentication.hooks
 export default {
   before: {
     all: [authenticate('jwt')],
-    find: [channelPermissionAuthenticate()],
+    find: [channelPermissionAuthenticate()
+      // (context: any): any => {
+      //   const { params } = context
+      //   const loggedInUser = extractLoggedInUserFromParams(params)
+      //   context.params.sequelize = {
+      //     include: [
+      //       {
+      //         model: context.app.service('message-status').Model,
+      //         where: {
+      //           userId: loggedInUser.userId
+      //         }
+      //       }
+      //     ],
+      //     nest: true
+      //   }
+      //
+      //   return context
+      // }
+    ],
     get: [],
     create: [],
     update: [],
