fbf55556190b640922d7c929be3592615f24b38f
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-04 09:37:35 +0300

    add WebXR Input components and system
    Former-commit-id: 0c19983451eed1c378709c8c8c5f61124326a3c5

---
+++ src/input/behaviors/WebXRInputBehaviors.ts
@@ -0,0 +1,5 @@
+import { Entity } from "ecsy"
+import Input from "../components/Input"
+import Behavior from "../../common/interfaces/Behavior"
+
+export function tracking(entity: Entity, { args: {} }) {}
