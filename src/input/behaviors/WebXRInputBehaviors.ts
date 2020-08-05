aafcc3b163f50a06f5450082ed64cc3f02f0db82
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-05 19:42:56 +0300

    add webxr input tracking example

--- src/input/behaviors/WebXRInputBehaviors.ts
+++ src/input/behaviors/WebXRInputBehaviors.ts
@@ -1,5 +1,20 @@
 import { Entity } from "ecsy"
 import Input from "../components/Input"
 import Behavior from "../../common/interfaces/Behavior"
+import {
+    //WebXRSpace,
+    WebXRViewPoint,
+    WebXRPointer,
+    WebXRMainController,
+    WebXRSecondController,
+  } from "../components/WebXR"
 
-export function tracking(entity: Entity, { args: {} }) {}
+export function tracking(entity: Entity, { args: {} }) {
+    const viewPoint = entity.getComponent(WebXRViewPoint)
+    const pointer = entity.getComponent(WebXRPointer)
+    const mainController = entity.getComponent(WebXRMainController)
+    const secondController = entity.getComponent(WebXRSecondController)
+    const poses = [viewPoint, pointer, mainController, secondController].map( ({pose}) => pose )
+
+
+}
