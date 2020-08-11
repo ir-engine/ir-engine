430501ed742265c8a490ce43a56278661a205a4e
mohrtw <mohrtw@gmail.com>
2020-08-10 17:23:49 -0700

    fixed jest tests; added tests for quaternion math
    Former-commit-id: 1789c1a848a8df2dba5264c62cc9ba66d40a32da

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-    runner: 'jest-runner-tsc',
-    displayName: 'tsc',
-    moduleFileExtensions: ['js','ts', 'tsx'],
-    testMatch: ['<rootDir>/src/**/*.ts'],
-    snapshotSerializers: ["three-snapshot-serializer"],
+export default {
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    testEnvironment: 'jest-environment-node',
+    transform: {
+      // "^.+\\.jsx?$": "babel-jest",
+      ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    testMatch: ['<rootDir>/test/**/*.ts'],
+    // snapshotSerializers: ["three-snapshot-serializer"],
   };
