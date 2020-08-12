eaa8b4ff46a2bb2bd1fcc6fd9c91a22f20806792
shawticus <shawmakesmagic@gmail.com>
2020-08-12 03:48:59 -0700

    Fix to run dev
    Former-commit-id: b2bc2bf10f663ba29a83fcfbc4a5aa639a2f04b2

--- src/physics/behavior/PhysicsBehaviors.ts
+++ src/physics/behavior/PhysicsBehaviors.ts
@@ -6,74 +6,9 @@ import { RaycastVehicle } from "cannon-es/src/objects/RaycastVehicle"
 import { Quaternion } from "cannon-es/src/math/Quaternion"
 import { Sphere } from "cannon-es/src/shapes/Sphere"
 import { Body } from "cannon-es/src/objects/Body"
-import { Entity, System } from "ecsy"
-import { Object3DComponent } from "ecsy-three"
+import { Entity } from "ecsy"
 import { TransformComponent } from "../../transform/components/TransformComponent"
 import { RigidBody } from "../components/RigidBody"
-import { VehicleBody } from "../components/VehicleBody"
-import { PhysicsWorld } from "../components/PhysicsWorld"
-import { quaternion } from "../systems/PhysicsSystem"
-
-export class PhysicsSystem extends System {
-  execute(dt, t) {
-    PhysicsWorld.instance.frame++
-    PhysicsWorld.instance._physicsWorld.step(PhysicsWorld.instance.timeStep)
-
-    for (const entity of this.queries.physicsRigidBody.added) {
-      const physicsRigidBody = entity.getComponent(RigidBody)
-      let object = physicsRigidBody.getObject3D()
-      object ? "" : (object = { userData: { body: {} } })
-      let body
-      if (physicsRigidBody.type === "box") body = _createBox(entity)
-      else if (physicsRigidBody.type === "cylinder") body = _createCylinder(entity)
-      else if (physicsRigidBody.type === "share") body = _createShare(entity)
-      else if (physicsRigidBody.type === "convex") body = _createConvexGeometry(entity, null)
-      else if (physicsRigidBody.type === "ground") body = _createGroundGeometry(entity)
-
-      object.userData.body = body
-      PhysicsWorld.instance._physicsWorld.addBody(body)
-    }
-
-    for (const entity of this.queries.vehicleBody.added) {
-      const object = entity.getComponent<Object3DComponent>(Object3DComponent).value
-
-      const vehicleComponent = entity.getComponent(VehicleBody) as VehicleBody
-
-      const [vehicle, wheelBodies] = _createVehicleBody(entity, vehicleComponent.convexMesh)
-      object.userData.vehicle = vehicle
-      vehicle.addToWorld(PhysicsWorld.instance._physicsWorld)
-
-      for (let i = 0; i < wheelBodies.length; i++) {
-        PhysicsWorld.instance._physicsWorld.addBody(wheelBodies[i])
-      }
-    }
-
-    for (const entity of this.queries.physicsRigidBody.results) {
-      //  if (rigidBody.weight === 0.0) continue;
-      const transform = entity.getMutableComponent(TransformComponent)
-      const object = entity.getComponent(Object3DComponent).value
-      const body = object.userData.body
-      //console.log(body);
-      transform.position = body.position
-
-      quaternion.set(body.quaternion.x, body.quaternion.y, body.quaternion.z, body.quaternion.w)
-
-      transform.rotation = quaternion.toArray()
-    }
-
-    for (const entity of this.queries.vehicleBody.results) {
-      //  if (rigidBody.weight === 0.0) continue;
-      const transform = entity.getMutableComponent(TransformComponent)
-      const object = entity.getComponent(Object3DComponent).value
-      const vehicle = object.userData.vehicle.chassisBody
-
-      transform.position = vehicle.position
-      //transform.position.y += 0.6
-      quaternion.set(vehicle.quaternion.x, vehicle.quaternion.y, vehicle.quaternion.z, vehicle.quaternion.w)
-      transform.rotation = vehicle.quaternion.toArray()
-    }
-  }
-}
 
 export function _createBox(entity) {
   const rigidBody = entity.getComponent(RigidBody)
