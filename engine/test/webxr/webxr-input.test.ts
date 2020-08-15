b105068063f6a60b383f7b7a7167bc573951e25d
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-15 14:37:20 +0300

    add test 'check global magic from the underworld'

--- engine/test/webxr/webxr-input.test.ts
+++ engine/test/webxr/webxr-input.test.ts
@@ -4,11 +4,12 @@ import "./webxr-input.mock"
 import { InputSystem } from "../../src/input/systems/InputSystem"
 import { initializeWorld, registerSystem } from "../../src/ecs"
 
-initializeWorld()
-registerSystem(InputSystem)
-// new InputSystem()
-
 test("check navigator", () => {
   expect("xr" in navigator).toBeTruthy()
   expect("requestSession" in (navigator as any).xr).toBeTruthy()
+})
+
+test("check global magic from the underworld", () => {
+  initializeWorld()
+  registerSystem(InputSystem)
 })
