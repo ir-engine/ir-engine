43d62b289556b9c0ba5ff9031ebba94a65a78596
shawticus <shawmakesmagic@gmail.com>
2020-08-16 10:59:46 -0700

    Unexpected token

--- packages/client/spoke/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import "abortcontroller-polyfill/dist/polyfill-patch-fetch";
-import App from "./ui/App";
-import Api from "./api/Api";
-
-const api = new Api();
-
-ReactDOM.render(<App api={api} />, document.getElementById("app"));
