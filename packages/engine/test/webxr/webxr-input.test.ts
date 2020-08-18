61d3eca0090791dd64549600642a5f98c81a7f24
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-18 12:57:03 +0300

    fix tests env, add "start XR sesion" test
    Former-commit-id: a1a9a322391e5027ae395d6bf5aa6634a59bcaee

--- packages/engine/test/webxr/webxr-input.test.ts
+++ packages/engine/test/webxr/webxr-input.test.ts
@@ -2,13 +2,27 @@ import "./webxr-input.mock"
 //jest.mock("./webxr-input.mock")
 
 import { InputSystem } from "../../src/input/systems/InputSystem"
-import { initializeWorld, registerSystem } from "../../src/ecs"
-
-initializeWorld()
-registerSystem(InputSystem)
-// new InputSystem()
+import { initializeEngine, registerSystem, getSystem } from "../../src/ecs"
 
 test("check navigator", () => {
   expect("xr" in navigator).toBeTruthy()
   expect("requestSession" in (navigator as any).xr).toBeTruthy()
+})
+
+test("check hidden magic from the globalised world", () => {
+  expect(() => {
+    initializeEngine()
+    registerSystem(InputSystem)
+  }).not.toThrowError()
+})
+
+test("start XR sesion", () => {
+  expect(() => {
+    const system = getSystem(InputSystem)
+    system.init({ onVRSupportRequested })
+  }).not.toThrowError()
+
+  function onVRSupportRequested(isSupported = false) {
+    expect(isSupported).toBeTruthy()
+  }
 })
