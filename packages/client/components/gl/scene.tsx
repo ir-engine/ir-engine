9c55218b23012ad0e81b8e96bc410d03b8fc8d1a
shawticus <shawmakesmagic@gmail.com>
2020-08-19 03:35:21 -0700

    Latest commit after screenshare

--- packages/client/components/gl/scene.tsx
+++ packages/client/components/gl/scene.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react'
 import { initializeEngine } from "@xr3ngine/engine/src/initialize"
-import { PlayerController } from "./PlayerController"
+import { PlayerController } from "../gl_examples/PlayerController"
 import { createPrefab } from '@xr3ngine/engine/src/common/functions/createPrefab'
 
-import { car } from "./Car"
+import { car } from "../gl_examples/Car"
 
 export const EnginePage = (): any => {
   useEffect(() => {
@@ -12,7 +12,7 @@ export const EnginePage = (): any => {
     createPrefab(PlayerController)
 
     createPrefab(car) 
-    
+
   },[])
   return (
 <h1> Engine loaded. </h1>
