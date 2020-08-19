2ca07bd0318af2652ab8001502c4ef2debd10a24
SmachuloTetiana <tanichk@ukr.net>
2020-08-19 22:26:09 +0300

    Add TSDoc comments
    Former-commit-id: 8cff586aa087719b0c93c2f626c2051b9a7028a2

--- packages/engine/src/renderer/interfaces/PostProcessingSchema.ts
+++ packages/engine/src/renderer/interfaces/PostProcessingSchema.ts
@@ -1,3 +1,11 @@
+/**
+ * @typedef {object} PostProcessingSchema
+ *
+ * Declare Indexable Types
+ * when PostProcessingSchema is indexed with a string, it will return object
+ * @property {string} key is always string type
+ *
+ */
 export interface PostProcessingSchema {
   [key: string]: {
     [key: string]: any
