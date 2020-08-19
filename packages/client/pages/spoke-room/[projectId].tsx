ef4e997145bd7415baa0ad94c7990bce6c9eaef0
mohrtw <mohrtw@gmail.com>
2020-08-19 10:26:03 -0700

    replaced references to armada with engine

--- packages/client/pages/spoke-room/[projectId].tsx
+++ packages/client/pages/spoke-room/[projectId].tsx
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router'
 import Error404 from '../404'
 import React, { useEffect } from 'react'
 import { client } from '../../redux/feathers'
-import { initializeEngine } from "@xr3ngine/engine/dist/armada"
-import { createEntity } from "@xr3ngine/engine/dist/armada"
+import { initializeEngine } from "@xr3ngine/engine"
+import { createEntity } from "@xr3ngine/engine"
 
 const projectRegex = /\/([A-Za-z0-9]+)\/([a-f0-9-]+)$/
 
