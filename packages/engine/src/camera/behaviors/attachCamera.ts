04384fe5264ad429d01543e78c9433be2c3feaf3
SmachuloTetiana <tanichk@ukr.net>
2020-08-21 20:02:50 +0300

    Add TSDoc comments in engine/camera
    Former-commit-id: 4d564684852d8661a43263304ac5a237c58b14f6

--- packages/engine/src/camera/behaviors/attachCamera.ts
+++ packages/engine/src/camera/behaviors/attachCamera.ts
@@ -1,7 +1,11 @@
 import { Behavior } from '../../common/interfaces/Behavior';
 import { Entity } from '../../ecs/classes/Entity';
 import { CameraComponent } from '../components/CameraComponent';
-
+/**
+ * Attach Camera
+ * 
+ * @param {Entity} entity - The Entity
+ */
 export const attachCamera: Behavior = (entity: Entity): void => {
   CameraComponent.instance.followTarget = entity;
 };
--- packages/engine/src/camera/types/CameraTypes.ts
+++ packages/engine/src/camera/types/CameraTypes.ts
@@ -1,3 +1,8 @@
+/** 
+ * @property {Number} Orthographic
+ * @property {Number} Perspective
+*/
+
 export const CameraTypes = {
   Orthographic: 0,
   Perspective: 1
