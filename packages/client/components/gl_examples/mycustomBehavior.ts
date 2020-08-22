845bc508b60545d13d3e80f0c686fd834253ea29
HydraFire <hydrafire.plus@gmail.com>
2020-08-22 17:16:03 +0300

    init

--- packages/client/components/gl_examples/mycustomBehavior.ts
+++ packages/client/components/gl_examples/mycustomBehavior.ts
@@ -1,8 +1,24 @@
 import { Entity } from "@xr3ngine/engine/src/ecs/classes/Entity";
 import { State } from "@xr3ngine/engine/src/state/components/State";
+import { ScaleComponent } from "@xr3ngine/engine/src/transform/components/ScaleComponent";
+import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
 import { addComponent } from "@xr3ngine/engine/src/ecs/functions/EntityFunctions";
+import { getMutableComponent, getComponent } from "@xr3ngine/engine/src/ecs/functions/EntityFunctions";
 
 export const myCustomBehavior = (entity: Entity) => {
-    addComponent(entity, State);
+  console.log(entity);
+  console.log('test');
+
+  addComponent(entity,ScaleComponent)
+  addComponent(entity,TransformComponent)
+  let scal = getMutableComponent(entity, ScaleComponent);
+  let follower = getMutableComponent(entity, TransformComponent);
+//  target = getComponent<TransformComponent>(entityOut, TransformComponent);
+
+  // follower.position = target.position
+  scal.scale[2] = 9
+  follower.position[1] = 8;
     console.log("set up the prefab!");
+    console.log(entity);
+
 };
--- packages/client/components/gl_examples/staticWorldColliders.ts
+++ packages/client/components/gl_examples/staticWorldColliders.ts
@@ -2,25 +2,17 @@ import { BoxBufferGeometry, Mesh } from "three";
 import { myCustomBehavior } from "./mycustomBehavior";
 import { Prefab } from "@xr3ngine/engine/src/common/interfaces/Prefab";
 import { addObject3DComponent } from "@xr3ngine/engine/src/common/defaults/behaviors/Object3DBehaviors";
-
+import { ScaleComponent } from "@xr3ngine/engine/src/transform/components/ScaleComponent";
 import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
-import { VehicleBody } from "@xr3ngine/engine/src/physics/components/VehicleBody";
 import { ColliderComponent } from "@xr3ngine/engine/src/physics/components/ColliderComponent";
 import { RigidBody } from "@xr3ngine/engine/src/physics/components/RigidBody";
 
-const myCoolCar = new BoxBufferGeometry(2,1,4);
+const floor = new BoxBufferGeometry(1,0.1,1);
 
-export const car: Prefab = {
+export const staticWorldColliders: Prefab = {
     components: [
-      { type: TransformComponent },
-      {
-        type: ColliderComponent,
-        data: { type: 'box', size: {x:1,y:1,z:1} }
-      },
-      {
-        type: RigidBody,
-        data: { mass: 10 }
-      }
+
+
     ],
     onCreate: [
         // add a 3d object
@@ -28,7 +20,7 @@ export const car: Prefab = {
             behavior: addObject3DComponent,
             args: {
                 obj3d: Mesh,
-                obj3dArgs: myCoolCar
+                obj3dArgs: floor
             }
         },
         {
