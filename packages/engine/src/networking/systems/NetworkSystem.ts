d3ccaf4de17ede02c68e08bc167c9b2dbbec47b0
shawticus <shawmakesmagic@gmail.com>
2020-08-23 14:28:30 -0700

    Update network system to delay connection

--- packages/engine/src/networking/systems/NetworkSystem.ts
+++ packages/engine/src/networking/systems/NetworkSystem.ts
@@ -6,8 +6,15 @@ import { NetworkClient } from '../components/NetworkClient';
 import { NetworkObject } from '../components/NetworkObject';
 
 export class NetworkSystem extends System {
-  init (attributes) {
+  public static instance: NetworkSystem = null
 
+  connectToServer(ip: string, port: number){
+    Network.instance.transport.initialize(ip, port);
+    Network.instance.isInitialized = true;
+  }
+
+  init (attributes) {
+    NetworkSystem.instance = this
 console.log("NetworkSystem schema: ")
 
     const { schema } = attributes
@@ -16,16 +23,16 @@ console.log("NetworkSystem schema: ")
     addComponent(networkEntity, Network);
 
     // Late initialization of network
-      Network.instance.schema = schema
-      console.log("Transport: " + Network.instance.schema.transport)
-      console.log("Schema transport: ", schema.transport)
-      Network.instance.transport = new (schema.transport)();
-      Network.instance.transport.initialize();
-      Network.instance.isInitialized = true;
-      console.log("Network inited")
+    Network.instance.schema = schema
+    Network.instance.transport = new (schema.transport)();
+    console.log("NetowrkSystem ready, run connectToServer to... connect to the server!")
   }
-
-  public static instance: NetworkSystem = null
+  
+  public execute (): void {
+    this.queryResults.network.all?.forEach((entity: Entity) => {
+      // console.log(entity)
+    })
+  }
 
   static queries: any = {
     network: {
@@ -39,9 +46,4 @@ console.log("NetworkSystem schema: ")
     }
   }
 
-  public execute (): void {
-    this.queryResults.network.all?.forEach((entity: Entity) => {
-      // console.log(entity)
-    })
-  }
 }
