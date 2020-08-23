a6870bc7991b02abbdf891b5b3be5fd7274c52da
HydraFire <hydrafire.plus@gmail.com>
2020-08-23 20:06:12 +0300

    engine files
    Former-commit-id: b6ff8c3bdc151accc50d25c3be5c264ad323c32c

--- packages/engine/src/physics/behaviors/addPlayerCollider.ts
+++ packages/engine/src/physics/behaviors/addPlayerCollider.ts
@@ -3,11 +3,12 @@ import { ColliderComponent } from '../components/ColliderComponent';
 import { RigidBody } from '../../physics/components/RigidBody';
 import { Entity } from '../../ecs/classes/Entity';
 import { addComponent } from '../../ecs/functions/EntityFunctions';
-import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
 
-export const addMeshCollider: Behavior = (entity: Entity) => {
+export const addPlayerCollider: Behavior = (entity: Entity) => {
 
-  addComponent(entity, ColliderComponent, { type: 'box', scale: [10, 0.1, 10] });
+
+    addComponent(entity, ColliderComponent, { type: 'box', scale: [1, 1, 1], mass: 10 });
+    addComponent(entity, RigidBody, { isKinematic: true });
 
   return entity;
 };
