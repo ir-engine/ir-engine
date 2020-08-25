7f3d94f37be53019577c6834143d43c8de7aae07
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-08-25 18:27:17 +0500

    Fix /Me and /PartyParticipantWindow props error

--- packages/engine/src/initialize.ts
+++ packages/engine/src/initialize.ts
@@ -135,14 +135,14 @@ export function initializeEngine (options: any = DefaultInitializationOptions) {
     registerSystem(WebGLRendererSystem, { priority: 999 });
   }
 
-  if (options.debug === true) {
-    // If we're in debug, add a gridhelper
-    const gridHelper = new GridHelper(1000, 100, 0xffffff, 0xeeeeee);
-    scene.add(gridHelper);
-    const entity = createEntity();
-    // Add an ambient light to the scene
-    addObject3DComponent(entity, { obj3d: AmbientLight });
-  }
+  // if (options.debug === true) {
+  //   // If we're in debug, add a gridhelper
+  //   const gridHelper = new GridHelper(1000, 100, 0xffffff, 0xeeeeee);
+  //   scene.add(gridHelper);
+  //   const entity = createEntity();
+  //   // Add an ambient light to the scene
+  //   addObject3DComponent(entity, { obj3d: AmbientLight });
+  // }
 
   // Start our timer!
   if (isBrowser) setTimeout(startTimer, 1000);
