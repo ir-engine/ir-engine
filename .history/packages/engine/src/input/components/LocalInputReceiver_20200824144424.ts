3a993521df8afb75998fbcd11fbc728f3242d734
shawticus <shawmakesmagic@gmail.com>
2020-08-24 18:34:11 -0700

    Remove .history

--- .history/packages/engine/src/input/components/LocalInputReceiver_20200824144424.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from "../../ecs/classes/Component";
-
-// Input inherits from BehaviorComponent, which adds .map and .data
-export class LocalInputReceiver extends Component<LocalInputReceiver> {}
--- .history/packages/engine/src/physics/components/index_20200824182126.ts
+++
@@ -1,9 +0,0 @@
-// created from 'create-ts-index'
-
-export * from './ColliderComponent';
-export * from './PhysicsManager';
-export * from './RaycastComponent';
-export * from './RigidBody';
-export * from './VehicleBody';
-export * from './VehicleComponent';
-export * from './WheelBody';
