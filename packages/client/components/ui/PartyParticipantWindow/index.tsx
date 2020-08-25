32e437ab0dbae1c886223595fe442615512a1ea3
Kyle Baran <kbaran@bitscoop.com>
2020-08-25 14:57:17 -0700

    Sort of got producer displaying on own screen, trying to get it to conditionally render.
    Former-commit-id: bcc102652fd247ca6a2f9eab610619f17d127a11

--- packages/client/components/ui/PartyParticipantWindow/index.tsx
+++ packages/client/components/ui/PartyParticipantWindow/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.scss';
 
 interface ContainerProportions {
@@ -8,39 +8,67 @@ interface ContainerProportions {
 
 interface Props {
   containerProportions?: ContainerProportions;
-  videoTrack?: any
+  videoStream?: any;
+  audioStream?: any;
+  peerId?: string
 }
 
 function PartyParticipantWindow (props: Props): JSX.Element {
+    const {
+        audioStream,
+        peerId,
+        videoStream
+    } = props;
   // Video and audio elements' ref
-  const videoRef = React.createRef<HTMLVideoElement>();
-  const audioRef = React.createRef<HTMLAudioElement>();
+    let videoEl = React.createRef<HTMLVideoElement>();
+    let audioEl = React.createRef<HTMLAudioElement>();
+
+    useEffect(() => {
+        videoEl.current.id = `${peerId}_video`;
+        videoEl.current.autoplay = true;
+        videoEl.current.setAttribute('playsinline', 'true');
+        videoEl.current.setAttribute('controls', 'false')
+        if (videoStream) {
+            videoEl.current.srcObject = new MediaStream([videoStream.track.clone()]);
+            (videoEl as any).current.mediaStream = videoStream;
+            videoEl.current.play().catch((err) => {
+                console.log('Video play error')
+                console.log(err)
+            })
+        }
+
+        audioEl.current.id = `${peerId}_audio`;
+        audioEl.current.setAttribute('playsinline', 'true');
+        audioEl.current.setAttribute('autoplay', 'true');
+        audioEl.current.setAttribute('controls', 'false')
+        audioEl.current.setAttribute('muted', 'false')
+        if (audioStream) {
+            audioEl.current.srcObject = new MediaStream([audioStream.track.clone()]);
+            (audioEl as any).current.mediaStream = audioStream;
+            audioEl.current.play().catch((err) => {
+                console.log('Audio play error')
+                console.log(err)
+            })
+        }
+        audioEl.current.volume = 0;
+    })
   // Add mediasoup integration logic here to feed single peer's stream to these video/audio elements
   return (
     <div className="videoContainer" style={props.containerProportions || {}}>
-      <video
-        ref={videoRef}
-        // className={classnames({
-        //   'is-me': isMe,
-        //   hidden: !videoVisible || !videoCanPlay,
-        //   'network-error':
-        //     videoVisible &&
-        //     videoMultiLayer &&
-        //     consumerCurrentSpatialLayer === null,
-        // })}
-        autoPlay
-        playsInline
-        muted
-        controls={false}
-      />
-
-      <audio
-        ref={audioRef}
-        autoPlay
-        playsInline
-        muted={true}
-        controls={false}
-      />
+        <video
+            ref={videoEl}
+            autoPlay
+            playsInline
+            muted
+            controls={false}
+        />
+        <audio
+            ref={audioEl}
+            autoPlay
+            playsInline
+            muted={true}
+            controls={false}
+        />
     </div>
   );
 }
