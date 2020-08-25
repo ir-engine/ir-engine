3848254208cb5eca5f76fc3fab73ec582b298c00
shawticus <shawmakesmagic@gmail.com>
2020-08-24 18:34:11 -0700

    Remove .history
    Former-commit-id: 3a993521df8afb75998fbcd11fbc728f3242d734

--- .history/packages/engine/src/actor/classes/interfaces/ICharacterState_20200824131952.ts
+++
@@ -1,8 +0,0 @@
-export interface ICharacterState {
-	canFindVehiclesToEnter: boolean; // Find a suitable car and run towards it
-	canEnterVehicles: boolean; // Actually get into the vehicle
-	canLeaveVehicles: boolean;
-
-	update(timeStep: number): void;
-	onInputChange(): void;
-}
