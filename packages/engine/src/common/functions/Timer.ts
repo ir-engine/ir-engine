777106af95d9255efc8e253eddbb17f23dd95aca
xiani <xianionline@gmail.com>
2020-08-25 14:46:38 +0300

    timer createFixedTimestep

--- packages/engine/src/common/functions/Timer.ts
+++ packages/engine/src/common/functions/Timer.ts
@@ -1,4 +1,5 @@
 import { isBrowser } from './isBrowser';
+import { now } from "./now";
 
 export function Timer (
   callbacks: { update?: Function, render?: Function },
@@ -8,7 +9,6 @@ export function Timer (
 
   let last = 0;
   let accumulated = 0;
-  //let tick = 0;
   let delta = 0;
   let frameId;
 
@@ -16,12 +16,8 @@ export function Timer (
     if (last !== null) {
       delta = (time - last) / 1000
       accumulated = accumulated + delta;
-      // while (accumulated > increment) {
-      //   if (callbacks.update) callbacks.update(increment, tick);
-      //   tick = tick + 1;
-      //   accumulated = accumulated - increment;
-      // }
-      if (callbacks.update) callbacks.update(accumulated, delta);
+
+      if (callbacks.update) callbacks.update(delta, accumulated);
     }
     last = time;
     if (callbacks.render) callbacks.render();
@@ -44,4 +40,61 @@ export function Timer (
 }
 function requestAnimationFrameOnServer (f) {
   setImmediate(() => f(Date.now()));
+}
+
+export function createFixedTimestep(updatesPerSecond:number, callback:(time:number)=>void):(delta:number)=>void {
+  const timestep = 1 / updatesPerSecond
+  const limit = timestep * 1000
+  const updatesLimit = updatesPerSecond
+
+  const subsequentErorrsLimit = 10
+  const subsequentErorrsResetLimit = 1000
+  let subsequentErorrsShown = 0
+  let shownErrorPreviously = false
+  let accumulator = 0
+
+  return delta => {
+    const start = now()
+    let timeUsed = 0
+    let updatesCount = 0
+
+    accumulator += delta
+
+    let accumulatorDepleted = accumulator < timestep
+    let timeout = timeUsed > limit
+    let updatesLimitReached = updatesCount > updatesLimit
+    while (!accumulatorDepleted && !timeout && !updatesLimitReached) {
+      callback(accumulator)
+
+      accumulator -= timestep
+      ++updatesCount
+
+      timeUsed = now() - start
+      accumulatorDepleted = accumulator < timestep
+      timeout = timeUsed > limit
+      updatesLimitReached = updatesCount >= updatesLimit
+    }
+
+    if (!accumulatorDepleted) {
+      if (subsequentErorrsShown <= subsequentErorrsLimit) {
+        console.error('Fixed timesteps SKIPPED time used ', timeUsed, 'ms (of ', limit, 'ms), made ', updatesCount, 'updates. skipped ', Math.floor(accumulator / timestep))
+        console.log('accumulatorDepleted', accumulatorDepleted, 'timeout', timeout, 'updatesLimitReached', updatesLimitReached)
+      } else {
+        if (subsequentErorrsShown > subsequentErorrsResetLimit) {
+          console.error('FixedTimestep', subsequentErorrsResetLimit, ' subsequent errors catched')
+          subsequentErorrsShown = subsequentErorrsLimit - 1
+        }
+      }
+
+      if (shownErrorPreviously) {
+        subsequentErorrsShown++
+      }
+      shownErrorPreviously = true
+
+      accumulator = accumulator % timestep
+    } else {
+      subsequentErorrsShown = 0
+      shownErrorPreviously = false
+    }
+  }
 }
