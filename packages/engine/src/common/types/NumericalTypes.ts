33459845f28cbf895bae5a4840c70879e322a919
shawticus <shawmakesmagic@gmail.com>
2020-08-26 21:06:45 -0700

    Refactor transform
    Former-commit-id: 75b72e8380f9ceab454384e715d21184d7494e64

--- packages/engine/src/common/types/NumericalTypes.ts
+++ packages/engine/src/common/types/NumericalTypes.ts
@@ -1,12 +1,11 @@
 import * as THREE from "three"
-import { default as vec2, default as vec3, vec4, mat3, mat4 } from 'gl-matrix';
 
 export type Binary = 0 | 1
 export type Scalar = number
-export type Vector2 = THREE.Vector2 | [number, number] |  vec2.vec2
-export type Vector3 = THREE.Vector3 | [number, number, number] | vec3.vec3
-export type Vector4 = THREE.Vector4 | [number, number, number, number] | vec4
-export type Matrix3 = mat3
-export type Matrix4 = mat4
+export type Vector2 = THREE.Vector2 | [number, number]
+export type Vector3 = THREE.Vector3 | [number, number, number]
+export type Vector4 = THREE.Vector4 | [number, number, number, number]
+export type Matrix3 = THREE.Matrix3
+export type Matrix4 = THREE.Matrix4
 
 export type NumericalType = Binary | Scalar | Vector2 | Vector3 | Vector4 | Matrix3 | Matrix4
--- packages/engine/src/transform/behaviors/transformBehavior.ts
+++ packages/engine/src/transform/behaviors/transformBehavior.ts
@@ -1,4 +1,3 @@
-import { vec3, quat } from 'gl-matrix';
 import { Quaternion } from 'three';
 import { Object3DComponent } from '../../common/components/Object3DComponent';
 import { Behavior } from '../../common/interfaces/Behavior';
@@ -7,30 +6,15 @@ import { TransformComponent } from '../components/TransformComponent';
 import { getMutableComponent } from '../../ecs/functions/EntityFunctions';
 const q: Quaternion = new Quaternion();
 let transform: TransformComponent;
-let _position: any
-let _velocity: any
 export const transformBehavior: Behavior = (entity: Entity, args: { event: MouseEvent }, delta): void => {
   transform = getMutableComponent(entity, TransformComponent);
-  vec3.set(_position, transform.position[0], transform.position[1], transform.position[2]);
-  vec3.set(_velocity, transform.velocity[0], transform.velocity[1], transform.velocity[2]);
-  vec3.transformQuat(_velocity, _velocity, quat.fromValues(transform.rotation[0],transform.rotation[1],transform.rotation[2],transform.rotation[3]))
-
-  // const object3DComponent = getMutableComponent<Object3DComponent>(entity, Object3DComponent);
-  // if (object3DComponent == undefined) return console.warn('No Object3D located ', console.log(entity.componentTypes));
-  // Apply velocity to position
-
-  // TODO: Apply with three math
-
-  // if (Math.abs(vec3.length(_velocity)) > 0) {
-  //   vec3.scale(_deltaV, _velocity, delta);
-  //   vec3.add(_position, _position, _deltaV);
-  //   transform.position[0] = _position[0];
-  //   transform.position[1] = _position[1];
-  //   transform.position[2] = _position[2];
+  // if(transform.velocity.length() > 0.0001){
+  //   // Do we need to scale velocity by dt?
+  //   transform.position.add(transform.velocity.multiplyScalar(delta))
   // }
 
-  // object3DComponent.value.position.set(_position[0], _position[1], _position[2]);
-
-  // TODO: Need to apply spin
-  // object3DComponent.value.rotation.setFromQuaternion(q.fromArray(transform.rotation));
+  const object3DComponent = getMutableComponent<Object3DComponent>(entity, Object3DComponent);
+  if (object3DComponent == undefined) return console.warn('No Object3D located ', console.log(entity.componentTypes));
+  object3DComponent.value.position.copy(transform.position)
+  object3DComponent.value.rotation.setFromQuaternion(transform.rotation)
 };
