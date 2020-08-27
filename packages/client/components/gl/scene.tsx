2cd08e6135654df30af08b5ac0be6bb97c819691
alexander-shvets <alexander.shvets.ua@gmail.com>
2020-08-27 09:40:07 +0300

    add audio file

--- packages/client/components/gl/scene.tsx
+++ packages/client/components/gl/scene.tsx
@@ -44,7 +44,7 @@ export const EnginePage: FunctionComponent = (props: any) => {
         enabled: true
       },
       audio: {
-        src: '/audio/108.ogg'
+        src: '/audio/djMagda.m4a'
       },
     };
     initializeEngine(InitializationOptions);
