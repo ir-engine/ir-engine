832d5e61c140f60aa528684ac5ff2f70126dbe75
shawticus <shawmakesmagic@gmail.com>
2020-08-27 16:32:13 -0700

    Remove duplicate test folder

--- test/services/accept-invite.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../server/app';
-
-describe('\'accept-invite\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('accept-invite');
-    expect(service).toBeTruthy();
-  });
-});
--- test/services/invite-type.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../server/app';
-
-describe('\'invite-type\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('invite-type');
-    expect(service).toBeTruthy();
-  });
-});
