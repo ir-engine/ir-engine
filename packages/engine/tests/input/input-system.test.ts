1e468abe3d11022d808ba51b7a2cecfaa34f3cf7
xiani <xianionline@gmail.com>
2020-08-28 18:25:15 +0300

    input system listeners cleanup
    Former-commit-id: 37de041637eae3d8d63f189f478778657b86b5ed

---
+++ packages/engine/tests/input/input-system.test.ts
@@ -0,0 +1,30 @@
+import { registerSystem } from "../../src/ecs/functions/SystemFunctions";
+import { InputSystem } from "../../src/input/systems/InputSystem";
+import { execute } from "../../src/ecs/functions/EngineFunctions";
+import { addComponent, createEntity, removeComponent } from "../../src/ecs/functions/EntityFunctions";
+import { Input } from "../../src/input/components/Input";
+import { CharacterInputSchema } from "../../src/templates/character/CharacterInputSchema";
+
+test("cleanup event listeners", () => {
+  const addListener = jest.spyOn(document, 'addEventListener')
+  const removeListener = jest.spyOn(document, 'removeEventListener')
+
+  registerSystem(InputSystem, { useWebXR: false });
+  const entity = createEntity()
+  addComponent(entity, Input, { schema: CharacterInputSchema })
+  execute();
+
+  expect(removeListener.mock.calls.length).toBe(0)
+
+  removeComponent(entity, Input)
+  execute();
+
+  // run through mocked addListener.mock.calls there should be parameters listed,
+  // add callbacks to Set, then loop removeListener.mock.calls and remove their callbacks from Set,
+  // in the end Set should be empty
+  const listenersSet:Set<Function> = new Set()
+  addListener.mock.calls.forEach((call:any) => listenersSet.add(call[1]))
+  removeListener.mock.calls.forEach((call:any) => listenersSet.delete(call[1]))
+
+  expect(listenersSet.size).toBe(0)
+})
