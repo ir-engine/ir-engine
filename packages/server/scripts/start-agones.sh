3cc66019d5281566454310fe6f17567ad5f32299
shawticus <shawmakesmagic@gmail.com>
2020-09-06 18:02:18 -0700

    Moved agones sidecar and binary

--- packages/server/scripts/start-agones.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo -e "\e[32mStarting Agones sidecar for local development..."
-cd ../bin/agones && ./sdk-server.linux.amd64 --local
--- packages/server/scripts/start-db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo -e "\e[32mInitializing mariadb..."
-docker stop xr3ngine
-docker rm xr3ngine
-docker-compose up
