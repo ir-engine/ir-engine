5f4ff0b89ae93d89031f2dca96fd8015352f22e0
Kyle Baran <kbaran@bitscoop.com>
2020-09-11 13:20:52 -0700

    Rewrote get-local-server-ip to use 'internal-ip' library.

--- packages/server/src/util/get-local-server-ip.ts
+++ packages/server/src/util/get-local-server-ip.ts
@@ -1,29 +1,16 @@
-import {networkInterfaces} from "os";
+import internalIp from 'internal-ip';
 
 interface ServerAddress {
-    ipAddress: string,
-    port: string
+    ipAddress: string;
+    port: string;
 }
 
-export default (): ServerAddress => {
-    const nets = networkInterfaces();
-    const results = Object.create(null); // or just '{}', an empty object
-    for (const name of Object.keys(nets)) {
-        for (const net of nets[name]) {
-            // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
-            if (net.family === 'IPv4' && !net.internal) {
-                if (!results[name]) {
-                    results[name] = [];
-                }
-
-                results[name].push(net.address);
-            }
-        }
-    }
-    console.log('Non-internal local ports:')
-    console.log(results)
+export default async (): Promise<ServerAddress> => {
+    const ip = await internalIp.v4()
+    console.log('internal ip:');
+    console.log(ip);
     return {
-        ipAddress: results.en0 ? results.en0[0] : results.eno1 ? results.eno1[0] : '127.0.0.1',
+        ipAddress: ip,
         port: '3030'
     }
 }
