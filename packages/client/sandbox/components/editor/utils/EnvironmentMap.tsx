0cda52ec84ac5d3a2ea8bf5cdaca58756c2d9929
Kyle Baran <kbaran@bitscoop.com>
2020-09-11 22:54:11 -0700

    Moved all editor components and page files to a sandbox directory that's ignored by client's tsconfig. Everything appears to build without errors now.

--- packages/client/sandbox/components/editor/utils/EnvironmentMap.tsx
+++ packages/client/sandbox/components/editor/utils/EnvironmentMap.tsx
@@ -1,12 +1,12 @@
 // @ts-nocheck
 import { CubeTextureLoader, RGBFormat } from "three";
 import { RethrownError } from "./errors";
-import negx from "../../../public/cubemap/negx.jpg";
-import negy from "../../../public/cubemap/negy.jpg";
-import negz from "../../../public/cubemap/negz.jpg";
-import posx from "../../../public/cubemap/posx.jpg";
-import posy from "../../../public/cubemap/posy.jpg";
-import posz from "../../../public/cubemap/posz.jpg";
+import negx from "../../../../public/cubemap/negx.jpg";
+import negy from "../../../../public/cubemap/negy.jpg";
+import negz from "../../../../public/cubemap/negz.jpg";
+import posx from "../../../../public/cubemap/posx.jpg";
+import posy from "../../../../public/cubemap/posy.jpg";
+import posz from "../../../../public/cubemap/posz.jpg";
 let cubeMapTexturePromise = null;
 export let environmentMap = null;
 export function loadEnvironmentMap() {
