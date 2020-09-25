a03de29ffb94c7e64d505073f4b9baea2c60b7f1
wife <tanya.vykloik@gmail.com>
2020-09-25 13:16:32 +0300

    cleanup dialog, navUserWidget, drawer on start page, change(temporary) theme coloring, make standard components to material UI components(in they wasn't material UI)
    Former-commit-id: ebd0d14074521a2f34d4b39b5d274157c32bda68

--- packages/client/theme.tsx
+++ packages/client/theme.tsx
@@ -1,25 +1,57 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
-// Create a theme instance.
+// // Create a theme instance.
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       main: '#556cd6'
+//     },
+//     secondary: {
+//       main: '#19857b'
+//     },
+//     error: {
+//       main: red.A400
+//     },
+//     background: {
+//       default: '#fff'
+//     }
+//   },
+//   typography: {
+//     fontFamily: '"Roboto", sans-serif'
+//   }
+// });
+
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6'
+      main: '#66B933'
     },
     secondary: {
-      main: '#19857b'
+      main: '#633d6d'
     },
     error: {
       main: red.A400
     },
     background: {
-      default: '#fff'
+      default: '#121212'
     }
   },
   typography: {
-    fontFamily: '"Roboto", sans-serif'
-  }
+    fontFamily: '"Roboto", sans-serif',
+      button: {
+        color: '#FFF'        
+      },
+  },
+  overrides: {
+    MuiFab: {
+      root: {
+        text:{
+          color: '#FFF' 
+        }           
+      },
+    },
+  },
 });
 
 export default theme;
