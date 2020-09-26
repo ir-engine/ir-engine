8b93a80b230aa0e4075ff5345fb0e393576212e1
Vyacheslav Solovjov <xianionline@gmail.com>
2020-09-26 09:49:51 +0300

    model viewer

--- packages/engine/src/templates/interactive/prefabs/RazerLaptop.ts
+++ packages/engine/src/templates/interactive/prefabs/RazerLaptop.ts
@@ -1,53 +1,15 @@
-/*
-razer_laptop.glb
-
-================================
-
-And this URL
-https://www.razer.com/gaming-laptops/Razer-Blade-Stealth-13/RZ09-03102E52-R3U1
-
-================================
-
-Blurb:
-Razer Blade Stealth 13 - 4K Touch 60Hz - GeForce GTX 1650 Ti Max-Q - Black
-The World’s First Gaming Ultrabook™
-US$1,999.99
-Specifications
-10th Gen Intel® Core™ i7-1065G7 Quad-Core Processor
-Windows 10 Home
-13.3" 4K Touch 60Hz w/ 4.9 mm slim side bezel
-NVIDIA® GeForce GTX 1650 Ti Max-Q (4GB GDDR5 VRAM)
-512GB
-16GB dual-channel (fixed)
-Single-zone RGB powered by Razer Chroma™
-
-================================
-
-Learn more button goes here: https://www.razer.com/gaming-laptops/razer-blade
-
-
-
-**********
-Bonus points if you can get this to work w/ mobile AR on a new page. https://modelviewer.dev/examples/webxr.html
-
- */
-
 import { Prefab } from "@xr3ngine/engine/src/common/interfaces/Prefab";
 import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
 import { addMeshCollider } from "@xr3ngine/engine/src/physics/behaviors/addMeshCollider";
 import { Interactive } from "../../../interaction/components/Interactive";
-import { getInCar } from "../../car/behaviors/getInCarBehavior";
-import { getInCarPossible } from "../../car/behaviors/getInCarPossible";
 import { addComponentFromSchema } from "../../../common/behaviors/addComponentFromSchema";
 import { AssetLoader } from "../../../assets/components/AssetLoader";
 import { Entity } from "../../../ecs/classes/Entity";
-import { createTrimesh } from "../../../physics/behaviors";
 import { onInteraction, onInteractionHover } from "../functions/commonInteractive";
 
 export const RazerLaptop: Prefab = {
   components: [
     { type: TransformComponent, data: { position: [3,0.5,3]} },
-    // { type: SoundEffect, data: { src: 'audio/honk.mp3', volume: 0.6 } },
     { type: Interactive, data: {
         onInteraction: onInteraction,
         onInteractionFocused: onInteractionHover,
@@ -58,7 +20,18 @@ export const RazerLaptop: Prefab = {
             url: 'https://www.razer.com/gaming-laptops/Razer-Blade-Stealth-13/RZ09-03102E52-R3U1',
             buyUrl: 'https://www.razer.com/product-added/RZ09-03102E52-R3U1',
             learnMoreUrl: 'https://www.razer.com/gaming-laptops/razer-blade',
-            modelUrl: 'assets/models/razer_laptop.glb'
+            modelUrl: 'models/razer_laptop.glb',
+            htmlContent: `<h5>Razer Blade Stealth 13 - 4K Touch 60Hz - GeForce GTX 1650 Ti Max-Q - Black</h5>
+The World’s First Gaming Ultrabook™<br />
+US$1,999.99<br />
+<strong>Specifications</strong>
+10th Gen Intel® Core™ i7-1065G7 Quad-Core Processor<br />
+Windows 10 Home<br />
+13.3" 4K Touch 60Hz w/ 4.9 mm slim side bezel<br />
+NVIDIA® GeForce GTX 1650 Ti Max-Q (4GB GDDR5 VRAM)<br />
+512GB<br />
+16GB dual-channel (fixed)<br />
+Single-zone RGB powered by Razer Chroma™`
           },
           interactionText: 'View product info'
         }
@@ -77,8 +50,6 @@ export const RazerLaptop: Prefab = {
           castShadow: true,
           onLoaded: (entityIn:Entity, args:unknown, delta:number, entityOut:Entity): void => {
 
-            // createTrimesh(vehicleCollider, offset, mass)
-
           }
         }
       }
@@ -86,7 +57,7 @@ export const RazerLaptop: Prefab = {
     {
       behavior: addMeshCollider,
       args: {
-        type: 'sphere', scale: [1, 1, 1], mass: 1
+        type: 'box', scale: [0.434122, 0.55483, 0.367724], mass: 1
       }
     },
   ]
