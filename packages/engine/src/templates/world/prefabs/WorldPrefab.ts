fab743f77c5f9896b88cfca1684fabfdca85d2dc
shawticus <shawmakesmagic@gmail.com>
2020-09-28 11:09:16 -0700

    Showroom updated

--- packages/engine/src/templates/world/prefabs/WorldPrefab.ts
+++ packages/engine/src/templates/world/prefabs/WorldPrefab.ts
@@ -13,7 +13,7 @@ export const WorldPrefab: Prefab = {
             args: {
                 component: AssetLoader,
                 componentArgs: {
-                    url: "models/Showroom2.glb",
+                    url: "models/worlds/Showroom.glb",
                     receiveShadow: true,
                     castShadow: false,
                     onLoaded: addWorldColliders
