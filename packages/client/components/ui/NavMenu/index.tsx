09a6da909e9ddd11063c551cae09a241165b6ad0
Tanya Vykliuk <tanya.vykloik@gmail.com>
2020-10-07 18:18:40 +0300

    remove logo navigationto home

--- packages/client/components/ui/NavMenu/index.tsx
+++ packages/client/components/ui/NavMenu/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import NavUserWidget from '../NavUserWidget';
 import AppBar from '@material-ui/core/AppBar';
-import Router from 'next/router';
 import './style.scss';
 
 export const NavMenu = (): any => {
-  const homeNav = (): void => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    Router.push('/');
-  };
   return (
     <AppBar className="appbar">
       <NavUserWidget />
