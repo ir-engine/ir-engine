1d82cda48e9b10f3b066635375a1b67921efdd61
Vyacheslav Solovjov <xianionline@gmail.com>
2020-10-12 17:31:57 +0300

    attempt to use AssetLoadingSystem

--- packages/engine/src/templates/interactive/prefabs/interactiveBox.ts
+++ packages/engine/src/templates/interactive/prefabs/interactiveBox.ts
@@ -1,4 +1,4 @@
-import { BoxBufferGeometry, Mesh, MeshPhongMaterial } from "three";
+import { AnimationMixer, BoxBufferGeometry, Mesh, MeshPhongMaterial } from "three";
 import { Prefab } from "@xr3ngine/engine/src/common/interfaces/Prefab";
 import { addObject3DComponent } from "@xr3ngine/engine/src/common/behaviors/Object3DBehaviors";
 import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
@@ -6,6 +6,15 @@ import { addMeshCollider } from "@xr3ngine/engine/src/physics/behaviors/addMeshC
 import { addMeshRigidBody } from "@xr3ngine/engine/src/physics/behaviors/addMeshRigidBody";
 import { Interactive } from "../../../interaction/components/Interactive";
 import { onInteraction, onInteractionHover } from "../functions/interactiveBox";
+import {
+    removeComponent,
+    addComponent,
+    getComponent,
+    getMutableComponent
+} from "../../../ecs/functions/EntityFunctions";
+import { AssetLoader } from "../../../assets/components/AssetLoader";
+import { Engine } from "../../../ecs/classes/Engine";
+import { CharacterComponent } from "../../character/components/CharacterComponent";
 
 const boxGeometry = new BoxBufferGeometry(1, 1, 1);
 const boxMaterial = new MeshPhongMaterial({ color: 'blue' });
@@ -19,7 +28,41 @@ export const interactiveBox: Prefab = {
             data: {
                 interactiveDistance: 3,
                 onInteractionFocused: onInteractionHover,
-                onInteraction: onInteraction
+                onInteraction: (entity, args, delta, entity2): void => {
+                    removeComponent(entity, AssetLoader, true)
+
+                    const avatarsList = [
+                        'Allison.glb',
+                        'Andy.glb',
+                        'Animation.glb',
+                        'Erik.glb',
+                        'Geoff.glb',
+                        'Jace.glb',
+                        'Rose.glb',
+                    ];
+                    const nextAvatarIndex = Math.round((avatarsList.length-1) * Math.random());
+                    console.log('nextAvatarIndex', nextAvatarIndex);
+                    const nextAvatarSrc = avatarsList[nextAvatarIndex];
+                    console.log('nextAvatarSrc', nextAvatarSrc);
+
+                    addComponent(entity, AssetLoader, {
+                        url: "models/avatars/" + nextAvatarSrc,
+                        receiveShadow: true,
+                        castShadow: true,
+                        onLoaded: (entity, args) => {
+                            console.log('loaded new avatar model', args);
+                            const actor = getMutableComponent(entity, CharacterComponent);
+                            actor.modelContainer.children = [ args.asset.scene ];
+                            console.log('actor.mixer', actor.mixer);
+
+                            // actor.mixer = new AnimationMixer(Engine.scene);
+                            // const action = actor.mixer.clipAction(idleAnimation);
+                            // action.setDuration(20);
+                            // action.play();
+                        }
+                    });
+                    //debugger;
+                }
             }
         }
     ],
