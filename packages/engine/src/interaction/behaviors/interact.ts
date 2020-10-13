4fe03373ea545ba2eb0802d9c3994fd4409c4c76
Andrii Blashchuk <ablashch@gmail.com>
2020-10-13 17:01:55 +0300

    mouse button action event fix v.1

--- packages/engine/src/interaction/behaviors/interact.ts
+++ packages/engine/src/interaction/behaviors/interact.ts
@@ -3,6 +3,10 @@ import { Entity } from "../../ecs/classes/Entity";
 import { Interactive } from "../components/Interactive";
 import { getComponent, hasComponent } from "../../ecs/functions/EntityFunctions";
 import { Interacts } from "../components/Interacts";
+import { Input } from "../../input/components/Input";
+import { DefaultInput } from "../../templates/shared/DefaultInput";
+import { CharacterComponent } from "../../templates/character/components/CharacterComponent";
+import { LifecycleValue } from "../../common/enums/LifecycleValue";
 
 /**
  *
@@ -10,25 +14,43 @@ import { Interacts } from "../components/Interacts";
  * @param args
  * @param delta
  */
-export const interact:Behavior = (entity: Entity, args:any, delta): void => {
+export const interact: Behavior = (entity: Entity, args: any, delta): void => {
   if (!hasComponent(entity, Interacts)) {
-    console.error('Attempted to call interact behavior, but actor does not have Interacts component');
-    return;
+    console.error(
+      'Attempted to call interact behavior, but actor does not have Interacts component'
+    )
+    return
   }
-  const { focusedInteractive:focusedEntity } = getComponent(entity, Interacts);
+  const { focusedInteractive: focusedEntity } = getComponent(entity, Interacts)
+  const input = getComponent(entity, Input)
+  // const mouseStarted = LifecycleValue.STARTED;
+  // const mouseEnded = LifecycleValue.ENDED;
+  // const mouseScreenPosition = getComponent(entity, Input).data.get(DefaultInput.SCREENXY);
+  let mouseScreenPosition = getComponent(entity, Input).data.get(DefaultInput.MOUSE_MOVEMENT)
+  let mouseButtonDownCheckStart = input.schema.inputButtonBehaviors[8].started[0].args.phaze
+  let mouseButtonDownCheckEnd = input.schema.inputButtonBehaviors[8].ended[0].args.phaze
 
-  if (!focusedEntity) {
-    // no available interactive object is focused right now
-    return;
-  }
+  console.log(mouseScreenPosition)
 
-  if (!hasComponent(focusedEntity, Interactive)) {
-    console.error('Attempted to call interact behavior, but target does not have Interactive component');
-    return;
-  }
+  if (mouseButtonDownCheckEnd && !mouseScreenPosition) {
+    if (!focusedEntity) {
+      // no available interactive object is focused right now
+      return
+    }
 
-  const interactive = getComponent(focusedEntity, Interactive);
-  if (interactive && typeof interactive.onInteraction === 'function') {
-    interactive.onInteraction(entity, args, delta, focusedEntity);
+    if (!hasComponent(focusedEntity, Interactive)) {
+      console.error(
+        'Attempted to call interact behavior, but target does not have Interactive component'
+      )
+      return
+    }
+
+    const interactive = getComponent(focusedEntity, Interactive)
+    if (interactive && typeof interactive.onInteraction === 'function') {
+      interactive.onInteraction(entity, args, delta, focusedEntity)
+    }
   }
-};
+  // else {
+
+  // }
+}
