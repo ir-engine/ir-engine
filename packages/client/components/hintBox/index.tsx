b71667e929922bd0547f20875d5f94ae51af9550
Tanya Vykliuk <tanya.vykloik@gmail.com>
2020-10-13 18:21:03 +0300

    add styles to tips snackbar; -move snackbar tips to component
    Former-commit-id: e81c7b456c6d473477b5911ffc387293c71969ea

--- packages/client/components/hintBox/index.tsx
+++ packages/client/components/hintBox/index.tsx
@@ -1,37 +1,64 @@
-import React, { CSSProperties, FunctionComponent } from "react";
-import './style.scss';
+// import React, { CSSProperties, FunctionComponent } from "react";
+// import './style.scss';
 
-export type HintBoxProps = {
-  layout: string;
-};
+// export type HintBoxProps = {
+//   layout: string;
+// };
 
-export const HintBox: FunctionComponent<HintBoxProps> = ({ layout }: HintBoxProps) => {
-  const keysHintsList = {
-    default: [{keys:['w','s','a','d'], title:'Move'},{keys:['space'], title:'Jump'}],
-    car: [{keys:['w','s','a','d'], title:'Move'},{keys:['e'], title:'get out of the car'}]
-  };
+// export const HintBox: FunctionComponent<HintBoxProps> = ({ layout }: HintBoxProps) => {
+//   const keysHintsList = {
+//     default: [{keys:['w','s','a','d'], title:'Move'},{keys:['space'], title:'Jump'}],
+//     car: [{keys:['w','s','a','d'], title:'Move'},{keys:['e'], title:'get out of the car'}]
+//   };
 
-  document.getElementById('control-hint-box') ?  document.getElementById('control-hint-box').style.opacity = '1' : null;
-
-  // setTimeout(()=>{
-  //     var fadeTarget = document.getElementById('control-hint-box');
-  //     var fadeEffect = setInterval(function () {
-  //         if (!fadeTarget.style.opacity) {
-  //             fadeTarget.style.opacity = '1';
-  //         }
-  //         if (parseFloat(fadeTarget.style.opacity) > 0) {
-  //             fadeTarget.style.opacity = (parseFloat(fadeTarget.style.opacity) - 0.1).toString();
-  //         } else {
-  //             clearInterval(fadeEffect);
-  //         }
-  //     }, 2000);
-  //     }, 2000);
+//   document.getElementById('control-hint-box') ?  document.getElementById('control-hint-box').style.opacity = '1' : null;
   
-  return <div id="control-hint-box" className='controlsHintBox'>
-            <h3>Controls</h3>
-            {keysHintsList[layout].map(line=> 
-              (<section key={line.title}>
-                {line.keys.map(key=><span key={key} className='keyButton'>{key}</span>)}
-                to {line.title}</section>))}    
-          </div>;
-};
+//   return <div id="control-hint-box" className='controlsHintBox'>
+//             <h3>Controls</h3>
+//             {keysHintsList[layout].map(line=> 
+//               (<section key={line.title}>
+//                 {line.keys.map(key=><span key={key} className='keyButton'>{key}</span>)}
+//                 to {line.title}</section>))}    
+//           </div>;
+// };
+import React from "react";
+
+import './style.scss';
+import { isMobileOrTablet } from "@xr3ngine/engine/src/common/functions/isMobile";
+import {generalStateList} from '../gl/scene';
+import { Button, Snackbar } from '@material-ui/core';
+import {ArrowheadLeftOutline} from '@styled-icons/evaicons-outline/ArrowheadLeftOutline';
+import {ArrowheadRightOutline} from '@styled-icons/evaicons-outline/ArrowheadRightOutline';
+import {DotCircle} from '@styled-icons/fa-regular/DotCircle';
+
+const HintBox = (props) =>{
+  const renderHintIcons = () =>{
+    switch(props.step){
+      case generalStateList.TUTOR_LOOKAROUND: 
+          return <section className="lookaround"><ArrowheadLeftOutline /><ArrowheadRightOutline /></section>; 
+      case generalStateList.TUTOR_MOVE:
+          return isMobileOrTablet() ? <section className="movearound"><DotCircle /></section> : '';
+      default : return '';
+    }
+  }
+  let message = '';
+  switch(props.step){
+    case generalStateList.TUTOR_LOOKAROUND: message=' Drag anywhere to look around';break;      
+    case generalStateList.TUTOR_MOVE: message= isMobileOrTablet() ? ' Use joystick to move' : 'Use mouse to move'; break;
+    case generalStateList.TUTOR_UNMUTE: message='Tap to unmute'; break;
+    case generalStateList.TUTOR_VIDEO: message='Tap to enable stream'; break;
+    default : message= '';break;
+  }     
+      
+  return message ? 
+                <>
+                  {renderHintIcons()}
+                  <Snackbar anchorOrigin={{vertical: 'bottom',horizontal: 'center'}} 
+                  className={`helpHintSnackBar ${props.step === generalStateList.TUTOR_MOVE ? 'right-content-width' : ''}`} open={true} 
+                  autoHideDuration={10000} message={message} 
+                  action={<Button onClick={props.action} color="primary">(Skip)</Button>} />
+                </>
+              :null
+}
+
+export default HintBox;
