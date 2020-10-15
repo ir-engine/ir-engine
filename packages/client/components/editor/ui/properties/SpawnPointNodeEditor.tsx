e0d844d84d52abd89bf77f73a60c73b7b706ec47
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-10-15 22:06:15 +0500

    Comment nodes that are not needed (kits) and remove Asset types panel from editor (since no kits are required for now). Fix Editor icons.
    Former-commit-id: b80e954766a138ef6905195eeb96e94a0b94a712

--- packages/client/components/editor/ui/properties/SpawnPointNodeEditor.tsx
+++ packages/client/components/editor/ui/properties/SpawnPointNodeEditor.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NodeEditor from "./NodeEditor";
+import { StreetView } from "@styled-icons/fa-solid/StreetView";
 type SpawnPointNodeEditorProps = {
   editor?: object;
   node?: object;
@@ -8,6 +9,7 @@ export default class SpawnPointNodeEditor extends Component<
   SpawnPointNodeEditorProps,
   {}
 > {
+  static iconComponent = StreetView;
   render() {
     return (
       <NodeEditor
