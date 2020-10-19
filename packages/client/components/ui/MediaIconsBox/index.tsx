0ee5689d5b81281f26d4b6452d783ef239e05d85
Tanya Vykliuk <tanya.vykloik@gmail.com>
2020-10-19 17:54:45 +0300

    change styles.module.scss to ComponentName.module.scss and in Component file replace classname=class-name to className={styles.classname}
    Former-commit-id: 0bd7e3983e758f65c8dd7da4278d60cd8a320117

--- packages/client/components/ui/MediaIconsBox/index.tsx
+++ packages/client/components/ui/MediaIconsBox/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import './style.module.scss';
 import { generalStateList } from '../../../redux/app/actions';
 import { Microphone } from '@styled-icons/boxicons-regular/Microphone';
 import { CameraVideo } from '@styled-icons/bootstrap/CameraVideo';
 import { Face } from '@styled-icons/boxicons-regular/Face';
 import { connect } from "react-redux";
 import { selectAppOnBoardingStep } from "../../../redux/app/selector";
+
+import styles from './MediaIconsBox.module.scss';
 
 const mapStateToProps = (state: any): any => {
   return {   
@@ -16,8 +17,8 @@ const mapStateToProps = (state: any): any => {
 
 const MediaIconsBox = (props) =>{
   return props.onBoardingStep === generalStateList.ALL_DONE ? 
-        <section className="drawer-box-container">
-          <section className="drawer-box">
+        <section className={styles.drawerBoxContainer}>
+          <section className={styles.drawerBox}>
             <Microphone size="3em" />
             <CameraVideo size="3em"  />
             <Face size="3em" />
