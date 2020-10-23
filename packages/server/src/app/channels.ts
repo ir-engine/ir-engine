a190d6fee2a4f7f04cfce4148a88501b7049bfad
Kyle Baran <kbaran@bitscoop.com>
2020-10-23 16:52:26 -0700

    Video chat among different parties is working.
    Fixed several bugs related to swtiching parties.
    
    
    Former-commit-id: da178fad9d1b66d8f3d147bf86401cebbe092b78

--- packages/server/src/app/channels.ts
+++ packages/server/src/app/channels.ts
@@ -67,12 +67,10 @@ export default (app: Application): void => {
               console.log('Joining allocated instance');
               console.log(user.instanceId);
               console.log((app as any).instance.id);
-              if (user.instanceId !== (app as any).instance.id) {
                 const instance = await app.service('instance').get((app as any).instance.id);
                 await app.service('instance').patch((app as any).instance.id, {
                   currentUsers: (instance.currentUsers as number) + 1
                 });
-              }
             }
             console.log('Patching user instanceId to ' + (app as any).instance.id);
             await app.service('user').patch(userId, {
