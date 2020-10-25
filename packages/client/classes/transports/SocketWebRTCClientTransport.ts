8c38a9f43d9268bd53d5d10b40a01c13a0a322ec
shawticus <shawmakesmagic@gmail.com>
2020-10-25 04:33:35 -0700

    Fixed early return

--- packages/client/classes/transports/SocketWebRTCClientTransport.ts
+++ packages/client/classes/transports/SocketWebRTCClientTransport.ts
@@ -176,11 +176,6 @@ export class SocketWebRTCClientTransport implements NetworkTransport {
     console.log("Loading mediasoup");
     if (this.mediasoupDevice.loaded !== true) await this.mediasoupDevice.load({ routerRtpCapabilities });
 
-    console.log("Joined world");
-    return Promise.resolve();
-
-      this.socket.emit(MessageTypes.Heartbeat.toString(), 1000);
-
       // Send heartbeat every second
       setInterval(() => {
         this.socket.emit(MessageTypes.Heartbeat.toString());
