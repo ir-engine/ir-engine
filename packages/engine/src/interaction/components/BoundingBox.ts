f9c45ca60f5e4d57ed9856d7c040e571e4980b07
Kyle Baran <kbaran@bitscoop.com>
2020-10-27 14:28:51 -0700

    Fixed missing properties in BoundingBox.ts Patched weirdness of types in interactBoxRaycast.ts via 'any'; this should be amended to make types match. Renamed a few references to 'Interactive' to 'Interactable' that hadn't been changed.
    Updated all socket.on()'s in SocketWebRTCServerTransport to re-fetch
    userId from socketId. Second connecting socket was overriding globally-
    accessible userId, leading to all sorts of bugs when incorrect
    transports or producers were being called.
    
    Commented out everything that uses youtube-dl since it was taken down.
    Removed 'video' route from use for the time being.

--- packages/engine/src/interaction/components/BoundingBox.ts
+++ packages/engine/src/interaction/components/BoundingBox.ts
@@ -3,6 +3,10 @@ import { Component } from "../../ecs/classes/Component";
 import { Types } from "../../ecs/types/Types";
 
 export class BoundingBox extends Component<BoundingBox> {
+  public box: Box3;
+  public boxArray: any[];
+  public dynamic: boolean;
+
   static schema = {
     box: { type:  Types.Ref, default: new Box3() },
     boxArray: { type: Types.Array, default: [] },
