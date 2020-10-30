7b7c0dc8a1802a5cfa05bf316370345c03e1f8c0
Tanya Vykliuk <tanya.vykloik@gmail.com>
2020-10-30 19:31:02 +0200

    handle lookaround/move/interact input scmene for separate onboarding intup schema

--- packages/client/components/ui/OnBoardingBox/index.tsx
+++ packages/client/components/ui/OnBoardingBox/index.tsx
@@ -7,7 +7,14 @@ import { selectAppOnBoardingStep } from "../../../redux/app/selector";
 import { generalStateList, setAppOnBoardingStep } from '../../../redux/app/actions';
 import store from "../../../redux/store";
 
+import { Thumbsticks } from '@xr3ngine/engine/src/common/enums/Thumbsticks';
+import { GamepadButtons } from '@xr3ngine/engine/src/input/enums/GamepadButtons';
+import { CharacterInputSchema } from "@xr3ngine/engine/src/templates/character/CharacterInputSchema";
+
 import styles from './OnBoardingBox.module.scss';
+import { DefaultInput } from "@xr3ngine/engine/src/templates/shared/DefaultInput";
+import { getMutableComponent } from "@xr3ngine/engine/src/ecs/functions/EntityFunctions";
+import { Input } from "@xr3ngine/engine/src/input/components/Input";
 
 const mapStateToProps = (state: any): any => {
   return {   
@@ -15,16 +22,80 @@ const mapStateToProps = (state: any): any => {
   };
 };
 
+interface Props {
+  onBoardingStep?: number;
+  actorEntity : Entity;
+}
 
-const OnBoardingBox = (props) =>{
-  const { onBoardingStep }  = props;
+const OnBoardingBox = (props : Props) =>{
+  const { onBoardingStep,actorEntity }  = props;
+
+  const addLookAroundEventListeners = () =>{
+    const InputComponent = getMutableComponent(actorEntity, Input);
+    InputComponent.schema.inputAxisBehaviors[DefaultInput.LOOKTURN_PLAYERONE] = {started : [{behavior:actorLooked}]};
+  }
+
+
+  const actorLooked = () =>{
+    const InputComponent = getMutableComponent(actorEntity, Input);
+    delete InputComponent.schema.inputAxisBehaviors[DefaultInput.LOOKTURN_PLAYERONE];
+    store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_MOVE));
+    //add
+    const keyboardInputMap = {
+      w: DefaultInput.FORWARD,
+      a: DefaultInput.LEFT,
+      s: DefaultInput.BACKWARD,
+      d: DefaultInput.RIGHT}
+    InputComponent.schema.keyboardInputMap = Object.assign(InputComponent.schema.keyboardInputMap, keyboardInputMap) ;
+    //touch/joystick
+    const gamepadInputMap = {
+      [GamepadButtons.DPad1]: DefaultInput.FORWARD, // DPAD 1
+      [GamepadButtons.DPad2]: DefaultInput.BACKWARD, // DPAD 2
+      [GamepadButtons.DPad3]: DefaultInput.LEFT, // DPAD 3
+      [GamepadButtons.DPad4]: DefaultInput.RIGHT // DPAD 4
+    }
+    InputComponent.schema.gamepadInputMap.axes[Thumbsticks.Left] =  DefaultInput.MOVEMENT_PLAYERONE;           
+    InputComponent.schema.gamepadInputMap.buttons = Object.assign(InputComponent.schema.gamepadInputMap.buttons, gamepadInputMap) ;
+
+    //keyboard
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.FORWARD].started.push({behavior:actorMoved});
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.BACKWARD].started.push({behavior:actorMoved});
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.LEFT].started.push({behavior:actorMoved});
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.RIGHT].started.push({behavior:actorMoved});
+    //joystick
+    InputComponent.schema.inputAxisBehaviors[DefaultInput.MOVEMENT_PLAYERONE].started.push({behavior:actorMoved});
+  }
+
+  const actorInteracted = () =>{
+    const InputComponent = getMutableComponent(actorEntity, Input);
+    store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_UNMUTE));
+    InputComponent.schema = CharacterInputSchema;
+  }
+
+  const actorMoved = () =>{
+    const InputComponent = getMutableComponent(actorEntity, Input);
+    //keyboard
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.FORWARD].started.pop();
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.BACKWARD].started.pop();
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.LEFT].started.pop();
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.RIGHT].started.pop();
+    //joystick
+    InputComponent.schema.inputAxisBehaviors[DefaultInput.MOVEMENT_PLAYERONE].started.pop();
+    
+    InputComponent.schema.keyboardInputMap.e = DefaultInput.INTERACT;
+    InputComponent.schema.gamepadInputMap.buttons[GamepadButtons.A] = DefaultInput.INTERACT;
+
+    InputComponent.schema.inputButtonBehaviors[DefaultInput.INTERACT] = {started : [{behavior:actorInteracted}]};
+    store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_INTERACT));
+  }
+
 
   let message = '';
   let action = null;
   switch(onBoardingStep){
-    case generalStateList.TUTOR_LOOKAROUND:message='Touch and Drag to look around'; action = ()=>store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_MOVE)); break;      
-    case generalStateList.TUTOR_MOVE: message= isMobileOrTablet() ? ' Use joystick to move' : 'Use mouse to move'; action = ()=>store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_INTERACT)); break;
-    case generalStateList.TUTOR_INTERACT: message= isMobileOrTablet() ? 'Use to interact' : 'Press E to interact'; action = ()=>store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_UNMUTE)); break;
+    case generalStateList.TUTOR_LOOKAROUND:message='Touch and Drag to look around'; addLookAroundEventListeners(); break;      
+    case generalStateList.TUTOR_MOVE: message= isMobileOrTablet() ? ' Use joystick to move' : 'Use mouse to move'; break;
+    case generalStateList.TUTOR_INTERACT: message= isMobileOrTablet() ? 'Use to interact' : 'Press E to interact'; break;
     case generalStateList.TUTOR_UNMUTE: message='Tap to toggle Mic'; action = ()=>store.dispatch(setAppOnBoardingStep(generalStateList.TUTOR_END)); break;
     // case generalStateList.TUTOR_VIDEO: message='Tap to enable stream'; action = ()=>store.dispatch(setAppOnBoardingStep(generalStateList.ALL_DONE));  break;
     default : message= '';break;
