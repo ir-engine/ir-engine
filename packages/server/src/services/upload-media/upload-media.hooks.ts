2f1b6e39607e4a09cff9f80af223c86e571bc49b
Saad Ahmed Siddiqui <saad_siddiqui@msn.com>
2020-11-05 01:28:52 +0500

    Send projectId from client upload. Fix Thumbnail update i.e. static-resource is updated instead of new creation everytime a project is saved.
    Former-commit-id: dbbc3680d72978627f006bc3a8cd49028115e85d

--- packages/server/src/services/upload-media/upload-media.hooks.ts
+++ packages/server/src/services/upload-media/upload-media.hooks.ts
@@ -35,35 +35,47 @@ const createOwnedFile = (options = {}) => {
     const body = params.body || {};
 
     const domain: string = config.aws.cloudfront.domain;
-    const resourceData = {
-      id: body.fileId,
-      name: data.name || body.name,
-      url: data.uri || data.url,
-      key: (data.uri || data.url)
-        .replace(`https://${domain}/`, ''),
-
-      content_type: data.mimeType || params.mimeType,
-      userId: body.userId,
-      metadata: data.metadata || body.metadata
-    };
-
-    /* if (context.params.skipResourceCreation === true) {
-      context.result = await context.app.service('owned-file').patch(context.params.patchId, {
-        url: resourceData.url,
-        metadata: resourceData.metadata
+    let savedFile;
+    if (body.projectId) { // Check if projectId is sent by the client and update thumbnail URL instead of creating new resource
+      const { thumbnailOwnedFileId } = await context.app.service('collection').Model.findOne({
+        where: {
+          sid: body.projectId
+        }
       })
-    } else { */
-    if (context.params.parentResourceId) {
-      (resourceData as any).parentResourceId = context.params.parentResourceId;
-    }
-    (resourceData as any).type = getBasicMimetype(resourceData.content_type);
+      savedFile = await context.app.service('static-resource').patch(thumbnailOwnedFileId, {
+        url: data.uri || data.url
+      });
+    } else {
+        const resourceData = {
+        id: body.fileId,
+        name: data.name || body.name,
+        url: data.uri || data.url,
+        key: (data.uri || data.url)
+          .replace(`https://${domain}/`, ''),
 
-    // Remap input from Editor to fit
-    const modifiedResourceData = {
-      ...resourceData,
-      mimeType: resourceData.content_type
-    };
-    const savedFile = await context.app.service('static-resource').create(modifiedResourceData);
+        content_type: data.mimeType || params.mimeType,
+        userId: body.userId,
+        metadata: data.metadata || body.metadata
+      };
+
+      /* if (context.params.skipResourceCreation === true) {
+        context.result = await context.app.service('owned-file').patch(context.params.patchId, {
+          url: resourceData.url,
+          metadata: resourceData.metadata
+        })
+      } else { */
+      if (context.params.parentResourceId) {
+        (resourceData as any).parentResourceId = context.params.parentResourceId;
+      }
+      (resourceData as any).type = getBasicMimetype(resourceData.content_type);
+
+      // Remap input from Editor to fit
+      const modifiedResourceData = {
+        ...resourceData,
+        mimeType: resourceData.content_type
+      };
+      savedFile = await context.app.service('static-resource').create(modifiedResourceData);
+    }
     context.result = {
       // This is to fulfill the editor response, as editor is expecting the below object
       file_id: savedFile.id,
