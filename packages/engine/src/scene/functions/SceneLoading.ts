40073c96209cf8fa474312ce4d7980bbe3352fbc
shawticus <shawmakesmagic@gmail.com>
2020-11-06 04:23:42 -0800

    location loads from editor using project ID

--- packages/engine/src/scene/functions/SceneLoading.ts
+++ packages/engine/src/scene/functions/SceneLoading.ts
@@ -8,29 +8,32 @@ export function loadScene (scene) {
     const entity = createEntity();
     addComponent(entity, SceneTagComponent);
     sceneEntity.components.forEach(component => {
-      console.log("Component loading is")
-      console.log(component);
-      console.log(component.name);
       loadComponent(entity, component);
     });
   });
 }
 
 export function loadComponent (entity, component) {
-  console.log("Loading componenent");
-  console.log(component);
-  console.log("on entity")
-  console.log(entity);
-
   if (SceneObjectLoadingSchema[component.name] === undefined) return console.warn("Couldn't load ", component.name);
   const componentSchema = SceneObjectLoadingSchema[component.name];
   // for each component in component name, call behavior
   componentSchema.behaviors?.forEach(b => {
     // For each value, from component.data
-    const values = b.values ? b.values.map(val => component.data[val]) : {};
+    const values = {}    
+    b.values?.forEach(val => {
+      // Does it have a from and to field? Let's map to that
+      if(val['from'] !== undefined) {
+        values[val['to']] = component.data[val['from']];
+      }
+      else {
+      // Otherwise raw data
+      values[val] = component.data[val];
+      }
+    });
     // Invoke behavior with args and spread args
     b.behavior(entity, { ...b.args, objArgs: { ...values } });
   });
+
   // for each component in component name, add copmponent
   componentSchema.components?.forEach(c => {
     // For each value, from component.data, add to args object
