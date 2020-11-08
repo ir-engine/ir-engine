a21961515d652ab0a51ab987060e7945eaca72bc
shawticus <shawmakesmagic@gmail.com>
2020-11-08 00:36:41 -0800

    Add stubbed out createfunctions
    Former-commit-id: c343ddb6ba929d028d800d4c520b88a591aca9f8

---
+++ packages/engine/src/scene/behaviors/createBoxCollider.ts
@@ -0,0 +1,5 @@
+import { Behavior } from '../../common/interfaces/Behavior';
+
+export const createBoxCollider: Behavior = (entity, args: {}) => {
+    console.warn("TODO: handle background, args are", args);
+};
---
+++ packages/engine/src/scene/behaviors/createTriggerVolume.ts
@@ -0,0 +1,5 @@
+import { Behavior } from '../../common/interfaces/Behavior';
+
+export const createTriggerVolume: Behavior = (entity, args: {}) => {
+    console.warn("TODO: handle background, args are", args);
+};
