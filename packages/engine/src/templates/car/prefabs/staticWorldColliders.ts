bb80de81fa9287f2efa5863afcf9e81c008cbca2
shawticus <shawmakesmagic@gmail.com>
2020-11-10 06:45:42 -0800

    Added static world collider to server for now, so character can run around
    Former-commit-id: 78a5b8b1688f35da990db164b6ad012c9ca32963

--- packages/engine/src/templates/car/prefabs/staticWorldColliders.ts
+++ packages/engine/src/templates/car/prefabs/staticWorldColliders.ts
@@ -2,6 +2,7 @@ import { Prefab } from "@xr3ngine/engine/src/common/interfaces/Prefab";
 import { ColliderComponent } from "@xr3ngine/engine/src/physics/components/ColliderComponent";
 import { TransformComponent } from "@xr3ngine/engine/src/transform/components/TransformComponent";
 import { BoxBufferGeometry, Mesh, MeshPhongMaterial } from "three";
+import { addObject3DComponent } from "../../../common/behaviors/Object3DBehaviors";
 
 const scale = [100, 0.1, 100];
 const floor = new BoxBufferGeometry(scale[0], scale[1], scale[2]);
@@ -9,14 +10,13 @@ const mat = new MeshPhongMaterial({ color: "#ffffff" });
 const floorMesh = new Mesh( floor, mat );
 floorMesh.receiveShadow = true;
 floorMesh.castShadow = false;
-floorMesh.visible = false;
+floorMesh.visible = true;
 
 export const staticWorldColliders: Prefab = {
     components: [
-      { type: TransformComponent, data: { position: [ 0, -0.05, 0 ]  } },
+      { type: TransformComponent, data: { position: [ 0, -1, 0 ]  } },
       { type: ColliderComponent, data: { type: 'box', scale: scale, mass: 0 }}
     ],
-    /*
     onAfterCreate: [
         // add a 3d object
          {
@@ -26,5 +26,4 @@ export const staticWorldColliders: Prefab = {
              }
          },
     ]
-    */
 };
