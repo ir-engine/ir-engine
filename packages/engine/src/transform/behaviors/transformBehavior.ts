b97517fe7a7ae12e67c80fadd73cb6e6029d7131
shawticus <shawmakesmagic@gmail.com>
2020-11-10 05:28:43 -0800

    Characters are still falling through floor with static world colliders, might be issue with the prefab
    Former-commit-id: efbc325c408aef6916bf9469184075cf981b2325

--- packages/engine/src/transform/behaviors/transformBehavior.ts
+++ packages/engine/src/transform/behaviors/transformBehavior.ts
@@ -10,16 +10,17 @@ export const transformBehavior: Behavior = (entity: Entity, args: { event: Mouse
   transform.position.addScaledVector(transform.velocity, delta);
 
   const object3DComponent = getMutableComponent<Object3DComponent>(entity, Object3DComponent);
-  if (!object3DComponent)
-    return;
+  if (!object3DComponent){
+    removeComponent(entity, TransformComponent);
+    return console.warn("Object is not an object3d", entity.id);
+  }
 
   if (!object3DComponent.value)
     return console.warn("object3D component on entity", entity.id, " is undefined");
 
-    if(!object3DComponent.value.position){
-      removeComponent(entity, TransformComponent);
-      return console.warn("object3D has no position, removing transform");
-    }
+    // if(!object3DComponent.value.position){
+    //   return console.warn("object3D has no position");
+    // }
 
   object3DComponent.value.position.copy(transform.position);
   object3DComponent.value.rotation.setFromQuaternion(transform.rotation);
