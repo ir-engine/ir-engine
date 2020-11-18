4f2203af5d067050796dded538b2f362f736b25a
shawticus <shawmakesmagic@gmail.com>
2020-11-17 21:34:12 -0800

    Try default CORS config

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -66,10 +66,7 @@ if (config.server.enabled) {
 
   // Enable security, CORS, compression, favicon and body parsing
   app.use(helmet());
-  app.use(cors({
-    origin: '*',
-    credentials: true
-  }));
+  app.use(cors());
   app.use(compress());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
