8f3bc2e2725ded77b8f2e59cc0d130625f250064
Tanya Vykliuk <tanya.vykloik@gmail.com>
2020-11-18 16:57:58 +0200

    add Device settings
    Former-commit-id: a2e99d2a9577b1625d0870e04361c316e6433033

--- packages/client/components/ui/Profile/UserSettings.tsx
+++ packages/client/components/ui/Profile/UserSettings.tsx
@@ -1,15 +1,8 @@
 import React, { ChangeEvent, useState } from 'react';
-import Grid from '@material-ui/core/Grid';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Typography from '@material-ui/core/Typography';
-import VolumeDown from '@material-ui/icons/VolumeDown';
-import VolumeUp from '@material-ui/icons/VolumeUp';
-import MicIcon from '@material-ui/icons/Mic';
-import MicOffIcon from '@material-ui/icons/MicOff';
 import Slider from '@material-ui/core/Slider';
 import Switch from '@material-ui/core/Switch';
 import styles from './Profile.module.scss';
@@ -38,65 +31,47 @@ const UserSettings = (): any => {
 
   return (
     <div className={styles.root}>
-      <Typography id="continuous-slider" gutterBottom style={{ padding: 10 }}>
-        Microphone volume
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item>
-          <MicOffIcon />
-        </Grid>
-        <Grid item xs>
-          <Slider
+      <section className={styles.settingRow}>
+        <Typography variant="h2" color="primary" className={styles.settingLabel}>Volume</Typography>
+        <span className={styles.settingValue}><Slider
             value={volume}
             onChange={handleVolume}
             aria-labelledby="continuous-slider"
-          />
-        </Grid>
-        <Grid item>
-          <MicIcon />
-        </Grid>
-      </Grid>
-      <Typography id="continuous-slider" gutterBottom style={{ padding: 10 }}>
-        Audio playback volume
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item>
-          <VolumeDown />
-        </Grid>
-        <Grid item xs>
-          <Slider
-            value={audio}
-            onChange={handleAudio}
-            aria-labelledby="continuous-slider"
-          />
-        </Grid>
-        <Grid item>
-          <VolumeUp />
-        </Grid>
-      </Grid>
-      <FormControl component="fieldset" style={{ margin: 10 }}>
-        <FormLabel component="legend">Video Quality</FormLabel>
-        <RadioGroup
-          aria-label="gender"
-          name="gender1"
-          value={radiovalue}
-          onChange={handleRadioValue}
-        >
-          <FormControlLabel value="high" control={<Radio />} label="High" />
-          <FormControlLabel value="low" control={<Radio />} label="Low" />
-        </RadioGroup>
-      </FormControl>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={state.checkedB}
-            onChange={handleChange}
-            name="checkedB"
-            color="primary"
-          />
-        }
-        label="Enter room muted"
-      />
+          /></span>
+      </section>
+      <section className={styles.settingRow}>
+      <Typography variant="h2" color="primary" className={styles.settingLabel}>Microphone</Typography>
+        <span className={styles.settingValue}><Slider
+              value={audio}
+              onChange={handleAudio}
+              aria-labelledby="continuous-slider"
+            /></span>
+      </section>
+      <section className={styles.settingRow}>
+      <Typography variant="h2" color="primary" className={styles.settingLabel}>Resolution</Typography>
+        <span className={styles.settingValue}>
+          <RadioGroup
+              aria-label="videoQuality"
+              name="videoQuality"
+              value={radiovalue}
+              onChange={handleRadioValue}
+            >
+              <FormControlLabel className={styles.controlsSvg} value="high" control={<Radio color="primary" />} label="High" />
+              <FormControlLabel className={styles.controlsSvg} value="low" control={<Radio color="primary" />} label="Low" />
+            </RadioGroup>
+        </span>
+      </section>
+      <section className={styles.settingRow}>
+      <Typography variant="h2" color="primary" className={styles.settingLabel}>Enter room muted</Typography>
+      <span className={styles.settingValue}>
+        <Switch
+              checked={state.checkedB}
+              onChange={handleChange}
+              name="checkedB"
+              color="primary"
+            />
+        </span>
+      </section>      
     </div>
   );
 };
