0c38be277f6486426520e5799b254c6f88d2d6e9
Andrii Blashchuk <ablashch@gmail.com>
2020-11-18 20:47:02 +0200

    Cubemap & equirectangular options to skybox added v.1
    Former-commit-id: 07a31f696ce334964d0f32c4eba8de4b43a5d483

--- packages/engine/src/scene/behaviors/createSkybox.ts
+++ packages/engine/src/scene/behaviors/createSkybox.ts
@@ -1,7 +1,10 @@
 import { Sky } from '@xr3ngine/engine/src/scene/classes/Sky';
+import { CubeTexture, TextureLoader } from 'three';
+import { CubeRefractionMapping } from 'three';
 import { Vector3 } from 'three';
 import { addObject3DComponent } from '../../common/behaviors/Object3DBehaviors';
-import { addComponent, getMutableComponent } from '../../ecs/functions/EntityFunctions';
+import { Engine } from '../../ecs/classes/Engine';
+import { addComponent, getComponent, getMutableComponent } from '../../ecs/functions/EntityFunctions';
 import { ScaleComponent } from '../../transform/components/ScaleComponent';
 import { SkyboxComponent } from '../components/SkyboxComponent';
 
@@ -9,21 +12,60 @@ export default function createSkybox(entity, args: {
   obj3d;
   objArgs: any
 }): void {
-  addObject3DComponent(entity, { obj3d: args.obj3d, objArgs: args.objArgs });
-  addComponent(entity, ScaleComponent);
-  const scaleComponent = getMutableComponent<ScaleComponent>(entity, ScaleComponent);
-  scaleComponent.scale = [args.objArgs.distance, args.objArgs.distance, args.objArgs.distance];
-  const uniforms = Sky.material.uniforms;
-  const sun = new Vector3();
-  const theta = Math.PI * (args.objArgs.inclination - 0.5);
-  const phi = 2 * Math.PI * (args.objArgs.azimuth - 0.5);
+  console.log(args.objArgs);
 
-  sun.x = Math.cos(phi);
-  sun.y = Math.sin(phi) * Math.sin(theta);
-  sun.z = Math.sin(phi) * Math.cos(theta);
-  uniforms.mieCoefficient.value = args.objArgs.mieCoefficient;
-  uniforms.mieDirectionalG.value = args.objArgs.mieDirectionalG;
-  uniforms.rayleigh.value = args.objArgs.rayleigh;
-  uniforms.turbidity.value = args.objArgs.turbidity;
-  uniforms.sunPosition.value = sun;
+  if (args.objArgs.skytype === "skybox") {
+
+    addObject3DComponent(entity, { obj3d: Sky, objArgs: args.objArgs });
+    addComponent(entity, ScaleComponent);
+    const scaleComponent = getMutableComponent<ScaleComponent>(entity, ScaleComponent);
+    scaleComponent.scale = [args.objArgs.distance, args.objArgs.distance, args.objArgs.distance];
+    const uniforms = Sky.material.uniforms;
+    const sun = new Vector3();
+    const theta = Math.PI * (args.objArgs.inclination - 0.5);
+    const phi = 2 * Math.PI * (args.objArgs.azimuth - 0.5);
+
+    sun.x = Math.cos(phi);
+    sun.y = Math.sin(phi) * Math.sin(theta);
+    sun.z = Math.sin(phi) * Math.cos(theta);
+    uniforms.mieCoefficient.value = args.objArgs.mieCoefficient;
+    uniforms.mieDirectionalG.value = args.objArgs.mieDirectionalG;
+    uniforms.rayleigh.value = args.objArgs.rayleigh;
+    uniforms.turbidity.value = args.objArgs.turbidity;
+    uniforms.sunPosition.value = sun;
+
+  } else if (args.objArgs.skyOptionValue === "cubemap") {
+    // const path = 'server/public/cubemaps';
+
+    const imageObj = new Image();
+
+    imageObj.onload = function () {
+      
+      let canvas, context;
+      const tileWidth = imageObj.height;
+      const canvases = [];
+
+
+      for (let i = 0; i < 6; i++) {
+
+        canvas = document.createElement('canvas');
+        context = canvas.getContext('2d');
+        canvas.height = tileWidth;
+        canvas.width = tileWidth;
+        context.drawImage(imageObj, tileWidth * i, 0, tileWidth, tileWidth, 0, 0, tileWidth, tileWidth);
+        canvases.push(canvas);
+
+      }
+
+      const textureCube = new CubeTexture(canvases);
+      // textureCube.mapping = CubeRefractionMapping;
+
+      Engine.scene.background = textureCube;
+    };
+    console.log(args.objArgs);
+    imageObj.src = args.objArgs.texture;
+  }
+  else if (args.objArgs.skyOptionValue === "equirectangular") {
+    Engine.scene.background = new TextureLoader().load(args.objArgs.texture);
+  }
 }
