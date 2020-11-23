7d8cc1b8888220c530aaa5c7e83002ec6d8010bf
Vyacheslav Solovjov <xianionline@gmail.com>
2020-11-23 17:26:17 +0200

    test createObjects in ClientNetworkSystem (Player prefab, with transforms)

---
+++ packages/engine/src/networking/interfaces/WorldState.ts
@@ -0,0 +1,66 @@
+import { LifecycleValue } from "../../common/enums/LifecycleValue";
+import { BinaryValue } from "../../common/enums/BinaryValue";
+import { PrefabAlias } from "../../common/types/PrefabAlias";
+
+export interface NetworkInputInterface {
+  networkId: string
+  buttons: {
+    input: string,
+    value: BinaryValue,
+    lifecycleState: LifecycleValue
+  }
+  axes1d: {
+    input: string,
+    value: number,
+    lifecycleState: LifecycleValue
+  }
+  axes2d: {
+    input: string,
+    valueX: number,
+    valueY: number,
+    lifecycleState: LifecycleValue
+  }
+}
+
+export interface NetworkClientDataInterface {
+  userId: string
+}
+
+export interface NetworkTransformsInterface {
+  networkId: string
+  x: number
+  y: number
+  z: number
+  qX: number
+  qY: number
+  qZ: number
+  qW: number
+}
+
+export interface NetworkObjectRemoveInterface {
+  networkId: number
+}
+
+export interface NetworkObjectCreateInterface {
+  networkId: string,
+  ownerId: string,
+  prefabType: PrefabAlias,
+  x: number,
+  y: number,
+  z: number,
+  qX: number,
+  qY: number,
+  qZ: number,
+  qW: number
+}
+
+export interface WorldStateInterface {
+  tick: number
+  transforms: NetworkTransformsInterface[]
+  inputs: NetworkInputInterface[]
+  states: any[],
+  clientsConnected: NetworkClientDataInterface[]
+  clientsDisconnected: NetworkClientDataInterface[]
+  createObjects: NetworkObjectCreateInterface[]
+  destroyObjects: NetworkObjectRemoveInterface[]
+}
