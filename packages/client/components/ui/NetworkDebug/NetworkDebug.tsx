15834761417adbb34c13e13339dd65ce2a64f8fc
shawticus <shawmakesmagic@gmail.com>
2020-11-22 16:00:48 -0800

    Open network debug with ` now

--- packages/client/components/ui/NetworkDebug/NetworkDebug.tsx
+++ packages/client/components/ui/NetworkDebug/NetworkDebug.tsx
@@ -1,66 +1,59 @@
 import { Engine } from "@xr3ngine/engine/src/ecs/classes/Engine";
 import { Network } from "@xr3ngine/engine/src/networking/components/Network";
-import React, { useLayoutEffect, useReducer, useState } from "react";
+import React, { useEffect, useLayoutEffect, useReducer, useRef, useState } from "react";
 import JSONTree from 'react-json-tree';
+import { useKeyPress } from "../../../functions/useKeyPress";
 
 export const NetworkDebug = () => {
-    const [networkData, setNetworkData] = useState({});
-    const [isShowing, setShowing] = useState(false);
+  const [isShowing, setShowing] = useState(false);
 
-    const [, forceUpdate] = useReducer(x => x + 1, 0);
-    const [remountCount, setRemountCount] = useState(0);
-    const refresh = () => setRemountCount(remountCount + 1);
- 
-  useLayoutEffect(() => {
-    let numberOfClients = Network.instance != null ? Object.keys(Network.instance.clients).length : 0;
-    const numberOfNetworkObjects = Network.instance != null ? Object.keys(Network.instance.networkObjects).length : 0;
-    const interval = setInterval(() => {
-        if( Network.instance != null &&
-            (Object.keys(Network.instance.clients).length !== numberOfClients ||
-            Object.keys(Network.instance.networkObjects).length !== numberOfNetworkObjects)
-        ) {
-            setNetworkData({...Network.instance});
-            forceUpdate();
-            refresh();
-        }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  const showingStateRef = useRef(isShowing);
 
-  const toggleShowing = () => {
-    setShowing(!isShowing);
-    forceUpdate();
-    refresh();
-  };
+  function setupListener() {
+    window.addEventListener('keydown', downHandler);
 
-      if(Network.instance !== null)
-      return (
-        <div style={{ position: "absolute", overflowY: "auto", top: 0, zIndex: 100000, height: "auto", maxHeight: "100vh", width: "auto", maxWidth: "50%" }}>
-            <button type="submit" value="toggleShowing" onClick={ toggleShowing }>{ isShowing ? "Hide" : "Show" }</button>
-            { isShowing &&
-            <div>
-            <div>
-              <h1>Network Object</h1>
-                <JSONTree data={{...Network.instance}} />
-              </div>
-              <div>
-              <h1>Network Clients</h1>
-                <JSONTree data={{...Network.instance.clients}} />
-              </div>
-              <div>
-              <h1>Network Objects</h1>
-                <JSONTree data={{...Network.instance.networkObjects}} />
-              </div>
-                <div>
-              <h1>Engine Entities</h1>
-                <JSONTree data={{...Engine.entities}} />
-              </div>
-              <button type="submit" value="Refresh" onClick={ refresh }>Refresh</button>
-              </div>
-            }
+  }
+
+  // If pressed key is our target key then set to true
+  function downHandler({ keyCode }) {
+    if (keyCode === 192) { // `
+      showingStateRef.current = !showingStateRef.current;
+      setShowing(showingStateRef.current);
+    }
+  }
+
+  // Add event listeners
+  useEffect(() => {
+    setupListener();
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  const [remountCount, setRemountCount] = useState(0);
+  const refresh = () => setRemountCount(remountCount + 1);
+
+  if (Network.instance !== null && isShowing) return (
+    <div style={{ position: "absolute", overflowY: "auto", top: 0, zIndex: 100000, height: "auto", maxHeight: "95%", width: "auto", maxWidth: "50%" }}>
+      <button type="submit" value="Refresh" onClick={refresh}>Refresh</button>
+      <div>
+        <div>
+          <h1>Network Object</h1>
+          <JSONTree data={{ ...Network.instance }} />
         </div>
-      );
-      return <div>Awaiting network connection...</div>; 
+        <div>
+          <h1>Network Clients</h1>
+          <JSONTree data={{ ...Network.instance.clients }} />
+        </div>
+        <div>
+          <h1>Network Objects</h1>
+          <JSONTree data={{ ...Network.instance.networkObjects }} />
+        </div>
+        <div>
+          <h1>Engine Entities</h1>
+          <JSONTree data={{ ...Engine.entities }} />
+        </div>
+      </div>
+    </div>
+  );
+  else return null;
 };
 
 export default NetworkDebug;
