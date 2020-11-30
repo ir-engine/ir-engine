ad6b2043dae1778956363a371c1170d0536f0b7c
shawticus <shawmakesmagic@gmail.com>
2020-11-29 18:10:39 -0800

    Remove volumetric from engine temporarily
    Former-commit-id: 14ae5f755c5015c77de5879ce1f774d656d7dff1

--- packages/engine/src/scene/components/VolumetricComponent.ts
+++ packages/engine/src/scene/components/VolumetricComponent.ts
@@ -1,6 +1,5 @@
 import { Component } from '../../ecs/classes/Component';
-import DracosisPlayer from '@xr3ngine/volumetric/src/Player'
 
 export default class VolumetricVideo extends Component<any> {
-    player: DracosisPlayer
+    player: any
 }
