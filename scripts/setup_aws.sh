6a61e940c9906354898131021f20b4b2bf1b8357
Kyle Baran <kbaran@bitscoop.com>
2020-11-30 15:58:30 -0800

    Debugging setup_aws script.

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -7,12 +7,18 @@ sudo ./aws/install
 
 set +x
 
+aws configure list
 echo setting access_key
 aws configure set aws_access_key_id $1
+aws configure list
 echo setting secret_key
 aws configure set aws_secret_access_key $2
+aws configure list
 echo setting region
 aws configure set default.region $3
 
+aws configure list
 echo updating kubeconfig
 aws eks update-kubeconfig --name xr3ngine-demo
+aws configure list
+echo finished setup_aws
