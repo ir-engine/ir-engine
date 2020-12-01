f14dd03a5ebb22f126f7779cc0661ac337ee844b
Kyle Baran <kbaran@bitscoop.com>
2020-11-30 16:03:33 -0800

    Debugging setup_aws script.

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -10,7 +10,6 @@ set +x
 aws configure list
 echo setting access_key
 aws configure set aws_access_key_id $1
-aws configure list
 echo setting secret_key
 aws configure set aws_secret_access_key $2
 aws configure list
