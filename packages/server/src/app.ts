1786d00c4717e33538cb92ad37b2bcaba8459a5a
Kyle Baran <kbaran@bitscoop.com>
2020-12-03 11:34:57 -0800

    Adding try/catch around server setup.
    Former-commit-id: 030b0edec3d8b5daf70397433b3c1760af9fd697

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -43,110 +43,129 @@ function healthPing(agonesSDK: AgonesSDK): void {
 app.set('nextReadyEmitter', emitter);
 
 if (config.server.enabled) {
-  app.configure(
-    swagger({
-      docsPath: '/openapi',
-      docsJsonPath: '/openapi.json',
-      uiIndex: path.join(__dirname, '../openapi.html'),
-      // TODO: Relate to server config, don't hardcode this here
-      specs: {
-        info: {
-          title: 'XR3ngine API Surface',
-          description: 'APIs for the XR3ngine application',
-          version: '1.0.0'
+  try {
+    app.configure(
+        swagger({
+          docsPath: '/openapi',
+          docsJsonPath: '/openapi.json',
+          uiIndex: path.join(__dirname, '../openapi.html'),
+          // TODO: Relate to server config, don't hardcode this here
+          specs: {
+            info: {
+              title: 'XR3ngine API Surface',
+              description: 'APIs for the XR3ngine application',
+              version: '1.0.0'
+            }
+          }
+        })
+    );
+
+    app.set('paginate', config.server.paginate);
+    app.set('authentication', config.authentication);
+
+    app.configure(sequelize);
+
+    // Enable security, CORS, compression, favicon and body parsing
+    app.use(helmet());
+    app.use(cors(
+        {
+          origin: true,
+          credentials: true
         }
+    ));
+    app.use(compress());
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+    app.use(favicon(path.join(config.server.publicDir, 'favicon.ico')));
+
+    // Set up Plugins and providers
+    app.configure(express.rest());
+    app.configure(socketio({
+      serveClient: false, handlePreflightRequest: (server, req, res) => {
+        // Set CORS headers
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET`');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.writeHead(200);
+        res.end();
       }
-    })
-  );
-
-  app.set('paginate', config.server.paginate);
-  app.set('authentication', config.authentication);
-
-  app.configure(sequelize);
-
-  // Enable security, CORS, compression, favicon and body parsing
-  app.use(helmet());
-  app.use(cors(
-    {
-      origin: true,
-      credentials: true
-    }
-  ));
-  app.use(compress());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(favicon(path.join(config.server.publicDir, 'favicon.ico')));
-
-  // Set up Plugins and providers
-  app.configure(express.rest());
-  app.configure(socketio({
-    serveClient: false, handlePreflightRequest: (server, req, res) => {
-      // Set CORS headers
-      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-      res.setHeader('Access-Control-Request-Method', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET`');
-      res.setHeader('Access-Control-Allow-Headers', '*');
-      res.writeHead(200);
-      res.end();
-    }
-  }, (io) => {
-    io.use((socket, next) => {
-      (socket as any).feathers.socketQuery = socket.handshake.query;
-      (socket as any).socketQuery = socket.handshake.query;
-      next();
-    });
-  }));
-
-  if (config.redis.enabled) {
-    app.configure(sync({
-      uri: config.redis.password != null ? `redis://${config.redis.address}:${config.redis.port}?password=${config.redis.password}` : `redis://${config.redis.address}:${config.redis.port}`
+    }, (io) => {
+      io.use((socket, next) => {
+        (socket as any).feathers.socketQuery = socket.handshake.query;
+        (socket as any).socketQuery = socket.handshake.query;
+        next();
+      });
     }));
 
-    (app as any).sync.ready.then(() => {
-      logger.info('Feathers-sync started');
+    if (config.redis.enabled) {
+      app.configure(sync({
+        uri: config.redis.password != null ? `redis://${config.redis.address}:${config.redis.port}?password=${config.redis.password}` : `redis://${config.redis.address}:${config.redis.port}`
+      }));
+
+      (app as any).sync.ready.then(() => {
+        logger.info('Feathers-sync started');
+      });
+    }
+
+    // Configure other middleware (see `middleware/index.js`)
+    app.configure(middleware);
+    app.configure(authentication);
+    // Set up our services (see `services/index.js`)
+
+    app.configure(feathersLogger(winston));
+
+    app.configure(services);
+    // Set up event channels (see channels.js)
+    app.configure(channels);
+
+    // Host the public folder
+    // Configure a middleware for 404s and the error handler
+
+    // Host the public folder
+    // Configure a middleware for 404s and the error handler
+
+    app.hooks(appHooks);
+
+    if (config.server.mode === 'api' || config.server.mode === 'realtime') {
+      (app as any).k8AgonesClient = K8s.api({
+        endpoint: `https://${process.env.KUBERNETES_SERVICE_HOST}:${process.env.KUBERNETES_PORT_443_TCP_PORT}`,
+        version: '/apis/agones.dev/v1',
+        auth: {
+          caCert: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'),
+          token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token')
+        }
+      });
+      (app as any).k8DefaultClient = K8s.api({
+        endpoint: `https://${process.env.KUBERNETES_SERVICE_HOST}:${process.env.KUBERNETES_PORT_443_TCP_PORT}`,
+        version: '/api/v1',
+        auth: {
+          caCert: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'),
+          token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token')
+        }
+      });
+    }
+
+    if ((process.env.KUBERNETES === 'true' && config.server.mode === 'realtime') || process.env.NODE_ENV === 'development' || config.server.mode === 'local') {
+      agonesSDK.connect();
+      agonesSDK.ready();
+      (app as any).agonesSDK = agonesSDK;
+      healthPing(agonesSDK);
+
+      // Create new gameserver instance
+      const gameServer = new WebRTCGameServer(app);
+      logger.info("Created new gameserver instance");
+    } else {
+      console.warn('Did not create gameserver');
+    }
+
+    app.use('/healthcheck', (req, res) => {
+      res.sendStatus(200);
     });
+  } catch(err) {
+    console.log('Server init failure');
+    console.log(err);
   }
-
-  // Configure other middleware (see `middleware/index.js`)
-  app.configure(middleware);
-  app.configure(authentication);
-  // Set up our services (see `services/index.js`)
-
-  app.configure(feathersLogger(winston));
-
-  app.configure(services);
-  // Set up event channels (see channels.js)
-  app.configure(channels);
-
-  // Host the public folder
-  // Configure a middleware for 404s and the error handler
-
-  // Host the public folder
-  // Configure a middleware for 404s and the error handler
-
-  app.hooks(appHooks);
-
-  if (config.server.mode === 'api' || config.server.mode === 'realtime') {
-    (app as any).k8AgonesClient = K8s.api({ endpoint: `https://${process.env.KUBERNETES_SERVICE_HOST}:${process.env.KUBERNETES_PORT_443_TCP_PORT}`, version: '/apis/agones.dev/v1', auth: { caCert: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'), token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token') } });
-    (app as any).k8DefaultClient = K8s.api({ endpoint: `https://${process.env.KUBERNETES_SERVICE_HOST}:${process.env.KUBERNETES_PORT_443_TCP_PORT}`, version: '/api/v1', auth: { caCert: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'), token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token') } });
-  }
-
-  if ((process.env.KUBERNETES === 'true' && config.server.mode === 'realtime') || process.env.NODE_ENV === 'development' || config.server.mode === 'local') {
-    agonesSDK.connect();
-    agonesSDK.ready();
-    (app as any).agonesSDK = agonesSDK;
-    healthPing(agonesSDK);
-
-    // Create new gameserver instance
-    const gameServer = new WebRTCGameServer(app);
-    logger.info("Created new gameserver instance");
-  } else {
-    console.warn('Did not create gameserver');
-  }
-
-  app.use('/healthcheck', (req, res) => {
-    res.sendStatus(200);
-  });
 }
 
 app.use(express.errorHandler({ logger } as any));
