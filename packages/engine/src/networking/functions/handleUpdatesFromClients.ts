4cc086913d66e716647c93deccc6d7219a2d4178
HydraFire <hydrafire.plus@gmail.com>
2020-12-10 13:29:05 +0200

    fix ts-ignore

--- packages/engine/src/networking/functions/handleUpdatesFromClients.ts
+++ packages/engine/src/networking/functions/handleUpdatesFromClients.ts
@@ -17,62 +17,45 @@ export function handleUpdatesFromClients() {
       clientInput = buffer;
     }
 
-  //@ts-ignore
+
       if(Network.instance.networkObjects[clientInput.networkId] === undefined) return;
-  //@ts-ignore
+
       const actor = getMutableComponent(Network.instance.networkObjects[clientInput.networkId].component.entity, CharacterComponent);
       actor.viewVector.set(
-  //@ts-ignore
         clientInput.viewVector.x,
-  //@ts-ignore
         clientInput.viewVector.y,
-  //@ts-ignore
         clientInput.viewVector.z
       )
       // Get input component
-  //@ts-ignore
+
       const input = getMutableComponent(Network.instance.networkObjects[clientInput.networkId].component.entity, Input);
       // Clear current data
       input?.data.clear();
 
       // Apply button input
-
-
-    //@ts-ignore
       for (let i = 0; i < clientInput.buttons.length; i++)
-  //@ts-ignore
         input?.data.set(clientInput.buttons[i].input,
           {
             type: InputType.BUTTON,
-  //@ts-ignore
             value: clientInput.buttons[i].value,
-  //@ts-ignore
             lifecycleState: clientInput.buttons[i].lifecycleState
           });
 
       // Axis 1D input
-  //@ts-ignore
-        for (let i = 0; i < clientInput.axes1d.length; i++)
-  //@ts-ignore
+      for (let i = 0; i < clientInput.axes1d.length; i++)
         input?.data.set(clientInput.axes1d[i].input,
           {
             type: InputType.ONEDIM,
-  //@ts-ignore
             value: clientInput.axes1d[i].value ?? clientInput.axes1d[i].valueX,
-  //@ts-ignore
             lifecycleState: clientInput.axes1d[i].lifecycleState
           });
 
-          // Axis 2D input
-  //@ts-ignore
+      // Axis 2D input
       for (let i = 0; i < clientInput.axes2d.length; i++)
-  //@ts-ignore
         input?.data.set(clientInput.axes2d[i].input,
           {
             type: InputType.TWODIM,
-  //@ts-ignore
             value: [clientInput.axes2d[i].valueX, clientInput.axes2d[i].valueY],
-  //@ts-ignore
             lifecycleState: clientInput.axes2d[i].lifecycleState
           });
 
