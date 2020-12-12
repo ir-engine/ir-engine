2797dbaf59048aaab35575605812e53d7b9ea1e1
xiani <xianionline@gmail.com>
2020-12-12 09:46:28 +0200

    fix loading gltf on ModelNode.src set

--- packages/engine/src/assets/functions/LoadGLTF.ts
+++ packages/engine/src/assets/functions/LoadGLTF.ts
@@ -2,14 +2,18 @@ import { DRACOLoader } from "../loaders/gltf/DRACOLoader";
 import { GLTFLoader } from "../loaders/gltf/GLTFLoader";
 import { AssetUrl } from "../types/AssetTypes";
 
-export async function LoadGLTF(url: AssetUrl) {
+export interface LoadGLTFResultInterface {
+ scene: any;
+ json: any;
+ stats: any;
+}
 
-    const gltf = await new Promise<{ scene: any; json: any; stats: any }>((resolve) => {
+export async function LoadGLTF(url: AssetUrl): Promise<LoadGLTFResultInterface> {
+    return await new Promise<LoadGLTFResultInterface>((resolve, reject) => {
         const loader = new GLTFLoader();
         const dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath('/loader_decoders/');
         loader.setDRACOLoader(dracoLoader);
-        loader.load(url, (gltf) => { resolve({ scene: gltf.scene, json: {}, stats: {} }); });
+        loader.load(url, (gltf) => { resolve({ scene: gltf.scene, json: {}, stats: {} }); }, null, (e) => { reject(e); });
     });
-    return gltf;
 }
