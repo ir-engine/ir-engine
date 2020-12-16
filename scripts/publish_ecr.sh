158ea28f8c23ad3f7a6bc3fa5b705ce31fc95b8c
Kyle Baran <kbaran@bitscoop.com>
2020-12-15 18:24:00 -0800

    Fixed script path reference.

--- scripts/publish_ecr.sh
+++ scripts/publish_ecr.sh
@@ -7,7 +7,7 @@ TAG=$2
 LABEL=$3
 
 aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
-node ./prune_ecr_images.js --repoName $REPO_NAME
+node ./scripts/prune_ecr_images.js --repoName $REPO_NAME
 
 docker tag $LABEL $ECR_URL/$REPO_NAME:$TAG
 docker tag $LABEL $ECR_URL/$REPO_NAME:latest_$STAGE
