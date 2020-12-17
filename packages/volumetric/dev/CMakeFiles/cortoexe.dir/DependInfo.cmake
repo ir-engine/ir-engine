f7e49d883f67290461bb9877dd7b8eb3b5cedfd4
shawticus <shawmakesmagic@gmail.com>
2020-12-17 13:22:05 -0800

    Fix dev folder
    Former-commit-id: 77274faff63ace22d494155c31ce4fc0531f6455

---
+++ packages/volumetric/dev/CMakeFiles/cortoexe.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/beast/Documents/GitHub/corto/src/main.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortoexe.dir/src/main.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/meshloader.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortoexe.dir/src/meshloader.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/tinyply.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortoexe.dir/src/tinyply.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/volumetric/dev/CMakeFiles/cortolib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/beast/Documents/GitHub/corto/src/bitstream.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/bitstream.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/color_attribute.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/color_attribute.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/corto_codec.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/corto_codec.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/cstream.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/cstream.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/decoder.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/decoder.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/encoder.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/encoder.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/normal_attribute.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/normal_attribute.cpp.o"
+  "/home/beast/Documents/GitHub/corto/src/tunstall.cpp" "/home/beast/Documents/GitHub/corto/CMakeFiles/cortolib.dir/src/tunstall.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
