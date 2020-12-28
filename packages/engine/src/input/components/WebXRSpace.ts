78ffcfc8e0d71d787734f23fc76be12b8df6de34
xiani <xianionline@gmail.com>
2020-12-28 15:13:36 +0200

    more renames of Component.schema to _schema
    Former-commit-id: a41217588eb01d6b86d591ad64b14628075c73b1

--- packages/engine/src/input/components/WebXRSpace.ts
+++ packages/engine/src/input/components/WebXRSpace.ts
@@ -2,7 +2,7 @@ import { Component } from '../../ecs/classes/Component';
 import { Types } from '../../ecs/types/Types';
 
 export class WebXRSpace extends Component<any> {
-  static schema = {
+  static _schema = {
     space: { type: Types.Ref },
     spaceType: { type: Types.String }
   }
--- packages/engine/src/interaction/components/InteractiveFocused.ts
+++ packages/engine/src/interaction/components/InteractiveFocused.ts
@@ -5,7 +5,7 @@ import { Entity } from "../../ecs/classes/Entity";
 export class InteractiveFocused extends Component<InteractiveFocused> {
   interacts: Entity
 
-  static schema = {
+  static _schema = {
     interacts: { type: Types.Ref }
   }
 }
