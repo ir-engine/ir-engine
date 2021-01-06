f62a4ad131bbf1b27e96858fc4829cea6ec32044
Tanya Vykliuk <tanya.vykloik@gmail.com>
2021-01-06 11:21:53 +0200

    Revert "-temporary set avatar (change avatar is BROKEN); - animation load - NOT READY;"
    This reverts commit 0d47e5bddae1fe424db7cf98b32344894d1f4a54.

--- packages/engine/src/templates/character/components/AnimationManager.ts
+++ packages/engine/src/templates/character/components/AnimationManager.ts
@@ -6,49 +6,37 @@ import { AnimationClip } from 'three';
 import { GLTFLoader } from "../../../assets/loaders/gltf/GLTFLoader";
 import { isClient } from "../../../common/functions/isClient";
 import { Types } from '../../../ecs/types/Types';
-import { resolve } from 'url';
 
 export class AnimationManager extends Component<AnimationManager> {
 	static instance: AnimationManager
 	public initialized = false
 
 	_animations: AnimationClip[] = []
-	_animationsList: any
-	getAnimations(type: string ='default'): Promise<AnimationClip[]> {
-		const animation = 'Animation';
-		// TODO: some future logic
-		// const animation = type === 'default' ? '' : '_'+type;
-		type = 'default';//temporary - for now all avatart uses default animation set
-		return new Promise(resolve=>{
-			if(this._animationsList && this._animationsList.has(type)){
-				console.log('return finded loaded already');
-				resolve(this._animationsList.get(type));
-			}else{
-				if (!isClient) {
-					resolve([]);
-					return;
-				}
-				
-				new GLTFLoader().load(`/models/avatars/${animation}.glb`, gltf => {
-						this._animations = gltf.animations;
-						this._animations?.forEach(clip => {
-							// TODO: make list of morph targets names
-							clip.tracks = clip.tracks.filter(track => !track.name.match(/^CC_Base_/));
-						});
-						this._animationsList.set(type, this._animations);
-						console.log('return and load new one', this._animationsList)
-						resolve(this._animations);
-					}
-				);
+	getAnimations(): Promise<AnimationClip[]> {
+		return new Promise(resolve => {
+			if (!isClient) {
+				resolve([]);
+				return;
 			}
-		})		
+
+			new GLTFLoader().load('/models/avatars/Animation.glb', gltf => {
+					this._animations = gltf.animations;
+					this._animations?.forEach(clip => {
+						// TODO: make list of morph targets names
+						clip.tracks = clip.tracks.filter(track => !track.name.match(/^CC_Base_/));
+					});
+					resolve(this._animations);
+				}
+			);
+		});
 	}
-	//TODO: add holder for saving loaded before animations
+
 	constructor () {
 		super();
+
 		AnimationManager.instance = this;
+
 		this.getAnimations();
-		this._animationsList = new Map();
 	}
 }
 
@@ -56,6 +44,5 @@ export class AnimationManager extends Component<AnimationManager> {
 new AnimationManager();
 
 AnimationManager._schema = {
-	animations: { type: Types.Array, default: [] },
-	// animationsList: { type: Types.Array, default: [] }
+	animations: { type: Types.Array, default: [] }
 };
