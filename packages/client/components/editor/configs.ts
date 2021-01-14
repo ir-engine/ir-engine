66195305d843dcf431753181b82d6efe0ce818ff
shawticus <shawmakesmagic@gmail.com>
2021-01-13 23:40:44 -0800

    revert store name

--- packages/client/components/editor/configs.ts
+++ packages/client/components/editor/configs.ts
@@ -23,6 +23,6 @@ get(configs, "SENTRY_DSN", process.env.SENTRY_DSN);
 (configs as any).longName = (): string => "Scene Editor";
 (configs as any).SERVER_URL = process.env.NODE_ENV === 'production' ? publicRuntimeConfig.apiServer : 'https://localhost:3030';
 (configs as any).APP_URL = process.env.NODE_ENV === 'production' ? publicRuntimeConfig.appServer : 'https://localhost:3000';
-(configs as any).FEATHERS_STORE_KEY = process.env.NODE_ENV === 'production' ? publicRuntimeConfig.feathersStoreKey : 'XR3ngine-Auth-Store';
+(configs as any).FEATHERS_STORE_KEY = process.env.NODE_ENV === 'production' ? publicRuntimeConfig.feathersStoreKey : 'TheOverlay-Auth-Store';
 
 export default configs;
