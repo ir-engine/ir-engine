4cc37e935cd0fb13e945ce441384259607fb9e51
green9016 <truegreenbelt@gmail.com>
2021-01-15 05:23:35 +0800

    use case && workflow of bot manager && bot action

--- packages/bot/run-bot.js
+++ packages/bot/run-bot.js
@@ -1,5 +1,7 @@
 const xr3ngineBot = require('./src/xr3ngine-bot');
 const AgonesSDK = require('@google-cloud/agones-sdk');
+const { BotAction } = require('./src/bot-action');
+const BotManager = require('./src/bot-manager');
 
 const agonesSDK = new AgonesSDK();
 
@@ -54,5 +56,38 @@ async function runBot() {
         process.exit(1);
     }
 }
+
+async function run() {
+    const domain = process.env.DOMAIN || 'localhost:3000';
+    const locationName = process.env.LOCATION_NAME || 'test';
+    const moveDuratioon = 2000;
+
+    const botManager = new BotManager();
+
+    botManager.addBot("bot1");
+    botManager.addBot("bot2");
+
+    botManager.addAction("bot1", new BotAction.connect(domain));
+    botManager.addAction("bot1", new BotAction.enterRoom(locationName));
+    botManager.addAction("bot1", new BotAction.moveLeft(moveDuratioon));
+    botManager.addAction("bot1", new BotAction.moveForward(moveDuratioon));
+    botManager.addAction("bot1", new BotAction.moveRight(moveDuratioon));
+    botManager.addAction("bot1", new BotAction.moveBackward(moveDuratioon));
+    botManager.addAction("bot1", new BotAction.sendMessage("Hello World! This is bot1."));
+
+    botManager.addAction("bot2", new BotAction.connect(domain));
+    botManager.addAction("bot2", new BotAction.enterRoom(locationName));
+    botManager.addAction("bot2", new BotAction.moveLeft(moveDuratioon));
+    botManager.addAction("bot2", new BotAction.moveForward(moveDuratioon));
+    botManager.addAction("bot2", new BotAction.moveRight(moveDuratioon));
+    botManager.addAction("bot2", new BotAction.moveBackward(moveDuratioon));
+    botManager.addAction("bot2", new BotAction.sendMessage("Hello World! This is bot2."));
+
+    botManager.addAction("monitor", new BotAction.monitor((stats) => console.log(stats)));
+
+    botManager.addAction("bot1", new BotAction.disconnect());
+    botManager.addAction("bot2", new BotAction.disconnect());
+}
+
 
 runBot();
