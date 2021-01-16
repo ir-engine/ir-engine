9b0aa72778e25f093eeb73085b31bd60bd5aa697
shawticus <shawmakesmagic@gmail.com>
2021-01-15 17:26:33 -0800

    Turn off tonemapping, remove dead interfaces from volumetric

--- packages/volumetric/src/Interfaces.ts
+++ packages/volumetric/src/Interfaces.ts
@@ -1,20 +1,4 @@
-import { BufferGeometry, Float32BufferAttribute } from 'three';
-
-
-export enum MessageType {
-    InitializationRequest = 0,
-    InitializationResponse = 1,
-    DataRequest = 2,
-    DataResponse = 3,
-    SetLoopRequest = 4,
-    SetStartFrameRequest = 5,
-    SetEndFrameRequest = 6,
-  };
-export interface Action {
-    type: MessageType,
-    value?: number | boolean | string
-}
-
+import { BufferGeometry } from 'three';
 export interface IFrameData {
     frameNumber: number;
     startBytePosition: number;
@@ -29,11 +13,6 @@ export interface IFileHeader {
     frameData: IFrameData[];
 }
 
-export interface IMeshTextureData {
-    frameNumber: number;
-    mesh: Buffer;
-}
-
 export interface KeyframeBuffer {
     keyframeNumber: number;
     frameNumber: number;
@@ -44,26 +23,4 @@ export interface IFrameBuffer {
     keyframeNumber: number;
     frameNumber: number;
     vertexBuffer: any;
-}
-
-export interface WorkerInitializationRequest extends Action {
-    startFrame: number;
-    endFrame: number;
-    loop: boolean;
-    filePath: string;
-    fileHeader: IFileHeader;
-    readStreamOffset: number;
-}
-
-export interface WorkerInitializationResponse extends Action {
-    isInitialized: boolean;
-}
-
-export interface WorkerDataRequest extends Action {
-    framesToFetch: number[];
-}
-
-export interface WorkerDataResponse extends Action {
-    buffers?: KeyframeBuffer[]
-    endReached: boolean;
 }
