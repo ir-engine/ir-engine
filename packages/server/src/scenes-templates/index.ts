c7bbf97e7392840700de1d75813fbe231a2b3460
XiaNi <xianionline@gmail.com>
2021-01-23 12:35:31 +0200

    template scene preview

--- packages/server/src/scenes-templates/index.ts
+++ packages/server/src/scenes-templates/index.ts
@@ -5,11 +5,13 @@ export const contents = [
   {
     id: '~tests-fall',
     name: testsFallContent.metadata.name,
+    image: '/scene-templates/tests-fall.jpg',
     data: testsFallContent
   },
   {
     id: '~sky-island',
     name: skyIslandContent.metadata.name,
+    image: '/scene-templates/sky-island.jpg',
     data: skyIslandContent
   }
 ];
@@ -23,7 +25,7 @@ export const indexes = contents.map(value => {
     },
     description: null,
     id: value.id,
-    images: { preview: { url: null } },
+    images: { preview: { url: value.image } },
     name: value.name,
     project_id: null,
     type: "scene_listing"
