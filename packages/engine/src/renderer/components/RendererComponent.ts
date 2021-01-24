7c8a821485402addef5adfaf53751e9e07d8aacd
NPatel10 <nayanpatel.np@yahoo.com>
2021-01-24 18:34:38 +0530

    Documentation for Renderer module in Engine. Added saperate tsconfig for typedoc.
    Former-commit-id: a0235a947239e3d5fbb7eb203ae159ae5a1b226c

--- packages/engine/src/renderer/components/RendererComponent.ts
+++ packages/engine/src/renderer/components/RendererComponent.ts
@@ -3,27 +3,32 @@ import { Component } from '../../ecs/classes/Component';
 import { Types } from '../../ecs/types/Types';
 import { EffectComposer } from '../../postprocessing/core/EffectComposer';
 import { DefaultPostProcessingSchema } from '../../templates/postprocessing/DefaultPostProcessingSchema';
+/** Component class for renderer. */
 export class RendererComponent extends Component<any> {
+  /** Instance of the renderer. */
   static instance: RendererComponent
   composer: EffectComposer
+  /** Is resize needed? */
   needsResize: boolean
+  /** Postprocessing schema. */
   postProcessingSchema: PostProcessingSchema
-  /**
-     * Constructs a new renderer
-     */
+  
+  /** Constructs a new renderer */
   constructor(postProcessingSchema?: PostProcessingSchema) {
     super();
     RendererComponent.instance = this;
     this.postProcessingSchema = postProcessingSchema ?? DefaultPostProcessingSchema;
   }
 
-  dispose() {
+  /** Dispose the component. */
+  dispose(): void {
     super.dispose();
     this.composer?.dispose();
     this.composer = null;
     RendererComponent.instance = null;
   }
 }
+
 /**
   * The scheme is used to set the default values of a component. 
   * The type field must be set for each property.
