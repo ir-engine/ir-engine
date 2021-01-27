45cf1ebba12a891dcd868a545ad4d2f8aa142081
root <root@pop-os.localdomain>
2021-01-27 20:19:46 +0200

    write swagger doc for componet service

--- packages/server/src/services/component/component.class.ts
+++ packages/server/src/services/component/component.class.ts
@@ -2,6 +2,7 @@ import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
 
 export class Component extends Service {
+  docs: any
   constructor (options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
   }
