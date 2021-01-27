6f2fa53edb6b8b4cc7e145de8545976e776a429c
abhinavshah421 <abhinavkumar1136@outlook.com>
2021-01-27 23:09:38 +0530

    commit changes for jsdoc
    Former-commit-id: 1fe899f0883202182627e42b8c64779e6b242c7a

--- packages/client/pages/editor/create.tsx
+++ packages/client/pages/editor/create.tsx
@@ -1,3 +1,6 @@
 import dynamic from "next/dynamic";
 
+/**
+ * Exporting component by dynamicaly loaded component.
+ */
 export default dynamic(() => import("../../components/editor/projects/CreateProjectPage"));
