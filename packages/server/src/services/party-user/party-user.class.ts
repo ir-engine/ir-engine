ed090c2351b1dbad7d91f611b6b42c87b1b7e249
root <root@pop-os.localdomain>
2021-01-30 16:15:24 +0200

    write swagger docs for group-user and location-admin services
    Former-commit-id: 721d058008377f2b867d9033dc967b9ed505b250

--- packages/server/src/services/party-user/party-user.class.ts
+++ packages/server/src/services/party-user/party-user.class.ts
@@ -3,6 +3,7 @@ import { Application } from '../../declarations';
 
 export class PartyUser extends Service {
   app: Application
+  docs: any
   constructor (options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
     this.app = app;
