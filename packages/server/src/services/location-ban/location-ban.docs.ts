5b41e95347636edd6e45d029c91670888eb0971c
root <root@pop-os.localdomain>
2021-01-30 09:52:01 +0200

    write swagger doc for party and project services

---
+++ packages/server/src/services/location-ban/location-ban.docs.ts
@@ -0,0 +1,14 @@
+export default {
+    definitions: {
+        'location-ban': {
+            type: 'object',
+            properties: {
+
+            }
+        },
+        'location-ban_list': {
+            type: 'array',
+            items: { $ref: '#/definitions/location-ban'}
+        }
+    }
+}
