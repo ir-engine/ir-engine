413c5302fb5063771bb772865f6ba8e738d7789b
root <root@pop-os.localdomain>
2021-02-01 20:40:05 +0200

    add authorization on services

---
+++ packages/server/src/services/accept-invite/accept-invite.docs.ts
@@ -0,0 +1,14 @@
+export default {
+    definitions: {
+        'a-i': {
+            type: "object",
+            properties: {
+
+            }
+        },
+        "a-i_list": {
+            type: "array",
+            items: { $ref: "#/definitions/a-i"}
+        }
+    }
+}
