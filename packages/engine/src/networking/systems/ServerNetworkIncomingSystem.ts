2455f9693771a7612ca1742616c45cf44552d8e6
root <root@pop-os.localdomain>
2021-02-02 08:00:13 +0200

    fix eslint

--- packages/engine/src/networking/systems/ServerNetworkIncomingSystem.ts
+++ packages/engine/src/networking/systems/ServerNetworkIncomingSystem.ts
@@ -39,9 +39,8 @@ const isClientNetworkInputInterface = (p: unknown): p is NetworkClientInputInter
  * @param buffer Client input interface buffer.
  */
 function handleUpdatesFromClients(buffer:any): void {
-  let clientInput: NetworkClientInputInterface;
+  const clientInput: NetworkClientInputInterface = ClientInputModel.fromBuffer(buffer);
 
-  clientInput = ClientInputModel.fromBuffer(buffer);
 
   if (Network.instance.networkObjects[clientInput.networkId] === undefined) {
     console.error('Network object not found for networkId', clientInput.networkId);
