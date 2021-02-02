bcad2ffdf094607e16e6d7561467736cb8a4aaf6
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-02-02 14:17:09 +0200

    initial commit for arc client as package xr3ngine
    Former-commit-id: d8a258103c831dbebfdb07489f60e9f628833cc1

---
+++ packages/arc/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const IndexPage = (props: any): any => {
+    return (
+        <div>
+            <h1>Arc Home Page</h1>
+        </div>
+    );
+};
+export default IndexPage;
