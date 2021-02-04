1b6d9d8fa34b99e7db501596f11e4e060a05deaa
kimenyi <kimenyikevin@gmail.com>
2021-02-04 17:45:11 +0200

    write tsdoc for collection and collection  type
    Former-commit-id: 620f6ad797fc0694e0a5550b244532ff5c5a4fdb

--- packages/server/src/services/collection-type/collection-type.class.ts
+++ packages/server/src/services/collection-type/collection-type.class.ts
@@ -1,6 +1,11 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
 
+/**
+ * A class for Collection type service 
+ * 
+ * @author Vyacheslav Solovjov
+ */
 export class CollectionType extends Service {
   public docs: any
   constructor (options: Partial<SequelizeServiceOptions>, app: Application) {
