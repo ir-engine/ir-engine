9d30993c4eca9834180fef30594a238c3852cb35
kimenyi <kimenyikevin@gmail.com>
2021-02-07 13:25:43 +0200

    write tsdoc for message and party

--- packages/server/src/services/meta/meta.class.ts
+++ packages/server/src/services/meta/meta.class.ts
@@ -15,6 +15,12 @@ export class Meta implements ServiceMethods<Data> {
     this.app = app;
   }
 
+  /**
+   * A function which is used to find meta 
+   * 
+   * @param params 
+   * @returns {@Object} 
+   */
   async find (params?: Params): Promise<Data> {
     // SPOKE HACK: This method is just returning the collection API endpoint for uploading the file from editor
     return {
@@ -22,12 +28,28 @@ export class Meta implements ServiceMethods<Data> {
     };
   }
 
+  /**
+   * A function which is used to get specific meta 
+   * 
+   * @param id of specific id 
+   * @param params 
+   * @returns {@Object} contains meta id and message 
+   * @author  Vyacheslav Solovjov
+   */
   async get (id: Id, params?: Params): Promise<Data> {
     return {
       id, text: `A new message with ID: ${id}!`
     };
   }
 
+  /**
+   * A function which is used to create new meta 
+   * 
+   * @param data for new meta 
+   * @param params 
+   * @returns {@Object} created meta 
+   * @author  Vyacheslav Solovjov
+   */
   async create (data: Data, params?: Params): Promise<Data> {
     if (Array.isArray(data)) {
       return await Promise.all(data.map(current => this.create(current, params)));
@@ -36,14 +58,39 @@ export class Meta implements ServiceMethods<Data> {
     return data;
   }
 
+  /**
+   * A function which used to update meta 
+   * 
+   * @param id 
+   * @param data used to update meta 
+   * @param params 
+   * @returns {@Object} update meta 
+   * @author  Vyacheslav Solovjov
+   */
   async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
     return data;
   }
 
+  /**
+   * A function which used to update meta 
+   * 
+   * @param id 
+   * @param data used to update meta 
+   * @param params 
+   * @returns {@Object} update meta 
+   * @author  Vyacheslav Solovjov
+   */
   async patch (id: NullableId, data: Data, params?: Params): Promise<Data> {
     return data;
   }
 
+  /**
+   * A function which is used to remove specific meta 
+   * 
+   * @param id of specific meta 
+   * @param params 
+   * @returns {@Object} removed data 
+   */
   async remove (id: NullableId, params?: Params): Promise<Data> {
     return { id };
   }
