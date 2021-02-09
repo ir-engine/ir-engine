b8d17ffeb5f21e7e376664c9d4b3fdc69756aa4c
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-02-09 13:18:42 +0200

    first changes
    Former-commit-id: d4ad24c3959f4217bcdcac02773267b8e3ea77eb

--- packages/client-core/components/social/Login.tsx
+++ packages/client-core/components/social/Login.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import getConfig from 'next/config';
 import MagicLinkEmail from '../ui/Auth/MagicLinkEmail';
 import { PasswordLogin } from '../ui/Auth/PasswordLogin';
