a3343b3fc48af3ef3173f6b00dd665af83e513fc
xiani <xianionline@gmail.com>
2021-02-12 19:08:31 +0200

    fixed file paths on volumetric page
    Former-commit-id: 06e6225946a5c2c78cb6474e64422f92587ae13e

--- packages/social/pages/volumetric.tsx
+++ packages/social/pages/volumetric.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { ToggleTest } from '../components/ToggleTest';
 import { VolumetricPlayer } from "../components/VolumetricPlayer";
 import styles from './index.module.scss';
 
 export const IndexPage = (props: any): any => {
-    const meshFilePath = typeof document !== 'undefined'? document.baseURI + "/liam.drcs" : "";
-    const videoFilePath = typeof document !== 'undefined'? document.baseURI + "/liam.mp4" : "";
+    const meshFilePath = typeof location !== 'undefined'? location.origin + "/volumetric/liam.drcs" : "";
+    const videoFilePath = typeof location !== 'undefined'? location.origin + "/volumetric/liam.mp4" : "";
     console.log("Mesh file path is", meshFilePath);
     return (
         <div className={styles.viewport}>
             <h1>Arc volumetric test</h1>
+          <ToggleTest></ToggleTest>
           {/* <Canvas> */}
             <VolumetricPlayer
               meshFilePath={meshFilePath}
