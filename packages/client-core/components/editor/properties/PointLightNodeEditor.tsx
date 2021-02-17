4a36cdf08ee391a39553c358f9ea314932df2479
abhinavshah421 <abhinavkumar1136@outlook.com>
2021-02-17 09:09:35 +0530

    changes for jsdocs in property files

--- packages/client-core/components/editor/properties/PointLightNodeEditor.tsx
+++ packages/client-core/components/editor/properties/PointLightNodeEditor.tsx
@@ -7,22 +7,39 @@ import ColorInput from "../inputs/ColorInput";
 import NumericInputGroup from "../inputs/NumericInputGroup";
 import LightShadowProperties from "./LightShadowProperties";
 import { Lightbulb } from "@styled-icons/fa-solid/Lightbulb";
+
+//Declairing properties for PointLightNodeEditor
 type PointLightNodeEditorProps = {
   editor?: object;
   node?: object;
 };
+
+/**
+ * [PointLightNodeEditor is used render editor view to customize component properties]
+ * @type {class component}
+ */
 export default class PointLightNodeEditor extends Component<
   PointLightNodeEditorProps,
   {}
 > {
+
+  //initializing iconComponent icon name
   static iconComponent = Lightbulb;
+
+  //initializing description will appears on editor view
   static description = "A light which emits in all directions from a single point.";
+
+  //function to handle changes in color property
   onChangeColor = color => {
     (this.props.editor as any).setPropertySelected("color", color);
   };
+
+  //function to handle changes in intensity
   onChangeIntensity = intensity => {
     (this.props.editor as any).setPropertySelected("intensity", intensity);
   };
+
+  //function to handle changes on range property
   onChangeRange = range => {
     (this.props.editor as any).setPropertySelected("range", range);
   };
