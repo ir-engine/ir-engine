769ccdd4f44a5c10f1560e05687237ef63466a40
shawticus <shawmakesmagic@gmail.com>
2021-02-18 04:31:42 -0800

    Add camera stuff, work out interface

---
+++ packages/capacitor-plugin-xr/android/src/main/java/com/xr3ngine/xr/Vector3.java
@@ -0,0 +1,10 @@
+public class Vector3 {
+    public float x = 0;
+    public float y = 0;
+    public float z = 0;
+    public Vector3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
