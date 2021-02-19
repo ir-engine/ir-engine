edc8efb9a6c20ff1a45014ddead75d25b78e54b3
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-02-19 11:14:28 +0200

    -mocked up Creator page
    Former-commit-id: e2be30ca6e7a92467b615c2b8154fda455c29ac6

---
+++ packages/social/pages/creator.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import AppFooter from "@xr3ngine/client-core/components/social/Footer";
+import Creator from "@xr3ngine/client-core/components/social/Creator";
+
+export default function CreatorPage() {
+ 
+  return (<>
+    <div>
+      <Creator />
+      <AppFooter user={{username:'username'}} />
+    </div>
+  </>
+  );
+}
