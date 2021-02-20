e692841e30ddfa4537289d00f68abe3090c1f901
shawticus <shawmakesmagic@gmail.com>
2021-02-19 18:00:01 -0800

    clean up plugin bridge and start test
    Former-commit-id: 11fa9040cebe64bfff5afb37029ae84f21c248b1

---
+++ packages/client/pages/plugintest.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import Xr3engineContact from '../components/xr3engineContact';
+import { Plugins, registerWebPlugin, WebPlugin } from '@capacitor/core';
+import { XRPluginPlugin } from "@xr3ngine/capacitor-plugin-xr/src/definitions";
+import '@xr3ngine/capacitor-plugin-xr/src/index';
+
+export const IndexPage = (): any => {
+    useEffect(() => {
+        const { XRPlugin } = Plugins;
+        registerWebPlugin(XRPlugin as any);
+        (XRPlugin as XRPluginPlugin).initialize({}).then(response => {
+            console.log(response);
+        });
+    }, []);
+    return (
+        <div className="lander">
+            
+        </div>
+    );
+};
+
+export default IndexPage;
