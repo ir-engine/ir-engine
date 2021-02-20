8f0614ee1cf2cf6700bac3529799f9465b391939
abhinavshah421 <abhinavkumar1136@outlook.com>
2021-02-20 09:56:36 +0530

    adding jsdoc changes
    Former-commit-id: 165021f21823fd502de905b81efe01aec5df2579

--- packages/client-core/components/editor/assets/sources/BingVideosSource.tsx
+++ packages/client-core/components/editor/assets/sources/BingVideosSource.tsx
@@ -1,9 +1,16 @@
 import VideoMediaSource from "../VideoMediaSource";
+
+/**
+ * [BingVideosSource componant used to provide a video explorer here we can seach bing videos used search bar]
+ * @type {class component}
+ */
 export default class BingVideosSource extends VideoMediaSource {
   id: string;
   name: string;
   searchLegalCopy: string;
   privacyPolicyUrl: string;
+
+  //initializing variables for this component
   constructor(api) {
     super(api);
     this.id = "bing_videos";
