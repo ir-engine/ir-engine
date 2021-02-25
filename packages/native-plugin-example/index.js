688c89af5ff9f3e871af38bf6fbeb8d1b1052e46
kimenyi <kimenyikevin@gmail.com>
2021-02-25 15:08:15 +0200

    add mobile changes
    Former-commit-id: 8822c4a24c8b4df9e5af7f244b0e0ae853c6a1db

--- packages/native-plugin-example/index.js
+++ packages/native-plugin-example/index.js
@@ -1,4 +1,3 @@
-const { createServer } = require('https');
 const http = require('http');
 const { parse } = require('url');
 const next = require('next');
@@ -8,25 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync('../../certs/key.pem'),
-  cert: fs.readFileSync('../../certs/cert.pem')
-};
-
 app.prepare().then(() => {
-  if (dev === true) {
-    createServer(httpsOptions, (req, res) => {
-      const parsedUrl = parse(req.url, true);
-      if (parsedUrl.pathname === '/healthcheck') {
-        res.sendStatus(200);
-      } else {
-        handle(req, res, parsedUrl);
-      }
-    }).listen(3000, err => {
-      if (err) throw err;
-      console.log('Client ready on https://127.0.0.1:3000');
-    });
-  } else {
     http.createServer((req, res) => {
       const parsedUrl = parse(req.url, true);
       if (parsedUrl.pathname === '/healthcheck') {
@@ -38,5 +19,4 @@ app.prepare().then(() => {
       if (err) throw err;
       console.log('Client ready on http://127.0.0.1:3000')
     })
-  }
 });
