ca9685e35bc13d47bf0985eaca9ec34e324b17c0
xiani <xianionline@gmail.com>
2021-03-01 20:21:33 +0200

    real feeds lists on client side

--- packages/client-core/redux/feed/service.ts
+++ packages/client-core/redux/feed/service.ts
@@ -15,31 +15,53 @@ export function getFeeds(type : string, limit?: number) {
       dispatch(fetchingFeeds());
       const feedsResults = [];
       if(type && type === 'featured'){
-        for(let i=0; i<51; i++){
-            feedsResults.push({ 
-                id: i, 
-                image :'https://picsum.photos/97/139',
-                viewsCount: random(1500)
-            })
-        }
-        dispatch(feedsFeaturedRetrieved(feedsResults));
+        const feedsResults = await client.service('feed').find({
+          query: {
+            action: 'featured'
+            // $limit: limit != null ? limit : getState().get('feed').get('limit'),
+            // $skip: skip != null ? skip : getState().get('feed').get('skip')
+          }
+        });
+        // for(let i=0; i<51; i++){
+        //     feedsResults.push({
+        //         id: i,
+        //         image :'https://picsum.photos/97/139',
+        //         viewsCount: random(1500)
+        //     })
+        // }
+
+        // TODO: remove map
+        dispatch(feedsFeaturedRetrieved(feedsResults.data.map(feed => {
+          return {
+            ...feed,
+            image: feed.preview
+          }
+        })));
       }else{
-        for(let i=0; i<20; i++){
-          feedsResults.push({ 
-                id: i,
-                creator:{
-                    id: '169',
-                    avatar :'https://picsum.photos/40/40',
-                    username: 'User username'
-                },
-                preview:'https://picsum.photos/375/210',
-                video:null,
-                title: 'Featured Artist Post',
-                fires: random(2000),
-                description: 'I recently understood the words of my friend Jacob West about music.'
-            })
-        }   
-        dispatch(feedsRetrieved(feedsResults));
+        // for(let i=0; i<20; i++){
+        //   feedsResults.push({
+        //         id: i,
+        //         creator:{
+        //             id: '169',
+        //             avatar :'https://picsum.photos/40/40',
+        //             username: 'User username'
+        //         },
+        //         preview:'https://picsum.photos/375/210',
+        //         video:null,
+        //         title: 'Featured Artist Post',
+        //         fires: random(2000),
+        //         description: 'I recently understood the words of my friend Jacob West about music.'
+        //     })
+        // }
+
+        const feedsResults = await client.service('feed').find({
+          query: {
+            // $limit: limit != null ? limit : getState().get('feed').get('limit'),
+            // $skip: skip != null ? skip : getState().get('feed').get('skip')
+          }
+        });
+        console.log('feedsResults', feedsResults);
+        dispatch(feedsRetrieved(feedsResults.data));
       }
       //  await client.service('feed').find({
       //   query: {
@@ -58,7 +80,7 @@ export function getFeed(feedId: string) {
   return async (dispatch: Dispatch): Promise<any> => {
     try {
       dispatch(fetchingFeeds());
-          const feed ={ 
+          const feed ={
             id: feedId,
             creator:{
                 id:'185',
@@ -75,7 +97,7 @@ export function getFeed(feedId: string) {
             stores: random(150),
             viewsCount:  random(15000),
             description: 'I recently understood the words of my friend Jacob West about music.'
-        } 
+        }
       // const feed = await client.service('feed').get(feedId);
       dispatch(feedRetrieved(feed));
     } catch(err) {
