2e9b4afc734351892c7b40b3989e772e6b0da145
abhinavshah421 <abhinavkumar1136@outlook.com>
2021-03-04 21:52:06 +0530

    checking changes for jsdoc

--- packages/client-core/components/editor/contexts/EditorContext.tsx
+++ packages/client-core/components/editor/contexts/EditorContext.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+
+/**
+ * [initializing EditorContext]
+ */
 export const EditorContext = React.createContext(null);
+
+/**
+ * [EditorContextProvider used to access value of component context]
+ */
 export const EditorContextProvider = EditorContext.Provider;
+
+/**
+ * [withEditor setting component context value]
+ */
 export function withEditor(Component) {
   return function EditorContextComponent(props) {
     return (
