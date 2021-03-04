be3d33a6fc498d8745366afaabf52f4094ff8425
xiani <xianionline@gmail.com>
2021-03-04 20:18:54 +0200

    massive code cleanup, database update

--- packages/server/src/services/feed/feed.class.ts
+++ packages/server/src/services/feed/feed.class.ts
@@ -1,9 +1,14 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
 import { Paginated, Params } from "@feathersjs/feathers";
-import { QueryTypes } from "sequelize";
+import { FindAndCountOptions, QueryTypes } from "sequelize";
 import { Feed as FeedInterface, FeedShort as FeedShortInterface, FeedDatabaseRow } from '../../../../common/interfaces/Feed';
 import { extractLoggedInUserFromParams } from "../auth-management/auth-management.utils";
+
+interface FindAndCountResultInterface<T> {
+  rows: T[];
+  count: number;
+}
 
 /**
  * A class for ARC Feed service
@@ -32,70 +37,52 @@ export class Feed extends Service {
     const {
       feed_bookmark:feedBookmarkModel,
       feed_fires:feedFiresModel,
-      user:userModel
+      user:userModel,
+      feed:feedModel
     } = this.app.get('sequelizeClient').models;
 
-    // const feeds = this.app.service('feed').Model.findAndCountAll({
-    const feeds = await super.find({
-      skip,
+
+    const options: FindAndCountOptions = {
+      offset: skip,
       limit,
-      // order: [ [ 'createdAt', 'ASC' ] ] // order not used in find?
-    }) as Paginated<FeedDatabaseRow>;
+      order: [ [ 'createdAt', 'ASC' ] ] // order not used in find?
+    };
 
     if (action === 'featured') {
-      const feedsResult: Paginated<FeedShortInterface> = {
-        data: [],
-        skip: feeds.skip,
-        limit: feeds.limit,
-        total: feeds.total,
-      };
+      options.where = { featured: true };
+      const feeds = await feedModel.findAndCountAll(options) as FindAndCountResultInterface<FeedDatabaseRow>;
 
       // use promise here as we will later get preview from static_resources
-      feedsResult.data = await Promise.all(feeds.data.map(async feed => {
+      const data = await Promise.all(feeds.rows.map(async feed => {
         const newFeed: FeedShortInterface = {
-          // isFired,
           id: feed.id,
           preview: "https://picsum.photos/375/210",
-          viewsCount: feed.viewCount
+          viewsCount: feed.viewsCount
         };
 
         return newFeed;
       }));
 
-      return feedsResult;
+      return {
+        data,
+        skip,
+        limit,
+        total: feeds.count,
+      };
     }
 
     // regular feeds
-
-    const feedsResult: Paginated<FeedInterface> = {
-      data: [],
-      skip: feeds.skip,
-      limit: feeds.limit,
-      total: feeds.total,
-    };
+    options.include = [
+      { model: userModel, as: 'user' },
+      { model: feedFiresModel, as: 'feed_fires' },
+    ];
+    const feeds = await feedModel.findAndCountAll(options) as FindAndCountResultInterface<FeedDatabaseRow>;
 
     const loggedInUser = extractLoggedInUserFromParams(params);
-    feedsResult.data = await Promise.all(feeds.data.map(async feed => {
-      const [user, feedFires ] = await Promise.all([
-        userModel.findOne({
-          where: {
-            id: feed.authorId
-          }
-        }),
-        feedFiresModel.findAndCountAll({
-          where: {
-            feedId: feed.id
-          }
-        }),
-        // feedBookmarkModel.findAndCountAll({
-        //   where: {
-        //     authorId: loggedInUser?.userId ?? '',
-        //     feedId: feed.id,
-        //   }
-        // }),
-      ]);
-
-      const isFired = loggedInUser?.userId? !!feedFires.rows.find(feedFire => feedFire.authorId === loggedInUser.userId) : false;
+    const data = feeds.rows.map(feed => {
+      // @ts-ignore
+      const { user, feed_fires } = feed;
+      const isFired = loggedInUser?.userId? !!feed_fires.find(feedFire => feedFire.authorId === loggedInUser.userId) : false;
       const isBookmarked = false;//loggedInUser?.userId? !!feedBookmarks.rows.find(bookmark => bookmark.authorId === loggedInUser.userId) : false;
 
       const newFeed: FeedInterface = {
@@ -108,7 +95,7 @@ export class Feed extends Service {
           verified : true,
         },
         description: feed.description,
-        fires: feedFires.count,
+        fires: feed_fires.count,
         isFired,
         isBookmarked,
         id: feed.id,
@@ -116,11 +103,18 @@ export class Feed extends Service {
         stores: 0,
         title: feed.title,
         video: "",
-        viewsCount: feed.viewCount
+        viewsCount: feed.viewsCount
       };
 
       return newFeed;
-    }));
+    });
+
+    const feedsResult = {
+      data,
+      skip: skip,
+      limit: limit,
+      total: feeds.count,
+    };
 
     return feedsResult;
   }
