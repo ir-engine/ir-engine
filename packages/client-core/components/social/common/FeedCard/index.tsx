7b64f4fc5ffc07392588499716bcec10badf6410
abhinavshah421 <abhinavkumar1136@outlook.com>
2021-03-05 06:18:02 +0530

    merging with dev
    Former-commit-id: 11076aa669b7b3218ad5c011df13f531c2244128

---
+++ packages/client-core/components/social/common/FeedCard/index.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import Router from "next/router";
+import { connect } from 'react-redux';
+
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
+import { Feed } from '@xr3ngine/common/interfaces/Feed';
+import { addFireToFeed, getFeedFires, removeFireToFeed } from '../../../../redux/feedFires/service';
+import { addViewToFeed } from '../../../../redux/feed/service';
+import { addBookmarkToFeed, removeBookmarkToFeed } from '../../../../redux/feedBookmark/service';
+import { selectFeedFiresState } from '../../../../redux/feedFires/selector';
+import CreatorAsTitle from '../CreatorAsTitle';
+
+import styles from './FeedCard.module.scss';
+
+const mapStateToProps = (state: any): any => {
+    return {
+        feedFiresState: selectFeedFiresState(state),
+    };
+  };
+
+  const mapDispatchToProps = (dispatch: Dispatch): any => ({
+    getFeedFires: bindActionCreators(getFeedFires, dispatch),
+    addFireToFeed: bindActionCreators(addFireToFeed, dispatch),
+    removeFireToFeed: bindActionCreators(removeFireToFeed, dispatch),
+    addBookmarkToFeed: bindActionCreators(addBookmarkToFeed, dispatch),
+    removeBookmarkToFeed: bindActionCreators(removeBookmarkToFeed, dispatch),
+    addViewToFeed : bindActionCreators(addViewToFeed, dispatch),
+});
+interface Props{
+    feed : Feed,
+    feedFiresState?: any,
+    getFeedFires?: typeof getFeedFires;
+    addFireToFeed? : typeof addFireToFeed;
+    removeFireToFeed?: typeof removeFireToFeed;
+    addBookmarkToFeed?: typeof addBookmarkToFeed;
+    removeBookmarkToFeed?: typeof removeBookmarkToFeed;
+    addViewToFeed?: typeof addViewToFeed;
+}
+const FeedCard = (props: Props) : any => {
+    const {feed, getFeedFires, feedFiresState, addFireToFeed, removeFireToFeed, addBookmarkToFeed, removeBookmarkToFeed, addViewToFeed} = props;
+    useEffect(()=>{if(feed){getFeedFires(feed.id);}}, []);
+    
+    const handleAddFireClick = (feedId) =>addFireToFeed(feedId, '150');
+    const handleRemoveFireClick = (feedId) =>removeFireToFeed(feedId, '150');
+
+    const handleAddBookmarkClick = (feedId) =>addBookmarkToFeed(feedId, '150');
+    const handleRemoveBookmarkClick = (feedId) =>removeBookmarkToFeed(feedId, '150');
+
+    const handlePlayVideo = (feedId) => {
+        addViewToFeed(feedId);
+    }
+    
+    return  feed ? <Card className={styles.tipItem} square={false} elevation={0} key={feed.id}>
+                <CreatorAsTitle creator={feed.creator} />                   
+                <CardMedia   
+                    className={styles.previewImage}                  
+                    image={feed.preview}
+                    title={feed.title}     
+                    onClick={()=>handlePlayVideo(feed.id)}               
+                />
+                <span className={styles.eyeLine}>{feed.viewsCount}<VisibilityIcon style={{fontSize: '16px'}}/></span>
+                <CardContent>
+                    <Typography className={styles.titleContainer} gutterBottom variant="h2">
+                        <span onClick={()=>Router.push('/feed')}>{feed.title}</span>
+                        <section>
+                            {feed.isFired ? 
+                                    <WhatshotIcon htmlColor="#FF6201" onClick={()=>handleRemoveFireClick(feed.id)} /> 
+                                    :
+                                    <WhatshotIcon htmlColor="#DDDDDD" onClick={()=>handleAddFireClick(feed.id)} />}
+                            <TelegramIcon />
+                            {feed.isBookmarked ? <BookmarkIcon onClick={()=>handleRemoveBookmarkClick(feed.id)} /> : <BookmarkBorderIcon onClick={()=>handleAddBookmarkClick(feed.id)} />}
+                        </section>
+                    </Typography>
+                    <Typography variant="h2" onClick={()=>console.log('Fires ',feedFiresState.get('feedFires'))}><span className={styles.flamesCount}>{feed.fires}</span>Flames</Typography>
+                    <Typography variant="h2">{feed.description}</Typography>
+                </CardContent>
+            </Card>
+        :''
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FeedCard);
---
+++ packages/engine/src/worker/VideoTexture.ts
@@ -0,0 +1,78 @@
+import {
+  Mapping,
+  RGBAFormat,
+  LinearFilter,
+  TextureDataType,
+  TextureFilter,
+  TextureEncoding,
+  DataTexture,
+  Wrapping,
+  VideoTexture as THREE_VideoTexture
+} from 'three';
+import { isWebWorker } from '../common/functions/getEnvironment';
+
+import type { VideoDocumentElementProxy } from './MessageQueue';
+
+class VideoTextureProxy extends DataTexture {
+  isVideoTexture = true;
+  videoProxy: VideoDocumentElementProxy;
+  constructor(
+    videoProxy: VideoDocumentElementProxy,
+    mapping?: Mapping,
+    wrapS?: Wrapping,
+    wrapT?: Wrapping,
+    magFilter?: TextureFilter,
+    minFilter?: TextureFilter,
+    type?: TextureDataType,
+    anisotropy?: number,
+    encoding?: TextureEncoding,
+  ) {
+    super(
+      /** @ts-ignore */
+      videoProxy.video,
+      /** @ts-ignore */
+      videoProxy.video.width,
+      /** @ts-ignore */
+      videoProxy.video.height,
+      RGBAFormat,
+      mapping,
+      wrapS,
+      wrapT,
+      magFilter,
+      minFilter,
+      type,
+      anisotropy,
+      encoding,
+    );
+    this.videoProxy = videoProxy;
+    this.minFilter = minFilter !== undefined ? minFilter : LinearFilter;
+    this.magFilter = magFilter !== undefined ? magFilter : LinearFilter;
+    this.generateMipmaps = false;
+    this.flipY = true;
+
+    const updateVideo = () => {
+      this.image = this.videoProxy.video as ImageData;
+      this.needsUpdate = true;
+      videoProxy.requestVideoFrameCallback(updateVideo);
+    };
+
+    if ('requestVideoFrameCallback' in videoProxy) {
+      videoProxy.requestVideoFrameCallback(updateVideo);
+    }
+  }
+  clone(): VideoTextureProxy {
+    return new VideoTextureProxy(this.videoProxy).copy(this);
+  }
+
+  update() {
+    if (
+      !this.videoProxy.requestVideoFrameCallback &&
+      this.videoProxy.readyState >= 2
+    ) {
+      this.image = this.videoProxy.video as ImageData;
+      this.needsUpdate = true;
+    }
+  }
+}
+
+export const VideoTexture = isWebWorker ? VideoTextureProxy : THREE_VideoTexture;
