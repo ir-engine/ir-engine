b8268eda1651fc93bc18e833e461d9bfce9e0806
NPatel10 <nayanpatel.np@yahoo.com>
2021-03-11 09:52:05 +0530

    Firefox Roation bug fix and other minor bug fixes

--- packages/server/src/gameserver/transports/WebRTCFunctions.ts
+++ packages/server/src/gameserver/transports/WebRTCFunctions.ts
@@ -88,20 +88,26 @@ export const handleConsumeDataEvent = (socket: SocketIO.Socket) => async (
 ): Promise<void> => {
     console.log('handleConsumeData');
     networkTransport = Network.instance.transport as any;
+
     const userId = getUserIdFromSocketId(socket.id);
     logger.info('Data Consumer being created on server by client: ' + userId);
+    if (Network.instance.clients[userId] == null) return;
+
     const newTransport: Transport = Network.instance.clients[userId].instanceRecvTransport;
     const outgoingDataProducer = networkTransport.outgoingDataProducer;
+
     const dataConsumer = await newTransport.consumeData({
         dataProducerId: outgoingDataProducer.id,
         appData: { peerId: userId, transportId: newTransport.id },
     });
+
     dataConsumer.on('producerclose', () => {
         dataConsumer.close();
         Network.instance.clients[userId].dataConsumers.delete(
             dataProducer.id
         );
     });
+    
     logger.info('Setting data consumer to room state');
     Network.instance.clients[userId].dataConsumers.set(
         dataProducer.id,
