fb9db440465b9d7b5c95be07a8c089cd8c0af102
kimenyi <kimenyikevin@gmail.com>
2021-03-11 07:45:54 +0200

    fix eslint

--- packages/server/src/services/feed/feed.class.ts
+++ packages/server/src/services/feed/feed.class.ts
@@ -223,11 +223,7 @@ export class Feed extends Service {
       return newFeed;
     }
 
-<<<<<<< HEAD
     async create (data : any,  params?: Params): Promise<any> {
-=======
-    async create (data: any): Promise<any> {
->>>>>>> 7d2f9e018... fix eslint
       const {feed:feedModel} = this.app.get('sequelizeClient').models;
       const loggedInUser = extractLoggedInUserFromParams(params);
       data.authorId = loggedInUser?.userId;
