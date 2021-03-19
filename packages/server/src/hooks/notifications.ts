09ef71afdae298460e4dae2530adf5974045ab41
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-03-19 18:34:56 +0200

    -add hooks to create/remove feed-fires and feed-bookmark, works
    Former-commit-id: 0f03eb4b3b0666d46369373a729ecc3a67a7baee

--- packages/server/src/hooks/notifications.ts
+++ packages/server/src/hooks/notifications.ts
@@ -1,19 +1,60 @@
 import { HookContext } from '@feathersjs/feathers';
 import logger from '../app/logger';
 
-export async function addFeedFire (context: any): Promise<void> {
-  // const { app, data } = context;
+export async function addFeedFire (context: any): Promise<HookContext> {
+    try {
+      const { result, params } = context;
+      const viewer = await context.app.service('feed').get(result.dataValues.feedId, params);
+      await context.app.service('notifications').create({
+        feedId: result.dataValues.feedId,
+        creatorAuthorId: result.dataValues.creatorId,
+        creatorViewerId: viewer.creator.id,
+        type:'feed-fire'
+      });
+      return context;
+    } catch (err) {
+      logger.error('NOTIFICATION AFTER FEED FIRE ERROR');
+      logger.error(err);
+    }
+}
 
-  // if (data.creator) {
-  //   try {
-  //     data.staticResourceId = result.id;
-  //     await app.service('attribution').create(data, context.params);
-  //     return context;
-  //   } catch (err) {
-  //     console.log('add-attribution error');
-  //     logger.error(err);
-  //   }
-  // } else {
-  //   return context;
-  // }
+export async function removeFeedFire (context: any): Promise<HookContext> {
+  try {
+    const { result, params } = context;
+    const notification = await context.app.service('notifications').find({...params, action: 'getNotificationId', feedId:result, type: 'feed-fire'});
+    await context.app.service('notifications').remove(notification.id);    
+    return context;
+  } catch (err) {
+    logger.error('NOTIFICATION AFTER REMOVE FEED FIRE ERROR');
+    logger.error(err);
+  }
+}
+
+export async function addFeedBookmark (context: any): Promise<HookContext> {
+  try {
+    const { result, params } = context;
+    const viewer = await context.app.service('feed').get(result.dataValues.feedId, params);
+    await context.app.service('notifications').create({
+      feedId: result.dataValues.feedId,
+      creatorAuthorId: result.dataValues.creatorId,
+      creatorViewerId: viewer.creator.id,
+      type:'feed-bookmark'
+    });
+    return context;
+  } catch (err) {
+    logger.error('NOTIFICATION AFTER FEED FIRE ERROR');
+    logger.error(err);
+  }
+}
+
+export async function removeFeedBookmark (context: any): Promise<HookContext> {
+try {
+  const { result, params } = context;
+  const notification = await context.app.service('notifications').find({...params, action: 'getNotificationId', feedId:result, type: 'feed-bookmark'});
+  await context.app.service('notifications').remove(notification.id);    
+  return context;
+} catch (err) {
+  logger.error('NOTIFICATION AFTER REMOVE FEED FIRE ERROR');
+  logger.error(err);
+}
 }
