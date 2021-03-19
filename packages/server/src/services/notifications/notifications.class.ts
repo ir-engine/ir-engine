e46a20cafdbd5d761a9fc8fbcaa2bfa63a57a546
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-03-19 15:57:51 +0200

    -added new notification table; -start add hook on feed-fire
    Former-commit-id: e3b400932e55fc04a10baeea71db59fe8c3bfb26

---
+++ packages/server/src/services/notifications/notifications.class.ts
@@ -0,0 +1,21 @@
+import { BadRequest } from '@feathersjs/errors';
+import { Params } from '@feathersjs/feathers';
+import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
+import { QueryTypes } from 'sequelize';
+import { Application } from '../../declarations';
+import { extractLoggedInUserFromParams } from '../auth-management/auth-management.utils';
+
+/**
+ * A class for ARC Feed service
+ */
+export class Notifications extends Service {
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app: Application
+  docs: any
+
+  constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
+    super(options);
+    this.app = app;
+  }
+
+}
