e3b400932e55fc04a10baeea71db59fe8c3bfb26
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-03-19 15:57:51 +0200

    -added new notification table; -start add hook on feed-fire

---
+++ packages/server/src/hooks/notifications.ts
@@ -0,0 +1,19 @@
+import { HookContext } from '@feathersjs/feathers';
+import logger from '../app/logger';
+
+export async function addFeedFire (context: any): Promise<void> {
+  // const { app, data } = context;
+
+  // if (data.creator) {
+  //   try {
+  //     data.staticResourceId = result.id;
+  //     await app.service('attribution').create(data, context.params);
+  //     return context;
+  //   } catch (err) {
+  //     console.log('add-attribution error');
+  //     logger.error(err);
+  //   }
+  // } else {
+  //   return context;
+  // }
+}
--- packages/server/src/services/feed-fires/feed-fires.hooks.ts
+++ packages/server/src/services/feed-fires/feed-fires.hooks.ts
@@ -1,3 +1,6 @@
+import { HookContext } from "@feathersjs/feathers";
+import logger from "../../app/logger";
+// import { addFeedFire } from "../../hooks/notifications";
 
 export default {
   before: {
@@ -14,7 +17,36 @@ export default {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      async (context: HookContext): Promise<HookContext> => {
+        try {
+          const { app, result } = context;
+          console.warn('contex', context)
+          // result.data.forEach(async (item) => {
+          //   if (item.subscriptions && item.subscriptions.length > 0) {
+          //     await Promise.all(item.subscriptions.map(async (subscription: any) => {
+          //       subscription.dataValues.subscriptionType = await context.app.service('subscription-type').get(subscription.plan);
+          //     }));
+          //   }
+
+          //   const userAvatarResult = await app.service('static-resource').find({
+          //     query: {
+          //       staticResourceType: 'user-thumbnail',
+          //       userId: item.id
+          //     }
+          //   });
+
+          //   if (userAvatarResult.total > 0 && item.dataValues) {
+          //     item.dataValues.avatarUrl = userAvatarResult.data[0].url;
+          //   }
+          // });
+          return context;
+        } catch (err) {
+          logger.error('USER AFTER FIND ERROR');
+          logger.error(err);
+        }
+      }
+    ],
     update: [],
     patch: [],
     remove: []
