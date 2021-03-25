8ab75830991728925b4aa379b7e69874fadb0044
HexaField <joshfield999@gmail.com>
2021-03-26 10:15:55 +1100

    cam follow query

--- packages/engine/src/camera/systems/CameraSystem.ts
+++ packages/engine/src/camera/systems/CameraSystem.ts
@@ -56,95 +56,90 @@ export class CameraSystem extends System {
     this.queryResults.followCameraComponent.added?.forEach(entity => {
       CameraComponent.instance.followTarget = entity;
     });
+    this.queryResults.followCameraComponent.removed?.forEach(entity => {
+      CameraComponent.instance.followTarget = null;
+    });
 
-    this.queryResults.cameraComponent.all?.forEach(entity => {
-      if (Engine.renderer?.xr?.isPresenting) return;
-      const cam = getComponent(entity, CameraComponent) as CameraComponent;
-      if (!!cam.followTarget && hasComponent(cam.followTarget, FollowCameraComponent)) {
+    // follow camera component should only ever be on the character
+    this.queryResults.followCameraComponent.all?.forEach(entity => {
+      console.log(entity)
+      const cameraDesiredTransform: DesiredTransformComponent = getMutableComponent(CameraSystem.activeCamera, DesiredTransformComponent) as DesiredTransformComponent; // Camera
+      if (!cameraDesiredTransform.position) {
+        cameraDesiredTransform.position = new Vector3();
+      }
+      if (!cameraDesiredTransform.rotation) {
+        cameraDesiredTransform.rotation = new Quaternion();
+      }
+      const actor: CharacterComponent = getMutableComponent<CharacterComponent>(entity, CharacterComponent as any);
+      const actorTransform = getMutableComponent(entity, TransformComponent);
 
-        const cameraDesiredTransform: DesiredTransformComponent = getMutableComponent(entity, DesiredTransformComponent) as DesiredTransformComponent; // Camera
-        if (!cameraDesiredTransform.position) {
-          cameraDesiredTransform.position = new Vector3();
-        }
-        if (!cameraDesiredTransform.rotation) {
-          cameraDesiredTransform.rotation = new Quaternion();
-        }
-        const actor: CharacterComponent = getMutableComponent<CharacterComponent>(cam.followTarget, CharacterComponent as any);
-        const actorTransform = getMutableComponent(cam.followTarget, TransformComponent);
+      const cameraFollow = getMutableComponent<FollowCameraComponent>(entity, FollowCameraComponent) as FollowCameraComponent;
 
-        const cameraFollow = getMutableComponent<FollowCameraComponent>(cam.followTarget, FollowCameraComponent) as FollowCameraComponent;
+      cameraDesiredTransform.rotationRate = isMobileOrTablet() || cameraFollow.mode === CameraModes.FirstPerson ? 5 : 3.5
+      cameraDesiredTransform.positionRate = isMobileOrTablet() || cameraFollow.mode === CameraModes.FirstPerson ? 3.5 : 2
 
-        cameraDesiredTransform.rotationRate = isMobileOrTablet() || cameraFollow.mode === CameraModes.FirstPerson ? 5 : 3.5
-        cameraDesiredTransform.positionRate = isMobileOrTablet() || cameraFollow.mode === CameraModes.FirstPerson ? 3.5 : 2
+      let camDist = cameraFollow.distance;
+      if (cameraFollow.mode === CameraModes.FirstPerson) camDist = 0.01;
+      else if (cameraFollow.mode === CameraModes.ShoulderCam) camDist = cameraFollow.minDistance;
+      else if (cameraFollow.mode === CameraModes.TopDown) camDist = cameraFollow.maxDistance;
 
-        let camDist = cameraFollow.distance;
-        if (cameraFollow.mode === CameraModes.FirstPerson) camDist = 0.01;
-        else if (cameraFollow.mode === CameraModes.ShoulderCam) camDist = cameraFollow.minDistance;
-        else if (cameraFollow.mode === CameraModes.TopDown) camDist = cameraFollow.maxDistance;
+      const phi = cameraFollow.mode === CameraModes.TopDown ? 85 : cameraFollow.phi;
 
-        const phi = cameraFollow.mode === CameraModes.TopDown ? 85 : cameraFollow.phi;
+      let targetPosition;
+      if (actor) { // this is for cars
+        const shoulderOffsetWorld = cameraFollow.offset.clone().applyQuaternion(actor.tiltContainer.quaternion);
+        targetPosition = actor.tiltContainer.getWorldPosition(vec3).add(shoulderOffsetWorld);
+      } else {
+        cameraDesiredTransform.rotationRate = 7;
+        cameraDesiredTransform.positionRate = 15;
+        targetPosition = actorTransform.position;
+      }
 
-        let targetPosition;
-        if (actor) { // this is for cars
-          const shoulderOffsetWorld = cameraFollow.offset.clone().applyQuaternion(actor.tiltContainer.quaternion);
-          targetPosition = actor.tiltContainer.getWorldPosition(vec3).add(shoulderOffsetWorld);
-        } else {
-          cameraDesiredTransform.rotationRate = 7;
-          cameraDesiredTransform.positionRate = 15;
-          targetPosition = actorTransform.position;
-        }
+      // Raycast for camera
+      const cameraRaycastStart = new Vec3(targetPosition.x, targetPosition.y, targetPosition.z);
+      const cameraTransform: TransformComponent = getMutableComponent(CameraSystem.activeCamera, TransformComponent)
+      const cameraRaycastEnd = new Vec3(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
 
-        // Raycast for camera
-        const cameraRaycastStart = new Vec3(targetPosition.x, targetPosition.y, targetPosition.z);
-        const cameraTransform: TransformComponent = getMutableComponent(CameraSystem.activeCamera, TransformComponent)
-        const cameraRaycastEnd = new Vec3(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+      const cameraRaycastOptions = {
+        collisionFilterMask: CollisionGroups.Default | CollisionGroups.Car,
+        skipBackfaces: true /* ignore back faces */
+      };
 
-        const cameraRaycastOptions = {
-          collisionFilterMask: CollisionGroups.Default | CollisionGroups.Car,
-          skipBackfaces: true /* ignore back faces */
-        };
+      if (!actor) { // this is for cars
+        cameraRaycastOptions.collisionFilterMask = CollisionGroups.Default;
+      }
 
-        if (!actor) { // this is for cars
-          cameraRaycastOptions.collisionFilterMask = CollisionGroups.Default;
-        }
+      cameraFollow.rayHasHit = PhysicsSystem.physicsWorld.raycastClosest(cameraRaycastStart, cameraRaycastEnd, cameraRaycastOptions, cameraFollow.rayResult);
 
-        cameraFollow.rayHasHit = PhysicsSystem.physicsWorld.raycastClosest(cameraRaycastStart, cameraRaycastEnd, cameraRaycastOptions, cameraFollow.rayResult);
+      if(cameraFollow.mode !== CameraModes.FirstPerson && cameraFollow.rayHasHit && cameraFollow.rayResult.distance < camDist && cameraFollow.rayResult.distance > 0.1) {
+        camDist = cameraFollow.rayResult.distance - 0.5;
+      }
 
-        if(cameraFollow.mode !== CameraModes.FirstPerson && cameraFollow.rayHasHit && cameraFollow.rayResult.distance < camDist && cameraFollow.rayResult.distance > 0.1) {
-          camDist = cameraFollow.rayResult.distance - 0.5;
-        }
+      cameraDesiredTransform.position.set(
+          targetPosition.x + camDist * Math.sin(cameraFollow.theta * PI_2Deg) * Math.cos(phi * PI_2Deg),
+          targetPosition.y + camDist * Math.sin(phi * PI_2Deg),
+          targetPosition.z + camDist * Math.cos(cameraFollow.theta * PI_2Deg) * Math.cos(phi * PI_2Deg)
+      );
 
-        cameraDesiredTransform.position.set(
-            targetPosition.x + camDist * Math.sin(cameraFollow.theta * PI_2Deg) * Math.cos(phi * PI_2Deg),
-            targetPosition.y + camDist * Math.sin(phi * PI_2Deg),
-            targetPosition.z + camDist * Math.cos(cameraFollow.theta * PI_2Deg) * Math.cos(phi * PI_2Deg)
-        );
+      direction.copy(cameraDesiredTransform.position);
+      direction = direction.sub(targetPosition).normalize();
 
-        direction.copy(cameraDesiredTransform.position);
-        direction = direction.sub(targetPosition).normalize();
+      mx.lookAt(direction, empty, upVector);
+      cameraDesiredTransform.rotation.setFromRotationMatrix(mx);
+      if (actor) {
+        actor.viewVector = new Vector3(0, 0, -1).applyQuaternion(cameraDesiredTransform.rotation)
+      } else {
+        cameraTransform.rotation.copy(cameraDesiredTransform.rotation);
+      }
 
-        mx.lookAt(direction, empty, upVector);
-        cameraDesiredTransform.rotation.setFromRotationMatrix(mx);
-        if (actor) {
-          actor.viewVector = new Vector3(0, 0, -1).applyQuaternion(cameraDesiredTransform.rotation)
-        } else {
-          cameraTransform.rotation.copy(cameraDesiredTransform.rotation);
-        }
-
-        // for pointer lock controls
-        // if(cameraFollow.mode === CameraModes.FirstPerson || cameraFollow.mode === CameraModes.ShoulderCam) {
-        //     cameraTransform.rotation.copy(cameraDesiredTransform.rotation);
-        // }
-        if (cameraFollow.mode === CameraModes.FirstPerson) {
-          cameraDesiredTransform.position.copy(targetPosition);
-        }
+      // for pointer lock controls
+      // if(cameraFollow.mode === CameraModes.FirstPerson || cameraFollow.mode === CameraModes.ShoulderCam) {
+      //     cameraTransform.rotation.copy(cameraDesiredTransform.rotation);
+      // }
+      if (cameraFollow.mode === CameraModes.FirstPerson) {
+        cameraDesiredTransform.position.copy(targetPosition);
       }
     });
-
-    this.queryResults.cameraComponent.changed?.forEach(entity => {
-      // applySettingsToCamera(entity)
-    });
-
   }
 }
 
