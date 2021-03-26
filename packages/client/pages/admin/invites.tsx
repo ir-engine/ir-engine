2008a117e518d02359c694d383e631ac5d8d5052
kimenyi <kimenyikevin@gmail.com>
2021-03-26 12:26:02 +0200

    rebase continue

--- packages/client/pages/admin/invites.tsx
+++ packages/client/pages/admin/invites.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import Dashboard from "@xr3ngine/client-core/components/ui/Layout/Dashboard";
 import { bindActionCreators, Dispatch } from "redux";
-import InvitesConsole from "@xr3ngine/client-core/components/ui/Admin/InvitesConsole";
+import InvitesConsole from "@xr3ngine/client-core/components/ui/Admin/Invite/index";
 import { doLoginAuto } from '@xr3ngine/client-core/redux/auth/service';
 import { connect } from 'react-redux';
 
