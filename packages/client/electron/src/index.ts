53b49e879a814c071696fc457e2654122c6baf8e
shawticus <shawmakesmagic@gmail.com>
2021-03-31 15:44:37 -0700

    Change package.json

--- packages/client/electron/src/index.ts
+++ packages/client/electron/src/index.ts
@@ -12,7 +12,7 @@ app.on("ready", () => {
 });
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function () {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
@@ -20,7 +20,7 @@ app.on("window-all-closed", function () {
   }
 });
 
-app.on("activate", function () {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (myCapacitorApp.getMainWindow().isDestroyed()) myCapacitorApp.init();
