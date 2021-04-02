bcb24b56a2a1277b5f7abda55d881c52479c93ea
Abhinav <abhinavkumar1136@outlook.com>
2021-04-02 08:33:42 +0530

    creating docusaurus build for npx serve
    Former-commit-id: 9c52749e518d9122e4b0544c16edd7ed6d9e4e76

--- packages/docs/build/assets/js/8b947b8d.cf1f9d90.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[703],{773:function(l){l.exports=JSON.parse('{"allTagsPath":"/build/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/build/blog/tags/hello"}')}}]);
