1761bbf2b487a9fbf159d34e69b574f1a4e57a95
shawticus <shawmakesmagic@gmail.com>
2021-04-04 00:12:35 -0700

    Missing mediastream reducer

--- packages/client-networking/src/reducers/reducers.ts
+++ packages/client-networking/src/reducers/reducers.ts
@@ -1,9 +1,11 @@
+import locationReducer from '@xr3ngine/client-core/src/social/reducers/location/reducers';
 import channelConnectionoReducer from './channelConnection/reducers';
 import instanceConnectionoReducer from './instanceConnection/reducers';
-import locationReducer from '@xr3ngine/client-core/src/social/reducers/location/reducers';
+import mediastreamReducer from './mediastream/reducers';
 import transportReducer from './transport/reducers';
 
 export default {
+  mediastream: mediastreamReducer,
   channelConnection: channelConnectionoReducer,
   instanceConnection: instanceConnectionoReducer,
   locations: locationReducer,
