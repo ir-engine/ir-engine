2463c7312adc85bcce3b2080002c98e22fa0649e
shawticus <shawmakesmagic@gmail.com>
2021-04-07 15:05:13 -0700

    Merge stuff, add rollup, add node sass

---
+++ packages/client-ml/rollup.config.js
@@ -0,0 +1,82 @@
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import html from '@rollup/plugin-html';
+import scss from 'rollup-plugin-scss';
+import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload';
+
+const isProd = process.env.NODE_ENV === 'production';
+const extensions = ['.js', '.ts', '.tsx'];
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    file: 'public/index.js',
+    format: 'iife',
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development'),
+    }),
+    resolve({
+      extensions,
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel({
+      extensions,
+      exclude: /node_modules/,
+      babelrc: false,
+      runtimeHelpers: true,
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        'react-require',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-object-rest-spread', {
+          useBuiltIns: true,
+        }],
+        ['@babel/plugin-transform-runtime', {
+          corejs: 3,
+          helpers: true,
+          regenerator: true,
+          useESModules: false,
+        }],
+      ],
+    }),
+    html({
+      fileName: 'index.html',
+      title: 'Rollup + TypeScript + React = ❤️',
+      template: ({ title }) => {
+        return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>${title}</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+  <div id="app"></div>
+  <script src="index.js"></script>
+</body>
+</html>
+`;
+      },
+    }),
+    scss({
+      output: 'public/index.css',
+    }),
+    (isProd && terser()),
+    (!isProd && livereload({
+      watch: 'public',
+    })),
+  ],
+};
