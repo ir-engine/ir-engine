731d48097ed54d55a877768980057685ef72c59b
alexmz <aleksmazur12@gmail.com>
2021-04-07 14:55:44 +0300

    Fix-Login/Creator

--- packages/social/pages/index.tsx
+++ packages/social/pages/index.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import AppHeader from "@xr3ngine/client-core/src/socialmedia/components/Header";
 import FeedMenu from "@xr3ngine/client-core/src/socialmedia/components/FeedMenu";
 import AppFooter from "@xr3ngine/client-core/src/socialmedia/components/Footer";
-import FlatSignIn from '@xr3ngine/client-core/src/socialmedia/components/Login';
 
 import { LoginUserHook } from "@xr3ngine/client-core/src/socialmedia/components/GlobalHook";
 import { selectCreatorsState } from "@xr3ngine/client-core/src/socialmedia/reducers/creator/selector";
@@ -13,38 +12,94 @@ import { Plugins } from '@capacitor/core';
 import styles from './index.module.scss';
 import { selectAuthState } from "@xr3ngine/client-core/src/user/reducers/auth/selector";
 import { connect } from "react-redux";
+import { User } from "@xr3ngine/common/interfaces/User";
+import { bindActionCreators, Dispatch } from "redux";
+import { doLoginAuto } from "@xr3ngine/client-core/src/user/reducers/auth/service";
+import { createCreator } from "@xr3ngine/client-core/src/socialmedia/reducers/creator/service";
+
 const { Example } = Plugins;
 
 const mapStateToProps = (state: any): any => {
   return {
     authState: selectAuthState(state),
+    auth: selectAuthState(state),
     creatorsState: selectCreatorsState(state),
+   
   };
 };
 
-const  Home = ({ authState, creatorsState }) => {
-  const { data, setLoginUser } = LoginUserHook();
+const mapDispatchToProps = (dispatch: Dispatch): any => ({
+  // loginUserByPassword: bindActionCreators(loginUserByPassword, dispatch),
+  doLoginAuto: bindActionCreators(doLoginAuto, dispatch),
+  createCreator: bindActionCreators(createCreator, dispatch)
+});
 
+
+
+const  Home = ({ authState, creatorsState, createCreator,  doLoginAuto, auth}) => {
+  
+ 
+ 
+  const { data, setLoginUser } = LoginUserHook();
   const [loginData, setLoginData] = useState(null);
   const [stories, setStories] = useState(null);
   const [suggestions, setSuggestions] = useState(null);
   const [feed, setFeed] = useState(null);
 
+    
+
   const updateLoginUser = (data: any) => {
     setLoginUser(data);
     setLoginData(data);
   };
+
+  const status = authState.get('authUser')?.identityProvider.type;
+  
+
+
+  useEffect(()=>{
+    if (status === 'guest') {
+    if(auth){
+      // const user = auth.get('authUser').identityProvider as User;
+      const user = auth.get('user') as User;
+      console.log(user);
+      const userId = user ? user.id : null;
+      if(userId){
+        createCreator();
+      }
+    }
+  }else{
+    if(auth){
+       const user = auth.get('authUser').identityProvider as User;
+      console.log(user);
+      const userId = user ? user.id : null;
+      if(userId){
+        createCreator();
+      }
+    }
+  }}
+  ,[auth]);
+
+
+ 
+ 
+
   useEffect(() => {
     if (Example) {
       Example.echo({ value: 'Example plugin detected' }).then(data => {
         console.log(data);
       });
     }
+  
   setLoginData({username:'username'});
+  doLoginAuto(true);
   }, []);
 
 
+ 
+
   useEffect(() => {
+   
     fetch("/api/loginUser")
       .then((response) => response.json())
       .then((data) => updateLoginUser(data));
@@ -63,9 +118,10 @@ const  Home = ({ authState, creatorsState }) => {
   }, []);
 
 
+
   return (<>
     <div className={styles.viewport}>
-     {authState.get('user')?.id && creatorsState?.get('currentCreator') ? 
+     {/* {authState.get('user')?.id && creatorsState?.get('currentCreator') ?  */}
       <><AppHeader logo="/assets/logoBlack.png" />
       <FeedMenu />
       {/* <MoreModalItems /> */}
@@ -77,11 +133,11 @@ const  Home = ({ authState, creatorsState }) => {
             })}
         </div> */}
       {/* <HomeRightBar data={suggestions} /> */}
-      <AppFooter /></> : 
-      <FlatSignIn logo="/assets/LogoColored.png" />}
+      <AppFooter /></>  
+       {/* <FlatSignIn logo="/assets/LogoColored.png" />} */}
     </div>
   </>
   );
 };
 
-export default connect(mapStateToProps)(Home);
+  export default connect(mapStateToProps,mapDispatchToProps)(Home);
