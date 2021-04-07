e3980831daabc1da13e4e746125364fa4d28d052
shawticus <shawmakesmagic@gmail.com>
2021-04-07 05:54:48 -0700

    Fix migration
    Former-commit-id: 430e2a7da080d7f8b06d83c9eef0197fef3e1bcb

--- packages/nft/migrations/2_deploy_contracts.js
+++ packages/nft/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 const colors = require('colors')
-const LVR = artifacts.require('LVR.sol')
+const XR3 = artifacts.require('XR3.sol')
 const { deployProxy } = require('@openzeppelin/truffle-upgrades')
 
 module.exports = async deployer => {
-  const app = await deployProxy(LVR, { deployer, initializer: 'initialize' })
+  const app = await deployProxy(XR3, { deployer, initializer: 'initialize' })
   const owner = await app.owner()
-  console.log(colors.grey(`LVR contract owner: ${owner}`))
-  console.log(colors.green('LVR contract address:'))
+  console.log(colors.grey(`XR3 contract owner: ${owner}`))
+  console.log(colors.green('XR3 contract address:'))
   console.log(colors.yellow(app.address))
 }
