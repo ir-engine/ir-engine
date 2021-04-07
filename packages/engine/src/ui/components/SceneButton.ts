0ae46df9b27733b742f367e8892f580846e69dd7
Kevin Roan <kevinnroan@gmail.com>
2021-04-08 06:51:38 +0800

    add hover attribute of button
    Former-commit-id: 6cae71366d366f39052992060b64573c6582c436

--- packages/engine/src/ui/components/SceneButton.ts
+++ packages/engine/src/ui/components/SceneButton.ts
@@ -46,8 +46,47 @@ class SceneButton extends Object3D {
 	
 		textBlock.position.set(pos[0], pos[1], pos[2]);
 	
+
 		this.add(textBlock);
+
+
+		const hoveredStateAttributes = {
+			state: "hovered",
+			attributes: {
+				offset: 0.035,
+				backgroundColor: new Color( 0x999999 ),
+				backgroundOpacity: 1,
+				fontColor: new Color( 0xffffff )
+			},
+		};
+	
+		const idleStateAttributes = {
+			state: "idle",
+			attributes: {
+				offset: 0.035,
+				backgroundColor: new Color( 0x666666 ),
+				backgroundOpacity: 0.3,
+				fontColor: new Color( 0xffffff )
+			},
+		};
+
+		const selectStateAttributes = {
+			state: "selected",
+			attributes: {
+				offset: 0.02,
+				backgroundColor: new Color( 0x777777 ),
+				fontColor: new Color( 0x222222 )
+			},
+			onSet: ()=> {
+				console.log('seleteced');
+			}
+		};
+
+		textBlock.setupState(hoveredStateAttributes);
+		textBlock.setupState(idleStateAttributes);
+		textBlock.setupState(selectStateAttributes);
 	  }
+
 }
 
 export default SceneButton;
