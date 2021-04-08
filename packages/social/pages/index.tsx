245c05befe445158300c3d87deade0ef87f04d61
Tanya Vykliuk <tanya.vykliuk@gmail.com>
2021-04-08 13:33:00 +0300

    fixed index page for social package

--- packages/social/pages/index.tsx
+++ packages/social/pages/index.tsx
@@ -8,101 +8,30 @@ import { selectCreatorsState } from "@xr3ngine/client-core/src/socialmedia/reduc
 import FlatSignIn from '@xr3ngine/client-core/src/socialmedia/components/Login';
 import {Stories} from '@xr3ngine/client-core/src/socialmedia/components/Stories';
 
-// import { Plugins } from '@capacitor/core';
-
 import styles from './index.module.scss';
 import { selectAuthState } from "@xr3ngine/client-core/src/user/reducers/auth/selector";
 import { connect } from "react-redux";
-import { User } from "@xr3ngine/common/interfaces/User";
-import { bindActionCreators, Dispatch } from "redux";
-import { doLoginAuto } from "@xr3ngine/client-core/src/user/reducers/auth/service";
-import { createCreator } from "@xr3ngine/client-core/src/socialmedia/reducers/creator/service";
+import { Dispatch } from "redux";
 
-const { Example } = Plugins;
         
 const mapStateToProps = (state: any): any => {
   return {
     authState: selectAuthState(state),
-    auth: selectAuthState(state),
     creatorsState: selectCreatorsState(state),
-   
   };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch): any => ({
-  // loginUserByPassword: bindActionCreators(loginUserByPassword, dispatch),
-  doLoginAuto: bindActionCreators(doLoginAuto, dispatch),
-  createCreator: bindActionCreators(createCreator, dispatch)
 });
 
 
 
-const  Home = ({ authState, creatorsState, createCreator,  doLoginAuto, auth}) => {
-  
- 
- 
-  const { data, setLoginUser } = LoginUserHook();
-  const [loginData, setLoginData] = useState(null);
-  const [stories, setStories] = useState(null);
-  const [suggestions, setSuggestions] = useState(null);
-  const [feed, setFeed] = useState(null);
+const  Home = ({ authState, creatorsState}) => {
 
-    
-
-  const updateLoginUser = (data: any) => {
-    setLoginUser(data);
-    setLoginData(data);
-  };
-
-  const status = authState.get('authUser')?.identityProvider.type;
-  
-
-
-  useEffect(()=>{
-    if (status === 'guest') {
-    if(auth){
-      // const user = auth.get('authUser').identityProvider as User;
-      const user = auth.get('user') as User;
-      console.log(user);
-      const userId = user ? user.id : null;
-      if(userId){
-        createCreator();
-      }
-    }
-  }else{
-    if(auth){
-       const user = auth.get('authUser').identityProvider as User;
-      console.log(user);
-      const userId = user ? user.id : null;
-      if(userId){
-        createCreator();
-      }
-    }
-  }}
-  ,[auth]);
-
-
- 
- 
-
-  useEffect(() => {
-    if (Example) {
-      Example.echo({ value: 'Example plugin detected' }).then(data => {
-        console.log(data);
-      });
-    }
-  
-  setLoginData({username:'username'});
-  doLoginAuto(true);
-  }, []);
-
- 
-
-  useEffect(() => {
-   
-    fetch("/api/loginUser")
-      .then((response) => response.json())
-      .then((data) => updateLoginUser(data));
+  const stories = [] as any [];
+  for(let i=0;i<20;i++){
+    stories.push({image:null});
+  }
 
   return (<>
     <div className={styles.viewport}>
@@ -111,15 +40,8 @@ const  Home = ({ authState, creatorsState, createCreator,  doLoginAuto, auth}) =
         <AppHeader logo="/assets/logoBlack.png" />
         <Stories stories={stories} />
         <FeedMenu />
-        {/* <MoreModalItems /> */}
-        {/* <div className="homepage-feed lg:mr-8 flex flex-col ">
-            {feed &&
-              feed.map((item: any) => {
-                return <FeedItem data={item} key={item.pid} />;
-              })}
-          </div> */}
-        {/* <HomeRightBar data={suggestions} /> */}
-        <AppFooter /></> : 
+        <AppFooter />
+      </> : 
         <FlatSignIn logo="/assets/LogoColored.png" />}
     </div>
   </>
