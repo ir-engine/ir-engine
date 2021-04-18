3ec9df3fa83b858cf63f78423caaa4f7fb9ada78
Kevin Roan <kevinnroan@gmail.com>
2021-04-18 11:55:20 -0700

    try to use shaka-player for video streaming in hls&dash

--- packages/engine/src/ui/components/VideoElement.ts
+++ packages/engine/src/ui/components/VideoElement.ts
@@ -1,6 +1,7 @@
 import { Block, Text } from "../../assets/three-mesh-ui";
 import { Object3D, Color, VideoTexture, TextureLoader, Texture, ClampToEdgeWrapping, RepeatWrapping, LinearFilter, RGBFormat, RGBAFormat } from "three";
 import {MeshBasicMaterial, FrontSide, PlaneGeometry, Mesh} from 'three';
+import shaka from 'shaka-player';
 
 class VideoElement extends Object3D {
     container: Block;
@@ -23,19 +24,21 @@ class VideoElement extends Object3D {
       const tag = document.getElementById('video360');
   
       let video = document.createElement( 'video' );
-      video.setAttribute('crossorigin', 'anonymous');
-      video.style.display = 'none';
-      tag.append( video );
-      video.loop = "loop";
-      video.src = url;
-      video.muted = true;
-      video.volume = 0.5;
+      // video.setAttribute('crossorigin', 'anonymous');
+      // video.style.display = 'none';
+      document.documentElement.append( video );
+      // video.loop = "loop";
+      // video.src = url;
+      // video.muted = true;
+      // video.volume = 0.5;
+      video.controls = true;
       video.autoplay = true;
       const texture = new VideoTexture(video);
       this.container.set({backgroundTexture: texture});
   
       video.addEventListener( 'canplaythrough', function () {
         var promise = video.play();
+        console.log('playing');
         if (promise !== undefined) {
           promise.then(_ => {
           }).catch(error => {
@@ -43,13 +46,27 @@ class VideoElement extends Object3D {
         }
       } );
   
-      video.addEventListener( 'play', function () {
-            //   video.pause();
-        });
+      // video.addEventListener( 'play', function () {
+      //       //   video.pause();
+      //   });
+
+      this.initShaka(video, url);
     }
 
     resize(width, height){
         this.container.set({ width: width, height: height });
+    }
+
+    initShaka(video, url){
+      shaka.polyfill.installAll();
+      const player = new shaka.Player(video);
+      window.player = player;      
+      var promise = player.load(url);
+      if (promise !== undefined) {
+        promise.then(_ => {
+          }).catch(error => {
+        });
+      }
     }
 }
 
