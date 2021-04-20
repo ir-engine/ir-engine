026df45d158720b18e01520bbba5566d87f3086b
Kyle Baran <kbaran@bitscoop.com>
2021-04-19 18:23:16 -0700

    Switched client config to be from production.json.
    Made new script to inline add NODE_CONFIG.publicRuntimeConfig to production.json config.
    config dependency should use that in production (still needs fixing).
    
    Added simple express server to client to serve up vite-built index.html for everything.

--- packages/client-core/src/feathers.ts
+++ packages/client-core/src/feathers.ts
@@ -2,7 +2,10 @@ import io from 'socket.io-client';
 import feathers from '@feathersjs/client';
 import { Config } from './helper';
 
+console.log('publicRuntimeConfig:');
+console.log(Config.publicRuntimeConfig);
 const apiServer = Config.publicRuntimeConfig.apiServer ?? (process.env.NODE_ENV === 'production' ? null : 'https://127.0.0.1:3030');
+console.log('apiServer:', apiServer);
 
 const feathersStoreKey: string = Config.publicRuntimeConfig.feathersStoreKey;
 const feathersClient: any = !Config.publicRuntimeConfig.offlineMode ? feathers() : undefined;
@@ -13,5 +16,7 @@ if(!Config.publicRuntimeConfig.offlineMode) {
     storageKey: feathersStoreKey
   }));
 }
+
+console.log(feathersClient);
 
 export const client = feathersClient;
