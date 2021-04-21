012973a5c4a5f6e6b9865fcce6e4973a8e8c3a6b
Hanzla Mateen <hanzlamateen@live.com>
2021-04-21 06:33:35 +0500

    Added parse mock method

--- packages/client-core/src/user/components/UserMenu/menus/ProfileMenu.tsx
+++ packages/client-core/src/user/components/UserMenu/menus/ProfileMenu.tsx
@@ -168,6 +168,19 @@ const ProfileMenu = (props: Props): any => {
 		// Use Credential Handler API to authenticate
 		const result: any = await navigator.credentials.get(didAuthQuery);
 		console.log(result);
+		const credentials: any = parseUserWalletCredentials(result);
+		console.log(credentials);
+	};
+
+	const parseUserWalletCredentials = (result) => {
+		return {
+			user: {
+				id: 'did:web:example.com',
+				displayName: 'alice',
+				icon: '<image data url of a sample user avatar icon goes here>',
+				// session // this will contain the access token and helper methods
+			}
+		};
 	};
 
 	return (
