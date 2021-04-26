f696d25e6623733a5a42f379a67be2f394d03592
Kyle Baran <kbaran@bitscoop.com>
2021-04-26 10:37:04 -0700

    Added dotenv-flow back to server-core appconfig.ts

--- packages/server-core/src/appconfig.ts
+++ packages/server-core/src/appconfig.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import appRootPath from 'app-root-path';
 import * as chargebeeInst from 'chargebee';
+import dotenv from 'dotenv-flow';
 // Load all the ENV variables from `.env`, then `.env.local`, into process.env
 import path from 'path';
 import url from 'url';
+
+if (process.env.KUBERNETES !== 'true') {
+  dotenv.config({
+    path: appRootPath.path
+  });
+}
 
 /**
  * Database
