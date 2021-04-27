aa4479d71076fa6878e99a5de6d207e69908492e
Kevin Roan <kevinnroan@gmail.com>
2021-04-26 20:06:32 -0700

    restructure ui & 360 video system

---
+++ packages/engine/src/ui/classes/UIPanelItem.ts
@@ -0,0 +1,14 @@
+import { Block, Text } from "../../assets/three-mesh-ui";
+import { Color } from "three";
+import { UIBaseElement } from "./UIBaseElement";
+
+export class UIPanelItem extends UIBaseElement {
+  constructor() {
+    super();
+    this.init();
+  }
+
+  init() {
+
+  }
+}
---
+++ packages/engine/src/video/systems/VideoSystem.ts
@@ -0,0 +1,15 @@
+import { System } from "../../ecs/classes/System";
+
+export class VideoSystem extends System {
+
+    constructor() {
+        super();
+    }
+
+    execute(delta, time): void {
+    }
+
+    System.queries = {
+                
+    };
+    
