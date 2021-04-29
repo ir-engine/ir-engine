475b51cc208e2955b675ed3564537eeee599643a
HexaField <joshfield999@gmail.com>
2021-04-29 15:13:43 +1000

    store ref to link wasm for client vite asset

--- packages/engine/src/physics/functions/loadPhysX.ts
+++ packages/engine/src/physics/functions/loadPhysX.ts
@@ -1,4 +1,6 @@
 import { receiveWorker } from "three-physx";
 import PHYSX from './physx.release.js';
+import PHYSXwasm from './physx.release.wasm';
+globalThis.PhysxWasmModule = PHYSXwasm; // make sure we link the asset
 
 PHYSX().then(receiveWorker);
