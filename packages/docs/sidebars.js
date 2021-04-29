5c30fa0fb0a00ce36f810e12a9ba2640835f3936
hanzlamateen <hanzlamateen@live.com>
2021-04-29 13:52:58 +0500

    Fixed nft package missing from docs

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -19,9 +19,9 @@ module.exports = {
               'client':  require('./sidebar/typedoc-client.js'),
               'server': require('./sidebar/typedoc-server.js'),
               'common': require('./sidebar/typedoc-common.js'),
-             // 'engine': require('./sidebar/typedoc-engine.js'),
+              // 'engine': require('./sidebar/typedoc-engine.js'),
               'gameserver': require('./sidebar/typedoc-gameserver.js'),
-              'nft': require('./sidebar/typedoc-nft.js'),
+              // 'nft': require('./sidebar/typedoc-nft.js'),
               'native-plugin-xr': require('./sidebar/typedoc-native-plugin-xr.js'),
               'server-core': require('./sidebar/typedoc-server-core.js'),
             }
