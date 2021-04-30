aa64623a85fb43e5c9f92be3c086d04692647968
Kyle Baran <kbaran@bitscoop.com>
2021-04-29 18:23:13 -0700

    server.js was still pointing to /dist instead of /www

--- packages/social/src/server.js
+++ packages/social/src/server.js
@@ -5,8 +5,8 @@ const packageRoot = require('app-root-path').path;
 const app = express();
 const PORT = process.env.HOST_PORT || 3000;
 
-app.use(express.static(path.join(packageRoot, 'packages', 'social', 'dist')));
+app.use(express.static(path.join(packageRoot, 'packages', 'social', 'www')));
 
-app.use('*', (req, res) => res.sendFile(path.join(packageRoot, 'packages', 'social', 'dist', 'index.html')));
+app.use('*', (req, res) => res.sendFile(path.join(packageRoot, 'packages', 'social', 'www', 'index.html')));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
