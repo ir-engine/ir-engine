a7b90071ea673d9351561df44d201af51f96da5f
shawticus <shawmakesmagic@gmail.com>
2021-05-02 15:03:06 -0700

    Only try commonjs transform on react-json-tree

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(() => {
   return {
     plugins: [
       commonjs({
-        include: /node_modules/
+        include: /react-json-tree/
       })
     ],
     server: {
