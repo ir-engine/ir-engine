4917e46cd2463886e638de1f1cf2358b44d94e56
Gheric Speiginer <gheric.speiginer@gmail.com>
2021-05-07 14:52:24 -0400

    Fix server w/ ClientStorage.ts

--- packages/engine/src/common/classes/ClientStorage.ts
+++ packages/engine/src/common/classes/ClientStorage.ts
@@ -1,23 +1,33 @@
-import { ImmortalDB } from 'immortal-db'
+import { isClient } from "../../common/functions/isClient";
 
 type StorageDatatypes = string | object | boolean | number;
 
 export class ClientStorage {
+
+  private static _db = (async () => {
+    if (!isClient) return new Map;
+    return (await import('immortal-db')).ImmortalDB;
+  })()
+
   static async get(key: string | number): Promise<StorageDatatypes> {
-    const value = await ImmortalDB.get(String(key));
+    const db = await this._db;
+    const value = db.get(String(key));
     try {
       return JSON.parse(value);
     } catch (e) {
       return value;
     }
   }
-  static async set(key: string | number, value: StorageDatatypes): Promise<string> {
+  static async set(key: string | number, value: StorageDatatypes): Promise<void> {
+    const db = await this._db;
     if(typeof value !== 'string') {
       value = JSON.stringify(value);
     }
-    return await ImmortalDB.set(String(key), value);
+    await db.set(String(key), value);
   }
   static async remove(key: string | number): Promise<void> {
-    return await ImmortalDB.remove(String(key));
+    const db = await this._db;
+    const sKey = String(key)
+    await (db instanceof Map ? db.delete(sKey) : db.remove(sKey))
   }
 }
