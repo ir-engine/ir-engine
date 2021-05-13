342d4d60dff83f23d71b6e85a9d98dd98c9921bd
HexaField <joshfield999@gmail.com>
2021-05-13 19:15:37 +1000

    fix dotenv for redis

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -100,7 +100,7 @@ if (config.server.enabled) {
 
     if (config.redis.enabled) {
       app.configure(sync({
-        uri: config.redis.password != null ? `redis://${config.redis.address}:${config.redis.port}?password=${config.redis.password}` : `redis://${config.redis.address}:${config.redis.port}`
+        uri: config.redis.password != null && config.redis.password !== '' ? `redis://${config.redis.address}:${config.redis.port}?password=${config.redis.password}` : `redis://${config.redis.address}:${config.redis.port}`
       }));
 
       (app as any).sync.ready.then(() => {
