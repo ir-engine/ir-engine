f5028237beb38f5d19a438a72f7a4adc84cdba15
aleksmaz <aleksmazur12@gmail.com>
2021-05-21 14:35:46 +0300

    fix

--- packages/client-core/src/socialmedia/components/WebXRPlugin/index.tsx
+++ packages/client-core/src/socialmedia/components/WebXRPlugin/index.tsx
@@ -321,7 +321,9 @@ export const WebXRPlugin = ({popupsState, updateNewFeedPageState, updateWebXRSta
                    console.log("filePath IS", filePath);
                    setSavedFilePath("file://" + filePath);
                    const videoPath = Capacitor.convertFileSrc(filePath);
-                   updateNewFeedPageState(true, videoPath)
+                   updateWebXRState(false);
+                   Plugins.XRPlugin.stop();
+                   updateNewFeedPageState(true, videoPath);
                }).catch(error => alert(error.message));
         }
 
