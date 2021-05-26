47e58e1a44883017725ad4527abd139827e6e3cd
Gleb512 <glebordinskijj@gmail.com>
2021-05-26 13:51:00 +0300

    File Revert

--- packages/client-core/src/socialmedia/components/ArMedia/index.tsx
+++ packages/client-core/src/socialmedia/components/ArMedia/index.tsx
@@ -72,14 +72,14 @@ const ArMedia = ({getArMedia, arMediaState, updateArMediaState, updateWebXRState
             </section>
         )}
       </section>
-
+      {!selectedItem ? null :
         <Button className={styles.startRecirding} onClick={() => {
           updateArMediaState(false);
-          updateWebXRState(true, null);
+          updateWebXRState(true, selectedItem.id);
         }} variant="contained">
           {t('social:arMedia.start')}
         </Button>
-
+      }
     </section>;
 };
 
