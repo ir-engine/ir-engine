4e4cad31d1a0c76b1398c36456e92a12fb878516
kimenyi <kimenyikevin@gmail.com>
2021-06-02 09:59:11 +0200

    remove unused code for user

--- packages/client-core/src/user/components/Dashboard/Dashboard.tsx
+++ packages/client-core/src/user/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -27,103 +27,14 @@ import {
     PersonAdd,
     PhotoAlbum,
     PhotoLibrary,
+    Settings,
     SupervisorAccount,
 } from '@material-ui/icons';
 import { Link } from "react-router-dom";
-import { deepOrange } from "@material-ui/core/colors";
 import Avatar from "@material-ui/core/Avatar";
 import { selectAuthState } from "../../reducers/auth/selector";
 import { connect } from 'react-redux';
-
-const drawerWidth = 200;
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-        },
-        appBar: {
-            zIndex: theme.zIndex.drawer + 1,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            backgroundColor: "#43484F"
-        },
-        appBarShift: {
-            marginLeft: drawerWidth,
-            width: `calc(100% - ${drawerWidth}px)`,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-        },
-        menuButton: {
-            marginRight: 36,
-            color: "white"
-        },
-        hide: {
-            display: 'none',
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-            whiteSpace: 'nowrap',
-        },
-        drawerOpen: {
-            width: `${drawerWidth}px !important`,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            backgroundColor: "#1f252d",
-        },
-        drawerClose: {
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            overflowX: 'hidden',
-            width: `${theme.spacing(7) + 1}px !important`,
-            [theme.breakpoints.up('sm')]: {
-                width: theme.spacing(9) + 1,
-            },
-            backgroundColor: "#1f252d",
-        },
-        toolbar: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            padding: theme.spacing(0, 1),
-            // necessary for content to be below app bar
-            ...theme.mixins.toolbar,
-        },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
-            backgroundColor: "#15171B",
-            minHeight: "100vh"
-        },
-        backdrop: {
-            zIndex: theme.zIndex.drawer + 1,
-            color: '#fff',
-        },
-        textLink: {
-            textDecoration: "none"
-        },
-        orange: {
-            color: theme.palette.getContrastText(deepOrange[500]),
-            backgroundColor: deepOrange[500]
-        },
-        marginLft: {
-            marginLeft: "10px"
-        },
-        avatarPosition: {
-           display: "flex",
-           marginLeft: "80%"
-        }
-    }),
-);
+import { useStyles } from "./styles";
 
 interface Props {
     children?: any,
@@ -148,7 +59,6 @@ const Dashboard = ({ children, authState }: Props) => {
     const classes = useStyles();
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
 	const { t } = useTranslation();
     const admin = authState.get("user");
     const isLoggedIn = authState.get("isLoggedIn");
@@ -303,6 +213,14 @@ const Dashboard = ({ children, authState }: Props) => {
                                 <PhotoLibrary style={{ color: "white" }} />
                             </ListItemIcon>
                             <ListItemText primary={t('user:dashboard.scenes')} />
+                        </ListItem>
+                    </Link>
+                    <Link to="/admin/bots" className={classes.textLink}>
+                        <ListItem style={{ color: "white" }} button>
+                            <ListItemIcon >
+                                <Settings style={{ color: "white" }} />
+                            </ListItemIcon>
+                            <ListItemText primary={t('user:dashboard.bots')} />
                         </ListItem>
                     </Link>
                 </List>
---
+++ packages/client/src/pages/admin/bot/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BotsCore from "@xrengine/client-core/src/admin/components/Bots"
+
+export default function Bots() {
+    return (<BotsCore/>);
+}
