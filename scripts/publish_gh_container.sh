241129e713c2ce8cb2818f81c177767e35b9743f
Kyle Baran <kbaran@bitscoop.com>
2021-06-29 14:29:50 -0700

    Tag image properly

--- scripts/publish_gh_container.sh
+++ scripts/publish_gh_container.sh
@@ -9,5 +9,5 @@ echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_USERNAME" --password-std
 
 echo ghcr.io/${GITHUB_USERNAME}/${LABEL}:${TAG}
 
-docker tag ${LABEL} ${LABEL}:${TAG}
+docker tag ${LABEL} ghcr.io/${GITHUB_USERNAME,,}/${LABEL}:${TAG}
 docker push ghcr.io/${GITHUB_USERNAME,,}/${LABEL}:${TAG}
