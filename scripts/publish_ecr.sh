a7a159347123ed282ce347d43b3895bc27f93d91
Kyle Baran <kbaran@bitscoop.com>
2021-06-30 16:47:24 -0700

    Get login password from ecr, not ecr-public

--- scripts/publish_ecr.sh
+++ scripts/publish_ecr.sh
@@ -6,7 +6,7 @@ STAGE=$1
 TAG=$2
 LABEL=$3
 
-aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
+aws ecr get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
 node ./scripts/prune_ecr_images.js --repoName $REPO_NAME
 
 docker tag $LABEL $ECR_URL/$REPO_NAME:$TAG
