a40c145072ad42033935b675a18704ab5bb74328
Yaroslav <yarikvfx@gmail.com>
2021-07-07 04:04:24 +0300

    spawn objects by client request  (#2335)
    * last goal do zero score and teleport on Tee-0
    
    * do spawn objects requires by client from game manager

---
+++ packages/engine/src/game/templates/Golf/behaviors/colourize.ts
@@ -0,0 +1,12 @@
+
+import { Behavior } from '../../../../common/interfaces/Behavior';
+import { Entity } from '../../../../ecs/classes/Entity';
+
+/**
+ * @author HydraFire <github.com/HydraFire>
+ */
+
+
+export const colourize: Behavior = (entityClub: Entity, args?: any, delta?: number, entityBall?: Entity, time?: number, checks?: any): void => {
+  // const game = getComponent(playerEntity, GamePlayer).game;
+}
