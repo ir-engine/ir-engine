7d82037bf320befd5c7a0e74003c2d14bc2613d0
HexaField <joshfield999@gmail.com>
2021-07-10 11:34:33 +1000

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+  root: true,
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   }
 }
