8e0fca257c0f53562b7134cc0f0fed4107c9987f
kimenyi kevin <kimenyikevin@gmail.com>
2021-07-13 20:51:39 +0200

    lint fix

--- packages/client-core/src/admin/reducers/admin/location/service.ts
+++ packages/client-core/src/admin/reducers/admin/location/service.ts
@@ -18,14 +18,14 @@ export function fetchLocationTypes() {
 
 export function patchLocation(id: string, location: any) {
   return async (dispatch: Dispatch): Promise<any> => {
-    try {      
+    try {
       const result = await client.service('location').patch(id, location)
-      dispatch(locationPatched(result));
+      dispatch(locationPatched(result))
     } catch (err) {
-      console.error(err);
-      dispatchAlertError(dispatch, err.message);
+      console.error(err)
+      dispatchAlertError(dispatch, err.message)
     }
-  };
+  }
 }
 
 export function removeLocation(id: string) {
