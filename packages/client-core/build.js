3e31f43ab765a0a4dbf94dd48ae8322c93ead8ac
Hanzla Mateen <hanzlamateen@live.com>
2021-07-15 12:29:57 +0500

    Changes for index meta url

--- packages/client-core/build.js
+++ packages/client-core/build.js
@@ -1,11 +1,16 @@
 const esbuild = require('esbuild');
 const sassPlugin = require('esbuild-plugin-sass')
+const metaUrlPlugin = require('@chialab/esbuild-plugin-meta-url')
 
 esbuild.build({
     entryPoints: ['index.ts'],
+    target: 'esnext',
     bundle: true,
     outfile: 'dist/client-core.es.js',
-    plugins: [sassPlugin()],
+    plugins: [
+        sassPlugin(),
+        metaUrlPlugin()
+    ],
     platform: "browser",
     define: {
         ["process.env.NODE_ENV"]: "'production'",
