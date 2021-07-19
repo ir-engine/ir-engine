b60941058bfc923c6cd6a862ae689cb60b91c9d5
HexaField <joshfield999@gmail.com>
2021-07-19 14:01:57 +1000

    entity input system now has a map instead of a schema

--- packages/engine/src/game/templates/Golf/behaviors/setupPlayerInput.ts
+++ packages/engine/src/game/templates/Golf/behaviors/setupPlayerInput.ts
@@ -27,21 +27,15 @@ export const setupPlayerInput = (entityPlayer: Entity) => {
   // override the default mapping and behavior of input schema and interact
   inputs.schema.inputMap.set('k', GolfInput.TELEPORT)
   inputs.schema.inputMap.set(GamepadButtons.A, GolfInput.TELEPORT)
-  inputs.schema.inputButtonBehaviors[GolfInput.TELEPORT] = {
-    started: [
-      {
-        behavior: (entity: Entity) => {
-          const { gameName, ownedObjects } = getComponent(entity, GamePlayer)
-          const game = getGameFromName(gameName)
-          const ballEntity = ownedObjects['GolfBall']
-          const ballTransform = getComponent(ballEntity, TransformComponent)
-          const position = ballTransform.position
-          console.log('teleporting to', position.x, position.y, position.z)
-          teleportPlayer(entity, position, new Quaternion())
-        }
-      }
-    ]
-  }
+  inputs.schema.behaviorMap.set(GolfInput.TELEPORT, (entity: Entity) => {
+    const { gameName, ownedObjects } = getComponent(entity, GamePlayer)
+    const game = getGameFromName(gameName)
+    const ballEntity = ownedObjects['GolfBall']
+    const ballTransform = getComponent(ballEntity, TransformComponent)
+    const position = ballTransform.position
+    console.log('teleporting to', position.x, position.y, position.z)
+    teleportPlayer(entity, position, new Quaternion())
+  })
 
   // DEBUG STUFF
   if (isDev) {
@@ -49,57 +43,45 @@ export const setupPlayerInput = (entityPlayer: Entity) => {
     const teleportballOut = 140
 
     inputs.schema.inputMap.set('h', teleportballkey)
-    inputs.schema.inputButtonBehaviors[teleportballkey] = {
-      started: [
-        {
-          behavior: (entity: Entity) => {
-            const { gameName, ownedObjects } = getComponent(entity, GamePlayer)
-            addStateComponent(entity, State.Waiting)
-            const game = getGameFromName(gameName)
+    inputs.schema.behaviorMap.set(teleportballkey, (entity: Entity) => {
+      const { gameName, ownedObjects } = getComponent(entity, GamePlayer)
+      addStateComponent(entity, State.Waiting)
+      const game = getGameFromName(gameName)
 
-            const gameScore = getStorage(entity, { name: 'GameScore' })
-            const holeEntity = game.gameObjects['GolfHole'][gameScore.score.goal]
-            const ballEntity = ownedObjects['GolfBall']
-            const position = new Vector3().copy(getComponent(holeEntity, TransformComponent).position)
-            position.y += 0.5
-            teleportObject(ballEntity, { position })
-          }
-        }
-      ]
-    }
+      const gameScore = getStorage(entity, { name: 'GameScore' })
+      const holeEntity = game.gameObjects['GolfHole'][gameScore.score.goal]
+      const ballEntity = ownedObjects['GolfBall']
+      const position = new Vector3().copy(getComponent(holeEntity, TransformComponent).position)
+      position.y += 0.5
+      teleportObject(ballEntity, { position })
+    })
 
     inputs.schema.inputMap.set('b', teleportballOut)
-    inputs.schema.inputButtonBehaviors[teleportballOut] = {
-      started: [
-        {
-          behavior: (entity: Entity) => {
-            const { ownedObjects } = getComponent(entity, GamePlayer)
-            const ballEntity = ownedObjects['GolfBall']
-            const collider = getMutableComponent(ballEntity, ColliderComponent)
-            collider.velocity.set(0, 0, 0)
-            collider.body.updateTransform({
-              translation: {
-                x: 0,
-                y: 10,
-                z: 0
-              },
-              rotation: {
-                x: 0,
-                y: 0,
-                z: 0,
-                w: 1
-              },
-              linearVelocity: {
-                x: 0,
-                y: 0,
-                z: 0
-              }
-            })
-            collider.body.setLinearVelocity(new Vector3(), true)
-            collider.body.setAngularVelocity(new Vector3(), true)
-          }
+    inputs.schema.behaviorMap.set(teleportballOut, (entity: Entity) => {
+      const { ownedObjects } = getComponent(entity, GamePlayer)
+      const ballEntity = ownedObjects['GolfBall']
+      const collider = getMutableComponent(ballEntity, ColliderComponent)
+      collider.velocity.set(0, 0, 0)
+      collider.body.updateTransform({
+        translation: {
+          x: 0,
+          y: 10,
+          z: 0
+        },
+        rotation: {
+          x: 0,
+          y: 0,
+          z: 0,
+          w: 1
+        },
+        linearVelocity: {
+          x: 0,
+          y: 0,
+          z: 0
         }
-      ]
-    }
+      })
+      collider.body.setLinearVelocity(new Vector3(), true)
+      collider.body.setAngularVelocity(new Vector3(), true)
+    })
   }
 }
