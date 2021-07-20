ed07511b6e6c6513e7f5aeb1e4aa024dc08b4715
HexaField <joshfield999@gmail.com>
2021-07-20 15:49:28 +1000

    fix club not showing up bug

--- packages/engine/src/game/templates/Golf/prefab/GolfClubPrefab.ts
+++ packages/engine/src/game/templates/Golf/prefab/GolfClubPrefab.ts
@@ -119,10 +119,11 @@ export const updateClub: Behavior = (
   const ownerNetworkId = getComponent(entityClub, NetworkObjectOwner).networkId
   const ownerEntity = Network.instance.networkObjects[ownerNetworkId].component.entity
 
-  if (!ownerEntity) return
+  // if (!ownerEntity) return
 
   const golfClubComponent = getMutableComponent(entityClub, GolfClubComponent)
-  if (!golfClubComponent.raycast) return
+  // if (!golfClubComponent.raycast) return
+
   const transformClub = getMutableComponent(entityClub, TransformComponent)
   const collider = getMutableComponent(entityClub, ColliderComponent)
 
@@ -235,7 +236,7 @@ export const initializeGolfClub = (entityClub: Entity) => {
   const ownerNetworkId = getComponent(entityClub, NetworkObjectOwner).networkId
   const ownerEntity = Network.instance.networkObjects[ownerNetworkId].component.entity
   const ownerPlayerNumber = Number(getComponent(ownerEntity, GamePlayer).role.substr(0, 1)) - 1
-  console.log(getComponent(ownerEntity, GamePlayer).role)
+
   const color = GolfColours[ownerPlayerNumber]
 
   golfClubComponent.raycast = PhysicsSystem.instance.addRaycastQuery(
