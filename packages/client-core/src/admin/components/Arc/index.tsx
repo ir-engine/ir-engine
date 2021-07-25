3106b181b79a007eddf921f17512889d2630d177
kimenyi kevin <kimenyikevin@gmail.com>
2021-07-25 18:00:56 +0200

    implement armedia update

--- packages/client-core/src/admin/components/Arc/index.tsx
+++ packages/client-core/src/admin/components/Arc/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import { bindActionCreators, Dispatch } from 'redux'
