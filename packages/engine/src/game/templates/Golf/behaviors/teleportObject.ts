abb7e68d301adb475da250ea2c0d5b4c0b244f5f
HydraFire <yarikvfx@gmail.com>
2021-07-25 21:18:28 +0300

    fix three bugs

--- packages/engine/src/game/templates/Golf/behaviors/teleportObject.ts
+++ packages/engine/src/game/templates/Golf/behaviors/teleportObject.ts
@@ -4,12 +4,8 @@ import { Behavior } from '../../../../common/interfaces/Behavior'
 import { Entity } from '../../../../ecs/classes/Entity'
 import { addComponent, getComponent, getMutableComponent, hasComponent, removeComponent } from '../../../../ecs/functions/EntityFunctions'
 import { ColliderComponent } from '../../../../physics/components/ColliderComponent'
-import { TransformComponent } from '../../../../transform/components/TransformComponent'
-import { GamePlayer } from '../../../components/GamePlayer'
-import { getGame, getTargetEntity } from '../../../functions/functions'
-import { removeStateComponent } from '../../../functions/functionsState'
-import { getStorage } from '../../../functions/functionsStorage'
-import { State } from '../../../types/GameComponents'
+import { findInterpolationSnapshot } from '../../../../physics/behaviors/findInterpolationSnapshot'
+import { Network } from '../../../../networking/classes/Network'
 /**
  * @author HydraFire <github.com/HydraFire>
  */
@@ -24,7 +20,7 @@ export const teleportObject: Behavior = (
 ): void => {
   const collider = getMutableComponent(entity, ColliderComponent)
   if(!collider)return;
-  isClient ? addComponent(entity, State.TeleportBall):'';
+  //isClient ? addComponent(entity, State.TeleportBall):'';
 
   collider.velocity.set(0, 0, 0)
 
@@ -68,10 +64,40 @@ export const removeVelocity: Behavior = (
   //collider.velocity.set(0,0,0);
   collider.body.setLinearDamping(10)
   collider.body.setAngularDamping(10)
-
+/*
   if (isClient && hasComponent(entity, State.TeleportBall)) {
     removeComponent(entity, State.TeleportBall);
   }
+  */
   //collider.body.setLinearVelocity(new Vector3(), true);
   //collider.body.setAngularVelocity(new Vector3(), true);
+}
+
+export const updateColliderPosition: Behavior = (
+  entity: Entity,
+  args?: any,
+  delta?: number,
+  entityTarget?: Entity,
+  time?: number,
+  checks?: any
+): void => {
+  console.log("CORRECT BALL FROM SERVER");
+  
+  const collider = getMutableComponent(entity, ColliderComponent)
+  const currentSnapshot = findInterpolationSnapshot(entity, Network.instance.snapshot)
+  if(currentSnapshot === undefined || collider === undefined ) return;
+
+  collider?.body.updateTransform({
+    translation: {
+      x: currentSnapshot.x,
+      y: currentSnapshot.y,
+      z: currentSnapshot.z
+    },
+    rotation: {
+      x: currentSnapshot.qX,
+      y: currentSnapshot.qY,
+      z: currentSnapshot.qZ,
+      w: currentSnapshot.qW
+    }
+  })
 }
