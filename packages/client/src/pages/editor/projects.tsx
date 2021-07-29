ae10310a65c866337e9a4bceb94bd8f47137dd5b
kimenyi kevin <kimenyikevin@gmail.com>
2021-07-29 22:50:00 +0200

    refactor project editor

--- packages/client/src/pages/editor/projects.tsx
+++ packages/client/src/pages/editor/projects.tsx
@@ -21,133 +21,24 @@ import {
   ProjectGridHeaderRow
 } from '@xrengine/client-core/src/world/components/editor/projects/ProjectGrid'
 import templates from '@xrengine/client-core/src/world/components/editor/projects/templates'
-import { ThemeContext } from '@xrengine/client-core/src/world/components/editor/theme'
 import { useHistory } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
-import styled from 'styled-components'
-import AdminConsole from '@xrengine/client-core/src/admin/components'
-
-/**
- * Creating styled component using section.
- * Used as a parent container in view.
- * @ProjectsSection
- *
- */
-
-export const ProjectsSection = (styled as any).section<{ flex?: number }>`
-	  padding-bottom: 100px;
-	  display: flex;
-	  flex: ${(props) => (props.flex === undefined ? 1 : props.flex)};
-
-	  &:first-child {
-	    padding-top: 100px;
-	  }
-
-	  h1 {
-	    font-size: 36px;
-	  }
-
-	  h2 {
-	    font-size: 16px;
-	  }
-`
-
-/**
- * Creating styled component using div.
- * Used to contain ProjectsHeader and ProjectGridContainer.
- * @ProjectsContainer
- *
- */
-
-export const ProjectsContainer = (styled as any).div`
-	  display: flex;
-	  flex: 1;
-	  flex-direction: column;
-	  margin: 0 auto;
-	  max-width: 1200px;
-	  padding: 0 20px;
-`
-
-/**
- * Creating styled component using section inheriting {ProjectsContainer}.
- * Used when user is newly onboard and has no existing projects.
- * @ProjectsContainer
- * @WelcomeContainer
- */
-const WelcomeContainer = styled(ProjectsContainer)`
-  align-items: center;
-  & > * {
-    text-align: center;
-  }
-  & > *:not(:first-child) {
-    margin-top: 20px;
-  }
-  h2 {
-    max-width: 480px;
-  }
-`
-
-/**
- * Creating styled component using div.
- * Used to show the projects page header content.
- * @ProjectsHeader
- *
- */
-export const ProjectsHeader = (styled as any).div`
-	  margin-bottom: 36px;
-	  display: flex;
-	  justify-content: space-between;
-	  align-items: center;
-`
-
-export const ProfileButton = (styled as any).div`
-    box-sizing: border-box;
-    border-radius: 50%;
-    margin: auto 10px;
-    cursor: pointer;
-    height: 40px;
-    width: 40px;
-    &.on {
-        .offIcon{
-            display: none;
-        }
-        .onIcon{
-            display: block;
-        }
-    }
-    &.off{
-        .offIcon{
-            display: block;
-        }
-        .onIcon{
-            display: none;
-        }
-    }
-
-
-    .onIcon{
-        color: rgba(122, 255, 100, 1)
-    }
-
-    > svg{
-        height: 1.2em;
-        width: 1.2em;
-        box-sizing: border-box;
-        cursor: pointer;
-        margin: 6px;
-    }
-    background-color: rgb(50, 170, 75);
-    color: white;
-    margin-right: 5px;
-
-    &:hover {
-        cursor: pointer;
-        background-color: rgb(70, 201, 97);
-    }
-`
+import LocationAdmin from '@xrengine/client-core/src/admin/components/Location'
+import {
+  TabPanel,
+  tapId,
+  useStyles,
+  ProjectsContainer,
+  ProjectsHeader,
+  WelcomeContainer,
+  ProjectsSection,
+  ProfileButton
+} from './projectUtility'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 /**
  *Defining contextMenuId for rendering menus.
@@ -169,20 +60,6 @@ type Props = {
   doLoginAuto?: any
   logoutUser?: typeof logoutUser
 }
-/**
- *Creating type ProjectsPageState.
- */
-type ProjectsPageState = { projects: any } & {
-  error: any
-  loading: boolean
-} & ((error: any) => any) & {
-    projects: any[]
-    loading: any
-    isAuthenticated: any
-    error: null
-    authUser: any
-    user: any
-  }
 
 /**
  * function to get authState.
@@ -215,7 +92,9 @@ const ProjectsPage = (props: Props) => {
   const { api, authState, doLoginAuto, logoutUser } = props
 
   const router = useHistory()
+  const classes = useStyles()
 
+  const [value, setValue] = React.useState(0)
   const [projects, setProjects] = useState([]) // constant projects initialized with an empty array.
   const [loading, setLoading] = useState(false) // constant loading initialized with false.
   const isAuthenticated = api.isAuthenticated() // intialized with value returning from api.isAuthenticated()
@@ -231,6 +110,10 @@ const ProjectsPage = (props: Props) => {
     console.log(authState)
     // We dont need to load projects if the user isn't logged in
   }, [])
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue)
+  }
 
   useEffect(() => {
     if (authUser?.accessToken != null && authUser.accessToken.length > 0 && user?.id != null) {
@@ -257,8 +140,6 @@ const ProjectsPage = (props: Props) => {
         })
     }
   }, [authUser, user])
-
-  const contextType = ThemeContext
 
   /**
    *function to delete project
@@ -344,56 +225,71 @@ const ProjectsPage = (props: Props) => {
           </ProjectGridHeaderRow>
         </ProjectGridHeader>
       )}
-      {authUser?.accessToken != null && authUser.accessToken.length > 0 && user?.id != null && (
-        <main>
-          {projects.length === 0 && !loading ? (
-            <ProjectsSection flex={0}>
-              <WelcomeContainer>
-                <h1>{t('editor.projects.welcomeMsg')}</h1>
-                <h2>{t('editor.projects.description')}</h2>
-                <MediumButton onClick={routeTo('/editor/tutorial')}>
-                  {t('editor.projects.lbl-startTutorial')}
-                </MediumButton>
-              </WelcomeContainer>
-            </ProjectsSection>
-          ) : null}
+
+      <div className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          aria-label="scrollable auto tabs example"
+          orientation="vertical"
+          className={classes.tabs}
+          classes={{ indicator: classes.indicator }}
+        >
+          <Tab label={t('editor.projects.projectHeader')} {...tapId(0)} />
+          <Tab label={t('editor.projects.locationHeader')} {...tapId(1)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          {authUser?.accessToken != null && authUser.accessToken.length > 0 && user?.id != null && (
+            <main>
+              {projects.length === 0 && !loading ? (
+                <ProjectsSection flex={0}>
+                  <WelcomeContainer>
+                    <h1>{t('editor.projects.welcomeMsg')}</h1>
+                    <h2>{t('editor.projects.description')}</h2>
+                    <MediumButton onClick={routeTo('/editor/tutorial')}>
+                      {t('editor.projects.lbl-startTutorial')}
+                    </MediumButton>
+                  </WelcomeContainer>
+                </ProjectsSection>
+              ) : null}
+              <ProjectsSection>
+                <ProjectsContainer>
+                  <ProjectGridContainer>
+                    <ProjectGridHeader>
+                      <ProjectGridHeaderRow />
+                      <ProjectGridHeaderRow>
+                        <Button onClick={routeTo('/editor/create')}>{t('editor.projects.lbl-newProject')}</Button>
+                      </ProjectGridHeaderRow>
+                    </ProjectGridHeader>
+                    <ProjectGridContent>
+                      {error && <ErrorMessage>{(error as any).message}</ErrorMessage>}
+                      {!error && (
+                        <ProjectGrid
+                          loading={loading}
+                          projects={projects}
+                          // newProjectPath="/editor/templates"
+                          newProjectPath="/editor/create"
+                          newProjectLabel={t('editor.projects.lbl-newProject')}
+                          contextMenuId={contextMenuId}
+                        />
+                      )}
+                    </ProjectGridContent>
+                  </ProjectGridContainer>
+                </ProjectsContainer>
+              </ProjectsSection>
+              <ProjectContextMenu />
+            </main>
+          )}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
           <ProjectsSection>
             <ProjectsContainer>
-              <ProjectsHeader>
-                <h1>{t('editor.projects.locationHeader')}</h1>
-              </ProjectsHeader>
-              <div className="mb-5">
-                <AdminConsole />
-              </div>
-              <ProjectsHeader>
-                <h1> {t('editor.projects.projectHeader')}</h1>
-              </ProjectsHeader>
-              <ProjectGridContainer>
-                <ProjectGridHeader>
-                  <ProjectGridHeaderRow />
-                  <ProjectGridHeaderRow>
-                    <Button onClick={routeTo('/editor/create')}>{t('editor.projects.lbl-newProject')}</Button>
-                  </ProjectGridHeaderRow>
-                </ProjectGridHeader>
-                <ProjectGridContent>
-                  {error && <ErrorMessage>{(error as any).message}</ErrorMessage>}
-                  {!error && (
-                    <ProjectGrid
-                      loading={loading}
-                      projects={projects}
-                      // newProjectPath="/editor/templates"
-                      newProjectPath="/editor/create"
-                      newProjectLabel={t('editor.projects.lbl-newProject')}
-                      contextMenuId={contextMenuId}
-                    />
-                  )}
-                </ProjectGridContent>
-              </ProjectGridContainer>
+              <LocationAdmin />
             </ProjectsContainer>
           </ProjectsSection>
-          <ProjectContextMenu />
-        </main>
-      )}
+        </TabPanel>
+      </div>
 
       {profileMenuOpen && (
         <ClickAwayListener onClickAway={() => setProfileMenuOpen(false)}>
