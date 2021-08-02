66fdbb16388e6838f92b865ace8657930962e9ca
aleksmaz <62301593+aleksmaz@users.noreply.github.com>
2021-08-02 23:14:20 +0300

    Update createMedia.ts

--- packages/engine/src/scene/behaviors/createMedia.ts
+++ packages/engine/src/scene/behaviors/createMedia.ts
@@ -8,7 +8,6 @@ import { addComponent, getComponent, getMutableComponent } from '../../ecs/funct
 import Video from '../classes/Video'
 import MediaComponent from '../components/MediaComponent'
 import AudioSource from '../classes/AudioSource'
-import CommonSlider from '../../../../client-core/src/common/components/Slider'
 import { PositionalAudioComponent } from '../../audio/components/PositionalAudioComponent'
 import { applyAudioMediaSettings } from './handleAudioSettings'
 
