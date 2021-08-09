6f1a5aa5959a71df0ada70be33684f9c8dffd5b6
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-08-09 11:54:12 -0700

    MAP-19: update map in editor after changing lat/long

--- packages/engine/src/editor/nodes/MapNode.ts
+++ packages/engine/src/editor/nodes/MapNode.ts
@@ -2,6 +2,7 @@ import { Mesh, Object3D, BoxBufferGeometry, Material } from 'three'
 import { createBuildings, createRoads, createGround, safelySetGroundScaleAndPosition } from '../../map/MeshBuilder'
 import { fetchVectorTiles, fetchRasterTiles } from '../../map/MapBoxClient'
 import EditorNodeMixin from './EditorNodeMixin'
+import { debounce } from 'lodash'
 
 export default class MapNode extends EditorNodeMixin(Object3D) {
   static legacyComponentName = 'map'
@@ -70,11 +71,20 @@ export default class MapNode extends EditorNodeMixin(Object3D) {
   async refreshGroundLayer() {
     const center = this.getStartLongLat()
     const rasterTiles = this.showRasterTiles ? await fetchRasterTiles(center) : []
+    this.mapLayers.ground.removeFromParent()
     this.mapLayers.ground = createGround(rasterTiles, center[1])
     this.applyScale(this.mapLayers.ground)
     safelySetGroundScaleAndPosition(this.mapLayers.ground, this.mapLayers.building)
     this.add(this.mapLayers.ground)
   }
+
+  debounceAndRefreshAllLayers = debounce(() => {
+    Object.values(this.mapLayers).forEach((layer) => {
+      layer.removeFromParent()
+    })
+    this.addMap(this.editor)
+  }, 3000)
+
   copy(source, recursive = true) {
     super.copy(source, recursive)
     return this
@@ -82,8 +92,9 @@ export default class MapNode extends EditorNodeMixin(Object3D) {
   onChange(prop?: string) {
     if (prop) {
       if (prop === 'showRasterTiles') {
-        this.mapLayers.ground.removeFromParent()
         this.refreshGroundLayer()
+      } else if (prop === 'startLatitude' || prop === 'startLongitude') {
+        this.debounceAndRefreshAllLayers()
       }
     } else {
       this.addMap(this.editor)
