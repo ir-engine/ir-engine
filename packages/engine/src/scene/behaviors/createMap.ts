64f3445744fec13b811acb5cfb86fd800446cb63
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-08-12 12:45:06 -0700

    MAP-1: clean up

--- packages/engine/src/scene/behaviors/createMap.ts
+++ packages/engine/src/scene/behaviors/createMap.ts
@@ -5,20 +5,16 @@ import { addComponent } from '../../ecs/functions/EntityFunctions'
 import { NavMeshComponent } from '../../navigation/component/NavMeshComponent'
 import { DebugNavMeshComponent } from '../../debug/DebugNavMeshComponent'
 import { Object3DComponent } from '../components/Object3DComponent'
-import { Mesh, MeshLambertMaterial, SphereGeometry } from 'three'
+import { Entity } from '../../ecs/classes/Entity'
 
-export async function createMap(entity, args: MapProps): Promise<void> {
-  const { mapMesh, navMesh, groundMesh, roadsMesh } = await create(Engine.renderer, args)
-  console.log('mapMesh', mapMesh)
+export async function createMap(entity: Entity, args: MapProps): Promise<void> {
+  const { mapMesh, navMesh, groundMesh } = await create(Engine.renderer, args)
   addComponent(entity, Object3DComponent, {
     value: mapMesh
   })
-  console.log('map added, navmesh?', navMesh)
-  // if (navMesh) {
   addComponent(entity, NavMeshComponent, {
     yukaNavMesh: navMesh,
     navTarget: groundMesh
   })
   addComponent(entity, DebugNavMeshComponent, null)
-  // }
 }
