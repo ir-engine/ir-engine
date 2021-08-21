cce1f5dda21308916458c4838c30eaf28ef310ec
HexaField <joshfield999@gmail.com>
2021-08-21 11:52:31 +1000

    add more logging and try / catch

--- tests/core/core.test.ts
+++ tests/core/core.test.ts
@@ -13,25 +13,40 @@ const sqrt2 = Math.sqrt(2)
 describe('My Bot Tests', () => {
 
   beforeAll(async () => {
-    await bot.launchBrowser()
-    await bot.enterLocation(`https://${domain}/location/${locationName}`)
-    await bot.awaitHookPromise(BotHooks.LocationLoaded)
-    await bot.runHook(BotHooks.InitializeBot)
-  }, maxTimeout)
+    try {
+      await bot.launchBrowser()
+      await bot.enterLocation(`https://${domain}/location/${locationName}`)
+      console.log('entered location')
+      await bot.awaitHookPromise(BotHooks.LocationLoaded)
+      console.log('location loaded')
+      await bot.runHook(BotHooks.InitializeBot)
+      console.log('bot initalised')
+    } catch (e) {
+      console.error(e)
+    }
+  }, 10 * maxTimeout)
 
   afterAll(async () => {
-    console.log('Tests finished, bot is now quitting!')
-    await bot.delay(1500)
-    await bot.quit()
+    try {
+      console.log('Tests finished, bot is now quitting!')
+      await bot.delay(1500)
+      await bot.quit()
+    } catch (e) {
+      console.error(e)
+    }
   }, maxTimeout)
 
   test('Can spawn in the world', async () => {
-    console.log('Running spawn in world test...')
-    await bot.delay(1000)
-    expect(
-      vector3.copy(await bot.runHook(BotHooks.GetPlayerPosition)).length()
-    ).toBeLessThan(sqrt2 * 2) // sqrt2 * 2 is the default size of our spawn area
-    console.log('Ran spawn in world test!')
+    try {
+      console.log('Running spawn in world test...')
+      await bot.delay(1000)
+      expect(
+        vector3.copy(await bot.runHook(BotHooks.GetPlayerPosition)).length()
+      ).toBeLessThan(sqrt2 * 2) // sqrt2 * 2 is the default size of our spawn area
+      console.log('Ran spawn in world test!')
+    } catch (e) {
+      console.error(e)
+    }
   })
 
 })
