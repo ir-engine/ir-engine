1a9cfb9f4759d93b42e64db661e4820ee762abad
Patrick <pscale01@gmail.com>
2021-08-22 20:29:39 -0700

    wip

--- packages/engine/src/map/GeoLabelNode.ts
+++ packages/engine/src/map/GeoLabelNode.ts
@@ -1,18 +1,28 @@
-import { Feature, Position } from 'geojson'
+import { Feature, LineString, Position } from 'geojson'
 import { Object3D, Matrix4, Vector3 } from 'three'
 import { Text } from 'troika-three-text'
-import { length, along } from '@turf/turf'
+import { length, along, nearestPointOnLine } from '@turf/turf'
 
 export class GeoLabelNode extends Object3D {
-  geoFeature: Feature
+  geoFeature: Feature<LineString>
+  transformGeoPosition: (position: Position) => Position
 
-  constructor(feature: Feature, transformPosition: (position: Position) => Position) {
+  constructor(feature: Feature<LineString>, transformGeoPosition: (position: Position) => Position) {
     super()
     this.geoFeature = feature
+    this.transformGeoPosition = transformGeoPosition
+    // this.geoFeature = {
+    //   ...feature,
+    //   geometry: {
+    //     ...feature.geometry,
+    //     coordinates: feature.geometry.coordinates.map(transformGeoPosition)
+    //   }
+    // }
 
     const text = new Text()
 
-    const point = transformPosition(along(feature as any, length(feature) / 2).geometry.coordinates)
+    const point = transformGeoPosition(along(this.geoFeature, length(this.geoFeature) / 2).geometry.coordinates)
+    // const point = along(this.geoFeature, length(this.geoFeature) / 2).geometry.coordinates
 
     text.text = feature.properties.name
     text.fontSize = 6
@@ -33,6 +43,13 @@ export class GeoLabelNode extends Object3D {
   }
   update(cameraOrientation: Matrix4, cameraPosition: Vector3) {
     this.quaternion.setFromRotationMatrix(cameraOrientation)
-    this.visible = this.position.distanceTo(cameraPosition) < 200
+    // this.visible = this.position.distanceTo(cameraPosition) < 200
+
+    const pointOnLine = this.transformGeoPosition(
+      nearestPointOnLine(this.geoFeature, [cameraPosition.x, cameraPosition.z]).geometry.coordinates
+    )
+
+    this.position.x = pointOnLine[0]
+    this.position.z = pointOnLine[1]
   }
 }
