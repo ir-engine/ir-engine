362425a7e28436f5adbe8970241e066637278f75
Kyle Baran <kbaran@bitscoop.com>
2021-08-23 17:51:56 -0700

    Reverting ecr login

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,9 +4,8 @@ set -x
 
 STAGE=$1
 LABEL=$2
-REGION=$3
 
-aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
+aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
 
 docker pull $ECR_URL/$REPO_NAME:latest_$STAGE || true
 docker build --cache-from $ECR_URL/$REPO_NAME:latest_$STAGE --tag $LABEL .
