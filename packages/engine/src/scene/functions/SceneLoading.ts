75ddc59ccd2d8146cacd9819a137b380024d0cec
Mohsen Heydari <mohsenheydari@live.com>
2021-08-26 10:06:56 +0200

    Interior mapping (#2960)
    * Added interior mapping node
    
    * Added DDS loader
    
    * Fixed cube axis issue

--- packages/engine/src/scene/functions/SceneLoading.ts
+++ packages/engine/src/scene/functions/SceneLoading.ts
@@ -45,6 +45,7 @@ import { Water } from '../classes/Water'
 import { TransformComponent } from '../../transform/components/TransformComponent'
 import { Object3DComponent } from '../components/Object3DComponent'
 import { UserdataComponent } from '../components/UserdataComponent'
+import { Interior } from '../classes/Interior'
 
 export enum SCENE_ASSET_TYPES {
   ENVMAP
@@ -329,6 +330,10 @@ export class WorldScene {
       case 'water':
         isClient && addObject3DComponent(entity, new Water(), component.data)
         isClient && addComponent(entity, UpdatableComponent, {})
+        break
+
+      case 'interior':
+        isClient && addObject3DComponent(entity, new Interior(), component.data)
         break
 
       case 'postprocessing':
