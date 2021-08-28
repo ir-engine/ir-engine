5ae4a68cf650846d51b0f174f10666556f77ae1f
xiani <xianionline@gmail.com>
2021-08-28 09:24:25 +0300

    test is wrong

--- packages/engine/tests/ikrig/misc.test.ts
+++ packages/engine/tests/ikrig/misc.test.ts
@@ -1,48 +1,31 @@
 import { Quaternion, Vector3 } from 'three'
 import { FORWARD, UP } from '../../src/ikrig/constants/Vector3Constants'
+import { applySwingAndTwist, computeSwingAndTwist } from '../../src/ikrig/functions/IKFunctions'
 
 test('twist swing', () => {
-  const qOriginal = new Quaternion(-0.4829629063606262, 0.2999502122402191, -0.129409521818161, 0.8124222159385681)
-  const qTarget = new Quaternion()
-  const originalForward = FORWARD.clone().applyQuaternion(qOriginal)
+  // const qPose = new Quaternion(-0.4829629063606262, 0.2999502122402191, -0.129409521818161, 0.8124222159385681)
+  // const qTPose = new Quaternion()
+  // current frame quaternion
+  const qATPose = new Quaternion(-0.06047876164010382, 1.2979864261117819e-8, 4.4180032320538633e-7, 0.9981694843012805)
+  const qAPose = new Quaternion(-0.049234336596575404, -0.02433079944441424, -0.0014453285286276222, 0.9984898113270149)
+  // tpose/zero frame quaternion
+  const qTPose = new Quaternion(-0.06047876164010382, 1.2979864261117819e-8, 4.4180032320538633e-7, 0.9981694843012805)
 
-  const st = computeSwingAndTwist(qTarget, qOriginal, FORWARD, UP)
+  const qTPoseInvert = qTPose.clone().invert()
+  const altForward = FORWARD.clone().applyQuaternion(qTPoseInvert)
+  const altUp = UP.clone().applyQuaternion(qTPoseInvert)
+  //const originalForward = FORWARD.clone().applyQuaternion(qPose)
+
+  const st = computeSwingAndTwist(qAPose, qATPose, FORWARD, UP)
 
   const quaternionFromSwingTwist = new Quaternion()
-  apply(quaternionFromSwingTwist, st.swing, st.twist, originalForward)
+  applySwingAndTwist(quaternionFromSwingTwist, st.swing, st.twist, altForward)
 
-  expect(quaternionFromSwingTwist.x).toBeCloseTo(qOriginal.x, 4)
-  expect(quaternionFromSwingTwist.y).toBeCloseTo(qOriginal.y, 4)
-  expect(quaternionFromSwingTwist.z).toBeCloseTo(qOriginal.z, 4)
-  expect(quaternionFromSwingTwist.w).toBeCloseTo(qOriginal.w, 4)
+  console.log('h', quaternionFromSwingTwist.toArray())
+  console.log('w', qAPose.toArray())
+
+  expect(quaternionFromSwingTwist.x).toBeCloseTo(qAPose.x, 4)
+  expect(quaternionFromSwingTwist.y).toBeCloseTo(qAPose.y, 4)
+  expect(quaternionFromSwingTwist.z).toBeCloseTo(qAPose.z, 4)
+  expect(quaternionFromSwingTwist.w).toBeCloseTo(qAPose.w, 4)
 })
-
-function computeSwingAndTwist(target: Quaternion, source: Quaternion, forward, up) {
-  const quatInverse = target.clone().invert(),
-    altForward = forward.clone().applyQuaternion(quatInverse),
-    altUp = up.clone().applyQuaternion(quatInverse)
-
-  const poseForward = new Vector3().copy(altForward).applyQuaternion(source),
-    poseUp = new Vector3().copy(altUp).applyQuaternion(source)
-
-  const swing = new Quaternion()
-    .setFromUnitVectors(forward, poseForward) // First we create a swing rotation from one dir to the other.
-    .multiply(target) // Then we apply it to the TBone Rotation, this will do a FWD Swing which will create
-
-  // a new Up direction based on only swing.
-  const swing_up = up.clone().applyQuaternion(swing)
-  let twist = swing_up.angleTo(poseUp)
-
-  const swing_lft = swing_up.clone().cross(poseForward)
-  const vec3Dot = swing_lft.clone().dot(poseUp)
-  if (vec3Dot >= 0) twist = -twist
-
-  return { swing, twist }
-}
-
-function apply(target: Quaternion, swing: Quaternion, twist: number, originalForward: Vector3) {
-  target.premultiply(swing) // PreMultiply our swing rotation to our target's current rotation.
-
-  const twistQ = new Quaternion().setFromAxisAngle(originalForward, twist)
-  target.premultiply(twistQ)
-}
