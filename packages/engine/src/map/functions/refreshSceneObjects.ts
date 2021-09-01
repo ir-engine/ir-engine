4f3c11fe94f979b4610ce251993ae4c7e7949a5e
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-09-01 12:12:44 -0700

    MAP-45: move refreshed scene object to player

--- packages/engine/src/map/functions/refreshSceneObjects.ts
+++ packages/engine/src/map/functions/refreshSceneObjects.ts
@@ -4,6 +4,7 @@ import { Entity } from '../../ecs/classes/Entity'
 import { ECSWorld } from '../../ecs/classes/World'
 import { createMapObjects } from '..'
 import { Object3DComponent } from '../../scene/components/Object3DComponent'
+import { TransformComponent } from '../../transform/components/TransformComponent'
 
 // const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
 // const mapScale = getComponent(mapEntity, TransformComponent).scale.x
@@ -41,7 +42,12 @@ import { Object3DComponent } from '../../scene/components/Object3DComponent'
 export async function refreshSceneObjects(mapEntity: Entity, ecsWorld: ECSWorld): Promise<void> {
   const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
   const { mapMesh } = await createMapObjects(map.center, map.args)
+  const mapTransform = getComponent(mapEntity, TransformComponent, false, ecsWorld)
+  const viewerTransform = getComponent(map.viewer, TransformComponent, false, ecsWorld)
 
   removeComponent(mapEntity, Object3DComponent, ecsWorld)
+  // TODO try using a setImmediate around addComponent
   addComponent(mapEntity, Object3DComponent, { value: mapMesh }, ecsWorld)
+
+  mapTransform.position.copy(viewerTransform.position)
 }
--- packages/engine/tests/map/functions/refreshSceneObjects.test.ts
+++ packages/engine/tests/map/functions/refreshSceneObjects.test.ts
@@ -1,11 +1,12 @@
 import { refreshSceneObjects } from '../../../src/map/functions/refreshSceneObjects'
 import { createMapObjects } from '../../../src/map'
-import { Object3D } from 'three'
+import { Object3D, Quaternion, Vector3 } from 'three'
 import { addComponent, createEntity, getComponent } from '../../../src/ecs/functions/EntityFunctions'
 import { Object3DComponent } from '../../../src/scene/components/Object3DComponent'
 import { World } from '../../../src/ecs/classes/World'
 import { Entity } from '../../../src/ecs/classes/Entity'
 import { MapComponent } from '../../../src/map/MapComponent'
+import { TransformComponent } from '../../../src/transform/components/TransformComponent'
 
 const createMapObjectsMock = createMapObjects as jest.Mock
 
@@ -19,16 +20,28 @@ jest.mock('../../../src/map', () => {
   }
 })
 
+function addTransformComponentWithPosition(entity: Entity, world: World, x: number, y: number, z: number) {
+  addComponent(
+    entity,
+    TransformComponent,
+    { position: new Vector3(x, y, z), rotation: new Quaternion(), scale: new Vector3() },
+    world.ecsWorld
+  )
+}
+
 describe('refreshSceneObjects', () => {
-  let world: World, mapEntity: Entity
+  let world: World, mapEntity: Entity, viewerEntity: Entity
 
   beforeEach(async () => {
     world = new World()
 
     mapEntity = createEntity(world.ecsWorld)
+    viewerEntity = createEntity(world.ecsWorld)
   })
   test('when scene objects exist already', async () => {
     addComponent(mapEntity, Object3DComponent, { value: new Object3D() }, world.ecsWorld)
+    addTransformComponentWithPosition(mapEntity, world, 0, 0, 0)
+    addTransformComponentWithPosition(viewerEntity, world, 13, 0, 42)
     const oldObject3DComponent = getComponent(mapEntity, Object3DComponent, false, world.ecsWorld)
     const center = [0, 0]
     const args = {}
@@ -37,7 +50,7 @@ describe('refreshSceneObjects', () => {
       MapComponent,
       {
         center,
-        viewer: 0,
+        viewer: viewerEntity,
         triggerRefreshRadius: 20,
         minimumSceneRadius: 80,
         args
@@ -48,10 +61,14 @@ describe('refreshSceneObjects', () => {
     await refreshSceneObjects(mapEntity, world.ecsWorld)
 
     const newObject3DComponent = getComponent(mapEntity, Object3DComponent, false, world.ecsWorld)
+    const mapTransformComponent = getComponent(mapEntity, TransformComponent, false, world.ecsWorld)
+    const viewerTransformComponent = getComponent(viewerEntity, TransformComponent, false, world.ecsWorld)
+
     expect(createMapObjects).toHaveBeenCalledTimes(1)
     expect(createMapObjects).toHaveBeenCalledWith(center, args)
     // Test it was removed then added to trigger SceneObjectSystem enter/exit queries
     expect(newObject3DComponent).not.toBe(oldObject3DComponent)
     expect(newObject3DComponent.value).toBe(createMapObjectsMock.mock.results[0].value.mapMesh)
+    expect(mapTransformComponent.position.equals(viewerTransformComponent.position)).toBe(true)
   })
 })
