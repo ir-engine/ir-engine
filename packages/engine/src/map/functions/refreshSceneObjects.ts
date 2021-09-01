be950ebcd786fa419bfc9f09ea872176f861d731
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-09-01 09:48:13 -0700

    MAP-45: refreshSceneObjects

---
+++ packages/engine/src/map/functions/refreshSceneObjects.ts
@@ -0,0 +1,47 @@
+import { getComponent, addComponent, removeComponent } from '../../ecs/functions/EntityFunctions'
+import { MapComponent } from '../MapComponent'
+import { Entity } from '../../ecs/classes/Entity'
+import { ECSWorld } from '../../ecs/classes/World'
+import { createMapObjects } from '..'
+import { Object3DComponent } from '../../scene/components/Object3DComponent'
+
+// const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
+// const mapScale = getComponent(mapEntity, TransformComponent).scale.x
+// const longtitude = viewerPosition.x / (111134.861111 * mapScale) + startLong
+// const latitude = -viewerPosition.z / (Math.cos((startLat * PI) / 180) * 111321.377778 * mapScale) + startLat
+// const loadingTile = llToTile([longtitude, latitude])
+// console.log('updateMap to tile', loadingTile)
+// const { mapMesh, navMesh, groundMesh } = await createMapObjects(map.center, [longtitude, latitude], map.args)
+// map.currentTile = loadingTile
+// /*
+// // TODO: it does not work this way, but probably should
+// removeComponent(entity, Object3DComponent)
+// addComponent(entity, Object3DComponent, { value: mapMesh })
+//  */
+// const mapObjectComponent = getComponent(entity, Object3DComponent)
+// const prevMapMesh = mapObjectComponent.value
+// mapMesh.position.copy(prevMapMesh.position)
+// mapMesh.rotation.copy(prevMapMesh.rotation)
+// mapMesh.scale.copy(prevMapMesh.scale)
+// prevMapMesh.parent.add(mapMesh)
+// prevMapMesh.removeFromParent()
+// mapObjectComponent.value = mapMesh
+// // getComponent(currentEnt, Object3DComponent).value.clear()
+// // getComponent(currentEnt, Object3DComponent).value.add(mapMesh)
+// // TODO check and fix
+// removeComponent(entity, NavMeshComponent)
+// if (navMesh && groundMesh) {
+//   addComponent(entity, NavMeshComponent, {
+//     yukaNavMesh: navMesh,
+//     navTarget: groundMesh
+//   })
+// }
+// map.loading = false
+
+export async function refreshSceneObjects(mapEntity: Entity, ecsWorld: ECSWorld): Promise<void> {
+  const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
+  const { mapMesh } = await createMapObjects(map.center, map.args)
+
+  removeComponent(mapEntity, Object3DComponent, ecsWorld)
+  addComponent(mapEntity, Object3DComponent, { value: mapMesh }, ecsWorld)
+}
--- packages/engine/tests/map/functions/refreshSceneObjects.test.ts
+++ packages/engine/tests/map/functions/refreshSceneObjects.test.ts
@@ -1,6 +1,13 @@
 import { refreshSceneObjects } from '../../../src/map/functions/refreshSceneObjects'
 import { createMapObjects } from '../../../src/map'
 import { Object3D } from 'three'
+import { addComponent, createEntity, getComponent } from '../../../src/ecs/functions/EntityFunctions'
+import { Object3DComponent } from '../../../src/scene/components/Object3DComponent'
+import { World } from '../../../src/ecs/classes/World'
+import { Entity } from '../../../src/ecs/classes/Entity'
+import {MapComponent} from '../../../src/map/MapComponent'
+
+const createMapObjectsMock = createMapObjects as jest.Mock
 
 jest.mock('../../../src/map', () => {
   return {
@@ -13,9 +20,34 @@ jest.mock('../../../src/map', () => {
 })
 
 describe('refreshSceneObjects', () => {
-  test('happy case', () => {
-    await refreshSceneObjects(mapEntity, ecsWorld)
+  let world: World, mapEntity: Entity
+
+  beforeEach(async () => {
+    world = new World()
+
+    mapEntity = createEntity(world.ecsWorld)
+  })
+  test('when scene objects exist already', async () => {
+    addComponent(mapEntity, Object3DComponent, { value: new Object3D() }, world.ecsWorld)
+    const oldObject3DComponent = getComponent(mapEntity, Object3DComponent, false, world.ecsWorld)
+    const center = [0, 0]
+    const args = {}
+    addComponent(mapEntity, MapComponent, {
+      center,
+      viewer: 0,
+      triggerRefreshRadius: 20,
+      minimumSceneRadius: 80,
+      args
+    }, world.ecsWorld)
+    await refreshSceneObjects(mapEntity, world.ecsWorld)
+
+    const newObject3DComponent = getComponent(mapEntity, Object3DComponent, false, world.ecsWorld)
     expect(createMapObjects).toHaveBeenCalledTimes(1)
     expect(createMapObjects).toHaveBeenCalledWith(center, args)
+    // Test it was removed then added to trigger SceneObjectSystem enter/exit queries
+    expect(newObject3DComponent).not.toBe(oldObject3DComponent)
+    expect(newObject3DComponent.value).toBe(
+      createMapObjectsMock.mock.results[0].value.mapMesh
+    )
   })
 })
