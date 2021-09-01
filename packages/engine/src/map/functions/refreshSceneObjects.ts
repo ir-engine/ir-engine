0e00ae5a69e162126e9ef8612f9402b7b5976147
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-09-01 13:44:44 -0700

    MAP-45: clean up

--- packages/engine/src/map/functions/refreshSceneObjects.ts
+++ packages/engine/src/map/functions/refreshSceneObjects.ts
@@ -6,39 +6,6 @@ import { createMapObjects } from '..'
 import { Object3DComponent } from '../../scene/components/Object3DComponent'
 import { TransformComponent } from '../../transform/components/TransformComponent'
 
-// const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
-// const mapScale = getComponent(mapEntity, TransformComponent).scale.x
-// const longtitude = viewerPosition.x / (111134.861111 * mapScale) + startLong
-// const latitude = -viewerPosition.z / (Math.cos((startLat * PI) / 180) * 111321.377778 * mapScale) + startLat
-// const loadingTile = llToTile([longtitude, latitude])
-// console.log('updateMap to tile', loadingTile)
-// const { mapMesh, navMesh, groundMesh } = await createMapObjects(map.center, [longtitude, latitude], map.args)
-// map.currentTile = loadingTile
-// /*
-// // TODO: it does not work this way, but probably should
-// removeComponent(entity, Object3DComponent)
-// addComponent(entity, Object3DComponent, { value: mapMesh })
-//  */
-// const mapObjectComponent = getComponent(entity, Object3DComponent)
-// const prevMapMesh = mapObjectComponent.value
-// mapMesh.position.copy(prevMapMesh.position)
-// mapMesh.rotation.copy(prevMapMesh.rotation)
-// mapMesh.scale.copy(prevMapMesh.scale)
-// prevMapMesh.parent.add(mapMesh)
-// prevMapMesh.removeFromParent()
-// mapObjectComponent.value = mapMesh
-// // getComponent(currentEnt, Object3DComponent).value.clear()
-// // getComponent(currentEnt, Object3DComponent).value.add(mapMesh)
-// // TODO check and fix
-// removeComponent(entity, NavMeshComponent)
-// if (navMesh && groundMesh) {
-//   addComponent(entity, NavMeshComponent, {
-//     yukaNavMesh: navMesh,
-//     navTarget: groundMesh
-//   })
-// }
-// map.loading = false
-
 export async function refreshSceneObjects(mapEntity: Entity, ecsWorld: ECSWorld): Promise<void> {
   const map = getComponent(mapEntity, MapComponent, false, ecsWorld)
   const { mapMesh } = await createMapObjects(map.center, map.args)
