de208f538879bfa4dc0e862f441b816f9f94945d
Abhishek Pathak <69243392+Abhishek-Pathak-Here@users.noreply.github.com>
2021-09-02 10:32:57 +0530

    Vedio Player

--- packages/editor/src/components/assets/AssetPreviewPanels/VedioPreviewPanel.tsx
+++ packages/editor/src/components/assets/AssetPreviewPanels/VedioPreviewPanel.tsx
@@ -3,5 +3,9 @@ import React from 'react'
 export const VedioPreviewPanel = (props) => {
   const url = props.resourceProps.resourceUrl
 
-  return <div>Vedio Preview Panel</div>
+  return (
+    <video src={url} controls={true}>
+      Your Browser doesn't support Video
+    </video>
+  )
 }
