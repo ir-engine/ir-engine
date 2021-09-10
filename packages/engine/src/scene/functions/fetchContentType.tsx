927faa2a7d6c9e206793b788ef3dfcd210d01251
Abhishek Pathak <69243392+Abhishek-Pathak-Here@users.noreply.github.com>
2021-09-10 17:13:11 +0530

    Use Fetch

--- packages/engine/src/scene/functions/fetchContentType.tsx
+++ packages/engine/src/scene/functions/fetchContentType.tsx
@@ -2,12 +2,12 @@
  * fetchContentType is used to get the header content type of response using url.
  *
  * @author Robert Long
+ * @author Abhishek Pathak
  * @param  {[type]}  url [ url to make the request]
  * @return {Promise}               [wait for the response and return response]
  */
 
 export const fetchContentType = async (url): Promise<any> => {
-  // const f = await fetchUrl(url, { method: 'HEAD' }).then((r) => r.headers.get('content-type'))
-  // console.log('Response: ' + Object.values(f))
-  // return f
+  const f = await fetch(url, { method: 'HEAD' }).then((r) => r.headers.get('content-type'))
+  return f
 }
