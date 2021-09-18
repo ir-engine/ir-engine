c7f3f312dfeb9a3977f48deae972a496ee00bc56
alextitonis <alexandrostitonis@gmail.com>
2021-09-18 12:51:39 +0300

    removed the timer system

--- packages/engine/src/common/timer/components/TimerComponent.ts
+++
@@ -1,9 +0,0 @@
-import { createMappedComponent } from '../../../ecs/functions/EntityFunctions'
-
-type TimerComponentType = {
-  currentTimer: number
-  timer: number
-  callback: (dt) => void
-}
-
-export const TimerComponent = createMappedComponent<TimerComponentType>()
