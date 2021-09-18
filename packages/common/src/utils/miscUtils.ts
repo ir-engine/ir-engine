e1e8b595b8be2bf8a807a70dc65a9247f8fac02e
alextitonis <alexandrostitonis@gmail.com>
2021-09-18 16:42:53 +0300

    avoidance of resending same poses over the network

--- packages/common/src/utils/miscUtils.ts
+++ packages/common/src/utils/miscUtils.ts
@@ -33,3 +33,15 @@ export function combineArrays(arrays: [[]]) {
 
   return res
 }
+
+export function arraysAreEqual(arr1: [], arr2: []): boolean {
+  if (arr1.length !== arr2.length) return false
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+
+  return true
+}
