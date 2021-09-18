d0e57dc6d90558bdb5788e734747533d282d1ef0
Patrick <pscale01@gmail.com>
2021-09-18 09:15:23 -0400

    MAP-45: only unify polygons/multipolygons

--- packages/engine/src/map/functions/unifyCachedFeatures.ts
+++ packages/engine/src/map/functions/unifyCachedFeatures.ts
@@ -1,13 +1,18 @@
 import { Feature } from 'geojson'
+import { every } from 'lodash'
 import FeatureCache from '../classes/FeatureCache'
 import findSplitFeatures from './findSplitFeatures'
 import unifyFeatures from './unifyFeatures'
+
+const allowedGeometryTypes: Feature['geometry']['type'][] = ['Polygon', 'MultiPolygon']
 
 export default function unifyCachedFeatures(cache: FeatureCache<Feature>) {
   for (const splitFeature of findSplitFeatures(cache.keys(), cache.values())) {
     const firstKey = splitFeature[0][0]
     const features = splitFeature.map(([_, feature]) => feature)
-    const unifiedFeature = unifyFeatures(features)
-    cache.setWithoutAffectingLastUsedTime(firstKey, unifiedFeature)
+    if (every(features.map((feature) => allowedGeometryTypes.indexOf(feature.geometry.type) >= 0))) {
+      const unifiedFeature = unifyFeatures(features)
+      cache.setWithoutAffectingLastUsedTime(firstKey, unifiedFeature)
+    }
   }
 }
