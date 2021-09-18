fcbc225a4a00e71cef68ab01ca2f9a134088825b
Patrick <pscale01@gmail.com>
2021-09-18 11:01:13 -0400

    MAP-45: fix type

--- packages/engine/src/map/functions/fetchUsingCache.ts
+++ packages/engine/src/map/functions/fetchUsingCache.ts
@@ -1,9 +1,9 @@
-import { IArrayKeyedMap } from '../types'
+import MapCache from '../classes/MapCache'
 import createUsingGetSet from './createUsingGetSet'
 
 export default function fetchUsingCache<CacheKey extends any[], Value>(fetch: (...args: CacheKey) => Promise<Value>) {
   const _fetchUsingCache = createUsingGetSet(fetch)
-  return async (cache: IArrayKeyedMap<CacheKey, Value>, fetchArgs: CacheKey) => {
+  return async (cache: MapCache<CacheKey, Value>, fetchArgs: CacheKey) => {
     return await _fetchUsingCache(
       cache.get.bind(cache),
       async function set(key: CacheKey, value: Promise<Value>) {
