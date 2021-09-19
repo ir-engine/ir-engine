7e2dce932c8a0d27963d48c59e487e4f8e842be0
Patrick <pscale01@gmail.com>
2021-09-19 19:58:14 -0400

    MAP-45: update how map is created in /location

--- packages/engine/src/scene/functions/createMap.ts
+++ packages/engine/src/scene/functions/createMap.ts
@@ -6,43 +6,17 @@ import { Object3DComponent } from '../components/Object3DComponent'
 import { Entity } from '../../ecs/classes/Entity'
 import { MapComponent } from '../../map/MapComponent'
 import { Group, Vector3 } from 'three'
-import ArrayKeyedMap from '../../map/classes/ArrayKeyedMap'
 import { createProductionPhases } from '../../map/functions/createProductionPhases'
 import execTasksCompletely from '../../map/functions/execTasksCompletely'
-import { FeatureKey, MapDerivedFeatureComplete, MapDerivedFeatureGeometry, TileKey, VectorTile } from '../../map/types'
-import FetchTileTask from '../../map/classes/FetchTileTask'
-import TileCache from '../../map/classes/TileCache'
-import FeatureCache from '../../map/classes/FeatureCache'
-import { Feature } from 'geojson'
-import ExtractTileFeaturesTask from '../../map/classes/ExtractTileFeaturesTask'
-import CreateGeometryTask from '../../map/classes/CreateGeometryTask'
-import CreateCompleteObjectTask from '../../map/classes/CreateCompleteObjectTask'
-
-const MAX_CACHED_FEATURES = 1024 * 8
+import createStore from '../../map/functions/createStore'
+import { useWorld } from '../../ecs/functions/SystemHooks'
 
 export async function createMap(entity: Entity, args: MapProps): Promise<void> {
   // TODO: handle "navigator.geolocation.getCurrentPosition" rejection?
   const center = await getStartCoords(args)
 
-  const minimumSceneRadius = 600
-
-  const mapComponent = {
-    center,
-    originalCenter: center,
-    triggerRefreshRadius: 200,
-    minimumSceneRadius,
-    fetchTilesTasks: new ArrayKeyedMap<TileKey, FetchTileTask>(),
-    tileCache: new TileCache<VectorTile>(16),
-    extractTilesTasks: new ArrayKeyedMap<TileKey, ExtractTileFeaturesTask>(),
-    featureCache: new FeatureCache<Feature>(MAX_CACHED_FEATURES),
-    geometryTasks: new ArrayKeyedMap<FeatureKey, CreateGeometryTask>(),
-    geometryCache: new FeatureCache<MapDerivedFeatureGeometry>(MAX_CACHED_FEATURES),
-    completeObjectsTasks: new ArrayKeyedMap<FeatureKey, CreateCompleteObjectTask>(),
-    completeObjects: new FeatureCache<MapDerivedFeatureComplete>(MAX_CACHED_FEATURES),
-    args
-  }
-
-  addComponent(entity, MapComponent, mapComponent)
+  const store = createStore(center, args, 200, 600)
+  addComponent(entity, MapComponent, store)
 
   const mapObject3D = new Group()
 
@@ -55,23 +29,24 @@ export async function createMap(entity: Entity, args: MapProps): Promise<void> {
     addComponent(entity, DebugNavMeshComponent, null)
   }
 
-  // TODO use UpdatableComponent for labels
-
   const phases = createProductionPhases(
-    mapComponent.fetchTilesTasks,
-    mapComponent.tileCache,
-    mapComponent.extractTilesTasks,
-    mapComponent.featureCache,
-    mapComponent.geometryTasks,
-    mapComponent.geometryCache,
-    mapComponent.completeObjectsTasks,
-    mapComponent.completeObjects,
-    mapComponent.center,
-    mapComponent.originalCenter,
-    mapComponent.minimumSceneRadius,
-    // TODO how to get transform components?
-    new Vector3(),
-    0.2
+    useWorld(),
+    store.fetchTilesTasks,
+    store.tileCache,
+    store.extractTilesTasks,
+    store.featureCache,
+    store.geometryTasks,
+    store.geometryCache,
+    store.completeObjectsTasks,
+    store.completeObjects,
+    store.labelTasks,
+    store.labelCache,
+    store.center,
+    store.originalCenter,
+    store.minimumSceneRadius,
+    // TODO how to get transform components? in the meantime, guessing isn't too bad
+    new Vector3(0, 0, 0),
+    1
   )
   await execTasksCompletely(phases)
 }
