0cd42935fc1b422317182220c7feaa603a29cee4
Patrick <pscale01@gmail.com>
2021-09-20 14:41:57 -0400

    MAP-45: fix build

--- packages/engine/src/map/util.ts
+++ packages/engine/src/map/util.ts
@@ -1,12 +1,4 @@
-import { ILayerName, TileFeaturesByLayer } from './types'
-import { Feature } from 'geojson'
 import { Vector3 } from 'three'
-
-export function collectFeaturesByLayer(layerName: ILayerName, vectorTiles: TileFeaturesByLayer[]): Feature[] {
-  return vectorTiles.reduce((accFeatures, tile) => {
-    return [...accFeatures, ...tile[layerName]]
-  }, [])
-}
 
 export function vector3ToArray2(vector: Vector3): [number, number] {
   return [vector.x, vector.z]
