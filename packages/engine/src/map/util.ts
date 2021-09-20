8b2060025b8452da87b722323444f9bb0e33760f
Patrick <pscale01@gmail.com>
2021-09-20 11:51:30 -0400

    MAP-45: add util

--- packages/engine/src/map/util.ts
+++ packages/engine/src/map/util.ts
@@ -11,3 +11,7 @@ export function collectFeaturesByLayer(layerName: ILayerName, vectorTiles: TileF
 export function vector3ToArray2(vector: Vector3): [number, number] {
   return [vector.x, vector.z]
 }
+
+export function multiplyArray<ArrayType extends number[]>(array: ArrayType, scalar: number): ArrayType {
+  return array.map((value) => value * scalar) as any
+}
