ca8324690b275b08ef8c792a94b2cb4c539e11e4
Patrick <pscale01@gmail.com>
2021-09-21 16:56:57 -0400

    MAP-45: switch to better(?) conversion from long, lat

--- packages/engine/src/map/units.ts
+++ packages/engine/src/map/units.ts
@@ -5,13 +5,8 @@ export const METERS_PER_LONGLAT = 111139
 export type LongLat = number[]
 
 export function toMetersFromCenter([lng, lat]: LongLat, [lngCenter, latCenter]: LongLat, sceneScale = 1): number[] {
-  return [(lng - lngCenter) * METERS_PER_LONGLAT * sceneScale, (lat - latCenter) * METERS_PER_LONGLAT * sceneScale]
-}
-
-// TODO try this instead
-export function toMetersFromCenter2([lng, lat]: LongLat, [lngCenter, latCenter]: LongLat, scale = 1): LongLat {
-  const x = (lng - lngCenter) * 111134.861111 * scale
-  const z = (lat - latCenter) * (Math.cos((latCenter * Math.PI) / 180) * 111321.377778) * scale
+  const x = (lng - lngCenter) * 111134.861111 * sceneScale
+  const z = (lat - latCenter) * (Math.cos((latCenter * Math.PI) / 180) * 111321.377778) * sceneScale
   return [x, z]
 }
 
