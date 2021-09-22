a07881f231adeffd0003f08247f2e72aba2147c0
Patrick <pscale01@gmail.com>
2021-09-22 13:34:54 -0400

    MAP-45: add utils

--- packages/engine/src/map/util.ts
+++ packages/engine/src/map/util.ts
@@ -1,9 +1,20 @@
-import { Vector3 } from 'three'
+import { Object3D, Vector3 } from 'three'
 
+// TODO(perf) add target param
 export function vector3ToArray2(vector: Vector3): [number, number] {
   return [vector.x, vector.z]
 }
 
+// TODO(perf) add target param
 export function multiplyArray<ArrayType extends number[]>(array: ArrayType, scalar: number): ArrayType {
   return array.map((value) => value * scalar) as any
+}
+
+export function addChildFast(parent: Object3D, child: Object3D, children = parent.children) {
+  child.parent = parent
+  children.push(child)
+}
+
+export function setPosition(object3d: Object3D, centerPoint: [number, number]) {
+  object3d.position.set(centerPoint[0], 0, centerPoint[1])
 }
