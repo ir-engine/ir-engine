19c8f1fa37945d49358c34dce680cf3c278d1180
Josh Field <joshfield999@gmail.com>
2021-09-25 10:47:39 +1000

    Reality Packs Loader (#3430)
    * Change functions that are not called in Editor
    
    * Little Cleanup
    
    * Start CustomScriptNode
    
    * Code node
    
    * Add tsx/ts support with customscriptnode
    
    * Add Custom Script Loading
    
    * Add .js
    
    * Load Code
    
    * RealityPackages
    
    * Fixes
    
    * Loading and Editor
    
    * Refactor
    
    * Fixes
    
    * reality pack loading works!!!!
    
    * reality pack node uses manifest data
    
    * loader works
    
    * add injection point to reality pack options
    
    * fix prepush
    
    Co-authored-by: Abhishek Pathak <69243392+Abhishek-Pathak-Here@users.noreply.github.com>

---
+++ packages/realitypacks/loader.ts
@@ -0,0 +1,13 @@
+import type { RealityPack } from '@xrengine/common/src/interfaces/RealityPack'
+import type { SystemModulePromise } from '@xrengine/engine/src/ecs/functions/SystemFunctions'
+
+export const importPack = async (packName): Promise<SystemModulePromise<any>[]> => {
+  const realityPackManifest = (await import(`./packs/${packName}/manifest.json`)) as RealityPack
+  const modules: SystemModulePromise<any>[] = []
+
+  for (const entryPoint of realityPackManifest.moduleEntryPoints) {
+    modules.push(import(`./packs/${realityPackManifest.name}/${entryPoint}`))
+  }
+
+  return modules
+}
