c6c74ce617d22ef4d86d256e7b7d4c0457afe8be
HexaField <joshfield999@gmail.com>
2021-09-26 19:20:05 +1000

    disable k8s client reload

--- packages/server-core/src/entities/content-pack/content-pack-helper.ts
+++ packages/server-core/src/entities/content-pack/content-pack-helper.ts
@@ -399,26 +399,26 @@ export async function populateRealityPack(
     params
   )
   await Promise.all(uploadPromises)
-  if (typeof (app as any).k8DefaultClient! == 'undefined') {
-    try {
-      console.log('Attempting to reload k8s clients!')
-      const restartClientsResponse = await (app as any).k8DefaultClient.patch(
-        `deployment/${config.server.releaseName}-xrengine-client`,
-        {
-          spec: {
-            template: {
-              metadata: {
-                annotations: {
-                  'kubectl.kubernetes.io/restartedAt': new Date().toISOString()
-                }
-              }
-            }
-          }
-        }
-      )
-      console.log('restartClientsResponse', restartClientsResponse)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+  // if (typeof (app as any).k8DefaultClient! == 'undefined') {
+  //   try {
+  //     console.log('Attempting to reload k8s clients!')
+  //     const restartClientsResponse = await (app as any).k8DefaultClient.patch(
+  //       `deployment/${config.server.releaseName}-xrengine-client`,
+  //       {
+  //         spec: {
+  //           template: {
+  //             metadata: {
+  //               annotations: {
+  //                 'kubectl.kubernetes.io/restartedAt': new Date().toISOString()
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     )
+  //     console.log('restartClientsResponse', restartClientsResponse)
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // }
 }
