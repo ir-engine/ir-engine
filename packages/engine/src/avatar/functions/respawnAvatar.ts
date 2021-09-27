1c3808d260ebd17eaa454497068da4bda29cf23d
HexaField <joshfield999@gmail.com>
2021-09-27 22:17:59 +1000

    fix respawn

--- packages/engine/src/avatar/functions/respawnAvatar.ts
+++ packages/engine/src/avatar/functions/respawnAvatar.ts
@@ -1,22 +1,18 @@
 import { Entity } from '../../ecs/classes/Entity'
 import { getComponent } from '../../ecs/functions/ComponentFunctions'
+import { useWorld } from '../../ecs/functions/SystemHooks'
 import { NetworkObjectComponent } from '../../networking/components/NetworkObjectComponent'
-import { dispatchFromClient } from '../../networking/functions/dispatch'
-import { NetworkWorldAction } from '../../networking/interfaces/NetworkWorldActions'
+import { dispatchFrom } from '../../networking/functions/dispatchFrom'
+import { NetworkWorldAction } from '../../networking/functions/NetworkWorldAction'
 import { SpawnPoseComponent } from '../components/SpawnPoseComponent'
 
 export const respawnAvatar = (entity: Entity) => {
   const { position, rotation } = getComponent(entity, SpawnPoseComponent)
   const networkObject = getComponent(entity, NetworkObjectComponent)
-  dispatchFromClient(
-    NetworkWorldAction.teleportObject(networkObject.networkId, [
-      position.x,
-      position.y,
-      position.z,
-      rotation.x,
-      rotation.y,
-      rotation.z,
-      rotation.w
-    ])
+  dispatchFrom(useWorld().hostId, () =>
+    NetworkWorldAction.teleportObject({
+      networkId: networkObject.networkId,
+      pose: [position.x, position.y, position.z, rotation.x, rotation.y, rotation.z, rotation.w]
+    })
   )
 }
