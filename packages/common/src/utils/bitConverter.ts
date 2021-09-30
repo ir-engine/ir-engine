5edb6cd143b1427b6bfcd7acfab6f607b4b43a61
Nate <mrtn.nathaniel@gmail.com>
2021-09-30 16:18:35 -0700

    small optimization

--- packages/common/src/utils/bitConverter.ts
+++ packages/common/src/utils/bitConverter.ts
@@ -10,8 +10,8 @@ export function ToInt(buffer, index) {
   return (buffer[index] | (buffer[index + 1] << 8) | (buffer[index + 2] << 16) | (buffer[index + 3] << 24)) >>> 0
 }
 
+const farr = new Float32Array(1)
 export function encodeFloat(float) {
-  const farr = new Float32Array(1)
   farr[0] = float
   const barr = new Int8Array(farr.buffer)
   return [...barr]
