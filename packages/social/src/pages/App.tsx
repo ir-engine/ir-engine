67a6be75b36fec1a75bd00edc17f08d19083ce65
Aleks951 <a.l.e.k.s.r.u.m.e.n98@gmail.com>
2021-09-30 06:53:47 +0300

    test

--- packages/social/src/pages/App.tsx
+++ packages/social/src/pages/App.tsx
@@ -111,9 +111,12 @@ const Home = ({ createCreator, doLoginAuto, creatorsState, webxrnativeState, cha
         {/* <Stories stories={stories} /> */}
         <FeedMenu view={view} setView={setView} />
         <AppFooter setView={setView} />
-        {currentCreator &&
-          (!!!currentCreator.terms || !!!currentCreator.policy) &&
-          auth.user.userRole.value === 'user' && <TermsAndPolicy />}
+        {currentCreator && (
+          // Сделано на время теста
+          // (!!!currentCreator.terms || !!!currentCreator.policy) &&
+          // auth.user.userRole.value === 'user' &&
+          <TermsAndPolicy />
+        )}
         <ArMediaPopup />
         <WebXRStart
           feedHintsOnborded={feedHintsOnborded}
