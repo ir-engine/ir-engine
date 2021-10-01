9c9919437cfc6be52db55cf46ae9ce0a580cd463
Nate <mrtn.nathaniel@gmail.com>
2021-09-30 21:00:39 -0700

    outgoing system pipelining & optimization

--- packages/engine/src/networking/systems/OutgoingNetworkSystem.ts
+++ packages/engine/src/networking/systems/OutgoingNetworkSystem.ts
@@ -14,6 +14,16 @@ import { isZero } from '@xrengine/common/src/utils/mathUtils'
 import { encodeVector3, encodeQuaternion } from '@xrengine/common/src/utils/encode'
 import { arraysAreEqual } from '@xrengine/common/src/utils/miscUtils'
 import { Action } from '../interfaces/Action'
+import { pipe } from 'bitecs'
+
+const networkTransformsQuery =
+  // isClient
+  //   ? defineQuery([NetworkObjectOwnerComponent, NetworkObjectComponent, TransformComponent]) :
+  defineQuery([NetworkObjectComponent, TransformComponent])
+
+const ikTransformsQuery = isClient
+  ? defineQuery([AvatarControllerComponent, XRInputSourceComponent])
+  : defineQuery([XRInputSourceComponent])
 
 function sendActions() {
   const incomingActions = Engine.defaultWorld.incomingActions
@@ -50,133 +60,6 @@ let prevWorldState: WorldStateInterface = {
   time: 0,
   pose: [],
   ikPose: []
-}
-
-export default async function OutgoingNetworkSystem(world: World): Promise<System> {
-  /**
-   * For the client, we only want to send out objects we have authority over,
-   *   which are the local avatar and any owned objects
-   * For the server, we want to send all objects
-   */
-
-  const networkTransformsQuery =
-    // isClient
-    //   ? defineQuery([NetworkObjectOwnerComponent, NetworkObjectComponent, TransformComponent]) :
-    defineQuery([NetworkObjectComponent, TransformComponent])
-
-  const ikTransformsQuery = isClient
-    ? defineQuery([AvatarControllerComponent, XRInputSourceComponent])
-    : defineQuery([XRInputSourceComponent])
-
-  return () => {
-    if (Engine.offlineMode) {
-      sendActions()
-      return
-    }
-
-    sendActions()
-
-    const newWorldState: WorldStateInterface = {
-      tick: world.fixedTick,
-      time: Date.now(),
-      pose: [],
-      ikPose: []
-    }
-
-    for (const entity of networkTransformsQuery(world)) {
-      const transformComponent = getComponent(entity, TransformComponent)
-      const networkObject = getComponent(entity, NetworkObjectComponent)
-
-      let vel = undefined! as number[]
-      let angVel = undefined
-      if (hasComponent(entity, VelocityComponent)) {
-        const velC = getComponent(entity, VelocityComponent)
-        if (isZero(velC.velocity) || velocityIsTheSame(networkObject.networkId, velC.velocity)) vel = [0]
-        else vel = encodeVector3(velC.velocity)
-      }
-      // const networkObjectOwnerComponent = getComponent(entity, NetworkObjectOwnerComponent)
-      // networkObjectOwnerComponent && console.log('outgoing', getComponent(entity, NameComponent).name, transformComponent.position)
-      // console.log('outgoing', getComponent(entity, NameComponent).name, transformComponent.position.toArray().concat(transformComponent.rotation.toArray()))
-      if (
-        !transformIsTheSame(
-          networkObject.networkId,
-          encodeVector3(transformComponent.position),
-          encodeQuaternion(transformComponent.rotation),
-          vel
-        )
-      )
-        newWorldState.pose.push({
-          networkId: networkObject.networkId,
-          position: encodeVector3(transformComponent.position),
-          rotation: encodeQuaternion(transformComponent.rotation),
-          linearVelocity: vel !== undefined ? vel : [0],
-          angularVelocity: angVel !== undefined ? angVel : [0]
-        })
-    }
-
-    const networkComponent = getComponent(world.localClientEntity, NetworkObjectComponent)
-    if (isClient && networkComponent) {
-      const transformComponent = getComponent(world.localClientEntity, TransformComponent)
-      let vel = undefined! as number[]
-      let angVel = undefined
-      if (hasComponent(world.localClientEntity, VelocityComponent)) {
-        const velC = getComponent(world.localClientEntity, VelocityComponent)
-        if (isZero(velC.velocity) || velocityIsTheSame(world.localClientEntity, velC.velocity)) vel = [0]
-        else vel = encodeVector3(velC.velocity)
-      }
-
-      if (
-        !transformIsTheSame(
-          networkComponent.networkId,
-          encodeVector3(transformComponent.position),
-          encodeQuaternion(transformComponent.rotation),
-          vel
-        )
-      )
-        newWorldState.pose.push({
-          networkId: networkComponent.networkId,
-          position: encodeVector3(transformComponent.position),
-          rotation: encodeQuaternion(transformComponent.rotation),
-          linearVelocity: vel !== undefined ? vel : [0],
-          angularVelocity: angVel !== undefined ? angVel : [0]
-        })
-    }
-
-    for (const entity of ikTransformsQuery(world)) {
-      const networkObject = getComponent(entity, NetworkObjectComponent)
-
-      const xrInputs = getComponent(entity, XRInputSourceComponent)
-
-      if (
-        !ikPoseIsTheSame(
-          networkObject.networkid,
-          encodeVector3(xrInputs.head.position),
-          encodeQuaternion(xrInputs.head.quaternion),
-          encodeVector3(xrInputs.controllerLeft.position),
-          encodeQuaternion(xrInputs.controllerLeft.quaternion),
-          encodeVector3(xrInputs.controllerRight.position),
-          encodeQuaternion(xrInputs.controllerRight.quaternion)
-        )
-      )
-        newWorldState.ikPose.push({
-          networkId: networkObject.networkId,
-          headPosePosition: encodeVector3(xrInputs.head.position),
-          headPoseRotation: encodeQuaternion(xrInputs.head.quaternion),
-          leftPosePosition: encodeVector3(xrInputs.controllerLeft.position),
-          leftPoseRotation: encodeQuaternion(xrInputs.controllerLeft.quaternion),
-          rightPosePosition: encodeVector3(xrInputs.controllerRight.position),
-          rightPoseRotation: encodeQuaternion(xrInputs.controllerRight.quaternion)
-        })
-    }
-
-    try {
-      const buffer = WorldStateModel.toBuffer(newWorldState)
-      Network.instance.transport.sendData(buffer)
-      prevWorldState = newWorldState
-    } catch (e) {
-      console.log('could not convert world state to a buffer, ' + e)
-    }
-  }
 }
 
 function velocityIsTheSame(netId, vel): boolean {
@@ -229,4 +112,148 @@ function ikPoseIsTheSame(netId, hp, hr, lp, lr, rp, rr): boolean {
   }
 
   return false
+}
+
+const queueUnchangedPoses = (world) => (newWorldState) => {
+  for (const entity of networkTransformsQuery(world)) {
+    const transformComponent = getComponent(entity, TransformComponent)
+    const networkObject = getComponent(entity, NetworkObjectComponent)
+
+    let vel = undefined! as number[]
+    let angVel = undefined
+    if (hasComponent(entity, VelocityComponent)) {
+      const velC = getComponent(entity, VelocityComponent)
+      if (isZero(velC.velocity) || velocityIsTheSame(networkObject.networkId, velC.velocity)) vel = [0]
+      else vel = encodeVector3(velC.velocity)
+    }
+    // const networkObjectOwnerComponent = getComponent(entity, NetworkObjectOwnerComponent)
+    // networkObjectOwnerComponent && console.log('outgoing', getComponent(entity, NameComponent).name, transformComponent.position)
+    // console.log('outgoing', getComponent(entity, NameComponent).name, transformComponent.position.toArray().concat(transformComponent.rotation.toArray()))
+    if (
+      !transformIsTheSame(
+        networkObject.networkId,
+        encodeVector3(transformComponent.position),
+        encodeQuaternion(transformComponent.rotation),
+        vel
+      )
+    )
+      newWorldState.pose.push({
+        networkId: networkObject.networkId,
+        position: encodeVector3(transformComponent.position),
+        rotation: encodeQuaternion(transformComponent.rotation),
+        linearVelocity: vel !== undefined ? vel : [0],
+        angularVelocity: angVel !== undefined ? angVel : [0]
+      })
+  }
+  return newWorldState
+}
+
+const queueUnchangedPosesForClient = (world) => (newWorldState) => {
+  const networkComponent = getComponent(world.localClientEntity, NetworkObjectComponent)
+  if (isClient && networkComponent) {
+    const transformComponent = getComponent(world.localClientEntity, TransformComponent)
+    let vel = undefined! as number[]
+    let angVel = undefined
+    if (hasComponent(world.localClientEntity, VelocityComponent)) {
+      const velC = getComponent(world.localClientEntity, VelocityComponent)
+      if (isZero(velC.velocity) || velocityIsTheSame(world.localClientEntity, velC.velocity)) vel = [0]
+      else vel = encodeVector3(velC.velocity)
+    }
+
+    if (
+      !transformIsTheSame(
+        networkComponent.networkId,
+        encodeVector3(transformComponent.position),
+        encodeQuaternion(transformComponent.rotation),
+        vel
+      )
+    )
+      newWorldState.pose.push({
+        networkId: networkComponent.networkId,
+        position: encodeVector3(transformComponent.position),
+        rotation: encodeQuaternion(transformComponent.rotation),
+        linearVelocity: vel !== undefined ? vel : [0],
+        angularVelocity: angVel !== undefined ? angVel : [0]
+      })
+  }
+  return newWorldState
+}
+
+const queueUnchangedIkPoses = (world) => (newWorldState) => {
+  for (const entity of ikTransformsQuery(world)) {
+    const { networkId } = getComponent(entity, NetworkObjectComponent)
+
+    const xrInputs = getComponent(entity, XRInputSourceComponent)
+
+    const headPosePosition = encodeVector3(xrInputs.head.position)
+    const headPoseRotation = encodeQuaternion(xrInputs.head.quaternion)
+    const leftPosePosition = encodeVector3(xrInputs.controllerLeft.position)
+    const leftPoseRotation = encodeQuaternion(xrInputs.controllerLeft.quaternion)
+    const rightPosePosition = encodeVector3(xrInputs.controllerRight.position)
+    const rightPoseRotation = encodeQuaternion(xrInputs.controllerRight.quaternion)
+
+    if (
+      !ikPoseIsTheSame(
+        networkId,
+        headPosePosition,
+        headPoseRotation,
+        leftPosePosition,
+        leftPoseRotation,
+        rightPosePosition,
+        rightPoseRotation
+      )
+    )
+      newWorldState.ikPose.push({
+        networkId,
+        headPosePosition,
+        headPoseRotation,
+        leftPosePosition,
+        leftPoseRotation,
+        rightPosePosition,
+        rightPoseRotation
+      })
+  }
+  return newWorldState
+}
+
+const sendBuffer = (newWorldState) => {
+  try {
+    const buffer = WorldStateModel.toBuffer(newWorldState)
+    Network.instance.transport.sendData(buffer)
+    prevWorldState = newWorldState
+  } catch (e) {
+    console.log('could not convert world state to a buffer, ' + e)
+  }
+  return newWorldState
+}
+
+export default async function OutgoingNetworkSystem(world: World): Promise<System> {
+  /**
+   * For the client, we only want to send out objects we have authority over,
+   *   which are the local avatar and any owned objects
+   * For the server, we want to send all objects
+   */
+
+  // prettier-ignore
+  const pipeline = pipe(
+    queueUnchangedPoses(world),
+    queueUnchangedPosesForClient(world),
+    queueUnchangedIkPoses(world),
+    sendBuffer
+  )
+
+  return () => {
+    sendActions()
+
+    if (Engine.offlineMode) return
+
+    const newWorldState: WorldStateInterface = {
+      tick: world.fixedTick,
+      time: Date.now(),
+      pose: [],
+      ikPose: []
+    }
+
+    pipeline(newWorldState)
+  }
 }
