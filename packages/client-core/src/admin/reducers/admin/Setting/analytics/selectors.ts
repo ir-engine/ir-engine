7720fb355fc3cfe7e0b9d5ed3005efff8f7568f0
Nate <mrtn.nathaniel@gmail.com>
2021-10-04 12:20:21 -0700

    merge dev

--- packages/client-core/src/admin/reducers/admin/Setting/analytics/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect'
-
-const selectState = (state: any): any => {
-  return state.get('settingAnalytics')
-}
-
-export const selectSettingAnalyticsState = createSelector([selectState], (settingAnalytics) => settingAnalytics)
--- packages/client-core/src/admin/reducers/contentPack/selector.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect'
-
-const selectState = (state: any): any => {
-  return state.get('contentPack')
-}
-export const selectContentPackState = createSelector([selectState], (contentPack) => contentPack)
