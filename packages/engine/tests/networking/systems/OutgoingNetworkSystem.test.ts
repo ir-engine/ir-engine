bf711b79bff3f69e1e4afc48c867da356320eb8f
Nate <mrtn.nathaniel@gmail.com>
2021-10-06 18:56:30 -0700

    outgoing net tests

--- packages/engine/tests/networking/systems/OutgoingNetworkSystem.test.ts
+++ packages/engine/tests/networking/systems/OutgoingNetworkSystem.test.ts
@@ -1,9 +1,57 @@
 import { initializeEngine, shutdownEngine } from '../../../src/initializeEngine'
 import { Engine } from '../../../src/ecs/classes/Engine'
 import { engineTestSetup } from '../../util/setupEngine'
-import assert from 'assert'
+import assert, { strictEqual } from 'assert'
+import { Network } from '../../../src/networking/classes/Network'
+import { forwardIncomingActionsFromOthersIfHost, rerouteActions, sendActionsOnTransport } from '../../../src/networking/systems/OutgoingNetworkSystem'
+import { CreateWorld, World } from '../../../src/ecs/classes/World'
+import { Action, ActionRecipients } from '../../../src/networking/interfaces/Action'
+import { UserId } from '@xrengine/common/src/interfaces/UserId'
+import { NetworkWorldAction } from '../../../src/networking/functions/NetworkWorldAction'
+import matches from 'ts-matches'
 
-// describe('OutgoingNetworkSystem Unit Tests', () => {
-// })
-// describe('OutgoingNetworkSystem Integration Tests', () => {
-// })
+describe('OutgoingNetworkSystem Unit Tests', () => {
+  it('should forwardIncomingActionsFromOthersIfHost', () => {
+		/* mock */
+		const world = World[CreateWorld]()
+
+    // make this engine user the host
+    // world.isHosting === true
+    Engine.userId = world.hostId
+
+		const tick = 0
+
+		world.fixedTick = tick
+
+		const action = NetworkWorldAction.spawnObject({
+      userId: '0' as UserId,
+			prefab: '',
+			parameters: {},
+			$tick: tick,
+      // make action come from another user
+			$from: '2' as UserId,
+      // send to server
+			$to: 'server' as ActionRecipients,
+		})
+		
+		world.incomingActions.add(action)
+
+		// const recepted: typeof action[] = []
+		// world.receptors.add(
+		// 	(a) => matches(a).when(NetworkWorldAction.spawnObject.matches, (a) => recepted.push(a))
+		// )
+
+		/* run */
+    forwardIncomingActionsFromOthersIfHost(world)
+
+		/* assert */
+    strictEqual(world.incomingActions.size, 0)
+    strictEqual(world.outgoingActions.size, 1)
+  })
+})
+
+describe('OutgoingNetworkSystem Integration Tests', () => {
+  it('should ', () => {
+    
+  })
+})
