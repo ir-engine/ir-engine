0a9e8e8929976964e2c690151ecfebf922102ed2
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2021-10-08 11:17:08 -0700

    MAP-45: clean up

--- packages/engine/src/map/index.ts
+++ packages/engine/src/map/index.ts
@@ -1,67 +1,7 @@
-//@ts-nocheck
-import { Position, Polygon, MultiPolygon } from 'geojson'
-import { NavMesh } from 'yuka'
+import { LongLat } from './functions/UnitConversionFunctions'
 import { MapProps } from './MapProps'
-import { NavMeshBuilder } from './NavMeshBuilder'
-import pc from 'polygon-clipping'
-import { computeBoundingBox, scaleAndTranslate } from './GeoJSONFns'
 
-let centerCoord = {}
-let centerTile = {}
-let scaleArg
-
-// export const enqueueTasks = async function (center: Position, minimumSceneRadius: number, args: MapProps) {
-//   console.log('enqueueMapTasks called with args:', center, args)
-//   const vectorTiles = await fetchVectorTiles(center, minimumSceneRadius)
-//   const rasterTiles = (args as any).showRasterTiles ? await fetchRasterTiles(center) : []
-
-//   const group = new Group()
-//   const buildingMesh = await createBuildings(vectorTiles, center)
-//   const groundMesh = createGroundMesh(rasterTiles as any, center[1])
-//   const roadsMesh = await createRoads(vectorTiles, center)
-//   const waterMesh = await createWater(vectorTiles, center)
-//   const landUseMesh = await createLandUse(vectorTiles, center)
-//   const labels = createLabels(vectorTiles)
-
-//   ;[buildingMesh, roadsMesh, waterMesh, landUseMesh, groundMesh].forEach((mesh) => {
-//     group.add(mesh)
-//   })
-
-//   // setGroundScaleAndPosition(groundMesh, buildingMesh)
-
-//   labels.forEach((label) => {
-//     group.add(label.object3d)
-//   })
-
-//   // TODO: use generateNavMesh as soon as it will work nice
-//   const navMesh = null // generateNavMesh(vectorTiles, center, args.scale.x * METERS_PER_DEGREE_LL)
-
-//   group.name = 'MapObject'
-//   // centerCoord = Object.assign(center)
-//   // centerTile = Object.assign(llToTile(center))
-//   // scaleArg = args.scale.x
-
-//   return { mapMesh: group, buildingMesh, groundMesh, roadsMesh, navMesh, labels }
-// }
-
-// const generateNavMesh = function (tiles: TileFeaturesByLayer[], center: Position, scale: number): NavMesh {
-//   const builder = new NavMeshBuilder()
-//   const gBuildings = tiles
-//     .reduce((acc, tiles) => acc.concat(tiles.building), [])
-//     .map((feature) => {
-//       return scaleAndTranslate(feature.geometry as Polygon | MultiPolygon, center as any, scale)
-//     })
-
-//   const gGround = computeBoundingBox(gBuildings)
-//   let gBuildingNegativeSpace = [gGround.coordinates]
-//   gBuildings.forEach((gPositiveSpace) => {
-//     gBuildingNegativeSpace = pc.difference(gBuildingNegativeSpace as any, gPositiveSpace.coordinates as any)
-//   })
-//   builder.addGeometry({ type: 'MultiPolygon', coordinates: gBuildingNegativeSpace })
-//   return builder.build()
-// }
-
-export function getStartCoords(props: MapProps): Promise<Position> {
+export function getStartCoords(props: MapProps): Promise<LongLat> {
   if (props.useDeviceGeolocation) {
     return new Promise((resolve, reject) =>
       navigator.geolocation.getCurrentPosition(({ coords }) => resolve([coords.longitude, coords.latitude]), reject)
@@ -69,17 +9,8 @@ export function getStartCoords(props: MapProps): Promise<Position> {
   }
 
   // Default to downtown ATL
-  return Promise.resolve([parseFloat(props.startLongitude) || -84.388, parseFloat(props.startLatitude) || 33.749])
-}
-
-export const getCoord = () => {
-  return centerCoord
-}
-
-export const getTile = () => {
-  return centerTile
-}
-
-export const getScaleArg = () => {
-  return scaleArg
+  return Promise.resolve([
+    props.startLongitude ? parseFloat(props.startLongitude) : -84.388,
+    props.startLatitude ? parseFloat(props.startLatitude) : 33.749
+  ])
 }
