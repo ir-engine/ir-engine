a1f74c594f6a53857aec432fb9b103e1a7a5d244
Josh Field <joshfield999@gmail.com>
2021-10-12 08:09:05 +1100

    clean up routes, add tsconfig to template realitypack

--- packages/client/src/route/EditorProtected.tsx
+++ packages/client/src/route/EditorProtected.tsx
@@ -1,7 +1,6 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
-import { connect, useDispatch } from 'react-redux'
-import { bindActionCreators, Dispatch } from 'redux'
+import { useDispatch } from 'react-redux'
 const editorProject = React.lazy(() => import('@xrengine/editor/src/pages/projects'))
 const editorProjID = React.lazy(() => import('@xrengine/editor/src/pages/projects/[projectId]'))
 const editorCreate = React.lazy(() => import('@xrengine/editor/src/pages/create'))
@@ -9,17 +8,10 @@ const UnauthorisedPage = React.lazy(() => import('@xrengine/client/src/pages/403
 import { useAuthState } from '@xrengine/client-core/src/user/reducers/auth/AuthState'
 import { AuthService } from '@xrengine/client-core/src/user/reducers/auth/AuthService'
 
-interface Props {
-  authState?: any
-  doLoginAuto?: any
-}
-
 const unauthorisedMessage = `The page you are looking for is eligible for authorised users only.`
 const loggingInMessage = `Authorising...`
 
-const EditorProtectedRoutes = (props: Props) => {
-  //const { authState, doLoginAuto } = props
-
+const EditorProtectedRoutes = () => {
   const dispatch = useDispatch()
   const scopes = useAuthState().user?.scopes?.value || []
   let isSceneAllowed = false
@@ -47,18 +39,17 @@ const EditorProtectedRoutes = (props: Props) => {
     <Switch>
       {isSceneAllowed ? (
         <>
-          <Route exact path="/editor/projects/:projectId" component={editorProjID} />
-          <Route exact path="/editor/projects" component={editorProject} />
-          <Route exact path="/editor/create" component={editorCreate} />
+          <Route exact path="/editor/:projectId" component={editorProjID} />
+          <Route exact path="/projects" component={editorProject} />
+          <Route exact path="/projects/create" component={editorCreate} />
         </>
       ) : (
         <>
-          <Route exact path="/editor/projects/:projectId" render={() => <UnauthorisedPage message={message} />} />
-          <Route exact path="/editor/projects" render={() => <UnauthorisedPage message={message} />} />
-          <Route exact path="/editor/create" render={() => <UnauthorisedPage message={message} />} />
+          <Route exact path="/editor/:projectId" render={() => <UnauthorisedPage message={message} />} />
+          <Route exact path="/projects" render={() => <UnauthorisedPage message={message} />} />
+          <Route exact path="/projects/create" render={() => <UnauthorisedPage message={message} />} />
         </>
       )}
-      <Redirect path="/editor" to="/editor/projects" />
     </Switch>
   )
 }
