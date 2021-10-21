06440e4eca00aa9dbcae865f49ad93eab909770b
Josh Field <joshfield999@gmail.com>
2021-10-21 16:04:22 +1100

    remove gallery

--- packages/gallery/src/pages/auth/confirm.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ConfirmEmail from '@xrengine/client-core/src/user/components/Auth/ConfirmEmailApp'
-
-export const ConfirmEmailPage = () => <ConfirmEmail logo="/assets/LogoColored.png" />
-
-export default ConfirmEmailPage
