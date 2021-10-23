54aacf31f9fa477fa85b1b3dcd05df6377d942e1
Kyle Baran <kbaran@bitscoop.com>
2021-10-23 14:19:05 -0700

    Fixes to build scripts.

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -9,7 +9,7 @@ REGION=$3
 docker container prune --force
 docker image prune --force
 
-aws ecr-public get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
+aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
 
 #docker pull $ECR_URL/$REPO_NAME:latest_$STAGE || true
 DOCKER_BUILDKIT=1 docker build --cache-from $ECR_URL/$REPO_NAME:latest_$STAGE --build-arg BUILDKIT_INLINE_CACHE=1 --tag $LABEL -f Dockerfile-build-deploy .
--- scripts/build_docker_builder.sh
+++ scripts/build_docker_builder.sh
@@ -4,9 +4,10 @@ set -x
 
 STAGE=$1
 LABEL=$2
+REGION=$3
 
 
-aws ecr-public get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
+aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
 
 #docker pull $ECR_URL/$REPO_NAME-builder:latest_$STAGE || true
 DOCKER_BUILDKIT=1 docker build --cache-from $ECR_URL/$REPO_NAME-builder:latest_$STAGE --build-arg BUILDKIT_INLINE_CACHE=1 --tag $LABEL -f Dockerfile-builder .
