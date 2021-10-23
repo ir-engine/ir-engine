2ab0ba7de7e2ea9245e1f8394daea5739e03aebf
Josh Field <joshfield999@gmail.com>
2021-10-23 22:17:54 +1100

    Revert "Update to projects api (#3829)"
    This reverts commit 19092924f9bf559fee9607464d45212c9853c6a6.

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,16 +4,11 @@ set -x
 
 STAGE=$1
 LABEL=$2
-REGION=$3
 
-<<<<<<< HEAD
-aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
-=======
 docker container prune --force
 docker image prune --force
 
 aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
->>>>>>> origin/dev
 
 #docker pull $ECR_URL/$REPO_NAME:latest_$STAGE || true
 DOCKER_BUILDKIT=1 docker build --cache-from $ECR_URL/$REPO_NAME:latest_$STAGE --build-arg BUILDKIT_INLINE_CACHE=1 --tag $LABEL -f Dockerfile-build-deploy .
