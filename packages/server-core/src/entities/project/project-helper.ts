ca172f8de96be7ae6f37831e67966c17748d5920
HexaField <joshfield999@gmail.com>
2021-10-25 08:40:58 +1100

    revert to old api namespace and use deployment patch

--- packages/server-core/src/entities/project/project-helper.ts
+++ packages/server-core/src/entities/project/project-helper.ts
@@ -2,11 +2,11 @@ import { Application } from '../../../declarations'
 import config from '../../appconfig'
 
 export const retriggerBuilderService = async (app: Application) => {
-  if (app.k8DefaultClient) {
+  if (app.k8AppsClient) {
     try {
       console.log('Attempting to reload k8s clients!')
-      const restartClientsResponse = await app.k8DefaultClient.patch(
-        `namespaces/default/pods/${config.server.releaseName}-builder-xrengine-builder`,
+      const restartClientsResponse = await app.k8AppsClient.patch(
+        `namespaces/default/deployments/${config.server.releaseName}-builder-xrengine-builder`,
         {
           spec: {
             template: {
