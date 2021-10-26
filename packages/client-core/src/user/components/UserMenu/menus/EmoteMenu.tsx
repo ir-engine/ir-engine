75010d52a4d220c2ebb6cc3c575b0af771ddab7d
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2021-10-26 14:53:25 +0500

    update svgs for emoticons to remove the circular radial shadows add pagination to the emote menu

--- packages/client-core/src/user/components/UserMenu/menus/EmoteMenu.tsx
+++ packages/client-core/src/user/components/UserMenu/menus/EmoteMenu.tsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button'
 import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 // @ts-ignore
 import styles from '../UserMenu.module.scss'
+import { NavigateNext, NavigateBefore } from '@material-ui/icons'
 import { hasComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
 import { LocalInputTagComponent } from '@xrengine/engine/src/input/components/LocalInputTagComponent'
 //import { LocalInputTagComponent } from '../../../../../engine/src/input/components/LocalInputTagComponent'
@@ -11,6 +12,13 @@ import { AnimationGraph } from '@xrengine/engine/src/avatar/animations/Animation
 import { Engine } from '@xrengine/engine/src/ecs/classes/Engine'
 
 const EmoteMenu = (props: any): any => {
+  const MAX_EMOTE_PER_PAGE = 6
+  const MIN_EMOTE_PER_PAGE = 5
+
+  const getEmotePerPage = () => (window.innerWidth > 768 ? MAX_EMOTE_PER_PAGE : MIN_EMOTE_PER_PAGE)
+  const [page, setPage] = useState(0)
+  const [imgPerPage, setImgPerPage] = useState(getEmotePerPage())
+
   let [menuRadius, setMenuRadius] = useState(window.innerWidth > 360 ? 182 : 150)
 
   let menuPadding = window.innerWidth > 360 ? 15 : 10
@@ -20,42 +28,6 @@ const EmoteMenu = (props: any): any => {
   let effectiveRadius = menuRadius - menuItemRadius - menuPadding / 2
 
   let [items, setItems] = useState([
-    /*{
-      body: <img src="/static/Dance1.svg" alt="Dance 1" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.DANCING_1 })
-      }
-    },
-    {
-      body: <img src="/static/Dance2.svg" alt="Dance 2" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.DANCING_2 })
-      }
-    },
-    {
-      body: <img src="/static/Dance3.svg" alt="Dance 3" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.DANCING_3 })
-      }
-    },
-    {
-      body: <img src="/static/Dance4.svg" alt="Dance 4" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.DANCING_4 })
-      }
-    },
-    {
-      body: <img src="/static/Clap.svg" alt="Clap" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.EMOTE, { animationName: AvatarAnimations.CLAP })
-      }
-    },
-    {
-      body: <img src="/static/Cry.svg" alt="Cry" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.EMOTE, { animationName: AvatarAnimations.CRY })
-      }
-    },
     {
       body: <img src="/static/Laugh.svg" alt="Laugh" />,
       containerProps: {
@@ -69,17 +41,11 @@ const EmoteMenu = (props: any): any => {
       }
     },
     {
-      body: <img src="/static/Kiss.svg" alt="Kiss" />,
-      containerProps: {
-        onClick: () => runAnimation(AvatarStates.EMOTE, { animationName: AvatarAnimations.KISS })
-      }
-    },
-    {
       body: <img src="/static/Wave.svg" alt="Wave" />,
       containerProps: {
         onClick: () => runAnimation(AvatarStates.EMOTE, { animationName: AvatarAnimations.WAVE })
       }
-    }*/
+    },
     {
       body: <img src="/static/grinning.svg" alt="Dance 4" />,
       containerProps: {
@@ -123,16 +89,17 @@ const EmoteMenu = (props: any): any => {
       containerProps: {
         onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.DANCING_3 })
       }
+    },
+    {
+      body: <img src="/static/restart.svg" />,
+      containerProps: {
+        onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.IDLE })
+      }
     }
-    // {
-    //   body: <img src="/static/restart.svg" />,
-    //   containerProps: {
-    //     onClick: () => runAnimation(AvatarStates.LOOPABLE_EMOTE, { animationName: AvatarAnimations.IDLE})
-    //   }
-    // }
   ])
 
   const calculateMenuRadius = () => {
+    setImgPerPage(getEmotePerPage())
     setMenuRadius(window.innerWidth > 360 ? 182 : 150)
     calculateOtherValues()
   }
@@ -163,6 +130,53 @@ const EmoteMenu = (props: any): any => {
     props.changeActiveMenu(null)
   }
 
+  const renderEmoteList = () => {
+    const itemList = []
+    const startIndex = page * imgPerPage
+    const endIndex = Math.min(startIndex + imgPerPage, items.length)
+    let angle = 360 / imgPerPage
+    let index = 0
+    let itemAngle = 0
+    let x = 0
+    let y = 0
+
+    for (let i = startIndex; i < endIndex; i++, index++) {
+      const emoticon = items[i]
+      itemAngle = angle * index + 270
+      x = effectiveRadius * Math.cos((itemAngle * Math.PI) / 280)
+      y = effectiveRadius * Math.sin((itemAngle * Math.PI) / 280)
+
+      itemList.push(
+        <div key={i}>
+          <Button
+            className={styles.menuItem}
+            {...emoticon.containerProps}
+            style={{
+              width: menuItemWidth,
+              height: menuItemWidth,
+              transform: `translate(${x}px , ${y}px)`
+            }}
+          >
+            {emoticon.body}
+          </Button>
+        </div>
+      )
+    }
+
+    return itemList
+  }
+
+  const loadNextEmotes = (e) => {
+    e.preventDefault()
+    if ((page + 1) * imgPerPage >= items.length) return
+    setPage(page + 1)
+  }
+  const loadPreviousEmotes = (e) => {
+    e.preventDefault()
+    if (page === 0) return
+    setPage(page - 1)
+  }
+
   return (
     <section className={styles.emoteMenu}>
       <ClickAwayListener onClickAway={closeMenu} mouseEvent="onMouseDown">
@@ -174,6 +188,15 @@ const EmoteMenu = (props: any): any => {
             borderWidth: menuThickness
           }}
         >
+          <div className={styles.itemContainerPrev}>
+            <button
+              type="button"
+              className={`${styles.iconBlock} ${page === 0 ? styles.disabled : ''}`}
+              onClick={loadPreviousEmotes}
+            >
+              <NavigateBefore />
+            </button>
+          </div>
           <div
             className={styles.menuItemBlock}
             style={{
@@ -181,26 +204,16 @@ const EmoteMenu = (props: any): any => {
               height: menuItemRadius
             }}
           >
-            {items.map((item, index) => {
-              const angle = 360 / items.length
-              const itemAngle = angle * index + 270
-              const x = effectiveRadius * Math.cos((itemAngle * Math.PI) / 280)
-              const y = effectiveRadius * Math.sin((itemAngle * Math.PI) / 280)
-              return (
-                <Button
-                  className={styles.menuItem}
-                  key={index}
-                  style={{
-                    width: menuItemWidth,
-                    height: menuItemWidth,
-                    transform: `translate(${x}px , ${y}px)`
-                  }}
-                  {...item.containerProps}
-                >
-                  {item.body}
-                </Button>
-              )
-            })}
+            {renderEmoteList()}
+          </div>
+          <div className={styles.itemContainerNext}>
+            <button
+              type="button"
+              className={`${styles.iconBlock} ${(page + 1) * imgPerPage >= items.length ? styles.disabled : ''}`}
+              onClick={loadNextEmotes}
+            >
+              <NavigateNext />
+            </button>
           </div>
         </div>
       </ClickAwayListener>
