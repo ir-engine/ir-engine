a8bc38e027e8c24fa946a477f65274728081bd0b
Josh Field <joshfield999@gmail.com>
2021-10-27 19:07:24 +1100

    project create modal

---
+++ packages/editor/src/components/projects/CreateProjectModal.tsx
@@ -0,0 +1,105 @@
+import classNames from 'classnames'
+import React, { useState } from 'react'
+import styles from '@xrengine/client-core/src/admin/components/Project/Projects.module.scss'
+import Backdrop from '@material-ui/core/Backdrop'
+import Button from '@material-ui/core/Button'
+import Fade from '@material-ui/core/Fade'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Modal from '@material-ui/core/Modal'
+import TextField from '@material-ui/core/TextField'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+interface Props {
+  open: boolean
+  handleClose: any
+  scenes?: any
+  avatars?: any
+  projects?: any
+}
+
+export const CreateProjectModal = (props: Props): any => {
+  const { open, handleClose, scenes } = props
+
+  const [processing, setProcessing] = useState(false)
+  const [error, setError] = useState('')
+  const [projectName, setProjectName] = useState('')
+
+  const showError = (err: string) => {
+    setError(err)
+    setTimeout(() => {
+      setError('')
+    }, 3000)
+  }
+
+  const createProject = async () => {
+    try {
+      if (projectName !== '') {
+        setProcessing(true)
+        // await uploadProject(projectName)
+        setProcessing(false)
+        closeModal()
+      }
+    } catch (err) {
+      setProcessing(false)
+      showError(err.message)
+    }
+  }
+
+  const closeModal = () => {
+    setProjectName('')
+    handleClose()
+  }
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={styles.modal}
+        open={open}
+        onClose={closeModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={props.open}>
+          <div
+            className={classNames({
+              [styles.paper]: true,
+              [styles['modal-content']]: true
+            })}
+          >
+            {processing === false && (
+              <div>
+                <FormControl>
+                  <div>
+                    <InputLabel id="nameSelect">Name</InputLabel>
+                    <TextField
+                      className={styles['pack-select']}
+                      id="nameSelect"
+                      value={projectName}
+                      onChange={(e) => setProjectName(e.target.value)}
+                    />
+                  </div>
+                  <Button type="submit" variant="contained" color="primary" onClick={createProject}>
+                    Create Project
+                  </Button>
+                </FormControl>
+              </div>
+            )}
+            {processing === true && (
+              <div className={styles.processing}>
+                <CircularProgress color="primary" />
+                <div className={styles.text}>Processing</div>
+              </div>
+            )}
+            {error && error.length > 0 && <h2 className={styles['error-message']}>{error}</h2>}
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  )
+}
