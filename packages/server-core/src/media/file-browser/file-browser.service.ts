46ecd84b2b66ebc1a7473d31655ede04ce6c86c5
Josh Field <joshfield999@gmail.com>
2021-10-28 09:39:41 +1100

    Remove Old Assets (#3885)
    * remove models, audio, avatars etc
    
    * remove a few more models and volumetric stuff
    
    * more assets

--- packages/server-core/src/media/file-browser/file-browser.service.ts
+++ packages/server-core/src/media/file-browser/file-browser.service.ts
@@ -2,20 +2,22 @@ import { Application } from '../../../declarations'
 import { FileBrowserService } from './file-browser.class'
 import hooks from './file-browser.hooks'
 
-export default (app: Application): any => {
-  const options = {
-    Model: null,
-    paginate: app.get('paginate'),
-    multi: true
+declare module '../../../declarations' {
+  interface ServiceTypes {
+    'file-browser': FileBrowserService
   }
+}
+
+export default (app: Application): any => {
+  const fileBrowser = new FileBrowserService()
+  // fileBrowser.docs = projectDocs
 
   /**
    * Initialize our service with any options it requires and docs
    *
    * @author Abhishek Pathak
    */
-  const event = new FileBrowserService()
-  app.use('/file-browser', new FileBrowserService())
+  app.use('file-browser', fileBrowser)
 
   /**
    * Get our initialized service so that we can register hooks
