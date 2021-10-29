52e7320b24d0f1a3146ca3e3be4ef5c61158f8bb
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2021-10-29 14:21:48 +0500

    update CloudsNodeEditor component to functional component

--- packages/editor/src/components/properties/CloudsNodeEditor.tsx
+++ packages/editor/src/components/properties/CloudsNodeEditor.tsx
@@ -22,89 +22,78 @@ type CloudsNodeEditorProps = {
  * @author Robert Long
  * @type {class component}
  */
-export class CloudsNodeEditor extends Component<CloudsNodeEditorProps> {
-  constructor(props: CloudsNodeEditorProps) {
-    super(props)
-    this.props = props
-  }
-
-  //setting iconComponent name
-  static iconComponent = Cloud
-
-  //setting description and will appears on editor view
-  static description = i18n.t('editor:properties.clouds.description')
-
-  declare props: CloudsNodeEditorProps
-
-  onChangeProperty = (name: string) => {
+const CloudsNodeEditor = (props: CloudsNodeEditorProps) => {
+  const onChangeProperty = (name: string) => {
     return (value) => {
       CommandManager.instance.setPropertyOnSelection(name, value)
     }
   }
 
   //rendering view
-  render() {
-    CloudsNodeEditor.description = this.props.t('editor:properties.clouds.description')
-    return (
-      <NodeEditor {...this.props} description={CloudsNodeEditor.description}>
-        <InputGroup name="Image" label={this.props.t('editor:properties.clouds.lbl-image')}>
-          <ImageInput value={this.props.node.texture} onChange={this.onChangeProperty('texture')} />
-        </InputGroup>
+  CloudsNodeEditor.description = props.t('editor:properties.clouds.description')
 
-        <InputGroup name="World Scale" label={this.props.t('editor:properties.clouds.lbl-wroldScale')}>
-          <Vector3Input
-            value={this.props.node.worldScale}
-            smallStep={0.01}
-            mediumStep={0.1}
-            largeStep={1}
-            onChange={this.onChangeProperty('worldScale')}
-          />
-        </InputGroup>
+  return (
+    <NodeEditor {...props} description={CloudsNodeEditor.description}>
+      <InputGroup name="Image" label={props.t('editor:properties.clouds.lbl-image')}>
+        <ImageInput value={props.node.texture} onChange={onChangeProperty('texture')} />
+      </InputGroup>
 
-        <InputGroup name="Dimensions" label={this.props.t('editor:properties.clouds.lbl-dimensions')}>
-          <Vector3Input
-            value={this.props.node.dimensions}
-            smallStep={1}
-            mediumStep={1}
-            largeStep={1}
-            onChange={this.onChangeProperty('dimensions')}
-          />
-        </InputGroup>
+      <InputGroup name="World Scale" label={props.t('editor:properties.clouds.lbl-wroldScale')}>
+        <Vector3Input
+          value={props.node.worldScale}
+          smallStep={0.01}
+          mediumStep={0.1}
+          largeStep={1}
+          onChange={onChangeProperty('worldScale')}
+        />
+      </InputGroup>
 
-        <InputGroup name="Noise Zoom" label={this.props.t('editor:properties.clouds.lbl-noiseZoom')}>
-          <Vector3Input
-            value={this.props.node.noiseZoom}
-            smallStep={0.01}
-            mediumStep={0.1}
-            largeStep={1}
-            onChange={this.onChangeProperty('noiseZoom')}
-          />
-        </InputGroup>
+      <InputGroup name="Dimensions" label={props.t('editor:properties.clouds.lbl-dimensions')}>
+        <Vector3Input
+          value={props.node.dimensions}
+          smallStep={1}
+          mediumStep={1}
+          largeStep={1}
+          onChange={onChangeProperty('dimensions')}
+        />
+      </InputGroup>
 
-        <InputGroup name="Noise Offset" label={this.props.t('editor:properties.clouds.lbl-noiseOffset')}>
-          <Vector3Input
-            value={this.props.node.noiseOffset}
-            smallStep={0.01}
-            mediumStep={0.1}
-            largeStep={1}
-            onChange={this.onChangeProperty('noiseOffset')}
-          />
-        </InputGroup>
+      <InputGroup name="Noise Zoom" label={props.t('editor:properties.clouds.lbl-noiseZoom')}>
+        <Vector3Input
+          value={props.node.noiseZoom}
+          smallStep={0.01}
+          mediumStep={0.1}
+          largeStep={1}
+          onChange={onChangeProperty('noiseZoom')}
+        />
+      </InputGroup>
 
-        <InputGroup name="Sprite Scale" label={this.props.t('editor:properties.clouds.lbl-spriteScale')}>
-          <Vector2Input value={this.props.node.spriteScaleRange} onChange={this.onChangeProperty('spriteScaleRange')} />
-        </InputGroup>
+      <InputGroup name="Noise Offset" label={props.t('editor:properties.clouds.lbl-noiseOffset')}>
+        <Vector3Input
+          value={props.node.noiseOffset}
+          smallStep={0.01}
+          mediumStep={0.1}
+          largeStep={1}
+          onChange={onChangeProperty('noiseOffset')}
+        />
+      </InputGroup>
 
-        <InputGroup name="Fog Color" label={this.props.t('editor:properties.clouds.lbl-fogColor')}>
-          <ColorInput value={this.props.node.fogColor} onChange={this.onChangeProperty('fogColor')} disabled={false} />
-        </InputGroup>
+      <InputGroup name="Sprite Scale" label={props.t('editor:properties.clouds.lbl-spriteScale')}>
+        <Vector2Input value={props.node.spriteScaleRange} onChange={onChangeProperty('spriteScaleRange')} />
+      </InputGroup>
 
-        <InputGroup name="Fog Range" label={this.props.t('editor:properties.clouds.lbl-fogRange')}>
-          <Vector2Input value={this.props.node.fogRange} onChange={this.onChangeProperty('fogRange')} />
-        </InputGroup>
-      </NodeEditor>
-    )
-  }
+      <InputGroup name="Fog Color" label={props.t('editor:properties.clouds.lbl-fogColor')}>
+        <ColorInput value={props.node.fogColor} onChange={onChangeProperty('fogColor')} disabled={false} />
+      </InputGroup>
+
+      <InputGroup name="Fog Range" label={props.t('editor:properties.clouds.lbl-fogRange')}>
+        <Vector2Input value={props.node.fogRange} onChange={onChangeProperty('fogRange')} />
+      </InputGroup>
+    </NodeEditor>
+  )
 }
+
+CloudsNodeEditor.iconComponent = Cloud
+CloudsNodeEditor.description = i18n.t('editor:properties.clouds.description')
 
 export default withTranslation()(CloudsNodeEditor)
