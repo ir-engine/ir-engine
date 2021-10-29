70de163745f7a42856d08c45e7a54d1476706758
Swarup Mukherjee <swpnit@gmail.com>
2021-10-29 09:39:24 +0530

    after format

--- packages/server-core/src/user/seeder-config.ts
+++ packages/server-core/src/user/seeder-config.ts
@@ -3,10 +3,6 @@ import { userSeed } from './user/user.seed'
 import { userRelationshipTypeSeed } from './user-relationship-type/user-relationship-type.seed'
 import { ServicesSeedConfig } from '@xrengine/common/src/interfaces/ServicesSeedConfig'
 
-export const userSeeds: Array<ServicesSeedConfig> = [
-  userRoleSeed,
-  userRelationshipTypeSeed,
-  userSeed
-]
+export const userSeeds: Array<ServicesSeedConfig> = [userRoleSeed, userRelationshipTypeSeed, userSeed]
 
 export default userSeeds
