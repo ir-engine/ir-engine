0092e10c8785f6b56dc07977abab71778155b66b
Swarup Mukherjee <swpnit@gmail.com>
2021-10-29 08:44:42 +0530

    inventoryItemType option changes

--- packages/server-core/src/entities/inventory-item-type/inventory-item-type.seed.ts
+++ packages/server-core/src/entities/inventory-item-type/inventory-item-type.seed.ts
@@ -4,8 +4,9 @@ export const inventoryItemTypeSeed = {
   path: 'inventory-item-type',
   randomize: false,
   templates: [
-    { type: inventoryItemType.scene },
-    { type: inventoryItemType.inventory },
-    { type: inventoryItemType.project }
+    { inventoryItemType: inventoryItemType.skill },
+    { inventoryItemType: inventoryItemType.weapon },
+    { inventoryItemType: inventoryItemType.ammo },
+    { inventoryItemType: inventoryItemType.player }
   ]
 }
