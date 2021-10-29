e607a9a2bb62a951c7de5d9fc77c1df9e975e062
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2021-10-29 12:59:02 +0500

    change AmbientLightNodeEditor to functional component

--- packages/editor/src/components/properties/AmbientLightNodeEditor.tsx
+++ packages/editor/src/components/properties/AmbientLightNodeEditor.tsx
@@ -11,6 +11,7 @@ import { CommandManager } from '../../managers/CommandManager'
 type AmbientLightNodeEditorProps = {
   node?: any
   t: Function
+  description: string
 }
 
 /**
@@ -21,47 +22,39 @@ type AmbientLightNodeEditorProps = {
  * @author Robert Long
  * @type {[component class]}
  */
-export class AmbientLightNodeEditor extends Component<AmbientLightNodeEditorProps, {}> {
-  //iconComponent used to show icon image on the ambient light element
-  static iconComponent = Sun
-  static description = i18n.t('editor:properties.ambientLight.description')
+const AmbientLightNodeEditor = (props: AmbientLightNodeEditorProps) => {
+  const node = props.node
 
   // used to change the color property of selected scene, when we change color property of ambient light
-  onChangeColor = (color) => {
+  const onChangeColor = (color) => {
     CommandManager.instance.setPropertyOnSelection('color', color)
   }
+
   // used to change the intensity of selected scene
-  onChangeIntensity = (intensity) => {
+  const onChangeIntensity = (intensity) => {
     CommandManager.instance.setPropertyOnSelection('intensity', intensity)
   }
 
-  /**
-   * Rendering ambient light view to customize ambient light element.
-   *
-   * @author Robert Long
-   */
-  render() {
-    AmbientLightNodeEditor.description = this.props.t('editor:properties.ambientLight.description')
-    const node = this.props.node
-    return (
-      <NodeEditor {...this.props} description={AmbientLightNodeEditor.description}>
-        <InputGroup name="Color" label={this.props.t('editor:properties.ambientLight.lbl-color')}>
-          <ColorInput value={node.color} onChange={this.onChangeColor} />
-        </InputGroup>
-        <NumericInputGroup
-          name="Intensity"
-          label={this.props.t('editor:properties.ambientLight.lbl-intensity')}
-          min={0}
-          smallStep={0.001}
-          mediumStep={0.01}
-          largeStep={0.1}
-          value={node.intensity}
-          onChange={this.onChangeIntensity}
-          unit="cd"
-        />
-      </NodeEditor>
-    )
-  }
+  return (
+    <NodeEditor {...props} description={props.t('editor:properties.ambientLight.description')}>
+      <InputGroup name="Color" label={props.t('editor:properties.ambientLight.lbl-color')}>
+        <ColorInput value={node.color} onChange={onChangeColor} />
+      </InputGroup>
+      <NumericInputGroup
+        name="Intensity"
+        label={props.t('editor:properties.ambientLight.lbl-intensity')}
+        min={0}
+        smallStep={0.001}
+        mediumStep={0.01}
+        largeStep={0.1}
+        value={node.intensity}
+        onChange={onChangeIntensity}
+        unit="cd"
+      />
+    </NodeEditor>
+  )
 }
+
+AmbientLightNodeEditor.iconComponent = Sun
 
 export default withTranslation()(AmbientLightNodeEditor)
