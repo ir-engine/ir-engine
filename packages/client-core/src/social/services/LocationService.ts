16d653fd16215490be57964de4d1847ab96d518e
HexaField <joshfield999@gmail.com>
2021-11-03 20:45:26 +1100

    fix hookstate receptor in LocationService.ts

--- packages/client-core/src/social/services/LocationService.ts
+++ packages/client-core/src/social/services/LocationService.ts
@@ -26,49 +26,53 @@ const state = createState({
 })
 
 store.receptors.push((action: LocationActionType): any => {
-  let newValues, updateMap
   state.batch((s) => {
     switch (action.type) {
       case 'LOCATIONS_RETRIEVED':
-        newValues = action.locations
-
-        if (s.locations.locations == null || s.updateNeeded.value === true) {
-          s.locations.locations.set(newValues.data)
-        } else {
-          s.locations.locations.set([...s.locations.locations.value, ...newValues.data])
-        }
-
-        s.locations.skip.set(newValues.skip)
-        s.locations.limit.set(newValues.limit)
-        s.locations.total.set(newValues.total)
-        return s.updateNeeded.set(false)
-
+        return s.merge({
+          locations: {
+            locations: action.locations.data,
+            limit: action.locations.limit,
+            skip: action.locations.skip,
+            total: action.locations.total
+          },
+          updateNeeded: false
+        })
       case 'FETCH_CURRENT_LOCATION':
-        return s.fetchingCurrentLocation.set(true)
-
+        return s.merge({
+          fetchingCurrentLocation: true
+        })
       case 'LOCATION_RETRIEVED':
-        newValues = action.location
-        newValues.locationSettings = newValues.location_setting
-
         let bannedUsers = [] as UserId[]
-        newValues?.location_bans?.forEach((ban) => {
+        ;(action.location as any)?.location_bans?.forEach((ban) => {
           bannedUsers.push(ban.userId)
         })
         bannedUsers = [...new Set(bannedUsers)]
-
-        s.currentLocation.location.set(newValues)
-        s.currentLocation.bannedUsers.set(bannedUsers)
-        s.currentLocationUpdateNeeded.set(false)
-        return s.fetchingCurrentLocation.set(false)
+        return s.merge({
+          currentLocation: {
+            location: {
+              ...action.location,
+              locationSettings: (action.location as any).location_settings
+            },
+            bannedUsers
+          },
+          currentLocationUpdateNeeded: false,
+          fetchingCurrentLocation: false
+        })
 
       case 'LOCATION_NOT_FOUND':
-        s.currentLocation.merge({ location: LocationSeed, bannedUsers: [] })
-        s.currentLocationUpdateNeeded.set(false)
-        s.fetchingCurrentLocation.set(false)
-        return s.invalidLocation.set(true)
+        return s.merge({
+          currentLocation: {
+            location: LocationSeed,
+            bannedUsers: []
+          },
+          currentLocationUpdateNeeded: false,
+          fetchingCurrentLocation: false,
+          invalidLocation: true
+        })
 
       case 'LOCATION_BAN_CREATED':
-        return s.currentLocationUpdateNeeded.set(true)
+        return s.merge({ currentLocationUpdateNeeded: true })
     }
   }, action.type)
 })
