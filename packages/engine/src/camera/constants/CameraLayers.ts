bc2efc67e75c61c3d9df5ef803c1968a3e5872de
HexaField <joshfield999@gmail.com>
2021-11-13 17:15:40 +1100

    add some new camera layers, default camera layer to avatar

--- packages/engine/src/camera/constants/CameraLayers.ts
+++ packages/engine/src/camera/constants/CameraLayers.ts
@@ -1,6 +1,8 @@
 export const CameraLayers = {
-  None: 0,
-  Avatar: 1,
+  Render: 0, // DEFAULT: TRUE - everything to render should have layer 0 enabled
+  Scene: 1, // DEFAULT: TRUE
   Portal: 2,
-  Scene: 3
+  Avatar: 3,
+  Gizmos: 4,
+  UI: 5
 }
