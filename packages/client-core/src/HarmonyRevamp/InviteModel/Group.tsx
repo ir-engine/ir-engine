f08e86e0b6e52731043201898ad5701259c7edb8
Kevin <kevingarry97@gmail.com>
2021-11-15 16:17:08 +0200

    implement empty box

--- packages/client-core/src/HarmonyRevamp/InviteModel/Group.tsx
+++ packages/client-core/src/HarmonyRevamp/InviteModel/Group.tsx
@@ -6,25 +6,25 @@ const Group = () => {
   const [type, setType] = React.useState('email')
   return (
     <React.Fragment>
-      <div className={`${classes.dFlex} ${classes.flexWrap} ${classes.alignCenter} ${classes.mx2}`}>
+      <div className={`${classes.dFlex} ${classes.flexWrap} ${classes.alignCenter} ${classes.mx0}`}>
         <a
           href="#"
           onClick={() => setType('email')}
-          className={`${type === 'email' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx2}`}
+          className={`${type === 'email' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx0}`}
         >
           <span>Email</span>
         </a>
         <a
           href="#"
           onClick={() => setType('phone')}
-          className={`${type === 'phone' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx2}`}
+          className={`${type === 'phone' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx0}`}
         >
           <span>Phone</span>
         </a>
         <a
           href="#"
           onClick={() => setType('code')}
-          className={`${type === 'code' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx2}`}
+          className={`${type === 'code' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${classes.mx0}`}
         >
           <span>Invite Code</span>
         </a>
@@ -32,7 +32,7 @@ const Group = () => {
           href="#"
           onClick={() => setType('friends')}
           className={`${type === 'friends' ? classes.bgPrimary : classes.border} ${classes.roundedCircle} ${
-            classes.mx2
+            classes.mx0
           }`}
         >
           <span>Friends</span>
@@ -40,6 +40,14 @@ const Group = () => {
       </div>
       <div className={classes.p5}>
         <form>
+          <div className="form-group">
+            <label htmlFor="" className={classes.mx2}>
+              <p>Group:</p>
+            </label>
+            <select className={classes.formControls}>
+              <option value="1">Test Group</option>
+            </select>
+          </div>
           {type === 'email' ? (
             <div className="form-group">
               <label htmlFor="" className={classes.mx2}>
@@ -49,17 +57,28 @@ const Group = () => {
             </div>
           ) : type === 'phone' ? (
             <div className="form-group">
-              <label htmlFor="">
+              <label htmlFor="" className={classes.mx2}>
                 <p>Phone:</p>
               </label>
               <input type="text" className={classes.formControls} placeholder="078XXXXXXX" />
             </div>
-          ) : (
+          ) : type === 'code' ? (
             <div className="form-group">
-              <label htmlFor="">
+              <label htmlFor="" className={classes.mx2}>
                 <p>Code:</p>
               </label>
               <input type="text" className={classes.formControls} placeholder="XXXXXX" />
+            </div>
+          ) : (
+            <div className="form-group">
+              <label htmlFor="" className={classes.mx2}>
+                <p>Friends:</p>
+              </label>
+              <select className={classes.formControls}>
+                <option value="1">Test Friend 1</option>
+                <option value="2">Test Friend 2</option>
+                <option value="3">Test Friend 3</option>
+              </select>
             </div>
           )}
           <div className={`${classes.dFlex} ${classes.my2}`} style={{ width: '100%' }}>
