a3124f5e0c416631dfa7648c93d770b8b0911325
Josh Field <joshfield999@gmail.com>
2021-11-18 10:27:01 +1100

    fix mimetypes being incorrectly set, increase max time for server core tests

--- packages/server-core/src/util/fileUtils.ts
+++ packages/server-core/src/util/fileUtils.ts
@@ -1,8 +1,5 @@
-export const getContentType = (url: string) => {
-  if (/.glb$/.test(url) === true) return 'glb'
-  if (/.jpeg$/.test(url) === true) return 'jpeg'
-  if (/.json$/.test(url) === true) return 'json'
-  if (/.ts$/.test(url) === true) return 'ts'
-  if (/.tsx$/.test(url) === true) return 'tsx'
-  return 'octet-stream'
+import mime from 'mime-types'
+
+export const getContentType = (url: string): string => {
+  return mime.lookup(url) || 'application/octet-stream'
 }
