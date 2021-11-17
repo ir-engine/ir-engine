4efedcbf2f3f035e85cc26302450e2629dd84c22
Josh Field <joshfield999@gmail.com>
2021-11-17 14:23:25 +1100

    path arg has fallback

--- packages/server-core/src/media/storageprovider/getCachedAsset.ts
+++ packages/server-core/src/media/storageprovider/getCachedAsset.ts
@@ -1,4 +1,4 @@
 export const getCachedAsset = (path: string, cacheDomain: string) => {
   if (!cacheDomain) throw new Error('No cache domain found - please check our storage provider configuration')
-  return new URL(path, 'https://' + cacheDomain).href
+  return new URL(path ?? '', 'https://' + cacheDomain).href
 }
