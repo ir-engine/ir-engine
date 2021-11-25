3a90beea89aaba24d1cce2ec3fbe5b8c2f460512
rondoor <webglsuperflow2021@gmail.com>
2021-11-25 17:37:54 -0600

    update functions

--- packages/client-core/src/util/cors.tsx
+++ packages/client-core/src/util/cors.tsx
@@ -1,6 +1,14 @@
 import i18n from 'i18next'
 import { Config } from '@xrengine/common/src/config'
 
-export const corsAnywhereUrl = (string: any): string => {
+export const corsAnywhereUrl = (url) => {
   return `${Config.publicRuntimeConfig.corsServer}/${url}`
+}
+
+export const isCorsAnywhereUrl = (url) => {
+  return url.includes(Config.publicRuntimeConfig.corsServer)
+}
+
+export const removeCorsAnywhereUrl = (url) => {
+  return url.replace(Config.publicRuntimeConfig.corsServer, '')
 }
