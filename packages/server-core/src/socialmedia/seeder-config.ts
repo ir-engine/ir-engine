2eb3db2ce44c2c3a498902b1bbe5a7b8cbaff874
Aleks951 <a.l.e.k.s.r.u.m.e.n98@gmail.com>
2021-11-26 17:04:38 +0200

    Delete social

--- packages/server-core/src/socialmedia/seeder-config.ts
+++
@@ -1,5 +0,0 @@
-import { ServicesSeedConfig } from '@xrengine/common/src/interfaces/ServicesSeedConfig'
-
-export const socialMediaSeeds: Array<ServicesSeedConfig> = []
-
-export default socialMediaSeeds
