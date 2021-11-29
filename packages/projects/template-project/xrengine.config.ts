0d4878231b929116f9754436c6c4f90502381a79
swpnit82 <48938076+swpnit82@users.noreply.github.com>
2021-11-29 10:29:24 +0530

    latest

---
+++ packages/projects/template-project/xrengine.config.ts
@@ -0,0 +1,11 @@
+import type { ProjectConfigInterface } from '@xrengine/projects/ProjectConfigInterface'
+
+const config: ProjectConfigInterface = {
+  onEvent: undefined,
+  thumbnail: '/static/xrengine_thumbnail.jpg',
+  routes: {},
+  services: undefined,
+  databaseSeed: undefined
+}
+
+export default config
