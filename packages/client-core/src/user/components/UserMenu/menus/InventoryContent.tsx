0f2c3ee9e6707516b504a9cbc156484b1f9f0f41
swpnit82 <48938076+swpnit82@users.noreply.github.com>
2021-12-01 15:49:45 +0530

    changes for new route

--- packages/client-core/src/user/components/UserMenu/menus/InventoryContent.tsx
+++ packages/client-core/src/user/components/UserMenu/menus/InventoryContent.tsx
@@ -1,7 +1,7 @@
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core'
-import { ArrowBackIos, FilterList } from '@material-ui/icons'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import makeStyles from '@mui/styles/makeStyles'
+import { ArrowBackIos, FilterList } from '@mui/icons-material'
 import React, { useEffect, useState } from 'react'
 import {
   Grid,
