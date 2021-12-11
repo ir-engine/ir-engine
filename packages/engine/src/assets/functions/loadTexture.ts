22e077a7726c355a3c6f88bdd32768fc860ba05d
Josh Field <joshfield999@gmail.com>
2021-12-11 14:05:55 +1100

    format

--- packages/engine/src/assets/functions/loadTexture.ts
+++ packages/engine/src/assets/functions/loadTexture.ts
@@ -4,8 +4,6 @@ import { isClient } from '../../common/functions/isClient'
 export default function loadTexture(src, textureLoader: any = new TextureLoader()) {
   return new Promise((resolve, reject) => {
     if (!isClient) resolve(null)
-    textureLoader.load(src, resolve, null, (error) =>
-      reject(new Error(`Error loading texture "${src}"`))
-    )
+    textureLoader.load(src, resolve, null, (error) => reject(new Error(`Error loading texture "${src}"`)))
   })
 }
