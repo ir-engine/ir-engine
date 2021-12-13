07b86ecd5a908223602ef8449bb3629e4ee0b165
Josh Field <joshfield999@gmail.com>
2021-12-14 08:11:19 +1100

    add back friend, group and invite services

---
+++ packages/client-core/src/social/services/FriendService.ts
@@ -0,0 +1,274 @@
+import { store, useDispatch } from '../../store'
+import { client } from '../../feathers'
+import { AlertService } from '../../common/services/AlertService'
+import { Config } from '@xrengine/common/src/config'
+import { UserAction } from '../../user/services/UserService'
+import { accessAuthState } from '../../user/services/AuthService'
+import { User } from '@xrengine/common/src/interfaces/User'
+import { UserRelationship } from '@xrengine/common/src/interfaces/UserRelationship'
+import { FriendResult } from '@xrengine/common/src/interfaces/FriendResult'
+import { createState, DevTools, useState, none, Downgraded } from '@hookstate/core'
+import _ from 'lodash'
+
+//State
+const state = createState({
+  friends: {
+    friends: [] as Array<User>,
+    total: 0,
+    limit: 5,
+    skip: 0
+  },
+  getFriendsInProgress: false,
+  updateNeeded: true
+})
+
+store.receptors.push((action: FriendActionType): any => {
+  let newValues, selfUser, otherUser, otherUserId
+  state.batch((s) => {
+    switch (action.type) {
+      case 'LOADED_FRIENDS':
+        newValues = action
+        if (s.updateNeeded.value === true) {
+          s.friends.friends.set(newValues.friends)
+        } else {
+          s.friends.friends.set([s.friends.friends.value, newValues.friends])
+        }
+        s.friends.skip.set(newValues.skip)
+        s.friends.limit.set(newValues.limit)
+        s.friends.total.set(newValues.total)
+        s.updateNeeded.set(false)
+        return s.getFriendsInProgress.set(false)
+
+      case 'CREATED_FRIEND':
+        newValues = action
+        const createdUserRelationship = newValues.userRelationship
+        s.friends.friends.set([...s.friends.friends.value, createdUserRelationship])
+      case 'PATCHED_FRIEND':
+        newValues = action
+        const patchedUserRelationship = newValues.userRelationship
+        selfUser = newValues.selfUser
+        otherUser =
+          patchedUserRelationship.userId === selfUser.id
+            ? patchedUserRelationship.relatedUser
+            : patchedUserRelationship.user
+
+        const patchedFriendIndex = s.friends.friends.value.findIndex((friendItem) => {
+          return friendItem != null && friendItem.id === otherUser.id
+        })
+        if (patchedFriendIndex === -1) {
+          return s.friends.friends.set([...s.friends.friends.value, otherUser])
+        } else {
+          return s.friends.friends[patchedFriendIndex].set(otherUser)
+        }
+
+      case 'REMOVED_FRIEND':
+        newValues = action
+        const removedUserRelationship = newValues.userRelationship
+        selfUser = newValues.selfUser
+        otherUserId =
+          removedUserRelationship.userId === selfUser.id
+            ? removedUserRelationship.relatedUserId
+            : removedUserRelationship.userId
+
+        const friendId = s.friends.friends.value.findIndex((friendItem) => {
+          return friendItem != null && friendItem.id === otherUserId
+        })
+
+        return s.friends.friends[friendId].set(none)
+      case 'FETCHING_FRIENDS':
+        return s.getFriendsInProgress.set(true)
+    }
+  }, action.type)
+})
+
+export const accessFriendState = () => state
+
+export const useFriendState = () => useState(state) as any as typeof state
+
+//Service
+export const FriendService = {
+  // export function getUserRelationshipasync (userId: string) {
+  // const dispatch = useDispatch(); {
+  //     // dispatch(actionProcessing(true))
+  //
+  //     console.log('------get relations-------', userId)
+  //     client.service('user-relationship').find({
+  //       query: {
+  //         userId
+  //       }
+  //     }).then((res: any) => {
+  //       console.log('relations------', res)
+  //       dispatch(loadedUserRelationship(res as Relationship))
+  //     })
+  //       .catch((err: any) => {
+  //         console.log(err)
+  //       })
+  //       // .finally(() => dispatch(actionProcessing(false)))
+  //   }
+  // }
+
+  getFriends: async (search: string, skip?: number, limit?: number) => {
+    const dispatch = useDispatch()
+    {
+      dispatch(FriendAction.fetchingFriends())
+      try {
+        const friendState = accessFriendState()
+        const friendResult = await client.service('user').find({
+          query: {
+            action: 'friends',
+            $limit: limit != null ? limit : friendState.friends.limit.value,
+            $skip: skip != null ? skip : friendState.friends.skip.value,
+            search
+          }
+        })
+        dispatch(FriendAction.loadedFriends(friendResult))
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+        dispatch(FriendAction.loadedFriends({ data: [], limit: 0, skip: 0, total: 0 }))
+      }
+    }
+  },
+
+  // function createRelationasync (userId: string, relatedUserId: string, type: 'friend' | 'blocking') {
+  // const dispatch = useDispatch(); {
+  //     client.service('user-relationship').create({
+  //       relatedUserId,
+  //       userRelationshipType: type
+  //     }).then((res: any) => {
+  //       console.log('add relations------', res)
+  //       dispatch(changedRelation())
+  //     })
+  //       .catch((err: any) => {
+  //         console.log(err)
+  //       })
+  //       // .finally(() => dispatch(actionProcessing(false)))
+  //   }
+  // }
+  //
+  removeFriend: async (relatedUserId: string) => {
+    const dispatch = useDispatch()
+    {
+      try {
+        await client.service('user-relationship').remove(relatedUserId)
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  //
+  // function patchRelationasync (userId: string, relatedUserId: string, type: 'friend') {
+  // const dispatch = useDispatch(); {
+  //     client.service('user-relationship').patch(relatedUserId, {
+  //       userRelationshipType: type
+  //     }).then((res: any) => {
+  //       console.log('Patching relationship to friend', res)
+  //       dispatch(changedRelation())
+  //     })
+  //       .catch((err: any) => {
+  //         console.log(err)
+  //       })
+  //       // .finally(() => dispatch(actionProcessing(false)))
+  //   }
+  // }
+
+  // export function requestFriend(userId: string, relatedUserId: string) {
+  //   return createRelation(userId, relatedUserId, 'friend')
+  // }
+  //
+  // export function blockUser(userId: string, relatedUserId: string) {
+  //   return createRelation(userId, relatedUserId, 'blocking')
+  // }
+  //
+  // export function acceptFriend(userId: string, relatedUserId: string) {
+  //   return patchRelation(userId, relatedUserId, 'friend')
+  // }
+  //
+  // export function declineFriend(userId: string, relatedUserId: string) {
+  //   return removeRelation(userId, relatedUserId)
+  // }
+  //
+  // export function cancelBlock(userId: string, relatedUserId: string) {
+  //   return removeRelation(userId, relatedUserId)
+  // }
+
+  unfriend: (relatedUserId: string) => {
+    return FriendService.removeFriend(relatedUserId)
+  }
+}
+if (!Config.publicRuntimeConfig.offlineMode) {
+  client.service('user-relationship').on('created', (params) => {
+    if (params.userRelationship.userRelationshipType === 'friend') {
+      store.dispatch(FriendAction.createdFriend(params.userRelationship))
+    }
+  })
+
+  client.service('user-relationship').on('patched', (params) => {
+    const patchedUserRelationship = params.userRelationship
+    const selfUser = accessAuthState().user
+    if (patchedUserRelationship.userRelationshipType === 'friend') {
+      store.dispatch(FriendAction.patchedFriend(patchedUserRelationship, selfUser.value))
+      if (
+        patchedUserRelationship.user.channelInstanceId != null &&
+        patchedUserRelationship.user.channelInstanceId === selfUser.channelInstanceId.value
+      )
+        store.dispatch(UserAction.addedChannelLayerUser(patchedUserRelationship.user))
+      if (patchedUserRelationship.user.channelInstanceId !== selfUser.channelInstanceId.value)
+        store.dispatch(UserAction.removedChannelLayerUser(patchedUserRelationship.user))
+    }
+  })
+
+  client.service('user-relationship').on('removed', (params) => {
+    const deletedUserRelationship = params.userRelationship
+    const selfUser = accessAuthState().user
+    if (deletedUserRelationship.userRelationshipType === 'friend') {
+      store.dispatch(FriendAction.removedFriend(deletedUserRelationship, selfUser.value))
+      if (
+        deletedUserRelationship.user.channelInstanceId != null &&
+        deletedUserRelationship.user.channelInstanceId === selfUser.channelInstanceId.value
+      )
+        store.dispatch(UserAction.addedChannelLayerUser(deletedUserRelationship.user))
+      if (deletedUserRelationship.user.channelInstanceId !== selfUser.channelInstanceId.value)
+        store.dispatch(UserAction.removedChannelLayerUser(deletedUserRelationship.user))
+    }
+  })
+}
+
+//Action
+export const FriendAction = {
+  loadedFriends: (friendResult: FriendResult) => {
+    return {
+      type: 'LOADED_FRIENDS' as const,
+      friends: friendResult.data,
+      total: friendResult.total,
+      limit: friendResult.limit,
+      skip: friendResult.skip
+    }
+  },
+  createdFriend: (userRelationship: UserRelationship) => {
+    return {
+      type: 'CREATED_FRIEND' as const,
+      userRelationship: userRelationship
+    }
+  },
+  patchedFriend: (userRelationship: UserRelationship, selfUser: User) => {
+    return {
+      type: 'PATCHED_FRIEND' as const,
+      userRelationship: userRelationship,
+      selfUser: selfUser
+    }
+  },
+  removedFriend: (userRelationship: UserRelationship, selfUser: User) => {
+    return {
+      type: 'REMOVED_FRIEND' as const,
+      userRelationship: userRelationship,
+      selfUser: selfUser
+    }
+  },
+  fetchingFriends: () => {
+    return {
+      type: 'FETCHING_FRIENDS' as const
+    }
+  }
+}
+
+export type FriendActionType = ReturnType<typeof FriendAction[keyof typeof FriendAction]>
---
+++ packages/client-core/src/social/services/GroupService.ts
@@ -0,0 +1,416 @@
+import { Config } from '@xrengine/common/src/config'
+import { store, useDispatch } from '../../store'
+import { AlertService } from '../../common/services/AlertService'
+import { client } from '../../feathers'
+import { UserAction } from '../../user/services/UserService'
+import { accessAuthState } from '../../user/services/AuthService'
+import { ChatService } from './ChatService'
+import waitForClientAuthenticated from '../../util/wait-for-client-authenticated'
+import { Group } from '@xrengine/common/src/interfaces/Group'
+import { GroupUser } from '@xrengine/common/src/interfaces/GroupUser'
+import { GroupResult } from '@xrengine/common/src/interfaces/GroupResult'
+import { createState, DevTools, useState, none, Downgraded } from '@hookstate/core'
+import _ from 'lodash'
+
+//State
+
+const state = createState({
+  groups: {
+    groups: [] as Array<Group>,
+    total: 0,
+    limit: 5,
+    skip: 0
+  },
+  invitableGroups: {
+    groups: [] as Array<Group>,
+    total: 0,
+    limit: 5,
+    skip: 0
+  },
+  getInvitableGroupsInProgress: false,
+  getGroupsInProgress: false,
+  invitableUpdateNeeded: true,
+  updateNeeded: true,
+  closeDetails: ''
+})
+
+store.receptors.push((action: GroupActionType): any => {
+  let newValues,
+    updateMap,
+    updateMapGroups,
+    updateMapGroupsChild,
+    updateMapGroupUsers,
+    groupUser,
+    updateGroup,
+    groupIndex,
+    groupUserIndex
+  state.batch((s) => {
+    switch (action.type) {
+      case 'LOADED_GROUPS':
+        newValues = action
+        if (s.updateNeeded.value === true) {
+          s.groups.groups.set(newValues.groups)
+        } else {
+          s.groups.groups.set([...s.groups.groups.value, ...newValues.groups])
+        }
+        s.groups.merge({ skip: newValues.skip, limit: newValues.limit, total: newValues.total })
+        s.updateNeeded.set(false)
+        return s.getGroupsInProgress.set(false)
+
+      case 'LOADED_INVITABLE_GROUPS':
+        newValues = action
+
+        if (s.updateNeeded.value === true) {
+          s.invitableGroups.groups.set(newValues.groups)
+        } else {
+          s.invitableGroups.groups.set([...s.invitableGroups.groups.value, ...newValues.groups])
+        }
+        s.invitableGroups.skip.set(newValues.skip)
+        s.invitableGroups.limit.set(newValues.limit)
+        s.invitableGroups.total.set(newValues.total)
+        return s.merge({ invitableUpdateNeeded: false, getInvitableGroupsInProgress: false })
+
+      case 'CREATED_GROUP':
+        newValues = action
+        return s.merge({ updateNeeded: true, invitableUpdateNeeded: true })
+      case 'PATCHED_GROUP':
+        newValues = action
+        updateGroup = newValues.group
+
+        groupIndex = s.groups.groups.value.findIndex((groupItem) => {
+          return groupItem != null && groupItem.id === groupUser.groupId
+        })
+        if (groupIndex !== -1) {
+          return s.groups.groups[groupIndex].set(updateGroup)
+        }
+        return s
+      case 'REMOVED_GROUP':
+        s.updateNeeded.set(true)
+        return s.invitableUpdateNeeded.set(true)
+      case 'INVITED_GROUP_USER':
+        return s
+      // .set('updateNeeded', true)
+      case 'LEFT_GROUP':
+        return s.updateNeeded.set(true)
+      case 'FETCHING_GROUPS':
+        return s.getGroupsInProgress.set(true)
+      case 'FETCHING_INVITABLE_GROUPS':
+        return s.getInvitableGroupsInProgress.set(true)
+      case 'CREATED_GROUP_USER':
+        newValues = action
+        groupUser = newValues.groupUser
+        groupIndex = s.groups.groups.value.findIndex((groupItem) => {
+          return groupItem != null && groupItem.id === groupUser.groupId
+        })
+        if (groupIndex !== -1) {
+          const group = s.groups.groups[groupIndex]
+          groupUserIndex = group.groupUsers.value.findIndex((groupUserItem) => {
+            return groupUserItem != null && groupUserItem.id === groupUser.id
+          })
+          if (groupUserIndex !== -1) {
+            group.groupUsers[groupUserIndex].set(groupUser)
+          } else {
+            group.groupUsers.merge(groupUser)
+          }
+        }
+
+      case 'PATCHED_GROUP_USER':
+        newValues = action
+        groupUser = newValues.groupUser
+        groupIndex = s.groups.groups.value.findIndex((groupItem) => {
+          return groupItem != null && groupItem.id === groupUser.groupId
+        })
+        if (groupIndex !== -1) {
+          const group = s.groups.groups[groupIndex]
+          groupUserIndex = group.groupUsers.value.findIndex((groupUserItem) => {
+            return groupUserItem != null && groupUserItem.id === groupUser.id
+          })
+          if (groupUserIndex !== -1) {
+            group.groupUsers[groupUserIndex].set(groupUser)
+          } else {
+            group.groupUsers.merge(groupUser)
+          }
+        }
+        // updateMap = s.groups.value
+        // updateMapGroups = updateMap.groups
+        // updateMapGroupsChild = _.find(updateMapGroups, (group) => {
+        //   return group != null && group.id === groupUser.groupId
+        // })
+        // if (updateMapGroupsChild != null) {
+        //   updateMapGroupsChild.groupUsers = updateMapGroupsChild.groupUsers.map((gUser) =>
+        //     gUser.id === groupUser.id ? groupUser : gUser
+        //   )
+        // }
+        // s.groups.groups.set(updateMapGroups)
+        return s
+
+      case 'REMOVED_GROUP_USER':
+        newValues = action
+        groupUser = newValues.groupUser
+        const self = newValues.self
+        groupIndex = s.groups.groups.value.findIndex((groupItem) => {
+          return groupItem != null && groupItem.id === groupUser.groupId
+        })
+        if (groupIndex !== -1) {
+          const group = s.groups.groups[groupIndex]
+          groupUserIndex = group.groupUsers.value.findIndex((groupUserItem) => {
+            return groupUserItem != null && groupUserItem.id === groupUser.id
+          })
+          if (groupUserIndex !== -1) {
+            group.groupUsers.merge({
+              [groupUserIndex]: none
+            })
+          }
+        }
+
+        return self === true ? s.merge({ closeDetails: groupUser.groupId, updateNeeded: true }) : s
+
+      case 'REMOVE_CLOSE_GROUP_DETAIL':
+        return s.closeDetails.set('')
+    }
+  }, action.type)
+})
+
+export const accessGroupState = () => state
+
+export const useGroupState = () => useState(state) as any as typeof state
+
+//Service
+export const GroupService = {
+  getGroups: async (skip?: number, limit?: number) => {
+    const dispatch = useDispatch()
+    {
+      dispatch(GroupAction.fetchingGroups())
+      const groupActionState = accessGroupState().value
+      try {
+        const groupResults = await client.service('group').find({
+          query: {
+            $limit: limit != null ? limit : groupActionState.groups.limit,
+            $skip: skip != null ? skip : groupActionState.groups.skip
+          }
+        })
+        dispatch(GroupAction.loadedGroups(groupResults))
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  createGroup: async (values: any) => {
+    const dispatch = useDispatch()
+    {
+      try {
+        await client.service('group').create({
+          name: values.name,
+          description: values.description
+        })
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  patchGroup: async (values: any) => {
+    const dispatch = useDispatch()
+    {
+      const patch = {}
+      if (values.name != null) {
+        ;(patch as any).name = values.name
+      }
+      if (values.description != null) {
+        ;(patch as any).description = values.description
+      }
+      try {
+        await client.service('group').patch(values.id, patch)
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  removeGroup: async (groupId: string) => {
+    const dispatch = useDispatch()
+    {
+      try {
+        const channelResult = (await client.service('channel').find({
+          query: {
+            groupId: groupId
+          }
+        })) as any
+        if (channelResult.total > 0) {
+          await client.service('channel').remove(channelResult.data[0].id)
+        }
+        await client.service('group').remove(groupId)
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  removeGroupUser: async (groupUserId: string) => {
+    const dispatch = useDispatch()
+    {
+      try {
+        await client.service('group-user').remove(groupUserId)
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+      }
+    }
+  },
+  getInvitableGroups: async (skip?: number, limit?: number) => {
+    const dispatch = useDispatch()
+    {
+      dispatch(GroupAction.fetchingInvitableGroups())
+      const groupActionState = accessGroupState().value
+      try {
+        await waitForClientAuthenticated()
+        const groupResults = await client.service('group').find({
+          query: {
+            invitable: true,
+            $limit: limit != null ? limit : groupActionState.groups.limit,
+            $skip: skip != null ? skip : groupActionState.groups.skip
+          }
+        })
+        dispatch(GroupAction.loadedInvitableGroups(groupResults))
+      } catch (err) {
+        AlertService.dispatchAlertError(err)
+        dispatch(GroupAction.loadedInvitableGroups({ data: [], limit: 0, skip: 0, total: 0 }))
+      }
+    }
+  }
+}
+if (!Config.publicRuntimeConfig.offlineMode) {
+  client.service('group-user').on('created', (params) => {
+    const newGroupUser = params.groupUser
+    const selfUser = accessAuthState().user
+    store.dispatch(GroupAction.createdGroupUser(newGroupUser))
+    if (
+      newGroupUser.user.channelInstanceId != null &&
+      newGroupUser.user.channelInstanceId === selfUser.channelInstanceId.value
+    )
+      store.dispatch(UserAction.addedChannelLayerUser(newGroupUser.user))
+    if (newGroupUser.user.channelInstanceId !== selfUser.channelInstanceId.value)
+      store.dispatch(UserAction.removedChannelLayerUser(newGroupUser.user))
+  })
+
+  client.service('group-user').on('patched', (params) => {
+    const updatedGroupUser = params.groupUser
+    const selfUser = accessAuthState().user
+    store.dispatch(GroupAction.patchedGroupUser(updatedGroupUser))
+    if (
+      updatedGroupUser.user.channelInstanceId != null &&
+      updatedGroupUser.user.channelInstanceId === selfUser.channelInstanceId.value
+    )
+      store.dispatch(UserAction.addedChannelLayerUser(updatedGroupUser.user))
+    if (updatedGroupUser.user.channelInstanceId !== selfUser.channelInstanceId.value)
+      store.dispatch(UserAction.removedChannelLayerUser(updatedGroupUser.user))
+  })
+
+  client.service('group-user').on('removed', (params) => {
+    const deletedGroupUser = params.groupUser
+    const selfUser = accessAuthState().user
+    store.dispatch(GroupAction.removedGroupUser(deletedGroupUser, params.self))
+    store.dispatch(UserAction.removedChannelLayerUser(deletedGroupUser.user))
+    if (deletedGroupUser.userId === selfUser.id.value)
+      ChatService.clearChatTargetIfCurrent('group', { id: params.groupUser.groupId })
+  })
+
+  client.service('group').on('created', (params) => {
+    store.dispatch(GroupAction.createdGroup(params.group))
+  })
+
+  client.service('group').on('patched', (params) => {
+    store.dispatch(GroupAction.patchedGroup(params.group))
+  })
+
+  client.service('group').on('removed', (params) => {
+    store.dispatch(GroupAction.removedGroup(params.group))
+    ChatService.clearChatTargetIfCurrent('group', params.group)
+  })
+
+  client.service('group').on('refresh', (params) => {
+    store.dispatch(GroupAction.createdGroup(params.group))
+  })
+}
+
+//Action
+export const GroupAction = {
+  loadedGroups: (groupResult: GroupResult) => {
+    return {
+      type: 'LOADED_GROUPS' as const,
+      groups: groupResult.data,
+      total: groupResult.total,
+      limit: groupResult.limit,
+      skip: groupResult.skip
+    }
+  },
+  createdGroup: (group: Group) => {
+    return {
+      type: 'CREATED_GROUP' as const,
+      group: group
+    }
+  },
+  patchedGroup: (group: Group) => {
+    return {
+      type: 'PATCHED_GROUP' as const,
+      group: group
+    }
+  },
+  removedGroup: (group: Group) => {
+    return {
+      type: 'REMOVED_GROUP' as const,
+      group: group
+    }
+  },
+  createdGroupUser: (groupUser: GroupUser) => {
+    return {
+      type: 'CREATED_GROUP_USER' as const,
+      groupUser: groupUser
+    }
+  },
+  patchedGroupUser: (groupUser: GroupUser) => {
+    return {
+      type: 'PATCHED_GROUP_USER' as const,
+      groupUser: groupUser
+    }
+  },
+  removedGroupUser: (groupUser: GroupUser, self: boolean) => {
+    return {
+      type: 'REMOVED_GROUP_USER' as const,
+      groupUser: groupUser,
+      self: self
+    }
+  },
+  invitedGroupUser: () => {
+    return {
+      type: 'INVITED_GROUP_USER' as const
+    }
+  },
+  leftGroup: () => {
+    return {
+      type: 'LEFT_GROUP' as const
+    }
+  },
+  fetchingGroups: () => {
+    return {
+      type: 'FETCHING_GROUPS' as const
+    }
+  },
+  loadedInvitableGroups: (groupResult: GroupResult) => {
+    return {
+      type: 'LOADED_INVITABLE_GROUPS' as const,
+      groups: groupResult.data,
+      total: groupResult.total,
+      limit: groupResult.limit,
+      skip: groupResult.skip
+    }
+  },
+  fetchingInvitableGroups: () => {
+    return {
+      type: 'FETCHING_INVITABLE_GROUPS' as const
+    }
+  },
+  removeCloseGroupDetail: () => {
+    return {
+      type: 'REMOVE_CLOSE_GROUP_DETAIL' as const
+    }
+  }
+}
+
+export type GroupActionType = ReturnType<typeof GroupAction[keyof typeof GroupAction]>
