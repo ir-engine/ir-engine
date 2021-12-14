0fdcb792ca43f20269be0301271a938e67b6f1f6
Josh Field <joshfield999@gmail.com>
2021-12-14 21:42:30 +1100

    Remove ARC Services (#4453)
    * Working version
    
    * Delete social
    
    * return services
    
    * Correct services
    
    * clean up
    
    * add back friend, group and invite services
    
    Co-authored-by: Aleks951 <a.l.e.k.s.r.u.m.e.n98@gmail.com>

--- packages/social/src/pages/auth/forgotpassword.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ForgotPassword from '@xrengine/client-core/src/user/components/Auth/ForgotPassword'
-
-export const ForgotPasswordPage = () => <ForgotPassword />
-
-export default ForgotPasswordPage
