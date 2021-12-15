aca21cad52179c2ee8796726f4f17b529c603e37
Josh Field <joshfield999@gmail.com>
2021-12-15 11:31:25 +1100

    add admin asset upload service and unify avatar upload

--- packages/common/src/interfaces/UploadAssetInterface.ts
+++ packages/common/src/interfaces/UploadAssetInterface.ts
@@ -1,4 +1,24 @@
-export type AssetUploadArguments = {
+export type AvatarUploadType = {
+  type: 'user-avatar-upload'
   files: (Blob | Buffer)[]
-  args: any
+  args: {
+    avatarName: string
+    isPublicAvatar: boolean
+  }
 }
+
+export type AdminAssetUploadArgumentsType = {
+  key: string
+  contentType: string
+  staticResourceType: string
+  userId?: string
+  name?: string
+}
+
+export type AdminAssetUploadType = {
+  type: 'admin-file-upload'
+  files: (Blob | Buffer)[]
+  args: Array<AdminAssetUploadArgumentsType>
+}
+
+export type AssetUploadType = AvatarUploadType | AdminAssetUploadType
