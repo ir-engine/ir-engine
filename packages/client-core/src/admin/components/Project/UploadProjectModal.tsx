a7edf4d226283358fbf4d6331b4cab397a41795c
大山 <92342281+rondoor124@users.noreply.github.com>
2021-12-16 03:33:12 -0600

    Github App OAuth #4198 (#4423)
    * github app update
    
    * updat package.json
    
    * change git pem to be a string and add to server settings table
    
    * update github env
    
    * update ui
    
    * update pem contents from DB
    
    * remove pem for local env
    
    * update admin panel
    
    * disable test mode
    
    * add appid on admin auth page
    
    * refreshAppConfig by patch
    
    * Fixed issue with project.update passing null as params to super.remove
    
    Co-authored-by: HexaField <joshfield999@gmail.com>
    Co-authored-by: Kyle Baran <kbaran@bitscoop.com>

--- packages/client-core/src/admin/components/Project/UploadProjectModal.tsx
+++ packages/client-core/src/admin/components/Project/UploadProjectModal.tsx
@@ -8,11 +8,18 @@ import Fade from '@mui/material/Fade'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import Modal from '@mui/material/Modal'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 import TextField from '@mui/material/TextField'
 import CircularProgress from '@mui/material/CircularProgress'
+import GroupIcon from '@mui/icons-material/Group'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import { useAuthState, AuthService } from '@xrengine/client-core/src/user/services/AuthService'
+import { GithubAppInterface } from '@xrengine/common/src/interfaces/GithubAppInterface'
 
 interface Props {
   open: boolean
+  repos: any
   handleClose: any
   scenes?: any
   avatars?: any
@@ -20,18 +27,26 @@ interface Props {
 }
 
 const UploadProjectModal = (props: Props): any => {
-  const { open, handleClose, scenes } = props
+  const authState = useAuthState()
+  const authType = useAuthState().authUser.identityProvider.type.value
+
+  const { open, handleClose, scenes, repos } = props
 
   const [processing, setProcessing] = useState(false)
   const [error, setError] = useState('')
   const [createOrPatch, setCreateOrPatch] = useState('patch')
   const [projectURL, setProjectURL] = useState('')
+  const [isPublicUrl, setIsPublicUrl] = useState(false)
   const dispatch = useDispatch()
   const showError = (err: string) => {
     setError(err)
     setTimeout(() => {
       setError('')
     }, 3000)
+  }
+
+  const trySelectPublicUrl = () => {
+    setIsPublicUrl(!isPublicUrl)
   }
 
   const tryUploadProject = async () => {
@@ -73,18 +88,62 @@ const UploadProjectModal = (props: Props): any => {
             {processing === false && createOrPatch === 'patch' && (
               <FormControl>
                 <div className={styles.inputConatiner}>
-                  <TextField
-                    className={styles['pack-select']}
-                    id="urlSelect"
-                    value={projectURL}
-                    placeholder={'URL'}
-                    onChange={(e) => setProjectURL(e.target.value)}
-                  />
+                  {!isPublicUrl && repos && repos.length != 0 ? (
+                    <Select
+                      labelId="demo-controlled-open-select-label"
+                      id="demo-controlled-open-select"
+                      value={projectURL}
+                      fullWidth
+                      displayEmpty
+                      onChange={(e) => setProjectURL(e.target.value)}
+                      name="projectURL"
+                    >
+                      <MenuItem value="" disabled>
+                        <em>Select Project</em>
+                      </MenuItem>
+                      {repos &&
+                        repos.map((el: any, i) => (
+                          <MenuItem value={`${el.repositoryPath.value}`} key={i}>
+                            {el.name.value} ({el.user.value})
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  ) : (
+                    <div>
+                      <label>Please insert github public url</label>
+                      <TextField
+                        className={styles['pack-select']}
+                        id="urlSelect"
+                        value={projectURL}
+                        placeholder={'URL'}
+                        onChange={(e) => setProjectURL(e.target.value)}
+                      />
+                    </div>
+                  )}
                 </div>
                 <div className={styles.buttonConatiner}>
-                  <Button type="submit" variant="contained" color="primary" onClick={tryUploadProject}>
+                  <Button
+                    type="submit"
+                    startIcon={<GitHubIcon />}
+                    variant="contained"
+                    color="primary"
+                    onClick={tryUploadProject}
+                  >
                     Upload Project
                   </Button>
+                  {repos && repos.length != 0 ? (
+                    <Button
+                      type="submit"
+                      startIcon={<GroupIcon />}
+                      variant="contained"
+                      color="primary"
+                      onClick={trySelectPublicUrl}
+                    >
+                      {!isPublicUrl ? 'Custom Public Url' : 'Select From List'}
+                    </Button>
+                  ) : (
+                    <></>
+                  )}
                 </div>
               </FormControl>
             )}
