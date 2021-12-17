0acdcbd1b15d07e3d9007aefe351f80d1f26eef7
alextoti <alexandrostitonis@gmail.com>
2021-12-17 13:15:09 +0200

    fixed the createAvatar test

--- packages/engine/src/avatar/functions/createAvatar.test.ts
+++ packages/engine/src/avatar/functions/createAvatar.test.ts
@@ -1,143 +1,65 @@
-import assert, { strictEqual } from 'assert'
-import { AnimationClip, AnimationMixer, Group, PerspectiveCamera, Quaternion, Vector3 } from 'three'
-import { createQuaternionProxy, createVector3Proxy } from '../../common/proxies/three'
+import { strictEqual } from 'assert'
+import { Quaternion, Vector3 } from 'three'
+import { TestNetwork } from '../../../tests/networking/TestNetwork'
 import { Engine } from '../../ecs/classes/Engine'
-import { Entity } from '../../ecs/classes/Entity'
 import { createWorld } from '../../ecs/classes/World'
 import { addComponent, hasComponent } from '../../ecs/functions/ComponentFunctions'
 import { createEntity } from '../../ecs/functions/EntityFunctions'
-import { ColliderComponent } from '../../physics/components/ColliderComponent'
+import { Network } from '../../networking/classes/Network'
+import { NetworkObjectComponent } from '../../networking/components/NetworkObjectComponent'
 import { CollisionComponent } from '../../physics/components/CollisionComponent'
 import { RaycastComponent } from '../../physics/components/RaycastComponent'
 import { VelocityComponent } from '../../physics/components/VelocityComponent'
-import { CollisionGroups } from '../../physics/enums/CollisionGroups'
-import { BodyType, SceneQueryType } from '../../physics/types/PhysicsTypes'
 import { NameComponent } from '../../scene/components/NameComponent'
 import { Object3DComponent } from '../../scene/components/Object3DComponent'
-import { ObjectLayers } from '../../scene/constants/ObjectLayers'
 import { TransformComponent } from '../../transform/components/TransformComponent'
-import { AnimationState } from '../animations/AnimationState'
-import { AvatarAnimationGraph } from '../animations/AvatarAnimationGraph'
-import { AnimationComponent } from '../components/AnimationComponent'
 import { AvatarAnimationComponent } from '../components/AvatarAnimationComponent'
 import { AvatarComponent } from '../components/AvatarComponent'
 import { SpawnPoseComponent } from '../components/SpawnPoseComponent'
-import { avatarHalfHeight, avatarHeight, avatarRadius, capsuleHeight, createAvatarController } from './createAvatar'
-
-const createAvatar = (world, pos, rot): Entity => {
-    const entity = createEntity(world)
-  
-    const position = createVector3Proxy(TransformComponent.position, entity)
-
-    const rotation = createQuaternionProxy(TransformComponent.rotation, entity)
-    const scale = new Vector3().copy(new Vector3(1, 1, 1))
-    const transform = addComponent(entity, TransformComponent, { position, rotation, scale })
-    transform.position.copy(pos)
-    transform.rotation.copy(rot)
-
-    const velocity = createVector3Proxy(VelocityComponent.velocity, entity)
-    addComponent(entity, VelocityComponent, { velocity })
-
-    const tiltContainer = new Group()
-    tiltContainer.name = 'Actor (tiltContainer)' + entity
-    tiltContainer.position.setY(avatarHalfHeight)
-
-    const modelContainer = new Group()
-    modelContainer.name = 'Actor (modelContainer)' + entity
-    tiltContainer.add(modelContainer)
-
-    addComponent(entity, AvatarComponent, {
-        undefined,
-        avatarHalfHeight,
-        avatarHeight,
-        modelContainer,
-        isGrounded:false
-    })
-
-    addComponent(entity, NameComponent, { name: 'test' })
-    addComponent(entity, AnimationComponent, {
-        mixer: new AnimationMixer(modelContainer),
-        animations: [] as AnimationClip[],
-        animationSpeed: 1
-    })
-
-    addComponent(entity, AvatarAnimationComponent, {
-        animationGraph: new AvatarAnimationGraph(),
-        currentState: new AnimationState(),
-        prevState: new AnimationState(),
-        prevVelocity: new Vector3()
-    })
-
-    addComponent(entity, Object3DComponent, { value: tiltContainer })
-    tiltContainer.traverse((o) => {
-      o.layers.disable(ObjectLayers.Scene)
-      o.layers.enable(ObjectLayers.Avatar)
-    })
-
-    const filterData = new PhysX.PxQueryFilterData()
-    filterData.setWords(CollisionGroups.Default | CollisionGroups.Ground | CollisionGroups.Trigger, 0)
-    const flags = PhysX.PxQueryFlag.eSTATIC.value | PhysX.PxQueryFlag.eDYNAMIC.value | PhysX.PxQueryFlag.eANY_HIT.value
-    filterData.setFlags(flags)
-
-    addComponent(entity, RaycastComponent, {
-      filterData,
-      type: SceneQueryType.Closest,
-      hits: [],
-      origin: new Vector3(0, avatarHalfHeight, 0),
-      direction: new Vector3(0, -1, 0),
-      maxDistance: avatarHalfHeight + 0.05,
-      flags
-    })
-
-    addComponent(entity, CollisionComponent, { collisions: [] })
-    
-
-    addComponent(entity, SpawnPoseComponent, {
-        position: new Vector3().copy(pos),
-        rotation: new Quaternion().copy(rot)
-      })
-      createAvatarController(entity)
-    
-    const shape = world.physics.createShape(
-        new PhysX.PxCapsuleGeometry(avatarRadius, capsuleHeight / 2),
-        world.physics.physics.createMaterial(0, 0, 0),
-        {
-          collisionLayer: CollisionGroups.Avatars,
-          collisionMask: CollisionGroups.Default | CollisionGroups.Ground
-        }
-      )
-      const body = world.physics.addBody({
-        shapes: [shape],
-        type: BodyType.DYNAMIC,
-        transform: {
-          translation: {
-            x: transform.position.x,
-            y: transform.position.y + avatarHalfHeight,
-            z: transform.position.z
-          },
-          rotation: new Quaternion()
-        },
-        userData: {
-          entity
-        }
-      })
-      body.setActorFlag(PhysX.PxActorFlag.eDISABLE_GRAVITY, true)
-      addComponent(entity, ColliderComponent, { body })
-
-    return entity
-  }
+import { createAvatar } from './createAvatar'
+import { NetworkId } from '@xrengine/common/src/interfaces/NetworkId'
 
 describe('createAvatar', () => {
 	let world
     
     beforeEach(async () => {
-            world = createWorld()
-            Engine.currentWorld = world
-        await world.physics.createScene()
-        })
+      /* hoist */
+      Network.instance = new TestNetwork()
+      world = createWorld()
+      Engine.currentWorld = world
+      await Engine.currentWorld.physics.createScene({ verbose: true })
+    })
+    
+    afterEach(() => {
+      Engine.currentWorld = null!
+      delete (globalThis as any).PhysX
+    })
     
     it('check fi avatar schema maps sizes are ok', () => {
-        const entity = createAvatar(world, new Vector3(0, 0, 0), new Quaternion())
+        Engine.userId = world.hostId
+        Engine.hasJoinedWorld = true
+        
+        // mock entity to apply incoming unreliable updates to
+        const entity = createEntity()
+        
+        const networkObject = addComponent(entity, NetworkObjectComponent, {
+          // remote owner
+          ownerId: Engine.userId,
+          networkId: 0 as NetworkId,
+          prefab: '',
+          parameters: {},
+        })
+
+        createAvatar({
+          prefab: 'avatar',
+          parameters: { position: new Vector3(-0.48624888685311896, 0, -0.12087574159728942), rotation: new Quaternion() },
+          type: 'network.SPAWN_OBJECT',
+          networkId: networkObject.networkId,
+          $from: Engine.userId,
+          $to: 'all',
+          $tick: Engine.currentWorld.fixedTick,
+          $cache: true
+        })
         
         strictEqual(hasComponent(entity, TransformComponent), true)
         strictEqual(hasComponent(entity, VelocityComponent), true)
