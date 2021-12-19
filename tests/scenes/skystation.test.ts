933852eb486177b819f743facf30e8b59129cb96
hanzlamateen <hanzlamateen@live.com>
2021-12-19 06:21:05 +0500

    Spawn multiple bots in the sky station world

--- tests/scenes/skystation.test.ts
+++ tests/scenes/skystation.test.ts
@@ -3,7 +3,7 @@ import { XREngineBot } from '@xrengine/bot/src/bot'
 import { BotHooks } from '@xrengine/engine/src/bot/enums/BotHooks'
 import assert from 'assert'
 
-const bot = new XREngineBot({ name: 'bot-1', verbose: true })
+const bots = Array.apply(null, Array(3)).map((_, index) => new XREngineBot({ name: `bot-${index}`, verbose: true }))
 const vector3 = new Vector3()
 
 const domain = process.env.APP_HOST
@@ -11,22 +11,28 @@ const locationName = 'sky-station'
 
 describe.only('Sky Station Scene Tests', () => {
   before(async () => {
-    await bot.launchBrowser()
-    await bot.enterLocation(`https://${domain}/location/${locationName}`)
-    await bot.awaitHookPromise(BotHooks.LocationLoaded)
-    await bot.runHook(BotHooks.InitializeBot)
-    await bot.delay(50000)
+    for (let bot of bots) {
+      await bot.launchBrowser()
+      await bot.enterLocation(`https://${domain}/location/${locationName}`)
+      await bot.awaitHookPromise(BotHooks.LocationLoaded)
+      await bot.runHook(BotHooks.InitializeBot)
+      await bot.delay(1000)
+    }
   })
 
   after(async () => {
-    await bot.delay(1500)
-    await bot.quit()
+    for (let bot of bots) {
+      await bot.delay(1500)
+      await bot.quit()
+    }
   })
 
-  it('Can spawn in the world', async () =>{
-    await bot.delay(1000)
-    const pos = await bot.runHook(BotHooks.GetPlayerPosition)
-    assert(vector3.copy(pos).length() < 45)
+  it('Can spawn multiple bots in the world', async () => {
+    for (let bot of bots) {
+      await bot.delay(1000)
+      const pos = await bot.runHook(BotHooks.GetPlayerPosition)
+      assert(vector3.copy(pos).length() < 45)
+    }
   })
 
 })
