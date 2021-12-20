78ca5997582749abe095ff8b51c505638e96ce78
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2021-12-20 21:53:25 +0500

    move channel provisioning out of harmony

--- packages/client/src/components/Harmony/index.tsx
+++ packages/client/src/components/Harmony/index.tsx
@@ -171,7 +171,6 @@ const Harmony = (props: Props): any => {
   const [profileMenuOpen, setProfileMenuOpen] = useState(false)
   const [warningRefreshModalValues, setWarningRefreshModalValues] = useState(initialRefreshModalValues)
   const [noGameserverProvision, setNoGameserverProvision] = useState(false)
-  const [channelDisconnected, setChannelDisconnected] = useState(false)
   const [hasAudioDevice, setHasAudioDevice] = useState(false)
   const [hasVideoDevice, setHasVideoDevice] = useState(false)
   const [callStartedFromButton, _setCallStartedFromButton] = useState(false)
@@ -262,9 +261,6 @@ const Harmony = (props: Props): any => {
       () => setNoGameserverProvision(true)
     )
 
-    EngineEvents.instance.addEventListener(SocketWebRTCClientTransport.EVENTS.CHANNEL_DISCONNECTED, () => {
-      if (activeAVChannelIdRef.current?.length > 0) setChannelDisconnected(true)
-    })
     EngineEvents.instance.addEventListener(SocketWebRTCClientTransport.EVENTS.CHANNEL_RECONNECTED, async () => {
       setChannelAwaitingProvision({
         id: activeAVChannelIdRef.current,
@@ -308,10 +304,6 @@ const Harmony = (props: Props): any => {
     if (selfUser?.channelInstanceId.value != null && userState.channelLayerUsersUpdateNeeded.value)
       UserService.getLayerUsers(false)
   }, [selfUser, userState.channelLayerUsersUpdateNeeded.value])
-
-  useEffect(() => {
-    TransportService.updateChannelTypeState()
-  }, [MediaStreams.instance.channelType, MediaStreams.instance.channelId])
 
   useEffect(() => {
     setActiveAVChannelId(transportState.channelId.value)
@@ -426,23 +418,6 @@ const Harmony = (props: Props): any => {
       setWarningRefreshModalValues(newValues)
     }
   }, [])
-
-  useEffect(() => {
-    if (channelDisconnected === true) {
-      const newValues = {
-        ...warningRefreshModalValues,
-        open: true,
-        title: 'Call disconnected',
-        body: "You've lost your connection to this call. We'll try to reconnect before the following time runs out, otherwise you'll hang up",
-        action: async () => endCall(),
-        parameters: [],
-        timeout: 30000,
-        closeAction: endCall
-      }
-      setWarningRefreshModalValues(newValues)
-      setChannelDisconnected(false)
-    }
-  }, [channelDisconnected])
 
   const handleWindowResize = () => {
     setDimensions({
@@ -861,24 +836,6 @@ const Harmony = (props: Props): any => {
   const openProfileMenu = (): void => {
     setProfileMenuOpen(true)
   }
-
-  useEffect(() => {
-    if (
-      channelConnectionState.instanceProvisioned.value === true &&
-      channelConnectionState.updateNeeded.value === true &&
-      channelConnectionState.instanceServerConnecting.value === false &&
-      channelConnectionState.connected.value === false
-    ) {
-      ChannelConnectionService.connectToChannelServer(channelConnectionState.channelId.value, isHarmonyPage)
-      MediaStreamService.updateCamVideoState()
-      MediaStreamService.updateCamAudioState()
-    }
-  }, [
-    channelConnectionState.instanceProvisioned,
-    channelConnectionState.updateNeeded,
-    channelConnectionState.instanceServerConnecting,
-    channelConnectionState.connected
-  ])
 
   const chatSelectors = (
     <div
