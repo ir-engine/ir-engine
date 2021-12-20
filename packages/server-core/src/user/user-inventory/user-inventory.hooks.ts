db24efc36e6b8098bc9e6df67128b52eacfb14fc
swpnit82 <48938076+swpnit82@users.noreply.github.com>
2021-12-20 23:05:39 +0000

    Origin/biab integration (#4452)
    * Change for BIAB
    
    * update .env file
    
    * review comments
    
    * var to const
    
    * biab-files
    
    * Biab and review comments

--- packages/server-core/src/user/user-inventory/user-inventory.hooks.ts
+++ packages/server-core/src/user/user-inventory/user-inventory.hooks.ts
@@ -2,10 +2,13 @@ import { disallow } from 'feathers-hooks-common'
 import addAssociations from '@xrengine/server-core/src/hooks/add-associations'
 import { HookContext } from '@feathersjs/feathers'
 import * as authentication from '@feathersjs/authentication'
+import axios from 'axios'
+import config from '../../appconfig'
+const { authenticate } = authentication.hooks
 
 export default {
   before: {
-    all: [],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
     create: [],
