43753b93f0944252dc8aff6b38de3fe970bac282
Josh Field <joshfield999@gmail.com>
2021-12-24 09:50:19 +1100

    remove more harmony stuff

--- packages/client/src/components/PartyParticipantWindow/index.tsx
+++ packages/client/src/components/PartyParticipantWindow/index.tsx
@@ -44,7 +44,6 @@ interface ContainerProportions {
 }
 
 interface Props {
-  harmony?: boolean
   containerProportions?: ContainerProportions
   peerId?: string
 }
@@ -61,7 +60,7 @@ const PartyParticipantWindow = (props: Props): JSX.Element => {
   const [audioTrackClones, setAudioTrackClones] = useState([])
   const [videoTrackClones, setVideoTrackClones] = useState([])
   const [volume, setVolume] = useState(100)
-  const { harmony, peerId } = props
+  const { peerId } = props
   const userState = useUserState()
   const videoRef = React.useRef<HTMLVideoElement>()
   const audioRef = React.useRef<HTMLAudioElement>()
@@ -164,14 +163,10 @@ const PartyParticipantWindow = (props: Props): JSX.Element => {
   }, [userHasInteracted.value])
 
   useEffect(() => {
-    if (harmony !== true && selfUser?.user_setting?.spatialAudioEnabled === true && audioRef.current != null)
-      audioRef.current.volume = 0
-    else if (
-      harmony === true
-      // (selfUser?.user_setting?.spatialAudioEnabled === false || selfUser?.user_setting?.spatialAudioEnabled === 0) &&
-      // Engine.spatialAudio
-    )
-      audioRef.current!.volume = volume / 100
+    if (selfUser?.user_setting?.spatialAudioEnabled === true && audioRef.current != null) audioRef.current.volume = 0
+    // (selfUser?.user_setting?.spatialAudioEnabled === false || selfUser?.user_setting?.spatialAudioEnabled === 0) &&
+    // Engine.spatialAudio
+    else audioRef.current!.volume = volume / 100
   }, [selfUser])
 
   useEffect(() => {
@@ -212,12 +207,10 @@ const PartyParticipantWindow = (props: Props): JSX.Element => {
         setAudioProducerPaused(audioStream.paused)
       }
       // TODO: handle 3d audio switch on/off
-      if (harmony !== true && selfUser?.user_setting?.spatialAudioEnabled === true) audioRef.current.volume = 0
-      if (
-        harmony === true
-        // selfUser?.user_setting?.spatialAudioEnabled === false ||
-        // (selfUser?.user_setting?.spatialAudioEnabled === 0 && Engine.spatialAudio)
-      ) {
+      if (selfUser?.user_setting?.spatialAudioEnabled === true) audioRef.current.volume = 0
+      // selfUser?.user_setting?.spatialAudioEnabled === false ||
+      // (selfUser?.user_setting?.spatialAudioEnabled === 0 && Engine.spatialAudio)
+      {
         audioRef.current.volume = volume / 100
         // PositionalAudioSystem.instance?.suspend()
       }
@@ -415,7 +408,6 @@ const PartyParticipantWindow = (props: Props): JSX.Element => {
         id={peerId + '_container'}
         className={classNames({
           [styles['party-chat-user']]: true,
-          [styles['harmony']]: harmony === true,
           [styles['self-user']]: isSelfUser,
           [styles['no-video']]: videoStream == null,
           [styles['video-paused']]: videoStream && (videoProducerPaused === true || videoStreamPaused === true),
@@ -484,19 +476,19 @@ const PartyParticipantWindow = (props: Props): JSX.Element => {
                   </IconButton>
                 </Tooltip>
               ) : null}
-              {harmony !== true && (
+              {
                 <Tooltip title="Open Picture in Picture">
                   <IconButton color="secondary" size="small" className={styles['audio-control']} onClick={togglePiP}>
                     <Launch className={styles.pipBtn} />
                   </IconButton>
                 </Tooltip>
-              )}
+              }
             </div>
             {audioProducerGlobalMute === true && <div className={styles['global-mute']}>Muted by Admin</div>}
             {audioStream &&
               audioProducerPaused === false &&
               audioProducerGlobalMute === false &&
-              (harmony === true || selfUser?.user_setting?.spatialAudioEnabled === false) && (
+              selfUser?.user_setting?.spatialAudioEnabled === false && (
                 <div className={styles['audio-slider']}>
                   {volume > 0 && <VolumeDown />}
                   {volume === 0 && <VolumeMute />}
