cd52aa471414fc9f4a895a1553945db42aed8c32
Kyle Baran <kbaran@bitscoop.com>
2021-12-27 23:37:20 -0800

    Use `cli` to run create-env-production.ts

--- packages/client/scripts/create-env-production.ts
+++ packages/client/scripts/create-env-production.ts
@@ -1,3 +1,4 @@
+import cli from 'cli'
 import fs from 'fs'
 import appRootPath from 'app-root-path'
 
@@ -5,7 +6,7 @@ const prefixRegex = /^VITE_/
 const lowercaseEnv = process.env.APP_ENV.toLowerCase()
 const envPath = appRootPath.path + `/.env.${lowercaseEnv}`
 
-function createEnvProduction() {
+cli.main(async () => {
   let output = ''
   for (let key of Object.keys(process.env)) {
     console.log('key', key, 'value', process.env[key])
@@ -18,6 +19,6 @@ function createEnvProduction() {
   console.log('output to write', output)
   if (fs.existsSync(envPath)) fs.rmSync(envPath)
   fs.writeFileSync(envPath, output)
-}
 
-createEnvProduction()
+  process.exit(0)
+})
