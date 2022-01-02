d644fab4fc08074d53e9f1fd18bb5b5c723b7826
HexaField <joshfield999@gmail.com>
2022-01-02 21:50:49 +1100

    fix trigger helper

--- packages/engine/src/scene/functions/loaders/TriggerVolumeFunctions.ts
+++ packages/engine/src/scene/functions/loaders/TriggerVolumeFunctions.ts
@@ -1,5 +1,5 @@
 import { ComponentJson } from '@xrengine/common/src/interfaces/SceneInterface'
-import { BoxBufferGeometry, BoxHelper, Mesh } from 'three'
+import { BoxBufferGeometry, Mesh, MeshBasicMaterial } from 'three'
 import {
   ComponentDeserializeFunction,
   ComponentSerializeFunction,
@@ -10,14 +10,12 @@ import { Entity } from '../../../ecs/classes/Entity'
 import { addComponent, getComponent } from '../../../ecs/functions/ComponentFunctions'
 import { useWorld } from '../../../ecs/functions/SystemHooks'
 import { ColliderComponent } from '../../../physics/components/ColliderComponent'
-import { CollisionComponent } from '../../../physics/components/CollisionComponent'
 import { CollisionGroups } from '../../../physics/enums/CollisionGroups'
 import { createCollider } from '../../../physics/functions/createCollider'
 import { TransformComponent } from '../../../transform/components/TransformComponent'
 import { EntityNodeComponent } from '../../components/EntityNodeComponent'
 import { Object3DComponent } from '../../components/Object3DComponent'
 import { TriggerVolumeComponent } from '../../components/TriggerVolumeComponent'
-import { ObjectLayers } from '../../constants/ObjectLayers'
 
 export const SCENE_COMPONENT_TRIGGER_VOLUME = 'trigger-volume'
 export const SCENE_COMPONENT_TRIGGER_VOLUME_DEFAULT_VALUES = {}
@@ -28,8 +26,10 @@ export const deserializeTriggerVolume: ComponentDeserializeFunction = (entity: E
   const rot = transform.rotation
   const scale = transform.scale
 
-  const geometry = new BoxBufferGeometry()
-  const boxMesh = new Mesh(geometry)
+  const boxMesh = new Mesh(
+    new BoxBufferGeometry(),
+    new MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true })
+  )
   boxMesh.position.set(pos.x, pos.y, pos.z)
   boxMesh.scale.set(scale.x, scale.y, scale.z)
   boxMesh.quaternion.set(rot.x, rot.y, rot.z, rot.w)
@@ -51,18 +51,13 @@ export const deserializeTriggerVolume: ComponentDeserializeFunction = (entity: E
   })
 
   if (Engine.isEditor) {
-    // A visual representation for the trigger
-    boxMesh.scale.multiplyScalar(2) // engine uses half-extents for box size, to be compatible with gltf and threejs
-    const box = new BoxHelper(boxMesh.clone(), 0xffff00)
-    box.layers.set(ObjectLayers.Gizmos)
-    addComponent(entity, Object3DComponent, { value: box })
+    addComponent(entity, Object3DComponent, { value: boxMesh })
   }
 
   if (Engine.isEditor) getComponent(entity, EntityNodeComponent)?.components.push(SCENE_COMPONENT_TRIGGER_VOLUME)
 }
 
 export const updateTriggerVolume: ComponentUpdateFunction = (entity: Entity, prop: any) => {
-  console.log('updateTriggerVolume', entity, prop)
   if (Engine.isEditor) {
     const world = useWorld()
     const transform = getComponent(entity, TransformComponent)
@@ -74,8 +69,10 @@ export const updateTriggerVolume: ComponentUpdateFunction = (entity: Entity, pro
     pose.rotation = transform.rotation
     shape.setLocalPose(pose)
 
-    if (prop.scale) getComponent(entity, Object3DComponent).value.scale.copy(prop.scale)
-    console.log(getComponent(entity, Object3DComponent).value)
+    const object3DComponent = getComponent(entity, Object3DComponent)
+    object3DComponent.value.position.copy(transform.position)
+    object3DComponent.value.rotation.set(transform.rotation.x, transform.rotation.y, transform.rotation.z, 'XYZ')
+    object3DComponent.value.scale.copy(transform.scale)
   }
 }
 
