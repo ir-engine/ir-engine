7640bd12629a569616337fa776901f707e940ee6
Josh Field <joshfield999@gmail.com>
2022-01-11 13:16:25 +1100

    remove more unused services, move scene service to projects folder

--- packages/server-core/src/world/services.ts
+++
@@ -1,5 +0,0 @@
-import Scene from './scene/scene.service'
-import Meta from './meta/meta.service'
-import ResolveMedia from './resolve-media/resolve-media.service'
-
-export default [ResolveMedia, Scene, Meta]
