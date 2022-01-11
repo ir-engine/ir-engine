1cb3a81994121974ae6bc9df3cf9c41ad8ec2e42
Josh Field <joshfield999@gmail.com>
2022-01-11 13:12:24 +1100

    remove unused services

--- packages/server-core/src/world/services.ts
+++ packages/server-core/src/world/services.ts
@@ -1,6 +1,5 @@
-import MediaSearch from './media-search/media-search.service'
 import Scene from './scene/scene.service'
 import Meta from './meta/meta.service'
 import ResolveMedia from './resolve-media/resolve-media.service'
 
-export default [MediaSearch, ResolveMedia, Scene, Meta]
+export default [ResolveMedia, Scene, Meta]
