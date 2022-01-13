0fe6c4d85280d1282a76ac5f9d9032e233ae9e07
hanzlamateen <hanzlamateen@live.com>
2022-01-13 13:22:02 +0500

    Added testbot in builder scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -x
 STAGE=$1
 TAG=$2
 
-helm upgrade --reuse-values --set analytics.image.repository=$ECR_URL/xrengine-$STAGE-analytics,analytics.image.tag=$TAG,api.image.repository=$ECR_URL/xrengine-$STAGE-api,api.image.tag=$TAG,gameserver.image.repository=$ECR_URL/xrengine-$STAGE-gameserver,gameserver.image.tag=$TAG,client.image.repository=$ECR_URL/xrengine-$STAGE-client,client.image.tag=$TAG $STAGE xrengine/xrengine
+helm upgrade --reuse-values --set analytics.image.repository=$ECR_URL/xrengine-$STAGE-analytics,analytics.image.tag=$TAG,api.image.repository=$ECR_URL/xrengine-$STAGE-api,api.image.tag=$TAG,gameserver.image.repository=$ECR_URL/xrengine-$STAGE-gameserver,gameserver.image.tag=$TAG,testbot.image.repository=$ECR_URL/xrengine-$STAGE-testbot,testbot.image.tag=$TAG,client.image.repository=$ECR_URL/xrengine-$STAGE-client,client.image.tag=$TAG $STAGE xrengine/xrengine
