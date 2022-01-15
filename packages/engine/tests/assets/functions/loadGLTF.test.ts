8213c40895a32bafb5ee92bd70a24ed54fdb835b
dinomut1 <heximhotep@gmail.com>
2022-01-14 21:35:14 -0800

    added lightmap unit test

--- packages/engine/tests/assets/functions/loadGLTF.test.ts
+++ packages/engine/tests/assets/functions/loadGLTF.test.ts
@@ -7,7 +7,7 @@ import {Mesh, Material, MeshBasicMaterial, Texture} from "three"
 describe("LoadGLTF", () => {
     describe("LoadLightmaps", () => {
         it("should load lightmapped gltf", async () => {
-            AssetLoader.load({url:"assets/lightmapTest.glb"}, async (gltf:GLTF) => {
+            AssetLoader.load({url:"assets/lightmap_test.glb"}, async (gltf:GLTF) => {
                 let sphere = gltf.scene.children[1]
                 let mat = sphere.material
                 assert.equal(mat.lightMap.type, Texture)
