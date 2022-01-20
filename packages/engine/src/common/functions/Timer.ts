6dad3a9c48a01e6c4501f0da1d8bd46a59541eec
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-01-19 22:12:06 -0500

    Revert "Cap frame deltas to 1/30 seconds"
    This reverts commit 5bca8bce1c8b3c669fadff6f635ade4aa2448698.

--- packages/engine/src/common/functions/Timer.ts
+++ packages/engine/src/common/functions/Timer.ts
@@ -9,8 +9,6 @@ type TimerUpdateCallback = (delta: number, elapsedTime: number) => any
 const TPS_REPORTS_ENABLED = false
 const TPS_REPORT_INTERVAL_MS = 10000
 
-const MAX_DELTA = 1 / 30
-
 export function Timer(update: TimerUpdateCallback): { start: Function; stop: Function; clear: Function } {
   let lastTime = null
   let elapsedTime = 0
@@ -47,7 +45,7 @@ export function Timer(update: TimerUpdateCallback): { start: Function; stop: Fun
 
     Engine.xrFrame = xrFrame
     if (lastTime !== null) {
-      delta = Math.min((time - lastTime) / 1000, MAX_DELTA)
+      delta = (time - lastTime) / 1000
 
       elapsedTime += delta
 
