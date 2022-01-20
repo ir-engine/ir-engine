a760ed68bb68833e5102723063c1c2152137fea0
HexaField <joshfield999@gmail.com>
2022-01-20 16:40:38 +1100

    change timer delta limit and make configurable

--- packages/engine/src/common/functions/Timer.ts
+++ packages/engine/src/common/functions/Timer.ts
@@ -9,9 +9,16 @@ type TimerUpdateCallback = (delta: number, elapsedTime: number) => any
 const TPS_REPORTS_ENABLED = false
 const TPS_REPORT_INTERVAL_MS = 10000
 
-const MAX_DELTA = 1 / 30
+const TimerConfig = {
+  MAX_DELTA: 1 / 10
+}
 
-export function Timer(update: TimerUpdateCallback): { start: Function; stop: Function; clear: Function } {
+export function Timer(
+  update: TimerUpdateCallback,
+  _config: Partial<typeof TimerConfig> = {}
+): { start: Function; stop: Function; clear: Function } {
+  const config = Object.assign({}, TimerConfig, _config)
+
   let lastTime = null
   let elapsedTime = 0
   let delta = 0
@@ -47,7 +54,7 @@ export function Timer(update: TimerUpdateCallback): { start: Function; stop: Fun
 
     Engine.xrFrame = xrFrame
     if (lastTime !== null) {
-      delta = Math.min((time - lastTime) / 1000, MAX_DELTA)
+      delta = Math.min((time - lastTime) / 1000, config.MAX_DELTA)
 
       elapsedTime += delta
 
