52e13c9e95ea753ad5ace7f48ee8631069af9358
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2022-01-21 12:37:30 +0200

    added history support on scene preview camera node

--- packages/editor/src/components/properties/ScenePreviewCameraNodeEditor.tsx
+++ packages/editor/src/components/properties/ScenePreviewCameraNodeEditor.tsx
@@ -5,6 +5,10 @@ import { useTranslation } from 'react-i18next'
 import { updateScenePreviewCamera } from '@xrengine/engine/src/scene/functions/loaders/ScenePreviewCameraFunctions'
 import CameraAltIcon from '@mui/icons-material/CameraAlt'
 import { EditorComponentType } from './Util'
+import { getComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
+import { Object3DComponent } from '@xrengine/engine/src/scene/components/Object3DComponent'
+import { CommandManager } from '../../managers/CommandManager'
+import { NameComponent } from '@xrengine/engine/src/scene/components/NameComponent'
 
 /**
  * ScenePreviewCameraNodeEditor provides the editor view to customize properties.
@@ -16,6 +20,11 @@ export const ScenePreviewCameraNodeEditor: EditorComponentType = (props) => {
   const { t } = useTranslation()
 
   const onSetFromViewport = () => {
+    const obj3d = getComponent(props.node.entity, Object3DComponent).value
+    CommandManager.instance.setProperty([props.node], {
+      component: NameComponent,
+      properties: { position: obj3d.position, rotation: obj3d.rotation }
+    })
     updateScenePreviewCamera(props.node.entity)
   }
 
