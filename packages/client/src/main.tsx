08efab5bdedf00e5c964d7af35152b8390350805
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2022-02-02 01:58:29 +0500

    use loading circle from client-core

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,30 +1,17 @@
+import { LoadingCircle } from '@xrengine/client-core/src/components/LoadingCircle'
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import './env-config'
 import './hookstate_devtools.es'
-import './pages/styles.scss'
 import { initialize } from './util'
 
 const AppPage = React.lazy(() => import('./pages/_app'))
-
-const Loading = () => {
-  return (
-    <div className={'body_loading'}>
-      <div className={'lds_ellipsis'}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  )
-}
 
 initialize()
   // then load the app
   .then((_) => {
     ReactDOM.render(
-      <Suspense fallback={<Loading />}>
+      <Suspense fallback={<LoadingCircle />}>
         <AppPage />
       </Suspense>,
       document.getElementById('root')
