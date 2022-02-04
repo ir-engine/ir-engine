871f3b42036aa08f21787b39a4390c5f87b17789
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-02-04 09:52:06 -0500

    Revert "Retain function names in minified build"
    This reverts commit 0b41d257714074a2f54bac5cb44848875413929c.

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -116,9 +116,6 @@ export default defineConfig(async (command) => {
         },
       },
     },
-    esbuild: {
-      keepNames: true,
-    }
   };
   if(process.env.APP_ENV === 'development' || process.env.VITE_LOCAL_BUILD === 'true') {
     returned.server.https = {
