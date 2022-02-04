22c618a5b782df29d909e86d943afe07ad15c9fe
大山 <92342281+rondoor124@users.noreply.github.com>
2022-02-04 01:35:07 -0600

    Bone Matching Improvement V2 (#5055)
    * add type and ref for bone structure, improve camera targeting
    
    * export camera system, improve target bone function
    
    * prototype separated mesh & FBX loader
    
    * update fbx prototype
    
    * update scale issue
    
    * Fix FBXLoader issue
    
    * update three-vrm
    
    * remove testing code
    
    * clean code
    
    * fix issue
    
    * fix check erros
    
    Co-authored-by: HexaField <joshfield999@gmail.com>

--- packages/engine/src/assets/functions/createGLTFLoader.ts
+++ packages/engine/src/assets/functions/createGLTFLoader.ts
@@ -4,6 +4,7 @@ import { NodeDRACOLoader } from '../loaders/gltf/NodeDracoLoader'
 import { DRACOLoader } from '../loaders/gltf/DRACOLoader'
 import { GLTFLoader } from '../loaders/gltf/GLTFLoader'
 import { isClient } from '../../common/functions/isClient'
+import { VRMLoaderPlugin } from '@pixiv/three-vrm'
 
 export const createGLTFLoader = (keepMaterials = false) => {
   const loader = new GLTFLoader()
@@ -13,6 +14,7 @@ export const createGLTFLoader = (keepMaterials = false) => {
   }
 
   loader.register((parser) => new GLTFInstancingExtension(parser))
+  loader.register((parser) => new VRMLoaderPlugin(parser))
 
   const dracoLoader: any = isClient ? new DRACOLoader() : new NodeDRACOLoader()
   // const dracoLoader = new DRACOLoader()
