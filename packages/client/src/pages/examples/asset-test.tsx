81900330cca72a6b2719d357af75566522cb8d70
HexaField <joshfield999@gmail.com>
2022-02-05 18:45:55 +1100

    fix error

--- packages/client/src/pages/examples/asset-test.tsx
+++ packages/client/src/pages/examples/asset-test.tsx
@@ -35,7 +35,7 @@ export default LocationPage
 let scene = new Scene()
 let animationMixers = [] as AnimationMixer[]
 let clips
-AssetLoader.load('/default_assets/Animations.glb', (gltf) => {
+AssetLoader.load({ url: '/default_assets/Animations.glb' }, (gltf) => {
   console.log(gltf)
   clips = gltf.animations
   clips.forEach((clip) => {
@@ -94,7 +94,7 @@ const DevPage = () => {
 
     scene.remove
 
-    getLoader().load(fileURL, (gltf) => {
+    AssetLoader.load({ url: fileURL }, (gltf) => {
       URL.revokeObjectURL(fileURL)
 
       scene.remove(model)
