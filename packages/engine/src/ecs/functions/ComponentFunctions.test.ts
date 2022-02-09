5738ffd5c6fdb9b11f6f5f21ae005e4450e55413
HexaField <joshfield999@gmail.com>
2022-02-09 14:59:47 +1100

    add most remaining component function tests

--- packages/engine/src/ecs/functions/ComponentFunctions.test.ts
+++ packages/engine/src/ecs/functions/ComponentFunctions.test.ts
@@ -3,7 +3,18 @@ import { Types } from 'bitecs'
 import { Vector3 } from 'three'
 import { Engine } from '../classes/Engine'
 import { createWorld } from '../classes/World'
-import { ComponentMap, createMappedComponent } from './ComponentFunctions'
+import {
+  addComponent,
+  ComponentMap,
+  createMappedComponent,
+  getAllComponents,
+  getComponent,
+  hasComponent,
+  removeComponent
+} from './ComponentFunctions'
+import { createEntity } from './EntityFunctions'
+import { useWorld } from './SystemHooks'
+import * as bitECS from 'bitecs'
 
 describe('ComponentFunctions', async () => {
   beforeEach(() => {
@@ -28,7 +39,9 @@ describe('ComponentFunctions', async () => {
 
     it('should create mapped component with SoA', () => {
       type Vector3ComponentType = {
-        position: Vector3
+        x: number
+        y: number
+        z: number
       }
       const { f32 } = Types
       const Vector3Schema = { x: f32, y: f32, z: f32 }
@@ -42,7 +55,199 @@ describe('ComponentFunctions', async () => {
       assert.equal((Vector3Component as any)._map.size, 0)
       assert.equal(ComponentMap.size, 1)
     })
-
-    it('should create mapped component with bitecs store', () => {})
   })
+
+  describe('addComponent', () => {
+    it('should add component', () => {
+      const TestComponent = createMappedComponent('TestComponent')
+
+      const entity = createEntity()
+      const component = addComponent(entity, TestComponent, {})
+
+      assert.ok(component)
+      assert.ok(bitECS.hasComponent(useWorld(), TestComponent, entity))
+    })
+
+    it('should add component with AoS values', () => {
+      const TestComponent = createMappedComponent<{ value: number }>('TestComponent')
+
+      const entity = createEntity()
+      const component = addComponent(entity, TestComponent, { value: 5 })
+
+      assert.ok(component)
+      assert.equal(component.value, 5)
+    })
+
+    it('should add component with SoA values', () => {
+      const { f32 } = Types
+      const ValueSchema = { value: f32 }
+      const TestComponent = createMappedComponent<{ value: number }, typeof ValueSchema>('TestComponent', ValueSchema)
+
+      const entity = createEntity()
+      const component = addComponent(entity, TestComponent, { value: 3 })
+
+      assert.ok(component)
+      assert.equal(TestComponent.value[entity], 3)
+    })
+
+    it('should throw on duplicate add component', () => {
+      const TestComponent = createMappedComponent('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, {})
+      assert.throws(() => addComponent(entity, TestComponent, {}))
+    })
+
+    it('should throw on null entity argument', () => {
+      assert.throws(() => addComponent(null!, null!, {}))
+      assert.throws(() => addComponent(undefined!, undefined!, {}))
+    })
+  })
+
+  describe('getComponent', () => {
+    it('should get component', () => {
+      const TestComponent = createMappedComponent('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, {})
+      const component = getComponent(entity, TestComponent)
+
+      assert.ok(component)
+    })
+
+    it('should get component with values', () => {
+      const TestComponent = createMappedComponent<{ value: number }>('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 2 })
+      const component = getComponent(entity, TestComponent)
+
+      assert.ok(component)
+      assert.equal(component.value, 2)
+    })
+
+    it('should get component with values', () => {
+      const { f32 } = Types
+      const ValueSchema = { value: f32 }
+      const TestComponent = createMappedComponent<{ value: number }, typeof ValueSchema>('TestComponent', ValueSchema)
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 4 })
+      const component = getComponent(entity, TestComponent)
+
+      assert.ok(component)
+      assert.equal(component.value, 4)
+    })
+
+    it('should throw on null entity argument', () => {
+      assert.throws(() => getComponent(null!, null!))
+      assert.throws(() => getComponent(undefined!, undefined!))
+    })
+  })
+
+  describe('hasComponent', () => {
+    it('should have component', () => {
+      const TestComponent = createMappedComponent('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, {})
+
+      assert.ok(hasComponent(entity, TestComponent))
+    })
+
+    it('should have component with AoS values', () => {
+      const TestComponent = createMappedComponent<{ value: number }>('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 2 })
+
+      assert.ok(hasComponent(entity, TestComponent))
+    })
+
+    it('should have component with SoA values', () => {
+      const { f32 } = Types
+      const ValueSchema = { value: f32 }
+      const TestComponent = createMappedComponent<{ value: number }, typeof ValueSchema>('TestComponent', ValueSchema)
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 4 })
+
+      assert.ok(hasComponent(entity, TestComponent))
+    })
+
+    it('should throw on null entity argument', () => {
+      assert.throws(() => hasComponent(null!, null!))
+      assert.throws(() => hasComponent(undefined!, undefined!))
+    })
+  })
+
+  describe('removeComponent', () => {
+    it('should have component', () => {
+      const TestComponent = createMappedComponent('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, {})
+
+      assert.ok(hasComponent(entity, TestComponent))
+
+      removeComponent(entity, TestComponent)
+
+      assert.ok(!hasComponent(entity, TestComponent))
+    })
+
+    it('should have component with AoS values', () => {
+      const TestComponent = createMappedComponent<{ value: number }>('TestComponent')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 2 })
+
+      assert.ok(hasComponent(entity, TestComponent))
+
+      removeComponent(entity, TestComponent)
+
+      assert.ok(!hasComponent(entity, TestComponent))
+    })
+
+    it('should have component with SoA values', () => {
+      const { f32 } = Types
+      const ValueSchema = { value: f32 }
+      const TestComponent = createMappedComponent<{ value: number }, typeof ValueSchema>('TestComponent', ValueSchema)
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent, { value: 4 })
+
+      assert.ok(hasComponent(entity, TestComponent))
+
+      removeComponent(entity, TestComponent)
+
+      assert.ok(!hasComponent(entity, TestComponent))
+    })
+
+    it('should throw on null entity argument', () => {
+      assert.throws(() => removeComponent(null!, null!))
+      assert.throws(() => removeComponent(undefined!, undefined!))
+    })
+  })
+
+  describe('getAllComponents', () => {
+    it('should get all components', () => {
+      const TestComponent1 = createMappedComponent('TestComponent1')
+      const TestComponent2 = createMappedComponent('TestComponent2')
+      const TestComponent3 = createMappedComponent('TestComponent3')
+
+      const entity = createEntity()
+      addComponent(entity, TestComponent1, {})
+      addComponent(entity, TestComponent2, {})
+      addComponent(entity, TestComponent3, {})
+
+      const [component1, component2, component3] = getAllComponents(entity)
+
+      assert.ok(component1)
+      assert.ok(component2)
+      assert.ok(component3)
+    })
+  })
+
+  // TODO
+  describe('defineQuery', () => {})
 })
