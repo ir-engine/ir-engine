1d580e41acff49d009fe6ffd4dd132b808609c83
Rishit Vaidya <rishit@masalagames.com>
2022-02-14 19:19:49 +0530

    Update AvatarControllerSystem.ts

--- packages/engine/src/avatar/AvatarControllerSystem.ts
+++ packages/engine/src/avatar/AvatarControllerSystem.ts
@@ -1,4 +1,6 @@
+import { Quaternion, Vector3 } from 'three'
 import { Engine } from '../ecs/classes/Engine'
+//import { System } from '../ecs/classes/System'
 import { defineQuery, getComponent } from '../ecs/functions/ComponentFunctions'
 import { LocalInputTagComponent } from '../input/components/LocalInputTagComponent'
 import { TransformComponent } from '../transform/components/TransformComponent'
@@ -6,21 +8,30 @@ import { AvatarComponent } from './components/AvatarComponent'
 import { AvatarControllerComponent } from './components/AvatarControllerComponent'
 import { moveAvatar } from './functions/moveAvatar'
 import { World } from '../ecs/classes/World'
+import { respawnAvatar } from './functions/respawnAvatar'
 import { ColliderComponent } from '../physics/components/ColliderComponent'
 import { XRInputSourceComponent } from '../xr/components/XRInputSourceComponent'
+import { detectUserInCollisions } from './functions/detectUserInCollisions'
 
 export class AvatarSettings {
   static instance: AvatarSettings = new AvatarSettings()
   walkSpeed = 1.5
-  runSpeed = 5
-  jumpHeight = 4
+  runSpeed = 10
+  jumpHeight = 20
+  isPortal = false
+  portal = new Vector3(0, 0, 0)
 }
 
 export default async function AvatarControllerSystem(world: World) {
+  const vector3 = new Vector3()
+  const quat = new Quaternion()
+  const quat2 = new Quaternion()
+
   const controllerQuery = defineQuery([AvatarControllerComponent])
   const localXRInputQuery = defineQuery([LocalInputTagComponent, XRInputSourceComponent, AvatarControllerComponent])
 
   return () => {
+    //console.log("Updating Avaatr")
     for (const entity of controllerQuery.exit(world)) {
       const controller = getComponent(entity, AvatarControllerComponent, true)
 
@@ -30,6 +41,7 @@ export default async function AvatarControllerSystem(world: World) {
 
       const avatar = getComponent(entity, AvatarComponent)
       if (avatar) {
+        ;``
         avatar.isGrounded = false
       }
     }
@@ -52,6 +64,9 @@ export default async function AvatarControllerSystem(world: World) {
     }
 
     for (const entity of controllerQuery(world)) {
+      // todo: replace this with trigger detection
+      detectUserInCollisions(entity)
+
       moveAvatar(world, entity, Engine.camera)
 
       const controller = getComponent(entity, AvatarControllerComponent)
@@ -61,15 +76,28 @@ export default async function AvatarControllerSystem(world: World) {
       const transform = getComponent(entity, TransformComponent)
 
       const pose = controller.controller.getPosition()
-      transform.position.set(pose.x, pose.y - avatar.avatarHalfHeight, pose.z)
 
-      collider.body.setGlobalPose(
-        {
-          translation: pose,
-          rotation: transform.rotation
-        },
-        true
-      )
+      pose.x += AvatarSettings.instance.portal.x
+      pose.y += AvatarSettings.instance.portal.y
+      pose.z += AvatarSettings.instance.portal.z
+
+      if (controller.movementEnabled) {
+        transform.position.set(pose.x, pose.y - avatar.avatarHalfHeight, pose.z)
+        collider.body.setGlobalPose(
+          {
+            translation: pose,
+            rotation: transform.rotation
+          },
+          true
+        )
+
+        if (AvatarSettings.instance.isPortal) {
+          AvatarSettings.instance.isPortal = false
+          AvatarSettings.instance.portal = new Vector3(0, 0, 0)
+        }
+      }
+
+      controller.controller.setPosition(pose)
 
       // TODO: implement scene lower bounds parameter
       if (transform.position.y < -10) {
