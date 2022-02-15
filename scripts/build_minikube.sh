94c3824d8abf71992a15522642985c7cd7c654f3
Kyle Baran <kbaran@bitscoop.com>
2022-02-15 15:41:52 -0800

    Fixed some issues encountered when testing on minikube.
    Authentication.class.ts:find() was mistakenly not unpacking the oauth field.
    
    build-minikube.sh now has default values for VITE_(CLIENT/SERVER/GAMESERVER)_HOST,
    installs project package.json's.
    
    Root Dockerfile uses VITE_(CLIENT/SERVER/GAMESERVER)_HOST values when building, installs git.
    
    Removed app.setup() from recent tests because that was double-registering services, which was leading
    to errors due to user aliases being used twice.
    
    Updated minikube documentation.

--- scripts/build_minikube.sh
+++ scripts/build_minikube.sh
@@ -37,8 +37,41 @@ else
   MYSQL_DATABASE=$MYSQL_DATABASE
 fi
 
+if [ -z "$VITE_CLIENT_HOST" ]
+then
+  VITE_CLIENT_HOST=local.theoverlay.io
+else
+  VITE_CLIENT_HOST=$VITE_CLIENT_HOST
+fi
+
+if [ -z "$VITE_SERVER_HOST" ]
+then
+  VITE_SERVER_HOST=api-local.theoverlay.io
+else
+  VITE_SERVER_HOST=$VITE_SERVER_HOST
+fi
+
+if [ -z "$VITE_GAMESERVER_HOST" ]
+then
+  VITE_GAMESERVER_HOST=gameserver-local.theoverlay.io
+else
+  VITE_GAMESERVER_HOST=$VITE_GAMESERVER_HOST
+fi
+
 docker start xrengine_minikube_db
 eval $(minikube docker-env)
-DOCKER_BUILDKIT=1 docker build -t xrengine --build-arg MYSQL_HOST=$MYSQL_HOST --build-arg MYSQL_PORT=$MYSQL_PORT --build-arg MYSQL_PASSWORD=$MYSQL_PASSWORD --build-arg MYSQL_USER=$MYSQL_USER --build-arg MYSQL_DATABASE=$MYSQL_DATABASE .
+
+mkdir -p ./project-package-jsons/projects/default-project
+cp packages/projects/default-project/package.json ./project-package-jsons/projects/default-project
+find packages/projects/projects/ -name package.json -exec bash -c 'mkdir -p ./project-package-jsons/$(dirname $1) && cp $1 ./project-package-jsons/$(dirname $1)' - '{}' \;
+DOCKER_BUILDKIT=1 docker build -t xrengine \
+  --build-arg MYSQL_HOST=$MYSQL_HOST \
+  --build-arg MYSQL_PORT=$MYSQL_PORT \
+  --build-arg MYSQL_PASSWORD=$MYSQL_PASSWORD \
+  --build-arg MYSQL_USER=$MYSQL_USER \
+  --build-arg MYSQL_DATABASE=$MYSQL_DATABASE \
+  --build-arg VITE_CLIENT_HOST=$VITE_CLIENT_HOST \
+  --build-arg VITE_SERVER_HOST=$VITE_SERVER_HOST \
+  --build-arg VITE_GAMESERVER_HOST=$VITE_GAMESERVER_HOST .
 
 DOCKER_BUILDKIT=1 docker build -t xrengine-testbot -f ./dockerfiles/testbot/Dockerfile-testbot .
