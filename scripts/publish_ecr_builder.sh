a16902f1a0ef1af9e7cbfa6b87c6a57acb2853be
Rajat <rajattalwar26@gmail.com>
2022-02-15 19:13:42 +0530

    revert changes to publish_ecr_builder.sh

--- scripts/publish_ecr_builder.sh
+++ scripts/publish_ecr_builder.sh
@@ -6,15 +6,15 @@ STAGE=$1
 TAG=$2
 LABEL=$3
 PRIVATE_ECR=$4
-REGION=ap-south-1
+REGION=$5
 
 if [ $PRIVATE_ECR == "true" ]
 then
   aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
   node ./scripts/prune_ecr_images.js --repoName $REPO_NAME-builder --region $REGION
 else
-  aws ecr-public get-login-password --region $REGION | docker login -u AWS --password-stdin $ECR_URL
-  node ./scripts/prune_ecr_images.js --repoName $REPO_NAME-builder --region $REGION --public
+  aws ecr-public get-login-password --region us-east-1 | docker login -u AWS --password-stdin $ECR_URL
+  node ./scripts/prune_ecr_images.js --repoName $REPO_NAME-builder --region us-east-1 --public
 fi
 
 docker tag $LABEL $ECR_URL/$REPO_NAME-builder:$TAG
