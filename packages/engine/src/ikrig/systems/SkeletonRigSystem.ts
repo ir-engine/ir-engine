31a9f22b16f1d938afb24c7644ab70cfeeb5fb5f
HexaField <joshfield999@gmail.com>
2022-02-16 21:11:37 +1100

    fix mock avatars, remove unneeded call in ikfunctions

--- packages/engine/src/ikrig/systems/SkeletonRigSystem.ts
+++ packages/engine/src/ikrig/systems/SkeletonRigSystem.ts
@@ -13,6 +13,8 @@ import { NetworkId } from '@xrengine/common/src/interfaces/NetworkId'
 import { random } from 'lodash'
 import { CameraIKComponent } from '../components/CameraIKComponent'
 import { applyCameraLook } from '../functions/IKSolvers'
+import { receiveActionOnce } from '../../networking/functions/matchActionOnce'
+import { EngineEvents } from '../../ecs/classes/EngineEvents'
 
 const logCustomTargetRigBones = (targetRig) => {
   if (targetRig.name !== 'custom') {
@@ -39,8 +41,8 @@ const logCustomTargetRigBones = (targetRig) => {
 const avatars = ['Gold', 'Green', 'Pink', 'Red', 'Silver', 'Yellow']
 
 const mockAvatars = () => {
-  for (let i = 0; i < 50; i++) {
-    const cyberbot = avatars[random(avatars.length)]
+  for (let i = 0; i < 100; i++) {
+    const cyberbot = avatars[random(avatars.length) ?? 'Gold']
     const avatarDetail = {
       thumbnailURL: `/projects/default-project/avatars/Cyberbot${cyberbot}.png`,
       avatarURL: `/projects/default-project/avatars/Cyberbot${cyberbot}.glb`,
@@ -54,16 +56,22 @@ const mockAvatars = () => {
 
     const networkId = (1000 + i) as NetworkId
 
-    dispatchLocal(NetworkWorldAction.createClient({ $from: userId, name: 'user', index: networkId }))
-    dispatchLocal({ ...NetworkWorldAction.spawnAvatar({ parameters, ownerIndex: networkId }), networkId })
-    dispatchLocal(NetworkWorldAction.avatarDetails({ avatarDetail }))
+    dispatchLocal({ ...NetworkWorldAction.createClient({ name: 'user', index: networkId }), $from: userId })
+    dispatchLocal({
+      ...NetworkWorldAction.spawnAvatar({ parameters, ownerIndex: networkId }),
+      networkId,
+      $from: userId
+    })
+    dispatchLocal({ ...NetworkWorldAction.avatarDetails({ avatarDetail }), $from: userId })
   }
 }
 
 export default async function SkeletonRigSystem(world: World) {
   const cameraIKQuery = defineQuery([IKRigComponent, CameraIKComponent])
   const ikposeQuery = defineQuery([IKPoseComponent, IKRigComponent, IKRigTargetComponent])
-  // mockAvatars()
+  // receiveActionOnce(EngineEvents.EVENTS.JOINED_WORLD, () => {
+  //   mockAvatars()
+  // })
   return () => {
     // Apply camera IK to the source skeleton
     for (const entity of cameraIKQuery()) {
