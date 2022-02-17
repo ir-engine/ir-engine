e31ab896200e0c58956b8cccbae6f5a446ad2c41
hanzlamateen <hanzlamateen@live.com>
2022-02-17 12:28:30 +0500

    Changes to add gameserver-load service

--- packages/server-core/src/networking/services.ts
+++ packages/server-core/src/networking/services.ts
@@ -2,12 +2,16 @@ import Instance from './instance/instance.service'
 import InstanceAttendance from './instance-attendance/instance-attendance.service'
 import InstanceProvision from './instance-provision/instance-provision.service'
 import InstanceAuthorizedUser from './instance-authorized-user/instance-authorized-user.service'
+import GameServerLoad from './gameserver-load/gameserver-load.service'
+import GameServerProvision from './gameserver-provision/gameserver-provision.service'
 import GameServerSubdomainProvision from './gameserver-subdomain-provision/gameserver-subdomain-provision.service'
 import RtcPorts from './rtc-ports/rtc-ports.service'
 
 export default [
   RtcPorts,
   Instance,
+  GameServerLoad,
+  GameServerProvision,
   GameServerSubdomainProvision,
   InstanceProvision,
   InstanceAttendance,
