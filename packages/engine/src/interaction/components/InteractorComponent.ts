c494d808299b5573c15f1ef8359a9c1a12a77ee3
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-03-05 08:20:56 -0500

    XRUI interactable modal improvements (#5419)
    * Improve object fit and transitions
    
    * Move tweens to AnimationSystem
    
    * Update object highlights
    
    * Update transitions
    
    * Improve interactibles
    
    * Cleanup interactive system
    
    * Fix interact raycast
    
    * Add stars!
    
    * Fix button alignment
    
    * Update InteractiveModalView.tsx

--- packages/engine/src/interaction/components/InteractorComponent.ts
+++ packages/engine/src/interaction/components/InteractorComponent.ts
@@ -4,9 +4,9 @@ import { Entity } from '../../ecs/classes/Entity'
 import { createMappedComponent } from '../../ecs/functions/ComponentFunctions'
 
 export type InteractorComponentType = {
-  focusedInteractive: Entity
+  focusedInteractive: Entity | null
   frustumCamera: PerspectiveCamera
-  subFocusedArray: any[]
+  subFocusedArray: Entity[]
 }
 
 export const InteractorComponent = createMappedComponent<InteractorComponentType>('InteractorComponent')
