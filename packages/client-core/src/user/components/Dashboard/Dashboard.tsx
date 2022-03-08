d96cd61bb76dd7e282269beb5b5dba953a19ec79
kimenyi kevin <kimenyikevin@gmail.com>
2022-03-09 01:31:35 +0200

    implemet logout for admin system

--- packages/client-core/src/user/components/Dashboard/Dashboard.tsx
+++ packages/client-core/src/user/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { ChevronLeft, ChevronRight, Menu } from '@mui/icons-material'
 import AppBar from '@mui/material/AppBar'
@@ -7,6 +7,8 @@ import Avatar from '@mui/material/Avatar'
 import CssBaseline from '@mui/material/CssBaseline'
 import Drawer from '@mui/material/Drawer'
 import IconButton from '@mui/material/IconButton'
+import MenuComponent from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 import { styled, useTheme } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -34,6 +36,15 @@ const Dashboard = ({ children }: Props) => {
   const [open, setOpen] = React.useState(false)
   const admin = authState.user
   const isLoggedIn = authState.isLoggedIn.value
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const openMenu = Boolean(anchorEl)
+  const handleClickMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   const handleDrawerOpen = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
     if (
@@ -65,15 +76,35 @@ const Dashboard = ({ children }: Props) => {
           <div className={classes.appBarHeadingContainer}>
             <Typography variant="h6">Dashboard</Typography>
             {admin?.name.value && (
-              <div className={classes.avatarPosition}>
+              <IconButton
+                className={classes.avatarPosition}
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={openMenu ? 'true' : undefined}
+                onClick={handleClickMenu}
+              >
                 <Avatar className={classes.orange}>{admin?.name?.value.charAt(0)?.toUpperCase()}</Avatar>
                 <Typography variant="h6" className={clsx(classes.marginLft, classes.appBarHeadingName)}>
                   {admin?.name.value}
                 </Typography>
-              </div>
+              </IconButton>
             )}
           </div>
         </Toolbar>
+
+        <MenuComponent
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={openMenu}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button'
+          }}
+        >
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </MenuComponent>
       </AppBar>
       <Drawer
         variant={open ? 'temporary' : 'permanent'}
