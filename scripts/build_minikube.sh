ebe008398f1282ebea0c0018a7740da069cfe894
Hanzla Mateen <hanzlamateen@live.com>
2022-03-09 08:43:50 +0500

    Added root-builder to minikube build script

--- scripts/build_minikube.sh
+++ scripts/build_minikube.sh
@@ -64,6 +64,9 @@ eval $(minikube docker-env)
 mkdir -p ./project-package-jsons/projects/default-project
 cp packages/projects/default-project/package.json ./project-package-jsons/projects/default-project
 find packages/projects/projects/ -name package.json -exec bash -c 'mkdir -p ./project-package-jsons/$(dirname $1) && cp $1 ./project-package-jsons/$(dirname $1)' - '{}' \;
+
+DOCKER_BUILDKIT=1 docker build -t root-builder -f dockerfiles/package-root/Dockerfile-root .
+
 DOCKER_BUILDKIT=1 docker build -t xrengine \
   --build-arg MYSQL_HOST=$MYSQL_HOST \
   --build-arg MYSQL_PORT=$MYSQL_PORT \
