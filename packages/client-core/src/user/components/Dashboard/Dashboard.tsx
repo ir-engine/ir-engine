669a87f4734bdcab178441dd52950d64a017ef78
kimenyi kevin <kimenyikevin@gmail.com>
2022-03-09 15:45:53 +0200

    implement logout on admin dashboard

--- packages/client-core/src/user/components/Dashboard/Dashboard.tsx
+++ packages/client-core/src/user/components/Dashboard/Dashboard.tsx
@@ -1,33 +1,18 @@
-import clsx from 'clsx'
-import React, { useState } from 'react'
-<<<<<<< HEAD
-import { useTranslation } from 'react-i18next'
-
-import ProfileMenu from '@xrengine/client-core/src/user/components/UserMenu/menus/ProfileMenu'
-=======
->>>>>>> d96cd61bb (implemet logout for admin system)
-
-import { ChevronLeft, ChevronRight, Menu } from '@mui/icons-material'
-import { Person } from '@mui/icons-material'
+import { ChevronLeft, ChevronRight, Menu, Person } from '@mui/icons-material'
 import { ClickAwayListener } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import CssBaseline from '@mui/material/CssBaseline'
 import Drawer from '@mui/material/Drawer'
 import IconButton from '@mui/material/IconButton'
-<<<<<<< HEAD
 import { useTheme } from '@mui/material/styles'
-=======
-import MenuComponent from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import { styled, useTheme } from '@mui/material/styles'
-import Toolbar from '@mui/material/Toolbar'
->>>>>>> d96cd61bb (implemet logout for admin system)
 import Typography from '@mui/material/Typography'
-
+import ProfileMenu from '@xrengine/client-core/src/user/components/UserMenu/menus/ProfileMenu'
+import clsx from 'clsx'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useAuthState } from '../../services/AuthService'
 import DashboardMenuItem from './DashboardMenuItem'
 import styles from './styles.module.scss'
-
 interface Props {
   children?: JSX.Element
 }
@@ -46,13 +31,8 @@ const Dashboard = ({ children }: Props) => {
   const [open, setOpen] = React.useState(false)
   const [profileMenuOpen, setProfileMenuOpen] = useState(false)
   const user = authState.user
-<<<<<<< HEAD
-=======
   const isLoggedIn = authState.isLoggedIn.value
-<<<<<<< HEAD
->>>>>>> implemet logout for admin system
   const { t } = useTranslation()
-=======
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const openMenu = Boolean(anchorEl)
   const handleClickMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -62,7 +42,6 @@ const Dashboard = ({ children }: Props) => {
   const handleClose = () => {
     setAnchorEl(null)
   }
->>>>>>> d96cd61bb (implemet logout for admin system)
 
   const handleDrawerOpen = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
     if (
@@ -77,12 +56,7 @@ const Dashboard = ({ children }: Props) => {
   return (
     <div>
       <CssBaseline />
-<<<<<<< HEAD
       <AppBar position="fixed" className={styles.appBar}>
-=======
-      <AppBar position="fixed" className={classes.appBar}>
-<<<<<<< HEAD
->>>>>>> implemet logout for admin system
         <nav className={styles.navbar}>
           <div className={styles.navContainer}>
             <IconButton
@@ -118,54 +92,6 @@ const Dashboard = ({ children }: Props) => {
             </div>
           </div>
         </nav>
-=======
-        <Toolbar className={classes.header}>
-          <IconButton
-            color="inherit"
-            style={{ color: 'white' }}
-            aria-label="open drawer"
-            onClick={handleDrawerOpen(true)}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open
-            })}
-            size="large"
-          >
-            <Menu />
-          </IconButton>
-          <div className={classes.appBarHeadingContainer}>
-            <Typography variant="h6">Dashboard</Typography>
-            {admin?.name.value && (
-              <IconButton
-                className={classes.avatarPosition}
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={openMenu ? 'true' : undefined}
-                onClick={handleClickMenu}
-              >
-                <Avatar className={classes.orange}>{admin?.name?.value.charAt(0)?.toUpperCase()}</Avatar>
-                <Typography variant="h6" className={clsx(classes.marginLft, classes.appBarHeadingName)}>
-                  {admin?.name.value}
-                </Typography>
-              </IconButton>
-            )}
-          </div>
-        </Toolbar>
-
-        <MenuComponent
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={openMenu}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button'
-          }}
-        >
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </MenuComponent>
->>>>>>> d96cd61bb (implemet logout for admin system)
       </AppBar>
       <Drawer
         variant={open ? 'temporary' : 'permanent'}
