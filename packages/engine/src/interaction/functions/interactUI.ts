218832c821b50ce9ebc4112685bddae87b4f91ca
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-03-09 11:45:48 -0500

    Fix some interactive system errors while scene is loading (#5439)

--- packages/engine/src/interaction/functions/interactUI.ts
+++ packages/engine/src/interaction/functions/interactUI.ts
@@ -195,16 +195,16 @@ const transitionStartTime = new Map<Entity, number>()
 export const updateInteractUI = (modelEntity: Entity, xrui: ReturnType<typeof createInteractUI>) => {
   if (Engine.isEditor) return
 
+  const world = useWorld()
   const uiContainer = getComponent(xrui.entity, XRUIComponent)?.container
   const anchoredPosition = ANCHORED_POSITION.get(modelEntity)!
   const anchoredRotation = ANCHORED_ROTATION.get(modelEntity)!
+  const localInteractor = getComponent(world.localClientEntity, InteractorComponent)
 
-  if (!uiContainer || !anchoredPosition) return
+  if (!uiContainer || !anchoredPosition || !localInteractor) return
 
   const modelGroup = getComponent(modelEntity, Object3DComponent).value
 
-  const world = useWorld()
-  const localInteractor = getComponent(world.localClientEntity, InteractorComponent)
   const hasFocus = localInteractor.focusedInteractive === modelEntity
   const interacted = hasComponent(modelEntity, InteractedComponent)
 
