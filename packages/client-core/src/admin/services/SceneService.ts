fd0660513f1e3245808436efe45ce68742cf3563
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-03-15 15:25:33 +0500

    refactor admin code

--- packages/client-core/src/admin/services/SceneService.ts
+++ packages/client-core/src/admin/services/SceneService.ts
@@ -1,6 +1,6 @@
 import { createState, useState } from '@speigg/hookstate'
 
-import { SceneData, SceneMetadata } from '@xrengine/common/src/interfaces/SceneInterface'
+import { SceneMetadata } from '@xrengine/common/src/interfaces/SceneInterface'
 
 import { client } from '../../feathers'
 import { store, useDispatch } from '../../store'
