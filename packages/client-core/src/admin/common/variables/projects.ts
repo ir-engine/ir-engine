2aa0c4d70f55716906491fc3e9e67f89e704e60f
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-03-15 14:52:22 +0500

    center align column headings in project route of admin panel

--- packages/client-core/src/admin/common/variables/projects.ts
+++ packages/client-core/src/admin/common/variables/projects.ts
@@ -2,18 +2,18 @@ export interface ProjectColumn {
   id: 'name' | 'update' | 'invalidate' | 'view' | 'action'
   label: string
   minWidth?: number
-  align?: 'right'
+  align?: 'right' | 'center'
 }
 
 export const projectsColumns: ProjectColumn[] = [
   { id: 'name', label: 'name', minWidth: 65 },
-  { id: 'update', label: 'Update', minWidth: 65 },
-  { id: 'invalidate', label: 'Invalidate Cache', minWidth: 65 },
-  { id: 'view', label: 'View Project Files', minWidth: 65 },
+  { id: 'update', label: 'Update', minWidth: 65, align: 'center' },
+  { id: 'invalidate', label: 'Invalidate Cache', minWidth: 65, align: 'center' },
+  { id: 'view', label: 'View Project Files', minWidth: 65, align: 'center' },
   {
     id: 'action',
     label: 'Remove',
     minWidth: 65,
-    align: 'right'
+    align: 'center'
   }
 ]
