a587f0dbb7118b6edb165399422a0195131902d6
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2022-03-16 13:12:09 +0200

    added i18n

--- packages/client-core/src/user/services/AuthService.ts
+++ packages/client-core/src/user/services/AuthService.ts
@@ -2,6 +2,7 @@ import { createState, Downgraded, useState } from '@speigg/hookstate'
 // TODO: Decouple this
 // import { endVideoChat, leave } from '@xrengine/engine/src/networking/functions/SocketWebRTCClientFunctions';
 import axios from 'axios'
+import i18n from 'i18next'
 import querystring from 'querystring'
 import { v1 } from 'uuid'
 
@@ -58,7 +59,7 @@ export const avatarFetchedReceptor = (s: typeof state, action: ReturnType<typeof
   for (let resource of resources) {
     const r = avatarData[(resource as any).name] || {}
     if (!r) {
-      console.warn('Avatar resource is empty, have you synced avatars to your static file storage?')
+      console.warn(i18n.t('user:avatar.warning-msg'))
       return
     }
     r[(resource as any).staticResourceType] = resource
@@ -261,7 +262,7 @@ export const AuthService = {
         dispatch(AuthAction.loadedUserData(user))
       })
       .catch((err: any) => {
-        AlertService.dispatchAlertError(new Error('Failed to load user data'))
+        AlertService.dispatchAlertError(new Error(i18n.t('common:error.loading-error')))
       })
   },
   loginUserByPassword: async (form: EmailLoginForm) => {
@@ -269,7 +270,7 @@ export const AuthService = {
     {
       // check email validation.
       if (!validateEmail(form.email)) {
-        AlertService.dispatchAlertError(new Error('Please input valid email address'))
+        AlertService.dispatchAlertError(new Error(i18n.t('common:error.validation-error', { type: 'email address' })))
 
         return
       }
@@ -296,7 +297,7 @@ export const AuthService = {
           AuthService.loadUserData(authUser.identityProvider.userId).then(() => (window.location.href = '/'))
         })
         .catch((err: any) => {
-          dispatch(AuthAction.loginUserError('Failed to login'))
+          dispatch(AuthAction.loginUserError(i18n.t('common:error.login-error')))
           AlertService.dispatchAlertError(err)
         })
         .finally(() => dispatch(AuthAction.actionProcessing(false)))
@@ -320,7 +321,7 @@ export const AuthService = {
         // loadXRAvatarForUpdatedUser(walletUser) // TODO
         dispatch(AuthAction.loadedUserData(walletUser))
       } catch (err) {
-        dispatch(AuthAction.loginUserError('Failed to login'))
+        dispatch(AuthAction.loginUserError(i18n.t('common:error.login-error')))
         AlertService.dispatchAlertError(err)
       } finally {
         dispatch(AuthAction.actionProcessing(false))
@@ -368,7 +369,7 @@ export const AuthService = {
         dispatch(AuthAction.actionProcessing(false))
         window.location.href = redirectSuccess
       } catch (err) {
-        dispatch(AuthAction.loginUserError('Failed to login'))
+        dispatch(AuthAction.loginUserError(i18n.t('common:error.login-error')))
         AlertService.dispatchAlertError(err)
         window.location.href = `${redirectError}?error=${err.message}`
         dispatch(AuthAction.actionProcessing(false))
@@ -521,7 +522,9 @@ export const AuthService = {
         const stripped = emailPhone.replace(/-/g, '')
         if (validatePhoneNumber(stripped)) {
           if (!enableSmsMagicLink) {
-            AlertService.dispatchAlertError(new Error('Please input valid email address'))
+            AlertService.dispatchAlertError(
+              new Error(i18n.t('common:error.validation-error', { type: 'email address' }))
+            )
 
             return
           }
@@ -530,13 +533,17 @@ export const AuthService = {
           emailPhone = '+1' + stripped
         } else if (validateEmail(emailPhone)) {
           if (!enableEmailMagicLink) {
-            AlertService.dispatchAlertError(new Error('Please input valid phone number'))
+            AlertService.dispatchAlertError(
+              new Error(i18n.t('common:error.validation-error', { type: 'phone number' }))
+            )
 
             return
           }
           type = 'email'
         } else {
-          AlertService.dispatchAlertError(new Error('Please input valid email or phone number'))
+          AlertService.dispatchAlertError(
+            new Error(i18n.t('common:error.validation-error', { type: 'email or phone number' }))
+          )
 
           return
         }
@@ -551,7 +558,7 @@ export const AuthService = {
         .then((res: any) => {
           console.log(res)
           dispatch(AuthAction.didCreateMagicLink(true))
-          AlertService.dispatchAlertSuccess('Login Magic Link was sent. Please check your Email or SMS.')
+          AlertService.dispatchAlertSuccess(i18n.t('user:auth.magiklink.success-msg'))
         })
         .catch((err: any) => {
           dispatch(AuthAction.didCreateMagicLink(false))
@@ -597,7 +604,7 @@ export const AuthService = {
         .then((res: any) => {
           const identityProvider = res as IdentityProvider
           if (identityProvider.userId != null) {
-            AlertService.dispatchAlertSuccess('Login Magic Link was sent. Please check your Email.')
+            AlertService.dispatchAlertSuccess(i18n.t('user:auth.magiklink.email-sent-msg'))
             return AuthService.loadUserData(identityProvider.userId)
           }
         })
@@ -627,7 +634,7 @@ export const AuthService = {
         .then((res: any) => {
           const identityProvider = res as IdentityProvider
           if (identityProvider.userId != null) {
-            AlertService.dispatchAlertSuccess('Login Magic Link was sent. Please check your SMS.')
+            AlertService.dispatchAlertSuccess(i18n.t('user:auth.magiklink.sms-sent-msg'))
             return AuthService.loadUserData(identityProvider.userId)
           }
         })
@@ -711,7 +718,7 @@ export const AuthService = {
         .service('user')
         .patch(selfUser.id.value, { avatarId: avatarName })
         .then((_) => {
-          AlertService.dispatchAlertSuccess('Avatar Uploaded Successfully.')
+          AlertService.dispatchAlertSuccess(i18n.t('user:avatar.upload-success-msg'))
           dispatchFrom(Engine.userId, () =>
             NetworkWorldAction.avatarDetails({
               avatarDetail: response
@@ -737,7 +744,7 @@ export const AuthService = {
           query: { keys }
         })
         .then((_) => {
-          AlertService.dispatchAlertSuccess('Avatar Removed Successfully.')
+          AlertService.dispatchAlertSuccess(i18n.t('user:avatar.remove-success-msg'))
           AuthService.fetchAvatarList()
         })
     }
@@ -768,7 +775,7 @@ export const AuthService = {
           name: name
         })
         .then((res: any) => {
-          AlertService.dispatchAlertSuccess('Username updated')
+          AlertService.dispatchAlertSuccess(i18n.t('user:usermenu.profile.update-msg'))
           dispatch(AuthAction.usernameUpdated(res))
         })
     }
