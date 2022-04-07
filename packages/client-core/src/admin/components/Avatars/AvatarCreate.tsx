4b1cac702cb82af5a9b24a6128b337a709852d3c
HexaField <joshfield999@gmail.com>
2022-04-07 18:33:29 +1000

    make same change in admin avatar create

--- packages/client-core/src/admin/components/Avatars/AvatarCreate.tsx
+++ packages/client-core/src/admin/components/Avatars/AvatarCreate.tsx
@@ -155,7 +155,7 @@ const AvatarCreate = ({ handleClose, open }) => {
 
     controls.minDistance = 0.1
     controls.maxDistance = 10
-    controls.target.set(0, 1.25, 0)
+    controls.target.set(0, 1.5, 0)
     controls.update()
 
     scene.children = scene.children.filter((c) => c.name !== 'avatar')
