66521ddabeba6c6306338db4be48b7132d6aed64
Nayan Patel <nayanpatel.np@yahoo.com>
2022-04-11 15:55:30 +0530

    Csm issue fix (#5684)
    * CSM Improvements, added useInCSM option in directional light, CSM enabling and disableing issue fix, changing the rotation of the direction light in which useInCSM is true will change direction of the lights in CSM
    
    * CSM changes and bug fixes

--- packages/editor/src/functions/sceneRenderFunctions.ts
+++ packages/editor/src/functions/sceneRenderFunctions.ts
@@ -228,6 +228,8 @@ export async function exportScene(options = {} as DefaultExportOptionsType) {
 }*/
 
 export function disposeScene() {
+  Engine.activeCSMLightEntity = null
+  Engine.directionalLightEntities = []
   if (Engine.activeCameraEntity) removeEntity(Engine.activeCameraEntity, true)
   if (SceneState.gizmoEntity) removeEntity(SceneState.gizmoEntity, true)
   if (SceneState.editorEntity) removeEntity(SceneState.editorEntity, true)
