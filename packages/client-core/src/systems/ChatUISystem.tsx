d60191c913f8cc3169e32c050efcc11dd4acd509
Gheric Speiginer <gheric.speiginer@gmail.com>
2022-04-12 12:55:19 -0400

    Fix text input

--- packages/client-core/src/systems/ChatUISystem.tsx
+++ packages/client-core/src/systems/ChatUISystem.tsx
@@ -9,6 +9,17 @@ import { createChatDetailView } from './ui/ChatDetailView'
 
 export default async function ChatUISystem(world: World) {
   const ui = createChatDetailView()
+
+  ui.container.then((container) => {
+    const el = container.containerElement as HTMLElement
+    // In this caes, it's necessary to keep the element visible in the DOM,
+    // otherwise it will not receive text input; of course, we don't want to
+    // actually display the real DOM elmeent since we are rendering it in 3D,
+    // so we simply move it out the way
+    el.style.visibility = 'visible'
+    el.style.top = '-100000px'
+  })
+
   return () => {
     const chatXRUI = getComponent(ui.entity, XRUIComponent)
     chatXRUI.container.scale.setScalar(0.5)
