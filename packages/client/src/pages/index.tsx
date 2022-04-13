4cdd8b5618b97d743e66577b96b928f6b1af82ba
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2022-04-13 14:48:31 +0200

    added ui pop up suggesting user to enable hardware acceleration

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import { getGPUTier } from 'detect-gpu'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { Trans, useTranslation } from 'react-i18next'
 import { Redirect } from 'react-router-dom'
@@ -7,6 +8,7 @@ import {
   ClientSettingService,
   useClientSettingState
 } from '@xrengine/client-core/src/admin/services/Setting/ClientSettingService'
+import WarningMessage from '@xrengine/client-core/src/common/components/WarningMessage/WarningMessage'
 import ProfileMenu from '@xrengine/client-core/src/user/components/UserMenu/menus/ProfileMenu'
 import { AuthService } from '@xrengine/client-core/src/user/services/AuthService'
 
@@ -15,8 +17,16 @@ const ROOT_REDIRECT: any = globalThis.process.env['VITE_ROOT_REDIRECT']
 export const HomePage = (): any => {
   const { t } = useTranslation()
   const clientSettingState = useClientSettingState()
+  const [isHardwareAccelerationEnabled, setIsHardwareAccelerationEnabled] = useState(true)
   const [clientSetting] = clientSettingState?.client?.value || []
 
+  useEffect(() => {
+    const checkGpuTier = async () => {
+      const gpuTier = await getGPUTier()
+      if (!gpuTier.fps && gpuTier.type == 'FALLBACK') setIsHardwareAccelerationEnabled(false)
+    }
+    checkGpuTier()
+  }, [])
   useEffect(() => {
     AuthService.doLoginAuto(true)
   }, [])
@@ -31,44 +41,46 @@ export const HomePage = (): any => {
     else window.location.href = ROOT_REDIRECT
   } else
     return (
-      <div className="lander">
-        <style>
-          {`
+      <Fragment>
+        {isHardwareAccelerationEnabled ? (
+          <div className="lander">
+            <style>
+              {`
             [class*=lander] {
                 pointer-events: auto;
             }
           `}
-        </style>
-        <Helmet>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;800&display=swap"
-            rel="stylesheet"
-          />
-        </Helmet>
-        <div className="main-background">
-          <div className="img-container">
-            {clientSetting?.appBackground && <img src={clientSetting.appBackground} alt="" />}
-          </div>
-        </div>
-        <nav className="navbar">
-          <div className="logo-section">
-            {clientSetting?.appTitle && <object className="lander-logo" data={clientSetting.appTitle} />}
-            <div className="logo-bottom">
-              {clientSetting?.appSubtitle && <span className="white-txt">{clientSetting.appSubtitle}</span>}
+            </style>
+            <Helmet>
+              <link
+                href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;800&display=swap"
+                rel="stylesheet"
+              />
+            </Helmet>
+            <div className="main-background">
+              <div className="img-container">
+                {clientSetting?.appBackground && <img src={clientSetting.appBackground} alt="" />}
+              </div>
             </div>
-          </div>
-        </nav>
-        <div className="main-section">
-          <div className="desc">
-            {clientSetting?.appDescription && (
-              <Trans t={t} i18nKey={clientSetting.appDescription}>
-                <span>{clientSetting.appDescription}</span>
-              </Trans>
-            )}
-          </div>
-          <div className="form-container">
-            <style>
-              {`
+            <nav className="navbar">
+              <div className="logo-section">
+                {clientSetting?.appTitle && <object className="lander-logo" data={clientSetting.appTitle} />}
+                <div className="logo-bottom">
+                  {clientSetting?.appSubtitle && <span className="white-txt">{clientSetting.appSubtitle}</span>}
+                </div>
+              </div>
+            </nav>
+            <div className="main-section">
+              <div className="desc">
+                {clientSetting?.appDescription && (
+                  <Trans t={t} i18nKey={clientSetting.appDescription}>
+                    <span>{clientSetting.appDescription}</span>
+                  </Trans>
+                )}
+              </div>
+              <div className="form-container">
+                <style>
+                  {`
                 [class*=menuPanel] {
                     position: unset;
                     bottom: 0px;
@@ -80,24 +92,28 @@ export const HomePage = (): any => {
                     pointer-events: auto;
                 }
               `}
-            </style>
-            <ProfileMenu />
+                </style>
+                <ProfileMenu />
+              </div>
+            </div>
+            <div className="link-container">
+              <div className="link-block">
+                {clientSetting?.appSocialLinks?.length > 0 &&
+                  clientSetting.appSocialLinks.map((social, index) => (
+                    <a key={index} target="_blank" className="icon" href={social.link}>
+                      <img src={social.icon} />
+                    </a>
+                  ))}
+              </div>
+              <div className="logo-bottom">
+                {clientSetting?.appSubtitle && <span className="white-txt">{clientSetting.appSubtitle}</span>}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="link-container">
-          <div className="link-block">
-            {clientSetting?.appSocialLinks?.length > 0 &&
-              clientSetting.appSocialLinks.map((social, index) => (
-                <a key={index} target="_blank" className="icon" href={social.link}>
-                  <img src={social.icon} />
-                </a>
-              ))}
-          </div>
-          <div className="logo-bottom">
-            {clientSetting?.appSubtitle && <span className="white-txt">{clientSetting.appSubtitle}</span>}
-          </div>
-        </div>
-      </div>
+        ) : (
+          <WarningMessage setIsHardwareAccelerationEnabled={setIsHardwareAccelerationEnabled} />
+        )}
+      </Fragment>
     )
 }
 
