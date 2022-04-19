fe6364229f7dd532fbef4daa66ac125e78ee70db
rondoor124 <rondoor124@gmail.com>
2022-04-19 16:00:49 -0500

    update material

--- packages/engine/src/scene/functions/loaders/SimpleMaterialFunctions.ts
+++ packages/engine/src/scene/functions/loaders/SimpleMaterialFunctions.ts
@@ -79,167 +79,7 @@ export const useSimpleMaterial = (obj: Mesh): void => {
       let vertexShader = ShaderLib.basic.vertexShader
       let fragmentShader = ShaderLib.basic.fragmentShader
 
-      // const vertexNonUVShader = `
-      //   void main() {
-      //     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      //   }
-      // `
-      // const vertexUVShader = [
-      //   'varying vec2 vUv;',
-      //   '#include <skinning_pars_vertex>',
-      //   'void main() {',
-      //   '#include <skinbase_vertex>',
-      //   '#include <begin_vertex>',
-      //   '#include <skinning_vertex>',
-      //   '#include <project_vertex>',
-      //   'vUv = uv;',
-      //   '}'
-      // ].join('\n')
-
-      // const fragmentColorShader = [
-      //   'uniform vec3 color;',
-      //   'void main() {',
-      //   '	gl_FragColor = vec4(color.r, color.g, color.b, 1.0);',
-      //   '}'
-      // ].join('\n')
-
-      // const fragmentTextureShader = [
-      //   'varying vec2 vUv;',
-      //   'uniform sampler2D map;',
-      //   'void main() {',
-      //   '	gl_FragColor = texture2D(map, vUv);',
-      //   '}'
-      // ].join('\n')
-
-      // const vertexShader = `
-      //   #include <common>
-      //   #include <uv_pars_vertex>
-      //   #include <uv2_pars_vertex>
-      //   #include <envmap_pars_vertex>
-      //   #include <color_pars_vertex>
-      //   #include <fog_pars_vertex>
-      //   #include <morphtarget_pars_vertex>
-      //   #include <skinning_pars_vertex>
-      //   #include <logdepthbuf_pars_vertex>
-      //   #include <clipping_planes_pars_vertex>
-      //   void main() {
-      //     #include <uv_vertex>
-      //     #include <uv2_vertex>
-      //     #include <color_vertex>
-      //     #include <morphcolor_vertex>
-      //     #if defined ( USE_ENVMAP ) || defined ( USE_SKINNING )
-      //       #include <beginnormal_vertex>
-      //       #include <morphnormal_vertex>
-      //       #include <skinbase_vertex>
-      //       #include <skinnormal_vertex>
-      //       #include <defaultnormal_vertex>
-      //     #endif
-      //     #include <begin_vertex>
-      //     #include <morphtarget_vertex>
-      //     #include <skinning_vertex>
-      //     #include <project_vertex>
-      //     #include <logdepthbuf_vertex>
-      //     #include <clipping_planes_vertex>
-      //     #include <worldpos_vertex>
-      //     #include <envmap_vertex>
-      //     #include <fog_vertex>
-      //   }
-      // `
-      // const fragment = `
-      //   uniform vec3 diffuse;
-      //   uniform float opacity;
-      //   #ifndef FLAT_SHADED
-      //     varying vec3 vNormal;
-      //   #endif
-      //   #include <common>
-      //   #include <dithering_pars_fragment>
-      //   #include <color_pars_fragment>
-      //   #include <uv_pars_fragment>
-      //   #include <uv2_pars_fragment>
-      //   #include <map_pars_fragment>
-      //   #include <alphamap_pars_fragment>
-      //   #include <alphatest_pars_fragment>
-      //   #include <aomap_pars_fragment>
-      //   #include <lightmap_pars_fragment>
-      //   #include <envmap_common_pars_fragment>
-      //   #include <envmap_pars_fragment>
-      //   #include <cube_uv_reflection_fragment>
-      //   #include <fog_pars_fragment>
-      //   #include <specularmap_pars_fragment>
-      //   #include <logdepthbuf_pars_fragment>
-      //   #include <clipping_planes_pars_fragment>
-      //   void main() {
-      //     #include <clipping_planes_fragment>
-      //     vec4 diffuseColor = vec4( diffuse, opacity );
-      //     #include <logdepthbuf_fragment>
-      //     #include <map_fragment>
-      //     #include <color_fragment>
-      //     #include <alphamap_fragment>
-      //     #include <alphatest_fragment>
-      //     #include <specularmap_fragment>
-      //     ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-      //     #ifdef USE_LIGHTMAP
-      //       vec4 lightMapTexel = texture2D( lightMap, vUv2 );
-      //       reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;
-      //     #else
-      //       reflectedLight.indirectDiffuse += vec3( 1.0 );
-      //     #endif
-      //     #include <aomap_fragment>
-      //     reflectedLight.indirectDiffuse *= diffuseColor.rgb;
-      //     vec3 outgoingLight = reflectedLight.indirectDiffuse;
-      //     #include <envmap_fragment>
-      //     #include <output_fragment>
-      //     #include <tonemapping_fragment>
-      //     #include <encodings_fragment>
-      //     #include <fog_fragment>
-      //     #include <premultiplied_alpha_fragment>
-      //     #include <dithering_fragment>
-      //   }
-      // `
-
-      var uniforms = {
-        diffuse: {
-          value: (prevMaterial as any).color
-        },
-        opacity: {
-          value: (prevMaterial as any).opacity
-        },
-
-        uvTransform: { value: new Matrix3() },
-		    uv2Transform: { value: new Matrix3() },
-
-        map: {
-          value: (prevMaterial as any).map
-        },
-
-        alphaMap: {
-          value: (prevMaterial as any).alphaMap
-        },
-		    alphaTest: {
-          value: (prevMaterial as any).alphaTest
-        },
-
-        envMap: {
-          value: Engine.scene.environment
-        },
-        
-        aoMap: {
-          value: (prevMaterial as any).aoMap
-        },
-
-        aoMapIntensity: {
-          value: (prevMaterial as any).aoMapIntensity
-        },
-
-        bumpMap: {
-          value: (prevMaterial as any).bumpMap
-        },
-
-        bumpScale: {
-          value: (prevMaterial as any).bumpScale
-        }
-      }
-
+      const lightEnbled = true
       const hasUV = obj.geometry.hasAttribute('uv')
       const hasMap = (<any>obj.material).map !== null
       const hasLightMap = (<any>obj.material).lightMap !== null
@@ -250,6 +90,10 @@ export const useSimpleMaterial = (obj: Mesh): void => {
       const hasAlphaMap = (<any>obj.material).alphaMap !== null
 
       let defines = {}
+      if (lightEnbled) {
+        defines["USE_LIGHT_ENABLE"] = ''
+      }
+
       if (hasMap) {
         defines["USE_MAP"] = ''
       }
@@ -277,69 +121,239 @@ export const useSimpleMaterial = (obj: Mesh): void => {
         defines["USE_ALPHAMAP"] = ''
       }
 
-      vertexShader = `
-        #include <common>
-        #include <uv_pars_vertex>                                         //hasUV
-        #include <envmap_pars_vertex>                                     //hasEnvMap
-        #include <normal_pars_vertex>
-        void main() {
-          #include <uv_vertex>                                            //hasUV
-          #include <color_vertex>                                         
-          #if defined ( USE_ENVMAP ) || defined ( USE_SKINNING )          //hasEnvMap
-            #include <beginnormal_vertex>
-            #include <morphnormal_vertex>
-            #include <skinbase_vertex>
-            #include <skinnormal_vertex>
-            #include <defaultnormal_vertex>
-            #include <normal_vertex>
-          #endif
-          #include <begin_vertex>
-          #include <project_vertex>
-          #include <worldpos_vertex>
-          #include <envmap_vertex>                                        //hasEnvMap               
-        }
-      `
+      var uniforms = {
+        diffuse: {
+          value: (prevMaterial as any).color
+        },
+        opacity: {
+          value: (prevMaterial as any).opacity
+        },
 
-      fragmentShader = `
-        uniform vec3 diffuse;
-        uniform float opacity;
-        #include <common>
-        #include <color_pars_fragment>
-        #include <uv_pars_fragment>                                       //hasUV
-        #include <map_pars_fragment>                                      //hasMap
-        #include <alphamap_pars_fragment>                                 //hasAlphaMap
-        #include <alphatest_pars_fragment>
-        #include <aomap_pars_fragment>                                    //hasAoMap
-        #include <envmap_common_pars_fragment>                            //hasEnvMap
-        #include <envmap_pars_fragment>                                   //hasEnvMap
-        #include <specularmap_pars_fragment>                              //hasSpecularMap
-        #include <normal_pars_fragment>
-        #include <bumpmap_pars_fragment>
-        #include <normalmap_pars_fragment>
-        void main() {
-          vec4 diffuseColor = vec4( diffuse, opacity );
-          #include <map_fragment>                                         //hasMap
-          #include <color_fragment>
-          #include <alphamap_fragment>                                    //hasAlphaMap
-	        #include <alphatest_fragment>
-          #include <normal_fragment_begin>
-	        #include <normal_fragment_maps>
-          #include <specularmap_fragment>                                 //hasSpecularMap
-          ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-          #ifdef USE_LIGHTMAP
-            vec4 lightMapTexel = texture2D( lightMap, vUv2 );
-            reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;
-          #else
-            reflectedLight.indirectDiffuse += vec3( 1.0 );
+        uvTransform: { value: new Matrix3() },
+		    uv2Transform: { value: new Matrix3() },
+
+        map: {
+          value: (prevMaterial as any).map
+        },
+
+        alphaMap: {
+          value: (prevMaterial as any).alphaMap
+        },
+
+		    alphaTest: {
+          value: (prevMaterial as any).alphaTest
+        },
+
+        envMap: {
+          value: Engine.scene.environment
+        },
+        
+        flipEnvMap: {
+          value: 1
+        },
+        
+        aoMap: {
+          value: (prevMaterial as any).aoMap
+        },
+
+        aoMapIntensity: {
+          value: (prevMaterial as any).aoMapIntensity
+        },
+
+        bumpMap: {
+          value: (prevMaterial as any).bumpMap
+        },
+
+        bumpScale: {
+          value: (prevMaterial as any).bumpScale
+        }
+      }
+
+      if (lightEnbled) {
+        uniforms = UniformsUtils.merge([UniformsLib.lights, uniforms])
+      }
+
+      vertexShader = [
+        `#define STANDARD`,
+        `varying vec3 vViewPosition;`,
+        `#ifdef USE_TRANSMISSION
+          varying vec3 vWorldPosition;
+        #endif`,
+        `#include <common>`,
+        `#include <uv_pars_vertex>`,
+        `#include <uv2_pars_vertex>`,
+        `// #include <displacementmap_pars_vertex>`,
+        `#include <color_pars_vertex>`,
+        `// #include <fog_pars_vertex>`,
+        `#include <normal_pars_vertex>`,
+        `// #include <morphtarget_pars_vertex>`,
+        `// #include <skinning_pars_vertex>`,
+        `#include <shadowmap_pars_vertex> `,                    //lightEnbled
+        `// #include <logdepthbuf_pars_vertex>`,
+        `// #include <clipping_planes_pars_vertex>`,
+
+        `void main() {`,
+        `#include <uv_vertex>`,
+        `#include <uv2_vertex>`,
+        `#include <color_vertex>`,
+
+        `#include <beginnormal_vertex>`,
+        `#include <morphnormal_vertex>`,
+        `#include <skinbase_vertex>`,
+        `#include <skinnormal_vertex>`,
+        `#include <defaultnormal_vertex>`,
+        `#include <normal_vertex>`,
+
+        `#include <begin_vertex>`,
+        `// #include <morphtarget_vertex>`,
+        `// #include <skinning_vertex>`,
+        `// #include <displacementmap_vertex>`,
+        `#include <project_vertex>`,
+        `// #include <logdepthbuf_vertex>`,
+        `// #include <clipping_planes_vertex>`,
+        `vViewPosition = - mvPosition.xyz;`,
+
+        `#include <worldpos_vertex>`,
+        `#include <shadowmap_vertex>`,
+        `// #include <fog_vertex>`,
+        `#ifdef USE_TRANSMISSION`,
+        `vWorldPosition = worldPosition.xyz;`,
+        `#endif`,
+        `}`
+      ].join('\n')
+
+      fragmentShader = [
+        `#define STANDARD`,
+        `
+        #ifdef PHYSICAL
+          #define IOR
+          #define SPECULAR
+        #endif
+        `,
+        `uniform vec3 diffuse;`,
+        `uniform vec3 emissive;`,
+        `uniform float roughness;`,
+        `uniform float metalness;`,
+        `uniform float opacity;`,
+        `
+        #ifdef IOR
+          uniform float ior;
+        #endif
+        `,
+        `
+        #ifdef SPECULAR
+          uniform float specularIntensity;
+          uniform vec3 specularTint;
+        
+          #ifdef USE_SPECULARINTENSITYMAP
+            uniform sampler2D specularIntensityMap;
           #endif
-          #include <aomap_fragment>                                       //hasAoMap
+        
+          #ifdef USE_SPECULARTINTMAP
+            uniform sampler2D specularTintMap;
+          #endif
+        #endif
+        `,
+        `
+        #ifdef USE_CLEARCOAT
+          uniform float clearcoat;
+          uniform float clearcoatRoughness;
+        #endif
+        `,
+        `
+        #ifdef USE_SHEEN
+          uniform vec3 sheenTint;
+          uniform float sheenRoughness;
+        #endif
+        `,
+        `varying vec3 vViewPosition;`,
+        `#include <common>`,
+        `#include <packing>`,
+        `// #include <dithering_pars_fragment>`,
+        `#include <color_pars_fragment>`,
+        `#include <uv_pars_fragment>`,
+        `#include <uv2_pars_fragment>`,
+        `#include <map_pars_fragment>`,
+        `// #include <alphamap_pars_fragment>`,
+        `// #include <alphatest_pars_fragment>`,
+        `// #include <aomap_pars_fragment>`,
+        `// #include <lightmap_pars_fragment>`,
+        `// #include <emissivemap_pars_fragment>`,
+        `#include <bsdfs>`,                                                //lightEnbled
+        `// #include <cube_uv_reflection_fragment>`,
+        `// #include <envmap_common_pars_fragment>`,
+        `#include <envmap_physical_pars_fragment>`,
+        `// #include <fog_pars_fragment>`,
+        `#include <lights_pars_begin>`,                                    //lightEnbled
+        `#include <normal_pars_fragment>`,                                 //lightEnbled
+        `#include <lights_physical_pars_fragment>`,                        //lightEnbled
+        `#include <transmission_pars_fragment>`,
+        `#include <shadowmap_pars_fragment>`,                              //lightEnbled
+        `#include <bumpmap_pars_fragment>`,
+        `#include <normalmap_pars_fragment>`,
+        `#include <clearcoat_pars_fragment>`,
+        `#include <roughnessmap_pars_fragment>`,                            //lightEnbled
+        `#include <metalnessmap_pars_fragment>`,                            //lightEnbled
+        `//#include <logdepthbuf_pars_fragment>`,
+        `//#include <clipping_planes_pars_fragment>`,
+        `void main() {`,
+        `// #include <clipping_planes_fragment>`,
+        `
+        vec4 diffuseColor = vec4( diffuse, opacity );
+        ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+        vec3 totalEmissiveRadiance = emissive;
+        `,
+        `// #include <logdepthbuf_fragment>`,
+        `#include <map_fragment>`,
+        `#include <color_fragment>`,
+        `#include <alphamap_fragment>`,
+        `#include <alphatest_fragment>`,
+        `#include <roughnessmap_fragment>`,
+        `#include <metalnessmap_fragment>`,
+        `#include <normal_fragment_begin>`,
+        `#include <normal_fragment_maps>`,
+        `#include <clearcoat_normal_fragment_begin>`,
+        `#include <clearcoat_normal_fragment_maps>`,
+        `//#include <emissivemap_fragment>`,
+        lightEnbled ? `
+          // accumulation
+          #include <lights_physical_fragment>
+          #include <lights_fragment_begin>
+          #include <lights_fragment_maps>
+          #include <lights_fragment_end>
+          // modulation
+          #include <aomap_fragment>
+          vec3 totalDiffuse = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse;
+          vec3 totalSpecular = reflectedLight.directSpecular + reflectedLight.indirectSpecular;
+          #include <transmission_fragment>
+          vec3 outgoingLight = totalDiffuse + totalSpecular + totalEmissiveRadiance;
+        ` : `
+        // accumulation (baked indirect lighting only
+          #ifdef USE_LIGHTMAP
+          vec4 lightMapTexel= texture2D( lightMap, vUv2 );
+          reflectedLight.indirectDiffuse += lightMapTexelToLinear( lightMapTexel ).rgb * lightMapIntensity;
+          #else
+          reflectedLight.indirectDiffuse += vec3( 1.0 );
+          #endif
+          // modulation
+          #include <aomap_fragment>
           reflectedLight.indirectDiffuse *= diffuseColor.rgb;
           vec3 outgoingLight = reflectedLight.indirectDiffuse;
-          #include <output_fragment>
-          #include <envmap_fragment>                                      //hasEnvMap
-          #include <encodings_fragment>
-        }
-      `
+        `, `
+        #ifdef USE_CLEARCOAT
+          float dotNVcc = saturate( dot( geometry.clearcoatNormal, geometry.viewDir ) );
+          vec3 Fcc = F_Schlick( material.clearcoatF0, material.clearcoatF90, dotNVcc );
+          outgoingLight = outgoingLight * ( 1.0 - clearcoat * Fcc ) + clearcoatSpecular * clearcoat;
+        #endif
+        `,
+        `#include <output_fragment>`,
+        `// #include <tonemapping_fragment>`,
+        `#include <encodings_fragment>`,
+        `// #include <fog_fragment>`,
+        `// #include <premultiplied_alpha_fragment>`,
+        `// #include <dithering_fragment>`,
+        `}`
+      ].join('\n')
 
       // Object.keys(ShaderLib.basic.uniforms).forEach((original) => {
       //   let key = original
@@ -382,7 +396,7 @@ export const useSimpleMaterial = (obj: Mesh): void => {
         vertexShader,
         fragmentShader,
         // fog: false,
-        lights: false,
+        lights: lightEnbled,
         transparent: true
       })
       obj.material.needsUpdate = true
