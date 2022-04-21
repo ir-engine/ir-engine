d3ecb305dc7e14c804f1d40c1f22d78da8c25d7f
Josh Field <joshfield999@gmail.com>
2022-04-21 10:04:36 +1000

    fix cicd

--- scripts/install-test-projects.sh
+++ scripts/install-test-projects.sh
@@ -1,3 +1,3 @@
 mkdir packages/projects/projects
 cd packages/projects/projects
-git clone https://github.com/XRFoundation/XREngine-asset-test-suite
+git clone https://github.com/XRFoundation/XREngine-development-test-suite
