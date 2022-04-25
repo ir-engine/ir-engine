dd4de6f2a61c9c63af44c67f96117390a694aec1
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2022-04-25 18:05:27 +0200

    fixes failed checks

--- packages/client-core/src/util/prepareReportIssue.ts
+++ packages/client-core/src/util/prepareReportIssue.ts
@@ -1,29 +1,37 @@
-export function prepareReportIssue() {
-  if (console.everything === undefined) {
-    console.everything = []
-    window.onerror = function (error, url, line) {
-      console.everything.push({
-        type: 'exception',
-        value: { error, url, line }
-      })
-      return false
-    }
-
-    function hookLogType(logType) {
-      const original = console[logType].bind(console)
-      return function () {
-        console.everything.push({
-          type: logType,
-          value: Array.from(arguments)
-        })
-        original.apply(console, arguments)
-      }
-    }
-
-    ;['log', 'error', 'warn', 'debug'].forEach((logType) => {
-      console[logType] = hookLogType(logType)
-    })
+declare global {
+  interface Console {
+    everything: any
   }
+}
+
+export function prepareReportIssue() {
+  try {
+    if (console.everything === undefined) {
+      console.everything = []
+      window.onerror = function (error, url, line) {
+        console.everything.push({
+          type: 'exception',
+          value: { error, url, line }
+        })
+        return false
+      }
+
+      function hookLogType(logType) {
+        const original = console[logType].bind(console)
+        return function () {
+          console.everything.push({
+            type: logType,
+            value: Array.from(arguments)
+          })
+          original.apply(console, arguments)
+        }
+      }
+
+      ;['log', 'error', 'warn', 'debug'].forEach((logType) => {
+        console[logType] = hookLogType(logType)
+      })
+    }
+  } catch (err) {}
 
   return {
     logs: console.everything,
