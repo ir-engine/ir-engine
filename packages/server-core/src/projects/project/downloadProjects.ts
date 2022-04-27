94bf2ba379df8636a1033bf991f286de8f22e08e
Kyle Baran <kbaran@bitscoop.com>
2022-04-27 11:12:32 -0700

    Fixes to project build process, rearranging of dependencies.
    If a project's services use libraries that are not part of base XREngine, prepare-database in the build
    process will error out if it has to touch those services. Added a section to downloadProjects.ts that
    runs npm install on each project after it's downloaded (skipping default-project).
    
    Moved some libraries from devDependencies to dependencies, since they're needed for project dependency
    installation. Removed global install of these dependencies since they should now always be installed.

--- packages/server-core/src/projects/project/downloadProjects.ts
+++ packages/server-core/src/projects/project/downloadProjects.ts
@@ -1,4 +1,5 @@
 import appRootPath from 'app-root-path'
+import { spawn } from 'child_process'
 import fs from 'fs'
 import path from 'path'
 
@@ -34,6 +35,24 @@ export const download = async (projectName) => {
     )
 
     logger.info(`[ProjectLoader]: Successfully downloaded and mounted project "${projectName}".`)
+    if (projectName !== 'default-project') {
+      const npmInstallPromise = new Promise((resolve) => {
+        const npmInstallProcess = spawn('npm', ['install'], { cwd: localProjectDirectory })
+        npmInstallProcess.once('exit', resolve)
+        npmInstallProcess.once('error', resolve)
+        npmInstallProcess.once('disconnect', resolve)
+        npmInstallProcess.stdout.on('data', (data) => console.log(data.toString()))
+      }).then(console.log)
+      await Promise.race([
+        npmInstallPromise,
+        new Promise<void>((resolve) => {
+          setTimeout(() => {
+            console.log(`WARNING: npm installing ${projectName} too long!`)
+            resolve()
+          }, 20 * 60 * 1000) // timeout after 10 minutes
+        })
+      ])
+    }
   } catch (e) {
     logger.error(e, `[ProjectLoader]: Failed to download project with error: ${e.message}`)
     return false
