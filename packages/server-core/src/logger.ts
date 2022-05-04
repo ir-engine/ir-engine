15fafe4166f52601779933732693c148033ebd4a
Ashish <ashishguptajiit@gmail.com>
2022-05-04 15:49:09 +0530

    async logger

--- packages/server-core/src/logger.ts
+++ packages/server-core/src/logger.ts
@@ -2,34 +2,17 @@ import pino from 'pino'
 
 let node = process.env.ELASTIC_HOST || "http://localhost:9200";
 
-const transport = pino.transport({
-  targets: [
-    {
-      level: 'debug',
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: true
-      }
-    },
-    {
-      level: 'debug',
-      target: 'pino-elasticsearch',
-      options: {
-        index: 'xr-engine',
-        consistency: 'one',
-        node: node
-      }
-    }
-  ]
+const pinoElastic = require('pino-elasticsearch')
+
+const streamToElastic = pinoElastic({
+  index: 'an-index',
+  consistency: 'one',
+  node: 'http://localhost:9200',
+  'es-version': 7,
+  'flush-bytes': 1000
 })
 
-transport.on('ready', function () {
-  process.exit(0)
-})
-
-
-const logger = pino(transport)
+const logger = pino({ level: 'info' }, streamToElastic);
 
 // const logger = pino({
 //   transport: {
