c07e8a13a39a8a845d58217cb14590b5c14d3880
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2022-05-04 14:42:53 +0200

    added login at editor no-access page

--- packages/client-core/src/admin/common/SubmitDialog.tsx
+++ packages/client-core/src/admin/common/SubmitDialog.tsx
@@ -9,6 +9,7 @@ import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import TextField from '@mui/material/TextField'
 
+import ProfileMenu from '../../user/components/UserMenu/menus/ProfileMenu'
 import styles from '../styles/admin.module.scss'
 
 const FormDialog = () => {
@@ -28,7 +29,7 @@ const FormDialog = () => {
       >
         <DialogTitle id="form-dialog-title"> {t('admin:components.dialog.notAccess')}</DialogTitle>
         <DialogContent>
-          <DialogContentText className={styles.spanNone}>
+          <DialogContentText className={`${styles.spanNone} ${styles.mgBottom}`}>
             {t('admin:components.dialog.askAccessResourceMessage')}
           </DialogContentText>
           <TextField autoFocus id="name" label="Username" type="text" fullWidth />
@@ -42,6 +43,24 @@ const FormDialog = () => {
           </Button>
         </DialogActions>
       </Dialog>
+      {!open && (
+        <div className="form-container">
+          <style>
+            {`
+                [class*=menuPanel] {
+                    position: absolute;
+                    top: 50%;
+                    right: 50%;
+                    left: auto;
+                    transform: translate(50%,-50%);
+                    margin: 0;
+                    pointer-events: auto;
+                }
+              `}
+          </style>
+          <ProfileMenu />
+        </div>
+      )}
     </div>
   )
 }
