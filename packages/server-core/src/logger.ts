596f8460bb6ac4859583444510f3cec0f7af7af8
Ashish <ashishguptajiit@gmail.com>
2022-05-04 15:15:10 +0530

    updated tranport

--- packages/server-core/src/logger.ts
+++ packages/server-core/src/logger.ts
@@ -2,28 +2,52 @@ import pino from 'pino'
 
 let node = process.env.ELASTIC_HOST || "http://localhost:9200";
 
-const logger = pino({
-  transport: {
-    targets: [
-      {
-        level: 'debug',
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: true
-        }
-      },
-      {
-        level: 'debug',
-        target: 'pino-elasticsearch',
-        options: {
-          index: 'xr-engine',
-          consistency: 'one',
-          node: node
-        }
+const transport = pino.transport({
+  targets: [
+    {
+      level: 'debug',
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: true
       }
-    ]
-  }
+    },
+    {
+      level: 'debug',
+      target: 'pino-elasticsearch',
+      options: {
+        index: 'xr-engine',
+        consistency: 'one',
+        node: node
+      }
+    }
+  ]
 })
+
+const logger = pino(transport)
+
+// const logger = pino({
+//   transport: {
+//     targets: [
+//       {
+//         level: 'debug',
+//         target: 'pino-pretty',
+//         options: {
+//           colorize: true,
+//           translateTime: true
+//         }
+//       },
+//       {
+//         level: 'debug',
+//         target: 'pino-elasticsearch',
+//         options: {
+//           index: 'xr-engine',
+//           consistency: 'one',
+//           node: node
+//         }
+//       }
+//     ]
+//   }
+// })
 
 export default logger
