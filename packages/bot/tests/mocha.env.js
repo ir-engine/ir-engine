a9d6d7be9da3600cc56fa07b9ae00c3d92d66fd5
HexaField <joshfield999@gmail.com>
2022-05-06 11:52:58 +1000

    remove bot

--- packages/bot/tests/mocha.env.js
+++
@@ -1,4 +0,0 @@
-// process.env.APP_ENV = 'test'
-process.env.TS_NODE_FILES = true
-process.env.TS_NODE_PROJECT = 'tsconfig.json'
-process.env.TS_NODE_COMPILER_OPTIONS = '{\"module\": \"commonjs\" }'
