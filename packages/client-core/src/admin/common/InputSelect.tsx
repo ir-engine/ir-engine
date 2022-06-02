b7ae99553ecce6f16d2b91bc0d7e702a56bb6865
Hanzla Mateen <hanzlamateen@live.com>
2022-06-02 10:59:12 +0500

    Standardized input control for admin panel (#6298)
    * Removed unused imports
    
    * Added tooltip for admin panel sidebar
    
    * Fixed size of modal for large screens
    
    * Standardized inputtext and inputselect
    
    * Fixed autocomplete label font and spacings

--- packages/client-core/src/admin/common/InputSelect.tsx
+++ packages/client-core/src/admin/common/InputSelect.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 
+import Box from '@mui/material/Box'
 import FormControl from '@mui/material/FormControl'
 import MenuItem from '@mui/material/MenuItem'
 import Paper from '@mui/material/Paper'
@@ -9,55 +11,68 @@ import Select from '@mui/material/Select'
 import styles from '../styles/admin.module.scss'
 
 interface Props {
-  formErrors: any
   value: string
   handleInputChange: (e: any) => void
   name: string
-  menu: any
+  menu: InputSelectProps[]
+  error?: string
+  label?: string
+  endControl?: ReactNode
 }
 
-const InputSelect = ({ formErrors, value, handleInputChange, name, menu }: Props) => {
+export interface InputSelectProps {
+  value: string
+  label: string
+}
+
+const InputSelect = ({ error, value, handleInputChange, name, menu, label, endControl }: Props) => {
   const { t } = useTranslation()
 
   return (
-    <Paper component="div" className={formErrors.length > 0 ? styles.redBorder : styles.createInput}>
-      <FormControl fullWidth disabled={menu.length > 0 ? false : true}>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          value={value}
-          fullWidth
-          onChange={handleInputChange}
-          name={name}
-          displayEmpty
-          className={styles.select}
-          MenuProps={{ classes: { paper: styles.selectPaper } }}
-        >
-          <MenuItem
-            value=""
-            disabled
-            classes={{
-              root: styles.menuItem
-            }}
-          >
-            <em>
-              {t('admin:components.common.select')} {name}
-            </em>
-          </MenuItem>
-          {menu.map((el, index) => (
-            <MenuItem
-              value={el.value}
-              key={index}
-              classes={{
-                root: styles.menuItem
-              }}
+    <React.Fragment>
+      {label && <label>{_.upperFirst(label)}</label>}
+      <Box sx={{ display: 'flex' }}>
+        <Paper component="div" className={error ? styles.redBorder : styles.createInput} sx={{ flexGrow: 1 }}>
+          <FormControl fullWidth disabled={menu.length > 0 ? false : true}>
+            <Select
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+              value={value}
+              fullWidth
+              onChange={handleInputChange}
+              name={name}
+              displayEmpty
+              className={styles.select}
+              MenuProps={{ classes: { paper: styles.selectPaper } }}
             >
-              {el.label}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Paper>
+              <MenuItem
+                value=""
+                disabled
+                classes={{
+                  root: styles.menuItem
+                }}
+              >
+                <em>
+                  {t('admin:components.common.select')} {label}
+                </em>
+              </MenuItem>
+              {menu.map((el, index) => (
+                <MenuItem
+                  value={el.value}
+                  key={index}
+                  classes={{
+                    root: styles.menuItem
+                  }}
+                >
+                  {el.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Paper>
+        {endControl}
+      </Box>
+    </React.Fragment>
   )
 }
 
