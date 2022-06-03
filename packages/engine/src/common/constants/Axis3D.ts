27b101bcd16407a4dc8717432aa0da8a4220a91b
Mohsen Heydari <mohsenheydari@live.com>
2022-06-03 20:22:12 +0000

    Updates Axis3D implementation

--- packages/engine/src/common/constants/Axis3D.ts
+++ packages/engine/src/common/constants/Axis3D.ts
@@ -1,44 +1,16 @@
 // Common readonly 3D Axis definitions
+import { Vector3 } from 'three'
 
 /** X Axis (1,0,0) */
-export const X = {
-  get x() {
-    return 1
-  },
-  get y() {
-    return 0
-  },
-  get z() {
-    return 0
-  }
-} as any
-
+export const X = Object.freeze(new Vector3(1, 0, 0))
 /** Y Axis (0,1,0) */
-export const Y = {
-  get x() {
-    return 0
-  },
-  get y() {
-    return 1
-  },
-  get z() {
-    return 0
-  }
-} as any
-
+export const Y = Object.freeze(new Vector3(0, 1, 0))
 /** Z Axis (0,0,1) */
-export const Z = {
-  get x() {
-    return 0
-  },
-  get y() {
-    return 0
-  },
-  get z() {
-    return 1
-  }
-} as any
+export const Z = Object.freeze(new Vector3(0, 0, 1))
 
 export const Right = X
+export const Left = Object.freeze(new Vector3().copy(Right).multiplyScalar(-1))
 export const Up = Y
+export const Down = Object.freeze(new Vector3().copy(Up).multiplyScalar(-1))
 export const Forward = Z
+export const Backward = Object.freeze(new Vector3().copy(Forward).multiplyScalar(-1))
