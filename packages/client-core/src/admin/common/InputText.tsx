aca414b7cd3b6584d2522502f6eb2b91766e43e7
Hanzla Mateen <hanzlamateen@live.com>
2022-06-07 09:38:37 +0500

    Updated input field to wrap mui textfield

--- packages/client-core/src/admin/common/InputText.tsx
+++ packages/client-core/src/admin/common/InputText.tsx
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 import React from 'react'
 
-import InputBase from '@mui/material/InputBase'
-import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
 
 import styles from '../styles/admin.module.scss'
 
@@ -14,25 +14,42 @@ interface Props {
   error?: string
   type?: string
   disabled?: boolean
+  endAdornment?: React.ReactNode
   handleInputChange?: (e: any) => void
 }
 
-const InputText = ({ value, handleInputChange, name, label, placeholder, error, type, disabled }: Props) => {
+const InputText = ({
+  value,
+  name,
+  label,
+  placeholder,
+  error,
+  type,
+  disabled,
+  endAdornment,
+  handleInputChange
+}: Props) => {
   return (
-    <React.Fragment>
-      {label && <label>{_.upperFirst(label)}</label>}
-      <Paper component="div" className={error ? styles.redBorder : styles.createInput}>
-        <InputBase
-          name={name}
-          type={type}
-          className={styles.input}
-          placeholder={placeholder ? placeholder : `Enter ${label}`}
-          value={value}
-          disabled={disabled}
-          onChange={handleInputChange}
-        />
-      </Paper>
-    </React.Fragment>
+    <Box sx={{ mb: 2 }}>
+      <TextField
+        className={styles.textField}
+        variant="outlined"
+        name={name}
+        type={type}
+        placeholder={placeholder ? placeholder : `Enter ${label}`}
+        label={_.upperFirst(label)}
+        value={value}
+        error={error ? true : false}
+        disabled={disabled}
+        size={'small'}
+        onChange={handleInputChange}
+        InputProps={{
+          className: styles.input,
+          endAdornment: endAdornment
+        }}
+        fullWidth
+      />
+    </Box>
   )
 }
 
