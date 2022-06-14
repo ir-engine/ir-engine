457b4c76f33812d6d3f35775ea6cc375147bb41a
Kyle Baran <kbaran@bitscoop.com>
2022-06-14 01:29:59 -0700

    Updated OAuth flow to allow a single user to have multiple connections. (#6156)
    * Updated OAuth flow to allow a single user to have multiple connections.
    
    * remove log
    
    Co-authored-by: Josh Field <joshfield999@gmail.com>

--- packages/server-core/src/user/strategies/linkedin.ts
+++ packages/server-core/src/user/strategies/linkedin.ts
@@ -10,13 +10,19 @@ export class LinkedInStrategy extends CustomOAuthStrategy {
     this.app = app
   }
 
-  async getEntityData(profile: any, params: Params): Promise<any> {
+  async getEntityData(profile: any, entity: any, params: Params): Promise<any> {
     const baseData = await super.getEntityData(profile, null, {})
-    const userId = params?.query ? params.query.userId : undefined
+    const authResult = await (this.app.service('authentication') as any).strategies.jwt.authenticate(
+      { accessToken: params?.authentication?.accessToken },
+      {}
+    )
+    const identityProvider = authResult['identity-provider']
+    const userId = identityProvider ? identityProvider.userId : params?.query ? params.query.userId : undefined
+
     return {
       ...baseData,
       email: profile.email,
-      type: 'linkdlin',
+      type: 'linkedin',
       userId
     }
   }
@@ -45,20 +51,28 @@ export class LinkedInStrategy extends CustomOAuthStrategy {
       await this.app.service('user-api-key').create({
         userId: entity.userId
       })
-    if (entity.type !== 'guest') {
+    if (entity.type !== 'guest' && identityProvider.type === 'guest') {
       await this.app.service('identity-provider').remove(identityProvider.id)
       await this.app.service('user').remove(identityProvider.userId)
+      return super.updateEntity(entity, profile, params)
     }
-    return super.updateEntity(entity, profile, params)
+    const existingEntity = await super.findEntity(profile, params)
+    if (!existingEntity) {
+      profile.userId = user.id
+      const newIP = await super.createEntity(profile, params)
+      if (entity.type === 'guest') await this.app.service('identity-provider').remove(entity.id)
+      return newIP
+    } else if (existingEntity.userId === identityProvider.userId) return existingEntity
+    else {
+      throw new Error('Another user is linked to this account')
+    }
   }
 
   async getRedirect(data: any, params: Params): Promise<string> {
     const redirectHost = config.authentication.callback.linkedin
     const type = params?.query?.userId ? 'connection' : 'login'
-
     if (Object.getPrototypeOf(data) === Error.prototype) {
       const err = data.message as string
-
       return redirectHost + `?error=${err}`
     } else {
       const token = data.accessToken as string
@@ -74,10 +88,8 @@ export class LinkedInStrategy extends CustomOAuthStrategy {
       let returned = redirectHost + `?token=${token}&type=${type}`
       if (path != null) returned = returned.concat(`&path=${path}`)
       if (instanceId != null) returned = returned.concat(`&instanceId=${instanceId}`)
-
       return returned
     }
   }
 }
-
 export default LinkedInStrategy
--- packages/server-core/src/user/user/user.hooks.ts
+++ packages/server-core/src/user/user/user.hooks.ts
@@ -29,6 +29,19 @@ const restrictUserPatch = (context: HookContext) => {
   return context
 }
 
+const restrictUserRemove = (context: HookContext) => {
+  if (context.params.isInternal) return context
+
+  // allow admins for all patch actions
+  const loggedInUser = context.params.user as UserDataType
+  if (loggedInUser.userRole === 'admin') return context
+
+  // only allow a user to patch it's own data
+  if (loggedInUser.id !== context.id) throw new Error('Must be an admin to delete another user')
+
+  return context
+}
+
 /**
  * This module used to declare and identify database relation
  * which will be used later in user service
@@ -142,7 +155,7 @@ export default {
       addScopeToUser()
     ],
     remove: [
-      iff(isProvider('external'), restrictUserRole('admin') as any),
+      iff(isProvider('external'), restrictUserRemove as any),
       async (context: HookContext): Promise<HookContext> => {
         try {
           const userId = context.id
