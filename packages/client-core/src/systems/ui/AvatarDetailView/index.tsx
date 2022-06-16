0aee19dd8ddd83264b74131ef2e0df20cfbef0b3
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-06-16 14:31:59 +0500

    refactor the avatar detail view component

--- packages/client-core/src/systems/ui/AvatarDetailView/index.tsx
+++ packages/client-core/src/systems/ui/AvatarDetailView/index.tsx
@@ -7,23 +7,8 @@ import { useEngineState } from '@xrengine/engine/src/ecs/classes/EngineState'
 import { createXRUI } from '@xrengine/engine/src/xrui/functions/createXRUI'
 import { useXRUIState } from '@xrengine/engine/src/xrui/functions/useXRUIState'
 
-import { useUserState } from '../../user/services/UserService'
-
-const styles = {
-  avatarName: {
-    fontSize: '60px',
-    backgroundColor: '#000000dd',
-    color: 'white',
-    fontFamily: "'Roboto', sans-serif",
-    border: '10px solid white',
-    borderRadius: '50px',
-    padding: '20px',
-    margin: '60px',
-    boxShadow: '#fff2 0 0 30px',
-    width: '400px',
-    textAlign: 'center'
-  }
-}
+import { useUserState } from '../../../user/services/UserService'
+import styleString from './index.scss'
 
 export function createAvatarDetailView(id: string) {
   return createXRUI(
@@ -46,12 +31,17 @@ const AvatarDetailView = () => {
   const worldState = useEngineState()
   const usersTyping = useState(worldState.usersTyping[detailState.id.value]).value
 
-  return user ? (
-    <div style={styles.avatarName as {}}>
-      {user.name.value}
-      {usersTyping && <h6 style={{ margin: 0, padding: 0 }}>{t('common:typing')}</h6>}
-    </div>
-  ) : (
-    <div></div>
+  return (
+    <>
+      <style>{styleString}</style>
+      {user ? (
+        <div className="avatarName">
+          {user.name.value}
+          {usersTyping && <h6 className="typingIndicator">{t('common:typing')}</h6>}
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   )
 }
