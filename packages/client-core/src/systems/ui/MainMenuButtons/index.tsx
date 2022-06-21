f63243f6800b61fec057dc1e392311d87eb23831
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-06-21 15:09:08 +0500

    add audio enable disable button

--- packages/client-core/src/systems/ui/MainMenuButtons/index.tsx
+++ packages/client-core/src/systems/ui/MainMenuButtons/index.tsx
@@ -7,15 +7,25 @@ import { respawnAvatar } from '@xrengine/engine/src/avatar/functions/respawnAvat
 import { Engine } from '@xrengine/engine/src/ecs/classes/Engine'
 import { EngineActions, useEngineState } from '@xrengine/engine/src/ecs/classes/EngineState'
 import { useWorld } from '@xrengine/engine/src/ecs/functions/SystemHooks'
+import { MediaStreams } from '@xrengine/engine/src/networking/systems/MediaStreamSystem'
 import { createXRUI } from '@xrengine/engine/src/xrui/functions/createXRUI'
 import { dispatchAction } from '@xrengine/hyperflux'
 
 import { Message as MessageIcon } from '@mui/icons-material'
-import LinkIcon from '@mui/icons-material/Link'
-import RefreshIcon from '@mui/icons-material/Refresh'
-import SettingsIcon from '@mui/icons-material/Settings'
+import { Link as LinkIcon, Mic, MicOff, Refresh as RefreshIcon, Settings as SettingsIcon } from '@mui/icons-material'
 
+import { MediaInstanceConnectionService } from '../../../common/services/MediaInstanceConnectionService'
+import { MediaStreamService, useMediaStreamState } from '../../../media/services/MediaStreamService'
 import { useChatState } from '../../../social/services/ChatService'
+import {
+  configureMediaTransports,
+  createCamAudioProducer,
+  endVideoChat,
+  leaveNetwork,
+  pauseProducer,
+  resumeProducer
+} from '../../../transports/SocketWebRTCClientFunctions'
+import { SocketWebRTCClientNetwork } from '../../../transports/SocketWebRTCClientNetwork'
 import { EmoteIcon } from '../../../user/components/UserMenu'
 import { MainMenuButtonState } from '../../state/MainMenuButtonState'
 import styleString from './index.scss'
@@ -39,6 +49,12 @@ const MainMenuButtons = () => {
   if (activeChannelMatch && activeChannelMatch.length > 0) {
     activeChannel = activeChannelMatch[1]
   }
+  const channelEntries = Object.values(channels).filter((channel) => !!channel) as any
+  const instanceChannel = channelEntries.find(
+    (entry) => entry.instanceId === Engine.instance.currentWorld.worldNetwork?.hostId
+  )
+  const mediastream = useMediaStreamState()
+  const isCamAudioEnabled = mediastream.isCamAudioEnabled
 
   useEffect(() => {
     activeChannel &&
@@ -103,11 +119,45 @@ const MainMenuButtons = () => {
     respawnAvatar(useWorld().localClientEntity)
   }
 
+  const checkEndVideoChat = async () => {
+    const mediaNetwork = Engine.instance.currentWorld.mediaNetwork as SocketWebRTCClientNetwork
+    if (
+      (MediaStreams.instance.audioPaused || MediaStreams.instance.camAudioProducer == null) &&
+      (MediaStreams.instance.videoPaused || MediaStreams.instance.camVideoProducer == null) &&
+      instanceChannel.channelType !== 'instance'
+    ) {
+      await endVideoChat(mediaNetwork, {})
+      if (mediaNetwork.socket?.connected === true) {
+        await leaveNetwork(mediaNetwork, false)
+        await MediaInstanceConnectionService.provisionServer(instanceChannel.id)
+      }
+    }
+  }
+
+  const handleMicClick = async () => {
+    const mediaNetwork = Engine.instance.currentWorld.mediaNetwork as SocketWebRTCClientNetwork
+    if (await configureMediaTransports(mediaNetwork, ['audio'])) {
+      if (MediaStreams.instance.camAudioProducer == null) await createCamAudioProducer(mediaNetwork)
+      else {
+        const audioPaused = MediaStreams.instance.toggleAudioPaused()
+        if (audioPaused) await pauseProducer(mediaNetwork, MediaStreams.instance.camAudioProducer)
+        else await resumeProducer(mediaNetwork, MediaStreams.instance.camAudioProducer)
+        checkEndVideoChat()
+      }
+      MediaStreamService.updateCamAudioState()
+    }
+  }
+
+  const MicIcon = isCamAudioEnabled.value ? Mic : MicOff
+
   return (
     <div className="container" xr-layer="true">
       <style>{styleString}</style>
       <div className="button" onClick={handleRespawnAvatar}>
         <RefreshIcon className="svgIcon" />
+      </div>
+      <div className="button" onClick={handleMicClick}>
+        <MicIcon />
       </div>
       <div className="button" onClick={toggleEmoteMenu}>
         <EmoteIcon />
