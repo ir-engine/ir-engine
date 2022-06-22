cd24338ff8c95cffceb4af2fd1571b670d7ce273
HexaField <joshfield999@gmail.com>
2022-06-23 09:51:52 +1000

    fix editor instance connection bug

--- packages/editor/src/components/realtime/WorldInstanceConnection.tsx
+++ packages/editor/src/components/realtime/WorldInstanceConnection.tsx
@@ -57,7 +57,11 @@ export const WorldInstanceConnection = () => {
   useEditorNetworkInstanceProvisioning()
 
   const onSelectInstance = (selectedInstance: string) => {
-    if (selectedInstance === 'None' || selectedInstance !== Engine.instance.currentWorld.worldNetwork?.hostId) {
+    if (
+      selectedInstance === 'None' ||
+      (Engine.instance.currentWorld.worldNetwork?.hostId &&
+        selectedInstance !== Engine.instance.currentWorld.worldNetwork?.hostId)
+    ) {
       if (worldNetworkHostId) {
         leaveNetwork(Engine.instance.currentWorld.worldNetwork as SocketWebRTCClientNetwork)
       }
