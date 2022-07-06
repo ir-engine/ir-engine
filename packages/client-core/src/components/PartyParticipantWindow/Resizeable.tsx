fd4e7f327829ca6522288422e2c8f24378e7df37
rondoor <rondoor124@gmail.com>
2022-07-06 18:08:13 -0500

    fix issues

--- packages/client-core/src/components/PartyParticipantWindow/Resizeable.tsx
+++ packages/client-core/src/components/PartyParticipantWindow/Resizeable.tsx
@@ -11,19 +11,22 @@ type PropsType = {
 }
 export const Resizeable = ({ isPiP, isScreenMe, children }: PropsType): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false)
+  const [isFixedWidth, setIsFixedWidth] = useState(false)
   let resizableComponent
 
   useEffect(() => {
     console.error(isOpen, isPiP)
     if (isOpen != isPiP && resizableComponent) {
-      resizableComponent.updateSize({ width: 'auto', height: '100%' })
+      resizableComponent.updateSize({ width: 'auto', height: 'auto' })
+      setIsFixedWidth(isPiP)
     }
     setIsOpen(isPiP)
   })
   return (
     <div
       className={classNames({
-        [styles['resizeable-screen']]: isPiP && isScreenMe
+        [styles['resizeable-screen']]: isPiP && isScreenMe,
+        [styles['resizeable-screen-fixed']]: isFixedWidth
       })}
     >
       <Resizable
@@ -51,6 +54,12 @@ export const Resizeable = ({ isPiP, isScreenMe, children }: PropsType): JSX.Elem
             left: 35,
             right: 35
           }
+        }}
+        onResizeStart={() => {
+          if (isFixedWidth) {
+            resizableComponent.updateSize({ width: 500, height: 'auto' })
+          }
+          setIsFixedWidth(false)
         }}
       >
         {children}
