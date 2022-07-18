8eefd70d2d62cb2dad4a4ed7c497318032375cc1
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-07-19 00:48:44 +0500

    add spectator checkbox and QR code in XRUI component of share menu

--- packages/client-core/src/systems/ui/ShareLocationDetailView/index.tsx
+++ packages/client-core/src/systems/ui/ShareLocationDetailView/index.tsx
@@ -1,13 +1,16 @@
 import { createState } from '@speigg/hookstate'
+import { QRCodeSVG } from 'qrcode.react'
 import React, { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { isShareAvailable } from '@xrengine/engine/src/common/functions/DetectFeatures'
+import { useEngineState } from '@xrengine/engine/src/ecs/classes/EngineState'
 import { createXRUI } from '@xrengine/engine/src/xrui/functions/createXRUI'
 
 import { FileCopy, Send } from '@mui/icons-material'
 
 import { useShareMenuHooks } from '../../../user/components/UserMenu/menus/ShareMenu'
+import XRCheckboxButton from '../../components/XRCheckboxButton'
 import XRInput from '../../components/XRInput'
 import XRTextButton from '../../components/XRTextButton'
 import styleString from './index.scss'
@@ -22,24 +25,40 @@ function createShareLocationDetailState() {
 
 const ShareLocationDetailView = () => {
   const { t } = useTranslation()
+  const engineState = useEngineState()
   const refLink = useRef() as React.MutableRefObject<HTMLInputElement>
 
-  const { copyLinkToClipboard, shareOnApps, packageInvite, handleChangeToken, shareLink, token } = useShareMenuHooks({
-    refLink
-  })
+  const { copyLinkToClipboard, shareOnApps, packageInvite, handleChangeToken, shareLink, token, toggleSpectatorMode } =
+    useShareMenuHooks({
+      refLink
+    })
 
   return (
     <>
       <style>{styleString}</style>
       <div className="container" xr-layer="true">
         <div className="header">
-          <h1 className="headerTitle">{t('user:usermenu.share.title')}</h1>
+          {engineState.shareTitle.value ? (
+            <h1 className="headerTitle">{engineState.shareTitle.value}</h1>
+          ) : (
+            <>
+              <h1 className="headerTitle">{t('user:usermenu.share.title')}</h1>
+              <XRCheckboxButton
+                onChange={toggleSpectatorMode}
+                labelContent={t('user:usermenu.share.lbl-spectator-mode')}
+              />
+            </>
+          )}
+          <div className="qrContainer">
+            <QRCodeSVG height={176} width={200} value={shareLink} />
+          </div>
           <XRInput
             ref={refLink}
             aria-invalid="false"
             disabled={true}
             type="text"
             value={shareLink}
+            endIcon={<FileCopy />}
             endIconClick={copyLinkToClipboard}
           />
           <XRInput
