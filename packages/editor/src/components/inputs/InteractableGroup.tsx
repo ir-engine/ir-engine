0017d56a4dea086006ffbf1e177e9ec6edac907a
HexaField <joshfield999@gmail.com>
2022-07-19 09:40:12 +1000

    remove interactables group editor

--- packages/editor/src/components/inputs/InteractableGroup.tsx
+++ packages/editor/src/components/inputs/InteractableGroup.tsx
@@ -1,40 +1,9 @@
 import React, { Fragment } from 'react'
-import { useTranslation } from 'react-i18next'
 
-import { getComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
-import { InteractableComponent } from '@xrengine/engine/src/interaction/components/InteractableComponent'
-
-import { EditorComponentType, updateProperty } from '../properties/Util'
-import InputGroup from './InputGroup'
-import NumericInputGroup from './NumericInputGroup'
-import StringInput from './StringInput'
+import { EditorComponentType } from '../properties/Util'
 
 export const InteractableGroup: EditorComponentType = (props) => {
-  const { t } = useTranslation()
-
-  const interactableComponent = getComponent(props.node.entity, InteractableComponent)?.value
-  if (!interactableComponent) return null!
-
-  return (
-    <Fragment>
-      <InputGroup name="Interaction Text" label={t('editor:properties.interaction.text')}>
-        <StringInput
-          value={interactableComponent.interactionText}
-          onChange={updateProperty(InteractableComponent, 'interactionText')}
-        />
-      </InputGroup>
-      <NumericInputGroup
-        name="Interaction Distance"
-        label={t('editor:properties.interaction.distance')}
-        onChange={updateProperty(InteractableComponent, 'interactionDistance')}
-        min={0}
-        smallStep={0.001}
-        mediumStep={0.01}
-        largeStep={0.1}
-        value={interactableComponent.interactionDistance}
-      />
-    </Fragment>
-  )
+  return <Fragment />
 }
 
 export default InteractableGroup
