016f2a98000d402d6e6ca71661d9d3a56c4d1886
HexaField <joshfield999@gmail.com>
2022-07-20 11:44:25 +1000

    fix debug in editor

--- packages/engine/src/debug/systems/DebugRenderer.ts
+++ packages/engine/src/debug/systems/DebugRenderer.ts
@@ -1,24 +1,26 @@
-import { ArrayCamera, BufferAttribute, BufferGeometry, LineBasicMaterial, LineSegments } from 'three'
+import { BufferAttribute, BufferGeometry, LineBasicMaterial, LineSegments } from 'three'
 
 import { Engine } from '../../ecs/classes/Engine'
 import { World } from '../../ecs/classes/World'
 import { EngineRenderer } from '../../renderer/WebGLRendererSystem'
 import { ObjectLayers } from '../../scene/constants/ObjectLayers'
 
-export const DebugRenderer = () => {
+export const DebugRenderer = (world: World) => {
   let enabled = false
 
   const lineMaterial = new LineBasicMaterial({ vertexColors: true })
   const _lineSegments = new LineSegments(new BufferGeometry(), lineMaterial)
-  Engine.instance.currentWorld.scene.add(_lineSegments)
   _lineSegments.frustumCulled = false
 
   return (world: World, _enabled: boolean) => {
     if (enabled !== _enabled) {
       enabled = _enabled
+
+      if (enabled) Engine.instance.currentWorld.scene.add(_lineSegments)
+      else Engine.instance.currentWorld.scene.remove(_lineSegments)
       _lineSegments.visible = enabled
-      // @ts-ignore
-      const xrCameras = EngineRenderer.instance.xrManager?.getCamera() as ArrayCamera
+
+      const xrCameras = EngineRenderer.instance.xrManager?.getCamera()
       if (enabled) {
         Engine.instance.currentWorld.camera.layers.enable(ObjectLayers.PhysicsHelper)
         if (xrCameras) xrCameras.cameras.forEach((camera) => camera.layers.enable(ObjectLayers.PhysicsHelper))
@@ -28,10 +30,10 @@ export const DebugRenderer = () => {
       }
     }
 
-    const debugRenderBuffer = world.physicsWorld.debugRender()
-    // console.log(debugRenderBuffer.vertices, debugRenderBuffer.colors)
-
-    _lineSegments.geometry.setAttribute('position', new BufferAttribute(debugRenderBuffer.vertices, 3))
-    _lineSegments.geometry.setAttribute('color', new BufferAttribute(debugRenderBuffer.colors, 4))
+    if (enabled) {
+      const debugRenderBuffer = world.physicsWorld.debugRender()
+      _lineSegments.geometry.setAttribute('position', new BufferAttribute(debugRenderBuffer.vertices, 3))
+      _lineSegments.geometry.setAttribute('color', new BufferAttribute(debugRenderBuffer.colors, 4))
+    }
   }
 }
