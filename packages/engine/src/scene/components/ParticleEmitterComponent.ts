a614d1be3c2485b0e17f8fd3a07abc70f416c1aa
dinomut1 <heximhotep@gmail.com>
2022-08-03 15:08:11 -0700

    implemented fromJSON. Added default emitter json

--- packages/engine/src/scene/components/ParticleEmitterComponent.ts
+++ packages/engine/src/scene/components/ParticleEmitterComponent.ts
@@ -1,37 +1,11 @@
 import { Color, Vector3 } from 'three'
 
+import { SceneJson } from '@xrengine/common/src/interfaces/SceneInterface'
+
 import { createMappedComponent } from '../../ecs/functions/ComponentFunctions'
 
 export type ParticleEmitterComponentType = {
-  initialPositions: number[]
-  initialAges: number[]
-  startSize: number
-  endSize: number
-  sizeRandomness: number
-  startVelocity: Vector3
-  endVelocity: Vector3
-  angularVelocity: number
-  particleCount: number
-  lifetime: number
-  lifetimes: number[]
-  lifetimeRandomness: number
-  particleSizeRandomness: number[]
-  ageRandomness: number
-  ages: number[]
-  colors: number[]
-  endColor: Color
-  middleColor: Color
-  startColor: Color
-  startOpacity: number
-  middleOpacity: number
-  endOpacity: number
-  colorCurve: string
-  velocityCurve: string
-  sizeCurve: string
-  worldScale: Vector3
-  inverseWorldScale: Vector3
-  count: number
-  src: string
+  src: any
 }
 
 export const ParticleEmitterComponent = createMappedComponent<ParticleEmitterComponentType>('ParticleEmitterComponent')
