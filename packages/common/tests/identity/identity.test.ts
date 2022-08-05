6a7d0568d8348df2a24dff0db4aec7388c332dea
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-08-04 21:07:45 -0400

    Remove chai

--- packages/common/tests/identity/identity.test.ts
+++ packages/common/tests/identity/identity.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import assert from 'assert'
 
 import { generateDid } from '../../src/identity'
 
@@ -7,16 +7,16 @@ const CREDENTIAL_SIGNING_SECRET_KEY_SEED = 'z1AZK4h5w5YZkKYEgqtcFfvSbWQ3tZ3ZFgmL
 describe('identity', () => {
   it('generates a did', async () => {
     const { didDocument, keyPairs, methodFor } = await generateDid(CREDENTIAL_SIGNING_SECRET_KEY_SEED)
-    expect(didDocument.id).to.equal('did:key:z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR')
+    assert.equal(didDocument.id, 'did:key:z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR')
 
     const keyId = didDocument.assertionMethod![0]
     const keyPair = keyPairs.get(keyId) as any
-    expect(keyPair).to.have.property('type', 'Ed25519VerificationKey2020')
-    expect(keyPair).to.have.property('controller', 'did:key:z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR')
+    assert.equal(keyPair.type, 'Ed25519VerificationKey2020')
+    assert.equal(keyPair.controller, 'did:key:z6Mkfeco2NSEPeFV3DkjNSabaCza1EoS3CmqLb1eJ5BriiaR')
 
     const signingKey = methodFor({ purpose: 'assertionMethod' })
-    expect(signingKey).to.have.property(
-      'privateKeyMultibase',
+    assert.equal(
+      signingKey.privateKeyMultibase,
       'zrv1WyGTYHqjHHHD8FuYbnMBsReXBXTbrqZrpfTHNFpeCKS1MDcGUodNfBmihrCiSwY7fxPnsGjCoVZ3e9pGLYHWREM'
     )
   })
