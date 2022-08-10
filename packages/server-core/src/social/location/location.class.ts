f8cc4a89c3df6659dd77fc3cf8a1b8cf8aff7662
Hanzla Mateen <hanzlamateen@live.com>
2022-08-10 07:29:11 +0500

    Fixed typos

--- packages/server-core/src/social/location/location.class.ts
+++ packages/server-core/src/social/location/location.class.ts
@@ -377,14 +377,14 @@ export class Location<T = LocationDataType> extends Service<T> {
       if (location.locationSettingsId != null)
         await this.app.service('location-settings').remove(location.locationSettingsId)
       try {
-        const routeItems = await (this.app.service('location-admin') as any).Model.findAll({
+        const locationAdminItems = await (this.app.service('location-admin') as any).Model.findAll({
           where: {
             locationId: id,
             userId: selfUser.id ?? null
           }
         })
-        routeItems.length &&
-          routeItems.forEach(async (route) => {
+        locationAdminItems.length &&
+          locationAdminItems.forEach(async (route) => {
             await this.app.service('location-admin').remove(route.dataValues.id)
           })
       } catch (err) {
