fa3f6962f92e286fbe01d6e23b0de9b540696891
Patrick <pscale01@gmail.com>
2022-08-11 18:36:29 -0700

    test: NavMeshSystem handles object3D add

--- packages/engine/src/navigation/systems/NavigationSystem.test.ts
+++ packages/engine/src/navigation/systems/NavigationSystem.test.ts
@@ -1,45 +1,59 @@
 import assert from 'assert'
 import proxyquire from 'proxyquire'
 import sinon from 'sinon'
-
-import { applyIncomingActions, dispatchAction } from '@xrengine/hyperflux'
+import { Object3D } from 'three'
 
 import { Engine } from '../../ecs/classes/Engine'
-import { EngineActions } from '../../ecs/classes/EngineState'
+import { Entity } from '../../ecs/classes/Entity'
 import { addComponent } from '../../ecs/functions/ComponentFunctions'
 import { createEntity } from '../../ecs/functions/EntityFunctions'
-import { SystemUpdateType } from '../../ecs/functions/SystemUpdateType'
 import { createEngine } from '../../initializeEngine'
 import { NavMesh } from '../../scene/classes/NavMesh'
 import { NavMeshComponent } from '../../scene/components/NavMeshComponent'
+import { Object3DComponent } from '../../scene/components/Object3DComponent'
 
 describe('NavigationSystem', async () => {
   let updateNavMesh = sinon.spy()
+  let sceneObjectUpdateAction: { entities: Entity[] }
+  const createActionQueue = () => () => [sceneObjectUpdateAction]
+
   let navigationSystem: () => void
   const { default: NavigationSystem } = proxyquire('./NavigationSystem', {
-    '../../scene/functions/loaders/NavMeshFunctions': { updateNavMesh }
+    '../../scene/functions/loaders/NavMeshFunctions': { updateNavMesh },
+    '@xrengine/hyperflux': { createActionQueue }
   })
 
   beforeEach(async () => {
     createEngine()
     navigationSystem = await NavigationSystem(Engine.instance.currentWorld)
+    updateNavMesh.resetHistory()
   })
 
   it('updates navMeshes on modifyProperty command', () => {
     const world = Engine.instance.currentWorld
     const entity = createEntity(world)
-    const action = EngineActions.sceneObjectUpdate({ entities: [entity] })
+    sceneObjectUpdateAction = { entities: [entity] }
 
     addComponent(entity, NavMeshComponent, { value: new NavMesh() })
-
-    world.pipelines[SystemUpdateType.PRE_RENDER] = NavigationSystem
-
-    action.$time = 0 // TODO: what's the right way to test handling actions?
-    dispatchAction(action)
-    applyIncomingActions(Engine.instance.store)
 
     navigationSystem()
 
     assert(updateNavMesh.calledWith(entity))
+  })
+
+  it('updates navMeshes when an Object3D is added', () => {
+    const world = Engine.instance.currentWorld
+    const entity = createEntity(world)
+
+    addComponent(entity, NavMeshComponent, { value: new NavMesh() })
+    addComponent(entity, Object3DComponent, { value: new Object3D() })
+
+    navigationSystem()
+
+    assert(updateNavMesh.calledWith(entity))
+
+    navigationSystem()
+
+    assert(updateNavMesh.calledOnce)
   })
 })
--- packages/engine/src/navigation/systems/NavigationSystem.ts
+++ packages/engine/src/navigation/systems/NavigationSystem.ts
@@ -4,15 +4,18 @@ import { EngineActions } from '../../ecs/classes/EngineState'
 import { World } from '../../ecs/classes/World'
 import { defineQuery } from '../../ecs/functions/ComponentFunctions'
 import { NavMeshComponent } from '../../scene/components/NavMeshComponent'
+import { Object3DComponent } from '../../scene/components/Object3DComponent'
 import { updateNavMesh } from '../../scene/functions/loaders/NavMeshFunctions'
 
 export default async function NavigationSystem(_: World) {
-  const navMeshQuery = defineQuery([NavMeshComponent])
+  const queryNavMesh = defineQuery([NavMeshComponent])
+  const queryNavMeshAndObject3D = defineQuery([NavMeshComponent, Object3DComponent])
 
   const modifyPropertyActionQueue = createActionQueue(EngineActions.sceneObjectUpdate.matches)
 
   return () => {
-    const entsWithNavMesh = navMeshQuery()
+    const entsWithNavMesh = queryNavMesh()
+    const entsWithNavMeshAndObject3DEnter = queryNavMeshAndObject3D.enter()
 
     const modifyPropertyActions = modifyPropertyActionQueue()
 
@@ -20,6 +23,10 @@ export default async function NavigationSystem(_: World) {
       for (const entity of action.entities) {
         if (entsWithNavMesh.includes(entity)) updateNavMesh(entity)
       }
+    }
+
+    for (const entity of entsWithNavMeshAndObject3DEnter) {
+      updateNavMesh(entity)
     }
   }
 }
