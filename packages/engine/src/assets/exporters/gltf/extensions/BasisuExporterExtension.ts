5c18a9d2b4daa80197408c7f488f6c9c5451d57d
dinomut1 <heximhotep@gmail.com>
2022-08-11 17:25:20 -0700

    more attempts at fixing ktx2 encoding

--- packages/engine/src/assets/exporters/gltf/extensions/BasisuExporterExtension.ts
+++ packages/engine/src/assets/exporters/gltf/extensions/BasisuExporterExtension.ts
@@ -13,48 +13,48 @@ export default class BasisuExporterExtension extends ExporterExtension {
     this.imgCache = new Map<any, number>()
   }
 
-  imgCache: Map<ImageData, number>
+  imgCache: Map<any, number>
   sampler: any
 
   writeTexture(texture: CompressedTexture, textureDef) {
     if (!texture.isCompressedTexture) return
     const writer = this.writer
     const img = texture.mipmaps[0]
-    if (this.imgCache.has(img)) {
-      const index = this.imgCache.get(img)!
+    const srcId = texture.source.uuid
+    if (this.imgCache.has(srcId)) {
+      console.log('found cached image')
+      const index = this.imgCache.get(srcId)!
       textureDef.extensions = textureDef.extensions ?? {}
       textureDef.extensions[this.name] = { source: index }
       textureDef.sampler = this.sampler
       return
     }
+    console.log('cache miss for uuid ' + srcId)
     const ktx2write = new KTX2Encoder()
     const imageDef: any = {
       width: img.width,
       height: img.height,
       mimeType: 'image/ktx2'
     }
+    if (!writer.json.images) writer.json.images = []
     const index = writer.json.images.push(imageDef) - 1
-    this.imgCache.set(img, index)
-    const dataBuf = new Uint8Array(img.data)
+    this.imgCache.set(srcId, index)
     writer.pending.push(
-      new Promise((resolve) =>
-        ktx2write
-          .encode({ width: img.width, height: img.height, data: dataBuf })
-          .then((arrayBuf) => {
-            const blob = new Blob([arrayBuf])
-            return writer.processBufferViewImage(blob)
-          })
-          .then((source) => {
-            if (!writer.json.images) writer.json.images = []
-            imageDef.bufferView = source
-
-            writer.extensionsUsed[this.name] = true
-            textureDef.extensions = textureDef.extensions ?? {}
-            textureDef.extensions[this.name] = { source: index }
-            textureDef.sampler = this.sampler
-          })
-          .then(resolve)
-      )
+      new Promise((resolve) => {
+        ktx2write.encode({ width: img.width, height: img.height, data: new Uint8Array(img.data) }).then((arrayBuf) => {
+          const blob = new Blob([arrayBuf])
+          writer
+            .processBufferViewImage(blob)
+            .then((source) => {
+              imageDef.bufferView = source
+              writer.extensionsUsed[this.name] = true
+              textureDef.extensions = textureDef.extensions ?? {}
+              textureDef.extensions[this.name] = { source: index }
+              textureDef.sampler = this.sampler
+            })
+            .then(resolve)
+        })
+      })
     )
   }
 }
