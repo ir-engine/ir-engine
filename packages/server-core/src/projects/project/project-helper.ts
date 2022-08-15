d7101220ba2613c31788019ea21e647b1180dc93
Hanzla Mateen <hanzlamateen@live.com>
2022-08-15 18:40:56 +0300

    Removed temp code

--- packages/server-core/src/projects/project/project-helper.ts
+++ packages/server-core/src/projects/project/project-helper.ts
@@ -17,14 +17,6 @@ export const retriggerBuilderService = async (app: Application) => {
     logger.error(e, `[Project Rebuild]: Failed to invalidate cache with error: ${e.message}`)
   }
 
-  // TEMP START
-  const kc = new k8s.KubeConfig()
-  kc.loadFromDefault()
-
-  app.k8AppsClient = kc.makeApiClient(k8s.AppsV1Api)
-  config.server.releaseName = 'dev'
-  // TEMP END
-
   // trigger k8s to re-run the builder service
   if (app.k8AppsClient) {
     try {
@@ -63,13 +55,6 @@ export const retriggerBuilderService = async (app: Application) => {
 }
 
 export const checkBuilderService = async (app: Application): Promise<boolean> => {
-  // TEMP START
-  const kc = new k8s.KubeConfig()
-  kc.loadFromDefault()
-
-  app.k8DefaultClient = kc.makeApiClient(k8s.CoreV1Api)
-  config.server.releaseName = 'dev'
-  // TEMP END
   let isRebuilding = true
 
   // check k8s to find the status of builder service
