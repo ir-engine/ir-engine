6859786204f5434ddffea4dbc213134bd210b61f
Patrick Canfield <pscale01@gmail.com>
2022-08-17 13:52:28 -0700

    refactor: add back for now

--- packages/engine/src/scene/components/NavMeshComponent.ts
+++ packages/engine/src/scene/components/NavMeshComponent.ts
@@ -3,6 +3,9 @@ import { NavMesh } from '../classes/NavMesh'
 
 export type NavMeshComponentType = {
   value: NavMesh
+  // TODO get rid of debugMode property
+  /** @deprecated don't use */
+  debugMode: Boolean
 }
 
 export const NavMeshComponent = createMappedComponent<NavMeshComponentType>('NavMeshComponent')
