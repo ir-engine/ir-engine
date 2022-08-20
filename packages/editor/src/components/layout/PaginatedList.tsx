4548d6f92d4b3991219679fd2818b8275f23f29f
dinomut1 <heximhotep@gmail.com>
2022-08-20 12:27:56 -0700

    improve paginated list

--- packages/editor/src/components/layout/PaginatedList.tsx
+++ packages/editor/src/components/layout/PaginatedList.tsx
@@ -3,6 +3,8 @@ import React, { JSXElementConstructor } from 'react'
 import { any } from '@xrengine/engine/src/common/functions/MatchesUtils'
 import { useHookEffect, useHookstate } from '@xrengine/hyperflux'
 
+import { Grid, Stack } from '@mui/material'
+
 import { Button } from '../inputs/Button'
 
 export default function PaginatedList({
@@ -18,7 +20,7 @@ export default function PaginatedList({
     ['countPerPage']?: number
   }
 }) {
-  const countPerPage = options?.countPerPage ?? 5
+  const countPerPage = options?.countPerPage ?? 7
   const currentPage = useHookstate(0)
 
   function getPageIndices() {
@@ -26,27 +28,55 @@ export default function PaginatedList({
     return [start, Math.min(start + countPerPage, list.length - 1)]
   }
 
-  function getPageView() {
-    return list.slice(...getPageIndices())
-  }
-
-  const pageData = getPageView()
   const pageView = useHookstate(getPageIndices())
   useHookEffect(() => {
     pageView.set(getPageIndices())
   }, [currentPage])
   return (
     <>
-      <span>
-        <Button onClick={() => currentPage.set(Math.min(list.length - 1, Math.max(0, currentPage.value - 1)))}>
-          -
-        </Button>
-        <Button onClick={() => currentPage.set(Math.min(list.length - 1, Math.max(0, currentPage.value + 1)))}>
-          +
-        </Button>
-      </span>
-      {pageData.slice(...pageView.value).map((item, index) => {
-        return element({ value: item, onChange: onChange(index) })
+      <Grid container>
+        <Grid item xs={1}>
+          <Button
+            onClick={() => currentPage.set(Math.min(list.length / countPerPage, Math.max(0, currentPage.value - 1)))}
+            style={{ width: 'auto' }}
+          >
+            -
+          </Button>
+        </Grid>
+        {[-2, -1, 0, 1, 2].map((idx) => {
+          if (!(currentPage.value + idx < 0 || currentPage.value + idx >= list.length / countPerPage))
+            return (
+              <Grid item xs={2}>
+                <Button
+                  disabled={idx === 0}
+                  onClick={() => currentPage.set(currentPage.value + idx)}
+                  style={{ width: 'auto' }}
+                >
+                  {currentPage.value + idx}
+                </Button>
+              </Grid>
+            )
+          else
+            return (
+              <Grid item xs={2}>
+                <div style={{ textAlign: 'center' }}>
+                  <p>·</p>
+                </div>
+              </Grid>
+            )
+        })}
+        <Grid item xs={1}>
+          <Button
+            onClick={() => currentPage.set(Math.min(list.length - 1, Math.max(0, currentPage.value + 1)))}
+            style={{ width: 'auto' }}
+          >
+            +
+          </Button>
+        </Grid>
+      </Grid>
+
+      {list.slice(...pageView.value).map((index) => {
+        return element(index)
       })}
     </>
   )
