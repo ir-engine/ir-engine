0c05d59dda66936aad067553df7e3a0d4a8f6fa2
HexaField <joshfield999@gmail.com>
2022-08-29 15:01:55 +1000

    replace spawn point round robin with permissioned spawn points

--- packages/editor/src/components/properties/SpawnPointNodeEditor.tsx
+++ packages/editor/src/components/properties/SpawnPointNodeEditor.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { getComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
+import { SpawnPointComponent } from '@xrengine/engine/src/scene/components/SpawnPointComponent'
+
 import StreetviewIcon from '@mui/icons-material/Streetview'
 
+import ArrayInputGroup from '../inputs/ArrayInputGroup'
 import NodeEditor from './NodeEditor'
-import { EditorComponentType } from './Util'
+import { EditorComponentType, updateProperty } from './Util'
 
 /**
  * SpawnPointNodeEditor component used to provide the editor view to customize Spawn Point properties.
@@ -14,13 +18,22 @@ import { EditorComponentType } from './Util'
 export const SpawnPointNodeEditor: EditorComponentType = (props) => {
   const { t } = useTranslation()
 
+  const spawnComponent = getComponent(props.node.entity, SpawnPointComponent)
   // initializing iconComponent icon name
   return (
     <NodeEditor
       {...props}
       name={t('editor:properties.spawnPoint.name')}
       description={t('editor:properties.spawnPoint.description')}
-    />
+    >
+      <ArrayInputGroup
+        name="Users"
+        prefix="User"
+        values={spawnComponent.permissionedUsers}
+        onChange={updateProperty(SpawnPointComponent, 'permissionedUsers')}
+        label={t('editor:properties.spawnPoint.users')}
+      ></ArrayInputGroup>
+    </NodeEditor>
   )
 }
 
--- packages/engine/src/scene/components/SpawnPointComponent.ts
+++ packages/engine/src/scene/components/SpawnPointComponent.ts
@@ -1,5 +1,14 @@
+import { UserId } from '@xrengine/common/src/interfaces/UserId'
+
 import { createMappedComponent } from '../../ecs/functions/ComponentFunctions'
 
-export const SpawnPointComponent = createMappedComponent<true>('SpawnPointComponent')
+export type SpawnPointComponentType = {
+  permissionedUsers: UserId[]
+}
+
+export const SpawnPointComponent = createMappedComponent<SpawnPointComponentType>('SpawnPointComponent')
 
 export const SCENE_COMPONENT_SPAWN_POINT = 'spawn-point'
+export const SCENE_COMPONENT_SPAWN_POINT_DEFAULT_DATA = {
+  permissionedUsers: []
+}
