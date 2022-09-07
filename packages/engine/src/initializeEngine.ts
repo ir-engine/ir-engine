609c7b762764fb79fad20cfa20f1c00b0afcc91d
dinomut1 <heximhotep@gmail.com>
2022-09-07 12:06:44 -0700

    add LoadVolumeComponent

--- packages/engine/src/initializeEngine.ts
+++ packages/engine/src/initializeEngine.ts
@@ -160,6 +160,11 @@ export const initializeCoreSystems = async (injectedSystems?: SystemModuleType<a
       uuid: 'xre.engine.AssetSystem',
       type: SystemUpdateType.FIXED_LATE,
       systemLoader: () => import('./scene/systems/AssetSystem')
+    },
+    {
+      uuid: 'xre.engine.LoadVolumeSystem',
+      type: SystemUpdateType.FIXED_LATE,
+      systemLoader: () => import('./scene/systems/LoadVolumeSystem')
     }
   )
 
