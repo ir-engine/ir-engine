0a15bbb048ecedcc4e5ebcee5e1ee8c90ac6e74f
Patrick Canfield <pscale01@gmail.com>
2022-09-15 17:48:23 -0700

    improve tests for state machine

--- packages/engine/src/common/functions/EntityStateMachineFunctions.test.ts
+++ packages/engine/src/common/functions/EntityStateMachineFunctions.test.ts
@@ -1,15 +1,24 @@
 import assert from 'assert'
+import { noop } from 'lodash'
 
 import { buildState, step } from './EntityStateMachineFunctions'
 
 describe('EntityStateMachineFunctions', () => {
   let didExecute = 0
   let didEnter = 0
+  let didExit = 0
   let shouldTransition = false
 
-  const StartState = buildState('Start', () => {
-    didExecute++
-  })
+  const StartState = buildState(
+    'Start',
+    () => {
+      didExecute++
+    },
+    noop,
+    () => {
+      didExit++
+    }
+  )
     .addTransition({
       description: "ya goof'd",
       getState: () => GoofState,
@@ -36,14 +45,17 @@ describe('EntityStateMachineFunctions', () => {
   ).completeBuild()
   const GoofState = buildState('Ya Goofed')
 
-  it('executes start then enters and returns the next state', () => {
+  it('executes start, exits it, then enters and returns the next state', () => {
     let nextState = step(StartState, 5 as any)
     assert(nextState === StartState, 'should transition to first state that passes test')
     assert(didExecute === 1, 'should execute')
+    assert(didExit === 0, 'should not yet exit')
+    assert(didEnter === 0, 'should not yet enter')
     shouldTransition = true
     nextState = step(StartState, 5 as any)
     assert.equal(didExecute, 2, 'should execute')
     assert(nextState === EndState, 'should transition to first state that passes test')
-    assert(didEnter === 1, 'should enter')
+    assert.equal(didExit, 1, 'should exit')
+    assert.equal(didEnter, 1, 'should enter')
   })
 })
