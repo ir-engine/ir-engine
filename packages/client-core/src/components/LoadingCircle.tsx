f6d38e97841aeab4ec0443320c5d5210659f4dc8
HexaField <joshfield999@gmail.com>
2022-10-12 19:35:33 +1100

    Add loading messages to the loading circle

--- packages/client-core/src/components/LoadingCircle.tsx
+++ packages/client-core/src/components/LoadingCircle.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 
 import CircularProgress from '@mui/material/CircularProgress'
 
-export function LoadingCircle() {
+type Props = {
+  message?: string
+}
+
+export function LoadingCircle(props: Props) {
   return (
     <div
       style={{
         height: '100vh',
         width: '100%',
         textAlign: 'center',
-        paddingTop: 'calc(50vh - 7px)'
+        paddingTop: 'calc(50vh - 7px)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
       }}
     >
       <CircularProgress />
+      {props.message}
     </div>
   )
 }
