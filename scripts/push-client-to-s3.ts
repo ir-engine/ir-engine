20f17cd86a09368ace9ba1ae54c57cda2a31851c
Kyle Baran <kbaran@bitscoop.com>
2022-10-22 21:29:01 -0700

    Fixed bug in pruning old S3 client files. (#7117)
    Vite sometimes generates the same hash for a built file as the prior run. These files were getting deleted
    at the end of the build process because they were being recorded before the client build as files that
    existed and should be deleted, leading to missing files at the end of the process.
    
    Made get-deletable-client-files.ts store the list of files to be pruned stored in S3. push-client-to-s3.ts
    now reads in the list from S3 and removes files whose names are the same as the just-built client files,
    then pushes that back to S3. delete-old-s3-files.ts gets the list from S3.

--- scripts/push-client-to-s3.ts
+++ scripts/push-client-to-s3.ts
@@ -19,6 +19,8 @@ cli.main(async () => {
         const storageProvider = getStorageProvider()
         const clientPath = path.resolve(appRootPath.path, `packages/client/dist`)
         const files = getFilesRecursive(clientPath)
+        let filesToPruneResponse = await storageProvider.getObject('client/S3FilesToRemove.json')
+        const filesToPush = []
         await Promise.all(
             files
                 .map((file) => {
@@ -39,6 +41,7 @@ cli.main(async () => {
                                 putData.Key = `client${filePathRelative}`
                             }
                             await storageProvider.putObject(putData, { isDirectory: false })
+                            filesToPush.push(`client${filePathRelative}`)
                             resolve()
                         } catch (e) {
                             logger.error(e)
@@ -48,6 +51,15 @@ cli.main(async () => {
                 })
         )
         console.log('Pushed client files to S3')
+        let filesToPrune = JSON.parse(filesToPruneResponse.Body.toString('utf-8'))
+        filesToPrune = filesToPrune.filter(file => filesToPush.indexOf(file) < 0)
+        const putData = {
+            Body: Buffer.from(JSON.stringify(filesToPrune)),
+            ContentType: 'application/json',
+            Key: 'client/S3FilesToRemove.json'
+        }
+        await storageProvider.putObject(putData, { isDirectory: false })
+        console.log('Pushed filtered list of files to remove to S3')
         process.exit(0)
     } catch(err) {
         console.log('Error in pushing client images to S3:');
