4b640d4ed41b1d50c6a4fd10791cb4fa11c98e28
Hanzla Mateen <hanzlamatee@live.com>
2022-10-24 15:47:07 +0500

    Setting 8th wall key in client configs

--- packages/client-core/src/admin/services/Setting/ClientSettingService.ts
+++ packages/client-core/src/admin/services/Setting/ClientSettingService.ts
@@ -1,5 +1,6 @@
 import { Paginated } from '@feathersjs/feathers'
 
+import config from '@xrengine/common/src/config'
 import { ClientSetting, PatchClientSetting } from '@xrengine/common/src/interfaces/ClientSetting'
 import multiLogger from '@xrengine/common/src/logger'
 import { matches, Validator } from '@xrengine/engine/src/common/functions/MatchesUtils'
@@ -23,6 +24,11 @@ export const ClientSettingsServiceReceptor = (action) => {
   const s = getState(AdminClientSettingsState)
   matches(action)
     .when(ClientSettingActions.fetchedClient.matches, (action) => {
+      const [clientSetting] = action.clientSettings.data
+      if (clientSetting.key8thWall) {
+        config.client.key8thWall = clientSetting.key8thWall
+      }
+
       return s.merge({ client: action.clientSettings.data, updateNeeded: false })
     })
     .when(ClientSettingActions.clientSettingPatched.matches, (action) => {
