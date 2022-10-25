a8809e28977be919e60c4e738c586f954e6ff239
HexaField <joshfield999@gmail.com>
2022-10-25 16:03:33 +1100

    default to auto mode to dollhouse

--- packages/engine/src/xr/XRScenePlacementShader.ts
+++ packages/engine/src/xr/XRScenePlacementShader.ts
@@ -1,11 +1,15 @@
+import { Not } from 'bitecs'
 import { Material, Mesh } from 'three'
 
 import { getState } from '@xrengine/hyperflux'
 
+import { Engine } from '../ecs/classes/Engine'
 import { World } from '../ecs/classes/World'
 import { defineQuery, getComponent, removeQuery } from '../ecs/functions/ComponentFunctions'
 import { GroupComponent } from '../scene/components/GroupComponent'
 import { SceneObjectComponent } from '../scene/components/SceneObjectComponent'
+import { SceneTagComponent } from '../scene/components/SceneTagComponent'
+import { VisibleComponent } from '../scene/components/VisibleComponent'
 import { XRState } from './XRState'
 
 type ScenePlacementMaterialType = {
@@ -23,39 +27,39 @@ type ScenePlacementMaterialType = {
  * @returns
  */
 export default async function XRScenePlacementShader(world: World) {
-  const sceneQuery = defineQuery([GroupComponent, SceneObjectComponent])
+  // const sceneQuery = defineQuery([GroupComponent, SceneObjectComponent, Not(SceneTagComponent), VisibleComponent])
   const xrState = getState(XRState)
 
   const execute = () => {
     const useShader =
       xrState.sessionActive.value && xrState.sessionMode.value === 'immersive-ar' && xrState.scenePlacementMode.value
-    for (const entity of sceneQuery()) {
-      for (const obj of getComponent(entity, GroupComponent)) {
-        obj.traverse((obj: Mesh<any, Material & ScenePlacementMaterialType>) => {
-          if (obj.material) {
-            const userData = obj.material.userData
-            if (useShader) {
-              if (!userData.ScenePlacement) {
-                userData.ScenePlacement = {
-                  previouslyTransparent: obj.material.transparent,
-                  previousOpacity: obj.material.opacity
-                }
-              }
-              obj.material.transparent = true
-              obj.material.opacity = 0.4
-            } else if (userData.ScenePlacement) {
-              obj.material.transparent = userData.ScenePlacement.previouslyTransparent
-              obj.material.opacity = userData.ScenePlacement.previousOpacity
-              delete userData.ScenePlacement
+    // for (const entity of sceneQuery()) {
+    //   for (const obj of getComponent(entity, GroupComponent)) {
+    world.scene.traverse((obj: Mesh<any, Material & ScenePlacementMaterialType>) => {
+      if (obj.material) {
+        const userData = obj.material.userData
+        if (useShader) {
+          if (!userData.ScenePlacement) {
+            userData.ScenePlacement = {
+              previouslyTransparent: obj.material.transparent,
+              previousOpacity: obj.material.opacity
             }
           }
-        })
+          obj.material.transparent = true
+          obj.material.opacity = 0.4
+        } else if (userData.ScenePlacement) {
+          obj.material.transparent = userData.ScenePlacement.previouslyTransparent
+          obj.material.opacity = userData.ScenePlacement.previousOpacity
+          delete userData.ScenePlacement
+        }
       }
-    }
+    })
+    //   }
+    // }
   }
 
   const cleanup = async () => {
-    removeQuery(world, sceneQuery)
+    // removeQuery(world, sceneQuery)
   }
 
   return { execute, cleanup }
