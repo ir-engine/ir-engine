85b4ef48b3af88b597d897fb33bc986781f6bd91
HexaField <joshfield999@gmail.com>
2022-11-16 09:24:44 +1100

    fix import

--- packages/editor/src/components/assets/AssetPreviewPanels/ModelPreviewPanel.tsx
+++ packages/editor/src/components/assets/AssetPreviewPanels/ModelPreviewPanel.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 import LoadingView from '@xrengine/client-core/src/admin/common/LoadingView'
-import { useRender3DPanelSystem } from '@xrengine/client-core/src/user/components/Panel3D/createRender3DPanelSystem'
+import { useRender3DPanelSystem } from '@xrengine/client-core/src/user/components/Panel3D/useRender3DPanelSystem'
 import { loadAvatarModelAsset } from '@xrengine/engine/src/avatar/functions/avatarFunctions'
 
 import styles from '../styles.module.scss'
