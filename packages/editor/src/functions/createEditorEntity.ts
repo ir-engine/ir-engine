1139d6f87c68981754eb790a12a77e5a2e3d9096
HexaField <joshfield999@gmail.com>
2022-11-16 14:49:18 +1100

    implement fly mode directly on the ECS and move to the engine

--- packages/editor/src/functions/createEditorEntity.ts
+++ packages/editor/src/functions/createEditorEntity.ts
@@ -1,27 +1,28 @@
-import { MathUtils } from 'three'
-
+import { AvatarInputSchema } from '@xrengine/engine/src/avatar/AvatarInputSchema'
 import { Entity } from '@xrengine/engine/src/ecs/classes/Entity'
-import { addComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
+import { setComponent } from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
 import { createEntity } from '@xrengine/engine/src/ecs/functions/EntityFunctions'
+import { InputComponent } from '@xrengine/engine/src/input/components/InputComponent'
+import { NameComponent } from '@xrengine/engine/src/scene/components/NameComponent'
 import { TransformMode } from '@xrengine/engine/src/scene/constants/transformConstants'
 
 import { EditorControlComponent } from '../classes/EditorControlComponent'
-import { FlyControlComponent } from '../classes/FlyControlComponent'
 import { EditorInputComponent } from '../classes/InputComponent'
 import { SceneState } from './sceneRenderFunctions'
 
 export const createEditorEntity = (): Entity => {
   const entity = createEntity()
-  addComponent(entity, FlyControlComponent, {
-    boostSpeed: 4,
-    moveSpeed: 4,
-    lookSensitivity: 5,
-    maxXRotation: MathUtils.degToRad(80)
+
+  setComponent(entity, NameComponent, 'Editor Control Entity')
+
+  setComponent(entity, InputComponent, {
+    schema: AvatarInputSchema,
+    data: new Map()
   })
 
-  addComponent(entity, EditorControlComponent, {})
+  setComponent(entity, EditorControlComponent, {})
 
-  addComponent(entity, EditorInputComponent, {
+  setComponent(entity, EditorInputComponent, {
     mappings: new Map(),
     activeMapping: null!,
     actionState: null!,
---
+++ packages/editor/src/systems/EditorFlyControlSystem.ts
@@ -0,0 +1,61 @@
+import { MathUtils, Matrix3, Vector3 } from 'three'
+
+import { FlyControlComponent } from '@xrengine/engine/src/avatar/components/FlyControlComponent'
+import { Engine } from '@xrengine/engine/src/ecs/classes/Engine'
+import { World } from '@xrengine/engine/src/ecs/classes/World'
+import {
+  addComponent,
+  defineQuery,
+  getComponent,
+  removeComponent,
+  removeQuery,
+  setComponent
+} from '@xrengine/engine/src/ecs/functions/ComponentFunctions'
+import { dispatchAction } from '@xrengine/hyperflux'
+
+import { EditorCameraComponent } from '../classes/EditorCameraComponent'
+import { ActionSets, EditorActionSet, FlyMapping } from '../controls/input-mappings'
+import { addInputActionMapping, getInput, removeInputActionMapping } from '../functions/parseInputActionMapping'
+import { SceneState } from '../functions/sceneRenderFunctions'
+import { EditorHelperAction } from '../services/EditorHelperState'
+
+export default async function FlyControlSystem(world: World) {
+  const tempVec3 = new Vector3()
+  const normalMatrix = new Matrix3()
+
+  const execute = () => {
+    const camera = Engine.instance.currentWorld.camera
+
+    if (getInput(EditorActionSet.disableFlyMode)) {
+      const cameraComponent = getComponent(Engine.instance.currentWorld.cameraEntity, EditorCameraComponent)
+
+      removeInputActionMapping(ActionSets.FLY)
+      const distance = camera.position.distanceTo(cameraComponent.center)
+      cameraComponent.center.addVectors(
+        camera.position,
+        tempVec3.set(0, 0, -distance).applyMatrix3(normalMatrix.getNormalMatrix(camera.matrix))
+      )
+
+      removeComponent(SceneState.editorEntity, FlyControlComponent)
+
+      dispatchAction(EditorHelperAction.changedFlyMode({ isFlyModeEnabled: false }))
+    }
+
+    if (getInput(EditorActionSet.flying)) {
+      addInputActionMapping(ActionSets.FLY, FlyMapping)
+
+      setComponent(SceneState.editorEntity, FlyControlComponent, {
+        boostSpeed: 4,
+        moveSpeed: 4,
+        lookSensitivity: 5,
+        maxXRotation: MathUtils.degToRad(80)
+      })
+
+      dispatchAction(EditorHelperAction.changedFlyMode({ isFlyModeEnabled: true }))
+    }
+  }
+
+  const cleanup = async () => {}
+
+  return { execute, cleanup }
+}
