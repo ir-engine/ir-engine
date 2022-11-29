175d3e59cd0fb9b1a8911c43580e1478c308011a
HexaField <joshfield999@gmail.com>
2022-11-29 13:44:16 +1100

    fix log depth buffer shaders

--- packages/engine/src/scene/functions/LogarithmicDepthBufferMaterialChunk.ts
+++ packages/engine/src/scene/functions/LogarithmicDepthBufferMaterialChunk.ts
@@ -1,13 +1,5 @@
 export default `
-#ifdef USE_LOGDEPTHBUF
-  #ifdef USE_LOGDEPTHBUF_EXT
-    vFragDepth = 1.0 + gl_Position.w;
-    vIsPerspective = 1.0;
-  #else
-    if ( isPerspectiveMatrix( projectionMatrix ) ) {
-      gl_Position.z = log2( max( 0.0001, gl_Position.w + 1.0 ) ) * logDepthBufFC - 1.0;
-      gl_Position.z *= gl_Position.w;
-    }
-  #endif
-#endif
+bool isPerspectiveMatrix( mat4 m ) {
+	return m[ 2 ][ 3 ] == - 1.0;
+}
 `
