4e073c32fd9acbed106d0a3364589d8bc36aa0d4
HexaField <joshfield999@gmail.com>
2022-12-28 11:47:16 +1100

    rotation stuff

--- packages/engine/src/camera/systems/SplineCameraSystem.ts
+++ packages/engine/src/camera/systems/SplineCameraSystem.ts
@@ -1,9 +1,10 @@
+import * as gl from 'gl-matrix'
 import { Euler, Plane, Quaternion, Vector3 } from 'three'
 
 import { World } from '@xrengine/engine/src/ecs/classes/World'
 
 import { ObjectDirection } from '../../common/constants/Axis3D'
-import { V_000, V_001, V_010 } from '../../common/constants/MathConstants'
+import { V_000, V_001, V_010, V_100 } from '../../common/constants/MathConstants'
 import { defineQuery, getComponent, getComponentState } from '../../ecs/functions/ComponentFunctions'
 import { CameraTrackComponent } from '../../scene/components/CameraTrackComponent'
 import { SplineComponent } from '../../scene/components/SplineComponent'
@@ -13,20 +14,23 @@ import { LocalTransformComponent, TransformComponent } from '../../transform/com
 export default async function SplineCameraSystem(world: World) {
   const cameraTrackQuery = defineQuery([SplineComponent, CameraTrackComponent, VisibleComponent])
 
-  const euler = new Euler()
-  const quat1 = new Quaternion()
-  const quat2 = new Quaternion()
-  const forward1 = new Vector3()
-  const forward2 = new Vector3()
+  const rotneg90x = new Quaternion().setFromAxisAngle(V_100, -Math.PI / 2)
+
+  let dualQuaternionFrom = gl.quat2.create()
+  let dualQuaternionTo = gl.quat2.create()
+  const dualQuaternionOut = gl.quat2.create()
+  const vec3 = gl.vec3.create()
+  const quat = gl.quat.create()
 
   const execute = () => {
+    // return
     const { deltaSeconds } = world
     for (const entity of cameraTrackQuery.enter()) {
       getComponentState(entity, CameraTrackComponent).alpha.set(0)
     }
 
     for (const entity of cameraTrackQuery()) {
-      const camera = getComponent(world.cameraEntity, LocalTransformComponent)
+      const cameraTransform = getComponent(world.cameraEntity, LocalTransformComponent)
 
       const transform = getComponent(entity, TransformComponent)
 
@@ -42,15 +46,27 @@ export default async function SplineCameraSystem(world: World) {
       const currentPosition = spline.splinePositions[currentPointNumber]
       const nextPosition = spline.splinePositions[nextPointNumber]
 
-      const currentRotation = cameraTrack.pointRotations[currentPointNumber]
-      const nextRotation = cameraTrack.pointRotations[nextPointNumber]
+      const currentRotation = cameraTrack.pointRotations[currentPointNumber].clone().multiply(rotneg90x)
+      const nextRotation = cameraTrack.pointRotations[nextPointNumber].clone().multiply(rotneg90x)
 
-      /** @todo replace naive lerp with a velocity based calculation */
+      /** @todo replace naive lerp with a spline division based calculation */
 
-      camera.position
+      // dualQuaternionFrom = gl.quat2.fromRotationTranslationValues(currentRotation.x, currentRotation.y, currentRotation.z, currentRotation.w, currentPosition.x, currentPosition.y, currentPosition.z)
+      // dualQuaternionTo = gl.quat2.fromRotationTranslationValues(nextRotation.x, nextRotation.y, nextRotation.z, nextRotation.w, nextPosition.x, nextPosition.y, nextPosition.z)
+
+      // gl.quat2.lerp(dualQuaternionOut, dualQuaternionFrom, dualQuaternionTo, cameraTrack.alpha - currentPointNumber)
+
+      // gl.quat2.getTranslation(vec3, dualQuaternionOut)
+      // cameraTransform.position.fromArray(vec3).add(transform.position)
+
+      // gl.quat2.getReal(quat, dualQuaternionOut as any)
+      // cameraTransform.rotation.copy(rotneg90x).multiply(new Quaternion().fromArray(quat))//.fromArray(quat)
+
+      cameraTransform.position
         .lerpVectors(currentPosition, nextPosition, cameraTrack.alpha - currentPointNumber)
         .add(transform.position)
-      camera.rotation.fastSlerpQuaternions(currentRotation, nextRotation, cameraTrack.alpha - currentPointNumber) //.invert().multiply(transform.rotation)
+
+      cameraTransform.rotation.slerpQuaternions(currentRotation, nextRotation, cameraTrack.alpha - currentPointNumber) //.invert().multiply(transform.rotation)
 
       /** remove roll from camera */
       // if (cameraTrack.disableRoll) {
--- packages/engine/src/scene/components/SplineComponent.ts
+++ packages/engine/src/scene/components/SplineComponent.ts
@@ -12,6 +12,7 @@ export const SplineComponent = defineComponent({
 
   onInit: (entity) => {
     const splinePositions = [] as Vector3[]
+    const splineDivisions = [] as Vector3[]
     const spline = new Spline(splinePositions)
     spline.name = `spline-helper-${entity}`
     addObjectToGroup(entity, spline)
@@ -19,23 +20,30 @@ export const SplineComponent = defineComponent({
 
     return {
       splinePositions,
+      splineDivisions,
+      divisions: 200,
       spline
     }
   },
 
   onSet: (entity, component, json) => {
     if (!json) return
+    if (matches.number.test(json.divisions)) {
+      component.divisions.set(json.divisions)
+    }
     if (matches.array.test(json.splinePositions)) {
       const points = [] as Vector3[]
       for (const pos of json.splinePositions) points.push(new Vector3(pos.x, pos.y, pos.z))
       component.splinePositions.set(points)
       component.spline.value.load(component.splinePositions.value)
+      component.splineDivisions.set(component.spline.value.curve.getSpacedPoints())
     }
   },
 
   toJSON: (entity, component) => {
     return {
-      splinePositions: component.spline.value._splineHelperObjects.map((obj) => obj.position)
+      splinePositions: component.spline.value._splineHelperObjects.map((obj) => obj.position),
+      divisions: component.divisions.value
     }
   },
 
