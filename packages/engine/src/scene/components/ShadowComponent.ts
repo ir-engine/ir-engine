4a488353aa3b7e692947a81666bece1d1dcc48d5
AidanCaruso <102248647+AidanCaruso@users.noreply.github.com>
2023-02-07 15:04:15 -0500

    Drop Shadows (#7543)
    * Add initial drop shadows system and component
    
    * Avatars cast drop shadows
    
    * Fix z fighting
    
    * Add simple distance scaling
    
    * Use query reactors to update shadow mesh, various optimizations
    
    * Remove extraneous layer
    
    * Move shadow matrix to global scope

--- packages/engine/src/scene/components/ShadowComponent.ts
+++ packages/engine/src/scene/components/ShadowComponent.ts
@@ -1,11 +1,63 @@
-import { createMappedComponent } from '../../ecs/functions/ComponentFunctions'
+import { useEffect } from 'react'
+import { InstancedMesh, Mesh, MeshBasicMaterial, Object3D, PlaneGeometry } from 'three'
+
+import { Entity } from '../../ecs/classes/Entity'
+import {
+  addComponent,
+  createMappedComponent,
+  defineComponent,
+  getComponent,
+  hasComponent,
+  useComponent
+} from '../../ecs/functions/ComponentFunctions'
+import { EntityReactorProps } from '../../ecs/functions/EntityFunctions'
+import { DropShadowComponent } from './DropShadowComponent'
+import { GroupComponent } from './GroupComponent'
 
 export type ShadowComponentType = {
   cast: boolean
   receive: boolean
 }
 
-export const ShadowComponent = createMappedComponent<ShadowComponentType>('ShadowComponent')
+export const ShadowComponent = defineComponent({
+  name: 'ShadowComponent',
+
+  onInit: (entity) => {
+    return {
+      cast: true,
+      receive: true,
+      castDropShadow: false
+    }
+  },
+
+  toJSON: (entity, component) => {
+    return {
+      cast: component.cast.value,
+      receive: component.receive.value,
+      castDropShadow: component.castDropShadow.value
+    }
+  },
+
+  onSet: (entity, component, json) => {
+    if (!json) return
+    if (json.cast && typeof json.cast == 'boolean') component.cast.set(json.cast)
+    if (json.receive && typeof json.receive == 'boolean') component.receive.set(json.receive)
+    if (json.castDropShadow && typeof json.castDropShadow == 'boolean')
+      component.castDropShadow.set(json.castDropShadow)
+  },
+
+  reactor: ShadowReactor
+})
+
+function ShadowReactor({ root }: EntityReactorProps) {
+  useEffect(() => {
+    const shadowComponent = getComponent(root.entity, ShadowComponent)
+
+    if (shadowComponent.castDropShadow) addComponent(root.entity, DropShadowComponent)
+  })
+
+  return null
+}
 
 export const SCENE_COMPONENT_SHADOW = 'shadow'
 export const SCENE_COMPONENT_SHADOW_DEFAULT_VALUES = {
--- packages/engine/src/scene/systems/ShadowSystem.ts
+++ packages/engine/src/scene/systems/ShadowSystem.ts
@@ -1,10 +1,25 @@
 import { useEffect } from 'react'
-import { DirectionalLight, Group, PerspectiveCamera } from 'three'
+import {
+  Box3,
+  DirectionalLight,
+  DoubleSide,
+  Group,
+  InstancedMesh,
+  Matrix4,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Quaternion,
+  Raycaster,
+  Sphere,
+  TextureLoader,
+  Vector3
+} from 'three'
 
 import { getState, startReactor, useHookstate } from '@xrengine/hyperflux'
 
 import { CSM } from '../../assets/csm/CSM'
-import CSMHelper from '../../assets/csm/CSMHelper'
+import { V_001 } from '../../common/constants/MathConstants'
 import { Engine } from '../../ecs/classes/Engine'
 import { Entity, UndefinedEntity } from '../../ecs/classes/Entity'
 import { World } from '../../ecs/classes/World'
@@ -16,11 +31,20 @@ import {
   removeQuery,
   useQuery
 } from '../../ecs/functions/ComponentFunctions'
+import { startQueryReactor } from '../../ecs/functions/SystemFunctions'
 import { EngineRendererState } from '../../renderer/EngineRendererState'
 import { EngineRenderer, getRendererSceneMetadataState } from '../../renderer/WebGLRendererSystem'
+import { TransformComponent } from '../../transform/components/TransformComponent'
 import { isHeadset, XRState } from '../../xr/XRState'
 import { DirectionalLightComponent } from '../components/DirectionalLightComponent'
+import { DropShadowComponent } from '../components/DropShadowComponent'
+import { GroupComponent } from '../components/GroupComponent'
 import { VisibleComponent } from '../components/VisibleComponent'
+import { ObjectLayers } from '../constants/ObjectLayers'
+
+export const shadowDirection = new Vector3(0, -1, 0)
+
+const defaultShadowMatrix = new Matrix4().multiplyScalar(0)
 
 export default async function ShadowSystem(world: World) {
   const directionalLightQuery = defineQuery([DirectionalLightComponent])
@@ -96,7 +120,80 @@ export default async function ShadowSystem(world: World) {
     return null
   })
 
+  const shadowComponentQuery = defineQuery([DropShadowComponent, GroupComponent])
+
+  const shadowOffset = new Vector3(0, 0.01, 0)
+  const shadowGeometry = new PlaneGeometry(1, 1, 1, 1)
+  const loader = new TextureLoader()
+  const texture = loader.load(
+    (process.env['VITE_FILE_SERVER'] ?? 'https://localhost:8642') + '/projects/default-project/public/drop-shadow.png'
+  )
+  const shadowMaterial = new MeshBasicMaterial({
+    map: texture,
+    side: DoubleSide,
+    transparent: true,
+    depthTest: true,
+    depthWrite: false
+  })
+
+  let dropShadows = new InstancedMesh(shadowGeometry, shadowMaterial, 0)
+  dropShadows.matrixAutoUpdate = false
+
+  const dropShadowReactor = startQueryReactor([DropShadowComponent, GroupComponent], function modifyShadowCount() {
+    world.scene.remove(dropShadows)
+    dropShadows = new InstancedMesh(shadowGeometry, shadowMaterial, shadowComponentQuery().length)
+    dropShadows.matrixAutoUpdate = false
+    dropShadows.layers.disable(ObjectLayers.Camera)
+    world.scene.add(dropShadows)
+    return null
+  })
+
+  let sceneObjects = Array.from(Engine.instance.currentWorld.objectLayerList[ObjectLayers.Camera] || [])
+
+  const CreateDropShadows = () => {
+    let index = 0
+    sceneObjects = Array.from(Engine.instance.currentWorld.objectLayerList[ObjectLayers.Camera] || [])
+
+    for (const entity of shadowComponentQuery()) {
+      const setDropShadowMatrix = (matrix: Matrix4) => {
+        dropShadows.setMatrixAt(index, matrix)
+        index++
+      }
+
+      const group = getComponent(entity, GroupComponent)
+
+      const transform = getComponent(entity, TransformComponent)
+
+      const raycaster = new Raycaster()
+      raycaster.firstHitOnly = true
+      raycaster.set(transform.position, shadowDirection)
+
+      const intersects = raycaster.intersectObjects(sceneObjects)
+      if (!intersects.length || !intersects[0].face) {
+        setDropShadowMatrix(defaultShadowMatrix)
+        continue
+      }
+
+      const sphere = new Sphere()
+      new Box3().setFromObject(group[0], false).getBoundingSphere(sphere)
+      const distanceShrinkBias = 3
+      const sizeBias = 1.5
+      const finalSize = sphere.radius * Math.min(distanceShrinkBias / intersects[0].distance, 1) * sizeBias
+
+      let shadowMatrix = new Matrix4()
+      const shadowRotation = new Quaternion().setFromUnitVectors(intersects[0].face.normal, V_001)
+
+      shadowMatrix.makeRotationFromQuaternion(shadowRotation)
+      shadowMatrix.scale(new Vector3(finalSize, finalSize, finalSize))
+      shadowMatrix.setPosition(intersects[0].point.add(shadowOffset))
+      setDropShadowMatrix(shadowMatrix)
+    }
+    dropShadows.instanceMatrix.needsUpdate = true
+  }
+
   const execute = () => {
+    CreateDropShadows()
+
     if (!EngineRenderer.instance.csm) return
     EngineRenderer.instance.csm.sourceLight.getWorldDirection(EngineRenderer.instance.csm.lightDirection)
     if (renderState.qualityLevel.value > 0) EngineRenderer.instance.csm.update()
@@ -105,7 +202,9 @@ export default async function ShadowSystem(world: World) {
 
   const cleanup = async () => {
     removeQuery(world, directionalLightQuery)
+    removeQuery(world, shadowComponentQuery)
     csmReactor.stop()
+    dropShadowReactor.stop()
   }
 
   return { execute, cleanup }
