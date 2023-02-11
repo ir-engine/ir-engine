d0963a5df58b43c12275e29fd1e5f9db5c515663
AidanCaruso <jamesajcme@gmail.com>
2023-02-10 23:56:57 -0500

    Add set function to dropshadow component

--- packages/engine/src/scene/components/DropShadowComponent.ts
+++ packages/engine/src/scene/components/DropShadowComponent.ts
@@ -10,5 +10,12 @@ export const DropShadowComponent = defineComponent({
       center: undefined as Vector3 | undefined,
       bias: 2
     }
+  },
+
+  onSet: (entity, component, json) => {
+    if (!json) return
+    if (typeof json.bias !== 'undefined') component.bias.set(json.bias)
+    if (typeof json.center !== 'undefined') component.center.set(json.center)
+    if (typeof json.radius !== 'undefined') component.radius.set(json.radius)
   }
 })
