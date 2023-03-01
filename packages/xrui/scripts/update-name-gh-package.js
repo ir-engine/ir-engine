7e6cb938f194ff1f91c6423c9bc280d8c4f34da6
Josh Field <joshfield999@gmail.com>
2023-03-01 11:55:40 +1100

    Fix XRUI package having wrong github org (#7653)

--- packages/xrui/scripts/update-name-gh-package.js
+++ packages/xrui/scripts/update-name-gh-package.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 (() => {
     const packageContents = JSON.parse(fs.readFileSync('./package.json').toString());
-    packageContents.name = '@xrfoundation/xrui';
+    packageContents.name = '@xrengine/xrui';
     fs.writeFileSync('./package.json', Buffer.from(JSON.stringify(packageContents)));
 })();
