f7257c50972abaff30793ce92d80da349b67acbc
David Gordon <david@hyperconstruct.io>
2023-03-10 14:04:29 -0800

    wip resource id transformer

--- packages/engine/src/assets/exporters/gltf/extensions/ResourceIDExtension.ts
+++ packages/engine/src/assets/exporters/gltf/extensions/ResourceIDExtension.ts
@@ -1,4 +1,4 @@
-import { Mesh } from 'three'
+import { Mesh, Texture } from 'three'
 
 import { ResourceID } from '../../../classes/ModelTransform'
 import { GLTFExporterPlugin } from '../GLTFExporter'
@@ -14,12 +14,21 @@ export default class ResourceIDExtension extends ExporterExtension implements GL
     this.name = 'EE_resourceId'
   }
 
-  writeImage(image: HTMLImageElement, imageDef: { [key: string]: any }) {
+  /*writeImage(image: HTMLImageElement, imageDef: { [key: string]: any }) {
     const result: ResourceIDExtensionType = {
       resourceId: image.id as ResourceID
     }
     imageDef.extensions = imageDef.extensions ?? {}
     imageDef.extensions[this.name] = result
+    this.writer.extensionsUsed[this.name] = true
+  }*/
+
+  writeTexture(map: Texture, textureDef: { [key: string]: any }) {
+    const result: ResourceIDExtensionType = {
+      resourceId: map.image.id as ResourceID
+    }
+    textureDef.extensions = textureDef.extensions ?? {}
+    textureDef.extensions[this.name] = result
     this.writer.extensionsUsed[this.name] = true
   }
 
