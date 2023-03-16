9694b94a55c1093f41321510f2bfda124e761fb4
HexaField <joshfield999@gmail.com>
2023-03-16 11:44:39 +1100

    can connect to world and media servers

--- packages/client/src/pages/recorder/[locationName].tsx
+++ packages/client/src/pages/recorder/[locationName].tsx
@@ -1,33 +1,53 @@
 import React, { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 
+import { API } from '@etherealengine/client-core/src/API'
+import { LocationInstanceConnectionServiceReceptor } from '@etherealengine/client-core/src/common/services/LocationInstanceConnectionService'
 import { LocationIcons } from '@etherealengine/client-core/src/components/LocationIcons'
+import LoadLocationScene from '@etherealengine/client-core/src/components/World/LoadLocationScene'
 import NetworkInstanceProvisioning from '@etherealengine/client-core/src/components/World/NetworkInstanceProvisioning'
 import { FriendService } from '@etherealengine/client-core/src/social/services/FriendService'
 import { LocationAction } from '@etherealengine/client-core/src/social/services/LocationService'
 import { AuthService } from '@etherealengine/client-core/src/user/services/AuthService'
 import { SceneService } from '@etherealengine/client-core/src/world/services/SceneService'
-import { dispatchAction } from '@etherealengine/hyperflux'
+import { MediaModule } from '@etherealengine/engine/src/audio/MediaModule'
+import { EngineActions, EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
+import { initSystems } from '@etherealengine/engine/src/ecs/functions/SystemFunctions'
+import { addActionReceptor, dispatchAction, getMutableState, removeActionReceptor } from '@etherealengine/hyperflux'
+import { loadEngineInjection } from '@etherealengine/projects/loadEngineInjection'
 import Mediapipe from '@etherealengine/ui/src/Mediapipe'
 
-export const Recorder = (): any => {
-  const { t } = useTranslation()
+const systems = [...MediaModule()]
 
+export const initializeEngineForRecorder = async () => {
+  if (getMutableState(EngineState).isEngineInitialized.value) return
+
+  const projects = API.instance.client.service('projects').find()
+
+  await initSystems(systems)
+  await loadEngineInjection(await projects)
+
+  dispatchAction(EngineActions.initializeEngine({ initialised: true }))
+  dispatchAction(EngineActions.sceneLoaded({}))
+}
+
+export const Recorder = (): any => {
   const params = useParams()
   AuthService.useAPIListeners()
   SceneService.useAPIListeners()
   FriendService.useAPIListeners()
 
-  const locationName = params.locationName
-
-  console.log('locationName', locationName)
+  const locationName = params.locationName!
 
   useEffect(() => {
-    if (!locationName) {
-      throw new Error('not implemented')
-    }
     dispatchAction(LocationAction.setLocationName({ locationName }))
+    initializeEngineForRecorder()
+
+    addActionReceptor(LocationInstanceConnectionServiceReceptor)
+
+    return () => {
+      removeActionReceptor(LocationInstanceConnectionServiceReceptor)
+    }
   }, [])
 
   return (
@@ -35,6 +55,7 @@ export const Recorder = (): any => {
       <div>{<Mediapipe />}</div>
       <NetworkInstanceProvisioning />
       <LocationIcons />
+      <LoadLocationScene />
     </div>
   )
 }
