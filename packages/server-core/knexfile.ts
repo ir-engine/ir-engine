ff62748dd7641459b3a221d14b76ad3f08047a85
hanzlamateen <hanzlamateen@live.com>
2023-03-28 16:14:49 +0500

    Added config knexfile

---
+++ packages/server-core/knexfile.ts
@@ -0,0 +1,23 @@
+import type { Knex } from 'knex'
+
+import appConfig from '@etherealengine/server-core/src/appconfig'
+
+const config: Knex.Config = {
+  client: 'mysql',
+  connection: appConfig.db.url,
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations'
+  }
+}
+
+// const config: Knex.Config = {
+//     client: "sqlite3",
+//     connection: {
+//       filename: "./dev.sqlite3"
+//     }
+// }
+
+console.log(config)
+
+module.exports = config
