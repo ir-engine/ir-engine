35266b8926ce20e177cc0045eee41eca34675bef
Arnav <arnavkul07@gmail.com>
2023-03-29 13:46:09 +0530

    Rebase Loading Circle uses tailwind

--- packages/ui/src/primitives/tailwind/LoadingCircle/LoadingCircle.test.tsx
+++ packages/ui/src/primitives/tailwind/LoadingCircle/LoadingCircle.test.tsx
@@ -5,11 +5,8 @@ import React from 'react'
 import LoadingCircle from './index'
 import { Default as story } from './index.stories'
 
-<<<<<<< HEAD
 //loadingCircle story test
 
-=======
->>>>>>> dev
 describe('LoadingCircle', () => {
   it('- should render', () => {
     const wrapper = shallow(<LoadingCircle {...story?.args} />)
