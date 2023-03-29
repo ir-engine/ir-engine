8bba2092e9816af6e8708b7f0a551c579193b6fc
jerknose <travis@travisbennett.com>
2023-03-29 15:50:47 -0700

    Adjust layout, pointer events, and toolbar.

--- packages/ui/src/primitives/tailwind/Video/index.tsx
+++ packages/ui/src/primitives/tailwind/Video/index.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 const Video = ({ className, ...props }, ref) => {
-  return <video className={twMerge(className)} ref={ref} {...props} />
+  return <video {...{ playsInline: true, autoplay: true }} className={twMerge(className)} ref={ref} {...props} />
 }
 
 export default forwardRef(Video)
