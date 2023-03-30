e18c732814adf36d3e3ea5a7de0b2aba3e5d7154
hanzlamateen <hanzlamateen@live.com>
2023-03-30 11:37:24 +0500

    Updated command to use hasTable & createTable as createTableIfNotExists is outdated.

--- packages/server-core/migrations/20230328095417_route.ts
+++ packages/server-core/migrations/20230328095417_route.ts
@@ -5,13 +5,17 @@ import type { Knex } from 'knex'
  * @returns { Promise<void> }
  */
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTableIfNotExists('route', (table) => {
-    table.string('id', 36).primary()
-    table.string('project', 255).nullable()
-    table.string('route', 255).nullable()
-    table.dateTime('createdAt').notNullable()
-    table.dateTime('updatedAt').notNullable()
-  })
+  const routeTableExists = await knex.schema.hasTable('route')
+
+  if (routeTableExists === false) {
+    await knex.schema.createTable('route', (table) => {
+      table.string('id', 36).primary()
+      table.string('project', 255).nullable()
+      table.string('route', 255).nullable()
+      table.dateTime('createdAt').notNullable()
+      table.dateTime('updatedAt').notNullable()
+    })
+  }
 }
 
 /**
