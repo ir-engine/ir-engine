79d4dd11e403277479243261e68a3d19eae5e4aa
David Gordon <david@hyperconstruct.io>
2023-03-30 22:49:47 -0700

    rebuild KTX2Encoder and KTX2Worker

--- packages/engine/src/assets/exporters/gltf/extensions/BasisuExporterExtension.ts
+++ packages/engine/src/assets/exporters/gltf/extensions/BasisuExporterExtension.ts
@@ -21,40 +21,42 @@ export default class BasisuExporterExtension extends ExporterExtension implement
     const writer = this.writer
 
     writer.pending.push(
-      new Promise(async (resolve) => {
-        const texture = (await createReadableTexture(_texture, { canvas: true, flipY: true })) as Texture
-        textureDef.sampler = this.sampler
-        const image: HTMLCanvasElement = texture.source.data
-
-        const ktx2write = new KTX2Encoder()
-        const imageDef: any = {
-          width: image.width,
-          height: image.height,
-          mimeType: 'image/ktx2'
-        }
-        if (!writer.json.images) writer.json.images = []
-        const imgIdx = writer.json.images.push(imageDef) - 1
-
-        const imgData: ImageDataType = image.getContext('2d')!.getImageData(0, 0, image.width, image.height) as any
-        ktx2write.encode(imgData).then(async (arrayBuffer) => {
-          const bufferIdx = writer.processBuffer(arrayBuffer)
-
-          const bufferViewDef = {
-            buffer: bufferIdx,
-            byteOffset: writer.byteOffset,
-            byteLength: arrayBuffer.byteLength
-          }
-
-          writer.byteOffset += arrayBuffer.byteLength
-          const bufferViewIdx = writer.json.bufferViews.push(bufferViewDef) - 1
-
-          imageDef.bufferView = bufferViewIdx
-
-          writer.extensionsUsed[this.name] = true
-          textureDef.extensions = textureDef.extensions ?? {}
-          textureDef.extensions[this.name] = { source: imgIdx }
+      new Promise((resolve) => {
+        createReadableTexture(_texture, { canvas: true, flipY: true }).then((texture: Texture) => {
           textureDef.sampler = this.sampler
-          resolve(null)
+          const image: HTMLCanvasElement = texture.source.data
+
+          const ktx2write = new KTX2Encoder()
+
+          const imageDef: any = {
+            width: image.width,
+            height: image.height,
+            mimeType: 'image/ktx2'
+          }
+          if (!writer.json.images) writer.json.images = []
+          const imgIdx = writer.json.images.push(imageDef) - 1
+
+          const imgData: ImageDataType = image.getContext('2d')!.getImageData(0, 0, image.width, image.height) as any
+          ktx2write.encode(imgData).then(async (arrayBuffer) => {
+            const bufferIdx = writer.processBuffer(arrayBuffer)
+
+            const bufferViewDef = {
+              buffer: bufferIdx,
+              byteOffset: writer.byteOffset,
+              byteLength: arrayBuffer.byteLength
+            }
+
+            writer.byteOffset += arrayBuffer.byteLength
+            const bufferViewIdx = writer.json.bufferViews.push(bufferViewDef) - 1
+
+            imageDef.bufferView = bufferViewIdx
+
+            writer.extensionsUsed[this.name] = true
+            textureDef.extensions = textureDef.extensions ?? {}
+            textureDef.extensions[this.name] = { source: imgIdx }
+            textureDef.sampler = this.sampler
+            resolve(null)
+          })
         })
       })
     )
