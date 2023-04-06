771dfb76e966aee036f6e99e8b7a37d5178199c0
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-04-06 08:43:14 +0530

    fix: update xrengine.config for test

--- packages/server-core/src/route/route/route.test.ts
+++ packages/server-core/src/route/route/route.test.ts
@@ -1,5 +1,6 @@
 import appRootPath from 'app-root-path'
 import assert from 'assert'
+import fs from 'fs'
 import path from 'path'
 import { v4 as uuid } from 'uuid'
 
@@ -19,6 +20,36 @@ const cleanup = async (app: Application, projectName: string) => {
   } catch (e) {}
 }
 
+const updateXREngineConfigForTest = (newProject: string) => {
+  const testXREngineConfig = `
+  import type { ProjectConfigInterface } from '@etherealengine/projects/ProjectConfigInterface'
+
+  const config: ProjectConfigInterface = {
+    onEvent: undefined,
+    thumbnail: '/static/etherealengine.png',
+    routes: {
+      test: {
+        component: () => import('@etherealengine/client/src/pages/index'),
+      },
+      "${newProject}": {
+        component: () => import('@etherealengine/client/src/pages/index'),
+      }
+    },
+    services: undefined,
+    databaseSeed: undefined
+  }
+  
+  export default config
+  `
+
+  const projectsRootFolder = path.join(appRootPath.path, 'packages/projects/projects/')
+  const projectLocalDirectory = path.resolve(projectsRootFolder, newProject)
+  const xrEngineConfigFilePath = path.resolve(projectLocalDirectory, 'xrengine.config.ts')
+
+  fs.rmSync(xrEngineConfigFilePath)
+  fs.writeFileSync(xrEngineConfigFilePath, testXREngineConfig)
+}
+
 describe('route.test', () => {
   let app: Application
   let newProject: string
@@ -36,6 +67,7 @@ describe('route.test', () => {
   it('should find the installed project routes', async () => {
     newProject = `test-project-${uuid()}`
     await app.service('project').create({ name: newProject }, params)
+    updateXREngineConfigForTest(newProject)
 
     const installedRoutes = await app.service('routes-installed').find()
     const route = installedRoutes.data.find((route) => route.project === newProject)
@@ -53,12 +85,14 @@ describe('route.test', () => {
     const activateResult = await app
       .service('route-activate')
       .create({ project: newProject, route: newProject, activate: true }, params)
-    const route = await app.service('route').find()
+    const fetchResult = await app.service('route').find({ query: { project: newProject } })
+    const route = fetchResult.data.find((d) => d.project === newProject)
 
     assert.ok(activateResult)
-    assert.equal(route.total, 1)
-    assert.equal(route.data.project, newProject)
-    assert.equal(route.data.route, newProject)
+    assert.equal(fetchResult.total, 1)
+
+    assert.equal(route?.project, newProject)
+    assert.equal(route?.route, newProject)
   })
 
   it('should deactivate a route', async () => {
