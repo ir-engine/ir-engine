2ea0aaf6aaa055a621dcea372e6002b91cd5f91a
jerknose <travis@travisbennett.com>
2023-04-07 15:09:09 -0700

    Generate precache sw / manifest in public.

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,8 @@
 import { t } from 'i18next'
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useRegisterSW } from 'virtual:pwa-register/react'
 
 import ErrorBoundary from '@etherealengine/client-core/src/common/components/ErrorBoundary'
 import { LoadingCircle } from '@etherealengine/client-core/src/components/LoadingCircle'
@@ -16,7 +17,32 @@ const AppPage = lazy(() => import('./pages/_app'))
 const AdminPage = lazy(() => import('./pages/admin'))
 const TailwindPage = lazy(() => import('./pages/_app_tw'))
 
+const options = {
+  immediate: true,
+  onNeedRefresh: () => {
+    console.log('onNeedRefresh')
+  },
+  onOfflineReady: () => {
+    console.log('onOfflineReady')
+  },
+  onRegistered: () => {
+    console.log('onRegistered')
+  },
+  onRegisteredSW: (swUrl, registration) => {
+    // eslint-disable-next-line no-console
+    console.log(`Service Worker at: ${swUrl}`)
+  },
+  onRegisterError: (e) => {
+    console.log('onRegisterError', e)
+  }
+}
+
 const App = () => {
+  const {
+    offlineReady: [offlineReady, setOfflineReady],
+    needRefresh: [needRefresh, setNeedRefresh]
+  } = useRegisterSW(options)
+
   return (
     <ErrorBoundary>
       <BrowserRouter>
