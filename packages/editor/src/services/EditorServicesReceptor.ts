84d6090789e5479ab069c77bab0c002364e3a411
HexaField <joshfield999@gmail.com>
2023-04-11 13:55:24 +1000

    Refactor Editor Error State

--- packages/editor/src/services/EditorServicesReceptor.ts
+++ packages/editor/src/services/EditorServicesReceptor.ts
@@ -1,17 +1,14 @@
 import { addActionReceptor, removeActionReceptor } from '@etherealengine/hyperflux'
 
-import { EditorErrorServiceReceptor } from './EditorErrorServices'
 import { EditorHelperServiceReceptor } from './EditorHelperState'
 import { EditorServiceReceptor } from './EditorServices'
 
 export const registerEditorReceptors = () => {
-  addActionReceptor(EditorErrorServiceReceptor)
   addActionReceptor(EditorHelperServiceReceptor)
   addActionReceptor(EditorServiceReceptor)
 }
 
 export const unregisterEditorReceptors = () => {
-  removeActionReceptor(EditorErrorServiceReceptor)
   removeActionReceptor(EditorHelperServiceReceptor)
   removeActionReceptor(EditorServiceReceptor)
 }
