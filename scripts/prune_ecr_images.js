3b10f7da973031e7abb97acd5906855327da15b9
usaurabh02 <saurabhu154@gmail.com>
2023-04-19 12:40:39 +0530

    Switched from AWS SDK v2 to v3

--- scripts/prune_ecr_images.js
+++ scripts/prune_ecr_images.js
@@ -1,32 +1,45 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const cli = require('cli');
-const AWS = require('aws-sdk');
+import { ECRClient } from '@aws-sdk/client-ecr'
+import { DescribeImagesCommand, ECRPUBLICClient } from '@aws-sdk/client-ecr-public'
 
-cli.enable('status');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const cli = require('cli')
+
+cli.enable('status')
 
 const options = cli.parse({
-    repoName: [false, 'Name of repository', 'string'],
-    public: [false, 'Whether or not the ECR repo is public', 'boolean'],
-    region: [false, 'Name of AWS region', 'string']
-});
+  repoName: [false, 'Name of repository', 'string'],
+  public: [false, 'Whether or not the ECR repo is public', 'boolean'],
+  region: [false, 'Name of AWS region', 'string']
+})
 
 cli.main(async () => {
-    try {
-        const ecr = options.public === true ? new AWS.ECRPUBLIC({region: 'us-east-1'}) : new AWS.ECR({ region: options.region || 'us-east-1' });
-        const result = await ecr.describeImages({repositoryName: options.repoName || 'etherealengine'}).promise();
-        const images = result.imageDetails;
-        const withoutLatest = images.filter(image => image.imageTags == null || (image.imageTags && image.imageTags.indexOf('latest_dev') < 0 && image.imageTags.indexOf('latest_prod') < 0));
-        const sorted = withoutLatest.sort((a, b) => b.imagePushedAt - a.imagePushedAt);
-        const toBeDeleted = sorted.slice(10,);
-        if (toBeDeleted.length > 0) {
-            const deleteParams = {
-                imageIds: toBeDeleted.map(image => { return { imageDigest: image.imageDigest } }),
-                repositoryName: options.repoName || 'etherealengine'
-            };
-            await ecr.batchDeleteImage(deleteParams).promise();
-        }
-    } catch(err) {
-        console.log('Error in deleting old ECR images:');
-        console.log(err);
+  try {
+    const ecr =
+      options.public === true
+        ? new ECRPUBLICClient({ region: 'us-east-1' })
+        : new ECRClient({ region: options.region || 'us-east-1' })
+    const input = { repositoryName: options.repoName || 'etherealengine' }
+    const command = new DescribeImagesCommand(input)
+    const response = await ecr.send(command)
+    const images = response.imageDetails
+    const withoutLatest = images.filter(
+      (image) =>
+        image.imageTags == null ||
+        (image.imageTags && image.imageTags.indexOf('latest_dev') < 0 && image.imageTags.indexOf('latest_prod') < 0)
+    )
+    const sorted = withoutLatest.sort((a, b) => b.imagePushedAt - a.imagePushedAt)
+    const toBeDeleted = sorted.slice(10)
+    if (toBeDeleted.length > 0) {
+      const deleteParams = {
+        imageIds: toBeDeleted.map((image) => {
+          return { imageDigest: image.imageDigest }
+        }),
+        repositoryName: options.repoName || 'etherealengine'
+      }
+      await ecr.batchDeleteImage(deleteParams).promise()
     }
-});
+  } catch (err) {
+    console.log('Error in deleting old ECR images:')
+    console.log(err)
+  }
+})
