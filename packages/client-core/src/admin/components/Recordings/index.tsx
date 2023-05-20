362bc0c26b9d398b36f3deae6a620c17dfd7b5a9
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-05-20 14:12:40 +0530

    chore: remove grid in recordings index

--- packages/client-core/src/admin/components/Recordings/index.tsx
+++ packages/client-core/src/admin/components/Recordings/index.tsx
@@ -1,25 +1,9 @@
 import React from 'react'
 
-import Grid from '@etherealengine/ui/src/primitives/mui/Grid'
-
-import Search from '../../common/Search'
-import styles from '../../styles/admin.module.scss'
 import RecordingsTable from './RecordingsTable'
 
 const Recordings = () => {
-  return (
-    <div>
-      <h1>recordings page</h1>
-      <Grid container spacing={1} className={styles.mb10px}>
-        <Grid item xs={12} sm={8}>
-          <Search text="instance" handleChange={() => {}} />
-        </Grid>
-      </Grid>
-      <RecordingsTable />
-      {/* <InstanceTable className={styles.rootTableWithSearch} search={search.value} />
-      {patchInstanceserverOpen.value && <PatchInstanceserver open onClose={() => patchInstanceserverOpen.set(false)} />} */}
-    </div>
-  )
+  return <RecordingsTable />
 }
 
 export default Recordings
