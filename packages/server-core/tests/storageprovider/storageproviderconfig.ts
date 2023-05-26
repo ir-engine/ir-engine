802b79f2785f1bd2568c03fcf206655a31959e43
Kyle Baran <kbaran@bitscoop.com>
2023-05-25 19:02:14 -0700

    Switched from AWS SDK v2 to v3 (#8002)
    Co-authored-by: usaurabh02 <saurabhu154@gmail.com>

--- packages/server-core/tests/storageprovider/storageproviderconfig.ts
+++ packages/server-core/tests/storageprovider/storageproviderconfig.ts
@@ -1,3 +1,5 @@
+import { DeleteObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3'
+import { CreateBucketCommand, HeadBucketCommand } from '@aws-sdk/client-s3'
 import appRootPath from 'app-root-path'
 import fs from 'fs'
 import path from 'path'
@@ -41,17 +43,19 @@ const localStorageAfterTest = (provider, testFolderName): Promise<any> => {
 
 const clearS3TestFolder = (provider: S3Provider, testFolderName: string): Promise<any> => {
   return new Promise((resolve) => {
-    provider.provider
-      .listObjectsV2({ Bucket: provider.bucket, Prefix: testFolderName })
-      .promise()
-      .then((res) => {
-        const promises: any[] = []
-        res.Contents!.forEach((element) => {
-          promises.push(provider.provider.deleteObject({ Bucket: provider.bucket!, Key: element.Key! }).promise())
-        })
-
-        Promise.all(promises).then(resolve)
+    const command = new ListObjectsV2Command({
+      Bucket: provider.bucket,
+      Prefix: testFolderName
+    })
+    provider.provider.send(command).then((res) => {
+      const promises: Promise<any>[] = []
+      res.Contents!.forEach((element) => {
+        const command = new DeleteObjectCommand({ Bucket: provider.bucket!, Key: element.Key! })
+        promises.push(provider.provider.send(command))
       })
+
+      Promise.all(promises).then(resolve)
+    })
   })
 }
 const s3StorageBeforeTest = async (provider: S3Provider): Promise<any> => {
@@ -59,12 +63,13 @@ const s3StorageBeforeTest = async (provider: S3Provider): Promise<any> => {
 
   let bucketExists
   try {
-    bucketExists = await provider.provider.headBucket({ Bucket: provider.bucket }).promise()
+    bucketExists = await provider.provider.send(new HeadBucketCommand({ Bucket: provider.bucket }))
   } catch (err) {
     if (err.code !== 'NotFound') throw err
   }
-  if (bucketExists == null)
-    await provider.provider.createBucket({ Bucket: provider.bucket, ACL: 'public-read' }).promise()
+  if (bucketExists == null) {
+    await provider.provider.send(new CreateBucketCommand({ Bucket: provider.bucket, ACL: 'public-read' }))
+  }
   return
 }
 const s3StorageAfterTest = (provider: S3Provider, testFolderName): Promise<any> =>
