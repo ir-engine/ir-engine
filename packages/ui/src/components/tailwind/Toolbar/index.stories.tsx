0dcbb66fc8590d609ba8fab02df67c893228f1c2
jerknose <travis@travisbennett.com>
2023-06-15 15:43:48 -0700

    Fixy

--- packages/ui/src/components/tailwind/Toolbar/index.stories.tsx
+++ packages/ui/src/components/tailwind/Toolbar/index.stories.tsx
@@ -17,17 +17,42 @@ export default {
 }
 export const Default = { args: Component.defaultProps }
 
+export const ShowLabels = {
+  args: {
+    ...Component.defaultProps,
+    showLabels: true
+  }
+}
+
+export const ButtonsSM = {
+  args: {
+    ...Component.defaultProps,
+    buttonSize: 'sm'
+  }
+}
+
+export const ButtonsMD = {
+  args: {
+    ...Component.defaultProps
+  }
+}
+
+export const ButtonsLG = {
+  args: {
+    ...Component.defaultProps,
+    buttonSize: 'lg'
+  }
+}
+
 export const CameraLoading = {
   args: {
     ...Component.defaultProps,
-    isRecording: true,
     videoStatus: 'loading'
   }
 }
 export const CameraReady = {
   args: {
     ...Component.defaultProps,
-    isRecording: true,
     videoStatus: 'ready'
   }
 }
@@ -35,7 +60,53 @@ export const CameraReady = {
 export const CameraOn = {
   args: {
     ...Component.defaultProps,
-    isRecording: true,
     videoStatus: 'active'
+  }
+}
+
+export const PoseLoading = {
+  args: {
+    ...CameraOn.args,
+    detectingStatus: 'loading'
+  }
+}
+
+export const PoseReady = {
+  args: {
+    ...CameraOn.args,
+    detectingStatus: 'inactive'
+  }
+}
+
+export const PoseDetecting = {
+  args: {
+    ...CameraOn.args,
+    detectingStatus: 'active',
+    recordingStatus: 'ready'
+  }
+}
+
+export const RecordingLoading = {
+  args: {
+    ...PoseDetecting.args,
+    recordingStatus: 'inactive',
+    detectingStatus: 'inactive',
+    isRecording: false
+  }
+}
+
+export const RecordingReady = {
+  args: {
+    ...PoseDetecting.args,
+    recordingStatus: 'ready',
+    isRecording: false
+  }
+}
+
+export const RecordingActive = {
+  args: {
+    ...PoseDetecting.args,
+    recordingStatus: 'active',
+    isRecording: true
   }
 }
--- packages/ui/src/pages/Capture/index.tsx
+++ packages/ui/src/pages/Capture/index.tsx
@@ -295,13 +295,19 @@ const CaptureDashboard = () => {
   }
 
   const isCamVideoEnabled =
-    mediaStreamState?.camVideoProducer?.value !== null && mediaStreamState.videoPaused.value !== null
+    mediaStreamState?.camVideoProducer?.value !== null && mediaStreamState?.videoPaused?.value === false
   const videoStatus =
-    mediaConnection?.connected?.value === false && videoActive?.value === false
+    !mediaConnection?.connected?.value && !videoActive?.value
       ? 'loading'
       : isCamVideoEnabled !== true
       ? 'ready'
       : 'active'
+  const recordingStatus =
+    !recordingState?.recordingID?.value && isDetecting?.value !== true
+      ? 'inactive'
+      : recordingState?.started?.value
+      ? 'active'
+      : 'ready'
 
   return (
     <div className="w-full container mx-auto">
@@ -413,13 +419,13 @@ const CaptureDashboard = () => {
       >
         <Header />
         <div className="w-full container mx-auto">
-          <div className="w-full px-2">
-            <div className="w-full relative ">
-              <div className="relative w-full h-auto max-w-full" style={{ backgroundColor: '#000000' }}>
-                <Video ref={videoRef} className="min-w-full h-auto max-w-full" />
+          <div className="w-full h-auto px-2">
+            <div className="w-full h-auto relative aspect-w-16 aspect-h-9 overflow-hidden">
+              <div className="absolute w-full h-auto top-0 left-0" style={{ backgroundColor: '#000000' }}>
+                <Video ref={videoRef} className="w-full h-auto" />
               </div>
               <div
-                className="object-contain absolute top-0 z-20 min-w-full h-auto max-w-full"
+                className="object-contain absolute top-0 left-0 z-20 min-w-full h-auto"
                 style={{ objectFit: 'contain', top: '0px' }}
               >
                 <Canvas ref={canvasRef} />
@@ -429,7 +435,7 @@ const CaptureDashboard = () => {
                   onClick={() => {
                     if (mediaConnection?.connected?.value) toggleWebcamPaused()
                   }}
-                  className="absolute btn btn-ghost bg-none h-full w-full container mx-auto m-0 p-0 top-0"
+                  className="absolute btn btn-ghost bg-none h-full w-full container mx-auto m-0 p-0 top-0 left-0"
                 >
                   <h1>{mediaConnection?.connected?.value ? 'Enable Camera' : 'Loading...'}</h1>
                 </button>
@@ -444,8 +450,8 @@ const CaptureDashboard = () => {
               onToggleRecording={onToggleRecording}
               toggleWebcam={toggleWebcamPaused}
               toggleDetecting={() => isDetecting.set((v) => !v)}
-              isRecording={recordingState.started.value}
-              recordingStatus={recordingState.recordingID.value}
+              isRecording={recordingState?.started?.value}
+              recordingStatus={recordingStatus}
               isVideoFlipped={isVideoFlipped}
               flipVideo={(v) => {
                 setIsVideoFlipped(v)
