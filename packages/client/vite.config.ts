79c6d509bc1f5f7fdddade9dbc349e8475ec9387
jerknose <travis@travisbennett.com>
2023-06-16 13:24:13 -0700

    remove react-json-tree

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -309,7 +309,7 @@ export default defineConfig(async () => {
     ].filter(Boolean),
     resolve: {
       alias: {
-        'react-json-tree': 'react-json-tree/lib/umd/react-json-tree.min.js',
+        'react-json-tree': 'react-json-tree/umd /react-json-tree.min.js',
         '@mui/styled-engine': '@mui/styled-engine-sc/'
       }
     },
