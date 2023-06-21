5c7038b4a0aa9d8e1417dc45cd7d9b7268ca3684
HexaField <joshfield999@gmail.com>
2023-06-21 10:05:56 +1000

    fix tsc errors and unnecessary packages in root

--- packages/editor/src/components/properties/three.quarks/ColorGenerator.tsx
+++ packages/editor/src/components/properties/three.quarks/ColorGenerator.tsx
@@ -25,7 +25,7 @@ Ethereal Engine. All Rights Reserved.
 
 import React, { useCallback } from 'react'
 import { Color } from 'three'
-import { FunctionJSON } from 'three.quarks/dist/three.quarks.esm'
+import { FunctionJSON } from 'three.quarks'
 
 import {
   ColorGeneratorJSON,
