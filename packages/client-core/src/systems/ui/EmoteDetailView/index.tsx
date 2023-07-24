79ca78c80db2635b67b073230bb71f54975edccc
jerknose <travis@travisbennett.com>
2023-07-24 10:18:51 -0700

    types

--- packages/client-core/src/systems/ui/EmoteDetailView/index.tsx
+++ packages/client-core/src/systems/ui/EmoteDetailView/index.tsx
@@ -46,14 +46,14 @@ const EmoteDetailView = () => {
   const [page, setPage] = useState(0)
 
   const imgPerPage = 7
-  let menuRadius = 250
-  let menuPadding = 25
+  const menuRadius = 250
+  const menuPadding = 25
   let menuThickness = menuRadius > 170 ? 70 : 60
   let menuItemWidth = menuThickness - menuPadding
   let menuItemRadius = menuItemWidth / 2
   let effectiveRadius = menuRadius - menuItemRadius - menuPadding / 2
 
-  let [items, setItems] = useState([
+  const [items, setItems] = useState([
     {
       body: <img src="/static/grinning.svg" alt="Dance 4" />,
       containerProps: {
@@ -122,6 +122,11 @@ const EmoteDetailView = () => {
     }
   ])
 
+  // const runAnimation = (stateName: string) => {
+  //   const entity = Engine.instance.localClientEntity
+  //   changeAvatarAnimationState(entity, stateName)
+  // }
+
   const calculateMenuRadius = () => {
     calculateOtherValues()
   }
@@ -142,7 +147,7 @@ const EmoteDetailView = () => {
     const itemList = [] as JSX.Element[]
     const startIndex = page * imgPerPage
     const endIndex = Math.min(startIndex + imgPerPage, items.length)
-    let angle = 360 / imgPerPage
+    const angle = 360 / imgPerPage
     let index = 0
     let itemAngle = 0
     let x = 0
