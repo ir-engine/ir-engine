266c7f02ff53649101f0fcdb8a23addc2028d50f
hanzlamateen <hanzlamateen@live.com>
2023-07-24 13:18:03 +0500

    Added location hooks

--- packages/server-core/src/social/location/location.hooks.ts
+++ packages/server-core/src/social/location/location.hooks.ts
@@ -27,17 +27,57 @@ import { iff, isProvider } from 'feathers-hooks-common'
 
 import verifyScope from '@etherealengine/server-core/src/hooks/verify-scope'
 
+import { hooks as schemaHooks } from '@feathersjs/schema'
+import { getValidator } from '@feathersjs/typebox'
 import authenticate from '../../hooks/authenticate'
 
+import {
+  locationDataSchema,
+  locationPatchSchema,
+  locationQuerySchema,
+  locationSchema
+} from '@etherealengine/engine/src/schemas/social/location.schema'
+import { dataValidator, queryValidator } from '@etherealengine/server-core/validators'
+
+import {
+  locationDataResolver,
+  locationExternalResolver,
+  locationPatchResolver,
+  locationQueryResolver,
+  locationResolver
+} from './location.resolvers'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const locationValidator = getValidator(locationSchema, dataValidator)
+const locationDataValidator = getValidator(locationDataSchema, dataValidator)
+const locationPatchValidator = getValidator(locationPatchSchema, dataValidator)
+const locationQueryValidator = getValidator(locationQuerySchema, queryValidator)
+
 export default {
+  around: {
+    all: [schemaHooks.resolveExternal(locationExternalResolver), schemaHooks.resolveResult(locationResolver)]
+  },
+
   before: {
-    all: [authenticate()],
+    all: [
+      authenticate(),
+      () => schemaHooks.validateQuery(locationQueryValidator),
+      schemaHooks.resolveQuery(locationQueryResolver)
+    ],
     find: [],
     get: [],
-    create: [iff(isProvider('external'), verifyScope('location', 'write') as any)],
-    update: [iff(isProvider('external'), verifyScope('location', 'write') as any)],
-    patch: [iff(isProvider('external'), verifyScope('location', 'write') as any)],
-    remove: [iff(isProvider('external'), verifyScope('location', 'write') as any)]
+    create: [
+      iff(isProvider('external'), verifyScope('location', 'write')),
+      () => schemaHooks.validateData(locationDataValidator),
+      schemaHooks.resolveData(locationDataResolver)
+    ],
+    update: [iff(isProvider('external'), verifyScope('location', 'write'))],
+    patch: [
+      iff(isProvider('external'), verifyScope('location', 'write')),
+      () => schemaHooks.validateData(locationPatchValidator),
+      schemaHooks.resolveData(locationPatchResolver)
+    ],
+    remove: [iff(isProvider('external'), verifyScope('location', 'write'))]
   },
 
   after: {
