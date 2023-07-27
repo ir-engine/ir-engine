e00cbaec622b1f5c0034ca577d46cdd7c56361b1
Gheric Speiginer <gheric.speiginer@gmail.com>
2023-07-27 11:44:13 -0700

    License headers

--- packages/engine/src/camera/systems/SplineCameraSystem.ts
+++ packages/engine/src/camera/systems/SplineCameraSystem.ts
@@ -1,15 +1,38 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
 import * as gl from 'gl-matrix'
-import { ArrowHelper, Euler, Plane, Quaternion, Vector3 } from 'three'
+import { ArrowHelper, Quaternion } from 'three'
 
 import { World } from '@xrengine/engine/src/ecs/classes/World'
 
-import { ObjectDirection } from '../../common/constants/Axis3D'
-import { V_000, V_001, V_010, V_100 } from '../../common/constants/MathConstants'
+import { V_100 } from '../../common/constants/MathConstants'
 import { defineQuery, getComponent, getComponentState } from '../../ecs/functions/ComponentFunctions'
 import { CameraTrackComponent } from '../../scene/components/CameraTrackComponent'
 import { SplineComponent } from '../../scene/components/SplineComponent'
 import { VisibleComponent } from '../../scene/components/VisibleComponent'
-import { setComputedTransformComponent } from '../../transform/components/ComputedTransformComponent'
 import { LocalTransformComponent, TransformComponent } from '../../transform/components/TransformComponent'
 
 export default async function SplineCameraSystem(world: World) {
