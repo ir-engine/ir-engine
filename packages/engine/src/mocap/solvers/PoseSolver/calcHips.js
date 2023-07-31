1444ba2aeaaddc9eb4af73a5f0033f86374e5ab8
jerknose <travis@travisbennett.com>
2023-07-31 14:18:13 -0700

    reverse

--- packages/engine/src/mocap/solvers/PoseSolver/calcHips.js
+++ packages/engine/src/mocap/solvers/PoseSolver/calcHips.js
@@ -1,3 +1,29 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+
 import Vector from "../utils/vector";
 import { clamp, remap } from "../utils/helpers";
 import { PI } from "./../constants";
@@ -12,8 +38,8 @@ export const calcHips = (lm3d, lm2d) => {
     const hipRight2d = Vector.fromArray(lm2d[24]);
     const shoulderLeft2d = Vector.fromArray(lm2d[11]);
     const shoulderRight2d = Vector.fromArray(lm2d[12]);
-    const hipCenter2d = hipLeft2d.lerp(hipRight2d, 1);
-    const shoulderCenter2d = shoulderLeft2d.lerp(shoulderRight2d, 1);
+    const hipCenter2d = hipLeft2d.lerp(hipRight2d, 0.5);
+    const shoulderCenter2d = shoulderLeft2d.lerp(shoulderRight2d, 0.5);
     const spineLength = hipCenter2d.distance(shoulderCenter2d);
     const hips = {
         position: {
