540cc0607250362142f1dd7f86e6d23e6b238c56
Hurairah Mateen <hurairahmateen@live.com>
2023-08-01 06:09:31 +0500

    Cleanup of instanceserver-subdomain-provision service (#8398)
    * Initial Commit
    
    * Updated files
    
    * Removed route53
    
    * Removed some lingering references to Route53 and instanceserver subdomains
    
    ---------
    
    Co-authored-by: Kyle Baran <kbaran@bitscoop.com>

--- packages/server-core/src/setting/aws-setting/migrations/20230731072108_drop-route53-column.ts
+++ packages/server-core/src/setting/aws-setting/migrations/20230731072108_drop-route53-column.ts
@@ -23,9 +23,34 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-export interface InstanceserverSubdomainProvisionInterface {
-  id: number
-  is_id: string
-  is_number: string
-  allocated: boolean
+import type { Knex } from 'knex'
+
+import { awsSettingPath } from '@etherealengine/engine/src/schemas/setting/aws-setting.schema'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex: Knex): Promise<void> {
+  const route53ColumnExists = await knex.schema.hasColumn(awsSettingPath, 'route53')
+
+  if (route53ColumnExists) {
+    await knex.schema.alterTable(awsSettingPath, async (table) => {
+      table.dropColumn('route53')
+    })
+  }
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex: Knex): Promise<void> {
+  const route53ColumnExists = await knex.schema.hasColumn(awsSettingPath, 'route53')
+
+  if (!route53ColumnExists) {
+    await knex.schema.alterTable(awsSettingPath, async (table) => {
+      table.json('route53').nullable()
+    })
+  }
 }
