042ffa1ee40bb24267f592de333cb5bf848ef818
Hanzla Mateen <hanzlamateen@live.com>
2023-08-01 21:59:06 +0500

    Fixed location model usage in instance service (#8408)

--- packages/server-core/src/networking/instance/instance.service.ts
+++ packages/server-core/src/networking/instance/instance.service.ts
@@ -114,13 +114,7 @@ export const getActiveInstancesForUserFriends = (app: Application) => async (dat
     const instances = (await app.service('instance').Model.findAll({
       where: {
         ended: false
-      },
-      include: [
-        {
-          model: app.service('location').Model,
-          required: true
-        }
-      ]
+      }
     })) as InstanceInterface[]
 
     const filteredInstances = (
@@ -159,6 +153,28 @@ export const getActiveInstancesForUserFriends = (app: Application) => async (dat
         })
       )
     ).filter(Boolean)
+
+    // TODO: Populating location property here manually. Once instance service is moved to feathers 5. This should be part of its resolver.
+
+    const locationIds = filteredInstances
+      .map((instance) => (instance?.locationId ? instance.locationId : undefined))
+      .filter((instance) => instance !== undefined) as string[]
+
+    const locations = (await app.service(locationPath)._find({
+      query: {
+        id: {
+          $in: locationIds
+        }
+      },
+      paginate: false
+    })) as LocationType[]
+
+    for (const instance of filteredInstances) {
+      if (instance && instance.locationId) {
+        instance.location = locations.find((item) => item.id === instance.locationId)!
+      }
+    }
+
     return filteredInstances
   } catch (err) {
     console.log(err)
