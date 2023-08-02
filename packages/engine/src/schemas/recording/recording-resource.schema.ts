91e336f9b33d8ea4fd929cf36587593bb0ddf884
MoizAdnan <moizadnan21@gmail.com>
2023-08-02 14:41:57 +0500

    Created migrations

---
+++ packages/engine/src/schemas/recording/recording-resource.schema.ts
@@ -0,0 +1,81 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import type { Static } from '@feathersjs/typebox'
+import { querySyntax, Type } from '@feathersjs/typebox'
+
+export const recordingResourcePath = 'recording-resource'
+
+export const recordingResourceMethods = ['find', 'get', 'create', 'patch', 'remove'] as const
+
+// Main data model schema
+export const recordingResourceSchema = Type.Object(
+  {
+    id: Type.String({
+      format: 'uuid'
+    }),
+    paymentPointer: Type.String(),
+    clientId: Type.String(),
+    clientSecret: Type.String(),
+    createdAt: Type.String({ format: 'date-time' }),
+    updatedAt: Type.String({ format: 'date-time' })
+  },
+  { $id: 'RecordingResource', additionalProperties: false }
+)
+export type RecordingResourceType = Static<typeof recordingResourceSchema>
+
+// Schema for creating new entries
+export const recordingResourceDataSchema = Type.Pick(
+  recordingResourceSchema,
+  ['paymentPointer', 'clientId', 'clientSecret'],
+  {
+    $id: 'RecordingResourceData'
+  }
+)
+export type RecordingResourceData = Static<typeof recordingResourceDataSchema>
+
+// Schema for updating existing entries
+export const recordingResourcePatchSchema = Type.Partial(recordingResourceSchema, {
+  $id: 'RecordingResourcePatch'
+})
+export type RecordingResourcePatch = Static<typeof recordingResourcePatchSchema>
+
+// Schema for allowed query properties
+export const recordingResourceQueryProperties = Type.Pick(recordingResourceSchema, [
+  'id',
+  'paymentPointer',
+  'clientId',
+  'clientSecret'
+])
+export const recordingResourceQuerySchema = Type.Intersect(
+  [
+    querySyntax(recordingResourceQueryProperties),
+    // Add additional query properties here
+    Type.Object({}, { additionalProperties: false })
+  ],
+  { additionalProperties: false }
+)
+export type RecordingResourceQuery = Static<typeof recordingResourceQuerySchema>
