c85f53fb22943d808a5e2e17e2501fbdb9642436
MoizAdnan <moizadnan21@gmail.com>
2023-08-03 17:38:04 +0500

    Added schema file

--- packages/engine/src/common/types/TypeboxUtils.ts
+++ packages/engine/src/common/types/TypeboxUtils.ts
@@ -23,6 +23,16 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-import { OpaqueType } from './OpaqueType'
+import { Kind, StringFormatOption, StringOptions, TSchema, Type } from '@feathersjs/typebox'
 
-export type RecordingID = OpaqueType<'RecordingID'> & string
+export interface TTypedString<T extends string, Format extends string = string> extends TSchema, StringOptions<Format> {
+  [Kind]: 'String'
+  static: T
+  type: T
+}
+
+export const TypedString = <T extends string, Format extends string = string>(
+  options?: StringOptions<StringFormatOption | Format>
+) => {
+  return Type.String(options) as TTypedString<T, Format>
+}
---
+++ packages/engine/src/schemas/recording/recording.schema.ts
@@ -0,0 +1,75 @@
+/*
+CPAL-1.0 License
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+The Original Code is Ethereal Engine.
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import { OpaqueType } from '@etherealengine/common/src/interfaces/OpaqueType'
+import { UserId } from '@etherealengine/common/src/interfaces/UserId'
+import type { Static } from '@feathersjs/typebox'
+import { querySyntax, Type } from '@feathersjs/typebox'
+import { TypedString } from '../../common/types/TypeboxUtils'
+
+export const recordingPath = 'recording'
+
+export const recordingMethods = ['find', 'get', 'create', 'patch', 'remove'] as const
+
+export type RecordingID = OpaqueType<'RecordingID'> & string
+
+// Main data model schema
+export const recordingSchema = Type.Object(
+  {
+    id: TypedString<RecordingID, 'uuid'>({
+      format: 'uuid'
+    }),
+    ended: Type.Boolean(),
+    schema: Type.String(),
+    userId: TypedString<UserId, 'uuid'>({
+      format: 'uuid'
+    }),
+
+    createdAt: Type.String({ format: 'date-time' }),
+    updatedAt: Type.String({ format: 'date-time' })
+  },
+  { $id: 'Recording', additionalProperties: false }
+)
+export type RecordingType = Static<typeof recordingSchema>
+
+// Schema for creating new entries
+export const recordingDataSchema = Type.Pick(recordingSchema, ['userId', 'ended', 'schema'], {
+  $id: 'RecordingData'
+})
+export type RecordingData = Static<typeof recordingDataSchema>
+
+// Schema for updating existing entries
+export const recordingPatchSchema = Type.Partial(recordingSchema, {
+  $id: 'RecordingPatch'
+})
+export type RecordingPatch = Static<typeof recordingPatchSchema>
+
+// Schema for allowed query properties
+export const recordingQueryProperties = Type.Pick(recordingSchema, ['id', 'userId'])
+export const recordingQuerySchema = Type.Intersect(
+  [
+    querySyntax(recordingQueryProperties),
+    // Add additional query properties here
+    Type.Object({}, { additionalProperties: false })
+  ],
+  { additionalProperties: false }
+)
+export type RecordingQuery = Static<typeof recordingQuerySchema>
