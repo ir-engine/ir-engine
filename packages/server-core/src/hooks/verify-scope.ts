15ee2e1fa506440e6b643fa018ba489a103c5f6b
hurairahmateen <hurairahmateen@live.com>
2023-08-17 14:29:40 +0500

    updated type

--- packages/server-core/src/hooks/verify-scope.ts
+++ packages/server-core/src/hooks/verify-scope.ts
@@ -44,7 +44,7 @@ export default (currentType: string, scopeToVerify: string) => {
     })) as ScopeType[]
     if (!scopes || scopes.length === 0) throw new NotFound('No scope available for the current user.')
 
-    const currentScopes = scopes.reduce((result, sc) => {
+    const currentScopes = scopes.reduce<string[]>((result, sc) => {
       if (sc.type.split(':')[0] === currentType) result.push(sc.type.split(':')[1])
       return result
     }, [])
@@ -65,7 +65,7 @@ export const checkScope = async (user: UserType, app: Application, currentType: 
   })) as ScopeType[]
   if (!scopes || scopes.length === 0) throw new NotFound('No scope available for the current user.')
 
-  const currentScopes = scopes.reduce((result, sc) => {
+  const currentScopes = scopes.reduce<string[]>((result, sc) => {
     if (sc.type.split(':')[0] === currentType) result.push(sc.type.split(':')[1])
     return result
   }, [])
