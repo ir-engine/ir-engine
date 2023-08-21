43d91ced4455d75ce98bed0ebf61c56f090f0ba7
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-08-21 20:52:17 +0530

    feat: labelled actions panel

--- packages/client-core/src/components/Debug/index.tsx
+++ packages/client-core/src/components/Debug/index.tsx
@@ -47,9 +47,10 @@ import { NetworkState } from '@etherealengine/engine/src/networking/NetworkState
 import { RendererState } from '@etherealengine/engine/src/renderer/RendererState'
 import { NameComponent } from '@etherealengine/engine/src/scene/components/NameComponent'
 import { UUIDComponent } from '@etherealengine/engine/src/scene/components/UUIDComponent'
-import { getMutableState, useHookstate } from '@etherealengine/hyperflux'
+import { NO_PROXY, getMutableState, useHookstate } from '@etherealengine/hyperflux'
 import Icon from '@etherealengine/ui/src/primitives/mui/Icon'
 
+import ActionsPanel from './ActionsPanel'
 import { StatsPanel } from './StatsPanel'
 import styles from './styles.module.scss'
 
@@ -105,10 +106,13 @@ const convertSystemTypeToDesiredType = (system: System): DesiredType => {
   return desired
 }
 
-export const Debug = ({ showingStateRef }) => {
+export const Debug = ({ showingStateRef }: { showingStateRef: React.MutableRefObject<boolean> }) => {
   useHookstate(getMutableState(EngineState).frameTime).value
   const rendererState = useHookstate(getMutableState(RendererState))
   const engineState = useHookstate(getMutableState(EngineState))
+
+  engineState.frameTime.value // make Engine.instance data reactive in the render tree
+
   const { t } = useTranslation()
   const hasActiveControlledAvatar =
     Engine.instance.localClientEntity &&
@@ -255,9 +259,10 @@ export const Debug = ({ showingStateRef }) => {
         </div>
       </div>
       <StatsPanel show={showingStateRef.current} />
+      <ActionsPanel />
       <div className={styles.jsonPanel}>
         <h1>{t('common:debug.entities')}</h1>
-        <JSONTree data={namedEntities.get({ noproxy: true })} />
+        <JSONTree data={namedEntities.get(NO_PROXY)} />
       </div>
       <div className={styles.jsonPanel}>
         <h1>{t('common:debug.entityTree')}</h1>
@@ -273,7 +278,7 @@ export const Debug = ({ showingStateRef }) => {
         <h1>{t('common:debug.state')}</h1>
         <JSONTree
           data={Engine.instance.store.stateMap}
-          postprocessValue={(v: any) => (v?.value && v?.get({ noproxy: true })) ?? v}
+          postprocessValue={(v: any) => (v?.value && v?.get(NO_PROXY)) ?? v}
         />
       </div>
       <div className={styles.jsonPanel}>
