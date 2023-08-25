6a5265641c23197539df8805befee0d13968b439
hanzlamateen <hanzlamateen@live.com>
2023-08-25 15:59:44 +0500

    Updated migration to ignore foreign key constrain

--- packages/server-core/src/social/location-type/migrations/20230620080804_location-type.ts
+++ packages/server-core/src/social/location-type/migrations/20230620080804_location-type.ts
@@ -54,9 +54,15 @@ export async function up(knex: Knex): Promise<void> {
  * @returns { Promise<void> }
  */
 export async function down(knex: Knex): Promise<void> {
-  const tableExists = await knex.schema.hasTable(locationTypePath)
+  const trx = await knex.transaction()
+  await trx.raw('SET FOREIGN_KEY_CHECKS=0')
+
+  const tableExists = await trx.schema.hasTable(locationTypePath)
 
   if (tableExists === true) {
-    await knex.schema.dropTable(locationTypePath)
+    await trx.schema.dropTable(locationTypePath)
   }
+
+  await trx.raw('SET FOREIGN_KEY_CHECKS=1')
+  await trx.commit()
 }
