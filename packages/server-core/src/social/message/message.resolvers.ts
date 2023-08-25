cb5d5e8cf1360fc23e8c73fee4e13337b6d97079
hurairahmateen <hurairahmateen@live.com>
2023-08-25 12:50:25 +0500

    create .schema and .resolvers file. updated .docs file

--- packages/server-core/src/social/message/message.resolvers.ts
+++ packages/server-core/src/social/message/message.resolvers.ts
@@ -23,8 +23,29 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-export interface MessageInterface {
-  id: string
-  text: string
-  isNotification: boolean
-}
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import { resolve } from '@feathersjs/schema'
+import { v4 } from 'uuid'
+
+import { MessageQuery, MessageType } from '@etherealengine/engine/src/schemas/social/message.schema'
+import type { HookContext } from '@etherealengine/server-core/declarations'
+
+import { getDateTimeSql } from '../../util/get-datetime-sql'
+
+export const messageResolver = resolve<MessageType, HookContext>({})
+
+export const messageExternalResolver = resolve<MessageType, HookContext>({})
+
+export const messageDataResolver = resolve<MessageType, HookContext>({
+  id: async () => {
+    return v4()
+  },
+  createdAt: getDateTimeSql,
+  updatedAt: getDateTimeSql
+})
+
+export const messagePatchResolver = resolve<MessageType, HookContext>({
+  updatedAt: getDateTimeSql
+})
+
+export const messageQueryResolver = resolve<MessageQuery, HookContext>({})
