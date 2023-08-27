c2c716f489f7cdeaaee3797cfdcdab86fad620fc
Hanzla Mateen <hanzlamateen@live.com>
2023-08-27 13:06:04 +0500

    Updated dev-reinit to recreate knex tables. (#8588)
    * Added rollback command
    
    * Updated migration to ignore foreign key constrain
    
    * Added migrate:rollback in dev-reinit method
    
    * Fixed error code being returned as 1

--- packages/server-core/src/user/login-token/migrations/20230809103211_login-token.ts
+++ packages/server-core/src/user/login-token/migrations/20230809103211_login-token.ts
@@ -78,9 +78,15 @@ export async function up(knex: Knex): Promise<void> {
  * @returns { Promise<void> }
  */
 export async function down(knex: Knex): Promise<void> {
-  const tableExists = await knex.schema.hasTable(loginTokenPath)
+  const trx = await knex.transaction()
+  await trx.raw('SET FOREIGN_KEY_CHECKS=0')
+
+  const tableExists = await trx.schema.hasTable(loginTokenPath)
 
   if (tableExists === true) {
-    await knex.schema.dropTable(loginTokenPath)
+    await trx.schema.dropTable(loginTokenPath)
   }
+
+  await trx.raw('SET FOREIGN_KEY_CHECKS=1')
+  await trx.commit()
 }
