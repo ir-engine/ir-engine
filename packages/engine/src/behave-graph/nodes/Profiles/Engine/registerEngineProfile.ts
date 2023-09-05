c6c8fd8ca5ef7de1d04d98bef02b260648700709
sybiote <ghoshr698@gmail.com>
2023-09-05 08:12:26 +0530

    action nodes

--- packages/engine/src/behave-graph/nodes/Profiles/Engine/registerEngineProfile.ts
+++ packages/engine/src/behave-graph/nodes/Profiles/Engine/registerEngineProfile.ts
@@ -40,6 +40,7 @@ import * as ComponentNodes from './Values/ComponentNodes'
 import * as CustomNodes from './Values/CustomNodes'
 import * as EntityNodes from './Values/EntityNodes'
 import { EntityValue } from './Values/EntityValue'
+import { getActionDispatchers } from './helper/actionHelper'
 import { getComponentGetters, getComponentSetters } from './helper/componentHelper'
 import { getStateGetters, getStateSetters } from './helper/stateHelper'
 
@@ -86,7 +87,8 @@ export const getEngineNodesMap = memo<Record<string, NodeDefinition>>(() => {
     ...getComponentSetters(),
     ...getComponentGetters(),
     ...getStateSetters(),
-    ...getStateGetters()
+    ...getStateGetters(),
+    ...getActionDispatchers()
   ]
   return Object.fromEntries(nodeDefinitions.map((nodeDefinition) => [nodeDefinition.typeName, nodeDefinition]))
 })
