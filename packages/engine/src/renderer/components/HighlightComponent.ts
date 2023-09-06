adb6ad2832d3f948234481f62ca935febd5df088
Daniel Belmes <3631206+DanielBelmes@users.noreply.github.com>
2023-09-06 13:19:43 -0700

    Add back animation for scale through tween system

--- packages/engine/src/renderer/components/HighlightComponent.ts
+++ packages/engine/src/renderer/components/HighlightComponent.ts
@@ -28,9 +28,18 @@ import { Mesh, Vector3 } from 'three'
 
 import { getMutableState, useHookstate } from '@etherealengine/hyperflux'
 
-import { defineComponent, useComponent } from '../../ecs/functions/ComponentFunctions'
+import { Tween } from '@tweenjs/tween.js'
+import { Entity } from '../../ecs/classes/Entity'
+import {
+  defineComponent,
+  getComponent,
+  removeComponent,
+  setComponent,
+  useComponent
+} from '../../ecs/functions/ComponentFunctions'
 import { useEntityContext } from '../../ecs/functions/EntityFunctions'
 import { GroupComponent } from '../../scene/components/GroupComponent'
+import { TweenComponent } from '../../transform/components/TweenComponent'
 import { RendererState } from '../RendererState'
 import { EngineRenderer, PostProcessingSettingsState } from '../WebGLRendererSystem'
 
@@ -46,15 +55,21 @@ export const HighlightComponent = defineComponent({
 
     useEffect(() => {
       const objs = [...group.value]
-      for (const object of objs)
+      for (const object of objs) {
         object.traverse((obj) => {
-          obj.type === 'Mesh' && addToSelection(obj as Mesh)
+          if (obj.type !== 'Mesh') return
+          addToSelection(obj as Mesh)
+          animateScale(entity, obj as Mesh, true)
         })
+      }
       return () => {
-        for (const object of objs)
+        for (const object of objs) {
           object.traverse((obj) => {
-            obj.type === 'Mesh' && removeFromSelection(obj as Mesh)
+            if (obj.type !== 'Mesh') return
+            removeFromSelection(obj as Mesh)
+            animateScale(entity, obj as Mesh, false)
           })
+        }
       }
     }, [group, postProcessingSettingsState.effects, postProcessingSettingsState.enabled, usePostProcessing])
 
@@ -62,14 +77,36 @@ export const HighlightComponent = defineComponent({
   }
 })
 
+const animateScale = (entity: Entity, obj: Mesh, grow: boolean) => {
+  const highlight = { scaler: 0 }
+  const tween = getComponent(entity, TweenComponent)
+  setComponent(
+    entity,
+    TweenComponent,
+    new Tween<any>(highlight)
+      .to(
+        {
+          scaler: 1
+        },
+        300
+      )
+      .onUpdate(() => {
+        const v = grow ? new Vector3(1.05, 1.05, 1.05) : new Vector3(1, 1, 1)
+        obj.scale.lerp(v, highlight.scaler)
+      })
+      .start()
+      .onComplete(() => {
+        removeComponent(entity, TweenComponent)
+      })
+  )
+}
+
 const addToSelection = (obj: Mesh) => {
   if (!EngineRenderer.instance.effectComposer?.HighlightEffect) return
-  obj.scale.add(new Vector3(0.1, 0.1, 0.1))
   EngineRenderer.instance.effectComposer.HighlightEffect.selection.add(obj)
 }
 
 const removeFromSelection = (obj: Mesh) => {
   if (!EngineRenderer.instance.effectComposer?.HighlightEffect) return
-  obj.scale.add(new Vector3(-0.1, -0.1, -0.1))
   EngineRenderer.instance.effectComposer.HighlightEffect.selection.delete(obj)
 }
