b96947a3354010c389e9bf765bf7ea00d2bd966b
MoizAdnan <moizadnan21@gmail.com>
2023-09-12 12:53:45 +0500

    Removed settings in patch condition

--- packages/server-core/src/projects/project/project.class.ts
+++ packages/server-core/src/projects/project/project.class.ts
@@ -215,7 +215,7 @@ export class ProjectService<T = ProjectType, ServiceParams extends Params = Proj
   }
 
   async patch(id: NullableId, data: ProjectPatch, params?: ProjectParams) {
-    if (data.repositoryPath && !data.settings) {
+    if (data.repositoryPath) {
       const repoPath = data.repositoryPath
       const user = params!.user!
 
