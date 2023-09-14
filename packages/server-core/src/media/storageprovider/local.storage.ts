3ed0424b64f0d055041f84e8c666512c88c955ed
Kyle Baran <kbaran@bitscoop.com>
2023-09-13 17:57:07 -0700

    Fixed some bugs with the file browser and project paths.
    React-DND does not populate a type for GLBs. This was causing it to make
    them as folders, which was not correct. Could not figure out where the type
    was being populated from, so added a hard check on the file name to avoid
    this.
    
    Large file check on S3 putObject was not accounting for being handed a path
    with no Body. Made that Body.length check optional to avoid errors.
    Made that on local putObject as well just in case.
    
    downloadProjects was tying to write files even if their Body had no length,
    i.e. was a folder, which would error out.

--- packages/server-core/src/media/storageprovider/local.storage.ts
+++ packages/server-core/src/media/storageprovider/local.storage.ts
@@ -176,7 +176,7 @@ export class LocalStorage implements StorageProviderInterface {
           reject(e)
         }
       })
-    } else if (data.Body.length > MULTIPART_CUTOFF_SIZE) {
+    } else if (data.Body?.length > MULTIPART_CUTOFF_SIZE) {
       return new Promise<boolean>((resolve, reject) => {
         try {
           const writeableStream = fs.createWriteStream(filePath)
