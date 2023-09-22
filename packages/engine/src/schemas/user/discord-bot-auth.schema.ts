fa9f3cd56a45b2fdcbf4bb4fe58547edf1f80688
MoizAdnan <moizadnan21@gmail.com>
2023-09-22 08:19:32 +0500

    Migrated discord-bot-auth service to feathers 5

--- packages/engine/src/schemas/user/discord-bot-auth.schema.ts
+++ packages/engine/src/schemas/user/discord-bot-auth.schema.ts
@@ -23,24 +23,8 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-import { Application } from '../../../declarations'
-import { DicscordBotAuth } from './discord-bot-auth.class'
-import hooks from './discord-bot-auth.hooks'
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
 
-declare module '@etherealengine/common/declarations' {
-  interface ServiceTypes {
-    'discord-bot-auth': DicscordBotAuth
-  }
-}
+export const discordBotAuthPath = 'discord-bot-auth'
 
-export default (app: Application) => {
-  const options = {
-    paginate: app.get('paginate')
-  }
-
-  const event = new DicscordBotAuth(options, app)
-  app.use('discord-bot-auth', event)
-
-  const service = app.service('discord-bot-auth')
-  service.hooks(hooks)
-}
+export const discordBotAuthMethods = ['find'] as const
