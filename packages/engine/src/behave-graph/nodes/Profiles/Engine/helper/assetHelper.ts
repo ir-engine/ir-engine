3b2d1ceadb3239c71e8e9bc880e5320269d9ab4e
David Gordon <94419856+dinomut1@users.noreply.github.com>
2023-09-25 13:25:52 -0700

    Transform system fixes (#8874)
    * serialize and deserialize local transform only
    
    * fix new editor nodes have no transform
    
    * clean up parentEntity references
    
    * refactoring old local transform calls
    
    * change setTransformComponent calls to setComponent
    
    * fix missing local transform in some cases
    
    * fix widgets missing local transform
    
    * remove comment
    
    ---------
    
    Co-authored-by: Gheric Speiginer <gheric.speiginer@gmail.com>
    Co-authored-by: HexaField <joshfield999@gmail.com>

--- packages/engine/src/behave-graph/nodes/Profiles/Engine/helper/assetHelper.ts
+++ packages/engine/src/behave-graph/nodes/Profiles/Engine/helper/assetHelper.ts
@@ -30,7 +30,6 @@ import { setComponent } from '../../../../../ecs/functions/ComponentFunctions'
 import { ImageComponent } from '../../../../../scene/components/ImageComponent'
 import { MediaComponent } from '../../../../../scene/components/MediaComponent'
 import { ModelComponent } from '../../../../../scene/components/ModelComponent'
-import { PrefabComponent } from '../../../../../scene/components/PrefabComponent'
 import { VideoComponent } from '../../../../../scene/components/VideoComponent'
 import { VolumetricComponent } from '../../../../../scene/components/VolumetricComponent'
 import { addEntityToScene } from './entityHelper'
@@ -44,10 +43,7 @@ export async function addMediaComponent(url: string, parent?: Entity | null, bef
 
   let node: Entity | null = null
 
-  if (contentType.startsWith('prefab/')) {
-    componentName = PrefabComponent.name
-    updateFunc = () => setComponent(node!, PrefabComponent, { src: url })
-  } else if (contentType.startsWith('model/')) {
+  if (contentType.startsWith('model/')) {
     componentName = ModelComponent.name
     updateFunc = () => setComponent(node!, ModelComponent, { src: url })
   } else if (contentType.startsWith('video/') || hostname.includes('twitch.tv') || hostname.includes('youtube.com')) {
