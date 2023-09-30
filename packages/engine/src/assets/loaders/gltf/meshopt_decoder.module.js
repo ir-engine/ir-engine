cbc1e17e6f2abd1359992873bd459d635806bcf1
Appaji <appaji12368@gmail.com>
2023-09-30 19:34:49 +0530

    Update meshopt decoder module
    `meshopt_decoder.module.js` stringifies some functions and spawns workers.
    Production build optimizes these files and changes the function names, which causes issues.
    It's better if we just hand over the function source, instead of stringifying.

--- packages/engine/src/assets/loaders/gltf/meshopt_decoder.module.js
+++ packages/engine/src/assets/loaders/gltf/meshopt_decoder.module.js
@@ -98,8 +98,36 @@ var MeshoptDecoder = (function() {
 		var source =
 			"var instance; var ready = WebAssembly.instantiate(new Uint8Array([" + new Uint8Array(unpack(wasm)) + "]), {})" +
 			".then(function(result) { instance = result.instance; instance.exports.__wasm_call_ctors(); });" +
-			"self.onmessage = workerProcess;" +
-			decode.toString() + workerProcess.toString();
+			"self.onmessage = workerProcess;" + 
+			`function decode(fun, target, count, size, source, filter) {
+				var sbrk = instance.exports.sbrk;
+				var count4 = (count + 3) & ~3;
+				var tp = sbrk(count4 * size);
+				var sp = sbrk(source.length);
+				var heap = new Uint8Array(instance.exports.memory.buffer);
+				heap.set(source, sp);
+				var res = fun(tp, count, size, sp, source.length);
+				if (res == 0 && filter) {
+					filter(tp, count4, size);
+				}
+				target.set(heap.subarray(tp, tp + count * size));
+				sbrk(tp - sbrk(0));
+				if (res != 0) {
+					throw new Error("Malformed buffer data: " + res);
+				}
+			}` +
+		`function workerProcess(event) {
+			ready.then(function() {
+				var data = event.data;
+				try {
+					var target = new Uint8Array(data.count * data.size);
+					decode(instance.exports[data.mode], target, data.count, data.size, data.source, instance.exports[data.filter]);
+					self.postMessage({ id: data.id, count: data.count, action: "resolve", value: target }, [ target.buffer ]);
+				} catch (error) {
+					self.postMessage({ id: data.id, count: data.count, action: "reject", value: error });
+				}
+			});
+		}`;
 
 		var blob = new Blob([source], {type: 'text/javascript'});
 		var url = URL.createObjectURL(blob);
