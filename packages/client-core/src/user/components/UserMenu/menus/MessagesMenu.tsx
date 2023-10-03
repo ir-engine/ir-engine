7ee15ad697ebca8ce42f8a6b385806bad7c2f9f8
hanzlamateen <hanzlamateen@live.com>
2023-10-03 20:42:09 +0500

    Fixed message menu text placeholder

--- packages/client-core/src/user/components/UserMenu/menus/MessagesMenu.tsx
+++ packages/client-core/src/user/components/UserMenu/menus/MessagesMenu.tsx
@@ -148,7 +148,7 @@ const MessagesMenu = (props: { channelID: ChannelID; name: string }): JSX.Elemen
               src={userThumbnail}
             />
           }
-          placeholder={''}
+          placeholder={t('user:messages.enterMessage')}
           sx={{ mb: 1, mt: 0 }}
           value={composingMessage.value}
           onChange={(e) => composingMessage.set(e.target.value)}
