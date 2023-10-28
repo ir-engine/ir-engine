8ebb6b1f47fb87e2a4cb227eba89a0795bc385a3
Josh Field <joshfield999@gmail.com>
2023-10-29 09:10:00 +1100

    AR, Camera Mode and Scene Scale Fixes (#9117)
    * fix avatar disconnect mobile ar and fix scaling of world and avatars independently
    
    * fixes
    
    * fix placement, input and a few other things
    
    * fixes
    
    * fix mobile input
    
    * make movement state reactive and hide touchpad when lifescale XR
    
    * fix movements and auto scaling
    
    * add manual scaling mode for mobile AR
    
    * fix testing changes, combine user scale states
    
    * add todo
    
    * add todo
    
    * update tracking space upon avatar model change
    
    * add true arg to head decap setcomponent
    
    * immersive ar fixes
    
    * fix placement material change
    
    * finalize immersive AR miniature mode
    
    * cleanup API
    
    ---------
    
    Co-authored-by: AidanCaruso <jamesajcme@gmail.com>

--- packages/engine/src/xr/XRState.ts
+++ packages/engine/src/xr/XRState.ts
@@ -26,8 +26,9 @@ Ethereal Engine. All Rights Reserved.
 import { Quaternion, Vector3 } from 'three'
 import matches from 'ts-matches'
 
-import { defineAction, defineState, getState, syncStateWithLocalStorage } from '@etherealengine/hyperflux'
+import { defineAction, defineState, getMutableState, getState, useHookstate } from '@etherealengine/hyperflux'
 
+import { useEffect } from 'react'
 import { AvatarInputSettingsState } from '../avatar/state/AvatarInputSettingsState'
 import { Entity } from '../ecs/classes/Entity'
 import { defineQuery, getComponent } from '../ecs/functions/ComponentFunctions'
@@ -49,6 +50,7 @@ export class XRAction {
     duration: matches.number
   })
 }
+
 // TODO: divide this up into the systems that manage these states
 export const XRState = defineState({
   name: 'XRState',
@@ -59,6 +61,8 @@ export const XRState = defineState({
       scenePosition: new Vector3(),
       sceneRotation: new Quaternion(),
       sceneScale: 1,
+      sceneScaleAutoMode: true,
+      sceneScaleTarget: 1,
       sceneRotationOffset: 0,
       scenePlacementMode: 'unplaced' as 'unplaced' | 'placing' | 'placed',
       supportedSessionModes: {
@@ -69,27 +73,98 @@ export const XRState = defineState({
       unassingedInputSources: [] as XRInputSource[],
       session: null as XRSession | null,
       sessionMode: 'none' as 'inline' | 'immersive-ar' | 'immersive-vr' | 'none',
-      avatarCameraMode: 'auto' as 'auto' | 'attached' | 'detached',
-      userAvatarHeightDifference: null as number | null,
       /** Stores the depth map data - will exist if depth map is supported */
       depthDataTexture: null as DepthDataTexture | null,
       is8thWallActive: false,
       viewerInputSourceEntity: 0 as Entity,
       viewerPose: null as XRViewerPose | null | undefined,
-      userEyeLevel: 1.8,
-      //to be moved to user_settings
-      userHeight: 0,
+      userAvatarHeightScale: 1,
       xrFrame: null as XRFrame | null
     }
   },
 
-  onCreate: (store, state) => {
-    syncStateWithLocalStorage(XRState, [
-      /** @todo replace this wither user_settings table entry */
-      'userEyeLevel'
-    ])
+  /**
+   * Gets the world scale according to avatar scaling factor and scene scale
+   * - divide any world-space distances by this to get the real-world distance
+   * @todo - can we think of a better name for this?
+   * @returns {number} the world scale
+   */
+  get worldScale(): number {
+    const { sceneScale, userAvatarHeightScale } = getState(XRState)
+    return sceneScale * userAvatarHeightScale
+  },
+
+  /**
+   * Gets the preferred controller entity - will return null if the entity is not in an active session or the controller is not available
+   * @param {boolean} offhand specifies to return the non-preferred hand instead
+   * @returns {Entity}
+   */
+  getPreferredInputSource: (offhand = false) => {
+    const xrState = getState(XRState)
+    if (!xrState.sessionActive) return
+    const avatarInputSettings = getState(AvatarInputSettingsState)
+    for (const inputSourceEntity of inputSourceQuery()) {
+      const inputSourceComponent = getComponent(inputSourceEntity, InputSourceComponent)
+      const source = inputSourceComponent.source
+      if (source.handedness === 'none') continue
+      if (!offhand && avatarInputSettings.preferredHand == source.handedness) return source
+      if (offhand && avatarInputSettings.preferredHand !== source.handedness) return source
+    }
   }
 })
+
+export const XRControlsState = defineState({
+  name: 'XRControlsState',
+  initial: () => ({
+    /**
+     * Specifies that the user has movement controls if:
+     * - in an immersive session
+     * - or in an immersive session with a world-space interaction mode
+     * - or in an immersive-ar and dollhouse mode
+     */
+    isMovementControlsEnabled: true,
+    /**
+     * Specifies that the camera is attached to the avatar if:
+     * - in an immersion session and not in placement mode or miniature mode
+     */
+    isCameraAttachedToAvatar: true,
+
+    avatarCameraMode: 'auto' as 'auto' | 'attached' | 'detached'
+  })
+})
+
+export const useXRMovement = () => {
+  const xrMovementState = getMutableState(XRControlsState)
+  const avatarCameraMode = useHookstate(xrMovementState.avatarCameraMode)
+
+  const xrState = getMutableState(XRState)
+  const sceneScale = useHookstate(xrState.sceneScale)
+  const scenePlacementMode = useHookstate(xrState.scenePlacementMode)
+  const session = useHookstate(xrState.session)
+
+  const getAvatarCameraMode = () => {
+    if (!session.value || scenePlacementMode.value === 'placing') return false
+    if (avatarCameraMode.value === 'auto') {
+      return sceneScale.value === 1
+    }
+    return avatarCameraMode.value === 'attached'
+  }
+
+  useEffect(() => {
+    xrMovementState.isCameraAttachedToAvatar.set(getAvatarCameraMode())
+  }, [avatarCameraMode, sceneScale, scenePlacementMode, session])
+
+  const getMovementControlsEnabled = () => {
+    const { sessionActive, sceneScale, sessionMode } = getState(XRState)
+    if (!sessionActive) return true
+    const isMiniatureScale = sceneScale !== 1
+    return sessionMode === 'immersive-ar' ? isMiniatureScale : true
+  }
+
+  useEffect(() => {
+    xrMovementState.isMovementControlsEnabled.set(getMovementControlsEnabled())
+  }, [sceneScale, scenePlacementMode])
+}
 
 export const ReferenceSpace = {
   /**
@@ -107,53 +182,7 @@ export const ReferenceSpace = {
 }
 globalThis.ReferenceSpace = ReferenceSpace
 
-/**
- * Gets the camera mode - either 'attached' or 'detached'
- * @returns the camera mode
- */
-export const getCameraMode = () => {
-  const { avatarCameraMode, sceneScale, scenePlacementMode, session, sessionMode } = getState(XRState)
-  if (!session || scenePlacementMode === 'placing') return 'detached'
-  if (avatarCameraMode === 'auto') {
-    if (session.interactionMode === 'screen-space') return 'detached'
-    return sceneScale === 1 || sessionMode == 'immersive-vr' ? 'attached' : 'detached'
-  }
-  return avatarCameraMode
-}
-
-/**
- * Specifies that the user has movement controls if:
- * - they are not in an immersive session
- * - they are in an immersive session with a world-space interaction mode
- * - they are in an immersive-ar session with a scene scale of 1
- * @returns {boolean} true if the user has movement controls
- */
-export const hasMovementControls = () => {
-  const { sessionActive, sceneScale, sessionMode, session } = getState(XRState)
-  if (!sessionActive) return true
-  if (session && session.interactionMode === 'world-space') return true
-  return sessionMode === 'immersive-ar' ? sceneScale !== 1 : true
-}
-
 const inputSourceQuery = defineQuery([InputSourceComponent])
-
-/**
- * Gets the preferred controller entity - will return null if the entity is not in an active session or the controller is not available
- * @param {boolean} offhand specifies to return the non-preferred hand instead
- * @returns {Entity}
- */
-export const getPreferredInputSource = (offhand = false) => {
-  const xrState = getState(XRState)
-  if (!xrState.sessionActive) return
-  const avatarInputSettings = getState(AvatarInputSettingsState)
-  for (const inputSourceEntity of inputSourceQuery()) {
-    const inputSourceComponent = getComponent(inputSourceEntity, InputSourceComponent)
-    const source = inputSourceComponent.source
-    if (source.handedness === 'none') continue
-    if (!offhand && avatarInputSettings.preferredHand == source.handedness) return source
-    if (offhand && avatarInputSettings.preferredHand !== source.handedness) return source
-  }
-}
 
 const userAgent = 'navigator' in globalThis ? navigator.userAgent : ''
 
