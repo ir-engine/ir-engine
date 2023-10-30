cfb03f4eacbcc9e2647c9911c4b1b30a46b13fe2
sybiote <ghoshr698@gmail.com>
2023-10-31 00:50:53 +0530

    remove minor ref error

--- packages/editor/src/components/inputs/NumericInput.tsx
+++ packages/editor/src/components/inputs/NumericInput.tsx
@@ -96,24 +96,27 @@ const StyledNumericInput = React.forwardRef(({ className = '', ...rest }: styled
   return <input ref={ref as any} className={`StyledNumericInput ${className}`} {...rest} />
 })
 const NumericInput = React.forwardRef(
-  ({
-    className,
-    unit,
-    prefix,
-    displayPrecision,
-    value,
-    convertFrom,
-    precision,
-    mediumStep,
-    onChange,
-    onRelease,
-    smallStep,
-    largeStep,
-    min,
-    max,
-    convertTo,
-    ...rest
-  }: NumericInputProp) => {
+  (
+    {
+      className,
+      unit,
+      prefix,
+      displayPrecision,
+      value,
+      convertFrom,
+      precision,
+      mediumStep,
+      onChange,
+      onRelease,
+      smallStep,
+      largeStep,
+      min,
+      max,
+      convertTo,
+      ...rest
+    }: NumericInputProp,
+    ref
+  ) => {
     const [tempValue, setTempValue] = useState<string | null>(null)
     const [focused, setFocused] = useState(false)
     const inputEl = useRef<HTMLInputElement>(null)
