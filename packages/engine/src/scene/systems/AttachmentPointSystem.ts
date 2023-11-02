415f301be5457d7805795c5d0911c7286db5ef72
Jiatong Yao <jiatong@ethereal.com>
2023-11-01 19:35:42 -0500

    update to AttachmentPointSystem

--- packages/engine/src/scene/systems/AttachmentPointSystem.ts
+++ packages/engine/src/scene/systems/AttachmentPointSystem.ts
@@ -23,9 +23,14 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
+import { hasComponent } from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
+import { EntityTreeComponent, iterateEntityNode } from '@etherealengine/engine/src/ecs/functions/EntityTree'
+import { getMutableState } from '@etherealengine/hyperflux'
 import { Vector3 } from 'three'
+import { SelectionState } from '../../../../../packages/editor/src/services/SelectionServices'
 import { Entity } from '../../ecs/classes/Entity'
-import { defineQuery, getComponent } from '../../ecs/functions/ComponentFunctions'
+import { defineQuery, getComponent, updateComponent } from '../../ecs/functions/ComponentFunctions'
+import { createEntity } from '../../ecs/functions/EntityFunctions'
 import { defineSystem } from '../../ecs/functions/SystemFunctions'
 import { TransformComponent } from '../../transform/components/TransformComponent'
 import { AttachmentPointComponent } from '../components/AttachmentPointComponent'
@@ -33,17 +38,63 @@ import { AttachmentPointComponent } from '../components/AttachmentPointComponent
 const execute = () => {
   const attachmentPointQuery = defineQuery([AttachmentPointComponent])
 
-  const existingPoints: Record<Entity, Vector3> = {}
-
-  for (const entity of attachmentPointQuery()) {
-    //get transform
-    const transform = getComponent(entity, TransformComponent)
-    // loop through existingPoints and check if any of them are close enough to attach to
-    // if so, attach to that point
-
-    //add transform to existingPoints
-    existingPoints[entity] = transform.position
+  function Distance(point1: Vector3, point2: Vector3) {
+    const dx = point2.x - point1.x
+    const dy = point2.y - point1.y
+    const dz = point2.z - point1.z
+    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz)
+    return distance
   }
+
+  //cauculate select entity
+  const selectionState = getMutableState(SelectionState) //access the list of currently selected entities
+  const select_entities = selectionState.selectedEntities.value.flatMap((entity: Entity) => {
+    return iterateEntityNode(
+      entity,
+      (e) => e,
+      (e) => hasComponent(e, AttachmentPointComponent)
+    )
+  })
+
+  //loop to caculate the distance
+  for (const select_entity of select_entities) {
+    const threshold = 1.0
+    let shortestDistance = Infinity
+    let distance
+    let node: Entity
+    node = createEntity()
+    const selectAttachmentPoint = getComponent(select_entity, TransformComponent)
+    const selectParententity = getComponent(select_entity, EntityTreeComponent).parentEntity
+    if (selectParententity) {
+      const selectTransform = getComponent(selectParententity, TransformComponent)
+
+      for (const entity of attachmentPointQuery()) {
+        const transform = getComponent(entity, TransformComponent)
+        const attachmentPoint = getComponent(entity, TransformComponent)
+        const Parententity = getComponent(select_entity, EntityTreeComponent).parentEntity
+        if (Parententity) {
+          const parentTransform = getComponent(Parententity, TransformComponent)
+          distance = Distance(
+            selectTransform.position.add(selectAttachmentPoint.position),
+            parentTransform.position.add(attachmentPoint.position)
+          )
+          if (distance < shortestDistance) {
+            shortestDistance = distance
+            node = entity
+            //}
+          }
+        }
+      }
+      if (shortestDistance < threshold) {
+        //offset between shortist attachment point and select point
+        const offset = getComponent(node, TransformComponent).position.sub(selectAttachmentPoint.position)
+        selectTransform.position.add(offset)
+        updateComponent(selectParententity, TransformComponent, selectTransform)
+      }
+    }
+  }
+
+  AttachmentPointSystem // if so, attach to that point
 }
 
 export const AttachmentPointSystem = defineSystem({
