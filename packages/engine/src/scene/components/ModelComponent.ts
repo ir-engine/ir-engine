0da64434b38928b4eb46449889d3751704574c13
Josh Field <joshfield999@gmail.com>
2023-11-04 02:45:17 +1100

    Compile Async for Model Component (#9203)
    * compile async for model component
    
    * old logic
    
    ---------
    
    Co-authored-by: David Gordon <94419856+dinomut1@users.noreply.github.com>

--- packages/engine/src/scene/components/ModelComponent.ts
+++ packages/engine/src/scene/components/ModelComponent.ts
@@ -32,6 +32,8 @@ import { VRM } from '@pixiv/three-vrm'
 import { AssetLoader } from '../../assets/classes/AssetLoader'
 import { GLTF } from '../../assets/loaders/gltf/GLTFLoader'
 import { LoopAnimationComponent } from '../../avatar/components/LoopAnimationComponent'
+import { CameraComponent } from '../../camera/components/CameraComponent'
+import { Engine } from '../../ecs/classes/Engine'
 import { EngineState } from '../../ecs/classes/EngineState'
 import {
   ComponentType,
@@ -47,6 +49,7 @@ import {
 import { entityExists, useEntityContext } from '../../ecs/functions/EntityFunctions'
 import { removeEntityNodeRecursively } from '../../ecs/functions/EntityTree'
 import { BoundingBoxComponent } from '../../interaction/components/BoundingBoxComponents'
+import { EngineRenderer } from '../../renderer/WebGLRendererSystem'
 import { SourceType } from '../../renderer/materials/components/MaterialSource'
 import { removeMaterialSource } from '../../renderer/materials/functions/MaterialLibraryFunctions'
 import { FrustumCullCameraComponent } from '../../transform/components/DistanceComponents'
@@ -173,8 +176,6 @@ function ModelReactor() {
               if (fileExtension == 'vrm') (model.asset as any).userData = { flipped: true }
               model.scene && removeObjectFromGroup(entity, model.scene)
               modelComponent.scene.set(loadedAsset.scene)
-              if (!hasComponent(entity, SceneAssetPendingTagComponent)) return
-              removeComponent(entity, SceneAssetPendingTagComponent)
             },
             (onprogress) => {
               if (!hasComponent(entity, SceneAssetPendingTagComponent)) return
@@ -206,6 +207,12 @@ function ModelReactor() {
 
     if (!scene) return
     addObjectToGroup(entity, scene)
+
+    EngineRenderer.instance.renderer
+      .compileAsync(scene, getComponent(Engine.instance.cameraEntity, CameraComponent), Engine.instance.scene)
+      .then(() => {
+        if (hasComponent(entity, SceneAssetPendingTagComponent)) removeComponent(entity, SceneAssetPendingTagComponent)
+      })
 
     if (groupComponent?.value?.find((group: any) => group === scene)) return
 
