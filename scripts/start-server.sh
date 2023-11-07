19a228e07fb2eed2c3eaa6386e23424deab9c458
Murad Khateeb <muradkhateeb788@gmail.com>
2023-11-07 12:37:18 +0500

    Update EE dockerfile to simplfy local development of EE

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 [ "$SERVER_MODE" = "task" ] && (cd packages/taskserver && npm run start)
-[ "$SERVER_MODE" = "client" ] && (cd packages/client && npm run start)
+#[ "$SERVER_MODE" = "client" ] && (cd packages/client && npm run start)
+[ "$SERVER_MODE" = "client" ] && (cd packages/client && npm run dev)
 [ "$SERVER_MODE" = "realtime" ] && (cd packages/instanceserver && npm run start)
 [ "$SERVER_MODE" = "api" ] && (cd packages/server && npm run start)
+
+#[ "$SERVER_MODE" = "task" ] && (cd packages/taskserver && npm run dev)
+#[ "$SERVER_MODE" = "client" ] && (cd packages/client && npm run dev)
+#[ "$SERVER_MODE" = "realtime" ] && (cd packages/instanceserver && npm run dev)
+#[ "$SERVER_MODE" = "api" ] && (cd packages/server && npm run dev)
