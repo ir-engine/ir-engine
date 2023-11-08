9391d2c2876690ffdc42e90b7642bf88ea8dc75c
AidanCaruso <jamesajcme@gmail.com>
2023-11-08 16:58:23 -0500

    use xr friendly lookat for interactable xrui tips

--- packages/engine/src/interaction/systems/InteractiveSystem.ts
+++ packages/engine/src/interaction/systems/InteractiveSystem.ts
@@ -24,7 +24,7 @@ Ethereal Engine. All Rights Reserved.
 */
 
 import { Not } from 'bitecs'
-import { Vector3 } from 'three'
+import { Euler, Quaternion, Vector3 } from 'three'
 
 import { defineState } from '@etherealengine/hyperflux'
 import { WebLayer3D } from '@etherealengine/xrui'
@@ -33,6 +33,7 @@ import { getState } from '@etherealengine/hyperflux'
 import { VRMHumanBoneName } from '@pixiv/three-vrm'
 import { AvatarComponent } from '../../avatar/components/AvatarComponent'
 import { getAvatarBoneWorldPosition } from '../../avatar/functions/avatarFunctions'
+import { V_010 } from '../../common/constants/MathConstants'
 import { Engine } from '../../ecs/classes/Engine'
 import { EngineState } from '../../ecs/classes/EngineState'
 import { Entity } from '../../ecs/classes/Entity'
@@ -73,12 +74,20 @@ export const InteractiveUI = new Map<Entity, InteractiveType>()
 export const InteractableTransitions = new Map<Entity, ReturnType<typeof createTransitionState>>()
 
 const vec3 = new Vector3()
+const flip = new Quaternion().setFromEuler(new Euler(0, Math.PI, 0))
 
 export const onInteractableUpdate = (entity: Entity, xrui: ReturnType<typeof createInteractUI>) => {
   const transform = getComponent(xrui.entity, TransformComponent)
+  transform.matrix.lookAt(
+    transform.position,
+    getComponent(Engine.instance.cameraEntity, TransformComponent).position,
+    V_010
+  )
+  transform.matrix.decompose(transform.position, transform.rotation, transform.scale)
+  transform.rotation.multiply(flip)
+
   if (!transform || !getComponent(Engine.instance.localClientEntity, TransformComponent)) return
   transform.position.copy(getComponent(entity, TransformComponent).position)
-  transform.rotation.copy(getComponent(entity, TransformComponent).rotation)
   transform.position.y += 1
   const transition = InteractableTransitions.get(entity)!
   getAvatarBoneWorldPosition(Engine.instance.localClientEntity, VRMHumanBoneName.Hips, vec3)
@@ -97,8 +106,6 @@ export const onInteractableUpdate = (entity: Entity, xrui: ReturnType<typeof cre
       mat.opacity = opacity
     })
   })
-  const cameraTransform = getComponent(Engine.instance.cameraEntity, TransformComponent)
-  transform.rotation.copy(cameraTransform.rotation)
 }
 
 export const getInteractiveUI = (entity: Entity) => InteractiveUI.get(entity)
