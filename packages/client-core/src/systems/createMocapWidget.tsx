8a5484c82f15cc0d883eaaf6737c0ec528f30e80
Daniel Belmes <danielbelmes@ymail.com>
2023-11-20 12:29:37 -0800

    Add entity context to createXrui, fix weird daisy config.

---
+++ packages/client-core/src/systems/createMocapWidget.tsx
@@ -0,0 +1,69 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import {
+  getComponent,
+  removeComponent,
+  setComponent
+} from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
+import { VisibleComponent } from '@etherealengine/engine/src/scene/components/VisibleComponent'
+import { createXRUI } from '@etherealengine/engine/src/xrui/functions/createXRUI'
+import { Widget, Widgets } from '@etherealengine/engine/src/xrui/Widgets'
+
+import { Engine } from '@etherealengine/engine/src/ecs/classes/Engine'
+import { InputComponent } from '@etherealengine/engine/src/input/components/InputComponent'
+import { EngineRenderer } from '@etherealengine/engine/src/renderer/WebGLRendererSystem'
+import { NameComponent } from '@etherealengine/engine/src/scene/components/NameComponent'
+import { setComputedTransformComponent } from '@etherealengine/engine/src/transform/components/ComputedTransformComponent'
+import { LocalTransformComponent } from '@etherealengine/engine/src/transform/components/TransformComponent'
+import { MocapWidgetUI } from './ui/MocapWidget/MocapWidgetUi'
+
+export function createMocapWidget() {
+  const ui = createXRUI(MocapWidgetUI)
+  setComponent(ui.entity, NameComponent, 'Mocap XRUI')
+
+  setComputedTransformComponent(ui.entity, Engine.instance.cameraEntity, () => {
+    const uiTransform = getComponent(ui.entity, LocalTransformComponent)
+    uiTransform.position.set(0.14, 0, 0)
+  })
+  removeComponent(ui.entity, VisibleComponent)
+
+  const widget: Widget = {
+    ui,
+    label: 'Mocap',
+    icon: 'Accessibility',
+    system: () => {
+      const inputComponent = getComponent(ui.entity, InputComponent)
+      const canvas = EngineRenderer.instance.renderer.domElement
+      if (inputComponent.inputSources.length > 0) {
+        canvas.style.cursor = 'pointer'
+      } else {
+        canvas.style.cursor = 'auto'
+      }
+    }
+  }
+
+  const id = Widgets.registerWidget(ui.entity, widget)
+}
---
+++ packages/client-core/src/systems/ui/MocapWidget/MocapWidgetUi.tsx
@@ -0,0 +1,78 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import tailwind from '@etherealengine/client/src/themes/tailwind.css?inline'
+import { defineState } from '@etherealengine/hyperflux'
+import React from 'react'
+
+export const MocapUIState = defineState({
+  name: 'MocapUIState',
+  initial: {
+    capturing: false,
+    calibratingHeight: false
+  }
+})
+
+export const MocapWidgetUI = () => {
+  return (
+    <>
+      <style type="text/css">{tailwind.toString()}</style>
+      <span className="flex flex-col justify-center space-y-1 h-48">
+        <span className="h-5 text-sm place-items-center card bg-base-300">Mocap Controls</span>
+        <button className="btn btn-sm btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+            />
+          </svg>
+        </button>
+        <button className="btn btn-sm btn-circle" onMouseOver={() => console.log('Hovering')}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+            />
+          </svg>
+        </button>
+      </span>
+    </>
+  )
+}
