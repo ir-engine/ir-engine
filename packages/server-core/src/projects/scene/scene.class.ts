f5e04110c991eae576070e742332c67d055e3a4e
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-12-12 11:44:34 +0530

    refactor: rename scene asset files

--- packages/server-core/src/projects/scene/scene.class.ts
+++ packages/server-core/src/projects/scene/scene.class.ts
@@ -51,7 +51,7 @@ import { ProjectParams } from '../project/project.class'
 import { getSceneData } from './scene-helper'
 const NEW_SCENE_NAME = 'New-Scene'
 
-const sceneAssetFiles = ['.scene.json', '.thumbnail.ktx2', '.envmap.ktx2']
+const SCENE_ASSET_FILES = ['.scene.json', '.thumbnail.ktx2', '.envmap.ktx2']
 
 export interface SceneParams extends Params<SceneQuery> {
   paginate?: false
@@ -179,7 +179,7 @@ export class SceneService
     }
 
     await Promise.all(
-      sceneAssetFiles.map((ext) =>
+      SCENE_ASSET_FILES.map((ext) =>
         storageProvider.moveObject(
           `default${ext}`,
           `${newSceneName}${ext}`,
@@ -190,16 +190,16 @@ export class SceneService
       )
     )
     try {
-      await storageProvider.createInvalidation(sceneAssetFiles.map((asset) => `${directory}${newSceneName}${asset}`))
+      await storageProvider.createInvalidation(SCENE_ASSET_FILES.map((asset) => `${directory}${newSceneName}${asset}`))
     } catch (e) {
       logger.error(e)
-      logger.info(sceneAssetFiles)
+      logger.info(SCENE_ASSET_FILES)
     }
 
     if (isDev) {
       const projectPathLocal = path.resolve(appRootPath.path, localDirectory)
       const defaultProjectPath = path.resolve(appRootPath.path, 'packages/projects/projects/default-project/')
-      for (const ext of sceneAssetFiles) {
+      for (const ext of SCENE_ASSET_FILES) {
         fs.copyFileSync(
           path.resolve(path.join(defaultProjectPath, 'default') + ext),
           path.resolve(path.join(projectPathLocal, newSceneName) + ext)
@@ -220,7 +220,7 @@ export class SceneService
     const directory = data.directory!
     const localDirectory = data.localDirectory!
 
-    for (const ext of sceneAssetFiles) {
+    for (const ext of SCENE_ASSET_FILES) {
       const oldSceneJsonName = `${oldSceneName}${ext}`
       const newSceneJsonName = `${newSceneName}${ext}`
 
@@ -236,7 +236,7 @@ export class SceneService
     }
 
     if (isDev) {
-      for (const ext of sceneAssetFiles) {
+      for (const ext of SCENE_ASSET_FILES) {
         const oldSceneJsonPath = path.resolve(appRootPath.path, `${localDirectory}${oldSceneName}${ext}`)
         if (fs.existsSync(oldSceneJsonPath)) {
           const newSceneJsonPath = path.resolve(appRootPath.path, `${localDirectory}${newSceneName}${ext}`)
@@ -280,10 +280,10 @@ export class SceneService
       }
 
       try {
-        await storageProvider.createInvalidation(sceneAssetFiles.map((asset) => `${directory}${name}${asset}`))
+        await storageProvider.createInvalidation(SCENE_ASSET_FILES.map((asset) => `${directory}${name}${asset}`))
       } catch (e) {
         logger.error(e)
-        logger.info(sceneAssetFiles)
+        logger.info(SCENE_ASSET_FILES)
       }
 
       if (isDev) {
@@ -322,20 +322,20 @@ export class SceneService
     const directory = params!.query!.directory!.toString()!
     const localDirectory = params!.query!.localDirectory!.toString()!
 
-    for (const ext of sceneAssetFiles) {
+    for (const ext of SCENE_ASSET_FILES) {
       const assetFilePath = path.resolve(appRootPath.path, `${localDirectory}/${sceneName}${ext}`)
       if (fs.existsSync(assetFilePath)) {
         fs.rmSync(path.resolve(assetFilePath))
       }
     }
 
-    await storageProvider.deleteResources(sceneAssetFiles.map((ext) => `${directory}${sceneName}${ext}`))
+    await storageProvider.deleteResources(SCENE_ASSET_FILES.map((ext) => `${directory}${sceneName}${ext}`))
 
     try {
-      await storageProvider.createInvalidation(sceneAssetFiles.map((asset) => `${directory}${sceneName}${asset}`))
+      await storageProvider.createInvalidation(SCENE_ASSET_FILES.map((asset) => `${directory}${sceneName}${asset}`))
     } catch (e) {
       logger.error(e)
-      logger.info(sceneAssetFiles)
+      logger.info(SCENE_ASSET_FILES)
     }
   }
 }
