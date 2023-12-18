30cfad0ad606eea6f5969a3ee8dbeb6189797b28
Michael Estes <michael@etherealengine.com>
2023-12-18 12:55:14 -0800

    Start of RenderOrderComponent

---
+++ packages/engine/tests/renderer/components/RenderOrderComponent.test.tsx
@@ -0,0 +1,67 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import { act, render } from '@testing-library/react'
+import assert from 'assert'
+import React from 'react'
+import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
+
+import { destroyEngine } from '../../../src/ecs/classes/Engine'
+import { setComponent } from '../../../src/ecs/functions/ComponentFunctions'
+import { createEntity } from '../../../src/ecs/functions/EntityFunctions'
+import { createEngine } from '../../../src/initializeEngine'
+import { RenderOrderComponent } from '../../../src/renderer/components/RenderOrderComponent'
+import { loadEmptyScene } from '../../util/loadEmptyScene'
+
+describe('RenderOrderComponent', () => {
+  beforeEach(async () => {
+    createEngine()
+    loadEmptyScene()
+  })
+
+  afterEach(() => {
+    return destroyEngine()
+  })
+
+  it('Sets renderOrder on object', async () => {
+    const entity = createEntity()
+    const geometry = new BoxGeometry(1, 1, 1)
+    const material = new MeshBasicMaterial({ color: 0xffff00 })
+    const mesh = new Mesh(geometry, material)
+
+    const renderOrder = 2
+
+    setComponent(entity, RenderOrderComponent, { renderObject: mesh, renderOrder: renderOrder })
+
+    const Reactor = RenderOrderComponent.reactor
+    const tag = <Reactor />
+    const { rerender, unmount } = render(tag)
+    await act(() => rerender(tag))
+
+    assert(mesh.renderOrder === renderOrder, 'Render order is set on mesh object')
+
+    unmount()
+  })
+})
