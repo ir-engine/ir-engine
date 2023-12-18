b2777cc63124e643689e303b9b5cba44449f2ec9
Kyle Baran <kbaran@bitscoop.com>
2023-12-18 09:23:41 -0800

    Call create-env-production from home context instead of cd'ing into client package

--- scripts/run-builder.sh
+++ scripts/run-builder.sh
@@ -27,7 +27,7 @@ rm package.json
 mv package.jsonmoved package.json
 npm run prepare-database >prepare-database-build-logs.txt 2>prepare-database-build-error.txt || npm run record-build-error -- --service=prepare-database
 test -s prepare-database-build-error.txt && npm run record-build-error -- --service=prepare-database
-cd packages/client && npx cross-env node --loader ts-node/esm scripts/create-env-production.ts >buildenv-build-logs.txt 2>buildenv-build-error.txt || npm run record-build-error -- --service=buildenv
+npx cross-env node --loader ts-node/esm packages/client/scripts/create-env-production.ts >buildenv-build-logs.txt 2>buildenv-build-error.txt || npm run record-build-error -- --service=buildenv
 test -s buildenv-build-error.txt && npm run record-build-error -- --service=buildenv
 if [ -n "$TWA_LINK" ]
 then
