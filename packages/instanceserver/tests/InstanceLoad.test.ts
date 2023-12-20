c2df790819db5a98bc4e7f6609da9137af5987d6
HexaField <joshfield999@gmail.com>
2023-12-20 13:31:38 +1100

    start adding instance server test

---
+++ packages/instanceserver/tests/InstanceLoad.test.ts
@@ -0,0 +1,73 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import assert from 'assert'
+
+import { Engine, destroyEngine } from '@etherealengine/engine/src/ecs/classes/Engine'
+
+import { EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
+import { getState } from '@etherealengine/hyperflux'
+import { Application } from '@etherealengine/server-core/declarations'
+import { onConnection } from '../src/channels'
+import { start } from '../src/start'
+
+describe('InstanceLoad', () => {
+  before(async () => {
+    const app = await start()
+    await app.setup()
+  })
+
+  it('should load location', async () => {
+    const app = Engine.instance.api as Application
+    const loadLocation = onConnection(app)
+
+    const query = {
+      provider: 'test',
+      headers: {},
+      socketQuery: {
+        token: '',
+        locationId: '',
+        instanceID: '',
+        channelId: '',
+        roomCode: '',
+        address: '',
+        port: 0,
+        EIO: '',
+        transport: '',
+        t: ''
+      },
+      instanceId: '',
+      channelId: undefined
+    } as any
+
+    await loadLocation(query)
+
+    assert.equal(getState(EngineState).sceneLoaded, true)
+  })
+
+  after(() => {
+    return destroyEngine()
+  })
+})
