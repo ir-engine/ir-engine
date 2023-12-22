bf527ad2f4d09a68b22b865d4b176ef7dedf4659
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-12-22 18:01:45 +0530

    complete the search bar

--- packages/editor/src/components/assets/SceneAssetsPanel.tsx
+++ packages/editor/src/components/assets/SceneAssetsPanel.tsx
@@ -24,7 +24,10 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 import Inventory2Icon from '@mui/icons-material/Inventory2'
-import React, { useEffect } from 'react'
+import { t } from 'i18next'
+import { TabData } from 'rc-dock'
+import React, { useCallback, useEffect, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { FixedSizeList } from 'react-window'
 
@@ -34,9 +37,9 @@ import { AssetLoader } from '@etherealengine/engine/src/assets/classes/AssetLoad
 import { AssetClass } from '@etherealengine/engine/src/assets/enum/AssetClass'
 import { Engine } from '@etherealengine/engine/src/ecs/classes/Engine'
 import { StaticResourceType, staticResourcePath } from '@etherealengine/engine/src/schemas/media/static-resource.schema'
-import { t } from 'i18next'
-import { TabData } from 'rc-dock'
-import { useTranslation } from 'react-i18next'
+import { CircularProgress } from '@mui/material'
+import { debounce } from 'lodash'
+import StringInput from '../inputs/StringInput'
 import { PanelDragContainer, PanelIcon, PanelTitle } from '../layout/Panel'
 import ImageNodeEditor from '../properties/ImageNodeEditor'
 import ModelNodeEditor from '../properties/ModelNodeEditor'
@@ -79,90 +82,162 @@ const StaticResourceItem = (props: {
   }
 }
 
+const ResourceFile = ({ resource }: { resource: StaticResourceType }) => {
+  const ResourceIcon = ResourceIcons[AssetLoader.getAssetClass(resource.key)]
+  return (
+    <div
+      key={resource.id}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: '10px'
+      }}
+    >
+      <ResourceIcon style={{ marginBottom: '5px', height: '70px', width: '70px' }} />
+      <span>{resource.key.split('/').at(-1)}</span>
+    </div>
+  )
+}
+
 const SceneAssetsPanel = () => {
   const { t } = useTranslation()
   const staticResources = useHookstate<CategorizedStaticResourceType[]>([])
   const categorizedStaticResources = useHookstate({} as Record<string, StaticResourceType[]>)
   const selectedCategory = useHookstate<string | null>(null)
+  const loading = useHookstate(false)
+  const searchText = useHookstate('')
+  const searchTimeoutCancelRef = useRef<(() => void) | null>(null)
+  const searchedStaticResources = useHookstate<StaticResourceType[]>([])
 
   useEffect(() => {
-    Engine.instance.api
-      .service(staticResourcePath)
-      .find({ query: { $sort: { mimeType: 1 } } })
-      .then((resources) => {
-        const categorizedResources: Record<string, StaticResourceType[]> = {}
-        const categorizedResourcesList: CategorizedStaticResourceType[] = []
-
-        let previousAssetClass: string | null = null
-
-        resources.data.forEach((resource) => {
-          const assetClass = AssetLoader.getAssetClass(resource.key)
-          if (previousAssetClass !== assetClass) {
-            categorizedResourcesList.push({ folderType: 'folder', assetClass })
-            previousAssetClass = assetClass
-            categorizedResources[previousAssetClass] = []
+    const staticResourcesFindApi = () =>
+      Engine.instance.api
+        .service(staticResourcePath)
+        .find({ query: { key: { $like: searchText.value || undefined }, $sort: { mimeType: 1 }, $limit: 100 } })
+        .then((resources) => {
+          if (searchText.value) {
+            searchedStaticResources.set(resources.data)
+            return
           }
-          categorizedResources[previousAssetClass].push(resource)
-          categorizedResourcesList.push({ folderType: 'staticResource', ...resource })
+
+          const categorizedResources: Record<string, StaticResourceType[]> = {}
+          const categorizedResourcesList: CategorizedStaticResourceType[] = []
+
+          let previousAssetClass: string | null = null
+
+          resources.data.forEach((resource) => {
+            const assetClass = AssetLoader.getAssetClass(resource.key)
+            if (previousAssetClass !== assetClass) {
+              categorizedResourcesList.push({ folderType: 'folder', assetClass })
+              previousAssetClass = assetClass
+              categorizedResources[previousAssetClass] = []
+            }
+            categorizedResources[previousAssetClass].push(resource)
+            categorizedResourcesList.push({ folderType: 'staticResource', ...resource })
+          })
+
+          categorizedStaticResources.set(categorizedResources)
+          staticResources.set(categorizedResourcesList)
+        })
+        .then(() => {
+          loading.set(false)
         })
 
-        categorizedStaticResources.set(categorizedResources)
-        staticResources.set(categorizedResourcesList)
-      })
-  }, [])
+    loading.set(true)
 
-  const ResourceList = ({ height, width }) => (
-    <FixedSizeList
-      height={height}
-      width={width}
-      itemSize={32}
-      itemCount={staticResources.length}
-      itemData={{
-        resources: staticResources.get(NO_PROXY),
-        selectedCategory: selectedCategory.value,
-        onClick: (resource: FolderType) => {
-          selectedCategory.set(resource.assetClass)
-        }
-      }}
-      itemKey={(index) => index}
-    >
-      {StaticResourceItem}
-    </FixedSizeList>
+    searchTimeoutCancelRef.current?.()
+    const debouncedSearchQuery = debounce(staticResourcesFindApi, 500)
+    debouncedSearchQuery()
+
+    searchTimeoutCancelRef.current = debouncedSearchQuery.cancel
+
+    return () => searchTimeoutCancelRef.current?.()
+  }, [searchText])
+
+  const ResourceList = useCallback(
+    ({ height, width }) => (
+      <FixedSizeList
+        height={height}
+        width={width}
+        itemSize={32}
+        itemCount={staticResources.length}
+        itemData={{
+          resources: staticResources.get(NO_PROXY),
+          selectedCategory: selectedCategory.value,
+          onClick: (resource: FolderType) => {
+            selectedCategory.set(resource.assetClass)
+            searchText.set('')
+          }
+        }}
+        itemKey={(index) => index}
+      >
+        {StaticResourceItem}
+      </FixedSizeList>
+    ),
+    [selectedCategory]
   )
 
-  const ResourceIcon = ResourceIcons[selectedCategory.get(NO_PROXY)!]
+  const ResourceItems = () => {
+    if (loading.value) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <CircularProgress />
+        </div>
+      )
+    }
+    if (searchText.value) {
+      if (staticResources.value)
+        return (
+          <>
+            {searchedStaticResources.map((resource) => (
+              <ResourceFile key={resource.value.id} resource={resource.get(NO_PROXY)} />
+            ))}
+          </>
+        )
+      return <div>{t('editor:layout.scene-assets.no-search-results')}</div>
+    }
+    if (selectedCategory.value) {
+      return (
+        <>
+          {categorizedStaticResources.value[selectedCategory.value!]?.map((resource) => (
+            <ResourceFile resource={resource} />
+          ))}
+        </>
+      )
+    }
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {t('editor:layout.scene-assets.no-category')}
+      </div>
+    )
+  }
 
   return (
-    <div style={{ display: 'flex', height: '100%', width: '100%', margin: '1rem auto' }}>
-      <div style={{ height: '100%', width: '50%' }}>
-        <AutoSizer onResize={ResourceList}>{ResourceList}</AutoSizer>
+    <div style={{ margin: '1rem auto', height: '100%', width: '100%' }}>
+      <div className={styles.searchContainer}>
+        <StringInput
+          placeholder={t('editor:layout.scene-assets.search-placeholder')}
+          value={searchText.value}
+          onChange={(event) => searchText.set(event.target.value)}
+        />
       </div>
-      <div
-        style={{
-          width: '50%',
-          display: 'grid',
-          gap: '40px 10px',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(90px, 1fr))',
-          gridAutoRows: '60px'
-        }}
-      >
-        {selectedCategory.value
-          ? categorizedStaticResources.value[selectedCategory.value!].map((resource) => (
-              <div
-                key={resource.id}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  flexDirection: 'column',
-                  marginTop: '10px'
-                }}
-              >
-                <ResourceIcon style={{ marginBottom: '5px', height: '70px', width: '70px' }} />
-                <span>{resource.key.split('/').at(-1)}</span>
-              </div>
-            ))
-          : t('editor:layout.scene-assets.no-category')}
+      <div style={{ display: 'flex', height: '100%', width: '100%', margin: '1rem auto' }}>
+        <div style={{ height: '100%', width: '50%' }}>
+          <AutoSizer onResize={ResourceList}>{ResourceList}</AutoSizer>
+        </div>
+        <div
+          style={{
+            width: '50%',
+            display: 'grid',
+            gap: '40px 10px',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(90px, 1fr))',
+            gridAutoRows: '60px'
+          }}
+        >
+          <ResourceItems />
+        </div>
       </div>
     </div>
   )
