81d7dae289f26562fc025518b1e3ad7e9e0ae964
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-12-29 06:47:46 +0530

    added tests for builder-info service (#9446)
    * added tests for builder-info
    
    * added license comment

--- packages/server-core/src/projects/builder-info/builder-info.test.ts
+++ packages/server-core/src/projects/builder-info/builder-info.test.ts
@@ -1,5 +1,6 @@
 /*
 CPAL-1.0 License
+
 The contents of this file are subject to the Common Public Attribution License
 Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -8,52 +9,41 @@ The License is based on the Mozilla Public License Version 1.1, but Sections 14
 and 15 have been added to cover use of software over a computer network and 
 provide for limited attribution for the Original Developer. In addition, 
 Exhibit A has been modified to be consistent with Exhibit B.
+
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
 specific language governing rights and limitations under the License.
+
 The Original Code is Ethereal Engine.
+
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Ethereal Engine team.
+
 All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
 Ethereal Engine. All Rights Reserved.
 */
 
-import { hooks as schemaHooks } from '@feathersjs/schema'
+import { destroyEngine } from '@etherealengine/engine/src/ecs/classes/Engine'
+import { builderInfoPath } from '@etherealengine/engine/src/schemas/projects/builder-info.schema'
+import assert from 'assert'
+import { Application } from '../../../declarations'
+import { createFeathersKoaApp } from '../../createApp'
+import { getEnginePackageJson } from '../project/project-helper'
 
-import { disallow, iff, isProvider } from 'feathers-hooks-common'
-import verifyScope from '../../hooks/verify-scope'
-import { builderInfoExternalResolver, builderInfoResolver } from './builder-info.resolvers'
+describe('builder-info.test', () => {
+  let app: Application
 
-export default {
-  around: {
-    all: [schemaHooks.resolveExternal(builderInfoExternalResolver), schemaHooks.resolveResult(builderInfoResolver)]
-  },
+  before(async () => {
+    app = createFeathersKoaApp()
+    await app.setup()
+  })
 
-  before: {
-    all: [],
-    find: [disallow()],
-    get: [iff(isProvider('external'), verifyScope('projects', 'read'))],
-    create: [disallow()],
-    update: [disallow()],
-    patch: [disallow()],
-    remove: [disallow()]
-  },
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
-} as any
+  after(async () => {
+    await destroyEngine()
+  })
+
+  it('should get the builder info', async () => {
+    const builderInfo = await app.service(builderInfoPath).get()
+    assert.equal(builderInfo.engineVersion, getEnginePackageJson().version)
+  })
+})
