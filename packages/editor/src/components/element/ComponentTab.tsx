9872ae374d6085ecf886543e5da478962aa5fd18
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-01-11 11:43:34 +0530

    import EntityNodeEditor dynamically

--- packages/editor/src/components/element/ComponentTab.tsx
+++ packages/editor/src/components/element/ComponentTab.tsx
@@ -23,7 +23,7 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import hierarchyStyles from '../hierarchy/styles.module.scss'
 
 import { Entity } from '@etherealengine/engine/src/ecs/classes/Entity'
@@ -34,47 +34,61 @@ import {
 } from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
 import { getMutableState } from '@etherealengine/hyperflux'
 import { useHookstate } from '@hookstate/core'
+import { CircularProgress } from '@mui/material'
 import { TabData } from 'rc-dock'
-import { EntityNodeEditor } from '../../functions/ComponentEditors'
 import { SelectionState } from '../../services/SelectionServices'
 import { PanelDragContainer, PanelIcon, PanelTitle } from '../layout/Panel'
+import { EditorComponentType } from '../properties/Util'
 import styles from '../styles.module.scss'
 
+/**import EntityNodeEditor dynamically (useful in circular import preventions) */
+const useDynamicEntityNodeEditor = () => {
+  const [EntityNodeEditor, setEntityNodeEditor] = useState<Map<Component, EditorComponentType> | null>(null)
+  useEffect(() => {
+    import('../../functions/ComponentEditors').then((imported) => setEntityNodeEditor(imported.EntityNodeEditor))
+  }, [])
+  return EntityNodeEditor
+}
+
 export const EntityComponentEditor = ({ entity, component }: { entity: Entity; component: Component }) => {
-  const selectedEntities = useHookstate(getMutableState(SelectionState).selectedEntities).value
   const componentMounted = useOptionalComponent(entity, component)
   if (!componentMounted || !hasComponent(entity, component)) return null
-  const Editor = EntityNodeEditor.get(component)!
-  // nodeEntity is used as key here to signal to React when the entity has changed,
-  // and to prevent state from being recycled between editor instances, which
-  // can cause hookstate to throw errors.
-  return (
+  const selectedEntities = useHookstate(getMutableState(SelectionState).selectedEntities).value
+  const EntityNodeEditor = useDynamicEntityNodeEditor()
+  const Editor = EntityNodeEditor?.get(component)
+
+  return Editor ? (
     <Editor
       key={`${entity}-${Editor.name}`}
       multiEdit={selectedEntities.length > 1}
       entity={entity}
       component={component}
     />
-  )
-}
-
-export const ComponentPanelContent = ({ entity, component }: { entity: Entity; component: Component }) => {
-  return (
-    <div className={hierarchyStyles.panelContainer}>
-      <div className={hierarchyStyles.panelSection}>
-        <EntityComponentEditor entity={entity} component={component} />
-      </div>
+  ) : (
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '1rem' }}>
+      <CircularProgress style={{ color: 'var(--textColor)' }} />
     </div>
   )
 }
 
 export const ComponentPanelTitle = ({ title, component }: { title: string; component: Component }) => {
+  const EntityNodeEditor = useDynamicEntityNodeEditor()
   return (
     <div className={styles.dockableTab}>
       <PanelDragContainer>
-        <PanelIcon as={EntityNodeEditor.get(component)?.iconComponent} size={12} />
+        {EntityNodeEditor && <PanelIcon as={EntityNodeEditor.get(component)?.iconComponent} size={12} />}
         <PanelTitle>{title}</PanelTitle>
       </PanelDragContainer>
+    </div>
+  )
+}
+
+const ComponentPanelContent = ({ entity, component }: { entity: Entity; component: Component }) => {
+  return (
+    <div className={hierarchyStyles.panelContainer}>
+      <div className={hierarchyStyles.panelSection}>
+        <EntityComponentEditor entity={entity} component={component} />
+      </div>
     </div>
   )
 }
