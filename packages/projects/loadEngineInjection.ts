64664828cfb2ca0f4668fcab674222cebc630e33
HexaField <joshfield999@gmail.com>
2024-01-17 17:03:03 +1100

    re-enable engine injection

--- packages/projects/loadEngineInjection.ts
+++ packages/projects/loadEngineInjection.ts
@@ -23,20 +23,24 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
+import { projectsPath } from '@etherealengine/common/src/schema.type.module'
+import { Engine } from '@etherealengine/engine/src/ecs/classes/Engine'
+import { loadConfigForProject } from './loadConfigForProject'
+
 export const loadEngineInjection = async () => {
-  // const projects = await Engine.instance.api.service(projectsPath).find()
-  // return Promise.all(
-  //   projects
-  //     .map(async (project) => {
-  //       try {
-  //         const projectConfig = (await loadConfigForProject(project))!
-  //         if (typeof projectConfig.worldInjection !== 'function') return null!
-  //         return (await projectConfig.worldInjection()).default()
-  //       } catch (e) {
-  //         console.error(`Failed to import world load event for project ${project} with reason ${e}`)
-  //         return null!
-  //       }
-  //     })
-  //     .filter(($) => !!$)
-  // )
+  const projects = await Engine.instance.api.service(projectsPath).find()
+  return Promise.all(
+    projects
+      .map(async (project) => {
+        try {
+          const projectConfig = (await loadConfigForProject(project))!
+          if (typeof projectConfig.worldInjection !== 'function') return null!
+          return (await projectConfig.worldInjection()).default()
+        } catch (e) {
+          console.error(`Failed to import world load event for project ${project} with reason ${e}`)
+          return null!
+        }
+      })
+      .filter(($) => !!$)
+  )
 }
