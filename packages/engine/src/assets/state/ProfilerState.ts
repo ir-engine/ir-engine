e5c66ff702cbec34bfb10c803cde2de0c9eeda65
Michael Estes <michael@etherealengine.com>
2024-02-09 15:01:12 -0800

    Texture render time check

--- packages/engine/src/assets/state/ProfilerState.ts
+++ packages/engine/src/assets/state/ProfilerState.ts
@@ -28,15 +28,90 @@ import { defineState, getMutableState } from '@etherealengine/hyperflux'
 import { isMobile } from '@etherealengine/spatial/src/common/functions/isMobile'
 import { EngineRenderer } from '@etherealengine/spatial/src/renderer/WebGLRendererSystem'
 import { getGPUTier } from 'detect-gpu'
-import { BufferAttribute, BufferGeometry, Mesh, MeshBasicMaterial, OrthographicCamera, Scene } from 'three'
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  DataTexture,
+  Mesh,
+  MeshBasicMaterial,
+  OrthographicCamera,
+  PlaneGeometry,
+  Scene
+} from 'three'
 
 export const ProfilerState = defineState({
   name: 'ProfilerState',
   initial: () => ({
     tier: 0,
-    meshRenderMs: 0
+    meshRenderMs: 0,
+    textureRenderMs: 0,
+    alphaRenderMs: 0
   })
 })
+
+const checkRender = (renderer: EngineRenderer, scene: Scene, onFinished: (ms: number) => void) => {
+  const frustumSize = 500
+  const aspect = window.innerWidth / window.innerHeight
+  const camera = new OrthographicCamera(
+    (frustumSize * aspect) / -2,
+    (frustumSize * aspect) / 2,
+    frustumSize / 2,
+    frustumSize / -2,
+    1,
+    1000
+  )
+  camera.position.set(-200, 200, 200)
+  scene.background = new Color(0xf0f0f0)
+  scene.add(camera)
+
+  const fallback = () => {
+    const end = profile()
+    renderer.renderer.render(scene, camera)
+    renderTime = end()
+    onFinished(renderTime)
+  }
+
+  let renderTime = 0
+  if (renderer.supportWebGL2) {
+    const gl = renderer.renderContext as WebGL2RenderingContext
+    const ext = gl.getExtension('EXT_disjoint_timer_query_webgl2')
+
+    // Not super well supported, no Safari support
+    if (ext) {
+      const startQuery = gl.createQuery()
+      const endQuery = gl.createQuery()
+
+      if (startQuery && endQuery) {
+        ext.queryCounterEXT(startQuery, ext.TIMESTAMP_EXT)
+        renderer.renderer.render(scene, camera)
+        ext.queryCounterEXT(endQuery, ext.TIMESTAMP_EXT)
+
+        requestAnimationFrame(function poll() {
+          const available = gl.getQueryParameter(endQuery!, gl.QUERY_RESULT_AVAILABLE)
+          const disjoint = gl.getParameter(ext.GPU_DISJOINT_EXT)
+
+          if (available && !disjoint) {
+            // Gets times in nanoseconds
+            const timeStart = gl.getQueryParameter(startQuery!, gl.QUERY_RESULT)
+            const timeEnd = gl.getQueryParameter(endQuery!, gl.QUERY_RESULT)
+            const renderTime = (timeEnd - timeStart) * 0.000001
+            onFinished(renderTime)
+          } else if (disjoint) fallback()
+
+          if (available || disjoint) {
+            gl.deleteQuery(startQuery)
+            gl.deleteQuery(endQuery)
+          } else {
+            requestAnimationFrame(poll)
+          }
+        })
+      } else fallback()
+    } else fallback()
+  } else fallback()
+
+  scene.remove(camera)
+}
 
 const createTriangle = (): Mesh => {
   const vertices = new Float32Array([
@@ -57,8 +132,7 @@ const createTriangle = (): Mesh => {
   return new Mesh(geometry, material)
 }
 
-const triangleCount = isMobile ? 5000 : 10000
-
+const triangleCount = isMobile ? 1250 : 5000
 const checkMeshRender = (renderer: EngineRenderer, onFinished: () => void) => {
   const profilerState = getMutableState(ProfilerState)
   const scene = new Scene()
@@ -69,60 +143,53 @@ const checkMeshRender = (renderer: EngineRenderer, onFinished: () => void) => {
     scene.add(mesh)
   }
 
-  const fallback = () => {
-    const end = profile()
-    renderer.renderer.render(scene, new OrthographicCamera())
-    renderTime = end()
+  checkRender(renderer, scene, (renderTime) => {
     console.log('Took ' + renderTime + 'ms to render ' + triangleCount + ' triangles')
     profilerState.meshRenderMs.set(renderTime)
     onFinished()
-  }
-
-  let renderTime = 0
-  if (renderer.supportWebGL2) {
-    const gl = renderer.renderContext as WebGL2RenderingContext
-    const ext = gl.getExtension('EXT_disjoint_timer_query_webgl2')
-
-    // Not super well supported, no Safari support
-    if (ext) {
-      const startQuery = gl.createQuery()
-      const endQuery = gl.createQuery()
-
-      if (startQuery && endQuery) {
-        ext.queryCounterEXT(startQuery, ext.TIMESTAMP_EXT)
-        renderer.renderer.render(scene, new OrthographicCamera())
-        ext.queryCounterEXT(endQuery, ext.TIMESTAMP_EXT)
-
-        requestAnimationFrame(function poll() {
-          const available = gl.getQueryParameter(endQuery!, gl.QUERY_RESULT_AVAILABLE)
-          const disjoint = gl.getParameter(ext.GPU_DISJOINT_EXT)
-
-          if (available && !disjoint) {
-            // Gets times in nanoseconds
-            const timeStart = gl.getQueryParameter(startQuery!, gl.QUERY_RESULT)
-            const timeEnd = gl.getQueryParameter(endQuery!, gl.QUERY_RESULT)
-            const renderTime = (timeEnd - timeStart) * 0.000001
-            console.log('Took ' + renderTime + 'ms to render ' + triangleCount + ' triangles')
-            profilerState.meshRenderMs.set(renderTime)
-            onFinished()
-          } else if (disjoint) fallback()
-
-          if (available || disjoint) {
-            gl.deleteQuery(startQuery)
-            gl.deleteQuery(endQuery)
-          } else {
-            requestAnimationFrame(poll)
-          }
-        })
-      } else fallback()
-    } else fallback()
-  } else fallback()
+  })
 
   for (const mesh of meshes) {
     mesh.geometry.dispose()
     ;(mesh.material as MeshBasicMaterial).dispose()
     scene.remove(mesh)
   }
+}
+const [r, g, b] = [255, 255, 255]
+const createPlane = (): Mesh => {
+  const data = new Uint8Array(size * size * 4)
+  for (let i = 0; i < size * size; i++) {
+    const stride = i * 4
+
+    data[stride] = r
+    data[stride + 1] = g
+    data[stride + 2] = b
+    data[stride + 3] = 1
+  }
+  const dataTexture = new DataTexture(data, size, size)
+  const geometry = new PlaneGeometry(1000, 1000)
+  const material = new MeshBasicMaterial({ map: dataTexture })
+  dataTexture.needsUpdate = true
+  return new Mesh(geometry, material)
+}
+
+const size = isMobile ? 1024 : 4096
+const chechTextureRender = (renderer: EngineRenderer, onFinished: () => void) => {
+  const profilerState = getMutableState(ProfilerState)
+  const scene = new Scene()
+
+  const mesh = createPlane()
+  scene.add(mesh)
+
+  checkRender(renderer, scene, (renderTime) => {
+    profilerState.textureRenderMs.set(renderTime)
+    console.log('Took ' + profilerState.textureRenderMs.value + 'ms to render ' + size + 'x' + size + ' texture ')
+    onFinished()
+  })
+
+  mesh.geometry.dispose()
+  ;(mesh.material as MeshBasicMaterial).dispose()
+  scene.remove(mesh)
 }
 
 export const buildProfilerState = async (renderer: EngineRenderer, onFinished: () => void) => {
@@ -132,6 +199,8 @@ export const buildProfilerState = async (renderer: EngineRenderer, onFinished: (
   profilerState.tier.set(gpuTier.tier)
 
   checkMeshRender(renderer, () => {
-    onFinished()
+    chechTextureRender(renderer, () => {
+      onFinished()
+    })
   })
 }
