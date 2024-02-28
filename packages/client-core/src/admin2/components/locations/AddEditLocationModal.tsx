ad90552f1e284a2369b2a38bc41a5aa2bd3cd3b5
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-02-28 09:59:49 +0530

    added admin location in tailwind

---
+++ packages/client-core/src/admin2/components/locations/AddEditLocationModal.tsx
@@ -0,0 +1,169 @@
+/*
+CPAL-1.0 License
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+The Original Code is Ethereal Engine.
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import { PopoverState } from '@etherealengine/client-core/src/common/services/PopoverState'
+import {
+  LocationData,
+  LocationID,
+  LocationType,
+  SceneID,
+  locationPath
+} from '@etherealengine/common/src/schema.type.module'
+import { getMutableState, useHookstate } from '@etherealengine/hyperflux'
+import { useMutation } from '@etherealengine/spatial/src/common/functions/FeathersHooks'
+import Input from '@etherealengine/ui/src/primitives/tailwind/Input'
+import LoadingCircle from '@etherealengine/ui/src/primitives/tailwind/LoadingCircle'
+import Modal from '@etherealengine/ui/src/primitives/tailwind/Modal'
+import Select from '@etherealengine/ui/src/primitives/tailwind/Select'
+import Toggle from '@etherealengine/ui/src/primitives/tailwind/Toggle'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { AdminSceneService, AdminSceneState } from '../../../admin/services/SceneService'
+
+const getDefaultErrors = () => ({
+  name: '',
+  maxUsers: '',
+  scene: ''
+})
+
+export default function AddEditLocationModal({ location }: { location?: LocationType }) {
+  const { t } = useTranslation()
+
+  const locationMutation = useMutation(locationPath)
+
+  const modalProcessing = useHookstate(false)
+  const errors = useHookstate(getDefaultErrors())
+
+  const name = useHookstate(location?.name || '')
+  const maxUsers = useHookstate(location?.maxUsersPerInstance || 0)
+  const scene = useHookstate(location?.sceneId || '')
+  const videoEnabled = useHookstate(location?.locationSetting.videoEnabled || false)
+  const audioEnabled = useHookstate(location?.locationSetting.audioEnabled || false)
+  const screenSharingEnabled = useHookstate(location?.locationSetting.screenSharingEnabled || false)
+
+  const scenes = useHookstate(getMutableState(AdminSceneState).scenes)
+
+  useEffect(() => {
+    AdminSceneService.fetchAdminScenes()
+  }, [])
+
+  const handleSubmit = () => {
+    if (!name.value) {
+      errors.name.set(t('admin:components.location.nameCantEmpty'))
+    }
+    if (!maxUsers.value) {
+      errors.maxUsers.set(t('admin:components.location.maxUserCantEmpty'))
+    }
+    if (!scene.value) {
+      errors.scene.set(t('admin:components.location.sceneCantEmpty'))
+    }
+    if (Object.values(errors.value).some((value) => value.length > 0)) {
+      return
+    }
+
+    const locationData: LocationData = {
+      name: name.value,
+      slugifiedName: '',
+      sceneId: `projects/${scene.value}.scene.json` as SceneID,
+      maxUsersPerInstance: maxUsers.value,
+      locationSetting: {
+        id: '',
+        locationId: '' as LocationID,
+        locationType: location?.locationSetting.locationType as 'private' | 'public' | 'showroom',
+        audioEnabled: audioEnabled.value,
+        screenSharingEnabled: screenSharingEnabled.value,
+        faceStreamingEnabled: false,
+        videoEnabled: videoEnabled.value,
+        createdAt: '',
+        updatedAt: ''
+      },
+      isLobby: false,
+      isFeatured: false
+    }
+
+    if (location?.id) {
+      locationMutation.patch(location.id, locationData)
+    } else {
+      locationMutation.create(locationData)
+    }
+  }
+
+  return (
+    <Modal
+      title={
+        location?.id ? t('admin:components.location.updateLocation') : t('admin:components.location.createLocation')
+      }
+      className="w-[50vw]"
+      onSubmit={handleSubmit}
+      onClose={() => {
+        PopoverState.hidePopupover()
+      }}
+      hideFooter={modalProcessing.value}
+    >
+      {modalProcessing.value ? (
+        <LoadingCircle className="w-[10vw]" />
+      ) : (
+        <div className="grid w-full gap-6">
+          <Input
+            label={t('admin:components.location.lbl-name')}
+            value={name.value}
+            onChange={(event) => name.set(event.target.value)}
+            error={errors.name.value}
+          />
+          <Input
+            type="number"
+            label={t('admin:components.location.lbl-maxuser')}
+            value={maxUsers.value}
+            onChange={(event) => maxUsers.set(Math.max(parseInt(event.target.value, 0), 0))}
+            error={errors.maxUsers.value}
+          />
+          <Select
+            label={t('admin:components.location.lbl-scene')}
+            currentValue={scene.value}
+            onChange={scene.set}
+            options={[
+              ...scenes.value.map((scene) => ({
+                name: `${scene.name} (${scene.project})`,
+                value: `${scene.project}/${scene.name}`
+              })),
+              { value: '', name: t('admin:components.location.selectScene'), disabled: true }
+            ]}
+            error={errors.scene.value}
+          />
+          <Toggle
+            label={t('admin:components.location.lbl-ve')}
+            value={videoEnabled.value}
+            onChange={videoEnabled.set}
+          />
+          <Toggle
+            label={t('admin:components.location.lbl-ae')}
+            value={audioEnabled.value}
+            onChange={audioEnabled.set}
+          />
+          <Toggle
+            label={t('admin:components.location.lbl-se')}
+            value={screenSharingEnabled.value}
+            onChange={screenSharingEnabled.set}
+          />
+        </div>
+      )}
+    </Modal>
+  )
+}
---
+++ packages/client-core/src/admin2/components/locations/LocationTable.tsx
@@ -0,0 +1,107 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { LocationType, SceneID, locationPath } from '@etherealengine/common/src/schema.type.module'
+
+import { PopoverState } from '@etherealengine/client-core/src/common/services/PopoverState'
+import { getMutableState } from '@etherealengine/hyperflux'
+import { useFind, useSearch } from '@etherealengine/spatial/src/common/functions/FeathersHooks'
+import { useHookstate } from '@hookstate/core'
+import { HiPencil, HiTrash } from 'react-icons/hi2'
+import { AuthState } from '../../../user/services/AuthService'
+import { userHasAccess } from '../../../user/userHasAccess'
+import DataTable from '../../common/Table'
+import { LocationRowType, locationColumns } from '../../common/constants/location'
+import AddEditLocationModal from './AddEditLocationModal'
+import RemoveLocationModal from './RemoveLocationModal'
+// import AddEditAvatarModal from './AddEditAvatarModal'
+// import RemoveAvatarModal from './RemoveAvatarModal'
+
+const transformLink = (link: string) => link.toLowerCase().replace(' ', '-')
+
+export default function LocationTable({ search }: { search: string }) {
+  const { t } = useTranslation()
+  const user = useHookstate(getMutableState(AuthState).user)
+
+  const adminLocationQuery = useFind(locationPath, {
+    query: {
+      action: 'admin',
+      $limit: 20,
+      $sort: {
+        name: 1
+      }
+    }
+  })
+
+  useSearch(
+    adminLocationQuery,
+    {
+      name: {
+        $like: `%${search}%`
+      },
+      sceneId: {
+        $like: `%${search}%` as SceneID
+      }
+    },
+    search
+  )
+
+  const createRows = (rows: readonly LocationType[]): LocationRowType[] =>
+    rows.map((row) => ({
+      name: <a href={`/location/${transformLink(row.name)}`}>{row.name}</a>,
+      sceneId: <a href={`/studio/${row.sceneId.split('/')[0]}`}>{row.sceneId}</a>,
+      maxUsersPerInstance: row.maxUsersPerInstance.toString(),
+      scene: row.slugifiedName,
+      locationType: row.locationSetting.locationType,
+      tags: row.isFeatured + ' ' + row.isLobby, // todo
+      videoEnabled: row.locationSetting.videoEnabled
+        ? t('admin:components.common.yes')
+        : t('admin:components.common.no'),
+      action: (
+        <div className="flex items-center justify-around">
+          <button
+            disabled={!userHasAccess('location:write')}
+            title={t('admin:components.common.view')}
+            className="border-theme-primary grid h-8 w-8 rounded-full border"
+            onClick={() => PopoverState.showPopupover(<AddEditLocationModal location={row} />)}
+          >
+            <HiPencil className="place-self-center text-[#0D9488]" />
+          </button>
+          <button
+            title={t('admin:components.common.delete')}
+            className="border-theme-primary grid h-8 w-8 rounded-full border"
+            onClick={() => PopoverState.showPopupover(<RemoveLocationModal location={row} />)}
+          >
+            <HiTrash className="place-self-center text-[#E11D48] dark:text-[#FB7185]" />
+          </button>
+        </div>
+      )
+    }))
+
+  return <DataTable query={adminLocationQuery} columns={locationColumns} rows={createRows(adminLocationQuery.data)} />
+}
