0440b063389b3bb44e23fd5d1c2596baabc0547a
AidanCaruso <jamesajcme@gmail.com>
2024-02-28 15:11:24 -0500

    fixes

--- packages/engine/src/avatar/functions/ditherShaderChunk.ts
+++ packages/engine/src/avatar/functions/ditherShaderChunk.ts
@@ -26,14 +26,14 @@ Ethereal Engine. All Rights Reserved.
 export const ditheringUniform = `  varying vec3 vWorldPosition;`
 
 export const ditheringVertex = `
-worldPosition = modelMatrix * vec4( position, 1.0 );
+worldPosition = modelMatrix * vec4( transformed, 1.0 );
 vWorldPosition = worldPosition.xyz;
 `
 
 /** glsl */
 export const ditheringAlphatestChunk = `
 // sample sine at screen space coordinates for dithering pattern
-float dither = sin( gl_FragCoord.x * 200.00)*sin( gl_FragCoord.y * 200.00);
+float dither = sin( gl_FragCoord.x * 5.00)*sin( gl_FragCoord.y * 5.00);
 float distance = length(cameraPosition - vWorldPosition);
 dither += pow(0.25/distance, 2.0)-1.0;
 diffuseColor.a = smoothstep( alphaTest, alphaTest + fwidth( diffuseColor.a ), diffuseColor.a );
