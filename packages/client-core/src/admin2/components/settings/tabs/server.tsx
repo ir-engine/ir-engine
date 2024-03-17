80f653712bc8c4710c663b60768ee0529d35c800
Appaji <appaji12368@gmail.com>
2024-03-17 13:53:49 +0530

    Add server tab

--- packages/client-core/src/admin2/components/settings/tabs/server.tsx
+++ packages/client-core/src/admin2/components/settings/tabs/server.tsx
@@ -23,13 +23,61 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
+import { serverSettingPath } from '@etherealengine/common/src/schema.type.module'
+import { useHookstate } from '@etherealengine/hyperflux'
+import { useFind, useMutation } from '@etherealengine/spatial/src/common/functions/FeathersHooks'
 import Accordion from '@etherealengine/ui/src/primitives/tailwind/Accordion'
+import Button from '@etherealengine/ui/src/primitives/tailwind/Button'
+import Input from '@etherealengine/ui/src/primitives/tailwind/Input'
+import LoadingCircle from '@etherealengine/ui/src/primitives/tailwind/LoadingCircle'
+import Text from '@etherealengine/ui/src/primitives/tailwind/Text'
+import Toggle from '@etherealengine/ui/src/primitives/tailwind/Toggle'
 import React, { forwardRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { HiMinus, HiPlusSmall } from 'react-icons/hi2'
 
 const ServerTab = forwardRef(({ open }: { open: boolean }, ref: React.MutableRefObject<HTMLDivElement>) => {
   const { t } = useTranslation()
+
+  const serverSetting = useFind(serverSettingPath).data.at(0)
+  console.log('serverSetting', serverSetting)
+  const id = serverSetting?.id
+
+  const gaTrackingId = useHookstate(serverSetting?.gaTrackingId)
+
+  console.log('gaTrackingId', gaTrackingId.value)
+  const githubWebhookSecret = useHookstate(serverSetting?.githubWebhookSecret)
+  const instanceserverUnreachableTimeoutSeconds = useHookstate(serverSetting?.instanceserverUnreachableTimeoutSeconds)
+  const dryRun = useHookstate(true)
+  const local = useHookstate(true)
+
+  const state = useHookstate({
+    loading: false,
+    errorMessage: ''
+  })
+
+  const patchServerSetting = useMutation(serverSettingPath).patch
+
+  const handleSubmit = (event) => {
+    if (!id) return
+    state.loading.set(true)
+    patchServerSetting(id, {
+      gaTrackingId: gaTrackingId.value,
+      githubWebhookSecret: githubWebhookSecret.value,
+      instanceserverUnreachableTimeoutSeconds: instanceserverUnreachableTimeoutSeconds.value
+    })
+      .then(() => {
+        state.set({ loading: false, errorMessage: '' })
+      })
+      .catch((e) => {
+        state.set({ loading: false, errorMessage: e.message })
+      })
+  }
+
+  const handleCancel = () => {
+    gaTrackingId.set(serverSetting?.gaTrackingId)
+    githubWebhookSecret.set(serverSetting?.githubWebhookSecret)
+  }
 
   return (
     <Accordion
@@ -40,7 +88,166 @@ const ServerTab = forwardRef(({ open }: { open: boolean }, ref: React.MutableRef
       ref={ref}
       open={open}
     >
-      <p>Hey</p>
+      <div className="mt-6 grid w-full grid-cols-2 gap-4">
+        <Input
+          containerClassname="col-span-1"
+          value={serverSetting?.mode || 'test'}
+          label={t('admin:components.setting.mode')}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.storageProvider')}
+          value={serverSetting?.storageProvider || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          value={serverSetting?.hostname || 'test'}
+          label={t('admin:components.setting.hostName')}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.googleAnalyticsTrackingId')}
+          value={gaTrackingId.value || ''}
+          onChange={(e) => gaTrackingId.set(e.target.value)}
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.port')}
+          value={serverSetting?.port || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.hub')}
+          value={serverSetting?.hub?.endpoint || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.clientHost')}
+          value={serverSetting?.clientHost || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.url')}
+          value={serverSetting?.url || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.rootDirectory')}
+          value={serverSetting?.rootDir || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.certPath')}
+          value={serverSetting?.certPath || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.publicDirectory')}
+          value={serverSetting?.publicDir || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.keyPath')}
+          value={serverSetting?.keyPath || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.nodeModulesDirectory')}
+          value={serverSetting?.nodeModulesDir || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.githubWebhookSecret')}
+          value={githubWebhookSecret.value || ''}
+          onChange={(e) => githubWebhookSecret.set(e.target.value)}
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.localStorageProvider')}
+          value={serverSetting?.localStorageProvider || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.releaseName')}
+          value={serverSetting?.releaseName || ''}
+          disabled
+        />
+
+        <Input
+          containerClassname="col-span-1"
+          label={t('admin:components.setting.instanceserverUnreachableTimeoutSeconds')}
+          value={instanceserverUnreachableTimeoutSeconds?.value || ''}
+          onChange={(e) => instanceserverUnreachableTimeoutSeconds.set(Number(e.target.value))}
+        />
+
+        <div className="col-span-1 mt-5 grid grid-cols-2">
+          <Toggle
+            className="col-span-1"
+            label={t('admin:components.setting.performDryRun')}
+            value={dryRun.value}
+            disabled
+            onChange={(value) => dryRun.set(value)}
+          />
+
+          <Toggle
+            className="col-span-1"
+            label={t('admin:components.setting.local')}
+            value={local.value}
+            disabled
+            onChange={(value) => local.set(value)}
+          />
+        </div>
+
+        {state.errorMessage.value && (
+          <div className="col-span-2">
+            <Text component="h3" className="text-red-400">
+              {state.errorMessage.value}
+            </Text>
+          </div>
+        )}
+
+        <div className="col-span-1 grid grid-cols-4 gap-6">
+          <Button className="col-span-1" fullWidth onClick={handleCancel}>
+            {t('admin:components.common.cancel')}
+          </Button>
+          <Button
+            className="col-span-1"
+            fullWidth
+            onClick={handleSubmit}
+            startIcon={state.loading.value && <LoadingCircle className="h-6 w-6" />}
+          >
+            {t('admin:components.common.save')}
+          </Button>
+        </div>
+      </div>
     </Accordion>
   )
 })
