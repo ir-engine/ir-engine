6c84b542b37dc5cae23cf2c4162293be63ebe7af
sybiote <ghoshr698@gmail.com>
2024-03-22 02:44:32 +0530

    scene kill height and setting editor improvements

---
+++ packages/engine/src/scene/systems/SceneKillHeightSystem.tsx
@@ -0,0 +1,77 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import {
+  SimulationSystemGroup,
+  UUIDComponent,
+  defineQuery,
+  defineSystem,
+  getComponent,
+  setComponent
+} from '@etherealengine/ecs'
+import { getState } from '@etherealengine/hyperflux'
+import { NetworkObjectAuthorityTag } from '@etherealengine/network'
+import { SpawnPoseState, TransformComponent } from '@etherealengine/spatial'
+import {
+  RigidBodyComponent,
+  RigidBodyFixedTagComponent
+} from '@etherealengine/spatial/src/physics/components/RigidBodyComponent'
+import { Not } from 'bitecs'
+import { SceneSettingsComponent } from '../components/SceneSettingsComponent'
+
+const heightKillApplicableQuery = defineQuery([
+  RigidBodyComponent,
+  NetworkObjectAuthorityTag,
+  Not(RigidBodyFixedTagComponent)
+])
+const settingsQuery = defineQuery([SceneSettingsComponent])
+
+const execute = () => {
+  const settingsEntities = settingsQuery()
+  const sceneKillHeight = settingsEntities.reduce((min, entity) => {
+    return Math.min(min, getComponent(entity, SceneSettingsComponent).sceneKillHeight)
+  }, Number.MAX_VALUE)
+  const killableEntities = heightKillApplicableQuery()
+  for (const entity of killableEntities) {
+    const rigidBodyTransform = getComponent(entity, RigidBodyComponent).position
+    if (rigidBodyTransform.y < sceneKillHeight) {
+      // reset entity
+      const uuid = getComponent(entity, UUIDComponent)
+      const spawnState = getState(SpawnPoseState)[uuid]
+      console.log('reseting ', entity, spawnState?.spawnPosition)
+      setComponent(entity, TransformComponent, {
+        position: spawnState?.spawnPosition,
+        rotation: spawnState?.spawnRotation
+      })
+      TransformComponent.dirtyTransforms[entity] = true
+    }
+  }
+}
+
+export const SceneKillHeightSystem = defineSystem({
+  uuid: 'ee.engine.SceneKillHeightSystem',
+  insert: { before: SimulationSystemGroup },
+  execute
+})
