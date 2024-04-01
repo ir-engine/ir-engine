307e63680dd5d5baa2aef79e058b4f1af93c4a46
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-04-02 02:40:20 +0530

    remove `react-color` and use `react-colorful` as colorinput (#9985)
    * remove `react-color` and use `react-colorful` as colorinput
    
    * also remove from vite config
    
    * Revert "remove `react-color` and use `react-colorful` as colorinput"
    
    This reverts commit 563de0716f87f92988364a93b3246ff730cd60f1.
    
    * replace with @uiw/react-color-sketch
    
    * fix admin, add response types
    
    ---------
    
    Co-authored-by: Josh Field <joshfield999@gmail.com>

--- packages/client-core/src/admin/common/SketchColorPicker.tsx
+++ packages/client-core/src/admin/common/SketchColorPicker.tsx
@@ -23,8 +23,9 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
+import { ColorResult } from '@uiw/color-convert'
+import SketchPicker from '@uiw/react-color-sketch'
 import React, { useEffect, useState } from 'react'
-import { SketchPicker } from 'react-color'
 
 import Button from '@etherealengine/ui/src/primitives/mui/Button'
 import Popover from '@etherealengine/ui/src/primitives/mui/Popover'
@@ -53,9 +54,8 @@ const SketchColorPicker = ({ name, value, onChange }: SketchColorPickerProps) =>
     }
   }, [value])
 
-  const handleChange = (color) => {
-    const rgbaColor = `rgba(${color.rgb.r},${color.rgb.g},${color.rgb.b},${color.rgb.a})`
-
+  const handleChange = (color: ColorResult) => {
+    const rgbaColor = color.hex
     setColor(rgbaColor)
     onChange(rgbaColor)
   }
