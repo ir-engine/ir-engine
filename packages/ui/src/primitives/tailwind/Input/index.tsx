669beca6b642c2366352e19c4b16eb2b17e7ab23
hanzlamateen <hanzlamateen@live.com>
2024-04-09 12:26:18 +0500

    Remove onEndIconClick prop from Input component

--- packages/ui/src/primitives/tailwind/Input/index.tsx
+++ packages/ui/src/primitives/tailwind/Input/index.tsx
@@ -39,7 +39,6 @@ export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   disabled?: boolean
   startComponent?: JSX.Element
   endComponent?: JSX.Element
-  onEndIconClick?: (e: any) => void
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -57,7 +56,6 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       disabled,
       startComponent,
       endComponent,
-      onEndIconClick,
       ...props
     },
     ref
@@ -95,11 +93,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           />
 
           {endComponent && (
-            <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-3.5">
-              <div onClick={onEndIconClick} className={onEndIconClick ? 'pointer-events-auto cursor-pointer' : ''}>
-                {endComponent}
-              </div>
-            </div>
+            <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-3.5">{endComponent}</div>
           )}
         </div>
         {description && <p className="text-theme-secondary self-stretch text-xs">{description}</p>}
