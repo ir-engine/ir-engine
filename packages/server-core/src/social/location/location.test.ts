b617c3f42db697f773f145b8267cdefb9269108c
HexaField <joshfield999@gmail.com>
2024-04-19 11:42:33 +1000

    add tests

--- packages/server-core/src/social/location/location.test.ts
+++ packages/server-core/src/social/location/location.test.ts
@@ -53,20 +53,12 @@ describe('location.test', () => {
   it('should create a new location', async () => {
     const name = `Test Location ${uuidv4()}`
 
-    const project = await app.service('project').find({
-      query: {
-        name: 'default-project'
-      }
-    })
-    if (project.total === 0) throw new Error(`Project default-project not found`)
-    const projectData = project.data[0]
-
     const scene = await app.service(assetPath).create({
       id: uuidv4(),
       name,
       assetURL: 'projects/default-project/test.scene.json',
       thumbnailURL: 'projects/default-project/test.thumbnail.jpg',
-      projectId: projectData.id
+      project: 'default-project'
     })
 
     const item = await app.service(locationPath).create(
--- packages/server-core/tests/util/createTestLocation.ts
+++ packages/server-core/tests/util/createTestLocation.ts
@@ -30,20 +30,11 @@ import { v4 as uuidv4 } from 'uuid'
 export const createTestLocation = async (app: Application, params = { isInternal: true } as any) => {
   const name = `Test Location ${uuidv4()}`
 
-  const project = await app.service('project').find({
-    query: {
-      name: 'default-project'
-    }
-  })
-  if (project.total === 0) throw new Error(`Project default-project not found`)
-  const projectData = project.data[0]
-
   const scene = await app.service(assetPath).create({
     id: uuidv4(),
-    name,
     assetURL: 'projects/default-project/test.scene.json',
     thumbnailURL: 'projects/default-project/test.thumbnail.jpg',
-    projectId: projectData.id
+    project: 'default-project'
   })
 
   return await app.service(locationPath).create(
