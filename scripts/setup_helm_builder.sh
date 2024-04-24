6950a6de435f47ce4b9f39d2fd442d9f08eba8c8
hanzlamateen <hanzlamateen@live.com>
2024-04-24 15:48:04 +0500

    Fixed setup helm in dev-deploy

--- scripts/setup_helm_builder.sh
+++ scripts/setup_helm_builder.sh
@@ -1,7 +1,12 @@
 set -e
 set -x
 
+# https://askubuntu.com/a/980632/1558816
+sudo apt-get -y clean
+rm -rf /var/lib/apt/lists/*
+sudo apt-get -y clean
 sudo apt-get -y update
+sudo apt-get -y upgrade
 
 #install kubectl
 curl -LO "https://dl.k8s.io/release/v1.23.6/bin/linux/amd64/kubectl"
@@ -11,7 +16,6 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-
 
 helm repo add etherealengine https://helm.etherealengine.org
 
