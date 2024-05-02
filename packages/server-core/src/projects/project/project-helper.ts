2b1d9a1e7d7923c2419ce2b0e2ed8fd9b217e32a
MoizAdnan <moizadnan21@gmail.com>
2024-05-02 14:59:29 +0500

    Removed logs

--- packages/server-core/src/projects/project/project-helper.ts
+++ packages/server-core/src/projects/project/project-helper.ts
@@ -1596,8 +1596,6 @@ export const updateProject = async (
 
   const latestProjectResult = await app.service(projectPath).get(returned.id)
 
-  logger.info('PROJECT_HELPER', latestProjectResult)
-
   await syncAllSceneJSONAssets([latestProjectResult], app)
 
   const k8BatchClient = getState(ServerState).k8BatchClient
