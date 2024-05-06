17ab8910e5e4a28881622b06ac676db26b19754f
Murad Khateeb <muradkhateeb788@gmail.com>
2024-05-06 06:01:03 +0500

    Adding additional loggin

--- packages/server-core/src/setting/authentication-setting/authentication-setting.seed.ts
+++ packages/server-core/src/setting/authentication-setting/authentication-setting.seed.ts
@@ -85,12 +85,10 @@ export async function seed(knex: Knex): Promise<void> {
           apple: {
             key: process.env.APPLE_CLIENT_ID,
             secret: process.env.APPLE_CLIENT_SECRET,
-            scope: ['openid', 'name', 'email'],
+            scope: ['openid'],
             response: ['raw', 'jwt'],
-            nonce: true,
             custom_params: {
-              response_type: 'code id_token',
-              response_mode: 'form_post'
+              response_type: 'code'
             }
           },
           discord: {
