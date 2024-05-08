190d2f7551d48748c1ea5683e29f978a52fe8602
Paul Kolesa <hakr@theinfinitereality.com>
2024-05-07 19:09:13 -0700

    Builds and creates middleware-setting table in DB with test values.

---
+++ packages/server-core/src/setting/middleware-setting/migrations/20240508011947_middleware-setting.ts
@@ -0,0 +1,101 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import { middlewareSettingPath } from '@etherealengine/common/src/schemas/setting/middleware-setting.schema'
+import type { Knex } from 'knex'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex: Knex): Promise<void> {
+  const oldTableName = 'middlewareSetting'
+
+  const oldNamedTableExists = await knex.schema.hasTable(oldTableName)
+  if (oldNamedTableExists) {
+    await knex.schema.renameTable(oldTableName, middlewareSettingPath)
+
+    const appleTouchIconColumnExists = await knex.schema.hasColumn(middlewareSettingPath, 'appleTouchIcon')
+    if (!appleTouchIconColumnExists) {
+      await knex.schema.alterTable(middlewareSettingPath, async (table) => {
+        table.string('appleTouchIcon', 255).nullable()
+      })
+    }
+  }
+
+  const tableExists = await knex.schema.hasTable(middlewareSettingPath)
+
+  if (tableExists === false) {
+    await knex.schema.createTable(middlewareSettingPath, (table) => {
+      //@ts-ignore
+      table.uuid('id').collate('utf8mb4_bin').primary()
+      table.string('logo', 255).nullable()
+      table.string('title', 255).nullable()
+      table.string('shortTitle', 255).nullable()
+      table.string('url', 255).nullable()
+      table.string('startPath', 255).notNullable()
+      table.string('releaseName', 255).nullable()
+      table.string('siteDescription', 255).nullable()
+      table.string('appleTouchIcon', 255).nullable()
+      table.string('favicon32px', 255).nullable()
+      table.string('favicon16px', 255).nullable()
+      table.string('icon192px', 255).nullable()
+      table.string('icon512px', 255).nullable()
+      table.string('webmanifestLink', 255).nullable()
+      table.string('swScriptLink', 255).nullable()
+      table.string('appBackground', 255).nullable()
+      table.string('appTitle', 255).nullable()
+      table.string('appSubtitle', 255).nullable()
+      table.string('appDescription', 255).nullable()
+      table.json('appSocialLinks').nullable()
+      table.json('themeSettings').nullable()
+      table.json('themeModes').nullable()
+      table.string('key8thWall', 255).nullable()
+      table.boolean('homepageLinkButtonEnabled').defaultTo(false)
+      table.string('homepageLinkButtonRedirect', 255).nullable()
+      table.string('homepageLinkButtonText', 255).nullable()
+      table.dateTime('createdAt').notNullable()
+      table.dateTime('updatedAt').notNullable()
+    })
+  }
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex: Knex): Promise<void> {
+  const trx = await knex.transaction()
+  await trx.raw('SET FOREIGN_KEY_CHECKS=0')
+
+  const tableExists = await trx.schema.hasTable(middlewareSettingPath)
+
+  if (tableExists === true) {
+    await trx.schema.dropTable(middlewareSettingPath)
+  }
+
+  await trx.raw('SET FOREIGN_KEY_CHECKS=1')
+  await trx.commit()
+}
