21c2d7e18151f78bc9599b23a262444feefa952a
MoizAdnan <moizadnan21@gmail.com>
2024-05-15 16:27:22 +0500

    Removed url

--- packages/editor/src/functions/sceneFunctions.tsx
+++ packages/editor/src/functions/sceneFunctions.tsx
@@ -89,7 +89,7 @@ export const saveSceneGLTF = async (
   const currentSceneDirectory = getState(EditorState).scenePath!.split('/').slice(0, -1).join('/')
   const [[newPath]] = await Promise.all(uploadProjectFiles(projectName, [file], [currentSceneDirectory]).promises)
 
-  const assetURL = new URL(newPath).toString().replace(fileServer, '').slice(1) // remove leading slash
+  const assetURL = newPath.replace(fileServer, '').slice(1) // remove leading slash
 
   if (sceneAssetID) {
     const result = await Engine.instance.api.service(assetPath).patch(sceneAssetID, { assetURL, project: projectName })
