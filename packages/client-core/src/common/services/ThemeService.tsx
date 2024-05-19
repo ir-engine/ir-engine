939faf6219831ece91b432cf9beb6274b35615dd
HexaField <joshfield999@gmail.com>
2024-05-19 11:29:39 +1000

    fix light theme highlight

--- packages/client-core/src/common/services/ThemeService.tsx
+++ packages/client-core/src/common/services/ThemeService.tsx
@@ -30,7 +30,7 @@ import { defineState, getMutableState, syncStateWithLocalStorage, useMutableStat
 const lightTheme = {
   'bg-primary': '#F5F5F5',
   'bg-secondary': '#FFFFFF',
-  'bg-highlight': '#F5F5F5',
+  'bg-highlight': '#D9D9D9',
   'bg-surface-main': '#FFFFFF',
   'bg-surface-input': '#FFFFFF',
   'bg-table-secondary': '#F9FAFB',
