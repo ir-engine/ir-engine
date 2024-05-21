2cd465b90997d7d770db5d7c47bd8094f4c79b42
David Gordon <david@hyperconstruct.io>
2024-05-20 18:11:42 -0700

    MVP assets panel category hierarchy

---
+++ packages/editor/src/components/assets/AssetsPanelCategories.ts
@@ -0,0 +1,136 @@
+export const AssetsPanelCategories = {
+  'Default Prefab': {},
+  Template: {
+    Backdrop: {},
+    Stage: {}
+  },
+  Model: {
+    NPC: {},
+    Avatar: {},
+    Animal: {},
+    Architectural: {
+      Floor: {},
+      Ceiling: {},
+      Wall: {}
+    },
+    'Basic Shape': {
+      Artistic: {},
+      Primitive: {
+        'Platonic Solid': {},
+        Fractal: {}
+      }
+    },
+    Kit: {
+      Gothic: {},
+      Cottage: {},
+      Scifi: {},
+      Modern: {},
+      Nature: {},
+      Ecommerce: {}
+    },
+    Prop: {
+      Furniture: {
+        Chairs: {},
+        Showcase: {},
+        Displays: {}
+      }
+    },
+    Terrain: {}
+  },
+  Material: {
+    Standard: {
+      Plastic: {},
+      Glass: {},
+      'Rough glass': {},
+      Metal: {},
+      Glow: {},
+      Rubber: {},
+      Water: {},
+      Bubble: {},
+      Diamond: {},
+      Clay: {},
+      'Car Paint': {},
+      Wood: {},
+      Stone: {},
+      Dirt: {},
+      Grass: {},
+      Cloth: {},
+      Bark: {},
+      Snow: {}
+    },
+    Advanced: {
+      Static: {},
+      Animated: {}
+    }
+  },
+  Image: {
+    Background: {},
+    Texture: {
+      Diffuse: {},
+      'Normal Maps': {},
+      Occlusion: {},
+      Metalness: {},
+      Roughness: {}
+    },
+    Tiling: {
+      Diffuse: {},
+      'Normal Map': {},
+      Occlusion: {},
+      Metalness: {},
+      Roughness: {}
+    },
+    Sprite: {}
+  },
+  Light: {},
+  Video: {},
+  Audio: {
+    Music: {},
+    'Sound FX': {}
+  },
+  Particle: {
+    Smoke: {},
+    Fire: {},
+    Lightning: {},
+    Portal: {},
+    Sparkle: {}
+  },
+  Skyboxes: {
+    'Time of Day': {
+      Morning: {},
+      Noon: {},
+      Evening: {},
+      Night: {}
+    },
+    Abstract: {},
+    'Low Contrast': {},
+    'High Contrast': {}
+  },
+  'Post Processing': {
+    Fog: {},
+    Cinematic: {}
+  },
+  Agent: {},
+  Genre: {
+    Office: {},
+    Gothic: {},
+    Scifi: {},
+    Cottage: {},
+    Modern: {},
+    Luxury: {},
+    Noir: {},
+    Nature: {
+      Jungle: {},
+      Arctic: {},
+      Boreal: {},
+      Desert: {}
+    },
+    Holiday: {
+      Passover: {},
+      'St Patrick’s Day': {},
+      'Yam Kippur': {},
+      'Veteran’s Day': {}
+    },
+    Abstract: {},
+    Fantasy: {}
+  }
+}
