ca654d22c72bfc50602a3b52c89292743b09c973
Liam Broza <mrhegemon@users.noreply.github.com>
2024-05-23 06:13:57 -0700

    Update ViewportPanel.tsx

--- packages/editor/src/components/panels/ViewportPanel.tsx
+++ packages/editor/src/components/panels/ViewportPanel.tsx
@@ -78,7 +78,7 @@ const ViewPortPanelContent = () => {
     <ViewportDnD />
   ) : (
     <div className={styles.bgImageBlock}>
-      <img src="/static/etherealengine.png" alt="" />
+      <img src="/static/irengine-logo.png" alt="" />
       <h2>{t('editor:selectSceneMsg')}</h2>
     </div>
   )
