d06cd2f087e54c477dab42f9110010cfe6ccb8fe
MoizAdnan <moizadnan21@gmail.com>
2024-05-23 10:28:37 +0500

    Removed export

--- packages/client-core/src/networking/NetworkInstanceProvisioning.tsx
+++ packages/client-core/src/networking/NetworkInstanceProvisioning.tsx
@@ -244,7 +244,7 @@ export const SocialMenus = {
   Messages: 'Messages'
 }
 
-export const SocialMenuFlag = 'ir.client.menu.social' as FeatureFlag
+const SocialMenuFlag = 'ir.client.menu.social' as FeatureFlag
 
 export const FriendMenus = () => {
   const { t } = useTranslation()
