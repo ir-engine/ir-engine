8039c951157807a40f4c6448a1832e7c8d2db2d9
Liam Broza <mrhegemon@users.noreply.github.com>
2024-05-23 06:16:28 -0700

    Update EditorNavbar.tsx

--- packages/editor/src/components/projects/EditorNavbar.tsx
+++ packages/editor/src/components/projects/EditorNavbar.tsx
@@ -40,7 +40,7 @@ export const EditorNavbar = () => {
       <div className={styles.navContainer}>
         <div
           className={styles.logoBlock}
-          style={{ backgroundImage: 'url(/static/irengine-logo.png)' }}
+          style={{ backgroundImage: 'url(/static/irengine-icon.png)' }}
           onClick={routeHome}
         ></div>
         <EditorNavbarProfile />
