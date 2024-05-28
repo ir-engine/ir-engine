3de13f0e1848346baf136ef4caac35746ff341d4
sybiote <ghoshr698@gmail.com>
2024-05-28 17:16:39 +0530

    remove unnecessary clone

--- packages/editor/src/constants/GizmoPresets.ts
+++ packages/editor/src/constants/GizmoPresets.ts
@@ -1,3 +1,28 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
 import { TransformMode } from '@etherealengine/engine/src/scene/constants/transformConstants'
 import { ObjectLayers } from '@etherealengine/spatial/src/renderer/constants/ObjectLayers'
 import {
@@ -229,7 +254,7 @@ export function setupGizmo(gizmoMap) {
 
   for (const name in gizmoMap) {
     for (let i = gizmoMap[name].length; i--; ) {
-      const object = gizmoMap[name][i][0].clone()
+      const object = gizmoMap[name][i][0]
       const position = gizmoMap[name][i][1]
       const rotation = gizmoMap[name][i][2]
       const scale = gizmoMap[name][i][3]
