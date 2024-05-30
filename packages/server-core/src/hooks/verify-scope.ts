dea2c180e796a1fce794bd834ab9f9fc1eb6bc4d
hanzlamateen <hanzlamateen@live.com>
2024-05-30 11:31:43 +0500

    Updated verify scope hook to conditionally not throw exception

--- packages/server-core/src/hooks/verify-scope.ts
+++ packages/server-core/src/hooks/verify-scope.ts
@@ -31,11 +31,16 @@ import { UserType } from '@etherealengine/common/src/schemas/user/user.schema'
 
 import { Application } from '../../declarations'
 
-export default (currentType: string, scopeToVerify: string) => {
+export default (currentType: string, scopeToVerify: string, throwError = true) => {
   return async (context: HookContext<Application>) => {
     if (context.params.isInternal) return context
+
     const loggedInUser = context.params.user as UserType
-    if (!loggedInUser || !loggedInUser.id) throw new NotAuthenticated('No logged in user')
+    if (!loggedInUser || !loggedInUser.id) {
+      if (throwError) throw new NotAuthenticated('No logged in user')
+      else return false
+    }
+
     const scopes = (await context.app.service(scopePath).find({
       query: {
         userId: loggedInUser.id
@@ -43,16 +48,28 @@ export default (currentType: string, scopeToVerify: string) => {
       user: loggedInUser,
       paginate: false
     })) as ScopeTypeInterface[]
-    if (!scopes || scopes.length === 0) throw new NotFound('No scope available for the current user.')
+
+    if (!scopes || scopes.length === 0) {
+      if (throwError) throw new NotFound('No scope available for the current user.')
+      else return false
+    }
 
     const currentScopes = scopes.reduce<string[]>((result, sc) => {
       if (sc.type.split(':')[0] === currentType) result.push(sc.type.split(':')[1])
       return result
     }, [])
+
     if (!currentScopes.includes(scopeToVerify)) {
-      if (scopeToVerify === 'admin') throw new Forbidden('Must be admin to perform this action')
-      else throw new Forbidden(`Unauthorized ${scopeToVerify} action on ${currentType}`)
+      if (throwError) {
+        let errorMessage = `Unauthorized ${scopeToVerify} action on ${currentType}`
+        if (scopeToVerify === 'admin') {
+          errorMessage = 'Must be admin to perform this action'
+        }
+
+        throw new Forbidden(errorMessage)
+      } else return false
     }
+
     return context
   }
 }
