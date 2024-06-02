4444028ed718bac8974e9a9459c4f5c75251c8c5
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-06-02 08:21:46 +0200

    fix: `RigidBodyComponent.enabledRotations` allowed type coercion
    `RigidBodyComponent.onSet` allowed type modification/coercion when passing an array of `length === 3` that has incorrectly typed values.
    This fix should stop `@ts-ignore` from accidentally modifying values that shouldn't be modified.

--- packages/spatial/src/physics/components/RigidBodyComponent.ts
+++ packages/spatial/src/physics/components/RigidBodyComponent.ts
@@ -90,8 +90,15 @@ export const RigidBodyComponent = defineComponent({
     if (typeof json.allowRolling === 'boolean') component.allowRolling.set(json.allowRolling)
     if (typeof json.canSleep === 'boolean') component.canSleep.set(json.canSleep)
     if (typeof json.gravityScale === 'number') component.gravityScale.set(json.gravityScale)
-    if (Array.isArray(json.enabledRotations) && json.enabledRotations.length === 3)
+    if (
+      Array.isArray(json.enabledRotations) &&
+      json.enabledRotations.length === 3 &&
+      typeof json.enabledRotations[0] === 'boolean' &&
+      typeof json.enabledRotations[1] === 'boolean' &&
+      typeof json.enabledRotations[2] === 'boolean'
+    ) {
       component.enabledRotations.set(json.enabledRotations)
+    }
   },
 
   toJSON: (entity, component) => {
