2028013a90cdbcf12c84a51e93781dffb62ca541
Daniel Belmes <danielbelmes@ymail.com>
2024-06-04 14:39:15 -0700

    remove duplicate svg

--- packages/client-core/src/admin/adminRoutes.tsx
+++ packages/client-core/src/admin/adminRoutes.tsx
@@ -56,11 +56,7 @@ const AdminTopBar = () => {
 
   return (
     <div className="flex h-16 w-full items-center justify-between bg-theme-surface-main px-8 py-4">
-      <img
-        src={theme.value === 'dark' ? 'static/ir-dark.svg' : 'static/ir-light.svg'}
-        alt="iR Engine Logo"
-        className="h-7 w-7"
-      />
+      <img src="static/ir.svg" alt="iR Engine Logo" className={`h-7 w-7${theme.value === 'light' ? ' invert' : ''}`} />
       <div className="">
         <Button onClick={toggleTheme} className="pointer-events-auto bg-transparent p-0">
           {theme.value === 'light' ? (
