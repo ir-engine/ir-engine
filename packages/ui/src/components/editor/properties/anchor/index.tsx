68b56b9c547dcaae4a6b1c1cc339f9428896490a
sybiote <ghoshr698@gmail.com>
2024-06-05 17:20:24 +0530

    sdf, anchor, instance and system

---
+++ packages/ui/src/components/editor/properties/anchor/index.tsx
@@ -0,0 +1,62 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { useComponent } from '@etherealengine/ecs/src/ComponentFunctions'
+import {
+  EditorComponentType,
+  commitProperty,
+  updateProperty
+} from '@etherealengine/editor/src/components/properties/Util'
+import { PersistentAnchorComponent } from '@etherealengine/spatial/src/xr/XRAnchorComponents'
+import InputGroup from '../../input/Group'
+import StringInput from '../../input/String'
+import NodeEditor from '../nodeEditor'
+
+export const PersistentAnchorNodeEditor: EditorComponentType = (props) => {
+  const { t } = useTranslation()
+
+  const anchor = useComponent(props.entity, PersistentAnchorComponent)
+
+  return (
+    <NodeEditor
+      {...props}
+      name={t('editor:properties.persistent-anchor.name')}
+      description={t('editor:properties.persistent-anchor.description')}
+    >
+      <InputGroup name="Volume" label={t('editor:properties.persistent-anchor.lbl-name')}>
+        <StringInput
+          value={anchor.name.value}
+          onChange={updateProperty(PersistentAnchorComponent, 'name')}
+          onRelease={commitProperty(PersistentAnchorComponent, 'name')}
+        />
+      </InputGroup>
+    </NodeEditor>
+  )
+}
+
+export default PersistentAnchorNodeEditor
