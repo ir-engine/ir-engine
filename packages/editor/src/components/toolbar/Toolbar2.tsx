ba946ad351dfd7a59e547cf7513534940dfcceb4
Andy Chen <andychen5671@gmail.com>
2024-06-06 09:19:32 -0400

    update nav logo

--- packages/editor/src/components/toolbar/Toolbar2.tsx
+++ packages/editor/src/components/toolbar/Toolbar2.tsx
@@ -125,12 +125,12 @@ export default function Toolbar() {
       <div className="flex items-center justify-between bg-theme-primary">
         <div className="flex items-center">
           <div
-            className="ml-3 mr-6 rounded-md bg-[#212226] p-1"
+            className="ml-3 mr-6"
             onClick={() => {
               // todo: redirect to studio
             }}
           >
-            <img src="static/ir.svg" alt="iR Engine Logo" className={`h-5 w-5 opacity-50`} />
+            <img src="favicon-32x32.png" alt="iR Engine Logo" className={`h-7 w-7 opacity-50`} />
           </div>
           <Button
             endIcon={<MdOutlineKeyboardArrowDown size="1em" className="-ml-3 text-[#A3A3A3]" />}
