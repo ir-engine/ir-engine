9b966534dab34801f0f0a9deb81fc1c176248add
Hurairah Mateen <hurairahmateen@live.com>
2024-06-10 18:08:50 +0500

    IR-48 Created a Tailwind AutoComplete component and updated Radio Component (#10254)
    * Add Autocomplete component
    
    * Update Autocomplete dropdown positioning
    
    * Update Autocomplete dropdown width
    
    * Refactor AutoComplete component to include onChange prop
    
    * Update Autocomplete options structure
    
    * Add useRef hook to track option selection in AutoComplete component
    
    * Refactor Radio component to include a description
    
    * Refactor AutoComplete component to remove unused code
    
    * Fix Autocomplete dropdown visibility issue
    
    * Fix project permission type bug
    
    * Fix project permission type in patch
    
    * IR-2538 Updated project-permission service to include createdBy column (#10346)
    
    * Add createdBy column to project-permission  service
    
    * Update migration name
    
    ---------
    
    Co-authored-by: Hanzla Mateen <hanzlamateen@live.com>
    
    * Cleaned autocomplete
    
    * feat: Refactor project-permission.hooks.ts to use explicit type casting
    
    * Updated tests
    
    * feat: Add 'type' parameter to createPermission function in ProjectService
    
    ---------
    
    Co-authored-by: Hanzla Mateen <hanzlamateen@live.com>

--- packages/common/src/schemas/projects/project-permission.schema.ts
+++ packages/common/src/schemas/projects/project-permission.schema.ts
@@ -47,6 +47,9 @@ export const projectPermissionSchema = Type.Object(
     userId: TypedString<UserID>({
       format: 'uuid'
     }),
+    createdBy: TypedString<UserID>({
+      format: 'uuid'
+    }),
     type: Type.String(),
     user: Type.Ref(userSchema),
     createdAt: Type.String({ format: 'date-time' }),
@@ -59,7 +62,7 @@ export interface ProjectPermissionType extends Static<typeof projectPermissionSc
 export interface ProjectPermissionDatabaseType extends Omit<ProjectPermissionType, 'user'> {}
 
 // Schema for creating new entries
-export const projectPermissionDataProperties = Type.Partial(projectPermissionSchema)
+export const projectPermissionDataProperties = Type.Pick(projectPermissionSchema, ['projectId', 'userId', 'type'])
 
 export const projectPermissionDataSchema = Type.Intersect(
   [
@@ -76,7 +79,7 @@ export const projectPermissionDataSchema = Type.Intersect(
 export interface ProjectPermissionData extends Static<typeof projectPermissionDataSchema> {}
 
 // Schema for updating existing entries
-export const projectPermissionPatchSchema = Type.Partial(projectPermissionSchema, {
+export const projectPermissionPatchSchema = Type.Pick(projectPermissionSchema, ['type'], {
   $id: 'ProjectPermissionPatch'
 })
 export interface ProjectPermissionPatch extends Static<typeof projectPermissionPatchSchema> {}
@@ -86,6 +89,7 @@ export const projectPermissionQueryProperties = Type.Pick(projectPermissionSchem
   'id',
   'projectId',
   'userId',
+  'createdBy',
   'type'
 ])
 export const projectPermissionQuerySchema = Type.Intersect(
