5e263f0e57d1ca13efb6b8013ef91dd62cf795a6
Michael Floyd <mbfloyd@theinfinitereality.com>
2024-06-12 13:49:18 -0700

    removed dev testing code

--- packages/spatial/src/renderer/WebGLRendererSystem.tsx
+++ packages/spatial/src/renderer/WebGLRendererSystem.tsx
@@ -285,7 +285,6 @@ export const getSceneParameters = (entities: Entity[]) => {
       vals.fog = getComponent(entity, FogComponent)
     }
     if (hasComponent(entity, GroupComponent)) {
-      const test = getComponent(entity, GroupComponent)
       vals.children.push(...getComponent(entity, GroupComponent)!)
     }
   }
