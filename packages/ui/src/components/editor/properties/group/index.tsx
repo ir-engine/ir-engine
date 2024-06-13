a29b00204ca43ffe34950854eab86650fd8944d6
sybiote <ghoshr698@gmail.com>
2024-06-13 22:09:14 +0530

    fix remove button placement

--- packages/ui/src/components/editor/properties/group/index.tsx
+++ packages/ui/src/components/editor/properties/group/index.tsx
@@ -44,7 +44,7 @@ const PropertyGroup = ({ name, icon, description, children, onClose, ...rest }: 
 
   return (
     <div className="justify-left flex w-full flex-col items-start rounded border-solid bg-[#242424] px-4 py-1.5">
-      <div className="flex items-center gap-2 text-[#FAFAFA]">
+      <div className="flex w-full items-center gap-2 text-[#FAFAFA]">
         <Button
           onClick={() => setMinimized(!minimized)}
           variant="outline"
