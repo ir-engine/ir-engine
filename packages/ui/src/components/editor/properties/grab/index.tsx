40bd008934d190e11f2b3893fdc4a6817e1a5d36
Rahul Ghosh <ghoshr698@gmail.com>
2024-06-14 13:32:10 +0530

    Update remaining node editors (#10324)
    * updated, scene, fog, render , media and audio  settings editor
    
    * boolean input fix and processing editor
    
    * primitive geometry node editor updated
    
    * model and model transform ( incomplete )
    
    * grab and interact editors
    
    * sdf, anchor, instance and system
    
    * particle and varaint
    
    * fix incorrect import
    
    * replace mui icons
    
    * remove type
    
    * replace spinner
    
    * pass check-errors
    
    * change name for 3drotaation icon to pass github action
    
    * add icons for all components
    
    * add facer icon
    
    * fix wrong import
    
    * media icon
    
    * change import name to pass check
    
    * switch out transform icon
    
    * varaint fix
    
    * fix remove button placement
    
    * add new node editors
    
    * chnage skeych picker backgorund
    
    ---------
    
    Co-authored-by: Daniel Belmes <3631206+DanielBelmes@users.noreply.github.com>
    Co-authored-by: Josh Field <joshfield999@gmail.com>

---
+++ packages/ui/src/components/editor/properties/grab/index.tsx
@@ -0,0 +1,72 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { isClient } from '@etherealengine/common/src/utils/getEnvironment'
+import { getComponent, hasComponent, UUIDComponent } from '@etherealengine/ecs'
+import { EditorComponentType } from '@etherealengine/editor/src/components/properties/Util'
+import { EditorControlFunctions } from '@etherealengine/editor/src/functions/EditorControlFunctions'
+import { GrabbableComponent } from '@etherealengine/engine/src/interaction/components/GrabbableComponent'
+import { InteractableComponent } from '@etherealengine/engine/src/interaction/components/InteractableComponent'
+import { grabbableInteractMessage } from '@etherealengine/engine/src/interaction/functions/grabbableFunctions'
+import { GiGrab } from 'react-icons/gi'
+import NodeEditor from '../nodeEditor'
+
+export const GrabbableComponentNodeEditor: EditorComponentType = (props) => {
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    if (isClient) {
+      if (!hasComponent(props.entity, InteractableComponent)) {
+        EditorControlFunctions.addOrRemoveComponent([props.entity], InteractableComponent, true, {
+          label: grabbableInteractMessage,
+          callbacks: [
+            {
+              callbackID: GrabbableComponent.grabbableCallbackName,
+              target: getComponent(props.entity, UUIDComponent)
+            }
+          ]
+        })
+      }
+    }
+  }, [])
+
+  return (
+    <NodeEditor
+      {...props}
+      name={t('editor:properties.grabbable.name')}
+      description={t('editor:properties.grabbable.description')}
+      icon={<GrabbableComponentNodeEditor.iconComponent />}
+    >
+      <div id={'grabbable-component'}></div>
+    </NodeEditor>
+  )
+}
+
+GrabbableComponentNodeEditor.iconComponent = GiGrab
+
+export default GrabbableComponentNodeEditor
