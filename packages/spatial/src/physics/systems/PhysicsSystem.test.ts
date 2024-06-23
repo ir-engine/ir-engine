105def1108dc55e69fbca5caf4efce5a53cc5811
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-06-23 23:22:00 +0200

    tst: UnitTests for `PhysicsSystem.smoothKinematicPose`:`multiplier 0`

--- packages/spatial/src/physics/systems/PhysicsSystem.test.ts
+++ packages/spatial/src/physics/systems/PhysicsSystem.test.ts
@@ -26,32 +26,175 @@ Ethereal Engine. All Rights Reserved.
 import { destroyEngine } from '@etherealengine/ecs/src/Engine'
 import { getMutableState } from '@etherealengine/hyperflux'
 
-import { SystemUUID } from '@etherealengine/ecs'
+import { World } from '@dimforge/rapier3d-compat'
+import {
+  Entity,
+  SystemUUID,
+  UndefinedEntity,
+  createEntity,
+  getComponent,
+  getMutableComponent,
+  removeEntity,
+  setComponent
+} from '@etherealengine/ecs'
 import assert from 'assert'
+import { Quaternion, Vector3 } from 'three'
+import { TransformComponent } from '../../SpatialModule'
+import { Vector3_Zero } from '../../common/constants/MathConstants'
 import { createEngine } from '../../initializeEngine'
 import { Physics } from '../classes/Physics'
+import { assertVecAllApproxNotEq, assertVecApproxEq } from '../classes/Physics.test'
+import { RigidBodyComponent } from '../components/RigidBodyComponent'
 import { PhysicsState } from '../state/PhysicsState'
-import { PhysicsSystem } from './PhysicsSystem'
+import { PhysicsSystem, smoothKinematicBody } from './PhysicsSystem'
 
-/**
- * @todo
- */
-describe.skip('PhysicsSystem', () => {
+// Epsilon Constants for Interpolation
+const LerpEpsilon = 0.000001
+/** @note three.js Quat.slerp fails tests at 6 significant figures, but passes at 5 */
+const SLerpEpsilon = 0.00001
+
+describe('smoothKinematicBody', () => {
+  type Step = { dt: number; substep: number }
+  function createStep(dt: number, substep: number): Step {
+    // @note Just an alias for readability
+    return { dt, substep }
+  }
+
+  function createExpectedLinear(entity: Entity, step: Step) {
+    const body = getComponent(entity, RigidBodyComponent)
+    const result = {
+      position: body.previousPosition.clone().lerp(body.targetKinematicPosition.clone(), step.substep).clone(),
+      rotation: body.previousRotation.clone().slerp(body.targetKinematicRotation.clone(), step.substep).clone()
+    }
+    return result
+  }
+
+  const DeltaTime = 1 / 60
+  const Start = {
+    position: new Vector3(1, 2, 3),
+    rotation: new Quaternion(0.5, 0.3, 0.2, 0.0).normalize()
+  }
+  const Final = {
+    position: new Vector3(4, 5, 6),
+    rotation: new Quaternion(0.0, 0.2, 0.8, 0.0).normalize()
+  }
+
+  const Step = {
+    Tenth: createStep(DeltaTime, 0.1),
+    Quarter: createStep(DeltaTime, 0.25),
+    Half: createStep(DeltaTime, 0.5),
+    One: createStep(DeltaTime, 1),
+    Two: createStep(DeltaTime, 2)
+  }
+
+  let testEntity = UndefinedEntity
+  let physicsWorld = undefined as World | undefined
+
   beforeEach(async () => {
     createEngine()
     await Physics.load()
-    getMutableState(PhysicsState).physicsWorld.set(Physics.createWorld())
+    physicsWorld = Physics.createWorld()
+    physicsWorld.timestep = DeltaTime
+    getMutableState(PhysicsState).physicsWorld.set(physicsWorld)
+
+    testEntity = createEntity()
+    setComponent(testEntity, TransformComponent)
+    setComponent(testEntity, RigidBodyComponent)
+    // Set the Start..Final values for interpolation
+    const body = getComponent(testEntity, RigidBodyComponent)
+    body.previousPosition.set(Start.position.x, Start.position.y, Start.position.z)
+    body.previousRotation.set(Start.rotation.x, Start.rotation.y, Start.rotation.z, Start.rotation.w)
+    body.targetKinematicPosition.set(Final.position.x, Final.position.y, Final.position.z)
+    body.targetKinematicRotation.set(Final.rotation.x, Final.rotation.y, Final.rotation.z, Final.rotation.w)
   })
 
   afterEach(() => {
+    removeEntity(testEntity)
     return destroyEngine()
   })
-})
 
-describe('smoothKinematicBody', () => {
-  // @todo
-  // it("should interpolate the position of the KinematicBody of the given entity", () => {})
-  // it("should interpolate the rotation of the KinematicBody of the given entity", () => {})
+  describe('when RigidbodyComponent.targetKinematicLerpMultiplier is set to 0 ...', () => {
+    beforeEach(() => {
+      getMutableComponent(testEntity, RigidBodyComponent).targetKinematicLerpMultiplier.set(0)
+    })
+
+    it('... should apply deterministic linear interpolation to the position of the KinematicBody of the given entity', () => {
+      // Check data before
+      const body = getComponent(testEntity, RigidBodyComponent)
+      const before = body.position.clone()
+      assertVecApproxEq(before, Vector3_Zero, 3, LerpEpsilon)
+
+      // Run and Check resulting data
+      smoothKinematicBody(testEntity, Step.Quarter.dt, Step.Quarter.substep)
+      const after = body.position.clone()
+      assertVecAllApproxNotEq(before, after, 3, LerpEpsilon)
+      assertVecApproxEq(after, createExpectedLinear(testEntity, Step.Quarter).position, 3, LerpEpsilon)
+      // Check the other Step cases
+      getComponent(testEntity, RigidBodyComponent).position.set(0, 0, 0) // reset for next case
+      smoothKinematicBody(testEntity, Step.Tenth.dt, Step.Tenth.substep)
+      assertVecApproxEq(body.position.clone(), createExpectedLinear(testEntity, Step.Tenth).position, 3, LerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).position.set(0, 0, 0) // reset for next case
+      smoothKinematicBody(testEntity, Step.Half.dt, Step.Half.substep)
+      assertVecApproxEq(body.position.clone(), createExpectedLinear(testEntity, Step.Half).position, 3, LerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).position.set(0, 0, 0) // reset for next case
+      smoothKinematicBody(testEntity, Step.One.dt, Step.One.substep)
+      assertVecApproxEq(body.position.clone(), createExpectedLinear(testEntity, Step.One).position, 3, LerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).position.set(0, 0, 0) // reset for next case
+      smoothKinematicBody(testEntity, Step.Two.dt, Step.Two.substep)
+      assertVecApproxEq(body.position.clone(), createExpectedLinear(testEntity, Step.Two).position, 3, LerpEpsilon)
+      // Check substep precision Step cases
+      const TestCount = 1_000_000
+      for (let divider = 1; divider <= TestCount; divider += 1_000) {
+        const step = createStep(DeltaTime, 1 / divider)
+        getComponent(testEntity, RigidBodyComponent).position.set(0, 0, 0) // reset for next case
+        smoothKinematicBody(testEntity, step.dt, step.substep)
+        assertVecApproxEq(body.position.clone(), createExpectedLinear(testEntity, step).position, 3, LerpEpsilon)
+      }
+    })
+
+    it('... should apply deterministic spherical linear interpolation to the rotation of the KinematicBody of the given entity', () => {
+      // Check data before
+      const body = getComponent(testEntity, RigidBodyComponent)
+      const before = body.rotation.clone()
+      assertVecApproxEq(before, new Quaternion(0, 0, 0, 1), 3, SLerpEpsilon)
+
+      // Run and Check resulting data
+      smoothKinematicBody(testEntity, Step.Quarter.dt, Step.Quarter.substep)
+      const after = body.rotation.clone()
+      assertVecAllApproxNotEq(before, after, 4, SLerpEpsilon)
+      assertVecApproxEq(after, createExpectedLinear(testEntity, Step.Quarter).rotation, 4, SLerpEpsilon)
+      // Check the other Step cases
+      getComponent(testEntity, RigidBodyComponent).rotation.set(0, 0, 0, 1) // reset for next case
+      smoothKinematicBody(testEntity, Step.Tenth.dt, Step.Tenth.substep)
+      assertVecApproxEq(body.rotation.clone(), createExpectedLinear(testEntity, Step.Tenth).rotation, 4, SLerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).rotation.set(0, 0, 0, 1) // reset for next case
+      smoothKinematicBody(testEntity, Step.Half.dt, Step.Half.substep)
+      assertVecApproxEq(body.rotation.clone(), createExpectedLinear(testEntity, Step.Half).rotation, 4, SLerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).rotation.set(0, 0, 0, 1) // reset for next case
+      smoothKinematicBody(testEntity, Step.One.dt, Step.One.substep)
+      assertVecApproxEq(body.rotation.clone(), createExpectedLinear(testEntity, Step.One).rotation, 4, SLerpEpsilon)
+      getComponent(testEntity, RigidBodyComponent).rotation.set(0, 0, 0, 1) // reset for next case
+      smoothKinematicBody(testEntity, Step.Two.dt, Step.Two.substep)
+      assertVecApproxEq(body.rotation.clone(), createExpectedLinear(testEntity, Step.Two).rotation, 4, SLerpEpsilon)
+      // Check substep precision Step cases
+      const TestCount = 1_000_000
+      for (let divider = 1; divider <= TestCount; divider += 1_000) {
+        const step = createStep(DeltaTime, 1 / divider)
+        getComponent(testEntity, RigidBodyComponent).rotation.set(0, 0, 0, 1) // reset for next case
+        smoothKinematicBody(testEntity, step.dt, step.substep)
+        assertVecApproxEq(body.rotation.clone(), createExpectedLinear(testEntity, step).rotation, 4, SLerpEpsilon)
+      }
+    })
+  })
+
+  describe('when RigidbodyComponent.targetKinematicLerpMultiplier is set to a value other than 0 ...', () => {
+    /** @todo LerpMultiplier 0  */
+    /** @todo LerpMultiplier 1  */
+    /** @todo LerpMultiplier ?? */
+    // getMutableComponent(testEntity, RigidBodyComponent).targetKinematicLerpMultiplier.set(5)
+    // it("... should apply gradual smoothing (aka exponential interpolation) to the position of the KinematicBody of the given entity", () => {})
+    // it("... should apply gradual smoothing (aka exponential interpolation) to the rotation of the KinematicBody of the given entity", () => {})
+  })
 })
 
 describe('PhysicsSystem', () => {
@@ -63,5 +206,8 @@ describe('PhysicsSystem', () => {
 
   describe('execute', () => {})
 
-  describe('reactor', () => {})
+  /**
+  // @note The reactor is currently just binding data onMount and onUnmount
+  // describe('reactor', () => {})
+  */
 })
