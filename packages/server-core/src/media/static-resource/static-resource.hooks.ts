7fcc34b5c1e5e16416618698300916e792220421
Daniel Belmes <danielbelmes@ymail.com>
2024-06-27 10:02:44 -0700

    action admin not needed

--- packages/server-core/src/media/static-resource/static-resource.hooks.ts
+++ packages/server-core/src/media/static-resource/static-resource.hooks.ts
@@ -32,7 +32,6 @@ import { HookContext } from '../../../declarations'
 import checkScope from '../../hooks/check-scope'
 import collectAnalytics from '../../hooks/collect-analytics'
 import enableClientPagination from '../../hooks/enable-client-pagination'
-import isAction from '../../hooks/is-action'
 import resolveProjectId from '../../hooks/resolve-project-id'
 import resolveProjectsByPermission from '../../hooks/resolveProjectsByPermission'
 import setLoggedinUserInBody from '../../hooks/set-loggedin-user-in-body'
@@ -182,7 +181,7 @@ export default {
       iff(
         isProvider('external'),
         iffElse(
-          (ctx: HookContext) => isAction('admin')(ctx) && checkScope('static_resource', 'read')(ctx),
+          checkScope('static_resource', 'read'),
           [],
           [
             iffElse(
