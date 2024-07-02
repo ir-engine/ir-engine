8dba8d57ad0d4bca244715da8de27d1e0d3d8cc1
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-07-02 11:39:29 +0200

    tst: UnitTests for `InputComponent` (phase 0)

--- packages/spatial/src/input/components/InputComponent.test.tsx
+++ packages/spatial/src/input/components/InputComponent.test.tsx
@@ -31,40 +31,132 @@ import {
   hasComponent,
   setComponent
 } from '@etherealengine/ecs/src/ComponentFunctions'
-import { Engine, destroyEngine } from '@etherealengine/ecs/src/Engine'
-import { ReactorReconciler } from '@etherealengine/hyperflux'
+import { destroyEngine } from '@etherealengine/ecs/src/Engine'
+import { ReactorReconciler, getState } from '@etherealengine/hyperflux'
 
+import { Entity, EntityUUID, UndefinedEntity, createEntity, removeEntity } from '@etherealengine/ecs'
 import { createEngine } from '@etherealengine/ecs/src/Engine'
+import { EngineState } from '../../EngineState'
 import { initializeSpatialEngine } from '../../initializeEngine'
+import { assertArrayEqual } from '../../physics/components/RigidBodyComponent.test'
 import { HighlightComponent } from '../../renderer/components/HighlightComponent'
 import { InputComponent } from './InputComponent'
 
+const InputComponentDefaults = {
+  inputSinks: ['Self'] as EntityUUID[],
+  activationDistance: 2,
+  highlight: true,
+  grow: false,
+  inputSources: [] as Entity[]
+}
+function assertInputComponentEq(A, B): void {
+  assertArrayEqual(A.inputSinks, B.inputSinks)
+  assert.equal(A.activationDistance, B.activationDistance)
+  assert.equal(A.highlight, B.highlight)
+  assert.equal(A.grow, B.grow)
+  assertArrayEqual(A.inputSources, B.inputSources)
+}
+
 describe('InputComponent', () => {
-  beforeEach(() => {
-    createEngine()
-    initializeSpatialEngine()
+  describe('IDs', () => {
+    it('should initialize the InputComponent.name field with the expected value', () => {
+      assert.equal(InputComponent.name, 'InputComponent')
+    })
   })
 
-  it('test input component', async () => {
-    const entity = Engine.instance.localFloorEntity
+  describe('onInit', () => {
+    let testEntity = UndefinedEntity
 
-    const json = { highlight: true, grow: true }
-    ReactorReconciler.flushSync(() => {
-      setComponent(entity, InputComponent, json)
-    })
-    const inputComponent = getComponent(entity, InputComponent)
-
-    assert(inputComponent.grow === json.grow)
-    assert(inputComponent.highlight === json.highlight)
-
-    ReactorReconciler.flushSync(() => {
-      getMutableComponent(entity, InputComponent).inputSources.merge([entity])
+    beforeEach(async () => {
+      createEngine()
+      testEntity = createEntity()
+      setComponent(testEntity, InputComponent)
     })
 
-    assert(hasComponent(entity, HighlightComponent))
-  })
+    afterEach(() => {
+      removeEntity(testEntity)
+      return destroyEngine()
+    })
 
-  afterEach(() => {
-    return destroyEngine()
+    it('should initialize the component with the expected default values', () => {
+      const data = getComponent(testEntity, InputComponent)
+      assertInputComponentEq(data, InputComponentDefaults)
+    })
+  }) // << onInit
+
+  describe('onSet', () => {
+    let testEntity = UndefinedEntity
+
+    beforeEach(async () => {
+      createEngine()
+      testEntity = createEntity()
+      setComponent(testEntity, InputComponent)
+    })
+
+    afterEach(() => {
+      removeEntity(testEntity)
+      return destroyEngine()
+    })
+
+    it('should change the values of an initialized InputComponent', () => {
+      const before = getComponent(testEntity, InputComponent)
+      assertInputComponentEq(before, InputComponentDefaults)
+      const Expected = {
+        inputSinks: ['SomeUUID'] as EntityUUID[],
+        activationDistance: 10_000,
+        highlight: false,
+        grow: true,
+        inputSources: [] as Entity[]
+      }
+      setComponent(testEntity, InputComponent, Expected)
+      const after = getComponent(testEntity, InputComponent)
+      assertInputComponentEq(after, Expected)
+    })
+
+    it('should not change values of an initialized InputComponent when the data passed had incorrect types', () => {
+      const before = getComponent(testEntity, InputComponent)
+      assertInputComponentEq(before, InputComponentDefaults)
+      const Incorrect = {
+        inputSinks: false,
+        activationDistance: false,
+        highlight: 46 & 2,
+        grow: 'invalid',
+        inputSources: [] as Entity[]
+      }
+      // @ts-ignore Override the linter to force-send invalid types
+      setComponent(testEntity, InputComponent, Incorrect)
+      const after = getComponent(testEntity, InputComponent)
+      assertInputComponentEq(after, InputComponentDefaults)
+    })
+  }) // << onSet
+
+  describe('reactor', () => {
+    beforeEach(() => {
+      createEngine()
+      initializeSpatialEngine()
+    })
+
+    afterEach(() => {
+      return destroyEngine()
+    })
+
+    it('should add a HighlightComponent to the entity when the InputComponent is set with `highlight: true', async () => {
+      const entity = getState(EngineState).localFloorEntity
+
+      const Expected = { highlight: true, grow: true }
+      ReactorReconciler.flushSync(() => {
+        setComponent(entity, InputComponent, Expected)
+      })
+      const result = getComponent(entity, InputComponent)
+
+      assert.equal(result.grow, Expected.grow)
+      assert.equal(result.highlight, Expected.highlight)
+
+      ReactorReconciler.flushSync(() => {
+        getMutableComponent(entity, InputComponent).inputSources.merge([entity])
+      })
+
+      assert(hasComponent(entity, HighlightComponent))
+    })
   })
 })
