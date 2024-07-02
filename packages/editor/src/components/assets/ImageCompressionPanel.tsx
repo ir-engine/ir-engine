d31c666fb1caa2b4e00bd880fad6b6bb5926b371
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-07-02 14:35:49 +0530

    fix check-errors

--- packages/editor/src/components/assets/ImageCompressionPanel.tsx
+++ packages/editor/src/components/assets/ImageCompressionPanel.tsx
@@ -141,8 +141,10 @@ export default function ImageCompressionPanel({
 
   return (
     <div className="max-h-[80vh] w-[35vw] overflow-y-auto rounded-xl bg-[#0E0F11]">
-      <div className="relative flex items-center justify-center px-8 py-3">
-        <Text className="leading-6">{t('editor:properties.model.transform.compressImage')}</Text>
+      <div className="relative mb-3 flex items-center justify-center px-8 py-3">
+        <Text className="leading-6">
+          {t('editor:properties.model.transform.compressImage', { name: selectedFile.fullName })}
+        </Text>
         <Button
           variant="outline"
           className="absolute right-0 border-0 dark:bg-transparent dark:text-[#A3A3A3]"
