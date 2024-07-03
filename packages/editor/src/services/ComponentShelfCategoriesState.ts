bb11640cbc94468b19486a0ad473744b6b80d20c
dtlehrer <19999194+dtlehrer@users.noreply.github.com>
2024-07-03 02:11:22 -0500

    Update ComponentShelfCategoriesState.ts

--- packages/editor/src/services/ComponentShelfCategoriesState.ts
+++ packages/editor/src/services/ComponentShelfCategoriesState.ts
@@ -134,7 +134,6 @@ export const ComponentShelfCategoriesState = defineState({
         ScreenshareTargetComponent,
         LookAtComponent
       ]
-      // Ecommerce: [ECommerceComponent]
     } as Record<string, Component[]>
   }
 })
