787a9051942a9b82b808fcc9db480583c32534b1
hanzlamateen <hanzlamateen@live.com>
2024-07-08 10:43:49 +0500

    Made propertyValue optional

--- packages/server-core/src/hooks/is-in-context.ts
+++ packages/server-core/src/hooks/is-in-context.ts
@@ -27,21 +27,27 @@ import { HookContext } from '../../declarations'
 
 /**
  * This hook is used to check a value in the context.
+ * If propertyValue is not provided then it will just
+ * check if that property exists.
  * If searchIn is not provided then it will search for
  * it in the query.
  */
-export default (propertyName: string, propertyValue: any, searchIn?: 'query' | 'params' | 'data') => {
+export default (propertyName: string, propertyValue?: any, searchIn?: 'query' | 'params' | 'data') => {
   return (context: HookContext): boolean => {
     if (searchIn === 'data') {
       if (Array.isArray(context.data)) {
-        return context.data.find((item) => item[propertyName] && item[propertyName] === propertyValue)
+        return context.data.find(
+          (item) => item[propertyName] && (!propertyValue || item[propertyName] === propertyValue)
+        )
       } else {
-        return context.data[propertyName] && context.data[propertyName] === propertyValue
+        return context.data[propertyName] && (!propertyValue || context.data[propertyName] === propertyValue)
       }
     } else if (searchIn === 'params') {
-      return context.params[propertyName] && context.params[propertyName] === propertyValue
+      return context.params[propertyName] && (!propertyValue || context.params[propertyName] === propertyValue)
     } else {
-      return context.params.query[propertyName] && context.params.query[propertyName] === propertyValue
+      return (
+        context.params.query[propertyName] && (!propertyValue || context.params.query[propertyName] === propertyValue)
+      )
     }
   }
 }
