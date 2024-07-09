98001391068be648ec65aea8a4561f8d441c6b80
Jiatong Yao <jiatong@ethereal.com>
2024-07-09 17:59:11 -0500

    clean up

--- packages/ui/src/components/editor/panels/Assets/container/index.tsx
+++ packages/ui/src/components/editor/panels/Assets/container/index.tsx
@@ -399,7 +399,7 @@ const AssetPanel = () => {
                       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
                       .join(' ')}%`
                   }
-                }
+                } //
               ])
             }
           : undefined,
