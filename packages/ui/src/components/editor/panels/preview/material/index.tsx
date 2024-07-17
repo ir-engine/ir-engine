ba2c1dafc3584e23b6ab0ef18f213986f56f9a5e
Michael Floyd <mbfloyd@theinfinitereality.com>
2024-07-17 14:35:51 -0700

    updated the material preview height so that the list of materials could still be seen when one is selected

--- packages/ui/src/components/editor/panels/preview/material/index.tsx
+++ packages/ui/src/components/editor/panels/preview/material/index.tsx
@@ -94,7 +94,7 @@ export const MaterialPreviewCanvas = () => {
 
   return (
     <>
-      <div id="materialPreview" className="aspect-square h-full min-h-[100px] w-full">
+      <div id="materialPreview" className="aspect-square h-full max-h-[200px] min-h-[100px] w-full">
         <canvas ref={panelRef} className="pointer-events-auto" />
       </div>
     </>
