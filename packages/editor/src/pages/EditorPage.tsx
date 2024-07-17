f73ed7792dc8d18c241c78229827eb63be916c63
sybiote <ghoshr698@gmail.com>
2024-07-17 21:53:07 +0530

    merge conflict resolution

--- packages/editor/src/pages/EditorPage.tsx
+++ packages/editor/src/pages/EditorPage.tsx
@@ -39,6 +39,7 @@ export const useStudioEditor = () => {
   const engineReady = useHookstate(false)
 
   useEffect(() => {
+    getMutableState(EngineState).isEditor.set(true)
     getMutableState(EngineState).isEditing.set(true)
     loadEngineInjection().then(() => {
       engineReady.set(true)
