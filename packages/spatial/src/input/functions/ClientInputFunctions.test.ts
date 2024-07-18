f354e71a7ca7cccaeee1be37d789cbd03be802c2
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-07-18 03:28:08 +0200

    tst: Split `ClientInputSystem.test.tsx` into multiple modules

---
+++ packages/spatial/src/input/functions/ClientInputFunctions.test.ts
@@ -0,0 +1,141 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+import {
+  createEngine,
+  createEntity,
+  destroyEngine,
+  Entity,
+  getComponent,
+  removeEntity,
+  setComponent,
+  UndefinedEntity
+} from '@etherealengine/ecs'
+import assert from 'assert'
+import sinon from 'sinon'
+import { EntityTreeComponent } from '../../transform/components/EntityTree'
+import { InputComponent } from '../components/InputComponent'
+import { InputSourceComponent } from '../components/InputSourceComponent'
+import ClientInputFunctions from './ClientInputFunctions'
+
+describe('ClientInputFunctions', () => {
+  describe('preventDefault', () => {
+    it('should call the preventDefault function of the given object/event', () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy }
+      assert.ok(!eventSpy.called)
+      ClientInputFunctions.preventDefault(Event)
+      assert.ok(eventSpy.called)
+    })
+  })
+
+  describe('preventDefaultKeyDown', () => {
+    /**
+    // @todo document.activeElement.tagName is readonly. How do we test this?
+    it("should do nothing if document.activeElement.tagName is INPUT or TEXTAREA", () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy }
+      assert.ok(!eventSpy.called)
+      document.activeElement?.tagName = 'INPUT'
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, false)
+      document.activeElement?.tagName = 'TEXTAREA'
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, false)
+    })
+    */
+
+    it('should call the preventDefault function of the event/object when its code property is Tab', () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy, code: 'Tab' }
+      assert.ok(!eventSpy.called)
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, true)
+    })
+
+    it('should call the preventDefault function of the event/object when its code property is Space', () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy, code: 'Space' }
+      assert.ok(!eventSpy.called)
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, true)
+    })
+
+    it('should call the preventDefault function of the event/object when its code property is Enter', () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy, code: 'Enter' }
+      assert.ok(!eventSpy.called)
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, true)
+    })
+
+    it('should not call the preventDefault function of the event/object when its code property is an unexpected value', () => {
+      const eventSpy = sinon.spy()
+      const Event = { preventDefault: eventSpy, code: 'UnexpectedValue' }
+      assert.ok(!eventSpy.called)
+      ClientInputFunctions.preventDefaultKeyDown(Event)
+      assert.equal(eventSpy.called, false)
+    })
+  })
+
+  describe('setInputSources', () => {
+    let testEntity = UndefinedEntity
+
+    beforeEach(async () => {
+      createEngine()
+      testEntity = createEntity()
+    })
+
+    afterEach(() => {
+      removeEntity(testEntity)
+      return destroyEngine()
+    })
+
+    it('should add the `@param inputSources` to the `InputComponent.inputSources` of each entity in the ancestor.InputComponent.inputSinks list', () => {
+      const sourceOne = createEntity()
+      const sourceTwo = createEntity()
+      setComponent(sourceOne, InputSourceComponent)
+      setComponent(sourceTwo, InputSourceComponent)
+      const SourcesList = [sourceOne, sourceTwo] as Entity[]
+
+      const parentEntity = createEntity()
+      setComponent(parentEntity, InputComponent)
+      setComponent(testEntity, EntityTreeComponent, { parentEntity: parentEntity })
+
+      // Sanity check before running
+      const before = getComponent(parentEntity, InputComponent).inputSources
+      for (const source of SourcesList) {
+        assert.equal(before.includes(source), false)
+      }
+
+      // Run the function and chech the result
+      ClientInputFunctions.setInputSources(parentEntity, SourcesList)
+      const result = getComponent(parentEntity, InputComponent).inputSources
+      for (const source of SourcesList) {
+        assert.equal(result.includes(source), true)
+      }
+    })
+  })
+})
---
+++ packages/spatial/src/input/functions/ClientInputHeuristics.test.ts
@@ -0,0 +1,95 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and 
+provide for limited attribution for the Original Developer. In addition, 
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Ethereal Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Ethereal Engine team.
+
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
+*/
+
+describe('ClientInputHeuristics', () => {
+  /**
+  // @todo
+  describe("assignInputSources", () => {})
+  describe("applyRaycastedInputHeuristics", () => {
+    // todo??
+    // set the `@param quaternion` to the sourceEid.worldRotation
+    // set the raycast direction to ObjectDirection.Forward rotated by sourceEid.worldRotation
+    // set the `@param raycast`.direction as the origin+(direction scaled by -0.01)
+    // set the ray to go from sourceEid.origin to raycast.direction
+    // set the raycaster to go from sourceEid.origin to raycast.direction
+    // set the scene layer in the raycaster
+    // test cases
+    // should apply the editor heuristic only when EngineState.isEditing is true
+    // should apply the XRUI heuristic only when EngineState.isEditing is false
+    // should apply the PhysicsColliders heuristic only when EngineState.isEditing is false
+    // should apply the BBoxes heuristic only when EngineState.isEditing is false
+    // should always apply the Meshes heuristic
+  })
+  describe("applyHeuristicProximity", () => {})
+
+  describe("applyHeuristicEditor", () => {
+    // Find the list of gizmoPickerObjects.entity for the gizmo heuristic    (Input, Visible, Group, TransformGizmo)
+    // Find the list of inputObjects  (Input, Visible, Group)
+    // if there are gizmoPickerObjects,
+    //   objects will be their GroupComponent arrays combined
+    //   the raycaster will enable ObjectLayers.TransformGizmo
+    // else:
+    //   objects will be the combined GroupComponent arrays of the inputObjects list
+    //   the raycaster will disable ObjectLayers.TransformGizmo
+    // ... for all hits of `@param caster`.intersectObjects( objects, recursive )
+      // find the first ancestor of the object hit that doesn't have a parent
+      // should not do anything if the ancestor object we found does not belong to an entity
+      // should add the parentObject.entity and hit.distance to the `@param intersectionData` for every object hit by the `@param caster`
+  })
+
+  // execute.(smaller functions)
+  describe("applyHeuristicXRUI", () => {
+    // for every entity of xruiQuery ...
+      // get the XRUIComponent of the entity, and do a WebContainer3D.hitTest with the `@param ray`
+      // should not do anything if ...
+      // ... we didn't hit anything
+      // ... we hit something, but its intersection.object is not marked as visible
+      // ... we hit something, but the material.opacity of the its intersection.object is less than 0.01
+      // should add the xruiQuery.entity and layerHit.intersection.distance to the `@param intersectionData`
+      //   for every object hit by the `@param caster`
+  })
+  describe("applyHeuristicPhysicsColliders", () => {
+    it("should not do anything if there is no PhysicsState.physicsWorld", () => {})
+    it("should not do anything if the given `@param raycast` does not hit any entities in the current PhysicsState.physicsWorld", () => {})
+    it("should add the hit.entity and hit.distance to the `@param intersectionData` for every hit of the `@param raycast`", () => {})
+  })
+  describe("applyHeuristicBBoxes", () => {
+    describe("for every entity stored in the InputState.inputBoundingBoxes Set<Entity> ...", () => {
+      it("... should not run if the entity does not have a BoundingBoxComponent", () => {})
+      it("... should not run if casting the `@param ray` towards `@param hitTarget` would not intersect the boundingBox of the entity", () => {})
+      it("... should add an entry to `@param intersectionData` containing the entity that was hit, and the distance to the hit (found with `ray.origin.distanceTo(hitTarget)`)", () => {})
+    })
+  })
+  describe("applyHeuristicMeshes", () => {
+    // when `@param isEditing` is true ...
+    // ... for the GroupComponents of all entities in the `meshesQuery()` that have a GroupComponent
+    // when `@param isEditing` is false ...
+    // ... for the objects contained in the GroupComponents, of all entities in the Array.from(InputState.inputMeshes) that have a GroupComponent
+    // ... for all hits of `@param caster`.intersectObjects( objects, recursive )
+      // should not do anything if the object hit does not have an entity or an ancestor with an entity
+      // should add the parentObject.entity and hit.distance to the `@param intersectionData` for every object hit by the `@param caster`", () => {})
+  })
+  */
+})
