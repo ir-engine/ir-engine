c695af1cacf7f77e8143cbc5d06eaa0c71a9717d
David Gordon <94419856+dinomut1@users.noreply.github.com>
2024-07-17 20:16:41 -0400

    fix ignore geometry to not delete all meshes (#10639)

--- packages/engine/src/scene/components/VideoComponent.tsx
+++ packages/engine/src/scene/components/VideoComponent.tsx
@@ -275,6 +275,7 @@ function VideoReactor() {
     const videoEntity = videoMeshEntity.value
     video.videoMeshEntity.set(videoEntity)
     mesh.name.set(`video-group-${entity}`)
+    mesh.userData['ignoreOnExport'] = true
     setComponent(videoEntity, EntityTreeComponent, { parentEntity: entity })
     setComponent(videoEntity, NameComponent, mesh.name.value)
     return () => {
