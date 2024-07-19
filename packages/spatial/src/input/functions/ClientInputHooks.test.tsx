2adf85e411c3935906f1fff08d4afc3d1a7ffb1d
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-07-19 23:13:01 +0200

    tst: UnitTests for `ClientInputHooks.useNonSpatialInputSources`

--- packages/spatial/src/input/functions/ClientInputHooks.test.tsx
+++ packages/spatial/src/input/functions/ClientInputHooks.test.tsx
@@ -23,10 +23,307 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
+import {
+  createEngine,
+  createEntity,
+  destroyEngine,
+  removeEntity,
+  setComponent,
+  UndefinedEntity
+} from '@etherealengine/ecs'
+import { startReactor } from '@etherealengine/hyperflux'
+import assert from 'assert'
+import sinon from 'sinon'
+import { MockEventListener } from '../../../tests/util/MockEventListener'
+import { VisibleComponent } from '../../renderer/components/VisibleComponent'
+import { TransformComponent } from '../../SpatialModule'
+import ClientInputHooks from './ClientInputHooks'
+
 describe('ClientInputHooks', () => {
+  describe('useNonSpatialInputSources', () => {
+    let testEntity = UndefinedEntity
+    let ev: MockEventListener
+
+    beforeEach(async () => {
+      createEngine()
+      testEntity = createEntity()
+      setComponent(testEntity, TransformComponent)
+      setComponent(testEntity, VisibleComponent)
+
+      ev = new MockEventListener()
+      globalThis.document.addEventListener = ev.addEventListener as any
+      globalThis.document.removeEventListener = ev.removeEventListener as any
+    })
+
+    afterEach(() => {
+      removeEntity(testEntity)
+      return destroyEngine()
+    })
+
+    it('should add a DOMMouseScroll EventListener to the DOM when mounted', () => {
+      const EvName = 'DOMMouseScroll'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should add a gesturestart EventListener to the DOM when mounted', () => {
+      const EvName = 'gesturestart'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should add a keyup EventListener to the DOM when mounted', () => {
+      const EvName = 'keyup'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should add two keydown EventListeners to the DOM when mounted', () => {
+      const EvName = 'keydown'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      assert.equal(ev.listeners[EvName].length, 2)
+    })
+
+    it('should add a touchstickmove EventListener to the DOM when mounted', () => {
+      const EvName = 'touchstickmove'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should add a touchgamepadbuttonup EventListener to the DOM when mounted', () => {
+      const EvName = 'touchgamepadbuttonup'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should add a touchgamepadbuttondown EventListener to the DOM when mounted', () => {
+      const EvName = 'touchgamepadbuttondown'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+    })
+
+    it('should remove the DOMMouseScroll EventListener from the DOM when mounted', () => {
+      const EvName = 'DOMMouseScroll'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    it('should remove the gesturestart EventListener from the DOM when mounted', () => {
+      const EvName = 'gesturestart'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    it('should remove the keyup EventListener from the DOM when mounted', () => {
+      const EvName = 'keyup'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    /** @todo Pending review. The implementation of the cleanup for this event seems broken */
+    it('should remove both keydown EventListeners from the DOM when mounted', () => {
+      const EvName = 'keydown'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    it('should remove the touchstickmove EventListener from the DOM when mounted', () => {
+      const EvName = 'touchstickmove'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    it('should remove the touchgamepadbuttonup EventListener from the DOM when mounted', () => {
+      const EvName = 'touchgamepadbuttonup'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+
+    it('should remove the touchgamepadbuttondown EventListener from the DOM when mounted', () => {
+      const EvName = 'touchgamepadbuttondown'
+      const reactorSpy = sinon.spy()
+      // Create the Reactor setup
+      const Reactor = () => {
+        reactorSpy()
+        ClientInputHooks.useNonSpatialInputSources()
+        return null
+      }
+      assert.equal(reactorSpy.callCount, 0)
+      assert.equal(ev.hasEvent(EvName), false)
+      // Create a reactor root to run the hook's reactor.
+      const root = startReactor(Reactor)
+      assert.equal(reactorSpy.callCount, 1)
+      assert.equal(ev.hasEvent(EvName), true)
+      const fiber = root.fiber
+      root.stop()
+      assert.equal(ev.hasEvent(EvName), false)
+    })
+  })
+
   /**
   // @todo
-  describe("useNonSpatialInputSources", () => {})
   describe("useGamepadInputSources", () => {})
   describe("useXRInputSources", () => {})
   describe("CanvasInputReactor", () => {})
