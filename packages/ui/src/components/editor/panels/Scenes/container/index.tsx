d3365cae1424acdca971785c6ef45023dda544fb
Andy Chen <andychen5671@gmail.com>
2024-07-25 13:59:35 -0400

    reusable sceneitem component

--- packages/ui/src/components/editor/panels/Scenes/container/index.tsx
+++ packages/ui/src/components/editor/panels/Scenes/container/index.tsx
@@ -107,83 +107,92 @@ export default function ScenesPanel() {
           <div className="relative h-full flex-1 overflow-y-auto px-4 py-3 pb-8">
             <div className="flex flex-wrap gap-4 pb-8">
               {scenes.map((scene) => (
-                <div
-                  key={scene.id}
-                  className="col-span-2 inline-flex h-64 w-64 cursor-pointer flex-col gap-2 rounded-lg bg-[#191B1F] p-3 lg:col-span-1"
-                >
-                  <img
-                    src={scene.thumbnailURL ?? 'static/ir.svg'}
-                    alt={scene.key}
-                    onError={(e) => {
-                      e.currentTarget.src = 'static/ir.svg'
-                    }}
-                    crossOrigin="anonymous"
-                    className="object-fit block h-44 w-full grow cursor-pointer self-center rounded-lg"
-                    onClick={() => onClickScene(scene)}
-                  />
-                  <div className="inline-flex justify-between self-stretch px-1">
-                    <div className="inline-flex w-full flex-col items-start justify-start">
-                      <Text className="truncate text-sm text-[#A3A3A3]">{getSceneName(scene)}</Text>
-                      <Text fontSize="xs" fontWeight="light" className="h-3.5 w-40 leading-5 text-neutral-100">
-                        {t('multitenancy:scene.edited')}{' '}
-                        {t('common:timeAgo', { time: timeAgo(new Date(scene.updatedAt)) })}
-                      </Text>
-                    </div>
-                    <div className="relative">
-                      <Popup
-                        keepInside
-                        trigger={
-                          <Button
-                            variant="transparent"
-                            startIcon={<BsThreeDotsVertical />}
-                            iconContainerClassName="mx-0"
-                            className="px-0"
-                          />
-                        }
-                      >
-                        <div className="flex flex-col">
-                          <Button
-                            variant="outline"
-                            size="small"
-                            fullWidth
-                            onClick={() => {
-                              PopoverState.showPopupover(
-                                <RenameSceneModal
-                                  sceneName={getSceneName(scene)}
-                                  refetch={scenesQuery.refetch}
-                                  scene={scene}
-                                />
-                              )
-                            }}
-                          >
-                            {t('editor:hierarchy.lbl-rename')}
-                          </Button>
-                          <Button
-                            variant="outline"
-                            size="small"
-                            fullWidth
-                            onClick={() => {
-                              {
-                                PopoverState.showPopupover(
-                                  <ConfirmDialog
-                                    text={t('editor:hierarchy.lbl-deleteScene', { sceneName: getSceneName(scene) })}
-                                    onSubmit={async () => deleteSelectedScene(scene)}
-                                  />
-                                )
-                              }
-                            }}
-                          >
-                            {t('editor:hierarchy.lbl-delete')}
-                          </Button>
-                        </div>
-                      </Popup>
-                    </div>
-                  </div>
-                </div>
+                <SceneItem
+                  scene={scene}
+                  handleOpenScene={() => onClickScene(scene)}
+                  refetchProjectsData={scenesQuery.refetch}
+                  getSceneName={getSceneName}
+                  deleteSelectedScene={deleteSelectedScene}
+                />
               ))}
             </div>
           </div>
         )}
+      </div>
+    </div>
+  )
+}
+
+export function SceneItem({ scene, handleOpenScene, refetchProjectsData, getSceneName, deleteSelectedScene }) {
+  const { t } = useTranslation()
+
+  return (
+    <div
+      key={scene.id}
+      className="col-span-2 inline-flex h-64 w-64 cursor-pointer flex-col gap-2 rounded-lg bg-[#191B1F] p-3 lg:col-span-1"
+    >
+      <img
+        src={scene.thumbnailURL ?? 'static/ir.svg'}
+        alt={scene.key}
+        onError={(e) => {
+          e.currentTarget.src = 'static/ir.svg'
+        }}
+        crossOrigin="anonymous"
+        className="object-fit block h-44 w-full grow cursor-pointer self-center rounded"
+        onClick={handleOpenScene}
+      />
+      <div className="inline-flex justify-between self-stretch px-1">
+        <div className="inline-flex w-full flex-col items-start justify-start">
+          <Text className="truncate text-sm text-[#A3A3A3]">{getSceneName(scene)}</Text>
+          <Text fontSize="xs" fontWeight="light" className="h-3.5 w-40 leading-5 text-neutral-100">
+            {t('multitenancy:scene.edited')} {t('common:timeAgo', { time: timeAgo(new Date(scene.updatedAt)) })}
+          </Text>
+        </div>
+        <div className="relative">
+          <Popup
+            keepInside
+            trigger={
+              <Button
+                variant="transparent"
+                startIcon={<BsThreeDotsVertical />}
+                iconContainerClassName="mx-0"
+                className="px-0"
+              />
+            }
+          >
+            <div className="flex flex-col">
+              <Button
+                variant="outline"
+                size="small"
+                fullWidth
+                onClick={() => {
+                  PopoverState.showPopupover(
+                    <RenameSceneModal sceneName={getSceneName(scene)} refetch={refetchProjectsData} scene={scene} />
+                  )
+                }}
+              >
+                {t('editor:hierarchy.lbl-rename')}
+              </Button>
+              <Button
+                variant="outline"
+                size="small"
+                fullWidth
+                onClick={() => {
+                  {
+                    PopoverState.showPopupover(
+                      <ConfirmDialog
+                        text={t('editor:hierarchy.lbl-deleteScene', { sceneName: getSceneName(scene) })}
+                        onSubmit={async () => deleteSelectedScene(scene)}
+                      />
+                    )
+                  }
+                }}
+              >
+                {t('editor:hierarchy.lbl-delete')}
+              </Button>
+            </div>
+          </Popup>
+        </div>
       </div>
     </div>
   )
