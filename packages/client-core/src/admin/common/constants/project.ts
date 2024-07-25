020dec16d7de9d1fe09f4933d7212352947e8f75
MoizAdnan <moizadnan21@gmail.com>
2024-07-25 15:40:30 +0500

    feat: Add project visibility column to admin table

--- packages/client-core/src/admin/common/constants/project.ts
+++ packages/client-core/src/admin/common/constants/project.ts
@@ -27,7 +27,7 @@ import { t } from 'i18next'
 
 import { ITableHeadCell } from '../Table'
 
-type IdType = 'name' | 'projectVersion' | 'enabled' | 'commitSHA' | 'commitDate' | 'actions'
+type IdType = 'name' | 'projectVersion' | 'enabled' | 'visibility' | 'commitSHA' | 'commitDate' | 'actions'
 
 export type ProjectRowType = Record<IdType, string | JSX.Element | undefined>
 
@@ -39,6 +39,7 @@ export const projectsColumns: IProjectColumn[] = [
   { id: 'name', sortable: true, label: t('admin:components.project.columns.name') },
   { id: 'projectVersion', label: t('admin:components.project.columns.projectVersion') },
   { id: 'enabled', label: t('admin:components.project.columns.enabled') },
+  { id: 'visibility', label: t('admin:components.project.columns.visibility') },
   { id: 'commitSHA', label: t('admin:components.project.columns.commitSHA') },
   { id: 'commitDate', sortable: true, label: t('admin:components.project.columns.commitDate') },
   { id: 'actions', label: t('admin:components.project.columns.actions') }
