6bc5901de2bbc8eb8996fa94c4087570b7e5d393
Andy Chen <andychen5671@gmail.com>
2024-07-25 11:09:06 -0400

    set some asset icons

--- packages/ui/src/components/editor/panels/Assets/icons.tsx
+++ packages/ui/src/components/editor/panels/Assets/icons.tsx
@@ -24,25 +24,20 @@ Ethereal Engine. All Rights Reserved.
 */
 
 import { BsStars } from 'react-icons/bs'
+import { FaRegCircle } from 'react-icons/fa6'
 import { FiSun } from 'react-icons/fi'
 import { LuWaves } from 'react-icons/lu'
+import { PiMountains } from 'react-icons/pi'
 import { RxCube } from 'react-icons/rx'
 import { TbMaximize, TbRoute } from 'react-icons/tb'
 
 import React from 'react'
 
 export const iconMap: { [key: string]: React.ReactElement } = {
-  Models: <RxCube />,
-  // 'Characters': <FaUser />,
-  // 'Environment': <FaTree />,
-  // 'Props': <FaCouch />,
-  // 'Everyday Objects': <FaUtensils />,
-  // 'Appliances': <FaHome />,
-  // 'Interiors': <FaCouch />,
-  // 'Elements': <FaPaintBrush />,
-  // 'Materials': <FaImages />,
-  Textures: <LuWaves />,
-  // 'Images': <FaImages />,
+  Model: <RxCube />,
+  Material: <FaRegCircle />,
+  Texture: <LuWaves />,
+  Image: <PiMountains />,
   Lighting: <FiSun />,
   'Particle system': <BsStars />,
   'Visual script': <TbRoute />
