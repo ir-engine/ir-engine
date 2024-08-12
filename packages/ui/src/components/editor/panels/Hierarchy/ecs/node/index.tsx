8d8d51183b50e75ffdd1f7ea4db54f96ed5bcbed
sybiote <ghoshr698@gmail.com>
2024-08-13 05:14:52 +0530

    remove context menu and visisbilty from new ecs panel

--- packages/ui/src/components/editor/panels/Hierarchy/ecs/node/index.tsx
+++ packages/ui/src/components/editor/panels/Hierarchy/ecs/node/index.tsx
@@ -23,41 +23,26 @@ All portions of the code written by the Ethereal Engine team are Copyright © 20
 Ethereal Engine. All Rights Reserved.
 */
 
-import React, { KeyboardEvent, StyleHTMLAttributes, useEffect } from 'react'
-import { DropTargetMonitor, useDrag, useDrop } from 'react-dnd'
-import { getEmptyImage } from 'react-dnd-html5-backend'
+import React, { KeyboardEvent, StyleHTMLAttributes } from 'react'
 
-import {
-  getAllComponents,
-  getComponent,
-  getOptionalComponent,
-  hasComponent,
-  useComponent,
-  useOptionalComponent
-} from '@etherealengine/ecs/src/ComponentFunctions'
+import { getAllComponents, useComponent, useOptionalComponent } from '@etherealengine/ecs/src/ComponentFunctions'
 import { Entity } from '@etherealengine/ecs/src/Entity'
 import { entityExists } from '@etherealengine/ecs/src/EntityFunctions'
 import { NameComponent } from '@etherealengine/spatial/src/common/NameComponent'
-import { EntityTreeComponent, isAncestor } from '@etherealengine/spatial/src/transform/components/EntityTree'
-import { PiEyeBold, PiEyeClosedBold } from 'react-icons/pi'
 
 import { MdKeyboardArrowDown, MdKeyboardArrowRight } from 'react-icons/md'
 
 import { getMutableState, getState, useHookstate } from '@etherealengine/hyperflux'
 
 import { UUIDComponent } from '@etherealengine/ecs'
-import { FileDataType } from '@etherealengine/editor/src/components/assets/FileBrowser/FileDataType'
 import useUpload from '@etherealengine/editor/src/components/assets/useUpload'
 import { HierarchyTreeNodeType } from '@etherealengine/editor/src/components/hierarchy/HierarchyTreeWalker'
-import { ItemTypes, SupportedFileTypes } from '@etherealengine/editor/src/constants/AssetTypes'
-import { EditorControlFunctions } from '@etherealengine/editor/src/functions/EditorControlFunctions'
-import { addMediaNode } from '@etherealengine/editor/src/functions/addMediaNode'
+import { ItemTypes } from '@etherealengine/editor/src/constants/AssetTypes'
 import { ComponentEditorsState } from '@etherealengine/editor/src/services/ComponentEditors'
 import { SelectionState } from '@etherealengine/editor/src/services/SelectionServices'
-import { VisibleComponent, setVisibleComponent } from '@etherealengine/spatial/src/renderer/components/VisibleComponent'
+import { VisibleComponent } from '@etherealengine/spatial/src/renderer/components/VisibleComponent'
 import { twMerge } from 'tailwind-merge'
 import TransformPropertyGroup from '../../../../properties/transform'
-import { DnDFileType } from '../../../Files/container'
 
 /**
  * getNodeElId function provides id for node.
@@ -109,15 +94,6 @@ export const HierarchyTreeNode = (props: HierarchyTreeNodeProps) => {
 
   const visible = useOptionalComponent(entity, VisibleComponent)
 
-  const toggleVisible = () => {
-    if (visible) {
-      EditorControlFunctions.addOrRemoveComponent([entity], VisibleComponent, false)
-    } else {
-      EditorControlFunctions.addOrRemoveComponent([entity], VisibleComponent, true)
-    }
-    setVisibleComponent(entity, !hasComponent(entity, VisibleComponent))
-  }
-
   const onClickToggle = (e: React.MouseEvent) => {
     e.stopPropagation()
     if (data.onToggle) data.onToggle(e, entity)
@@ -131,164 +107,6 @@ export const HierarchyTreeNode = (props: HierarchyTreeNodeProps) => {
   }
   const onClickNode = (e: React.MouseEvent) => data.onClick(e, entity)
   const onChangeNodeName = (e: React.ChangeEvent<HTMLInputElement>) => data.onChangeName(entity, e.target.value)
-
-  const [, drag, preview] = useDrag({
-    type: ItemTypes.Node,
-    item: (): DragItemType => {
-      const selectedEntities = SelectionState.getSelectedEntities()
-
-      if (selectedEntities.includes(node.entity)) {
-        const multiple = selectedEntities.length > 1
-        return {
-          type: ItemTypes.Node,
-          multiple,
-          value: multiple ? selectedEntities : selectedEntities[0]
-        }
-      }
-      return {
-        type: ItemTypes.Node,
-        multiple: false,
-        value: node.entity
-      }
-    },
-    canDrag: () =>
-      !SelectionState.getSelectedEntities().some(
-        (entity) => !getOptionalComponent(entity, EntityTreeComponent)?.parentEntity
-      ),
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging()
-    })
-  })
-
-  const dropItem = (node: HierarchyTreeNodeType, place: 'On' | 'Before' | 'After') => {
-    let parentNode: Entity | undefined
-    let beforeNode: Entity
-
-    if (place === 'Before') {
-      const entityTreeComponent = getOptionalComponent(node.entity, EntityTreeComponent)
-      parentNode = entityTreeComponent?.parentEntity
-      beforeNode = node.entity
-    } else if (place === 'After') {
-      const entityTreeComponent = getOptionalComponent(node.entity, EntityTreeComponent)
-      parentNode = entityTreeComponent?.parentEntity
-      const parentTreeComponent = getOptionalComponent(entityTreeComponent?.parentEntity!, EntityTreeComponent)
-      if (
-        parentTreeComponent &&
-        !node.lastChild &&
-        parentNode &&
-        parentTreeComponent?.children.length > node.childIndex + 1
-      ) {
-        beforeNode = parentTreeComponent.children[node.childIndex + 1]
-      }
-    } else {
-      parentNode = node.entity
-    }
-
-    if (!parentNode)
-      return () => {
-        console.warn('parent is not defined')
-      }
-
-    return (item: FileDataType | DnDFileType | DragItemType, monitor: DropTargetMonitor): void => {
-      if (parentNode) {
-        if ('files' in item) {
-          const dndItem: any = monitor.getItem()
-          const entries = Array.from(dndItem.items).map((item: any) => item.webkitGetAsEntry())
-
-          //uploading files then adding as media to the editor
-          data.onUpload(entries).then((assets) => {
-            if (!assets) return
-            for (const asset of assets) {
-              addMediaNode(asset, parentNode, beforeNode)
-            }
-          })
-          return
-        }
-
-        if ('url' in item) {
-          addMediaNode(item.url, parentNode, beforeNode)
-          return
-        }
-
-        if ('type' in item && item.type === ItemTypes.Component) {
-          EditorControlFunctions.createObjectFromSceneElement(
-            [{ name: (item as any).componentJsonID }],
-            parentNode,
-            beforeNode
-          )
-          return
-        }
-      }
-
-      EditorControlFunctions.reparentObject(
-        Array.isArray((item as DragItemType).value)
-          ? ((item as DragItemType).value as Entity[])
-          : [(item as DragItemType).value as Entity],
-        beforeNode,
-        parentNode === null ? undefined : parentNode
-      )
-    }
-  }
-
-  const canDropItem = (entityNode: Entity, dropOn?: boolean) => {
-    return (item: DragItemType, monitor: DropTargetMonitor): boolean => {
-      if (!monitor.isOver()) {
-        return false
-      }
-
-      if (!dropOn) {
-        const entityTreeComponent = getComponent(entityNode, EntityTreeComponent)
-        if (!entityTreeComponent) {
-          return false
-        }
-      }
-      if (item.type === ItemTypes.Node) {
-        const entityTreeComponent = getComponent(entityNode, EntityTreeComponent)
-
-        return (
-          (dropOn || !!entityTreeComponent.parentEntity) &&
-          !(item.multiple
-            ? (item.value as Entity[]).some((otherObject) => isAncestor(otherObject, entityNode))
-            : isAncestor(item.value as Entity, entityNode))
-        )
-      }
-      return true
-    }
-  }
-
-  const [{ canDropBefore, isOverBefore }, beforeDropTarget] = useDrop({
-    accept: [ItemTypes.Node, ItemTypes.File, ItemTypes.Component, ...SupportedFileTypes],
-    drop: dropItem(node, 'Before'),
-    canDrop: canDropItem(node.entity),
-    collect: (monitor) => ({
-      canDropBefore: monitor.canDrop(),
-      isOverBefore: monitor.isOver()
-    })
-  })
-
-  const [{ canDropAfter, isOverAfter }, afterDropTarget] = useDrop({
-    accept: [ItemTypes.Node, ItemTypes.File, ItemTypes.Component, ...SupportedFileTypes],
-    drop: dropItem(node, 'After'),
-    canDrop: canDropItem(node.entity),
-    collect: (monitor) => ({
-      canDropAfter: monitor.canDrop(),
-      isOverAfter: monitor.isOver()
-    })
-  })
-
-  const [{ canDropOn, isOverOn }, onDropTarget] = useDrop({
-    accept: [ItemTypes.Node, ItemTypes.File, ItemTypes.Component, ...SupportedFileTypes],
-    drop: dropItem(node, 'On'),
-    canDrop: canDropItem(node.entity, true),
-    collect: (monitor) => ({
-      canDropOn: monitor.canDrop(),
-      isOverOn: monitor.isOver()
-    })
-  })
-
-  useEffect(() => {
-    preview(getEmptyImage(), { captureDraggingState: true })
-  }, [preview])
 
   const icons = entityExists(node.entity)
     ? getAllComponents(node.entity)
@@ -312,25 +130,14 @@ export const HierarchyTreeNode = (props: HierarchyTreeNodeProps) => {
       )}
     >
       <div
-        ref={drag}
         id={getNodeElId(node)}
         tabIndex={0}
         onKeyDown={onNodeKeyDown}
         onClick={onClickNode}
-        onContextMenu={(event) => props.onContextMenu(event, entity)}
+        onContextMenu={(event) => event.stopPropagation()}
         className="py-.5 ml-3.5 h-9 justify-between bg-inherit pr-2"
       >
-        <div
-          className={twMerge('h-1', isOverBefore && canDropBefore && 'bg-white')}
-          style={{ marginLeft: `${node.depth * 1.25}em` }}
-          ref={beforeDropTarget}
-        />
-
-        <div
-          className="flex items-center bg-inherit pr-2"
-          style={{ paddingLeft: `${node.depth * 1.25}em` }}
-          ref={onDropTarget}
-        >
+        <div className="flex items-center bg-inherit pr-2" style={{ paddingLeft: `${node.depth * 1.25}em` }}>
           {node.isLeaf ? (
             <div className="w-5 shrink-0" />
           ) : (
@@ -368,25 +175,8 @@ export const HierarchyTreeNode = (props: HierarchyTreeNodeProps) => {
                 </div>
               )}
             </div>
-            <button
-              type="button"
-              className="m-0 h-5 w-5 flex-shrink-0 border-none p-0 hover:opacity-80"
-              onClick={toggleVisible}
-            >
-              {visible ? (
-                <PiEyeBold className="font-small text-[#6B7280]" />
-              ) : (
-                <PiEyeClosedBold className="font-small text-[#42454d]" />
-              )}
-            </button>
           </div>
         </div>
-
-        <div
-          className={twMerge('h-1', isOverAfter && canDropAfter && 'bg-white')}
-          style={{ marginLeft: `${node.depth * 1.25}em` }}
-          ref={afterDropTarget}
-        />
       </div>
     </li>
   )
