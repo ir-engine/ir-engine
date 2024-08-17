cfca06f84f18235a1ba2e79c939c05314e95dfe3
HexaField <joshfield999@gmail.com>
2024-08-17 11:54:51 +1000

    fix public path

--- packages/engine/src/scene/components/UVOL1Component.ts
+++ packages/engine/src/scene/components/UVOL1Component.ts
@@ -55,7 +55,6 @@ import { useExecute } from '@etherealengine/ecs/src/SystemFunctions'
 import { AnimationSystemGroup } from '@etherealengine/ecs/src/SystemGroups'
 import { getMutableState, getState } from '@etherealengine/hyperflux'
 import { iOS } from '@etherealengine/spatial/src/common/functions/isMobile'
-import { EngineState } from '@etherealengine/spatial/src/EngineState'
 import { addObjectToGroup, removeObjectFromGroup } from '@etherealengine/spatial/src/renderer/components/GroupComponent'
 import { RendererComponent } from '@etherealengine/spatial/src/renderer/WebGLRendererSystem'
 
@@ -161,7 +160,7 @@ function UVOL1Reactor() {
   useEffect(() => {
     if (!getState(AssetLoaderState).cortoLoader) {
       const loader = new CORTOLoader()
-      loader.setDecoderPath(getState(EngineState).publicPath + '/loader_decoders/')
+      loader.setDecoderPath(Engine.instance.store.publicPath + '/loader_decoders/')
       loader.preload()
       const assetLoaderState = getMutableState(AssetLoaderState)
       assetLoaderState.cortoLoader.set(loader)
