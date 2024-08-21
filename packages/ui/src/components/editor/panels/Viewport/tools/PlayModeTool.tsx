3c350f03c57578733ef36aabb4db6674665d7b25
Sam Mazer <sam.mazer@theinfinitereality.com>
2024-08-21 10:59:59 -0700

    useEffect based playMode from isEditing to spawn/despawn avatar and play mode logic. Cleanup of warnings that required getOptionalComponent but were using getComponent

--- packages/ui/src/components/editor/panels/Viewport/tools/PlayModeTool.tsx
+++ packages/ui/src/components/editor/panels/Viewport/tools/PlayModeTool.tsx
@@ -35,7 +35,7 @@ import { VisualScriptActions, visualScriptQuery } from '@ir-engine/engine'
 import { AvatarComponent } from '@ir-engine/engine/src/avatar/components/AvatarComponent'
 import { getRandomSpawnPoint } from '@ir-engine/engine/src/avatar/functions/getSpawnPoint'
 import { spawnLocalAvatarInWorld } from '@ir-engine/engine/src/avatar/functions/receiveJoinWorld'
-import { dispatchAction, getMutableState, getState, useHookstate } from '@ir-engine/hyperflux'
+import { dispatchAction, getMutableState, getState, useHookstate, useImmediateEffect } from '@ir-engine/hyperflux'
 import { WorldNetworkAction } from '@ir-engine/network'
 import { EngineState } from '@ir-engine/spatial/src/EngineState'
 import { FollowCameraComponent } from '@ir-engine/spatial/src/camera/components/FollowCameraComponent'
@@ -47,45 +47,61 @@ import { HiOutlinePause, HiOutlinePlay } from 'react-icons/hi2'
 import Button from '../../../../../primitives/tailwind/Button'
 import Tooltip from '../../../../../primitives/tailwind/Tooltip'
 
-const PlayModeTool = () => {
+/**
+ * Returns true if we stopped play mode, false if we were not in play mode
+ */
+export const onStopPlayMode = (): boolean => {
+  const entity = AvatarComponent.getSelfAvatarEntity()
+  if (entity) {
+    dispatchAction(WorldNetworkAction.destroyEntity({ entityUUID: getComponent(entity, UUIDComponent) }))
+    removeEntity(entity)
+    const viewerEntity = getState(EngineState).viewerEntity
+    removeComponent(viewerEntity, ComputedTransformComponent)
+    removeComponent(viewerEntity, FollowCameraComponent)
+    removeComponent(viewerEntity, TargetCameraRotationComponent)
+    // getMutableState(EngineState).isEditing.set(true)
+    visualScriptQuery().forEach((entity) => dispatchAction(VisualScriptActions.stop({ entity })))
+    // stop all visual script logic
+  }
+  return !!entity
+}
+
+export const onStartPlayMode = () => {
+  const authState = getState(AuthState)
+  // const authState = useHookstate(getMutableState(AuthState))
+  const avatarDetails = authState.user.avatar //.value
+
+  const avatarSpawnPose = getRandomSpawnPoint(Engine.instance.userID)
+  const currentScene = getComponent(getState(EditorState).rootEntity, UUIDComponent)
+
+  if (avatarDetails)
+    spawnLocalAvatarInWorld({
+      parentUUID: currentScene,
+      avatarSpawnPose,
+      avatarID: avatarDetails.id!,
+      name: authState.user.name //.value
+    })
+
+  // todo
+  // getMutableState(EngineState).isEditing.set(false)
+  // run all visual script logic
+  visualScriptQuery().forEach((entity) => dispatchAction(VisualScriptActions.execute({ entity })))
+  transformGizmoControlledQuery().forEach((entity) => removeComponent(entity, TransformGizmoControlledComponent))
+  //just remove all gizmo in the scene
+}
+
+const PlayModeTool: React.FC = () => {
   const { t } = useTranslation()
 
   const isEditing = useHookstate(getMutableState(EngineState).isEditing)
-  const authState = useHookstate(getMutableState(AuthState))
 
   const onTogglePlayMode = () => {
-    const entity = AvatarComponent.getSelfAvatarEntity()
-    if (entity) {
-      dispatchAction(WorldNetworkAction.destroyEntity({ entityUUID: getComponent(entity, UUIDComponent) }))
-      removeEntity(entity)
-      removeComponent(Engine.instance.cameraEntity, ComputedTransformComponent)
-      removeComponent(Engine.instance.cameraEntity, FollowCameraComponent)
-      removeComponent(Engine.instance.cameraEntity, TargetCameraRotationComponent)
-      getMutableState(EngineState).isEditing.set(true)
-      visualScriptQuery().forEach((entity) => dispatchAction(VisualScriptActions.stop({ entity })))
-      // stop all visual script logic
-    } else {
-      const avatarDetails = authState.user.avatar.value
-
-      const avatarSpawnPose = getRandomSpawnPoint(Engine.instance.userID)
-      const currentScene = getComponent(getState(EditorState).rootEntity, UUIDComponent)
-
-      if (avatarDetails)
-        spawnLocalAvatarInWorld({
-          parentUUID: currentScene,
-          avatarSpawnPose,
-          avatarID: avatarDetails.id!,
-          name: authState.user.name.value
-        })
-
-      // todo
-      getMutableState(EngineState).isEditing.set(false)
-      // run all visual script logic
-      visualScriptQuery().forEach((entity) => dispatchAction(VisualScriptActions.execute({ entity })))
-      transformGizmoControlledQuery().forEach((entity) => removeComponent(entity, TransformGizmoControlledComponent))
-      //just remove all gizmo in the scene
-    }
+    getMutableState(EngineState).isEditing.set(!isEditing) //getState(EngineState).isEditing)
   }
+
+  useImmediateEffect(() => {
+    !isEditing.value ? onStartPlayMode() : onStopPlayMode()
+  }, [isEditing])
 
   return (
     <div id="preview" className="flex items-center">
