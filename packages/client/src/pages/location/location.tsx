5e97f4af982bbf179bc02514587b79127b1da01c
David Gordon <94419856+dinomut1@users.noreply.github.com>
2024-08-22 16:46:26 -0700

    IR-3984 viewer transition problems (#65)
    * break apart viewer initialization from spatial initialization
    
    * detect stale pointer entities and remove them
    
    * fix locations and mock spatial engine
    
    * fix tests
    
    * fix avatar spawn test

--- packages/client/src/pages/location/location.tsx
+++ packages/client/src/pages/location/location.tsx
@@ -24,7 +24,7 @@ Infinite Reality Engine. All Rights Reserved.
 */
 
 import { t } from 'i18next'
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useEffect, useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import '../../engine'
@@ -33,10 +33,19 @@ import Debug from '@ir-engine/client-core/src/components/Debug'
 import { useEngineInjection } from '@ir-engine/client-core/src/components/World/EngineHooks'
 import { useEngineCanvas } from '@ir-engine/client-core/src/hooks/useEngineCanvas'
 import LocationPage from '@ir-engine/client-core/src/world/Location'
+import { destroySpatialEngine, initializeSpatialEngine } from '@ir-engine/spatial/src/initializeEngine'
 import LoadingView from '@ir-engine/ui/src/primitives/tailwind/LoadingView'
 
 const LocationRoutes = () => {
   const ref = useRef<HTMLElement>(document.body)
+
+  useEffect(() => {
+    initializeSpatialEngine()
+    return () => {
+      destroySpatialEngine()
+    }
+  }, [])
+
   useEngineCanvas(ref)
 
   const projectsLoaded = useEngineInjection()
