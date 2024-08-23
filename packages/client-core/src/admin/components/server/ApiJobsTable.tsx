a651ff31f75bdcec3bb1037bb2e969752b69f38c
MoizAdnan <moizadnan21@gmail.com>
2024-08-23 16:35:35 +0500

    feat: Add status badges to ApiJobsTable

--- packages/client-core/src/admin/components/server/ApiJobsTable.tsx
+++ packages/client-core/src/admin/components/server/ApiJobsTable.tsx
@@ -30,8 +30,27 @@ import { apiJobPath, ApiJobType } from '@ir-engine/common/src/schema.type.module
 import { useFind } from '@ir-engine/spatial/src/common/functions/FeathersHooks'
 
 import { toDisplayDateTime } from '@ir-engine/common/src/utils/datetime-sql'
+import Badge from '@ir-engine/ui/src/primitives/tailwind/Badge'
 import { apiJobColumns } from '../../common/constants/api-job'
 import DataTable from '../../common/Table'
+
+function ApiJobStatus({ apiJob }: { apiJob: ApiJobType }) {
+  const { t } = useTranslation()
+
+  return (
+    <div className="flex items-center justify-between gap-3">
+      {apiJob.status === 'succeeded' && (
+        <Badge className="rounded" variant="success" label={t('admin:components.server.serverStatus.succeeded')} />
+      )}
+      {apiJob.status === 'pending' && (
+        <Badge className="rounded" variant="warning" label={t('admin:components.server.serverStatus.pending')} />
+      )}
+      {apiJob.status === 'failed' && (
+        <Badge className="rounded" variant="danger" label={t('admin:components.server.serverStatus.failed')} />
+      )}
+    </div>
+  )
+}
 
 export default function ApiJobsTable() {
   const { t } = useTranslation()
@@ -49,7 +68,7 @@ export default function ApiJobsTable() {
     rows.map((row) => ({
       id: row.id.toString(),
       name: row.name,
-      status: row.status,
+      status: <ApiJobStatus apiJob={row} />,
       startTime: toDisplayDateTime(row.startTime),
       endTime: toDisplayDateTime(row.endTime),
       returnData: row.returnData
