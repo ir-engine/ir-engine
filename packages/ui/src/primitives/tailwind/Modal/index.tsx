445bdf09b7d3ab0d97c9f7380212c29e8664fa6a
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-08-26 13:29:47 +0530

    add id on modal div

--- packages/ui/src/primitives/tailwind/Modal/index.tsx
+++ packages/ui/src/primitives/tailwind/Modal/index.tsx
@@ -138,10 +138,10 @@ const Modal = ({
 }: ModalProps) => {
   const twClassName = twMerge('relative z-50 max-h-[80vh] w-full bg-theme-surface-main', className)
   return (
-    <div className={twClassName}>
+    <div data-test-id={id} className={twClassName}>
       <div className="relative rounded-lg shadow">
         {onClose && <ModalHeader title={title} onClose={onClose} />}
-        {rawChildren && rawChildren}
+        {rawChildren}
         {children && <div className="h-fit max-h-[60vh] w-full overflow-y-auto px-10 py-6">{children}</div>}
 
         {!hideFooter && (
