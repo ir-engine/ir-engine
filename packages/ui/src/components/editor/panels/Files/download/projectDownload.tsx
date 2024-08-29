7cbc46ecdb9e805b588c894283cc67eab89d896e
sybiote <ghoshr698@gmail.com>
2024-08-30 03:25:31 +0530

    add fetching stage and UI

--- packages/ui/src/components/editor/panels/Files/download/projectDownload.tsx
+++ packages/ui/src/components/editor/panels/Files/download/projectDownload.tsx
@@ -33,17 +33,23 @@ import { defineState, getMutableState, useMutableState } from '@ir-engine/hyperf
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import Progress from '../../../../../primitives/tailwind/Progress'
+import LoadingView from '../../../../../primitives/tailwind/LoadingView'
 
 const DownloadProjectState = defineState({
   name: 'DownloadProjectState',
   initial: () => ({
     total: 0,
     progress: 0,
+    isFetching:false,
     isDownloading: false
   })
 })
 
 export const handleDownloadProject = async (projectName: string, selectedDirectory: string) => {
+  const downloadState = getMutableState(DownloadProjectState)
+
+  downloadState.isFetching.set(true) // start Fetching
+
   const data = await API.instance
     .service(archiverPath)
     .get(null, { query: { project: projectName } })
@@ -53,11 +59,17 @@ export const handleDownloadProject = async (projectName: string, selectedDirecto
     })
   if (!data) return
 
-  const downloadState = getMutableState(DownloadProjectState)
+  const response = await fetch(`${config.client.fileServer}/${data}`)
+  
+  downloadState.isFetching.set(false) // Fetch completed
+  
+  if(!response.ok) {
+    NotificationService.dispatchNotify(`Failed to fetch project ${response.status} ` , { variant: 'error' })
+    return
+  }
 
   downloadState.isDownloading.set(true) // Start Download
-
-  const response = await fetch(`${config.client.fileServer}/${data}`)
+  
   const totalBytes = parseInt(response.headers.get('Content-Length') || '0', 10)
   downloadState.total.set(totalBytes) // Set the total bytes
 
@@ -75,6 +87,7 @@ export const handleDownloadProject = async (projectName: string, selectedDirecto
 
   const blob = new Blob(chunks)
   downloadState.isDownloading.set(false) // Mark as completed
+  downloadState.isFetching.set(false) // redundant set
   downloadState.progress.set(0)
   downloadState.total.set(0)
 
@@ -92,20 +105,33 @@ export const ProjectDownloadProgress = () => {
   const { t } = useTranslation()
   const downloadState = useMutableState(DownloadProjectState)
   const isDownloading = downloadState.isDownloading.value
+  const isFetching = downloadState.isFetching.value
+
   const completed = bytesToSize(downloadState.progress.value)
   const total = bytesToSize(downloadState.total.value)
   const progress = (downloadState.progress.value / downloadState.total.value) * 100
 
-  return isDownloading ? (
-    <div className="flex h-auto w-full justify-center pb-2 pt-2">
-      <div className="flex w-1/2">
-        <span className="inline-block pr-2 text-xs font-normal leading-none text-theme-primary">
-          {t('editor:layout.filebrowser.downloadingProject', { completed, total })}
-        </span>
-        <div className="basis-1/2">
-          <Progress value={progress} />
-        </div>
-      </div>
-    </div>
-  ) : null
+  return (
+    <>
+      {isDownloading && 
+        <div className="flex h-auto w-full justify-center pb-2 pt-2">
+          <div className="flex w-1/2">
+            <span className="inline-block pr-2 text-xs font-normal leading-none text-theme-primary">
+              {t('editor:layout.filebrowser.downloadingProject', { completed, total })}
+            </span>    
+              <div className="basis-1/2">
+                <Progress value={progress} />
+              </div>     
+          </div>
+        </div>}
+      {isFetching && (
+          <LoadingView
+          titleClassname="mt-0"
+          containerClassName="flex-row mt-1"
+          className="mx-2 my-auto h-6 w-6"
+          title={t('editor:layout.filebrowser.fetchingProject')}
+        />
+      )}
+    </>
+  )
 }
