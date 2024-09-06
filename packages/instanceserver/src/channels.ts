588dd8e13d304f75a396956abcbf33ee66798aef
MoizAdnan <moizadnan21@gmail.com>
2024-09-06 10:27:58 +0500

    Refactor instance initialization in channels.ts

--- packages/instanceserver/src/channels.ts
+++ packages/instanceserver/src/channels.ts
@@ -198,7 +198,7 @@ const initializeInstance = async ({
    * If it's not, then throw an error and don't connect, because something is wrong.
    */
 
-  const existingInstanceResult = (await app.service(instancePath).find({
+  const existingInstanceResult = (await app.service(instancePath)._find({
     query: existingInstanceQuery,
     headers
   })) as Paginated<InstanceType>
