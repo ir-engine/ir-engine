a588739100c34c240dcc8709930cc1e82ed82f50
Appaji <appaji12368@gmail.com>
2024-09-17 13:59:41 +0530

    Add agree to marketing translation
    and set default close function to modal

--- packages/ui/src/primitives/tailwind/Modal/index.tsx
+++ packages/ui/src/primitives/tailwind/Modal/index.tsx
@@ -25,6 +25,7 @@ Infinite Reality Engine. All Rights Reserved.
 
 import React, { ReactNode } from 'react'
 
+import { PopoverState } from '@ir-engine/client-core/src/common/services/PopoverState'
 import { useTranslation } from 'react-i18next'
 import { MdClose } from 'react-icons/md'
 import { twMerge } from 'tailwind-merge'
@@ -123,7 +124,9 @@ export const ModalFooter = ({
 const Modal = ({
   id,
   title,
-  onClose,
+  onClose = () => {
+    PopoverState.hidePopupover()
+  },
   onSubmit,
   hideFooter,
   rawChildren,
