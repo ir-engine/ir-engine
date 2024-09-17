d4ce7cd4c72d7a72601d832c0e40d2d1c13ea701
Andy Chen <andychen5671@gmail.com>
2024-09-17 13:02:58 -0400

    remove useragentdata

--- packages/editor/src/functions/browserCheck.ts
+++ packages/editor/src/functions/browserCheck.ts
@@ -25,12 +25,7 @@ Infinite Reality Engine. All Rights Reserved.
 
 export const isSupportedBrowser = () => {
   const userAgent = window.navigator.userAgent
-  const winNav = window.navigator as any
-
-  const isGoogleChrome =
-    /Chrome/.test(userAgent) && typeof winNav.userAgentData !== 'undefined'
-      ? winNav.userAgentData.brands?.some((item) => item.brand === 'Google Chrome')
-      : !/Chromium|Edg|OPR|Brave|CriOS/.test(userAgent)
+  const isGoogleChrome = /Chrome/.test(userAgent) && !/Chromium|Edg|OPR|Brave|CriOS/.test(userAgent)
   const isSafari = /^((?!chrome|androidg).)*safari/i.test(userAgent)
 
   return isGoogleChrome || isSafari
