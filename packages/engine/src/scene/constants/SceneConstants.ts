29f65647a700f44765ae7cb12e09ba4a46c4bc8c
Rahul Ghosh <ghoshr698@gmail.com>
2024-09-18 07:24:39 +0530

    Update packages/engine/src/scene/constants/SceneConstants.ts
    add space
    
    Co-authored-by: Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>

--- packages/engine/src/scene/constants/SceneConstants.ts
+++ packages/engine/src/scene/constants/SceneConstants.ts
@@ -33,11 +33,11 @@ export const SceneComplexityWeights = {
 
 // these are thresholds for the scene complexity
 export const SceneComplexity = {
-  VeryLight: { label: 'VeryLight', value: 50000 } as const,
+  VeryLight: { label: 'Very Light', value: 50000 } as const,
   Light: { label: 'Light', value: 100000 } as const,
   Medium: { label: 'Medium', value: 150000 } as const,
   Heavy: { label: 'Heavy', value: 200000 } as const,
-  VeryHeavy: { label: 'VeryHeavy', value: 300000 } as const
+  VeryHeavy: { label: 'Very Heavy', value: 300000 } as const
 }
 
 export type SceneComplexityCategoryType = (typeof SceneComplexity)[keyof typeof SceneComplexity]['label']
