37f83dfb3afcd1206b59eb95be1380fa0b46d293
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-09-24 21:28:30 +0530

    fixes for storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -5,7 +5,7 @@ CPAL-1.0 License
 The contents of this file are subject to the Common Public Attribution License
 Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
-https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
+https://github.com/EtherealEngine/etherealengine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
 and 15 have been added to cover use of software over a computer network and 
 provide for limited attribution for the Original Developer. In addition, 
@@ -15,19 +15,17 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
 specific language governing rights and limitations under the License.
 
-The Original Code is Infinite Reality Engine.
+The Original Code is Ethereal Engine.
 
 The Original Developer is the Initial Developer. The Initial Developer of the
-Original Code is the Infinite Reality Engine team.
+Original Code is the Ethereal Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
-Infinite Reality Engine. All Rights Reserved.
+All portions of the code written by the Ethereal Engine team are Copyright © 2021-2023 
+Ethereal Engine. All Rights Reserved.
 */
 
 import type { StorybookConfig } from '@storybook/react-vite'
 import { mergeConfig } from 'vite'
-
-const host = process.env['VITE_APP_HOST']
 
 const config: StorybookConfig = {
   env: (config) => ({
@@ -36,7 +34,13 @@ const config: StorybookConfig = {
       path: '../../.env.local'
     }).parsed
   }),
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  typescript: {
+    reactDocgen: false
+  },
+  stories: [
+    '../src/primitives/tailwind/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/components/editor/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-toolbars'),
@@ -72,38 +76,8 @@ const config: StorybookConfig = {
           '@': require('path').resolve(__dirname, '../../client/public')
         }
       },
-      server: {
-        ...userConfig?.server,
-        proxy: {
-          ...userConfig?.server?.proxy,
-          cors: false,
-          '^3030': {
-            target: `https://${host}:3030`,
-            changeOrigin: true,
-            secure: false,
-            ws: true
-          },
-          '^3031': {
-            target: `https://${host}:3031`,
-            changeOrigin: true,
-            secure: false,
-            ws: true
-          },
-          '/sfx': {
-            target: `https://${host}:3000`,
-            changeOrigin: true,
-            secure: false,
-            // replace port 6006 with 3000
-            pathRewrite: { '^6006': '3000' }
-          },
-          '/fonts': {
-            target: `https://${host}:3000`,
-            changeOrigin: true,
-            secure: false,
-            // replace port 6006 with 3000
-            pathRewrite: { '^6006': '3000' }
-          }
-        }
+      build: {
+        cssMinify: false
       },
       plugins: []
     })
