8e95569f8ed8e198e872c4f350490b8f921c67a2
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-09-24 10:44:54 +0530

    restore assetpanelcategoriesstate

--- packages/editor/src/panels/assets/helpers.ts
+++ packages/editor/src/panels/assets/helpers.ts
@@ -23,6 +23,8 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
+import { AssetsPanelCategories } from '../../services/AssetPanelCategoriesState'
+
 export type Category = {
   name: string
   object: object
@@ -30,145 +32,6 @@ export type Category = {
   isLeaf: boolean
   depth: number
 }
-
-const ASSET_PANEL_CATEGORIES = Object.freeze({
-  'Default Prefab': {},
-
-  Model: {
-    // NPC: {},
-    // Avatar: {},
-    // Animal: {},
-    Template: {
-      // Backdrop: {},
-      // Stage: {}
-    },
-    Architectural: {
-      Floor: {},
-      Ceiling: {},
-      Wall: {}
-    },
-    // 'Basic Shape': {
-    //   Artistic: {},
-    //   Primitive: {
-    //     'Platonic Solid': {},
-    //     Fractal: {}
-    //   }
-    // },
-    Kit: {
-      Gothic: {},
-      Cottage: {},
-      Scifi: {},
-      Modern: {},
-      Nature: {},
-      Ecommerce: {},
-      Noir: {}
-    },
-    Prop: {
-      // Furniture: {
-      //   Chairs: {},
-      //   Showcase: {},
-      //   Displays: {}
-      // }
-    }
-    // Terrain: {}
-  },
-  Material: {
-    // Standard: {
-    //   Plastic: {},
-    //   Glass: {},
-    //   'Rough glass': {},
-    //   Metal: {},
-    //   Glow: {},
-    //   Rubber: {},
-    //   Water: {},
-    //   Bubble: {},
-    //   Diamond: {},
-    //   Clay: {},
-    //   'Car Paint': {},
-    //   Wood: {},
-    //   Stone: {},
-    //   Dirt: {},
-    //   Grass: {},
-    //   Cloth: {},
-    //   Bark: {},
-    //   Snow: {}
-    // },
-    // Advanced: {
-    //   Static: {},
-    //   Animated: {}
-    // }
-  },
-  Image: {
-    // Background: {},
-    // Texture: {
-    //   Diffuse: {},
-    //   'Normal Maps': {},
-    //   Occlusion: {},
-    //   Metalness: {},
-    //   Roughness: {}
-    // },
-    // Tiling: {
-    //   Diffuse: {},
-    //   'Normal Map': {},
-    //   Occlusion: {},
-    //   Metalness: {},
-    //   Roughness: {}
-    // },
-    // Sprite: {}
-  },
-  // Light: {},
-  Video: {},
-  Audio: {
-    // Music: {},
-    // 'Sound FX': {}
-  },
-  Particle: {
-    // Smoke: {},
-    // Fire: {},
-    // Lightning: {},
-    // Portal: {},
-    // Sparkle: {}
-  },
-  Skybox: {
-    // 'Time of Day': {
-    //   Morning: {},
-    //   Noon: {},
-    //   Evening: {},
-    //   Night: {}
-    // },
-    // Abstract: {},
-    // 'Low Contrast': {},
-    // 'High Contrast': {}
-  },
-  'Post Processing': {
-    // Fog: {},
-    // Cinematic: {}
-  }
-  // Agent: {},
-  // Genre: {
-  //   Office: {},
-  //   Gothic: {},
-  //   Scifi: {},
-  //   Cottage: {},
-  //   Modern: {},
-  //   Luxury: {},
-  //   Noir: {},
-  //   Nature: {
-  //     Jungle: {},
-  //     Arctic: {},
-  //     Boreal: {},
-  //     Desert: {}
-  //   },
-  //   Holiday: {
-  //     Passover: {},
-  //     'St Patrick’s Day': {},
-  //     'Yam Kippur': {},
-  //     'Veteran’s Day': {}
-  //   },
-  //   Abstract: {},
-  //   Fantasy: {}
-  // }
-})
 
 export function iterativelyListTags(obj: object): string[] {
   const tags: string[] = []
@@ -216,7 +79,7 @@ export function mapCategoriesHelper(collapsedCategories: { [key: string]: boolea
       }
     }
   }
-  generateCategories(ASSET_PANEL_CATEGORIES)
+  generateCategories(AssetsPanelCategories.initial)
   return result
 }
 
