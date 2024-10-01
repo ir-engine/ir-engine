7a4fdab9348d2040f6bfe799757d59b72851a63f
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2024-10-01 17:39:22 +0530

    new designs for radio

--- packages/ui/src/primitives/tailwind/Radio/index.stories.tsx
+++ packages/ui/src/primitives/tailwind/Radio/index.stories.tsx
@@ -23,29 +23,85 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import Component from './index'
+import React, { useEffect, useState } from 'react'
+import { ArgTypes } from 'storybook/internal/types'
+import Radio, { RadioProps } from './index'
 
-export default {
-  title: 'Primitives/Tailwind/Radio',
-  component: Component,
-  parameters: {
-    componentSubtitle: 'Radio',
-    jest: 'Radio.test.tsx',
-    design: {
-      type: 'figma',
-      url: ''
-    }
+const argTypes: ArgTypes = {
+  disabled: {
+    control: 'boolean'
+  },
+  selected: {
+    control: 'boolean'
+  },
+  label: {
+    control: 'text'
+  },
+  description: {
+    control: 'text'
+  },
+  horizontal: {
+    control: 'boolean'
+  },
+  numberOfRadios: {
+    control: 'number',
+    name: 'Number of Radios',
+    description: 'The number of radios to generate'
   }
 }
 
-export const Default = {
+export default {
+  title: 'Primitives/Tailwind/Radio',
+  component: Radio,
+  parameters: {
+    componentSubtitle: 'Radio',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ln2VDACenFEkjVeHkowxyi/iR-Engine-Design-Library-File?node-id=2786-21237&node-type=frame&t=n2wfUzH1bzwYsyob-0'
+    }
+  },
+  argTypes,
   args: {
-    options: [
-      { name: 'A1', value: 'a1' },
-      { name: 'B2', value: 'b2' },
-      { name: 'C3', value: 'c3' }
-    ],
-    currentValue: 'b2',
-    onChange: () => {}
+    numberOfRadios: 1,
+    label: '',
+    description: ''
   }
+}
+
+const RadioRootRenderer = ({
+  description,
+  label,
+  disabled,
+  numberOfRadios,
+  horizontal,
+  selected
+}: Pick<RadioProps, 'description' | 'label' | 'disabled'> & {
+  numberOfRadios: number
+  horizontal: boolean
+  selected: boolean
+}) => {
+  const options =
+    numberOfRadios > 1
+      ? Array.from({ length: numberOfRadios }, (_, idx) => ({
+          label: `${label} ${idx + 1}`,
+          description: `${description} ${idx + 1}`,
+          value: `${idx + 1}`
+        }))
+      : [{ label, description, value: '1' }]
+
+  const [value, setValue] = useState<string | undefined>(undefined)
+  useEffect(() => {
+    if (selected) {
+      setValue(options[0].value)
+    }
+  }, [selected])
+
+  return (
+    <Radio options={options} value={value} onChange={(v) => setValue(v)} disabled={disabled} horizontal={horizontal} />
+  )
+}
+
+export const Default = {
+  name: 'Default',
+  render: RadioRootRenderer
 }
