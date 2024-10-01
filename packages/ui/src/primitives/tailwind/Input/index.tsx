f1c3dd4d8897bb44c8032a7b2c273b611d0cf256
Appaji <appaji12368@gmail.com>
2024-10-02 02:49:36 +0530

    new designs for Input

--- packages/ui/src/primitives/tailwind/Input/index.tsx
+++ packages/ui/src/primitives/tailwind/Input/index.tsx
@@ -23,122 +23,42 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import React, { forwardRef, InputHTMLAttributes } from 'react'
-import { HiXCircle } from 'react-icons/hi'
+import React, { useId, useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
+import InputBase, { InputBaseProps } from '../InputBase'
+import InputLabel from '../InputBase/InputLabel'
 
-import Label from '../Label'
-
-export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
-  value: string | number
-  label?: string
-  containerClassName?: string
-  description?: string
-  type?: InputHTMLAttributes<HTMLInputElement>['type']
-  onChange?: InputHTMLAttributes<HTMLInputElement>['onChange']
-  error?: string
-  disabled?: boolean
-  startComponent?: JSX.Element
-  endComponent?: JSX.Element
-  variant?: 'outlined' | 'underlined' | 'onboarding'
-  labelClassname?: string
-  errorBorder?: boolean
-  maxLength?: number
+export interface InputProps extends InputBaseProps {
+  labelText: string
+  labelPosition: 'top' | 'left'
+  infoText?: string
 }
 
-const variants = {
-  outlined: ' ',
-  underlined: 'bg-transparent border-0 border-b rounded-none placeholder:text-neutral-200 placeholder:text-[17px]'
-}
+const Input = (
+  { labelText, labelPosition, infoText, required, fullWidth, id, ...props }: InputProps,
+  ref: React.ForwardedRef<HTMLInputElement>
+) => {
+  const tempId = useId()
+  const inputId = id || tempId
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      className,
-      containerClassName,
-      label,
-      type = 'text',
-      error,
-      description,
-      value,
-      itemType,
-      onChange,
-      disabled,
-      startComponent,
-      endComponent,
-      variant = 'outlined',
-      labelClassname,
-      errorBorder,
-      ...props
-    },
-    ref
-  ) => {
-    // Define a base style for the onboarding variant and conditionally apply error styling
-    const onboardingVariantStyle = errorBorder
-      ? 'bg-transparent border border-rose-600 rounded-none placeholder:text-neutral-200 placeholder:text-[17px]'
-      : 'bg-transparent border border-neutral-500 rounded-none placeholder:text-neutral-200 placeholder:text-[17px]'
+  const containerClass = useMemo(() => {
+    let _class = `flex ${fullWidth ? 'w-full' : 'w-fit'}`
 
-    const twClassname = twMerge(
-      'text-base font-normal tracking-tight',
-      'textshadow-sm flex h-9 w-full rounded-lg border border-theme-primary bg-theme-surfaceInput px-3.5 py-5 transition-colors',
-      'file:border-0 file:bg-theme-surfaceInput file:text-sm file:font-medium',
-      'dark:[color-scheme:dark]',
-      'focus-visible:ring-ring placeholder:text-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-      variant !== 'outlined' ? '' : 'focus-visible:ring-1',
-      startComponent ? 'ps-10' : undefined,
-      endComponent ? 'pe-10' : undefined,
-      variant === 'onboarding' ? onboardingVariantStyle : variants[variant],
-      className
-    )
-
-    const containerVariants = {
-      outlined: 'gap-2',
-      underlined: '',
-      onboarding: ''
+    if (labelPosition === 'top') {
+      _class = twMerge(_class, 'flex-col items-start gap-y-2')
+    } else if (labelPosition === 'left') {
+      _class = twMerge(_class, 'flex-row items-center gap-x-2')
     }
 
-    const twcontainerClassName = twMerge(
-      'flex w-full flex-col items-center',
-      containerVariants[variant],
-      containerClassName
-    )
+    return _class
+  }, [labelText, labelPosition, fullWidth])
 
-    const containerClass =
-      variant === 'outlined' ? 'bg-theme-surface-main relative h-full w-full' : ' relative h-full w-full'
-    const labelClass = variant === 'outlined' ? '' : 'text-neutral-500 text-xs'
+  return (
+    <div className={containerClass}>
+      <InputLabel htmlFor={inputId} labelText={labelText} infoText={infoText} required={required} />
+      <InputBase id={inputId} required={required} {...props} ref={ref} />
+    </div>
+  )
+}
 
-    return (
-      <div className={twcontainerClassName}>
-        {label && <Label className={twMerge(`self-stretch ${labelClass}`, labelClassname)}>{label}</Label>}
-        <div className={containerClass}>
-          {startComponent && (
-            <div className="pointer-events-auto absolute inset-y-0 left-0 start-0 ml-2 flex items-center ps-0">
-              {startComponent}
-            </div>
-          )}
-          <input
-            ref={ref}
-            disabled={disabled}
-            type={type}
-            className={twClassname}
-            value={value}
-            onChange={onChange}
-            {...props}
-          />
-
-          {endComponent && (
-            <div className="pointer-events-auto absolute inset-y-0 end-0 flex items-center">{endComponent}</div>
-          )}
-        </div>
-        {description && <p className="self-stretch text-xs text-theme-secondary">{description}</p>}
-        {error && (
-          <p className="inline-flex items-center gap-2.5 self-start text-sm text-theme-iconRed">
-            <HiXCircle /> {error}
-          </p>
-        )}
-      </div>
-    )
-  }
-)
-
-export default Input
+export default React.forwardRef(Input)
