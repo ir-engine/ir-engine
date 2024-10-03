17bd87b3402fd7b160cefbb3c9b22275430c3440
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2024-10-03 10:02:40 +0500

    move mailchimp component to MT project

--- packages/client-core/src/admin/components/settings/index.tsx
+++ packages/client-core/src/admin/components/settings/index.tsx
@@ -37,7 +37,6 @@ import EmailTab from './tabs/email'
 import FeaturesTab from './tabs/features'
 import HelmTab from './tabs/helm'
 import InstanceServerTab from './tabs/instanceServer'
-import MailchimpTab from './tabs/mailchimp'
 import MetabaseTab from './tabs/metabase'
 import ProjectTab from './tabs/project'
 import RedisTab from './tabs/redis'
@@ -98,10 +97,6 @@ export const SettingsTabsData = [
   {
     label: t('admin:components.setting.zendesk.header'),
     Component: ZendeskTab
-  },
-  {
-    label: t('admin:components.setting.mailchimp.header'),
-    Component: MailchimpTab
   }
 ]
 
