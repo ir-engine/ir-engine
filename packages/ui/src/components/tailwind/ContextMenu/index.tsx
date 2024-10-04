b055913f49a46836ef2c610dc91979afb6fc5cc6
Andy Chen <andychen5671@gmail.com>
2024-10-04 09:59:54 -0400

    lint

--- packages/ui/src/components/tailwind/ContextMenu/index.tsx
+++ packages/ui/src/components/tailwind/ContextMenu/index.tsx
@@ -51,10 +51,7 @@ const ShowContextMenu = ({
   anchorEvent,
   children,
   ...props
-}: {
-  onClose: () => void
-  anchorEvent: React.MouseEvent
-} & Omit<PopupProps, 'trigger' | 'onClose'>) => {
+}: { onClose: () => void; anchorEvent: React.MouseEvent } & Omit<PopupProps, 'trigger' | 'onClose'>) => {
   const offset = useHookstate({ x: anchorEvent.clientX, y: anchorEvent.clientY })
   const ref = useRef<HTMLDivElement>(null)
   useEffect(() => {
@@ -89,9 +86,6 @@ export const ContextMenu = ({
   anchorEvent,
   children,
   ...props
-}: {
-  onClose: () => void
-  anchorEvent?: React.MouseEvent
-} & Omit<PopupProps, 'trigger' | 'onClose'>) => {
+}: { onClose: () => void; anchorEvent?: React.MouseEvent } & Omit<PopupProps, 'trigger' | 'onClose'>) => {
   return anchorEvent ? <ShowContextMenu anchorEvent={anchorEvent} children={children} {...props} /> : null
 }
