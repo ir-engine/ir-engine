5f7015af9f273c8a9792c9a7b9af92eb083bf005
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-10-11 20:02:16 +0200

    fmt: Standarize the names of some `XRComponents.ts` components

--- packages/spatial/src/xr/XRComponents.ts
+++ packages/spatial/src/xr/XRComponents.ts
@@ -211,7 +211,7 @@ export const XRRightHandComponent = defineComponent({
 })
 
 export const XRHitTestComponent = defineComponent({
-  name: 'XRHitTest',
+  name: 'XRHitTestComponent',
   schema: S.Object({
     options: S.Type<XRTransientInputHitTestOptionsInit | XRHitTestOptionsInit>(),
     source: S.Type<XRHitTestSource>(),
@@ -264,7 +264,7 @@ export const XRHitTestComponent = defineComponent({
 })
 
 export const XRAnchorComponent = defineComponent({
-  name: 'XRAnchor',
+  name: 'XRAnchorComponent',
   schema: S.Object({
     anchor: S.Type<XRAnchor>()
   }),
@@ -285,7 +285,7 @@ export const XRAnchorComponent = defineComponent({
 })
 
 export const XRSpaceComponent = defineComponent({
-  name: 'XRSpace',
+  name: 'XRSpaceComponent',
 
   schema: S.Object({
     space: S.Type<XRSpace>(),
