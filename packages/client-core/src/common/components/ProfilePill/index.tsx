a7ca195012014a9f143e055581cf2d4c7d6fb98c
HexaField <joshfield999@gmail.com>
2024-10-27 10:17:22 +1100

    fix

--- packages/client-core/src/common/components/ProfilePill/index.tsx
+++ packages/client-core/src/common/components/ProfilePill/index.tsx
@@ -33,11 +33,14 @@ import { UserMenus } from '../../../user/UserUISystem'
 import { PopupMenuServices } from '../../../user/components/UserMenu/PopupMenuService'
 import { useUserAvatarThumbnail } from '../../../user/functions/useUserAvatarThumbnail'
 import { AuthState } from '../../../user/services/AuthService'
+import { identityProviderPath } from '@ir-engine/common/src/schema.type.module'
+import { useFind } from '@ir-engine/common'
 
 const ProfilePill = () => {
   const user = getMutableState(AuthState).user
   const avatarThumbnail = useUserAvatarThumbnail(user.value.id)
-  const email = user.value.identityProviders.find((ip) => ip.type === 'email')?.accountIdentifier
+  const identityProvidersQuery = useFind(identityProviderPath)
+  const email = identityProvidersQuery.data.find((ip) => ip.type === 'email')?.accountIdentifier
   const popUpOpened = useHookstate(false)
   return (
     <Popup
