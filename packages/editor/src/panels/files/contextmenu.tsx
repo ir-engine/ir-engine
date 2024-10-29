1c3cbf115c9863da40f2af7d6f043a46a44b9021
Jiatong Yao <jiatong@ethereal.com>
2024-10-29 12:25:14 -0500

    fix for place object

--- packages/editor/src/panels/files/contextmenu.tsx
+++ packages/editor/src/panels/files/contextmenu.tsx
@@ -124,7 +124,7 @@ export function FileContextMenu({
                 const vec3 = new Vector3()
                 getSpawnPositionAtCenter(vec3)
                 selectedFiles
-                  .filter((file) => !file.isFolder)
+                  .filter((file) => !file.isFolder.value)
                   .map((file) => {
                     addMediaNode(file.url.value, undefined, undefined, [
                       { name: TransformComponent.jsonID, props: { position: vec3 } }
@@ -142,7 +142,7 @@ export function FileContextMenu({
               data-testid="files-panel-file-item-context-menu-place-object-at-origin-button"
               onClick={() => {
                 selectedFiles
-                  .filter((file) => !file.isFolder)
+                  .filter((file) => !file.isFolder.value)
                   .map((file) => {
                     addMediaNode(file.url.value)
                   })
