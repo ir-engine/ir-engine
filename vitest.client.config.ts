4d9a83f42a91733f36c8c6630d4df73ecbaa0894
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-10-30 15:15:01 +0100

    tst: Remove `basic` reporter when on CI. Unify threshold/watermark cfg

--- vitest.client.config.ts
+++ vitest.client.config.ts
@@ -23,7 +23,11 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
+
+const reporters = !process.env.CI ? ['basic'] : configDefaults.reporters // Use default report config on CI.
+const watermark = [80, 95] as [number, number]
+const threshold = 80
 
 export default defineConfig({
   test: {
@@ -31,23 +35,23 @@ export default defineConfig({
     passWithNoTests: true,
     testTimeout: 2 * 60 * 1000,
     hookTimeout: 2 * 60 * 1000,
-    reporters: ['basic'],
+    reporters: reporters,
     slowTestThreshold: 1000,
     coverage: {
       reporter: ['html'],
       provider: 'istanbul',
       thresholds: {
         perFile: true,
-        statements: 80,
-        branches: 80,
-        functions: 80,
-        lines: 80
+        statements: threshold,
+        branches: threshold,
+        functions: threshold,
+        lines: threshold
       },
       watermarks: {
-        statements: [80, 95],
-        branches: [80, 95],
-        functions: [80, 95],
-        lines: [80, 95]
+        statements: watermark,
+        branches: watermark,
+        functions: watermark,
+        lines: watermark
       }
     }
   }
