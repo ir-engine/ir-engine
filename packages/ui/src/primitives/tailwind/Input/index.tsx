3e1e619fbf00ed3c042d1bc0b9451cc5428254e3
Appaji <appaji12368@gmail.com>
2024-10-30 15:59:29 +0530

    Add helper text

--- packages/ui/src/primitives/tailwind/Input/index.tsx
+++ packages/ui/src/primitives/tailwind/Input/index.tsx
@@ -23,45 +23,157 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import React, { useId, useMemo } from 'react'
-import { IoHelpCircleOutline } from 'react-icons/io5'
+import React, { useId, useLayoutEffect, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
-import InputBase, { InputBaseProps } from '../InputBase'
-import InputLabel from '../InputBase/InputLabel'
+import { HelpIconSm } from '../../../icons'
+import Tooltip from '../Tooltip'
 
-export interface InputProps extends InputBaseProps {
-  labelText: string
-  labelPosition: 'top' | 'left'
-  infoText?: string
+const variantSizes = {
+  xs: 'h-6 py-0.5 px-2',
+  l: 'h-8 py-1.5 px-2',
+  xl: 'h-10 py-2.5 px-2'
+} as const
+
+export interface InputBaseProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  variantSize: keyof typeof variantSizes
+
+  /**
+   * Optional React node to display at the start (left) of the s field.
+   * Typically used for icons or other UI elements that provide additional context.
+   */
+  startComponent?: React.ReactNode
+
+  /**
+   * Optional React node to display at the end (right) of the input field.
+   * Typically used for icons, buttons, or other UI elements that provide actions or additional information.
+   */
+  endComponent?: React.ReactNode
+
+  /**
+   * Specifies the validation state of the input field, affecting its outline color and the color of helper text.
+   * - `success` indicates a successful input.
+   * - `error` indicates an error in the input.
+   */
+  state: 'success' | 'error'
+
+  /**
+   * Optional helper text that provides additional information about the input field.
+   * When set, this will only be displayed when a valid `state` (`success` or `error`) is set.
+   * The color of the helper text is determined by the current state.
+   */
+  helperText?: string
+
   fullWidth?: boolean
 }
 
-const Input = (
-  { labelText, labelPosition, infoText, fullWidth, required, id, ...props }: InputProps,
+interface LabelProps {
+  labelText: string
+  labelPosition: 'top' | 'left'
+  infoText?: string
+}
+
+export type InputProps = InputBaseProps & LabelProps
+
+const InputBase = (
+  {
+    variantSize,
+    startComponent,
+    endComponent,
+    state,
+    helperText,
+    labelText,
+    labelPosition,
+    infoText,
+    required,
+    id,
+    fullWidth,
+    ...props
+  }: InputProps,
   ref: React.ForwardedRef<HTMLInputElement>
 ) => {
+  const containerClass = twMerge(
+    'flex w-full items-center gap-x-2 rounded-md border-[0.5px] border-[#42454D] bg-[#141619] transition-colors duration-300',
+    variantSizes[variantSize],
+    'hover:border-[#9CA0AA] hover:bg-[#191B1F]',
+    'has-[:focus]:border-[#375DAF] has-[:focus]:outline-none',
+    'has-[:disabled]:border-[#42454D] has-[:disabled]:bg-[#191B1F]',
+    state === 'success' && 'border-[#10B981]',
+    state === 'error' && 'border-[#C3324B]'
+  )
+
+  const inputClass = twMerge(
+    'peer order-2 h-full w-full bg-inherit text-[#9CA0AA] outline-none focus:text-[#F5F5F5] disabled:text-[#6B6F78]'
+  )
   const tempId = useId()
   const inputId = id || tempId
-  const comp = <IoHelpCircleOutline />
-  console.log(comp, typeof comp)
-  const containerClass = useMemo(() => {
-    let _class = `flex ${fullWidth ? 'w-full' : 'w-fit'}`
 
-    if (labelPosition === 'top') {
-      _class = twMerge(_class, 'flex-col items-start gap-y-2')
-    } else if (labelPosition === 'left') {
-      _class = twMerge(_class, 'flex-row items-center gap-x-2')
+  const labelRef = useRef<HTMLLabelElement>(null)
+
+  const [helperOffset, setHelperOffset] = useState('')
+  useLayoutEffect(() => {
+    const updateHelperTextPosition = () => {
+      if (labelPosition === 'left' && labelRef.current) {
+        setHelperOffset(`${labelRef.current.offsetWidth + 8}px`)
+      } else {
+        setHelperOffset('')
+      }
     }
 
-    return _class
-  }, [labelText, labelPosition, fullWidth])
+    updateHelperTextPosition()
+
+    window.addEventListener('resize', updateHelperTextPosition)
+    return () => {
+      window.removeEventListener('resize', updateHelperTextPosition)
+    }
+  }, [labelPosition])
 
   return (
-    <div className={containerClass}>
-      <InputLabel htmlFor={inputId} labelText={labelText} infoText={infoText} required={required} />
-      <InputBase id={inputId} required={required} {...props} ref={ref} />
+    <div className={`flex flex-col gap-y-2 ${fullWidth ? 'w-full' : 'w-fit'}`}>
+      <div className={`flex ${labelPosition === 'top' ? 'flex-col gap-y-2' : 'flex-row items-center gap-x-2'}`}>
+        {labelText && (
+          <label htmlFor={inputId} className="block text-xs font-medium" ref={labelRef}>
+            <div className="flex flex-row items-center gap-x-1.5">
+              <div className="flex flex-row items-center gap-x-0.5">
+                {required && <span className="text-sm text-[#E11D48]">*</span>}
+                <span className="text-xs text-[#D3D5D9]">{labelText}</span>
+              </div>
+
+              {infoText && (
+                <Tooltip content={infoText}>
+                  <HelpIconSm className="text-[#9CA0AA]" />
+                </Tooltip>
+              )}
+            </div>
+          </label>
+        )}
+
+        <div className={containerClass}>
+          <input spellCheck={false} className={inputClass} ref={ref} id={inputId} {...props} />
+          {startComponent && (
+            <div className="order-1 flex items-center justify-center text-[#9CA0AA] peer-disabled:text-[#42454D]">
+              {startComponent}
+            </div>
+          )}
+          {endComponent && (
+            <div className="order-3 flex items-center justify-center text-[#9CA0AA] peer-disabled:text-[#42454D]">
+              {endComponent}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {helperText && (
+        <span
+          className={`text-xs ${state === 'success' ? 'text-[#0D9467]' : 'text-[#C3324B]'}`}
+          style={{
+            translate: helperOffset
+          }}
+        >
+          {helperText}
+        </span>
+      )}
     </div>
   )
 }
 
-export default React.forwardRef(Input)
+export default React.forwardRef(InputBase)
