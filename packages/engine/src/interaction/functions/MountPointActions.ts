5c7136dc5c608467aa747c4921a4929b7aa6f7d7
Sam Mazer <sam.mazer@theinfinitereality.com>
2024-10-31 15:39:40 -0700

    Revert "refactoring from record to Map for lookup times"
    This reverts commit e0125d71558d38127642e456902876c0c7e96ced.

--- packages/engine/src/interaction/functions/MountPointActions.ts
+++ packages/engine/src/interaction/functions/MountPointActions.ts
@@ -26,7 +26,7 @@ Infinite Reality Engine. All Rights Reserved.
 import matches from 'ts-matches'
 
 import { EntityUUID, matchesEntityUUID } from '@ir-engine/ecs'
-import { defineAction, defineState, getMutableState, getState } from '@ir-engine/hyperflux'
+import { defineAction, defineState, getMutableState, none } from '@ir-engine/hyperflux'
 import { NetworkTopics, WorldNetworkAction } from '@ir-engine/network'
 
 export class MountPointActions {
@@ -42,8 +42,8 @@ export class MountPointActions {
 export const MountPointState = defineState({
   name: 'MountPointState',
   initial: () => ({
-    mountsToMountedEntities: new Map<EntityUUID, EntityUUID>(),
-    mountedEntitiesToMounts: new Map<EntityUUID, EntityUUID>()
+    mountsToMountedEntities: {} as Record<EntityUUID, EntityUUID>,
+    mountedEntitiesToMounts: {} as Record<EntityUUID, EntityUUID>
   }),
 
   receptors: {
@@ -61,21 +61,21 @@ export const MountPointState = defineState({
   }
 })
 const addEntry = (targetMount: EntityUUID, mountedEntity: EntityUUID) => {
-  const state = getState(MountPointState)
-  state.mountsToMountedEntities.set(targetMount, mountedEntity)
-  state.mountedEntitiesToMounts.set(mountedEntity, targetMount)
+  const state = getMutableState(MountPointState)
+  state.mountsToMountedEntities[targetMount].merge(mountedEntity)
+  state.mountedEntitiesToMounts[mountedEntity].merge(targetMount)
 }
 
 const removeEntry = (targetMount: EntityUUID, mountedEntity: EntityUUID) => {
-  const state = getState(MountPointState)
-  state.mountsToMountedEntities.delete(targetMount)
-  state.mountedEntitiesToMounts.delete(mountedEntity)
+  const state = getMutableState(MountPointState)
+  state.mountsToMountedEntities[targetMount].set(none)
+  state.mountedEntitiesToMounts[mountedEntity].set(none)
 }
 
 const onEntityDestroyed = (entityUUID: EntityUUID) => {
-  const state = getState(MountPointState)
-  if (state.mountedEntitiesToMounts.has(entityUUID)) {
-    const mountUUID = state.mountedEntitiesToMounts.get(entityUUID)
+  const state = getMutableState(MountPointState)
+  if (entityUUID in state.mountedEntitiesToMounts.value) {
+    const mountUUID = state.mountedEntitiesToMounts[entityUUID].value
     if (mountUUID) {
       removeEntry(mountUUID, entityUUID)
     }
