ee4bcd94b1770de0348443a576ec45c1da2cb07e
Michael Estes <michael@etherealengine.com>
2024-11-01 10:10:13 -0700

    Add back shadow material for not visible

--- packages/engine/src/scene/components/GroundPlaneComponent.ts
+++ packages/engine/src/scene/components/GroundPlaneComponent.ts
@@ -23,17 +23,18 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { Mesh, MeshStandardMaterial, PlaneGeometry } from 'three'
+import {
+  Mesh,
+  MeshStandardMaterial,
+  MeshStandardMaterialParameters,
+  PlaneGeometry,
+  ShadowMaterial,
+  ShadowMaterialParameters
+} from 'three'
 
 import { UUIDComponent } from '@ir-engine/ecs'
 import { defineComponent, removeComponent, setComponent, useComponent } from '@ir-engine/ecs/src/ComponentFunctions'
-import {
-  createEntity,
-  entityExists,
-  generateEntityUUID,
-  removeEntity,
-  useEntityContext
-} from '@ir-engine/ecs/src/EntityFunctions'
+import { createEntity, generateEntityUUID, removeEntity, useEntityContext } from '@ir-engine/ecs/src/EntityFunctions'
 import { S } from '@ir-engine/ecs/src/schemas/JSONSchemas'
 import { useHookstate } from '@ir-engine/hyperflux'
 import { ColliderComponent } from '@ir-engine/spatial/src/physics/components/ColliderComponent'
@@ -65,22 +66,29 @@ export const GroundPlaneComponent = defineComponent({
       const meshEntity = createEntity()
       setComponent(meshEntity, EntityTreeComponent, { parentEntity: entity })
       setComponent(meshEntity, UUIDComponent, generateEntityUUID())
+      setComponent(meshEntity, VisibleComponent)
       return meshEntity
     }).value
 
-    const mesh = useMeshComponent(
-      meshEntity,
-      () => new PlaneGeometry(10000, 10000),
-      () => new MeshStandardMaterial()
-    )
+    const createMaterial = () => {
+      const materialParams = {
+        polygonOffset: true,
+        polygonOffsetFactor: -0.01,
+        polygonOffsetUnits: 1,
+        color: component.color.value
+      } as MeshStandardMaterialParameters | ShadowMaterialParameters
+
+      return component.visible.value
+        ? new MeshStandardMaterial(materialParams)
+        : new ShadowMaterial({ opacity: 0.5, ...materialParams })
+    }
+
+    const mesh = useMeshComponent(meshEntity, () => new PlaneGeometry(10000, 10000), createMaterial)
 
     useLayoutEffect(() => {
       const meshVal = mesh.value as Mesh<PlaneGeometry, MeshStandardMaterial>
       meshVal.geometry.rotateX(-Math.PI / 2)
       meshVal.name = 'GroundPlaneMesh'
-      meshVal.material.polygonOffset = true
-      meshVal.material.polygonOffsetFactor = -0.01
-      meshVal.material.polygonOffsetUnits = 1
 
       setComponent(entity, ObjectLayerMaskComponent, ObjectLayerMasks.Scene)
       setComponent(entity, RigidBodyComponent, { type: BodyTypes.Fixed })
@@ -102,12 +110,7 @@ export const GroundPlaneComponent = defineComponent({
     }, [component.color])
 
     useLayoutEffect(() => {
-      if (component.visible.value) {
-        setComponent(meshEntity, VisibleComponent)
-        return () => {
-          if (entityExists(meshEntity)) removeComponent(meshEntity, VisibleComponent)
-        }
-      }
+      mesh.material.set(createMaterial())
     }, [component.visible.value])
 
     return null
