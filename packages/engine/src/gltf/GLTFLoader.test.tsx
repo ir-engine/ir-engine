5bde253400a4fa587813566963b56be0e40fc455
Sam Mazer <sam.mazer@theinfinitereality.com>
2024-11-08 15:54:08 -0800

    fixed some missing imports, undid the commenting out of the tests, fixed spy init

--- packages/engine/src/gltf/GLTFLoader.test.tsx
+++ packages/engine/src/gltf/GLTFLoader.test.tsx
@@ -37,8 +37,10 @@ import { applyIncomingActions, getMutableState, getState } from '@ir-engine/hype
 import { DirectionalLightComponent, PointLightComponent, SpotLightComponent } from '@ir-engine/spatial'
 import { CameraComponent } from '@ir-engine/spatial/src/camera/components/CameraComponent'
 import { RapierWorldState } from '@ir-engine/spatial/src/physics/classes/Physics'
+import { BoneComponent } from '@ir-engine/spatial/src/renderer/components/BoneComponent.ts'
 import { MeshComponent } from '@ir-engine/spatial/src/renderer/components/MeshComponent'
 import { SceneComponent } from '@ir-engine/spatial/src/renderer/components/SceneComponents'
+import { SkinnedMeshComponent } from '@ir-engine/spatial/src/renderer/components/SkinnedMeshComponent.ts'
 import {
   MaterialInstanceComponent,
   MaterialStateComponent
@@ -52,6 +54,7 @@ import { InstancedMesh, MathUtils, MeshStandardMaterial } from 'three'
 import { afterEach, beforeEach, describe, it } from 'vitest'
 import { overrideFileLoaderLoad } from '../../tests/util/loadGLTFAssetNode'
 import { AssetLoaderState } from '../assets/state/AssetLoaderState'
+import { AnimationComponent } from '../avatar/components/AnimationComponent.ts'
 import { GLTFComponent } from './GLTFComponent'
 import { GLTFDocumentState } from './GLTFDocumentState'
 import { KHRUnlitExtensionComponent, MaterialDefinitionComponent } from './MaterialDefinitionComponent'
@@ -153,11 +156,11 @@ describe('GLTF Loader', () => {
 
     const dracoLoader = getState(AssetLoaderState).gltfLoader.dracoLoader!
 
-    const spy = Sinon.spy()
-    dracoLoader.preload = () => {
-      spy()
-      return dracoLoader
-    }
+    const spy = Sinon.spy(dracoLoader, 'preload')
+    // dracoLoader.preload = () => {
+    //   spy()
+    //   return dracoLoader
+    // }
 
     setComponent(entity, UUIDComponent, generateEntityUUID())
     setComponent(entity, GLTFComponent, { src: draco_gltf })
@@ -304,64 +307,62 @@ describe('GLTF Loader', () => {
     unmount()
   })
 
-  /** todo figure out why this test is failing intermittently and re-enable it once fixed */
-  // it('can load morph targets', async () => {
-  //   const entity = setupEntity()
-  //
-  //   setComponent(entity, UUIDComponent, generateEntityUUID())
-  //   setComponent(entity, GLTFComponent, { src: morph_gltf })
-  //
-  //   const { rerender, unmount } = render(<></>)
-  //   applyIncomingActions()
-  //   await act(() => rerender(<></>))
-  //
-  //   const instanceID = GLTFComponent.getInstanceID(entity)
-  //   const gltfDocumentState = getState(GLTFDocumentState)
-  //   const gltf = gltfDocumentState[instanceID]
-  //
-  //   const meshEntity = getChildrenWithComponents(entity, [MeshComponent])[0]
-  //   const mesh = getComponent(meshEntity, MeshComponent)
-  //   assert(mesh.geometry.morphAttributes)
-  //   assert(mesh.geometry.morphTargetsRelative)
-  //
-  //   unmount()
-  // })
+  it('can load morph targets', async () => {
+    const entity = setupEntity()
 
-  /** todo figure out why this test is failing intermittently and re-enable it once fixed */
-  // it('can load skinned meshes with bones and animations', async () => {
-  //   const entity = setupEntity()
-  //
-  //   setComponent(entity, UUIDComponent, generateEntityUUID())
-  //   setComponent(entity, GLTFComponent, { src: skinned_gltf })
-  //
-  //   const { rerender, unmount } = render(<></>)
-  //   applyIncomingActions()
-  //   await act(() => rerender(<></>))
-  //
-  //   const instanceID = GLTFComponent.getInstanceID(entity)
-  //   const gltfDocumentState = getState(GLTFDocumentState)
-  //   const gltf = gltfDocumentState[instanceID]
-  //
-  //   const joints = gltf.skins!.reduce((accum, skin) => {
-  //     if (skin.joints) accum.push(...skin.joints)
-  //     return accum
-  //   }, [] as number[])
-  //
-  //   const skinnedMeshEntities = getChildrenWithComponents(entity, [SkinnedMeshComponent])
-  //   const boneEntities = getChildrenWithComponents(entity, [BoneComponent])
-  //   const animationComponent = getComponent(entity, AnimationComponent)
-  //
-  //   assert(skinnedMeshEntities.length === gltf.skins!.length)
-  //   assert(boneEntities.length === joints.length)
-  //   assert(animationComponent.animations.length === gltf.animations!.length)
-  //
-  //   for (const anim of animationComponent.animations) {
-  //     const gltfAnim = gltf.animations!.find((a) => a.name === anim.name)
-  //     assert(gltfAnim?.channels.length === anim.tracks.length)
-  //   }
-  //
-  //   unmount()
-  // })
+    setComponent(entity, UUIDComponent, generateEntityUUID())
+    setComponent(entity, GLTFComponent, { src: morph_gltf })
+
+    const { rerender, unmount } = render(<></>)
+    applyIncomingActions()
+    await act(() => rerender(<></>))
+
+    const instanceID = GLTFComponent.getInstanceID(entity)
+    const gltfDocumentState = getState(GLTFDocumentState)
+    const gltf = gltfDocumentState[instanceID]
+
+    const meshEntity = getChildrenWithComponents(entity, [MeshComponent])[0]
+    const mesh = getComponent(meshEntity, MeshComponent)
+    assert(mesh.geometry.morphAttributes)
+    assert(mesh.geometry.morphTargetsRelative)
+
+    unmount()
+  })
+
+  it('can load skinned meshes with bones and animations', async () => {
+    const entity = setupEntity()
+
+    setComponent(entity, UUIDComponent, generateEntityUUID())
+    setComponent(entity, GLTFComponent, { src: skinned_gltf })
+
+    const { rerender, unmount } = render(<></>)
+    applyIncomingActions()
+    await act(() => rerender(<></>))
+
+    const instanceID = GLTFComponent.getInstanceID(entity)
+    const gltfDocumentState = getState(GLTFDocumentState)
+    const gltf = gltfDocumentState[instanceID]
+
+    const joints = gltf.skins!.reduce((accum, skin) => {
+      if (skin.joints) accum.push(...skin.joints)
+      return accum
+    }, [] as number[])
+
+    const skinnedMeshEntities = getChildrenWithComponents(entity, [SkinnedMeshComponent])
+    const boneEntities = getChildrenWithComponents(entity, [BoneComponent])
+    const animationComponent = getComponent(entity, AnimationComponent)
+
+    assert(skinnedMeshEntities.length === gltf.skins!.length)
+    assert(boneEntities.length === joints.length)
+    assert(animationComponent.animations.length === gltf.animations!.length)
+
+    for (const anim of animationComponent.animations) {
+      const gltfAnim = gltf.animations!.find((a) => a.name === anim.name)
+      assert(gltfAnim?.channels.length === anim.tracks.length)
+    }
+
+    unmount()
+  })
 
   it('can load cameras', async () => {
     const entity = setupEntity()
