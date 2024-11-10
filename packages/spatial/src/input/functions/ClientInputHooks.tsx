7506e4f1ac1ea80a6fe9fb8fe483682fda413fd3
HexaField <joshfield999@gmail.com>
2024-11-10 21:22:19 +1100

    add debug logs

--- packages/spatial/src/input/functions/ClientInputHooks.tsx
+++ packages/spatial/src/input/functions/ClientInputHooks.tsx
@@ -350,9 +350,11 @@ export const CanvasInputReactor = () => {
 export const MeshInputReactor = () => {
   const entity = useEntityContext()
   const shouldReceiveInput = useAncestorWithComponents(entity, [InputComponent])
+  console.log('MeshInputReactor', { shouldReceiveInput })
 
   useImmediateEffect(() => {
     const inputState = getState(InputState)
+    console.log('useImmediateEffect', { shouldReceiveInput })
     if (shouldReceiveInput) inputState.inputMeshes.add(entity)
     else inputState.inputMeshes.delete(entity)
   }, [shouldReceiveInput])
