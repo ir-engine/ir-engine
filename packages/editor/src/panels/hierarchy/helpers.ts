43121c4490f317437fb85f61c9f25c6c6179b464
sybiote <ghoshr698@gmail.com>
2024-11-13 21:11:39 +0530

    replace model with GTLF component

--- packages/editor/src/panels/hierarchy/helpers.ts
+++ packages/editor/src/panels/hierarchy/helpers.ts
@@ -35,9 +35,9 @@ import {
   UUIDComponent
 } from '@ir-engine/ecs'
 import { AllFileTypes } from '@ir-engine/engine/src/assets/constants/fileTypes'
+import { GLTFComponent } from '@ir-engine/engine/src/gltf/GLTFComponent'
 import { GLTFSnapshotState } from '@ir-engine/engine/src/gltf/GLTFState'
 import { nodeIsChild } from '@ir-engine/engine/src/gltf/gltfUtils'
-import { ModelComponent } from '@ir-engine/engine/src/scene/components/ModelComponent'
 import { SourceComponent } from '@ir-engine/engine/src/scene/components/SourceComponent'
 import { getModelSceneID } from '@ir-engine/engine/src/scene/functions/loaders/ModelFunctions'
 import { getMutableState, getState } from '@ir-engine/hyperflux'
@@ -144,7 +144,7 @@ function buildHierarchyTree(
   }
   array.push(item)
 
-  if (hasComponent(entity, ModelComponent) && showModelChildren) {
+  if (hasComponent(entity, GLTFComponent) && showModelChildren) {
     const modelSceneID = getModelSceneID(entity)
     const snapshotState = getState(GLTFSnapshotState)
     const snapshots = snapshotState[modelSceneID]
