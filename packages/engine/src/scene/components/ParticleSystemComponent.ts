172a79fcafe0f91a2cf94777d4e956055ca4fbf5
Jiatong Yao <jiatong@ethereal.com>
2024-11-22 13:40:34 -0600

    update gravity force default setting

--- packages/engine/src/scene/components/ParticleSystemComponent.ts
+++ packages/engine/src/scene/components/ParticleSystemComponent.ts
@@ -607,7 +607,7 @@ export const BehaviorJSONDefaults: { [type: string]: BehaviorJSON } = {
   GravityForce: {
     type: 'GravityForce',
     center: [0, 0, 0],
-    magnitude: 1
+    magnitude: 10
   },
   ColorOverLife: {
     type: 'ColorOverLife',
