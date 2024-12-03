d9c76b53e061da4e102700b58dcee447fb4012bb
HexaField <joshfield999@gmail.com>
2024-12-03 16:01:30 +1100

    revert

--- packages/engine/src/gltf/GLTFComponent.tsx
+++ packages/engine/src/gltf/GLTFComponent.tsx
@@ -173,7 +173,14 @@ export const GLTFComponent = defineComponent({
   },
 
   useSceneLoaded(entity: Entity) {
-    return useOptionalComponent(entity, GLTFComponent)?.progress?.value === 100
+    const gltfComponent = useOptionalComponent(entity, GLTFComponent)
+    const instanceID = GLTFComponent.useInstanceID(entity)
+    const document = useMutableState(GLTFDocumentState)[instanceID].value
+    if (!gltfComponent || !document) return false
+
+    const dependencies = gltfComponent.dependencies
+    const progress = gltfComponent.progress.value
+    return componentDependenciesLoaded(dependencies.value as ComponentDependencies | undefined) && progress === 100
   },
 
   isSceneLoaded(entity: Entity) {
