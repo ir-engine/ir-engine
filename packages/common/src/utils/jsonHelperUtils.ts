0c390a885151be3d343c13655d60dce1a127a465
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2024-12-06 11:51:38 +0500

    Add flattening and unflattening utility functions with detailed documentation

--- packages/common/src/utils/jsonHelperUtils.ts
+++ packages/common/src/utils/jsonHelperUtils.ts
@@ -25,6 +25,34 @@ Infinite Reality Engine. All Rights Reserved.
 
 export type FlattenedEntry = { key: string; value: any }
 
+/**
+ * Flattens a nested object into an array of key-value pairs, where each key is a string representing the path to the value in the original object.
+ *
+ * @param obj - The object to be flattened.
+ * @param parentKey - The base path to be prepended to each key in the flattened result. Defaults to an empty string.
+ * @returns An array of objects, each containing a `key` and `value` property representing the flattened key-value pairs.
+ *
+ * @example
+ * ```typescript
+ * const nestedObject = {
+ *   a: {
+ *     b: {
+ *       c: 1,
+ *       d: [2, 3]
+ *     },
+ *     e: true
+ *   }
+ * };
+ * const result = flattenObjectToArray(nestedObject);
+ * // result:
+ * // [
+ * //   { key: 'a.b.c', value: 1 },
+ * //   { key: 'a.b.d.[0]', value: 2 },
+ * //   { key: 'a.b.d.[1]', value: 3 },
+ * //   { key: 'a.e', value: true }
+ * // ]
+ * ```
+ */
 export function flattenObjectToArray(obj: Record<string, any>, parentKey: string = ''): FlattenedEntry[] {
   const result: FlattenedEntry[] = []
 
@@ -53,6 +81,34 @@ export function flattenObjectToArray(obj: Record<string, any>, parentKey: string
   return result
 }
 
+/**
+ * Converts a flattened array of key-value pairs into a nested object.
+ *
+ * @param {FlattenedEntry[]} flattenedArray - The array of key-value pairs to unflatten.
+ * @returns {Record<string, any>} - The resulting nested object.
+ *
+ * @example
+ * ```typescript
+ * const flattenedArray = [
+ *   { key: 'a.b.c', value: '1' },
+ *   { key: 'a.b.d', value: 'true' },
+ *   { key: 'e[0]', value: 'false' },
+ *   { key: 'e[1]', value: '2' }
+ * ];
+ * const result = unflattenArrayToObject(flattenedArray);
+ * console.log(result);
+ * // Output:
+ * // {
+ * //   a: {
+ * //     b: {
+ * //       c: '1',
+ * //       d: true
+ * //     }
+ * //   },
+ * //   e: [false, '2']
+ * // }
+ * ```
+ */
 export function unflattenArrayToObject(flattenedArray: FlattenedEntry[]): Record<string, any> {
   const result: Record<string, any> = {}
 
