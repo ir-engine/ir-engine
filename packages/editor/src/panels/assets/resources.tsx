28abd4877b7da380dde8ff2dd5d4a874965a2bf9
Michael Floyd <mbfloyd@theinfinitereality.com>
2024-12-10 12:24:15 -0800

    aligned the "previous" to the left

--- packages/editor/src/panels/assets/resources.tsx
+++ packages/editor/src/panels/assets/resources.tsx
@@ -328,7 +328,7 @@ function ResourceItems() {
               <div className="mt-4 flex h-2.5 w-[calc(100%_-_16px)] flex-row border-t-[0.5px] border-solid pt-1 text-[smaller] text-gray-500">
                 {i > 0 && (
                   <button
-                    className="text-grey-500 mr-auto flex items-center justify-center px-4 py-2 text-xs"
+                    className="text-grey-500 mr-auto flex items-center justify-center px-0 py-2 text-xs"
                     onClick={() => handleScrollToPage(i - 1)} // Scroll to the previous page
                   >
                     {'Previous'}
