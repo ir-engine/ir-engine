b9a32b81f42f47e27b997ab0a3dda4812f3ec38a
Gheric Speiginer <gheric.speiginer@gmail.com>
2025-01-16 00:47:36 -0800

    Update AvatarIKSystem.test.tsx

--- packages/engine/src/avatar/systems/AvatarIKSystem.test.tsx
+++ packages/engine/src/avatar/systems/AvatarIKSystem.test.tsx
@@ -150,12 +150,12 @@ describe('AvatarIKSystem', () => {
       })
     )
     applyIncomingActions()
-    await vi.waitFor(() => {
-      expect(
+    await vi.waitUntil(() => {
+      return (
         UUIDComponent.getEntityByUUID(rightHandUuid) &&
-          UUIDComponent.getEntityByUUID(headUuid) &&
-          UUIDComponent.getEntityByUUID(leftHandUuid)
-      ).toBeTruthy()
+        UUIDComponent.getEntityByUUID(headUuid) &&
+        UUIDComponent.getEntityByUUID(leftHandUuid)
+      )
     })
 
     const headEntity = UUIDComponent.getEntityByUUID(headUuid)
@@ -184,7 +184,7 @@ describe('AvatarIKSystem', () => {
     AvatarIKTargetComponent.blendWeight[leftFootEntity] = 1
     AvatarIKTargetComponent.blendWeight[rightFootEntity] = 1
 
-    await vi.waitFor(() => {
+    await vi.waitUntil(() => {
       SystemDefinitions.get(TransformDirtyUpdateSystem)?.execute()
       SystemDefinitions.get(TransformSystem)?.execute()
       SystemDefinitions.get(TransformDirtyCleanupSystem)?.execute()
@@ -210,10 +210,13 @@ describe('AvatarIKSystem', () => {
         getComponent(avatarEntity, AvatarRigComponent).bonesToEntities.rightFoot,
         new Vector3()
       )
-      expect(rightHandIkPos.distanceTo(rightHandPosition) < 0.1).toBeTruthy()
-      expect(leftHandIkPos.distanceTo(leftHandPosition) < 0.1).toBeTruthy()
-      expect(leftFootIkPos.distanceTo(leftFootPosition) < 0.1).toBeTruthy()
-      expect(rightFootIkPos.distanceTo(rightFootPosition) < 0.1).toBeTruthy()
+
+      return (
+        rightHandIkPos.distanceTo(rightHandPosition) < 0.1 &&
+        leftHandIkPos.distanceTo(leftHandPosition) < 0.1 &&
+        leftFootIkPos.distanceTo(leftFootPosition) < 0.1 &&
+        rightFootIkPos.distanceTo(rightFootPosition) < 0.1
+      )
     }, 1000)
   })
 })
