42e23c185fc9b582028684eb5c4935477ad7e0bd
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2025-01-25 14:13:41 +0100

    tst: Move `create/removeEntity` tests to `EntityFunctions.test.tsx`

--- packages/ecs/src/ECSLayers.test.tsx
+++ packages/ecs/src/ECSLayers.test.tsx
@@ -23,11 +23,16 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
+/**
+ * @note
+ * Other related code that also has ECSLayers specific tests:
+ * - EntityFunctions.test.tsx : createEntity removeEntity
+ * - UUIDComponent.test.tsx   : Almost every function
+ * */
+
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
-import { HyperFlux } from '@ir-engine/hyperflux'
 import { TransformComponent } from '@ir-engine/spatial/src/transform/components/TransformComponent'
-import * as bitECS from 'bitecs'
 import {
   defineComponent,
   getComponent,
@@ -44,87 +49,8 @@ import {
 import { createEntity } from './createEntity'
 import { createEngine, destroyEngine } from './Engine'
 import { Entity, UndefinedEntity } from './Entity'
-import { entityExists, removeEntity } from './EntityFunctions'
+import { entityExists } from './EntityFunctions'
 import { defineQuery } from './QueryFunctions'
-
-/** @todo Move this describe to `EntityFunctions.test.tsx` */
-describe('createEntity', () => {
-  beforeEach(() => {
-    createEngine()
-  })
-
-  afterEach(() => {
-    destroyEngine()
-  })
-
-  it('should use Layers.Simulation as the default value for `@param layerID` when it is omitted', () => {
-    const Expected = Layers.Simulation
-    // Set the data as expected
-    const testEntity = createEntity()
-    // Run and Check the result
-    const result = getComponent(testEntity, LayerComponent)
-    expect(result).toBe(Expected)
-  })
-
-  it('should create a new entity by calling bitECS.addEntity with HyperFlux.store as its world argument', () => {
-    // Set the data as expected
-    const testEntity = createEntity()
-    // Run and Check the result
-    const result = bitECS.entityExists(HyperFlux.store, testEntity)
-    expect(result).toBeTruthy()
-  })
-
-  it('should set a LayerComponent on the newly created entity with `@param layerID` as its layer argument', () => {
-    // Set the data as expected
-    const expectedLayer = Layers.Authoring
-    const testEntity = createEntity(expectedLayer)
-    // Run and Check the result
-    const result = getComponent(testEntity, LayerComponent)
-    expect(result).toBeTruthy()
-    expect(result).toBe(expectedLayer)
-  })
-
-  it('should return the newly created entity', () => {
-    const result = createEntity()
-    expect(result).not.toBe(undefined)
-    expect(result).toBeTruthy()
-    expect(entityExists(result)).toBeTruthy()
-  })
-
-  it('should throw an error when `@param layerID` is not a valid LayerID', () => {
-    expect(() => createEntity(42_000 as LayerID)).toThrowError()
-  })
-}) //:: createEntity
-
-/** @todo Move this describe to `EntityFunctions.test.tsx` */
-describe('removeEntity', () => {
-  beforeEach(() => {
-    createEngine()
-  })
-
-  afterEach(() => {
-    destroyEngine()
-  })
-
-  it('should call bitECS.removeEntity with HyperFlux.store and `@param entity` as arguments', () => {
-    // Set the data as expected
-    const testEntity = bitECS.addEntity(HyperFlux.store) as Entity
-    // Run and Check the result
-    removeEntity(testEntity)
-    const result = bitECS.entityExists(HyperFlux.store, testEntity)
-    expect(result).toBeFalsy()
-  })
-
-  /**
-  // @note
-  // Just for reference. These tests require circular logic that cannot be solved
-  // Cannot check if the process of removing an entity is not happening on a falsy entity (aka already does not exist)
-  // Cannot check if removing all components from an entity has been triggered on an entity that after the process does not exist
-  it.todo('should not do anything if `@param entity` is falsy', () => {})
-  it.todo('should not do anything if the result of `entityExists(entity)` is falsy', () => {})
-  it.todo('should call removeAllComponents with `@param entity`', () => {})
-  */
-}) //:: removeEntity
 
 describe('LayerFunctions', () => {
   beforeEach(() => {
--- packages/ecs/src/EntityFunctions.test.tsx
+++ packages/ecs/src/EntityFunctions.test.tsx
@@ -25,12 +25,15 @@ Infinite Reality Engine. All Rights Reserved.
 
 import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 
+import { HyperFlux } from '@ir-engine/hyperflux'
+import * as bitECS from 'bitecs'
+import { getComponent, LayerComponent, LayerID, Layers } from './ComponentFunctions'
 import { createEngine, destroyEngine } from './Engine'
-import { UndefinedEntity } from './Entity'
-import { entityExists } from './EntityFunctions'
+import { Entity, UndefinedEntity } from './Entity'
+import { entityExists, removeEntity } from './EntityFunctions'
 import { createEntity } from './createEntity'
 
-describe('EntityFunctions', async () => {
+describe('createEntity', () => {
   beforeEach(() => {
     createEngine()
   })
@@ -38,18 +41,80 @@ describe('EntityFunctions', async () => {
     return destroyEngine()
   })
 
-  describe('createEntity', () => {
-    it('should return a valid entity ID that returns true when given to `entityExists`', () => {
-      const result = createEntity()
-      expect(entityExists(result)).toBeTruthy()
-    })
+  it('should return a valid entity ID that returns true when given to `entityExists`', () => {
+    const result = createEntity()
+    expect(entityExists(result)).toBeTruthy()
+  })
 
-    it('should never return UndefinedEntity', () => {
-      const result = createEntity()
-      expect(result).not.toBe(UndefinedEntity)
-    })
-  }) //:: createEntity
+  it('should never return UndefinedEntity', () => {
+    const result = createEntity()
+    expect(result).not.toBe(UndefinedEntity)
+  })
 
-  /** @todo */
-  describe('removeEntity', () => {}) //:: removeEntity
-})
+  it('should use Layers.Simulation as the default value for `@param layerID` when it is omitted', () => {
+    const Expected = Layers.Simulation
+    // Set the data as expected
+    const testEntity = createEntity()
+    // Run and Check the result
+    const result = getComponent(testEntity, LayerComponent)
+    expect(result).toBe(Expected)
+  })
+
+  it('should create a new entity by calling bitECS.addEntity with HyperFlux.store as its world argument', () => {
+    // Set the data as expected
+    const testEntity = createEntity()
+    // Run and Check the result
+    const result = bitECS.entityExists(HyperFlux.store, testEntity)
+    expect(result).toBeTruthy()
+  })
+
+  it('should set a LayerComponent on the newly created entity with `@param layerID` as its layer argument', () => {
+    // Set the data as expected
+    const expectedLayer = Layers.Authoring
+    const testEntity = createEntity(expectedLayer)
+    // Run and Check the result
+    const result = getComponent(testEntity, LayerComponent)
+    expect(result).toBeTruthy()
+    expect(result).toBe(expectedLayer)
+  })
+
+  it('should return the newly created entity', () => {
+    const result = createEntity()
+    expect(result).not.toBe(undefined)
+    expect(result).toBeTruthy()
+    expect(entityExists(result)).toBeTruthy()
+  })
+
+  it('should throw an error when `@param layerID` is not a valid LayerID', () => {
+    expect(() => createEntity(42_000 as LayerID)).toThrowError()
+  })
+}) //:: createEntity
+
+describe('removeEntity', () => {
+  beforeEach(() => {
+    createEngine()
+  })
+
+  afterEach(() => {
+    destroyEngine()
+  })
+
+  it('should call bitECS.removeEntity with HyperFlux.store and `@param entity` as arguments', () => {
+    // Set the data as expected
+    const testEntity = bitECS.addEntity(HyperFlux.store) as Entity
+    // Run and Check the result
+    removeEntity(testEntity)
+    const result = bitECS.entityExists(HyperFlux.store, testEntity)
+    expect(result).toBeFalsy()
+  })
+
+  /**
+  // @note
+  // Just for reference. These tests require circular logic that cannot be solved
+  // Cannot check if the process of removing an entity is not happening on a falsy entity (aka already does not exist)
+  // Cannot check if removing all components from an entity has been triggered on an entity that after the process does not exist
+  it.todo('should not do anything if `@param entity` is falsy', () => {})
+  it.todo('should not do anything if the result of `entityExists(entity)` is falsy', () => {})
+  it.todo('should call removeAllComponents with `@param entity`', () => {})
+  */
+}) //:: removeEntity
