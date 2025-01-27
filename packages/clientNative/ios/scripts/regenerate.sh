e9b1bc7f6a062b8bfbee2a5bf63b696d5d480270
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2025-01-27 13:32:24 -0500

    Update polygen to v0.1.0 (#53)
    * Update package
    
    * Patch memory.js
    
    * Update project config

--- packages/clientNative/ios/scripts/regenerate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# if which npx >/dev/null; then
-#  echo "[Polygen] Regenerating codegen artifacts"
-#  npx polygen generate
-# else
- echo "[Polygen] `npx` command not found. Skipping auto-generating"
-# fi
