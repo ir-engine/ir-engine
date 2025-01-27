cd47cc3a275e6fb93add9f444cbb74ed90573ead
Andy Chen <andychen5671@gmail.com>
2025-01-27 12:55:22 -0500

    overflow x hidden

--- packages/ui/src/primitives/tailwind/Select/index.tsx
+++ packages/ui/src/primitives/tailwind/Select/index.tsx
@@ -339,7 +339,7 @@ const Select = ({
 
           {open && (
             <div
-              className={`absolute z-50 flex w-full flex-col overflow-y-auto rounded-lg ${
+              className={`absolute z-50 flex w-full flex-col overflow-y-auto overflow-x-hidden rounded-lg ${
                 positioning.direction === 'down' && 'top-[calc(100%+0.5rem)]'
               } ${positioning.direction === 'up' && 'bottom-[calc(100%+0.5rem)]'}`}
               style={{
