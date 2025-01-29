865a9eff98f21c707bac36aa7bb072e62e44c170
Aidan Caruso <lonedevr@gmail.com>
2025-01-28 23:52:29 -0500

    Fix avatar ik system test

--- packages/engine/src/avatar/systems/AvatarIKSystem.test.tsx
+++ packages/engine/src/avatar/systems/AvatarIKSystem.test.tsx
@@ -45,10 +45,10 @@ import {
   TransformDirtyUpdateSystem,
   TransformSystem
 } from '@ir-engine/spatial/src/transform/systems/TransformSystem'
-import { mockSpatialEngine } from '@ir-engine/spatial/tests/util/mockSpatialEngine'
 import { Quaternion, Vector3 } from 'three'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { mockAnimatedAvatar } from '../../../tests/avatar/mockAnimatedAvatar'
+import { startEngineReactor } from '../../../tests/startEngineReactor'
 import { overrideFileLoaderLoad } from '../../../tests/util/loadGLTFAssetNode'
 import { AvatarRigComponent } from '../components/AvatarAnimationComponent'
 import { AvatarIKComponent, AvatarIKTargetComponent, IKMatrixComponent } from '../components/AvatarIKComponents'
@@ -56,7 +56,7 @@ import { NormalizedBoneComponent } from '../components/NormalizedBoneComponent'
 import '../state/AvatarIKTargetState'
 import { AvatarNetworkAction } from '../state/AvatarNetworkActions'
 import { AnimationSystem } from './AnimationSystem'
-import { AvatarAnimationSystem, AvatarAnimationSystemReactor } from './AvatarAnimationSystem'
+import { AvatarAnimationSystem } from './AvatarAnimationSystem'
 import { AvatarIkReactor, AvatarIKSystem } from './AvatarIKSystem'
 
 const default_url = 'packages/projects/default-project/assets'
@@ -65,7 +65,7 @@ describe('AvatarIKSystem', () => {
 
   beforeEach(async () => {
     createEngine()
-    mockSpatialEngine()
+    startEngineReactor()
   })
 
   afterEach(() => {
@@ -79,7 +79,6 @@ describe('AvatarIKSystem', () => {
     setComponent(avatarEntity, UUIDComponent, avatarUuid)
     setComponent(avatarEntity, AvatarIKComponent)
     startReactor(AvatarIkReactor)
-    startReactor(AvatarAnimationSystemReactor)
     const rig = getComponent(avatarEntity, AvatarRigComponent)
 
     // no idea why this is necessary
@@ -87,7 +86,7 @@ describe('AvatarIKSystem', () => {
       const bone = getOptionalComponent(entity as unknown as Entity, NormalizedBoneComponent)
       if (bone) bone.quaternion.fastSlerp = Quaternion.prototype.fastSlerp
     }
-
+    console.log('WOW THE RIG', rig)
     await vi.waitFor(() => {
       expect(
         getOptionalComponent(rig.bonesToEntities.rightUpperArm, IKMatrixComponent) &&
