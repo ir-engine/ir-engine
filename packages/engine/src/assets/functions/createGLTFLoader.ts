d09cc894041cf104574a90146bb1bc3e29404eed
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2025-01-31 20:53:43 -0500

    Fix Avatar Texture Loading (#57)
    * Fix extension support
    
    * Patch expo GL

--- packages/engine/src/assets/functions/createGLTFLoader.ts
+++ packages/engine/src/assets/functions/createGLTFLoader.ts
@@ -54,12 +54,8 @@ export const initializeKTX2Loader = (loader: GLTFLoader) => {
       hasFeature: () => false,
       extensions: new Map([
         ['WEBGL_compressed_texture_astc', true],
-        ['WEBGL_compressed_texture_etc1', false],
         ['WEBGL_compressed_texture_etc', true],
-        ['WEBGL_compressed_texture_s3tc', false],
-        ['EXT_texture_compression_bptc', false],
-        ['WEBGL_compressed_texture_pvrtc', true],
-        ['WEBKIT_WEBGL_compressed_texture_pvrtc', false]
+        ['WEBGL_compressed_texture_pvrtc', true]
       ]),
       capabilities: {
         isWebGL2: true
