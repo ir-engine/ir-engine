c5b9fb6d79d8fb4901203bb8daa8d031a03aef38
Appaji <appaji12368@gmail.com>
2025-02-05 19:05:06 +0530

    remove junk

--- packages/client-core/src/user/menus/SettingsMenu.tsx
+++ packages/client-core/src/user/menus/SettingsMenu.tsx
@@ -356,8 +356,6 @@ function GraphicsTab() {
 export default function SettingsMenu() {
   const { t } = useTranslation()
 
-  // relative z-50 h-fit max-h-[90vh] w-[50vw] min-w-[720px] max-w-2xl overflow-y-auto rounded-2xl bg-surface-1 p-6 mdh:max-h-[60vh] mdh:p-10
-
   return (
     <div className="relative z-50 h-fit max-h-[90vh] min-h-[50vh] w-[50vw] min-w-[720px] max-w-2xl overflow-y-auto rounded-2xl bg-surface-1 p-6 text-text-secondary mdh:max-h-[60vh] mdh:p-10">
       <div className="mb-[17px]">
