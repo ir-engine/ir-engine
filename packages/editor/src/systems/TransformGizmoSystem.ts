cfdb3d98e0eaa8cb8f588f78c733b3ae64a258ae
HexaField <joshfield999@gmail.com>
2025-02-08 12:44:37 +1100

    fix export regressions

--- packages/editor/src/systems/TransformGizmoSystem.ts
+++ packages/editor/src/systems/TransformGizmoSystem.ts
@@ -42,10 +42,10 @@ import { ObjectLayers } from '@ir-engine/spatial/src/renderer/constants/ObjectLa
 import { TransformGizmoTagComponent } from '@ir-engine/spatial/src/transform/components/TransformComponent'
 import { MathUtils, Raycaster, Vector3 } from 'three'
 import { TransformGizmoControlComponent } from '../classes/gizmo/transform/TransformGizmoControlComponent'
+import { TransformGizmoControlledComponent } from '../classes/gizmo/transform/TransformGizmoControlledComponent'
 import { controlUpdate, gizmoUpdate, planeUpdate } from '../functions/transformGizmoHelper'
 import { EditorHelperState } from '../services/EditorHelperState'
 import { SelectionState } from '../services/SelectionServices'
-import { TransformGizmoControlledComponent } from '../classes/gizmo/transform/TransformGizmoControlledComponent'
 
 const transformGizmoControllerQuery = defineQuery([TransformGizmoControlComponent])
 
@@ -113,7 +113,10 @@ const useGizmoControl = (entities: Entity[]) => {
   const controlledEntity = entities[entities.length - 1]
 
   const gizmoControlledComponent = useOptionalComponent(controlledEntity, TransformGizmoControlledComponent)
-  const gizmoControlComponent = useOptionalComponent(gizmoControlledComponent ? gizmoControlledComponent.controller.value : UndefinedEntity, TransformGizmoControlComponent)
+  const gizmoControlComponent = useOptionalComponent(
+    gizmoControlledComponent ? gizmoControlledComponent.controller.value : UndefinedEntity,
+    TransformGizmoControlComponent
+  )
   const editorHelperState = useMutableState(EditorHelperState)
 
   useEffect(() => {
