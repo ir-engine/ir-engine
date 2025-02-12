b3d93096be1308ab5cf7503045ac7ea645c08e09
Appaji <appaji12368@gmail.com>
2025-02-12 22:54:02 +0530

    remove unrelated classes (because they're not in tailwind config yet)

--- packages/ui/src/primitives/tailwind/Modal/index.tsx
+++ packages/ui/src/primitives/tailwind/Modal/index.tsx
@@ -141,7 +141,7 @@ const Modal = ({
   submitButtonDisabled,
   showCloseButton = true
 }: ModalProps) => {
-  const twClassName = twMerge('animate-expandModal absolute z-50 w-full bg-surface-1 will-change-transform', className)
+  const twClassName = twMerge('absolute z-50 w-full bg-surface-1', className)
   return (
     <div data-test-id={id} className={twClassName}>
       <div className="relative rounded-lg shadow">
