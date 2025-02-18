6a8a7c2c55c660f3bcfb8b16793af90afb696e9f
Andy Chen <andychen5671@gmail.com>
2025-02-17 20:17:57 -0500

    close context menu on unpublish

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -396,6 +396,7 @@ export default function AddEditLocationModal(props: {
             onMouseDown={(e) => {
               e.stopPropagation()
               unPublishLocation()
+              anchorEvent.set(null)
             }}
           />
         </div>
