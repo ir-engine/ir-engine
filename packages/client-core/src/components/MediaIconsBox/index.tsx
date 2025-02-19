00d50c650f5024100fd3769659a91a8176c22312
Appaji <appaji12368@gmail.com>
2025-02-20 00:22:46 +0530

    replace icon with stock icon

--- packages/client-core/src/components/MediaIconsBox/index.tsx
+++ packages/client-core/src/components/MediaIconsBox/index.tsx
@@ -44,7 +44,13 @@ import multiLogger from '@ir-engine/common/src/logger'
 import { EngineState } from '@ir-engine/ecs'
 import { isMobile } from '@ir-engine/spatial/src/common/functions/isMobile'
 import { Tooltip } from '@ir-engine/ui'
-import { Microphone01Lg, MicrophoneOff, Screenshare } from '@ir-engine/ui/src/icons'
+import {
+  Microphone01Lg,
+  MicrophoneOff,
+  Screenshare,
+  VideoRecorderLg,
+  VideoRecorderOffLg
+} from '@ir-engine/ui/src/icons'
 import LoadingView from '@ir-engine/ui/src/primitives/tailwind/LoadingView'
 import { IoAccessibility } from 'react-icons/io5'
 import { MdFlipCameraAndroid, MdOutlineViewInAr } from 'react-icons/md'
@@ -167,27 +173,11 @@ export const MediaIconsBox = () => {
                 logger.info({ event_name: 'toggle_camera', value: isCamVideoEnabled })
               }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
-                fill="none"
-                viewBox="0 0 24 24"
-                role="img"
-                stroke="currentColor"
-                className="h-6 w-6 text-[#080808]"
-              >
-                <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
-                  <path d="M22 8.931c0-.605 0-.908-.12-1.049a.5.5 0 0 0-.42-.173c-.183.014-.397.228-.826.657L17 12l3.634 3.634c.429.429.643.643.827.657a.5.5 0 0 0 .42-.173c.119-.14.119-.444.119-1.05zM2 9.8c0-1.68 0-2.52.327-3.162a3 3 0 0 1 1.311-1.311C4.28 5 5.12 5 6.8 5h5.4c1.68 0 2.52 0 3.162.327a3 3 0 0 1 1.311 1.311C17 7.28 17 8.12 17 9.8v4.4c0 1.68 0 2.52-.327 3.162a3 3 0 0 1-1.311 1.311C14.72 19 13.88 19 12.2 19H6.8c-1.68 0-2.52 0-3.162-.327a3 3 0 0 1-1.311-1.311C2 16.72 2 15.88 2 14.2z" />
-                  <path
-                    className={twMerge(
-                      'transition-transform duration-150 ease-in-out',
-                      isCamVideoEnabled ? 'scale-0' : 'scale-100'
-                    )}
-                    d="M23 23 L1 1"
-                  />
-                </g>
-              </svg>
+              {isCamVideoEnabled ? (
+                <VideoRecorderLg className="h-6 w-6 text-[#080808]" />
+              ) : (
+                <VideoRecorderOffLg className="h-6 w-6 text-[#080808]" />
+              )}
               {!!mediaStreamState.webcamMediaStream.value != mediaStreamState.webcamEnabled.value && (
                 <LoadingView className="absolute bottom-0 left-0 top-0 h-16 w-16" />
               )}
