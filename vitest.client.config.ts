967b396def370283239ec8ce9498da1fa59c6fce
HexaField <joshfield999@gmail.com>
2025-02-20 19:49:43 +1100

    change import for webxr emulator patch node script

--- vitest.client.config.ts
+++ vitest.client.config.ts
@@ -29,12 +29,8 @@ const reporters = !process.env.CI ? ['basic'] : configDefaults.reporters // Use 
 const watermark = [80, 95] as [number, number]
 const threshold = 80
 
-import appRootPath from 'app-root-path'
-import path from 'path'
-
 export default defineConfig({
   test: {
-    setupFiles: [path.resolve(appRootPath.path, 'packages/spatial/tests/util/patchNode.ts')],
     environment: 'jsdom',
     maxConcurrency: 1,
     passWithNoTests: true,
