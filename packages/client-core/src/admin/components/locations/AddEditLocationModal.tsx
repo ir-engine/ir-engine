72d3eb751dad2d5c079d5846b5b3c44e89fcccc0
Jiatong Yao <jiatong@ethereal.com>
2025-02-21 14:51:55 -0600

    fix error

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -209,7 +209,8 @@ export default function AddEditLocationModal(props: {
             meshEntity.push(entity)
             const transform = getComponent(entity, TransformComponent)
             const meshRootEntity = findMeshRootEntity(entity, rootEntity)
-            if (!exportParentEntity.includes(meshRootEntity) && meshRootEntity !== null) {
+            if (meshRootEntity === null) return
+            if (!exportParentEntity.includes(meshRootEntity as Entity)) {
               exportParentEntity.push(meshRootEntity as Entity)
             }
 
