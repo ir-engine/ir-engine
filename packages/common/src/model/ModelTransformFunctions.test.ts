eb879d7813aacc04836c5a20abf6897e12b51be3
Sam Mazer <sam.mazer@theinfinitereality.com>
2025-02-24 11:47:20 -0800

    updating test cases

--- packages/common/src/model/ModelTransformFunctions.test.ts
+++ packages/common/src/model/ModelTransformFunctions.test.ts
@@ -30,9 +30,24 @@ import { MATCH_ASSET_PROJECT_FILENAME_REGEX } from './ModelTransformFunctions'
 describe('Model Transform Functions', () => {
   describe('MATCH_ASSET_PROJECT_FILENAME_REGEX', () => {
     it('should match valid asset project paths', () => {
-      const validProjectPaths = ['https://ir.world:8642/projects/ir-engine/assets/public/collisioncube-LOD0.glb']
+      const validProjectPaths = [
+        'https://ir.world:8642/projects/ir-engine/default-project/assets/collisioncube-LOD0.glb',
+        'https://ir.world:8642/projects/ir-engine/default-project/public/collisioncube-LOD0.glb',
+        'https://ir.world:8642/projects/ir-engine/testproj/assets/collisioncube-LOD0.glb',
+        'https://ir.world:8642/projects/ir-engine/testproj/public/collisioncube-LOD0.glb'
+      ]
       validProjectPaths.forEach((filename) => {
-        assert.ok(MATCH_ASSET_PROJECT_FILENAME_REGEX.test(filename), `Expected '${filename}' to be invalid`)
+        assert.ok(MATCH_ASSET_PROJECT_FILENAME_REGEX.test(filename), `Expected '${filename}' to be valid`)
+      })
+    })
+
+    it('should not match invalid asset project paths', () => {
+      const invalidProjectPaths = [
+        'https://ir.world:8642/projects/ir-engine/default-project/blah/collisioncube-LOD0.glb',
+        'https://ir.world:8642/projects/ir-engine/default-project/custom-prefabs/collisioncube-LOD0.glb'
+      ]
+      invalidProjectPaths.forEach((filename) => {
+        assert.ok(!MATCH_ASSET_PROJECT_FILENAME_REGEX.test(filename), `Expected '${filename}' to be invalid`)
       })
     })
   })
