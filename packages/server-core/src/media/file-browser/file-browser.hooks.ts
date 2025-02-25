d18511d9b4cfc59590af091df1e121b220cb6c1d
Andres <asulbaran@theinfinitereality.com>
2025-02-25 10:43:23 -0600

    mend

--- packages/server-core/src/media/file-browser/file-browser.hooks.ts
+++ packages/server-core/src/media/file-browser/file-browser.hooks.ts
@@ -40,7 +40,7 @@ import { FileBrowserService } from './file-browser.class'
 
 const cleanFileName = () => {
   return async (context: HookContext<FileBrowserService>) => {
-    context.data.path = cleanFileNameString(context.data.path, true)
+    context.data.path = cleanFileNameString(context.data.path, true, true)
     return context
   }
 }
