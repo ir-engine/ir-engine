c6a94c790b77a6c9ff96dc711385eedf056ad08b
shayan.sadeghieh <shayan.sadeghieh@datatonic.com>
2025-02-27 19:22:08 -0500

    elif updates

--- scripts/run-builder.sh
+++ scripts/run-builder.sh
@@ -176,8 +176,10 @@ else
     echo "PRUNING GCP ARTIFACT REGISTRY REPOS"
 
     # Determine suffix based on APP_HOST
-    if [[ "$APP_HOST" =~ "mt-rc-int" ]]; then
-      SUFFIX="-mt-rc-int"
+    if [[ "$APP_HOST" =~ "preview" ]] || [[ "$APP_HOST" =~ "mt-stg" ]]; then
+      SUFFIX="mt"
+    elif [[ "$APP_HOST" =~ "mt-rc" ]]; then
+      SUFFIX="-mt-rc"
     elif [[ "$APP_HOST" =~ "mt-int" ]]; then
       SUFFIX="-mt-int"
     elif [[ "$APP_HOST" =~ "mt-qat" ]]; then
