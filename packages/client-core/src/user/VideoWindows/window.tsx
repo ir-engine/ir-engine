3257fc41dceebd40ba4332d1de983aff94d38942
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2025-02-28 20:25:45 +0530

    show window avatar bubble only when it is video is on

--- packages/client-core/src/user/VideoWindows/window.tsx
+++ packages/client-core/src/user/VideoWindows/window.tsx
@@ -370,8 +370,8 @@ export const SingleVideoWindow = ({ peerID, type }: Props): JSX.Element => {
 
   useEffect(() => {
     videoElement.draggable = false
-    document.getElementById(peerID + '-' + type + '-video-container')!.append(videoElement)
-    document.getElementById(peerID + '-' + type + '-audio-container')!.append(audioElement)
+    document.getElementById(peerID + '-' + type + '-video-container')?.append(videoElement)
+    document.getElementById(peerID + '-' + type + '-audio-container')?.append(audioElement)
   }, [])
 
   useEffect(() => {
@@ -396,7 +396,13 @@ export const SingleVideoWindow = ({ peerID, type }: Props): JSX.Element => {
       }}
     >
       {(!videoMediaStream || videoStreamPaused) && (
-        <img src={avatarThumbnail} alt={t('user:avatar.avatar')} crossOrigin="anonymous" draggable={false} />
+        <img
+          src={avatarThumbnail}
+          alt={t('user:avatar.avatar')}
+          crossOrigin="anonymous"
+          draggable={false}
+          className="hidden lg:inline"
+        />
       )}
       <span
         className="[&>video]:h-full [&>video]:w-full [&>video]:object-cover"
