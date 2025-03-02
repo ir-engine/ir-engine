3ed94a9c293967d77c0371fef24d11d9f567333c
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2025-03-02 07:08:11 +0500

    update moderation table labels to use new translation keys

--- packages/client-core/src/admin/components/moderation/ModerationBanTable.tsx
+++ packages/client-core/src/admin/components/moderation/ModerationBanTable.tsx
@@ -36,13 +36,13 @@ import { LocationLabel } from './common/LocationLabel'
 import { UserDisplayName } from './common/UserDisplayName'
 
 const moderationBanColumns: ITableHeadCell[] = [
-  { id: 'username', label: t('admin:components.moderation.username') },
-  { id: 'userId', label: t('admin:components.moderation.userId') },
-  { id: 'reason', label: t('admin:components.moderation.reason') },
-  { id: 'space', label: t('admin:components.moderation.space') },
-  { id: 'ipAddress', label: t('admin:components.moderation.ipAddress') },
-  { id: 'dateReported', label: t('admin:components.moderation.dateReported') },
-  { id: 'action', label: t('admin:components.moderation.action') }
+  { id: 'username', label: t('admin:components.moderation.columns.username') },
+  { id: 'userId', label: t('admin:components.moderation.columns.userId') },
+  { id: 'reason', label: t('admin:components.moderation.columns.reason') },
+  { id: 'space', label: t('admin:components.moderation.columns.space') },
+  { id: 'ipAddress', label: t('admin:components.moderation.columns.ipAddress') },
+  { id: 'dateReported', label: t('admin:components.moderation.columns.dateReported') },
+  { id: 'action', label: t('admin:components.moderation.columns.action') }
 ]
 
 export default function ModerationBanTable({ search }) {
