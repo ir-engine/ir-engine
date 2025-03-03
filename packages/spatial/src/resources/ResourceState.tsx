800a6031513e19f6c41a3aee5bb4290f210369b0
Gheric Speiginer <gheric.speiginer@gmail.com>
2025-03-02 21:24:47 -0800

    Update ResourceState.tsx

--- packages/spatial/src/resources/ResourceState.tsx
+++ packages/spatial/src/resources/ResourceState.tsx
@@ -654,7 +654,7 @@ export const ResourceState = defineState({
     resources: {} as Record<string, Resource>,
     totalVertexCount: 0,
     totalBufferCount: 0,
-    debug: true
+    debug: false
   }),
 
   debugLog: (...data: any[]) => {
