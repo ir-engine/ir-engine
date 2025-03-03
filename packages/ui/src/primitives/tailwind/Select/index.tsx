f2393b191e86a8e13b6deefe8b2c6baceb807a82
mike floyd <mbfloyd@theinfinitereality.com>
2025-03-03 14:01:55 -0800

    Corrected the issue with the select input throwing an warning 'No corresponding option found. Defaulting to null.'  when no options were provided to match against. Corrected issue with the ProjectState not having any project data to build an array of options off of

--- packages/ui/src/primitives/tailwind/Select/index.tsx
+++ packages/ui/src/primitives/tailwind/Select/index.tsx
@@ -174,16 +174,20 @@ const Select = ({
       return
     }
 
-    const index = filteredOptions.findIndex((option) => option.value === localValue)
+    if (filteredOptions.length > 0) {
+      const index = filteredOptions.findIndex((option) => option.value === localValue)
 
-    if (index === -1) {
-      if (searchMode === undefined) {
-        console.warn('No corresponding option found. Defaulting to null.')
-        setDisplayText('')
-        return
+      if (index === -1) {
+        if (searchMode === undefined) {
+          console.warn('No corresponding option found. Defaulting to null.')
+          setDisplayText('')
+          return
+        }
+      } else {
+        setDisplayText(filteredOptions[index].label)
       }
     } else {
-      setDisplayText(filteredOptions[index].label)
+      setDisplayText('')
     }
   }, [localValue, selectedOptionIndex, filteredOptions])
 
