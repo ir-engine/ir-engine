18cfb70cc5dc0834ed6e351b1591d075c2b5d010
Daniel Belmes <danielbelmes@ymail.com>
2025-03-03 15:31:00 -0800

    add type to key

--- packages/engine/src/scene/systems/EnvironmentSystem.tsx
+++ packages/engine/src/scene/systems/EnvironmentSystem.tsx
@@ -74,18 +74,18 @@ const EnvMapReactor = () => {
       {materialComponentEntities.map((materialComponentEntity, index) => {
         switch (envMapComponent) {
           case 'Skybox':
-            return <EnvMapSkyboxReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
+            return <EnvMapSkyboxReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
           case 'Cubemap':
-            return <EnvMapCubemapReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
+            return <EnvMapCubemapReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
           case 'Equirectangular':
-            return <EnvMapEquirectangularReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
+            return <EnvMapEquirectangularReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
           case 'Color':
-            return <EnvMapColorReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
+            return <EnvMapColorReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
           case 'Bake':
-            return <EnvMapBakeReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
+            return <EnvMapBakeReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} />
           case 'Probes':
             return (
-              <EnvmapProbesReactor key={materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} key={materialComponentEntity} />
+              <EnvmapProbesReactor key={envMapComponent + '-' + materialComponentEntity + '-' + index} entity={materialComponentEntity} rootEntity={entity} key={materialComponentEntity} />
             )
         }
       })}
