bfb7f31341a6e81b3e9b6cb2ed1c1f0c7fd73017
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2025-03-04 20:08:41 +0530

    make the report moderation menu mobile friendly
    - reduce padding on report user menu to match designs

--- packages/client-core/src/user/VideoWindows/index.tsx
+++ packages/client-core/src/user/VideoWindows/index.tsx
@@ -175,7 +175,7 @@ const ReportUserWindow = () => {
   if (!reportedPeerId || !reportedUserId || !reportedUser) return null
 
   return (
-    <div className="fixed right-[10%] top-[5%] grid grid-cols-[auto_minmax(0,1fr)] items-center gap-x-4 rounded-xl bg-surface-4 p-4 pb-3 lg:right-[5%]">
+    <div className="fixed right-[10%] top-[5%] grid grid-cols-[auto_minmax(0,1fr)] items-center gap-x-4 rounded-xl bg-surface-4 p-3 lg:right-[5%]">
       <div className="h-[100px] w-[100px]">
         <AvatarImage size="fill" className="rounded-none" src={avatarThumbnail} />
       </div>
