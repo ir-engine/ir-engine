78c22f63fb5510ff254163662d9aeaab548a5a91
Kyle Baran <kbaran@bitscoop.com>
2025-03-04 10:22:58 -0800

    fixup

--- packages/server-core/src/createApp.ts
+++ packages/server-core/src/createApp.ts
@@ -27,6 +27,10 @@ Infinite Reality Engine. All Rights Reserved.
 
 import { feathers } from '@feathersjs/feathers'
 import { bodyParser, errorHandler, koa, rest } from '@feathersjs/koa'
+//Padding this with comments
+//Padding this with comments
+//Padding this with comments
+//Padding this with comments
 //@ts-ignore
 import { AppsV1Api, BatchV1Api, CoreV1Api, CustomObjectsApi, KubeConfig } from '@kubernetes/client-node'
 // Do not delete, this is used even if some IDEs show it as unused
