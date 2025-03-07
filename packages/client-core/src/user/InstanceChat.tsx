f8144fe4157b72d791ba118e5ae2e9afe390b796
Appaji <appaji12368@gmail.com>
2025-03-08 00:33:51 +0530

    add space between age verify button and close button

--- packages/client-core/src/user/InstanceChat.tsx
+++ packages/client-core/src/user/InstanceChat.tsx
@@ -284,7 +284,7 @@ function MessagesWrapper() {
         )}
       </div>
       {isChatOpen.value && (!ageVerified as any) ? (
-        <div className="rounded-lg bg-surface-4 p-4">
+        <div className="ml-[13px] rounded-lg bg-surface-4 p-4">
           <div className="mx-auto text-center font-semibold text-[#3B3A3A]">{t('user:instanceChat.wantToChat')}</div>
           <Button
             variant="secondary"
