b624213cde141dbe4a7e2f5226701fb5ebd56536
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2025-03-10 18:35:13 +0530

    added report user on instance chat
    - refactor report user hook into report user state for state usage in both instance chat and video windows
    - introduce `ReportUserState`
    - remove `ReportUserProvider`

--- packages/client-core/src/user/VideoWindows/hook.tsx
+++ packages/client-core/src/user/VideoWindows/hook.tsx
@@ -21,7 +21,7 @@ Infinite Reality Engine. All Rights Reserved.
 import { DrawingUtils } from '@mediapipe/tasks-vision'
 import hark from 'hark'
 import { t } from 'i18next'
-import React, { createContext, RefObject, useContext, useEffect, useRef } from 'react'
+import React, { RefObject, useEffect, useRef } from 'react'
 
 import { AuthState } from '@ir-engine/client-core/src/user/services/AuthService'
 import { useGet } from '@ir-engine/common'
@@ -44,26 +44,6 @@ import { useUserAvatarThumbnail } from '../../hooks/useUserAvatarThumbnail'
 export interface Props {
   peerID: PeerID
   type: 'screen' | 'cam'
-}
-
-const ReportUserContext = createContext({ reportedPeerId: undefined! as State<PeerID | undefined> })
-
-export const ReportUserProvider = ({ children }: { children: React.ReactNode }) => {
-  const reportedPeerId = useHookstate(undefined as PeerID | undefined)
-  return <ReportUserContext.Provider value={{ reportedPeerId: reportedPeerId }}>{children}</ReportUserContext.Provider>
-}
-
-export const useReportUser = () => {
-  const { reportedPeerId } = useContext(ReportUserContext)
-  return {
-    reportedPeerId: reportedPeerId.value,
-    setReportedPeerId: (peerId: PeerID) => {
-      reportedPeerId.set(peerId)
-    },
-    resetPeerId: () => {
-      reportedPeerId.set(undefined)
-    }
-  }
 }
 
 export const useUserMediaWindowHook = ({ peerID, type }: Props) => {
