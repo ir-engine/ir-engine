1648e59bfb1cb18122132cc25372155624659e5e
Gheric Speiginer <gheric.speiginer@gmail.com>
2025-03-12 13:56:27 -0700

    iPhone texture discarding

--- packages/spatial/src/resources/ResourceState.tsx
+++ packages/spatial/src/resources/ResourceState.tsx
@@ -51,12 +51,12 @@ import {
 import { NO_PROXY, State, defineState, getMutableState, getState, none } from '@ir-engine/hyperflux'
 import { ObjectComponent } from '@ir-engine/spatial/src/renderer/components/ObjectComponent'
 
+import { ReferenceSpaceState } from '@ir-engine/spatial'
 import React, { useEffect } from 'react'
 import { Geometry } from '../common/constants/Geometry'
 import { isIPhone } from '../common/functions/isMobile'
 import iterateObject3D from '../common/functions/iterateObject3D'
 import { ColliderComponent } from '../physics/components/ColliderComponent'
-import { ReferenceSpaceState } from '../ReferenceSpaceState'
 import { PerformanceState } from '../renderer/PerformanceState'
 import { RendererComponent } from '../renderer/WebGLRendererSystem'
 
@@ -279,17 +279,24 @@ const resourceCallbacks = {
         const renderer = getComponent(viewer, RendererComponent)
         const gl = renderer.renderContext as WebGL2RenderingContext
         if (discardUponUpload && gl.fenceSync && isIPhone) {
-          const sync = gl.fenceSync(gl.SYNC_GPU_COMMANDS_COMPLETE, 0)
+          const sync = gl.fenceSync(
+            gl.SYNC_GPU_COMMANDS_COMPLETE,
+            gl.getParameter(gl.MAX_CLIENT_WAIT_TIMEOUT_WEBGL) - 1
+          )
           if (sync) {
             const checkSync = () => {
               const status = gl.clientWaitSync(sync, 0, 0)
-              if (status === gl.TIMEOUT_EXPIRED) {
-                requestAnimationFrame(checkSync)
-              } else {
+              if (
+                status === gl.TIMEOUT_EXPIRED ||
+                status === gl.ALREADY_SIGNALED ||
+                status === gl.CONDITION_SATISFIED
+              ) {
                 gl.deleteSync(sync)
                 resource.metadata.merge({ onGPU: true, discarded: true })
                 asset.source.data = null
                 asset.mipmaps = []
+              } else {
+                requestAnimationFrame(checkSync)
               }
             }
             requestAnimationFrame(checkSync)
