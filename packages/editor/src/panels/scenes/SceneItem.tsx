5ef3a3b769ab22898a383b653420a7a68293b9d6
sybiote <ghoshr698@gmail.com>
2025-03-13 04:31:57 +0530

    Update Tooltip position in SceneItem component

--- packages/editor/src/panels/scenes/SceneItem.tsx
+++ packages/editor/src/panels/scenes/SceneItem.tsx
@@ -94,7 +94,7 @@ export default function SceneItem({
       <div className="inline-flex items-start justify-between self-stretch">
         <div className="inline-flex w-full flex-col items-start justify-start">
           <div className="space-between flex w-full flex-row">
-            <Tooltip content={sceneName}>
+            <Tooltip content={sceneName} position="top">
               <Text
                 component="h3"
                 fontWeight="semibold"
