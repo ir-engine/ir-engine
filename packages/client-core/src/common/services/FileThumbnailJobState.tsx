0fe56a180bf47fb10aecdcc703638fe4b6458c42
mike floyd <mbfloyd@theinfinitereality.com>
2025-03-13 14:28:25 -0700

    added a debounced timeout to trigger a refetchResources 0.5 sec after the last thumbnailJobState.jobs.length reaction

--- packages/client-core/src/common/services/FileThumbnailJobState.tsx
+++ packages/client-core/src/common/services/FileThumbnailJobState.tsx
@@ -160,7 +160,7 @@ const uploadThumbnail = async (src: string, projectName: string, blob: Blob | nu
           }
           updateThumbnailKey(staticResourceId)
         } else {
-          console.error('static Resource not foudn for key - ', fileKeyKey)
+          console.error('static Resource not found for key - ', fileKeyKey)
         }
       })
       .catch((e) => console.error(e))
@@ -185,7 +185,8 @@ export const FileThumbnailJobState = defineState({
   name: 'FileThumbnailJobState',
   initial: {
     seenResources: [] as string[],
-    jobs: [] as ThumbnailJob[]
+    jobs: [] as ThumbnailJob[],
+    refreshIndex: 0
   },
   reactor: () => <ThumbnailJobReactor />,
   removeCurrentJob: () => {
