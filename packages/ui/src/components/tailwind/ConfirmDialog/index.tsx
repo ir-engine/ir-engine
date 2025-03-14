71492b71638e483ba5a5836888bed9c4b3142ed7
Appaji <appaji12368@gmail.com>
2025-03-14 21:42:10 +0530

    Modify breakpoints of ConfirmDialog

--- packages/ui/src/components/tailwind/ConfirmDialog/index.tsx
+++ packages/ui/src/components/tailwind/ConfirmDialog/index.tsx
@@ -62,10 +62,10 @@ export const ConfirmDialog = ({ title, text, onSubmit, onClose, modalProps }: Co
         PopoverState.hidePopupover()
         onClose?.()
       }}
-      className="h-[90dvh] w-[50vw] min-w-[720px] max-w-2xl bg-surface-1 mdh:h-auto mdh:min-w-fit"
+      className="h-[90dvh] w-[50vw] min-w-[720px] max-w-2xl bg-surface-1 smh:h-auto smh:min-w-fit"
       submitLoading={modalProcessing.value}
       rawChildren={
-        <div className="flex h-[calc(90dvh-4rem-4.5rem)] flex-col items-center justify-center gap-2 mdh:h-auto">
+        <div className="flex h-[calc(90dvh-4rem-4.5rem)] flex-col items-center justify-center gap-2 smh:h-auto smh:py-2">
           <Text className="text-text-secondary">{text}</Text>
           {errorText.value && <Text className="text-red-700	">{errorText.value}</Text>}
         </div>
