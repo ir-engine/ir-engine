eeb14347132e5c8efd3c91ec715867153b3f69eb
mike floyd <mbfloyd@theinfinitereality.com>
2025-03-18 15:37:36 -0700

    updated added items to name their entity instance to be named after the filename without the file extension. If their is a sibling entity with the same or incremented name, add a incremented number to the name

--- packages/editor/src/functions/utils.ts
+++ packages/editor/src/functions/utils.ts
@@ -23,8 +23,11 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 import { isApple } from '@ir-engine/common/src/utils/getDeviceStats'
-import { Entity, getOptionalComponent } from '@ir-engine/ecs'
+import { Entity, EntityTreeComponent, getComponent, getOptionalComponent } from '@ir-engine/ecs'
 import { GLTFComponent } from '@ir-engine/engine/src/gltf/GLTFComponent.tsx'
+import { getState } from '@ir-engine/hyperflux'
+import { NameComponent } from '@ir-engine/spatial/src/common/NameComponent'
+import { EditorState } from '../services/EditorServices'
 
 export function isEntityGlb(entity: Entity): boolean {
   const gltfComponent = getOptionalComponent(entity, GLTFComponent)
@@ -43,3 +46,20 @@ export function getStepSize(event, smallStep, mediumStep, largeStep) {
 }
 
 export const cmdOrCtrlString = isApple() ? 'meta' : 'ctrl'
+
+export const getIncreamentedName = (requestedName: string, parentEntity = getState(EditorState).rootEntity) => {
+  const siblings = getComponent(parentEntity, EntityTreeComponent).children
+  let counter = 0
+  siblings.forEach((child) => {
+    const name = getComponent(child, NameComponent)
+    const match = name.match(/^(.*?)\s+(\d+)?$/)
+    const adjustedName = match ? match[1] : name
+    if (adjustedName === requestedName || name === requestedName) {
+      counter++
+    }
+  })
+  if (counter > 0) {
+    requestedName = `${requestedName} ${counter}`
+  }
+  return requestedName
+}
