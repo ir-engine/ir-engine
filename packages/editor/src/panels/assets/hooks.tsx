fa451ebd64ef07b3da94d52cd4f10555707e3d1b
mike floyd <mbfloyd@theinfinitereality.com>
2025-03-18 16:28:49 -0700

    fixed issue where the newly fetched resources were being merged into the existing list of resources instead of setting the list

--- packages/editor/src/panels/assets/hooks.tsx
+++ packages/editor/src/panels/assets/hooks.tsx
@@ -122,12 +122,19 @@ export const AssetsQueryProvider = ({ children }: { children: ReactNode }) => {
         .then((fetchedResources) => {
           if (abortController.signal.aborted) return
 
-          if (staticResourcesPagination.skip.value > 0 && previousSearchQuery === search.query.value && !forceRefresh) {
+          if (
+            !forceRefresh &&
+            staticResourcesPagination.skip.value > 0 &&
+            previousSearchQuery === search.query.value &&
+            !forceRefresh
+          ) {
             resources.merge(fetchedResources.data)
           } else {
             resources.set(fetchedResources.data)
           }
+
           staticResourcesPagination.merge({ total: resources.length })
+
           resourcesLoading.set(false)
         })
     }
