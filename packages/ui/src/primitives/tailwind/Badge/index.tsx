f5b10d01916a5bc806a195b121a566cf482635e5
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2025-03-24 10:17:56 +0530

    inline twclassname

--- packages/ui/src/primitives/tailwind/Badge/index.tsx
+++ packages/ui/src/primitives/tailwind/Badge/index.tsx
@@ -43,14 +43,14 @@ const variantMap = {
 const Badge = ({ label, className, variant }: BadgeProps) => {
   variant = variant || 'neutral'
 
-  const twClassName = twMerge(
-    'flex h-fit items-center justify-around gap-x-1.5	rounded-full px-2.5 py-0.5',
-    variantMap[variant],
-    className
-  )
-
   return (
-    <div className={twClassName}>
+    <div
+      className={twMerge(
+        'flex h-fit items-center justify-around gap-x-1.5	rounded-full px-2.5 py-0.5',
+        variantMap[variant],
+        className
+      )}
+    >
       <span className="font-semibold text-white">{label}</span>
     </div>
   )
