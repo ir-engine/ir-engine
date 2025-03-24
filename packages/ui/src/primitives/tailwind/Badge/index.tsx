4e0c1a5c00ede02ff3b2459621c1fe47adf156f0
Appaji <52322531+CITIZENDOT@users.noreply.github.com>
2025-03-24 10:39:09 +0530

    Fix badge colors (#1481)
    * Fix badge colors
    
    * inline twclassname
    
    * add storybook args
    
    ---------
    
    Co-authored-by: aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>

--- packages/ui/src/primitives/tailwind/Badge/index.tsx
+++ packages/ui/src/primitives/tailwind/Badge/index.tsx
@@ -29,63 +29,29 @@ import { twMerge } from 'tailwind-merge'
 export interface BadgeProps {
   label: string
   className?: string
-  textClassName?: string
-  icon?: React.ReactNode
   variant?: 'success' | 'successLight' | 'danger' | 'neutral' | 'warning'
 }
 
 const variantMap = {
-  success: {
-    containerClass: '',
-    iconColor: '#15803d',
-    textClass: 'text-green-900 dark:text-white'
-  },
-  successLight: {
-    containerClass: '',
-    iconColor: '#9ACD32',
-    textClass: 'text-[#9ACD32]-900 dark:text-white'
-  },
-  danger: {
-    containerClass: '',
-    iconColor: '#f43f5e',
-    textClass: 'text-white'
-  },
-  neutral: {
-    containerClass: 'bg-stone-200 dark:bg-gray-800',
-    iconColor: 'black',
-    textClass: 'text-black dark:text-white'
-  },
-  warning: {
-    containerClass: '',
-    iconColor: '#d6a407',
-    textClass: 'text-yellow-900 dark:text-white'
-  }
-}
+  success: 'bg-ui-hover-success',
+  successLight: 'bg-ui-hover-success opacity-80',
+  danger: 'bg-ui-hover-error',
+  neutral: 'bg-gray-700',
+  warning: 'bg-ui-hover-warning'
+} as const
 
-const Badge = ({ label, className, textClassName, icon, variant }: BadgeProps) => {
-  let twClassName = twMerge('flex h-fit items-center justify-around gap-x-1.5	rounded-full px-2.5 py-0.5', className)
-
-  let twTextClassName = textClassName
-
-  let variantIconColor: null | string = null
-
-  if (variant && variantMap[variant]) {
-    const { containerClass, iconColor, textClass } = variantMap[variant]
-    twClassName = twMerge(containerClass, twClassName)
-    twTextClassName = twMerge(textClass, twTextClassName)
-    variantIconColor = iconColor
-  }
+const Badge = ({ label, className, variant }: BadgeProps) => {
+  variant = variant || 'neutral'
 
   return (
-    <div className={twClassName}>
-      {variantIconColor && React.isValidElement(icon)
-        ? React.cloneElement(icon, {
-            ...icon.props,
-            // @ts-ignore
-            color: variantIconColor
-          })
-        : icon}
-      <span className={twTextClassName}>{label}</span>
+    <div
+      className={twMerge(
+        'flex h-fit items-center justify-around gap-x-1.5	rounded-full px-2.5 py-0.5',
+        variantMap[variant],
+        className
+      )}
+    >
+      <span className="font-semibold text-white">{label}</span>
     </div>
   )
 }
