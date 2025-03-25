43678f17d8c7b6d8e83a1202df7dd49936fd7e51
MbfloydIR <144718558+MbfloydIR@users.noreply.github.com>
2025-03-25 12:43:17 -0700

    Fix Thumbnails: added timeout to trigger resources refetch (#1378)
    added a debounced timeout to trigger a refetchResources 0.5 sec after the last thumbnailJobState.jobs.length reaction
    
    Co-authored-by: MbfloydIR <144718558+MbfloydIR@users.noreply.github.com>

--- packages/client-core/src/common/services/FileThumbnailJobState.tsx
+++ packages/client-core/src/common/services/FileThumbnailJobState.tsx
@@ -162,7 +162,7 @@ const uploadThumbnail = async (src: string, projectName: string, blob: Blob | nu
           }
           updateThumbnailKey(staticResourceId)
         } else {
-          console.error('static Resource not foudn for key - ', fileKeyKey)
+          console.error('static Resource not found for key - ', fileKeyKey)
         }
       })
       .catch((e) => console.error(e))
@@ -187,7 +187,8 @@ export const FileThumbnailJobState = defineState({
   name: 'FileThumbnailJobState',
   initial: {
     seenResources: [] as string[],
-    jobs: [] as ThumbnailJob[]
+    jobs: [] as ThumbnailJob[],
+    refreshIndex: 0
   },
   reactor: () => <ThumbnailJobReactor />,
   removeCurrentJob: () => {
