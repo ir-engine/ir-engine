6bd7f199f51dc9e898d7ea622e92613caa698c56
mike floyd <mbfloyd@theinfinitereality.com>
2025-03-25 14:16:21 -0700

    re-added default value

--- packages/spatial/src/transform/functions/ObjectFitFunctions.ts
+++ packages/spatial/src/transform/functions/ObjectFitFunctions.ts
@@ -47,7 +47,7 @@ const SCREEN_SIZE = new Vector2()
 
 export type ContentFitType = 'cover' | 'contain' | 'stretch' | 'vertical' | 'horizontal'
 export const ContentFitTypeSchema = (init?: ContentFitType) =>
-  S.LiteralUnion(['stretch', 'cover', 'contain', 'vertical', 'horizontal'], init)
+  S.LiteralUnion(['stretch', 'cover', 'contain', 'vertical', 'horizontal'], init ?? 'contain')
 
 // yes, multiple by the same direction twice, as the local coordinate changes with each rotation
 const _handRotation = new Quaternion()
