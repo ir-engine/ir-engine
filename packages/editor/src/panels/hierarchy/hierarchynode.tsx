b0667875ef07f33ad7e2e903c1102de0bc9b10bf
hzraslan <hzraslan@gmail.com>
2025-03-26 19:09:22 -0400

    rm-before

--- packages/editor/src/panels/hierarchy/hierarchynode.tsx
+++ packages/editor/src/panels/hierarchy/hierarchynode.tsx
@@ -413,14 +413,6 @@ export default React.memo(function HierarchyTreeNode(props: ListChildComponentPr
           rootEntity === entity ? 'p-2' : 'py-1 pl-10 pr-2'
         )}
       >
-        <div
-          className={twMerge(
-            isOverBefore && canDropBefore && `${theme.theme.value === 'dark' ? 'bg-black' : 'bg-white'}`
-          )}
-          style={{ marginLeft: `${node.depth * 0.75}rem` }}
-          ref={beforeDropTarget}
-        />
-
         <div className="flex w-full items-center justify-between gap-x-2 bg-inherit pr-2" ref={onDropTarget}>
           {node.isLeaf ? (
             <div className="w-5 shrink-0" />
