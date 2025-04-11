d3c1e55bd5d87cfbeb5d75e433b5fd7cfbe265a3
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2025-04-11 11:09:51 +0500

    Add reference number to moderation schema and update related components

--- packages/common/src/schemas/moderation/moderation.schema.ts
+++ packages/common/src/schemas/moderation/moderation.schema.ts
@@ -72,6 +72,7 @@ export const moderationSchema = Type.Object(
     updatedBy: TypedString<UserID>({
       format: 'uuid'
     }),
+    referenceNumber: Type.Integer(),
     createdAt: Type.String({ format: 'date-time' }),
     updatedAt: Type.String({ format: 'date-time' })
   },
@@ -102,6 +103,7 @@ export interface ModerationPatch extends Static<typeof moderationPatchSchema> {}
 export const moderationQueryProperties = Type.Pick(moderationSchema, [
   'id',
   'type',
+  'referenceNumber',
   'reportedLocationId',
   'reportedUserId',
   'abuseReason',
@@ -111,6 +113,9 @@ export const moderationQuerySchema = Type.Intersect(
   [
     querySyntax(moderationQueryProperties, {
       id: {
+        $like: Type.String()
+      },
+      referenceNumber: {
         $like: Type.String()
       }
     }),
