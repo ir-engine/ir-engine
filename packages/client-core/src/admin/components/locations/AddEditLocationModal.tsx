0d1c1d322b046ff4abaa00a1e11dc410b987d3c6
hzraslan <hzraslan@gmail.com>
2025-04-16 17:46:10 -0500

    dev-pull

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -375,7 +375,6 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
           true,
           saveScenePath + '/' + scenename
         )
-        throw new Error('No scene found')
         await handlePublish(true)
         //re-open the original scene
         const studioUrl = `${window.location.origin}/studio?project=${projectName}&scenePath=${scenePath}`
