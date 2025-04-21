796781356c30cfe75b459d93de103861758f1c67
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2025-04-21 21:39:04 +0500

    prevent email resolution for non-find methods in moderationExternalResolver

--- packages/server-core/src/moderation/moderation/moderation.resolvers.ts
+++ packages/server-core/src/moderation/moderation/moderation.resolvers.ts
@@ -56,12 +56,15 @@ export const moderationResolver = resolve<ModerationType, HookContext>({
 
 export const moderationExternalResolver = resolve<ModerationType, HookContext>({
   reportedUserEmail: virtual(async (moderation: ModerationType, context: HookContext) => {
+    if (context.method !== 'find') return undefined
     return resolveUserEmail(moderation.reportedUserId, context)
   }),
   createdByEmail: virtual(async (moderation: ModerationType, context: HookContext) => {
+    if (context.method !== 'find') return undefined
     return resolveUserEmail(moderation.createdBy, context)
   })
 })
+
 export const moderationDataResolver = resolve<ModerationType, HookContext>({
   id: async () => {
     return uuidv4() as ModerationID
