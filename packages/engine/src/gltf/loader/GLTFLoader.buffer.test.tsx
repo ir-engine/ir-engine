736d9198a351cadabe72d229fe08ec7df8f22d31
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2025-04-22 20:36:03 +0200

    tst: Unit Tests for `glTF.buffers` (phase 0)

--- packages/engine/src/gltf/loader/GLTFLoader.buffer.test.tsx
+++ packages/engine/src/gltf/loader/GLTFLoader.buffer.test.tsx
@@ -28,8 +28,37 @@ Infinite Reality Engine. All Rights Reserved.
  * Unit Test suite for loading the `glTF.buffers` root property and all its children.
  * Based on glTF 2.0 specification requirements.
  * */
-import { describe, it } from 'vitest'
+import { createEngine, destroyEngine } from '@ir-engine/ecs'
+import { act, render } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { startEngineReactor } from '../../../tests/startEngineReactor'
+import { overrideFileLoaderLoad } from '../../../tests/util/loadGLTFAssetNode'
+import { mockGLTF, mockGLTFOptions } from '../../../tests/util/mockGLTF'
+import { DependencyCache, GLTFLoaderFunctions } from '../GLTFLoaderFunctions'
 
+beforeEach(() => {
+  // Clear the dependency cache before each test
+  DependencyCache.clear()
+})
+
+overrideFileLoaderLoad()
+
+beforeEach(async () => {
+  createEngine()
+  startEngineReactor()
+
+  await act(() => render(null))
+})
+
+afterEach(() => {
+  destroyEngine()
+})
+
+/**
+ * @todo
+ * Cannot possibly tested in our current GLTFLoader implementation
+ * It requires a GLTFLoader gltf root properties validation function that does not exist.
+ * */
 describe('glTF.buffers Property', () => {
   it.todo('MAY be undefined', () => {})
   it.todo('MUST be an array of `buffer` objects when defined', () => {})
@@ -37,32 +66,96 @@ describe('glTF.buffers Property', () => {
 }) //:: glTF.buffers
 
 describe('glTF: Buffer Type', () => {
+  function mockGLTFMinimalBuffer() {
+    const result = mockGLTF()
+    result.buffers = [
+      {
+        byteLength: 1
+      }
+    ]
+    return result
+  }
+
   describe('uri', () => {
-    it.todo('MAY be undefined', () => {})
-    it.todo('MUST be a `string` type when defined', () => {})
-    it.todo('MUST be a path relative to the glTF file when not absolute', () => {})
-    it.todo('MUST be a follow the iri-reference format when defined', () => {})
+    it('MAY be undefined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      delete options.document.buffers![0].uri
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).resolves.not.toThrow()
+    })
+
+    it('MUST be a `string` type when defined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      options.document.buffers![0].uri = 42 as any // Not a string
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
+
+    /** @todo https://www.npmjs.com/package/validate-iri */
+    it.todo('MUST follow the iri-reference format when defined', () => {})
     it.todo('MUST be base64 encoded when it starts with data:', () => {})
+    it.todo('MUST be a path relative to the glTF file when not absolute', () => {})
   }) //:: uri
 
   describe('byteLength', () => {
-    it.todo('MUST be defined', () => {})
-    it.todo('MUST be an `integer` type', () => {})
-    it.todo('MUST have a value in range [1..]', () => {})
+    /** @todo Should throw. Our implementation does not respect the specification for glTF.buffer.byteLength */
+    it.fails('MUST be defined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      // @ts-expect-error Delete, even if mandatory, to provoke the error
+      delete options.document.buffers![0].byteLength
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
+
+    /** @todo Should throw. Our implementation does not respect the specification for glTF.buffer.byteLength */
+    it.fails('MUST be an `integer` type', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      options.document.buffers![0].byteLength = 1.42 // Not an integer
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
+
+    /** @todo Should throw. Our implementation does not respect the specification for glTF.buffer.byteLength */
+    it.fails('MUST have a value in range [1..]', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      options.document.buffers![0].byteLength = 0 // Not in range [1..]
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
+
     it.todo('MUST match the total size of the buffer data in bytes', () => {})
   }) //:: byteLength
 
   describe('name', () => {
-    it.todo('MAY be undefined', () => {})
-    it.todo('MUST be a `string` type when defined', () => {})
+    it('MAY be undefined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      delete options.document.buffers![0].name
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).resolves.not.toThrow()
+    })
+
+    /** @todo Should throw. Our implementation does not respect the specification for glTF.buffer.name */
+    it.fails('MUST be a `string` type when defined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      options.document.buffers![0].name = 42 as any // Not a string
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
   }) //:: name
 
   describe('extensions', () => {
-    it.todo('MAY be undefined', () => {})
-    it.todo('MUST be a JSON object when defined', () => {})
+    it('MAY be undefined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      delete options.document.buffers![0].extensions
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).resolves.not.toThrow()
+    })
+
+    /** @todo Should throw. Our implementation does not respect the specification for glTF.buffer.extensions */
+    it.fails('MUST be a JSON object when defined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      options.document.buffers![0].extensions = 42 as any // Not a JSON object
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).rejects.toThrowError()
+    })
   }) //:: extensions
 
   describe('extras', () => {
-    it.todo('MAY be undefined', () => {})
+    it('MAY be undefined', () => {
+      const options = mockGLTFOptions(mockGLTFMinimalBuffer())
+      delete options.document.buffers![0].extras
+      expect(GLTFLoaderFunctions.loadBuffer(options, 0)).resolves.not.toThrow()
+    })
   }) //:: extras
 }) //:: glTF: Buffer
