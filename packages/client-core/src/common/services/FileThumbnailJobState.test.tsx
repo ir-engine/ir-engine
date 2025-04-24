d9c007ca984d2c83330e179a278fa62eba3080db
Jiatong Yao <jiatong@ethereal.com>
2025-04-24 12:46:27 -0500

    use sinon

--- packages/client-core/src/common/services/FileThumbnailJobState.test.tsx
+++ packages/client-core/src/common/services/FileThumbnailJobState.test.tsx
@@ -1,6 +1,5 @@
 /*
 CPAL-1.0 License
-
 The contents of this file are subject to the Common Public Attribution License
 Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -9,107 +8,104 @@ The License is based on the Mozilla Public License Version 1.1, but Sections 14
 and 15 have been added to cover use of software over a computer network and 
 provide for limited attribution for the Original Developer. In addition, 
 Exhibit A has been modified to be consistent with Exhibit B.
-
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
 specific language governing rights and limitations under the License.
-
 The Original Code is Infinite Reality Engine.
-
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
-
 All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { createEngine, destroyEngine } from '@ir-engine/ecs/src/Engine'
-import { getMutableState } from '@ir-engine/hyperflux'
-import assert from 'assert'
+import * as Common from '@ir-engine/common'
+import { FileBrowserContentType } from '@ir-engine/common/src/schema.type.module'
+import { createEngine, destroyEngine } from '@ir-engine/ecs'
+import { act, render, waitFor } from '@testing-library/react'
+import React from 'react'
+import sinon from 'sinon'
 import { afterEach, beforeEach, describe, it } from 'vitest'
 import { FileThumbnailJobState } from './FileThumbnailJobState'
 
-describe('FileThumbnailJobState', () => {
+describe('FileThumbnailJobState (React-safe + Sinon only)', () => {
   const testKey = 'projects/ir-engine/default-project/public/test.glb'
-  beforeEach(async () => {
+  const testUrl = 'https://domain/' + testKey
+
+  const filesQueryData: FileBrowserContentType[] = [
+    {
+      key: testKey,
+      name: 'test',
+      size: 1000,
+      type: 'glb',
+      url: testUrl
+    }
+  ]
+  let useFindStub: sinon.SinonStub
+
+  beforeEach(() => {
     createEngine()
+
+    useFindStub = sinon.stub(Common, 'useFind').returns({
+      data: [
+        {
+          id: '1',
+          key: testKey,
+          project: 'default-project',
+          url: testUrl,
+          thumbnailKey: null,
+          width: null,
+          height: null,
+          depth: null,
+          type: 'glb'
+        }
+      ],
+      total: 1,
+      setSort: sinon.fake(),
+      setLimit: sinon.fake(),
+      setPage: sinon.fake(),
+      search: sinon.fake(),
+      page: 1,
+      skip: 0,
+      limit: 10,
+      sort: {},
+      status: 'success',
+      error: '',
+      refetch: sinon.fake()
+    })
   })
 
-  afterEach(async () => {
+  afterEach(() => {
+    sinon.restore()
     destroyEngine()
   })
 
-  it('should add thumbnail jobs for files without thumbnails', async () => {
-    const jobState = getMutableState(FileThumbnailJobState)
-    const seenResources = jobState.seenResources.thumbnail
-    const fakeResources = [
-      {
-        id: '1',
-        key: testKey,
-        url: 'https://domain/' + testKey,
-        project: 'default-project',
-        thumbnailKey: null,
-        type: 'glb'
-      }
-    ]
+  const TestThumbnailComponent = () => {
+    FileThumbnailJobState.useGenerateThumbnails(filesQueryData)
 
-    for (const resource of fakeResources) {
-      if (seenResources.value.includes(resource.key)) continue
-      seenResources.merge([resource.key])
+    return null
+  }
+  const TestDimensionComponent = () => {
+    FileThumbnailJobState.useGenerateDimensions(filesQueryData)
 
-      if (resource.type === 'thumbnail') continue
+    return null
+  }
 
-      const ext = resource.key.split('.').pop() ?? ''
-      if (resource.thumbnailKey != null || !['glb', 'gltf', 'fbx'].includes(ext)) {
-        continue
-      }
+  it('should add thumbnail jobs using useGenerateThumbnails', async () => {
+    await act(async () => {
+      render(<TestThumbnailComponent />)
+    })
 
-      if (jobState.jobs.value.filter((fj) => fj.key === resource.url && fj.jobType === 'thumbnail').length < 1) {
-        jobState.jobs.merge([
-          {
-            key: resource.url,
-            project: resource.project!,
-            jobType: 'thumbnail'
-          }
-        ])
-      }
-    }
-    assert.ok(jobState.jobs.value.length > 0, 'Thumbnail job was added')
+    await waitFor(() => {
+      sinon.assert.called(useFindStub)
+    })
   })
-  it('should add dimension jobs for glb/gltf files without dimension data', async () => {
-    const jobState = getMutableState(FileThumbnailJobState)
-    const seenDimensions = jobState.seenResources.dimension
+  it('should add dimension jobs using useGenerateDimenshion', async () => {
+    await act(async () => {
+      render(<TestDimensionComponent />)
+    })
 
-    const fakeResources = [
-      {
-        id: '1',
-        key: testKey,
-        url: 'https://domain/' + testKey,
-        project: 'default-project',
-        type: 'glb',
-        width: null,
-        height: null,
-        depth: null
-      }
-    ]
-
-    for (const resource of fakeResources) {
-      if (!seenDimensions.value.includes(resource.key)) {
-        seenDimensions.merge([resource.key])
-
-        if (!jobState.jobs.value.some((job) => job.key === resource.url && job.jobType === 'dimension')) {
-          jobState.jobs.merge([
-            {
-              key: resource.url,
-              project: resource.project!,
-              jobType: 'dimension'
-            }
-          ])
-        }
-      }
-    }
-
-    const jobKeys = jobState.jobs.value.map((j) => j.jobType)
-    assert.ok(jobKeys.includes('dimension'), 'Dimension job was added')
+    await waitFor(() => {
+      sinon.assert.called(useFindStub)
+    })
   })
 })
