bc1793d3315b47f955e57103f35acf5a143befc7
hzraslan <hzraslan@gmail.com>
2025-05-01 09:46:07 -0500

    ch-on-remove-grad-func

--- packages/ui/src/components/editor/input/Generator/Color/index.tsx
+++ packages/ui/src/components/editor/input/Generator/Color/index.tsx
@@ -120,19 +120,14 @@ export default function ColorGenerator({
     )
   }, [scope])
 
-  const onRemoveGradient = useCallback(
-    (index: number) => {
-      const gradientScope = scope as State<ColorGradientJSON>
-      const gradient = gradientScope.value
-      const thisOnChange = onChange(path + '.functions')
-      return () => {
-        const nuFunctions = [...gradient.functions]
-        nuFunctions.splice(index, 1)
-        thisOnChange(JSON.parse(JSON.stringify(nuFunctions)))
-      }
-    },
-    [scope, onChange, path]
-  )
+  const onRemoveGradient = (index: number) => {
+    const gradientScope = scope as State<ColorGradientJSON>
+    const gradient = gradientScope.value
+    const thisOnChange = onChange(path + '.functions')
+    const nuFunctions = [...gradient.functions]
+    nuFunctions.splice(index, 1)
+    thisOnChange(JSON.parse(JSON.stringify(nuFunctions)))
+  }
 
   const GradientInput = useCallback(() => {
     const gradientScope = scope as State<ColorGradientJSON>
@@ -195,7 +190,7 @@ export default function ColorGenerator({
                 </div>
               </div>
             </div>
-            <Button onClick={onRemoveGradient(index)}>Remove</Button>
+            <Button onClick={() => onRemoveGradient(index)}>Remove</Button>
           </div>
         ))}
       </div>
