64fe1f7f78d30452791a7c25567863e97df623c2
Michael Estes <michael@etherealengine.com>
2025-05-06 18:39:01 -0700

    Error

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -290,7 +290,7 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
           false
         )
         EditorControlFunctions.modifyProperty([combinedMeshEntity], GLTFComponent, { src: srcURL })
-        EditorControlFunctions.modifyProperty([combinedMeshEntity], VisibleComponent, { visible: true })
+        EditorControlFunctions.modifyProperty([combinedMeshEntity], VisibleComponent, { value: true })
 
         for (const entity of exportParentEntity) {
           const url = getComponent(entity, GLTFComponent).src
