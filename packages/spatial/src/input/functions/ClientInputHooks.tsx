5b18aad0b985cb8fc0bb59944ca1fe4433dcaeb9
Adrian <agcacho22@gmail.com>
2025-05-07 12:28:51 -0700

    stop propogation

--- packages/spatial/src/input/functions/ClientInputHooks.tsx
+++ packages/spatial/src/input/functions/ClientInputHooks.tsx
@@ -346,6 +346,7 @@ export const CanvasInputReactor = () => {
       axes[MouseScroll.HorizontalScroll] = normalizedValues.spinX
       axes[MouseScroll.VerticalScroll] = normalizedValues.spinY
       event.preventDefault()
+      event.stopPropogation()
     }
 
     canvas.addEventListener('dragstart', ClientInputFunctions.preventDefault, false)
