503fb1320cac2c42e655fa197b6f01decaabb989
quaiquai-ir <matthew.mcquaigue@infinitereality.com>
2025-05-13 16:33:13 -0400

    split between indirect and direct sampler testing. direct should fail as we dont follow specification for root level samplers

--- packages/engine/src/gltf/loader/GLTFLoader.sampler.test.tsx
+++ packages/engine/src/gltf/loader/GLTFLoader.sampler.test.tsx
@@ -25,12 +25,16 @@ Infinite Reality Engine. All Rights Reserved.
 
 /**
  * @fileoverview
- * Unit Test suite for validating `glTF.samplers` indirectly through texture loading.
+ * Unit Test suite for validating `glTF.samplers` directly and indirectly through texture loading.
  * Based on glTF 2.0 specification requirements.
  */
-import { describe, expect, it } from 'vitest'
+import { createEngine, destroyEngine } from '@ir-engine/ecs'
+import { act, render } from '@testing-library/react'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { startEngineReactor } from '../../../tests/startEngineReactor'
+import { overrideFileLoaderLoad } from '../../../tests/util/loadGLTFAssetNode'
 import { mockGLTF, mockGLTFOptions } from '../../../tests/util/mockGLTF'
-import { GLTFLoaderFunctions } from '../GLTFLoaderFunctions'
+import { DependencyCache, GLTFLoaderFunctions } from '../GLTFLoaderFunctions'
 
 // describe('glTF.samplers Property', () => {
 //   it.todo('MAY be undefined', () => {})
@@ -83,9 +87,28 @@ import { GLTFLoaderFunctions } from '../GLTFLoaderFunctions'
 //   }) //:: extras
 // }) //:: glTF: Sampler
 
+beforeEach(() => {
+  // Clear the dependency cache before each test
+  DependencyCache.clear()
+})
+
+overrideFileLoaderLoad()
+
+beforeEach(async () => {
+  createEngine()
+  startEngineReactor()
+
+  await act(() => render(null))
+})
+
+afterEach(() => {
+  destroyEngine()
+})
+
 describe('glTF.samplers Property (indirect validation)', () => {
   it('MAY be undefined', async () => {
     const options = mockGLTFOptions(mockGLTF())
+    console.log('options', options)
     delete options.document.samplers
     const texture = await GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)
     expect(texture).toBeDefined()
@@ -106,7 +129,7 @@ describe('glTF.samplers Property (indirect validation)', () => {
   })
 })
 
-describe('glTF: Sampler Type (indirect validation)', () => {
+describe('glTF: Sampler Type (direct and indirect validation)', () => {
   function mockGLTFWithSampler(sampler: any) {
     const result = mockGLTF()
     result.samplers = [sampler]
@@ -144,5 +167,38 @@ describe('glTF: Sampler Type (indirect validation)', () => {
   it('MUST have `wrapS` and `wrapT` as one of the allowed values', async () => {
     const options = mockGLTFOptions(mockGLTFWithSampler({ wrapS: 42, wrapT: 42 })) // Invalid values
     await expect(GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)).rejects.toThrowError()
+  })
+
+  it('MAY have undefined `minFilter`', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({}))
+    const texture = await GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)
+    expect(texture.minFilter).toBeDefined() // Default value
+  })
+
+  it('MUST have `minFilter` as an integer when defined', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({ minFilter: 9987.42 })) // Invalid type
+    await expect(GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)).rejects.toThrowError()
+  })
+
+  it('MUST have `minFilter` as one of the allowed values', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({ minFilter: 42 })) // Invalid value
+    await expect(GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)).rejects.toThrowError()
+  })
+
+  it('MAY have undefined `extensions`', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({}))
+    const texture = await GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)
+    expect(texture).toBeDefined()
+  })
+
+  it('MUST have `extensions` as a JSON object when defined', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({ extensions: 42 })) // Invalid type
+    await expect(GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)).rejects.toThrowError()
+  })
+
+  it('MAY have undefined `extras`', async () => {
+    const options = mockGLTFOptions(mockGLTFWithSampler({}))
+    const texture = await GLTFLoaderFunctions.loadTextureImage(options, 0, 0, {} as any)
+    expect(texture).toBeDefined()
   })
 })
