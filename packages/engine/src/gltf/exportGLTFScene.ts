0987d17505d9ef420c8192ceefdfda3eb0f38bae
hzraslan <hzraslan@gmail.com>
2025-05-15 12:13:20 -0500

    remove-log

--- packages/engine/src/gltf/exportGLTFScene.ts
+++ packages/engine/src/gltf/exportGLTFScene.ts
@@ -403,7 +403,6 @@ export async function exportGLTFScene(
     entityPromises: new Map<Entity, Promise<number | undefined>>(),
     cache
   }
-  console.log('Debug:exportGLTFScene', context)
 
   if (exportRoot) {
     context.entityPromises.set(entity, exportEntity(entity, gltf, context))
