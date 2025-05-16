ac50c3d910567e5b0a6d7234efcbf45365ca0926
Adrian <agcacho22@gmail.com>
2025-05-16 12:40:43 -0700

    undo editor changes

--- packages/editor/scripts/start-storybook.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/
-
-
-. ../../.env.local
-
-SSL_KEY="../../$KEY"
-SSL_CERT="../../$CERT"
-
-cross-env storybook dev -p 6006 -h $VITE_APP_HOST --ci --disable-telemetry --https --ssl-key $SSL_KEY --ssl-cert $SSL_CERT
