20d0fa9c37346453cc3e4428ba86dbfb52ca2163
Muhammad Shafiq <shafiqkhuidad@gmail.com>
2025-05-16 19:44:07 +0500

    Fix string quotes in getCountryFromIP function

--- packages/server-core/src/moderation/moderation/ip-geolocation-helper.ts
+++ packages/server-core/src/moderation/moderation/ip-geolocation-helper.ts
@@ -50,7 +50,7 @@ export interface IpInfoResponse {
  */
 export const getCountryFromIP = async (ipAddress: string | undefined): Promise<string | undefined> => {
   if (!ipAddress || ipAddress === '::1' || ipAddress === 'localhost') {
-    return "Local System"
+    return 'Local System'
   }
 
   try {
