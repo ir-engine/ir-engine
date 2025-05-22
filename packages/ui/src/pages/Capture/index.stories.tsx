a0de02c4ee3f38254f31f580d8bde0fae04987d0
Adrian <agcacho22@gmail.com>
2025-05-22 09:29:04 -0700

    cleanup

--- packages/ui/src/pages/Capture/index.stories.tsx
+++ packages/ui/src/pages/Capture/index.stories.tsx
@@ -121,6 +121,7 @@ export default {
   component: Component,
   decorators,
   parameters: {
+    chromatic: { disable: true },
     reactRouter: {
       routePath: '/capture/:locationName',
       routeParams: { locationName: 'default' }
