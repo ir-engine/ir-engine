8c6ec0b9627f3f9d911643e62c3cb6799720eb32
Adrian <agcacho22@gmail.com>
2025-05-22 13:39:35 -0700

    add settings menu

--- packages/ui/src/components/Settings/index.tsx
+++ packages/ui/src/components/Settings/index.tsx
@@ -23,7 +23,7 @@ All portions of the code written by the Infinite Reality Engine team are Copyrig
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'motion/react'
 import React, { useState } from 'react'
 
 // Import icons from the icons module
