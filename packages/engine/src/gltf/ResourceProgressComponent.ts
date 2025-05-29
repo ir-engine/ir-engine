47580c31544adda4a1bdecb51161fc1ae37a45ed
Michael Estes <michael@etherealengine.com>
2025-05-29 11:54:21 -0700

    Typo

--- packages/engine/src/gltf/ResourceProgressComponent.ts
+++ packages/engine/src/gltf/ResourceProgressComponent.ts
@@ -49,7 +49,7 @@ export const ResourceProgressComponent = defineComponent({
   ),
 
   setResource(entity: Entity, url: string, progress: number, total: number) {
-    if (entityExists(entity)) return
+    if (!entityExists(entity)) return
     setComponent(entity, ResourceProgressComponent)
 
     const percentage = total ? Math.floor((progress / total) * 100) : 0
