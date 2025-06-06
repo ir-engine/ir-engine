cee33e064c2366acc3a006d5ad45586471fe2929
Adrian <agcacho22@gmail.com>
2025-06-06 14:18:54 -0700

    undo toolbar change

--- packages/client-core/src/components/Glass/ToolbarAndSidebar.tsx
+++ packages/client-core/src/components/Glass/ToolbarAndSidebar.tsx
@@ -162,15 +162,17 @@ const Header = ({ tabs = [], heading, handleSidebarClose, handleSidebarBack, has
     <div className={headerContainerStyles}>
       <div className={headerInnerStyles}>
         {hasHistory ? <div className={twMerge(buttonContainer_base, backButtonStyles)}>{backButton}</div> : <></>}
-        <div style={{ textShadow: `0 0.025em 0.08em hsla(0, 0%, 0%, 0.2)` }} className={headingsStyles}>
-          {hasHistory ? <div className={headerBackButtonStyles}>{backButton}</div> : <></>}
-          <h2 className={twMerge(`mx-auto font-dm-sans lg:block`, tabs.length ? `hidden` : ``)}>{heading}</h2>
-          {tabs.map((tabProps) => {
-            return <Tab {...tabProps} />
-          })}
+        <div className={twMerge(buttonContainer_base, closeButtonStyles)}>
           <MenuButton className={`text-3xl`} onClick={handleSidebarClose}>
             <XCloseLg />
           </MenuButton>
+        </div>
+        <div style={{ textShadow: `0 0.025em 0.08em hsla(0, 0%, 0%, 0.2)` }} className={headingsStyles}>
+          {hasHistory ? <div className={headerBackButtonStyles}>{backButton}</div> : <></>}
+          <h2 className={twMerge(`lg:block`, tabs.length ? `hidden` : ``)}>{heading}</h2>
+          {tabs.map((tabProps) => {
+            return <Tab {...tabProps} />
+          })}
         </div>
       </div>
     </div>
