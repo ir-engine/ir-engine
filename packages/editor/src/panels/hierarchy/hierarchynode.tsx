edd43ce3a471766a8e65fc24812189da0fa27f0c
hzraslan <hzraslan@gmail.com>
2025-06-09 15:30:43 -0500

    hierachy-indent

--- packages/editor/src/panels/hierarchy/hierarchynode.tsx
+++ packages/editor/src/panels/hierarchy/hierarchynode.tsx
@@ -434,7 +434,7 @@ export default React.memo(function HierarchyTreeNode(props: ListChildComponentPr
             'flex items-center justify-between gap-x-2 bg-inherit pr-2',
             rootEntity === entity ? 'p-2' : 'py-1 pr-2'
           )}
-          style={{ marginLeft: `${node.depth * 0.75}rem` }}
+          style={{ marginLeft: `${node.depth * 1.75}rem` }}
           ref={onDropTarget}
         >
           {node.isLeaf ? (
