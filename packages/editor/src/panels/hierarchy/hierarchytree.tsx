35eb7e4955da3a48cea7bca9496dce0f2b147473
hzraslan <hzraslan@gmail.com>
2025-06-10 09:35:00 -0500

    border-hierarchytree

--- packages/editor/src/panels/hierarchy/hierarchytree.tsx
+++ packages/editor/src/panels/hierarchy/hierarchytree.tsx
@@ -179,7 +179,7 @@ export function Contents() {
   return (
     <div
       ref={ref}
-      className={twMerge('h-5/6 overflow-hidden bg-ui-background', isOver && canDrop && 'border border-dotted')}
+      className={twMerge('h-5/6 overflow-hidden bg-surface-1', isOver && canDrop && 'border border-dotted')}
       data-testid="hierarchy-panel-scene-item-list"
     >
       <FixedSizeList
