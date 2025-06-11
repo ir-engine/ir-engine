6ca42fbdcaa07d8b48763b3ec377957022335d90
HexaField <joshfield999@gmail.com>
2025-06-12 07:08:18 +1000

    revert test

--- packages/engine/src/scene/systems/ShadowSystem.test.tsx
+++ packages/engine/src/scene/systems/ShadowSystem.test.tsx
@@ -1759,7 +1759,7 @@ describe('ShadowSystem', async () => {
       await flushAll()
 
       expect(root.reflection().hasSuspendedOrTimeoutInTree).toBeFalsy()
-      expect(resultSpy).toHaveBeenCalledTimes(entities.length * 2 + 1)
+      expect(resultSpy).toHaveBeenCalledTimes(entities.length * 2)
     })
 
     it('should call DropShadowReactor once for every entity that has the components [VisibleComponent, ShadowComponent] when useShadowsEnabled is falsy and shadowTexture is truthy', async () => {
@@ -1808,7 +1808,7 @@ describe('ShadowSystem', async () => {
       await flushAll()
 
       expect(root.reflection().hasSuspendedOrTimeoutInTree).toBeFalsy()
-      expect(resultSpy).toHaveBeenCalledTimes(entities.length + 1)
+      expect(resultSpy).toHaveBeenCalledTimes(entities.length)
     })
   }) //:: reactor
 }) //:: ShadowSystem
