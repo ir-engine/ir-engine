8c7263cf0fdff9157be88f63cae589f19fafa311
Adrian <agcacho22@gmail.com>
2025-06-11 13:37:18 -0700

    remove log

--- packages/client-core/src/components/Settings/AudioScreen.tsx
+++ packages/client-core/src/components/Settings/AudioScreen.tsx
@@ -1,3 +1,28 @@
+/*
+CPAL-1.0 License
+
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0. (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
+The License is based on the Mozilla Public License Version 1.1, but Sections 14
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
+Exhibit A has been modified to be consistent with Exhibit B.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Infinite Reality Engine.
+
+The Original Developer is the Initial Developer. The Initial Developer of the
+Original Code is the Infinite Reality Engine team.
+
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2025
+Infinite Reality Engine. All Rights Reserved.
+*/
+
 import multiLogger from '@ir-engine/common/src/logger'
 import { AudioState } from '@ir-engine/engine/src/audio/AudioState'
 import { useMutableState } from '@ir-engine/hyperflux'
@@ -31,7 +56,6 @@ export default function AudioScreen() {
           max={1}
           step={0.01}
           onChange={(value: number) => {
-            console.log({ value })
             audioState.masterVolume.set(value)
             logger.analytics({ event_name: `set_total_volume`, event_value: value })
           }}
