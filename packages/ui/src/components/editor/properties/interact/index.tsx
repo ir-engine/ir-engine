8ceec9b7e0262f60364dc42d5a7a7160932a9d24
mike floyd <mbfloyd@theinfinitereality.com>
2025-06-20 12:47:46 -0700

    fixed issues with intractable components

--- packages/ui/src/components/editor/properties/interact/index.tsx
+++ packages/ui/src/components/editor/properties/interact/index.tsx
@@ -139,7 +139,7 @@ export const InteractableComponentNodeEditor: EditorComponentType = (props) => {
       <div id={`callback-list`}>
         {interactableComponent.callbacks.map((callback, index) => {
           const targetOption = callbackQuery.find((o) => o.value === callback.target.value)
-          if (!targetOption) return null
+          //if (!targetOption) return null
           const target = targetOption ? targetOption.value : 'Self'
           return (
             <div key={'callback' + index} className="space-y-2">
