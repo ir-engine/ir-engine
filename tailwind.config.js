c79d71932b753134c983b0e334ce9a1859d3e8eb
Adrian <agcacho22@gmail.com>
2025-06-24 13:10:37 -0700

    fix tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,13 +28,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   mode: 'jit',
   content: [
-    './packages/client/**/*.{ts,tsx}',
-    './packages/client-core/**/*.{ts,tsx}',
-    './packages/common/**/*.{ts,tsx}',
-    './packages/engine/**/*.{ts,tsx}',
-    './packages/editor/**/*.{ts,tsx}',
-    './packages/projects/projects/**/*.{ts,tsx}',
-    './packages/ui/**/*.{ts,tsx}',
+    '../client/**/*.{ts,tsx}',
+    '../client-core/**/*.{ts,tsx}',
+    '../common/**/*.{ts,tsx}',
+    '../engine/**/*.{ts,tsx}',
+    '../editor/**/*.{ts,tsx}',
+    '../projects/projects/**/*.{ts,tsx}',
+    '../ui/**/*.{ts,tsx}',
     '!**/node_modules/**'
   ],
   darkMode: ['class', '[data-theme="dark"]'],
