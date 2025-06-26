e9a71e85374ca177a8d5d8f4780731c2451afc05
hzraslan <hzraslan@gmail.com>
2025-06-26 16:29:09 -0500

    template

--- packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
+++ packages/client-core/src/admin/components/locations/AddEditLocationModal.tsx
@@ -105,8 +105,6 @@ type AddEditLocationModalProps = Readonly<{
   sceneModified?: boolean
   inStudio?: boolean
   projectFullName?: string
-  onCompress?: (newGltfUrl: string) => void
-
   onPublish?: () => Promise<void>
   onPublishSuccess?: (location: LocationType) => void
 }>
@@ -333,7 +331,6 @@ export default function AddEditLocationModal(props: AddEditLocationModalProps) {
 
             setComponent(gltfEntity, NameComponent, fileName + '-compressed')
             setComponent(gltfEntity, GLTFComponent, { src: newGLTFURL })
-            props?.onCompress && fileName !== scenename && props.onCompress(newGLTFURL)
           } catch (error) {
             console.error(error)
           }
