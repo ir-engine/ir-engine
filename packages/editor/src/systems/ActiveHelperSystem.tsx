b857c081caec90019bbb1bc570865d63297552cf
sybiote <ghoshr698@gmail.com>
2025-06-28 05:10:38 +0530

    Enhance visibility management for node icons in WebGLRendererSystem

--- packages/editor/src/systems/ActiveHelperSystem.tsx
+++ packages/editor/src/systems/ActiveHelperSystem.tsx
@@ -51,6 +51,7 @@ import { ObjectComponent } from '@ir-engine/spatial/src/renderer/components/Obje
 import { ObjectLayerMaskComponent } from '@ir-engine/spatial/src/renderer/components/ObjectLayerComponent'
 import { setVisibleComponent, VisibleComponent } from '@ir-engine/spatial/src/renderer/components/VisibleComponent'
 import { ObjectLayerMasks, ObjectLayers } from '@ir-engine/spatial/src/renderer/constants/ObjectLayers'
+import { RendererState } from '@ir-engine/spatial/src/renderer/RendererState'
 import {
   BOUNDING_BOX_COLORS,
   BoundingBoxComponent,
@@ -126,6 +127,8 @@ const ActiveHelperReactor: React.FC<ComponentHelperEntry> = (helper) => {
   const entity = useEntityContext()
   const editorHelperState = useHookstate(getMutableState(EditorHelperState))
   const engineState = useHookstate(getMutableState(EngineState))
+  const engineRendererSettings = useMutableState(RendererState)
+
   const selectedEntities = SelectionState.useSelectedEntities()
   const selected = useHookstate<boolean>(false)
   const lineEntitiesState = useHookstate<Entity[]>([])
@@ -187,8 +190,15 @@ const ActiveHelperReactor: React.FC<ComponentHelperEntry> = (helper) => {
     'icon-helper'
   )
 
+  // manage input state
   const hovered = InputComponent.useHasFocus(studioIconEntity)
+  useEffect(() => {
+    const authoringEntity = getAuthoringCounterpart(entity)
+    const isSelected = selectedEntities.some((e) => e === authoringEntity)
+    selected.set(isSelected)
+  }, [selectedEntities, entity, selected])
 
+  // manage input execution
   const inputExecutionCallback = useCallback(() => {
     if (studioIconEntity === UndefinedEntity || !entityExists(studioIconEntity)) return
     if (entity === UndefinedEntity || !entityExists(entity)) return
@@ -229,9 +239,35 @@ const ActiveHelperReactor: React.FC<ComponentHelperEntry> = (helper) => {
     lineEntitiesState.value,
     selected.value
   ])
-
   InputComponent.useExecuteWithInput(inputExecutionCallback, InputExecutionOrder.Before, true)
 
+  //manage icon visibility
+  useEffect(() => {
+    const setGizmoVisibility = (visible: boolean) => {
+      if (studioIconEntity === UndefinedEntity) return
+
+      const entitiesToUpdate = [studioIconEntity, ...directionalEntitiesState.value, ...lineEntitiesState.value]
+
+      for (const entityToUpdate of entitiesToUpdate) {
+        setVisibleComponent(entityToUpdate, visible)
+      }
+    }
+
+    const shouldBeVisible =
+      engineState.isEditing.value &&
+      editorHelperState.gizmoEnabled.value &&
+      engineRendererSettings.nodeIconVisibility.value
+    setGizmoVisibility(shouldBeVisible)
+  }, [
+    engineState.isEditing.value,
+    editorHelperState.gizmoEnabled.value,
+    engineRendererSettings.nodeIconVisibility.value,
+    studioIconEntity,
+    directionalEntitiesState.value,
+    lineEntitiesState.value
+  ])
+
+  //manage volume visibility
   useEffect(() => {
     if (effectiveHelper?.volume === undefined) return
 
@@ -282,33 +318,6 @@ const ActiveHelperReactor: React.FC<ComponentHelperEntry> = (helper) => {
       }
     }
   }, [selected, hovered, effectiveHelper?.volume, visibility, editorHelperState.volumeVisibility, entity])
-
-  useEffect(() => {
-    const authoringEntity = getAuthoringCounterpart(entity)
-    const isSelected = selectedEntities.some((e) => e === authoringEntity)
-    selected.set(isSelected)
-  }, [selectedEntities, entity, selected])
-
-  useEffect(() => {
-    const setGizmoVisibility = (visible: boolean) => {
-      if (studioIconEntity === UndefinedEntity) return
-
-      const entitiesToUpdate = [studioIconEntity, ...directionalEntitiesState.value, ...lineEntitiesState.value]
-
-      for (const entityToUpdate of entitiesToUpdate) {
-        setVisibleComponent(entityToUpdate, visible)
-      }
-    }
-
-    const shouldBeVisible = engineState.isEditing.value && editorHelperState.gizmoEnabled.value
-    setGizmoVisibility(shouldBeVisible)
-  }, [
-    engineState.isEditing.value,
-    editorHelperState.gizmoEnabled.value,
-    studioIconEntity,
-    directionalEntitiesState.value,
-    lineEntitiesState.value
-  ])
 
   if (!effectiveHelper.reactor) return null
 
