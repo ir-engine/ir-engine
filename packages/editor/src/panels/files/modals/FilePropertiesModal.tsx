5ed92abd8c88f39dcdd3cf266dec2a85a3e35e53
Jose Galván <jgalvan@napster.com>
2025-07-02 14:28:49 -0600

    fix: show size unit in properties menu for selected files

--- packages/editor/src/panels/files/modals/FilePropertiesModal.tsx
+++ packages/editor/src/panels/files/modals/FilePropertiesModal.tsx
@@ -34,6 +34,7 @@ import {
   fileBrowserUploadPath,
   staticResourcePath
 } from '@ir-engine/common/src/schema.type.module'
+import { bytesToSize } from '@ir-engine/common/src/utils/btyesToSize'
 import { NO_PROXY, State, getMutableState, startReactor, useHookstate, useMutableState } from '@ir-engine/hyperflux'
 import { Button, Input } from '@ir-engine/ui'
 import Modal from '@ir-engine/ui/src/primitives/tailwind/Modal'
@@ -63,6 +64,7 @@ export default function FilePropertiesModal() {
   const editedField = useHookstate<string | null>(null)
   const tagInput = useHookstate<string>('')
   const sharedTags = useHookstate<string[]>([])
+  const totalSize = useHookstate<string>('')
 
   let title: string
   let filename: string
@@ -233,6 +235,32 @@ export default function FilePropertiesModal() {
     }
   }
 
+  useEffect(() => {
+    const getTotalSize = () => {
+      return bytesToSize(
+        files
+          .map((file) => {
+            const sizeStr = file.size || '0'
+            const regex = /^([\d.]+)\s*([A-Za-z]+)$/
+            const matches = sizeStr.match(regex)
+
+            if (!matches) return 0
+
+            const value = parseFloat(matches[1])
+            const unit = matches[2]
+
+            const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+            const unitIndex = sizes.findIndex((size) => size.toLowerCase() === unit.toLowerCase())
+            if (unitIndex === -1) return 0
+
+            return value * Math.pow(1024, unitIndex)
+          })
+          .reduce((total, value) => total + value, 0)
+      )
+    }
+    totalSize.set(getTotalSize())
+  }, [files])
+
   return (
     <Modal
       title={title}
@@ -326,7 +354,7 @@ export default function FilePropertiesModal() {
         <div className="grid grid-cols-2 gap-2">
           <Text className="text-end">{t('editor:layout.filebrowser.fileProperties.size')}</Text>
           <Text className="" data-testid="files-panel-file-item-properties-file-size">
-            {files.map((file) => file.size).reduce((total, value) => total + parseInt(value ?? '0'), 0)}
+            {totalSize.value}
           </Text>
         </div>
         {fileStaticResources.length > 0 && (
