f4212c04fdf8efdcdcff3dad61ef07e63c238e92
hzraslan <hzraslan@gmail.com>
2025-07-07 15:56:03 -0500

    clean-up

--- packages/client-core/src/networking/AvatarSpawnSystem.tsx
+++ packages/client-core/src/networking/AvatarSpawnSystem.tsx
@@ -77,6 +77,7 @@ export const AvatarSpawnReactor = (props: { sceneEntity: Entity }) => {
   const userID = useMutableState(EngineState).userID.value
   const { sceneEntity } = props
   const searchParams = useMutableState(SearchParamState)
+
   const spectateEntity = useHookstate(getSearchParamFromURL('spectate') as EntityID)
 
   const settingsQuery = useChildrenWithComponents(sceneEntity, [SceneSettingsComponent])
